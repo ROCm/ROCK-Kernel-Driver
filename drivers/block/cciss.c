ec5637efb4204f20083f7b1ddcf160d72acc2fd3
Jens Axboe <axboe@burns.home.kernel.dk>
2002-02-11 11:40:31 +0100

    change cciss driver version number.  Applies to 2.5.4-pre3 -- steve.cameron@compaq.com

--- drivers/block/cciss.c
+++ drivers/block/cciss.c
@@ -44,12 +44,12 @@
 #include <linux/genhd.h>
 
 #define CCISS_DRIVER_VERSION(maj,min,submin) ((maj<<16)|(min<<8)|(submin))
-#define DRIVER_NAME "Compaq CISS Driver (v 2.4.5)"
-#define DRIVER_VERSION CCISS_DRIVER_VERSION(2,4,5)
+#define DRIVER_NAME "Compaq CISS Driver (v 2.5.0)"
+#define DRIVER_VERSION CCISS_DRIVER_VERSION(2,5,0)
 
 /* Embedded module documentation macros - see modules.h */
 MODULE_AUTHOR("Charles M. White III - Compaq Computer Corporation");
-MODULE_DESCRIPTION("Driver for Compaq Smart Array Controller 5300");
+MODULE_DESCRIPTION("Driver for Compaq Smart Array Controller 5xxx v. 2.5.0");
 MODULE_LICENSE("GPL");
 
 #include "cciss_cmd.h"
