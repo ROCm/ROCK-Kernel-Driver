3f9c56698dc1a119d8f17bbcad1568624f6c09d1
Jean Tourrilhes <jt@hpl.hp.com>
2002-04-03 18:59:13 -0500

    IrDA trivial fixes: o [CORRECT] Handle signals while IrSock is blocked on Tx o [CORRECT] Fix race condition in LAP when receiving with pf bit o [CRITICA] Prevent queuing Tx data before IrComm is ready o [FEATURE] Warn user of common misuse of IrLPT

--- net/irda/af_irda.c
+++ net/irda/af_irda.c
@@ -1286,6 +1286,9 @@ static int irda_sendmsg(struct socket *sock, struct msghdr *msg, int len,
 		/* Check if we are still connected */
 		if (sk->state != TCP_ESTABLISHED)
 			return -ENOTCONN;
+		/* Handle signals */
+		if (signal_pending(current)) 
+			return -ERESTARTSYS;
 	}
 
 	/* Check that we don't send out to big frames */
