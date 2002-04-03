76d2c6be5475db4691a17b8f0c6a9b0ed6b0a60a
Linus Torvalds <torvalds@penguin.transmeta.com>
2002-04-02 22:09:00 -0800

    vmalloc_to_page() should be usable for everybody (see discussion on kernel mailing list)

--- kernel/ksyms.c
+++ kernel/ksyms.c
@@ -107,7 +107,7 @@ EXPORT_SYMBOL(kmalloc);
 EXPORT_SYMBOL(kfree);
 EXPORT_SYMBOL(vfree);
 EXPORT_SYMBOL(__vmalloc);
-EXPORT_SYMBOL_GPL(vmalloc_to_page);
+EXPORT_SYMBOL(vmalloc_to_page);
 EXPORT_SYMBOL(mem_map);
 EXPORT_SYMBOL(remap_page_range);
 EXPORT_SYMBOL(max_mapnr);
