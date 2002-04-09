d686b2229c3ad99e2d3dde92d7343485ffbeae03
David S. Miller <davem@nuts.ninka.net>
2002-04-08 23:15:13 -0700

    Ipv6 build fix: Use named struct initializers for ipip6_fb_tunnel_dev

--- net/ipv6/sit.c
+++ net/ipv6/sit.c
@@ -64,7 +64,8 @@ static int ipip6_fb_tunnel_init(struct net_device *dev);
 static int ipip6_tunnel_init(struct net_device *dev);
 
 static struct net_device ipip6_fb_tunnel_dev = {
-	"sit0", 0x0, 0x0, 0x0, 0x0, 0, 0, 0, 0, 0, NULL, ipip6_fb_tunnel_init,
+	name: "sit0",
+	init: ipip6_fb_tunnel_init
 };
 
 static struct ip_tunnel ipip6_fb_tunnel = {
