6aa116b0fb88e16927e4d6c80049d8993166000c
David Mosberger <davidm@wailua.hpl.hp.com>
2002-04-10 03:27:33 -0700

    setup.c:   Move disabling of 8259 irqs into iosapic.c.

--- arch/ia64/dig/setup.c
+++ arch/ia64/dig/setup.c
@@ -33,7 +33,6 @@
  * is sufficient (the IDE driver will autodetect the drive geometry).
  */
 char drive_info[4*16];
-extern int pcat_compat;
 
 void __init
 dig_setup (char **cmdline_p)
@@ -83,16 +82,4 @@ dig_setup (char **cmdline_p)
 void __init
 dig_irq_init (void)
 {
-	if (pcat_compat) {
-		/*
-		 * Disable the compatibility mode interrupts (8259 style), needs IN/OUT support
-		 * enabled.
-		 */
-		printk("%s: Disabling PC-AT compatible 8259 interrupts\n", __FUNCTION__);
-		outb(0xff, 0xA1);
-		outb(0xff, 0x21);
-	} else {
-		printk("%s: System doesn't have PC-AT compatible dual-8259 setup. "
-		       "Nothing to be done\n", __FUNCTION__);
-	}
 }
