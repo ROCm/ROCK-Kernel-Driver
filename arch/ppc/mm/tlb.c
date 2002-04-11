c7ad40cc2b77557310ceb5a2a7bc000713a80950
Paul Mackerras <paulus@nanango.paulus.ozlabs.org>
2002-04-11 16:49:37 +1000

    Add flush_tlb_kernel_range for PPC and clean up the PPC tlb flushing code a little.

--- arch/ppc/mm/tlb.c
+++ arch/ppc/mm/tlb.c
@@ -41,6 +41,7 @@
  *  - flush_tlb_mm(mm) flushes the specified mm context TLB's
  *  - flush_tlb_page(vma, vmaddr) flushes one page
  *  - flush_tlb_range(vma, start, end) flushes a range of pages
+ *  - flush_tlb_kernel_range(start, end) flushes kernel pages
  *
  * since the hardware hash table functions as an extension of the
  * tlb as far as the linux tables are concerned, flush it too.
@@ -48,87 +49,19 @@
  */
 
 /*
- * Flush all tlb/hash table entries (except perhaps for those
- * mapping RAM starting at PAGE_OFFSET, since they never change).
+ * 750 SMP is a Bad Idea because the 750 doesn't broadcast all
+ * the cache operations on the bus.  Hence we need to use an IPI
+ * to get the other CPU(s) to invalidate their TLBs.
  */
-void
-local_flush_tlb_all(void)
+#ifdef CONFIG_SMP_750
+#define FINISH_FLUSH	smp_send_tlb_invalidate(0)
+#else
+#define FINISH_FLUSH	do { } while (0)
+#endif
+
+static void flush_range(struct mm_struct *mm, unsigned long start,
+			unsigned long end)
 {
-	struct vm_area_struct vma;
-
-	/* aargh!!! */
-	/*
-	 * Just flush the kernel part of the address space, that's
-	 * all that the current callers of this require.
-	 * Eventually I hope to persuade the powers that be that
-	 * we can and should dispense with flush_tlb_all().
-	 *  -- paulus.
-	 */
-	vma.vm_mm = &init_mm;
-	local_flush_tlb_range(&vma, TASK_SIZE, ~0UL);
-
-#ifdef CONFIG_SMP
-	smp_send_tlb_invalidate(0);
-#endif /* CONFIG_SMP */
-}
-
-/*
- * Flush all the (user) entries for the address space described
- * by mm.  We can't rely on mm->mmap describing all the entries
- * that might be in the hash table.
- */
-void
-local_flush_tlb_mm(struct mm_struct *mm)
-{
-	if (Hash == 0) {
-		_tlbia();
-		return;
-	}
-
-	if (mm->map_count) {
-		struct vm_area_struct *mp;
-		for (mp = mm->mmap; mp != NULL; mp = mp->vm_next)
-			local_flush_tlb_range(mp, mp->vm_start, mp->vm_end);
-	} else {
-		struct vm_area_struct vma;
-		vma.vm_mm = mm;
-		local_flush_tlb_range(&vma, 0, TASK_SIZE);
-	}
-
-#ifdef CONFIG_SMP
-	smp_send_tlb_invalidate(0);
-#endif	
-}
-
-void
-local_flush_tlb_page(struct vm_area_struct *vma, unsigned long vmaddr)
-{
-	struct mm_struct *mm;
-	pmd_t *pmd;
-
-	if (Hash == 0) {
-		_tlbie(vmaddr);
-		return;
-	}
-	mm = (vmaddr < TASK_SIZE)? vma->vm_mm: &init_mm;
-	pmd = pmd_offset(pgd_offset(mm, vmaddr), vmaddr);
-	if (!pmd_none(*pmd))
-		flush_hash_pages(mm->context, vmaddr, pmd_val(*pmd), 1);
-#ifdef CONFIG_SMP
-	smp_send_tlb_invalidate(0);
-#endif	
-}
-
-
-/*
- * For each address in the range, find the pte for the address
- * and check _PAGE_HASHPTE bit; if it is set, find and destroy
- * the corresponding HPTE.
- */
-void
-local_flush_tlb_range(struct vm_area_struct *vma, unsigned long start, unsigned long end)
-{
-	struct mm_struct *mm = vma->vm_mm;
 	pmd_t *pmd;
 	unsigned long pmd_end;
 	int count;
@@ -156,8 +89,87 @@ local_flush_tlb_range(struct vm_area_struct *vma, unsigned long start, unsigned 
 		start = pmd_end + 1;
 		++pmd;
 	}
+}
 
-#ifdef CONFIG_SMP
-	smp_send_tlb_invalidate(0);
-#endif	
+/*
+ * Flush all tlb/hash table entries (except perhaps for those
+ * mapping RAM starting at PAGE_OFFSET, since they never change).
+ */
+void
+flush_tlb_all(void)
+{
+	/*
+	 * Just flush the kernel part of the address space, that's
+	 * all that the current callers of this require.
+	 * Eventually I hope to persuade the powers that be that
+	 * we can and should dispense with flush_tlb_all().
+	 *  -- paulus.
+	 *
+	 * In fact this should never get called now that we
+	 * have flush_tlb_kernel_range.  -- paulus
+	 */
+	printk(KERN_ERR "flush_tlb_all called from %p\n",
+	       __builtin_return_address(0));
+	flush_range(&init_mm, TASK_SIZE, ~0UL);
+	FINISH_FLUSH;
+}
+
+/*
+ * Flush kernel TLB entries in the given range
+ */
+void flush_tlb_kernel_range(unsigned long start, unsigned long end)
+{
+	flush_range(&init_mm, start, end);
+	FINISH_FLUSH;
+}
+
+/*
+ * Flush all the (user) entries for the address space described
+ * by mm.  We can't rely on mm->mmap describing all the entries
+ * that might be in the hash table.
+ */
+void flush_tlb_mm(struct mm_struct *mm)
+{
+	if (Hash == 0) {
+		_tlbia();
+		return;
+	}
+
+	if (mm->map_count) {
+		struct vm_area_struct *mp;
+		for (mp = mm->mmap; mp != NULL; mp = mp->vm_next)
+			flush_range(mp->vm_mm, mp->vm_start, mp->vm_end);
+	} else {
+		flush_range(mm, 0, TASK_SIZE);
+	}
+	FINISH_FLUSH;
+}
+
+void flush_tlb_page(struct vm_area_struct *vma, unsigned long vmaddr)
+{
+	struct mm_struct *mm;
+	pmd_t *pmd;
+
+	if (Hash == 0) {
+		_tlbie(vmaddr);
+		return;
+	}
+	mm = (vmaddr < TASK_SIZE)? vma->vm_mm: &init_mm;
+	pmd = pmd_offset(pgd_offset(mm, vmaddr), vmaddr);
+	if (!pmd_none(*pmd))
+		flush_hash_pages(mm->context, vmaddr, pmd_val(*pmd), 1);
+	FINISH_FLUSH;
+}
+
+
+/*
+ * For each address in the range, find the pte for the address
+ * and check _PAGE_HASHPTE bit; if it is set, find and destroy
+ * the corresponding HPTE.
+ */
+void flush_tlb_range(struct vm_area_struct *vma, unsigned long start,
+		     unsigned long end)
+{
+	flush_range(vma->vm_mm, start, end);
+	FINISH_FLUSH;
 }
