d514d18f14fad6e4caf601aeab9b597ff2533e40
Linus Torvalds <torvalds@home.transmeta.com>
2002-04-10 20:28:15 -0700

    Don't stomp on old ia_valid when changing user/group.
    Found by Andrew Morton <akpm@zip.com.au>

--- fs/open.c
+++ fs/open.c
@@ -524,11 +524,11 @@ static int chown_common(struct dentry * dentry, uid_t user, gid_t group)
 		goto out;
 	newattrs.ia_valid =  ATTR_CTIME;
 	if (user != (uid_t) -1) {
-		newattrs.ia_valid =  ATTR_UID;
+		newattrs.ia_valid |= ATTR_UID;
 		newattrs.ia_uid = user;
 	}
 	if (group != (gid_t) -1) {
-		newattrs.ia_valid =  ATTR_GID;
+		newattrs.ia_valid |= ATTR_GID;
 		newattrs.ia_gid = group;
 	}
 	if (!S_ISDIR(inode->i_mode))
