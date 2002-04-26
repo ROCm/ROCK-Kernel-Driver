698788604dad24e648f7264f17816e81cceacb46
Kai Germaschewski <kai@tp1.ruhr-uni-bochum.de>
2002-04-26 03:11:12 -0500

    Move the IPv6 symbols into the ipv6 module
    Currently, we're trying to export the IPv6 modules from netsyms.c,
    which can't work when netsyms is built-in, but IPv6 is modular. Fix is to
    move the IPv6 symbols to the ipv6 module. Actually, netsyms.c could use a
    lot more of this kind of untangling.
    
    Also, move exporting the secure_*v6* symbols to drivers/char/random.c, 
    since that's where they're defined.
    
    The reason why things worked before is that the ipv6 module doesn't have
    a EXPORT_NO_SYMBOLS directive, so _all_ of its symbols where exported by
    modutils.

---
+++ net/ipv6/ipv6_syms.c
@@ -0,0 +1,12 @@
+
+#include <linux/module.h>
+#include <net/ipv6.h>
+#include <net/addrconf.h>
+#include <net/ip6_route.h>
+
+EXPORT_SYMBOL(ipv6_addr_type);
+EXPORT_SYMBOL(icmpv6_send);
+EXPORT_SYMBOL(ndisc_mc_map);
+EXPORT_SYMBOL(register_inet6addr_notifier);
+EXPORT_SYMBOL(unregister_inet6addr_notifier);
+EXPORT_SYMBOL(ip6_route_output);
