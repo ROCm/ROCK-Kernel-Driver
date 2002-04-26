b8272a3618e352303a4afb98a189300dd3a92cfc
Anton Blanchard <anton@samba.org>
2002-04-26 10:40:50 +1000

    ppc64: Remove WANT_PAGE_VIRTUAL now the compiler bug is fixed

--- include/asm-ppc64/page.h
+++ include/asm-ppc64/page.h
@@ -130,13 +130,6 @@ extern void xmon(struct pt_regs *excp);
 
 #define PAGE_BUG(page) do { BUG(); } while (0)
 
-/*
- * XXX A bug in the current ppc64 compiler prevents an optimisation
- * where a divide is replaced by a multiply by shifted inverse. For
- * the moment use page->virtaul
- */
-#define WANT_PAGE_VIRTUAL 1
-
 /* Pure 2^n version of get_order */
 extern __inline__ int get_order(unsigned long size)
 {
