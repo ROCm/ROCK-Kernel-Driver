92a234c2f9668868a5c68e47c998c4d7ea37bbc4
Oliver Neukum <oliver@neukum.name>
2002-05-02 00:31:51 -0700

    [PATCH] tiny race with devfs in printer
    USB printer bugfix
    
    looking through printer.c in preparation for shifting devfs
    support to usbcore I noticed that printer advertises a device
    through devfs before it can be opened.
    As devfs, or more precisely devfsd can be used to trigger actions
    this matters and is wrong.

--- drivers/usb/class/printer.c
+++ drivers/usb/class/printer.c
@@ -871,6 +871,9 @@ static void *usblp_probe(struct usb_device *dev, unsigned int ifnum,
 	usblp_check_status(usblp, 0);
 #endif
 
+	/* add a table entry so the device works when advertised */
+	usblp_table[usblp->minor] = usblp;
+
 	/* If we have devfs, create with perms=660. */
 	sprintf(name, "lp%d", usblp->minor);
 	usblp->devfs = devfs_register(usb_devfs_handle, name,
@@ -886,7 +889,7 @@ static void *usblp_probe(struct usb_device *dev, unsigned int ifnum,
 		usblp->current_protocol, usblp->dev->descriptor.idVendor,
 		usblp->dev->descriptor.idProduct);
 
-	return usblp_table[usblp->minor] = usblp;
+	return usblp;
 
 abort:
 	if (usblp) {
