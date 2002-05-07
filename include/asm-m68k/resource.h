0e20953695fe83963fdf8aacd055feb7af19af4d
Roman Zippel <zippel@linux-m68k.org>
2002-05-06 20:11:48 -0700

    [PATCH] m68k: rlimits update [16/20]
    Update INIT_RLIMITS.

--- include/asm-m68k/resource.h
+++ include/asm-m68k/resource.h
@@ -13,7 +13,7 @@
 #define RLIMIT_RSS	5		/* max resident set size */
 #define RLIMIT_NPROC	6		/* max number of processes */
 #define RLIMIT_NOFILE	7		/* max number of open files */
-#define RLIMIT_MEMLOCK	8		/* max locked-in-memory address space*/
+#define RLIMIT_MEMLOCK	8		/* max locked-in-memory address space */
 #define RLIMIT_AS	9		/* address space limit */
 #define RLIMIT_LOCKS	10		/* maximum file locks held */
 
@@ -27,19 +27,19 @@
 
 #ifdef __KERNEL__
 
-#define INIT_RLIMITS	\
-{                       \
-  {LONG_MAX, LONG_MAX}, \
-  {LONG_MAX, LONG_MAX}, \
-  {LONG_MAX, LONG_MAX}, \
-  {_STK_LIM, LONG_MAX}, \
-  {       0, LONG_MAX}, \
-  {LONG_MAX, LONG_MAX}, \
-  {0, 0},		\
-  {INR_OPEN, INR_OPEN}, \
-  {LONG_MAX, LONG_MAX}, \
-  {LONG_MAX, LONG_MAX}, \
-  {LONG_MAX, LONG_MAX}  \
+#define INIT_RLIMITS					\
+{							\
+	{ RLIM_INFINITY, RLIM_INFINITY },		\
+	{ RLIM_INFINITY, RLIM_INFINITY },		\
+	{ RLIM_INFINITY, RLIM_INFINITY },		\
+	{      _STK_LIM, RLIM_INFINITY },		\
+	{             0, RLIM_INFINITY },		\
+	{ RLIM_INFINITY, RLIM_INFINITY },		\
+	{             0,             0 },		\
+	{      INR_OPEN,     INR_OPEN  },		\
+	{ RLIM_INFINITY, RLIM_INFINITY },		\
+	{ RLIM_INFINITY, RLIM_INFINITY },		\
+        { RLIM_INFINITY, RLIM_INFINITY },		\
 }
 
 #endif /* __KERNEL__ */
