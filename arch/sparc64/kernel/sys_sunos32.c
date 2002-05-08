4999d6e7ba0565a8186b7a4f61c588409d672abe
David S. Miller <davem@nuts.ninka.net>
2002-05-08 12:56:36 -0700

    Sparc: Kill references to buffermem_pages.

--- arch/sparc64/kernel/sys_sunos32.c
+++ arch/sparc64/kernel/sys_sunos32.c
@@ -156,8 +156,7 @@ asmlinkage int sunos_brk(u32 baddr)
 	 * simple, it hopefully works in most obvious cases.. Easy to
 	 * fool it, but this should catch most mistakes.
 	 */
-	freepages = atomic_read(&buffermem_pages) >> PAGE_SHIFT;
-	freepages += get_page_cache_size();
+	freepages = get_page_cache_size();
 	freepages >>= 1;
 	freepages += nr_free_pages();
 	freepages += nr_swap_pages;
