f39747147d47e703764ee330beaae4bf0f4f1057
Anton Blanchard <anton@samba.org>
2002-05-08 11:39:43 +1000

    ppc64: SIGURG fix from Chris Yeoh

--- arch/ppc64/kernel/signal32.c
+++ arch/ppc64/kernel/signal32.c
@@ -1373,7 +1373,7 @@ int do_signal32(sigset_t *oldset, struct pt_regs *regs)
 				continue;
 
 			switch (signr) {
-			case SIGCONT: case SIGCHLD: case SIGWINCH:
+			case SIGCONT: case SIGCHLD: case SIGWINCH: case SIGURG:
 				continue;
 
 			case SIGTSTP: case SIGTTIN: case SIGTTOU:
