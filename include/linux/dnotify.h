4c91e0087a34139fe37530a0d711907248c108ff
Stephen Rothwell <sfr@canb.auug.org.au>
2002-05-08 22:03:28 -0700

    [PATCH] Directory Notification Fix
    This patch fixes directory notification so that notifications get
    cancelled when a process exits.  This make dnotify MUCH more stable and
    usable :-)

--- include/linux/dnotify.h
+++ include/linux/dnotify.h
@@ -1,14 +1,13 @@
 /*
  * Directory notification for Linux
  *
- * Copyright 2000 (C) Stephen Rothwell
+ * Copyright (C) 2000,2002 Stephen Rothwell
  */
 
 #include <linux/fs.h>
 
 struct dnotify_struct {
 	struct dnotify_struct *	dn_next;
-	int			dn_magic;
 	unsigned long		dn_mask;	/* Events to be notified
 						   see linux/fcntl.h */
 	int			dn_fd;
@@ -16,9 +15,8 @@ struct dnotify_struct {
 	fl_owner_t		dn_owner;
 };
 
-#define DNOTIFY_MAGIC	0x444E4F54
-
 extern void __inode_dir_notify(struct inode *, unsigned long);
+extern void dnotify_flush(struct file *filp, fl_owner_t id);
 extern int fcntl_dirnotify(int, struct file *, unsigned long);
 
 static inline void inode_dir_notify(struct inode *inode, unsigned long event)
