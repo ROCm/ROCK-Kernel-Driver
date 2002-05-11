4f37393a37add11cbab6a142aee738d8f8725f3b
Anton Altaparmakov <aia21@cantab.net>
2002-05-11 17:00:08 +0100

    NTFS 2.0.7 release: pure cleanups.

--- fs/ntfs/mft.c
+++ fs/ntfs/mft.c
@@ -25,8 +25,6 @@
 
 #include "ntfs.h"
 
-#define MAX_BUF_PER_PAGE (PAGE_CACHE_SIZE / 512)
-
 /**
  * __format_mft_record - initialize an empty mft record
  * @m:		mapped, pinned and locked for writing mft record
