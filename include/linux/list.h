866559c19b5edb4166a85c41cc2a5a77846ad4f6
Linus Torvalds <torvalds@home.transmeta.com>
2002-06-02 03:43:24 -0700

    Allow <linux/list.h> to be used even without NULL defined yet.

--- include/linux/list.h
+++ include/linux/list.h
@@ -94,8 +94,8 @@ static __inline__ void __list_del(struct list_head * prev,
 static __inline__ void list_del(struct list_head *entry)
 {
 	__list_del(entry->prev, entry->next);
-	entry->next = NULL;
-	entry->prev = NULL;
+	entry->next = (void *) 0;
+	entry->prev = (void *) 0;
 }
 
 /**
