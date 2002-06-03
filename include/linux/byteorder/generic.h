c8e2aa5971bb27e81e3037d78e8b060ffa676d1f
Dan Kegel <dank@kegel.com>
2002-06-02 22:35:05 -0700

    [PATCH] must be __KERNEL__ for byteorder/generic.h
      Here's that patch again (MIME this time, so tabs don't get
      lost by my silly gui mailer); applies cleanly against against 2.4.19-pre8.
      Nobody commented on it last time I posted it, and it does
      make compiling gcc easier, so I guess that makes it trivial patch
      monkey fodder.  Or am I making a silly mistake?

--- include/linux/byteorder/generic.h
+++ include/linux/byteorder/generic.h
@@ -123,6 +123,7 @@
 #endif
 
 
+#if defined(__KERNEL__)
 /*
  * Handle ntohl and suches. These have various compatibility
  * issues - like we want to give the prototype even though we
@@ -146,16 +147,10 @@
  * Do the prototypes. Somebody might want to take the
  * address or some such sick thing..
  */
-#if defined(__KERNEL__) || (defined (__GLIBC__) && __GLIBC__ >= 2)
 extern __u32			ntohl(__u32);
 extern __u32			htonl(__u32);
-#else
-extern unsigned long int	ntohl(unsigned long int);
-extern unsigned long int	htonl(unsigned long int);
-#endif
 extern unsigned short int	ntohs(unsigned short int);
 extern unsigned short int	htons(unsigned short int);
-
 
 #if defined(__GNUC__) && (__GNUC__ >= 2) && defined(__OPTIMIZE__)
 
@@ -164,17 +159,14 @@ extern unsigned short int	htons(unsigned short int);
 #define ___ntohl(x) __be32_to_cpu(x)
 #define ___ntohs(x) __be16_to_cpu(x)
 
-#if defined(__KERNEL__) || (defined (__GLIBC__) && __GLIBC__ >= 2)
 #define htonl(x) ___htonl(x)
 #define ntohl(x) ___ntohl(x)
-#else
-#define htonl(x) ((unsigned long)___htonl(x))
-#define ntohl(x) ((unsigned long)___ntohl(x))
-#endif
 #define htons(x) ___htons(x)
 #define ntohs(x) ___ntohs(x)
 
 #endif /* OPTIMIZE */
+
+#endif /* KERNEL */
 
 
 #endif /* _LINUX_BYTEORDER_GENERIC_H */
