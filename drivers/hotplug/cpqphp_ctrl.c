ed0f878e09f97f972fdd29db0e49d7e28e87cc36
Greg Kroah-Hartman <greg@kroah.com>
2002-06-19 22:18:35 -0700

    [PATCH] drivers/hotplug/cpqphp.h must include tqueue.h

--- drivers/hotplug/cpqphp_ctrl.c
+++ drivers/hotplug/cpqphp_ctrl.c
@@ -31,6 +31,7 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/slab.h>
+#include <linux/tqueue.h>
 #include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <linux/wait.h>
