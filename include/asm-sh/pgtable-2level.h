06fca2f0df3a7b059bbe06a4553063f0ce2e563f
James Mayer <james@cobaltmountain.com>
2002-07-14 23:11:57 -0700

    [PATCH] Typo in linux_include_asm-sh_pgtable-2level.h

--- include/asm-sh/pgtable-2level.h
+++ include/asm-sh/pgtable-2level.h
@@ -42,7 +42,7 @@ static inline void pgd_clear (pgd_t * pgdp) 	{ }
  */
 #define set_pte(pteptr, pteval) (*(pteptr) = pteval)
 /*
- * (pmds are folded into pgds so this doesnt get actually called,
+ * (pmds are folded into pgds so this doesn't get actually called,
  * but the define is needed for a generic inline function.)
  */
 #define set_pmd(pmdptr, pmdval) (*(pmdptr) = pmdval)
