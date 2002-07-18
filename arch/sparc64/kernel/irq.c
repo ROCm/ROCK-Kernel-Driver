0055bcd2355fe7925cb324fb7da62b28c335e0b7
David S. Miller <davem@nuts.ninka.net>
2002-07-18 10:13:19 -0700

    arch/sparc64/kernel/irq.c: Fix preemption bug in __global_cli.

--- arch/sparc64/kernel/irq.c
+++ arch/sparc64/kernel/irq.c
@@ -650,8 +650,9 @@ void __global_cli(void)
 
 	__save_flags(flags);
 	if(flags == 0) {
-		int cpu = smp_processor_id();
+		int cpu;
 		__cli();
+		cpu = smp_processor_id();
 		if (! local_irq_count(cpu))
 			get_irqlock(cpu);
 	}
