390fdff598335e08afc216ff7b759b3142d5a079
David Woodhouse <dwmw2@infradead.org>
2002-07-23 13:13:21 +0100

    Remove licensing noise from individual JFFS2 files to refer to a single 'LICENCE' file, which contains the new licence info. JFFS2 is no longer dual-licensed under RHEPL and GPL; eCos is now GPL'd (with an exception for linking application) and hence so is JFFS2.

--- fs/jffs2/erase.c
+++ fs/jffs2/erase.c
@@ -5,31 +5,7 @@
  *
  * Created by David Woodhouse <dwmw2@cambridge.redhat.com>
  *
- * The original JFFS, from which the design for JFFS2 was derived,
- * was designed and implemented by Axis Communications AB.
- *
- * The contents of this file are subject to the Red Hat eCos Public
- * License Version 1.1 (the "Licence"); you may not use this file
- * except in compliance with the Licence.  You may obtain a copy of
- * the Licence at http://www.redhat.com/
- *
- * Software distributed under the Licence is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing rights and
- * limitations under the Licence.
- *
- * The Original Code is JFFS2 - Journalling Flash File System, version 2
- *
- * Alternatively, the contents of this file may be used under the
- * terms of the GNU General Public License version 2 (the "GPL"), in
- * which case the provisions of the GPL are applicable instead of the
- * above.  If you wish to allow the use of your version of this file
- * only under the terms of the GPL and not to allow others to use your
- * version of this file under the RHEPL, indicate your decision by
- * deleting the provisions above and replace them with the notice and
- * other provisions required by the GPL.  If you do not delete the
- * provisions above, a recipient may use your version of this file
- * under either the RHEPL or the GPL.
+ * For licensing information, see the file 'LICENCE' in this directory.
  *
  * $Id: erase.c,v 1.35 2002/03/08 15:11:24 dwmw2 Exp $
  *
