edcc875fae3cd4f20a5a3b9ba33a90ad9c0cb11d
Dan Aloni <da-x@gmx.net>
2002-07-26 22:23:54 +0300

    i385 mm cleanup:   + cleanup init.c and split into pgtable.c   + split declaration of _text, _etext outside into sections.h

---
+++ include/asm-generic/sections.h
@@ -0,0 +1,11 @@
+#ifndef _ASM_GENERIC_SECTIONS_H_
+#define _ASM_GENERIC_SECTIONS_H_
+
+/* References to section boundaries */
+
+extern char _text, _etext;
+extern char _data, _edata;
+extern char __bss_start;
+extern char __init_begin, __init_end;
+
+#endif /* _ASM_GENERIC_SECTIONS_H_ */
