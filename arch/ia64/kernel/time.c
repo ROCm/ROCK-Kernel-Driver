afaeab62d2f997e06a9d8d56dca897896921307a
David Mosberger <davidm@wailua.hpl.hp.com>
2002-07-25 19:23:00 -0700

    ia64: Fix do_profile() to account kernel ticks spent on behalf of pid 0.  Clean 	up arch/ia64/config.in (based on patch by Keith Owens).

--- arch/ia64/kernel/time.c
+++ arch/ia64/kernel/time.c
@@ -39,21 +39,22 @@ do_profile (unsigned long ip)
 	extern unsigned long prof_cpu_mask;
 	extern char _stext;
 
+	if (!prof_buffer)
+		return;
+
 	if (!((1UL << smp_processor_id()) & prof_cpu_mask))
 		return;
 
-	if (prof_buffer && current->pid) {
-		ip -= (unsigned long) &_stext;
-		ip >>= prof_shift;
-		/*
-		 * Don't ignore out-of-bounds IP values silently, put them into the last
-		 * histogram slot, so if present, they will show up as a sharp peak.
-		 */
-		if (ip > prof_len - 1)
-			ip = prof_len - 1;
+	ip -= (unsigned long) &_stext;
+	ip >>= prof_shift;
+	/*
+	 * Don't ignore out-of-bounds IP values silently, put them into the last
+	 * histogram slot, so if present, they will show up as a sharp peak.
+	 */
+	if (ip > prof_len - 1)
+		ip = prof_len - 1;
 
-		atomic_inc((atomic_t *) &prof_buffer[ip]);
-	}
+	atomic_inc((atomic_t *) &prof_buffer[ip]);
 }
 
 /*
