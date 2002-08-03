09188a07e898416583785b27fb6deedfca1af975
Russell King <rmk@arm.linux.org.uk>
2002-08-03 04:36:37 -0700

    [PATCH] 11: 2.5.29-8390
    Unfortunately, someone changed the inb and friends definitions and
    broke ARM.  This patch allows ARM to build NE2000 based drivers
    again.

--- drivers/net/8390.h
+++ drivers/net/8390.h
@@ -111,8 +111,7 @@ struct ei_device {
  
 #if defined(CONFIG_MAC) ||  \
     defined(CONFIG_ARIADNE2) || defined(CONFIG_ARIADNE2_MODULE) || \
-    defined(CONFIG_HYDRA) || defined(CONFIG_HYDRA_MODULE) || \
-    defined(CONFIG_ARM_ETHERH) || defined(CONFIG_ARM_ETHERH_MODULE)
+    defined(CONFIG_HYDRA) || defined(CONFIG_HYDRA_MODULE)
 #define EI_SHIFT(x)	(ei_local->reg_offset[x])
 #undef inb
 #undef inb_p
@@ -124,6 +123,8 @@ struct ei_device {
 #define inb_p(port)   in_8(port)
 #define outb_p(val,port)  out_8(port,val)
 
+#elif defined(CONFIG_ARM_ETHERH) || defined(CONFIG_ARM_ETHERH_MODULE)
+#define EI_SHIFT(x)	(ei_local->reg_offset[x])
 #else
 #define EI_SHIFT(x)	(x)
 #endif
