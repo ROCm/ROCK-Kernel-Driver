d741b43179dd5d87b40b6b8e279b85b8a221ed30
Jamey Hicks <jamey.hicks@hp.com>
2002-08-03 01:50:55 +0100

    [ARM PATCH] 1221/1: add irq definitions for H3800, which has interrupt controller in asic This adds the definitions for the IRQs coming from the H3800 asic.

--- include/asm-arm/arch-sa1100/irqs.h
+++ include/asm-arm/arch-sa1100/irqs.h
@@ -133,6 +133,8 @@
 #elif defined(CONFIG_SA1100_GRAPHICSCLIENT) || \
       defined(CONFIG_SA1100_GRAPHICSMASTER)
 #define NR_IRQS			(IRQ_BOARD_END)
+#elif defined(CONFIG_SA1100_H3800)
+#define NR_IRQS                 (IRQ_BOARD_END)
 #else
 #define NR_IRQS			(IRQ_BOARD_START)
 #endif
@@ -148,3 +150,24 @@
 /* PT Digital Board Interrupts (CONFIG_SA1100_PT_SYSTEM3) */
 #define IRQ_SYSTEM3_SA1111	(IRQ_BOARD_START + 0)
 #define IRQ_SYSTEM3_SMC9196	(IRQ_BOARD_START + 1)
+
+/* H3800-specific IRQs (CONFIG_SA1100_H3800) */
+#define H3800_KPIO_IRQ_START    (IRQ_BOARD_START)
+#define IRQ_H3800_KEY           (IRQ_BOARD_START + 0)
+#define IRQ_H3800_SPI           (IRQ_BOARD_START + 1)
+#define IRQ_H3800_OWM           (IRQ_BOARD_START + 2)
+#define IRQ_H3800_ADC           (IRQ_BOARD_START + 3)
+#define IRQ_H3800_UART_0        (IRQ_BOARD_START + 4)
+#define IRQ_H3800_UART_1        (IRQ_BOARD_START + 5)
+#define IRQ_H3800_TIMER_0       (IRQ_BOARD_START + 6)
+#define IRQ_H3800_TIMER_1       (IRQ_BOARD_START + 7)
+#define IRQ_H3800_TIMER_2       (IRQ_BOARD_START + 8)
+#define H3800_KPIO_IRQ_COUNT    9
+
+#define H3800_GPIO_IRQ_START    (IRQ_BOARD_START + 9)
+#define IRQ_H3800_PEN           (IRQ_BOARD_START + 9)
+#define IRQ_H3800_SD_DETECT     (IRQ_BOARD_START + 10)
+#define IRQ_H3800_EAR_IN        (IRQ_BOARD_START + 11)
+#define IRQ_H3800_USB_DETECT    (IRQ_BOARD_START + 12)
+#define IRQ_H3800_SD_CON_SLT    (IRQ_BOARD_START + 13)
+#define H3800_GPIO_IRQ_COUNT    5
