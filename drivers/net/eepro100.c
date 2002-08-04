d865fb545eeb3327b0d692f5366ae51d033f2fe2
Pavel Machek <pavel@ucw.cz>
2002-08-04 04:34:11 -0400

    Remove unnecessary prototypes in eepro100 net driver

--- drivers/net/eepro100.c
+++ drivers/net/eepro100.c
@@ -530,10 +530,6 @@ static int eepro100_init_one(struct pci_dev *pdev,
 		const struct pci_device_id *ent);
 
 static void eepro100_remove_one (struct pci_dev *pdev);
-#ifdef CONFIG_PM
-static int eepro100_suspend (struct pci_dev *pdev, u32 state);
-static int eepro100_resume (struct pci_dev *pdev);
-#endif
 
 static int do_eeprom_cmd(long ioaddr, int cmd, int cmd_len);
 static int mdio_read(long ioaddr, int phy_id, int location);
