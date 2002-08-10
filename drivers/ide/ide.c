b84e52f829876090f63c4aa8a0025a4996c54ad5
Paul Menage <pmenage@ensim.com>
2002-08-10 02:28:51 -0700

    [PATCH] Fix misspelling of "sector" in ide.c

--- drivers/ide/ide.c
+++ drivers/ide/ide.c
@@ -251,7 +251,7 @@ static struct ata_bit_messages ata_status_msgs[] = {
 
 static struct ata_bit_messages ata_error_msgs[] = {
 	{ ICRC_ERR|ABRT_ERR,	ABRT_ERR,		"drive status error"	},
-	{ ICRC_ERR|ABRT_ERR,	ICRC_ERR,		"bad sectorr"		},
+	{ ICRC_ERR|ABRT_ERR,	ICRC_ERR,		"bad sector"		},
 	{ ICRC_ERR|ABRT_ERR,	ICRC_ERR|ABRT_ERR,	"invalid checksum"	},
 	{ ECC_ERR,		ECC_ERR,		"uncorrectable error"	},
 	{ ID_ERR,		ID_ERR,			"sector id not found"   },
