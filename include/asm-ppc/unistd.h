5d7e01f8f7af2900e089be28054adb56614a9d13
Paul Mackerras <paulus@samba.org>
2002-08-28 16:21:09 +1000

    PPC32: add the bits needed for AIO and sendfile64 support.
    This is extra syscall entries + KM_IRQ0/1.

--- include/asm-ppc/unistd.h
+++ include/asm-ppc/unistd.h
@@ -233,6 +233,12 @@
 #define __NR_sched_getaffinity	223
 #define __NR_security		224
 #define __NR_tuxcall		225
+#define __NR_sendfile64		226
+#define __NR_io_setup		227
+#define __NR_io_destroy		228
+#define __NR_io_getevents	229
+#define __NR_io_submit		230
+#define __NR_io_cancel		231
 
 #define __NR(n)	#n
 
