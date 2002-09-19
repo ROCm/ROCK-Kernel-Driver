ac221462e28a685a0e39c53ca5d690ea08fd4e95
Anton Blanchard <anton@samba.org>
2002-09-19 11:13:05 +1000

    ppc64: remove some old syscalls from the 64bit syscall table

--- arch/ppc64/kernel/syscalls.c
+++ arch/ppc64/kernel/syscalls.c
@@ -218,32 +218,6 @@ asmlinkage int sys_uname(struct old_utsname * name)
 	return err;
 }
 
-asmlinkage int sys_olduname(struct oldold_utsname * name)
-{
-	int error;
-	
-	if (!name)
-		return -EFAULT;
-	if (!access_ok(VERIFY_WRITE,name,sizeof(struct oldold_utsname)))
-		return -EFAULT;
-  
-	down_read(&uts_sem);
-	error = __copy_to_user(&name->sysname,&system_utsname.sysname,__OLD_UTS_LEN);
-	error -= __put_user(0,name->sysname+__OLD_UTS_LEN);
-	error -= __copy_to_user(&name->nodename,&system_utsname.nodename,__OLD_UTS_LEN);
-	error -= __put_user(0,name->nodename+__OLD_UTS_LEN);
-	error -= __copy_to_user(&name->release,&system_utsname.release,__OLD_UTS_LEN);
-	error -= __put_user(0,name->release+__OLD_UTS_LEN);
-	error -= __copy_to_user(&name->version,&system_utsname.version,__OLD_UTS_LEN);
-	error -= __put_user(0,name->version+__OLD_UTS_LEN);
-	error -= __copy_to_user(&name->machine,&system_utsname.machine,__OLD_UTS_LEN);
-	error = __put_user(0,name->machine+__OLD_UTS_LEN);
-	up_read(&uts_sem);
-
-	error = error ? -EFAULT : 0;
-	
-	return error;
-}
 
 asmlinkage time_t sys64_time(time_t* tloc)
 {
