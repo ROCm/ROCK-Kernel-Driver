8b755139164c05fcff5e56617d2fa9886fea1825
Patrick Mochel <mochel@osdl.org>
2002-09-23 01:19:20 -0700

    Driver model: improve support for system devices.
    - Create struct sys_device to describe system-level devices (CPUs, PICs, etc.). This 
      structure includes a 'name' and 'id' field for drivers to fill in with a simple
      canonical name (like 'pic' or 'floppy') and the id of the device relative to its 
      discovery in the system (it's enumerated value).
    
      The core then constructs the bus_id for the device from these, giving them meaningful
      names when exporting them to userspace:
    
    # tree -d /sys/root/sys/
    /sys/root/sys/
    |-- pic0
    `-- rtc0
    
    - Replace 
    	int register_sys_device(struct device * dev);
    	with 
    	int sys_device_register(struct sys_device * sysdev);
    
    - Fixup the users of the API.
    
    - Add a system_bus_type for devices to associate themselves with. This provides a 
      bus/system/ directory in driverfs that looks like:
    
    # tree -d /sys/bus/system/
    /sys/bus/system/
    |-- devices
    |   |-- pic0 -> ../../../root/sys/pic0
    |   `-- rtc0 -> ../../../root/sys/rtc0
    `-- drivers
        `-- pic

--- drivers/base/sys.c
+++ drivers/base/sys.c
@@ -6,44 +6,78 @@
  * 
  * This exports a 'system' bus type. 
  * By default, a 'sys' bus gets added to the root of the system. There will
- * always be core system devices. Devices can use register_sys_device() to
+ * always be core system devices. Devices can use sys_device_register() to
  * add themselves as children of the system bus.
  */
 
 #include <linux/device.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/slab.h>
-#include <linux/errno.h>
+#include <linux/err.h>
 
+/* The default system device parent. */
 static struct device system_bus = {
        .name           = "System Bus",
        .bus_id         = "sys",
 };
 
-int register_sys_device(struct device * dev)
+/**
+ *	sys_device_register - add a system device to the tree
+ *	@sysdev:	device in question
+ *
+ *	The hardest part about this is getting the ancestry right.
+ *	If the device has a parent - super! We do nothing.
+ *	If the device doesn't, but @dev->root is set, then we're
+ *	dealing with a NUMA like architecture where each root
+ *	has a system pseudo-bus to foster the device.
+ *	If not, then we fallback to system_bus (at the top of 
+ *	this file). 
+ *
+ *	One way or another, we call device_register() on it and 
+ *	are done.
+ *
+ *	The caller is also responsible for initializing the bus_id 
+ *	and name fields of @sysdev->dev.
+ */
+int sys_device_register(struct sys_device * sysdev)
 {
-       int error = -EINVAL;
+	if (!sysdev)
+		return -EINVAL;
 
-       if (dev) {
-               if (!dev->parent)
-                       dev->parent = &system_bus;
-               error = device_register(dev);
-       }
-       return error;
+	if (!sysdev->dev.parent)
+		sysdev->dev.parent = &system_bus;
+
+	/* make sure bus type is set */
+	if (!sysdev->dev.bus)
+		sysdev->dev.bus = &system_bus_type;
+
+	/* construct bus_id */
+	snprintf(sysdev->dev.bus_id,BUS_ID_SIZE,"%s%u",sysdev->name,sysdev->id);
+
+	pr_debug("Registering system device %s\n", sysdev->dev.bus_id);
+
+	return device_register(&sysdev->dev);
 }
 
-void unregister_sys_device(struct device * dev)
+void sys_device_unregister(struct sys_device * sysdev)
 {
-       if (dev)
-               put_device(dev);
+	if (sysdev)
+		put_device(&sysdev->dev);
 }
+
+struct bus_type system_bus_type = {
+	.name		= "system",
+};
 
 static int sys_bus_init(void)
 {
-       return device_register(&system_bus);
+	bus_register(&system_bus_type);
+	return device_register(&system_bus);
 }
 
 postcore_initcall(sys_bus_init);
-EXPORT_SYMBOL(register_sys_device);
-EXPORT_SYMBOL(unregister_sys_device);
+EXPORT_SYMBOL(system_bus_type);
+EXPORT_SYMBOL(sys_device_register);
+EXPORT_SYMBOL(sys_device_unregister);
