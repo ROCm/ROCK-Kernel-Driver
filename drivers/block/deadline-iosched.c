33fb2ab558d9463ee3f1b22a07117cd49d3a40b2
Jens Axboe <axboe@suse.de>
2002-09-26 18:39:15 -0700

    [PATCH] io scheduler update
    This fixes a problem with the deadline io scheduler, if the correct
    insertion point is at the front of the list.  This is something that we
    never have gotten right in 2.4 either.
    
    The problem is that the elevator merge function has to return a pointer
    to a struct request, and for front insert we really have to return the
    head of the list which cannot be expressed as a request of course.
    
    The real issue is that the elevator_merge function actually performs two
    functions - it scans for a merge, and if it can't find any, it selects
    and insertion point.  It's done this way for efficiency reasons, even if
    the design isn't all that clean.
    
    So we change the io scheduler merge functions to get passed a pointer to
    a list_head pointer instead.  This works for both inserts and merges.
    In addition, deadline checks if it really should insert at the very
    front.
    
    Also don't pass in request to elv_try_last_merge(), the very name of the
    function suggests that it's q->last_merge that we are interested in.

--- drivers/block/deadline-iosched.c
+++ drivers/block/deadline-iosched.c
@@ -138,7 +138,7 @@ deadline_find_hash(struct deadline_data *dd, sector_t offset)
 }
 
 static int
-deadline_merge(request_queue_t *q, struct request **req, struct bio *bio)
+deadline_merge(request_queue_t *q, struct list_head **insert, struct bio *bio)
 {
 	struct deadline_data *dd = q->elevator.elevator_data;
 	const int data_dir = bio_data_dir(bio);
@@ -150,9 +150,11 @@ deadline_merge(request_queue_t *q, struct request **req, struct bio *bio)
 	/*
 	 * try last_merge to avoid going to hash
 	 */
-	ret = elv_try_last_merge(q, req, bio);
-	if (ret != ELEVATOR_NO_MERGE)
+	ret = elv_try_last_merge(q, bio);
+	if (ret != ELEVATOR_NO_MERGE) {
+		*insert = q->last_merge;
 		goto out;
+	}
 
 	/*
 	 * see if the merge hash can satisfy a back merge
@@ -161,12 +163,15 @@ deadline_merge(request_queue_t *q, struct request **req, struct bio *bio)
 		BUG_ON(__rq->sector + __rq->nr_sectors != bio->bi_sector);
 
 		if (elv_rq_merge_ok(__rq, bio)) {
-			*req = __rq;
+			*insert = &__rq->queuelist;
 			ret = ELEVATOR_BACK_MERGE;
 			goto out;
 		}
 	}
 
+	/*
+	 * scan list from back to find insertion point.
+	 */
 	entry = sort_list = &dd->sort_list[data_dir];
 	while ((entry = entry->prev) != sort_list) {
 		__rq = list_entry_rq(entry);
@@ -177,8 +182,8 @@ deadline_merge(request_queue_t *q, struct request **req, struct bio *bio)
 		if (!(__rq->flags & REQ_CMD))
 			continue;
 
-		if (!*req && bio_rq_in_between(bio, __rq, sort_list))
-			*req = __rq;
+		if (!*insert && bio_rq_in_between(bio, __rq, sort_list))
+			*insert = &__rq->queuelist;
 
 		if (__rq->flags & REQ_BARRIER)
 			break;
@@ -189,10 +194,19 @@ deadline_merge(request_queue_t *q, struct request **req, struct bio *bio)
 		if (__rq->sector - bio_sectors(bio) == bio->bi_sector) {
 			ret = elv_try_merge(__rq, bio);
 			if (ret != ELEVATOR_NO_MERGE) {
-				*req = __rq;
+				*insert = &__rq->queuelist;
 				break;
 			}
 		}
+	}
+
+	/*
+	 * no insertion point found, check the very front
+	 */
+	if (!*insert && !list_empty(sort_list)) {
+		__rq = list_entry_rq(sort_list->next);
+		if (bio->bi_sector + bio_sectors(bio) < __rq->sector)
+			*insert = sort_list;
 	}
 
 out:
