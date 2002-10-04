09212816ad75542db6f3b5a9f6e857dc433b46b8
Martin Schwidefsky <schwidefsky@de.ibm.com>
2002-10-03 21:49:59 -0700

    [PATCH] s390 update (22/27): s390_process_IRQ.
    Cleanup s390_process_IRQ a little, the ending_status argument is never
    really used.

--- drivers/s390/cio/cio.c
+++ drivers/s390/cio/cio.c
@@ -1065,9 +1065,9 @@ s390_reset_flags_after_ending_status(int irq)
  * for cc=0 and cc=1 after tsch
  */
 static inline int
-s390_process_IRQ_normal(unsigned int irq,
-			int ending_status)
+s390_process_IRQ_normal(unsigned int irq)
 {
+	int ending_status;
 	unsigned int fctl;	/* function control */
 	unsigned int stctl;	/* status   control */
 	unsigned int actl;	/* activity control */
@@ -1315,8 +1315,7 @@ s390_process_IRQ_normal(unsigned int irq,
  * for cc=3 after tsch
  */
 static inline int
-s390_process_IRQ_notoper(unsigned int irq,
-			 int ending_status)
+s390_process_IRQ_notoper(unsigned int irq)
 {
 	devstat_t *dp;
 	devstat_t *udp;
@@ -1379,7 +1378,7 @@ s390_process_IRQ_notoper(unsigned int irq,
 	 * take fast exit if no handler is available
 	 */
 	if (!ioinfo[irq]->ui.flags.ready)
-		return (ending_status);
+		return 0;
 	
 	memcpy (udp, &(ioinfo[irq]->devstat), 
 		sizeof(devstat_t) - 
@@ -1417,7 +1416,6 @@ s390_process_IRQ (unsigned int irq)
 	int irb_cc;		/* cond code from irb */
 
 	int issense = 0;
-	int ending_status = 0;
 	devstat_t *dp;
 	devstat_t *udp;
 	scsw_t *scsw;
@@ -1610,25 +1608,14 @@ s390_process_IRQ (unsigned int irq)
 
 	switch (irb_cc) {
 	case 1:		/* status pending */
-
 		dp->flag |= DEVSTAT_STATUS_PENDING;
 
 	case 0:		/* normal i/o interruption */
+		return s390_process_IRQ_normal(irq);
 
-		ending_status = s390_process_IRQ_normal(irq, ending_status);
-
-
-		break;
-
-	case 3:		/* device/path not operational */
-
-		ending_status = s390_process_IRQ_notoper(irq, ending_status); 
-
-		break;
-
+	default:	/* device/path not operational */
+		return s390_process_IRQ_notoper(irq);
 	}
-
-	return (ending_status);
 }
 
 /*
