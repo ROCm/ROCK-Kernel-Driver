1fbcea01f6dbf2ce07741776f61d0925ff2927c6
Alan Cox <alan@lxorguk.ukuu.org.uk>
2002-10-08 00:57:47 -0700

    [PATCH] first pass at seagate st-02 for 2.5

--- drivers/scsi/seagate.h
+++ drivers/scsi/seagate.h
@@ -7,30 +7,30 @@
  */
 
 #ifndef _SEAGATE_H
-	#define SEAGATE_H
-/*
-	$Header
-*/
-#ifndef ASM
-int seagate_st0x_detect(Scsi_Host_Template *);
-int seagate_st0x_command(Scsi_Cmnd *);
-int seagate_st0x_queue_command(Scsi_Cmnd *, void (*done)(Scsi_Cmnd *));
+#define SEAGATE_H
+
+static int seagate_st0x_detect(Scsi_Host_Template *);
+static int seagate_st0x_command(Scsi_Cmnd *);
+static int seagate_st0x_queue_command(Scsi_Cmnd *, void (*done)(Scsi_Cmnd *));
 
 static int seagate_st0x_abort(Scsi_Cmnd *);
-const char *seagate_st0x_info(struct Scsi_Host *);
-static int seagate_st0x_reset(Scsi_Cmnd *, unsigned int); 
+static const char *seagate_st0x_info(struct Scsi_Host *);
+static int seagate_st0x_bus_reset(Scsi_Cmnd *);
+static int seagate_st0x_device_reset(Scsi_Cmnd *);
+static int seagate_st0x_host_reset(Scsi_Cmnd *);
 
-#define SEAGATE_ST0X  {  detect:         seagate_st0x_detect,		\
-			 info:           seagate_st0x_info,		\
-			 command:        seagate_st0x_command,		\
-			 queuecommand:   seagate_st0x_queue_command,	\
-			 abort:          seagate_st0x_abort,		\
-			 reset:          seagate_st0x_reset,		\
-			 can_queue:      1,				\
-			 this_id:        7,				\
-			 sg_tablesize:   SG_ALL,			\
-			 cmd_per_lun:    1,				\
+#define SEAGATE_ST0X  {  detect:         seagate_st0x_detect,			\
+			 info:           seagate_st0x_info,			\
+			 command:        seagate_st0x_command,			\
+			 queuecommand:   seagate_st0x_queue_command,		\
+			 eh_abort_handler:	seagate_st0x_abort,		\
+			 eh_bus_reset_handler:  seagate_st0x_bus_reset,		\
+			 eh_host_reset_handler: seagate_st0x_host_reset,	\
+			 eh_device_reset_handler:seagate_st0x_device_reset,	\
+			 can_queue:      1,					\
+			 this_id:        7,					\
+			 sg_tablesize:   SG_ALL,				\
+			 cmd_per_lun:    1,					\
 			 use_clustering: DISABLE_CLUSTERING}
-#endif /* ASM */
 
 #endif /* _SEAGATE_H */
