e37f880c8ef1af7c38facb6b84c2934422965684
Dave Jones <davej@codemonkey.org.uk>
2002-10-07 19:25:33 -0700

    [PATCH] unify slab namespace.
    Other slabs have '_' to separate words instead of ' '.
    Same patch went into 2.4

--- fs/dnotify.c
+++ fs/dnotify.c
@@ -144,7 +144,7 @@ void __inode_dir_notify(struct inode *inode, unsigned long event)
 
 static int __init dnotify_init(void)
 {
-	dn_cache = kmem_cache_create("dnotify cache",
+	dn_cache = kmem_cache_create("dnotify_cache",
 		sizeof(struct dnotify_struct), 0, 0, NULL, NULL);
 	if (!dn_cache)
 		panic("cannot create dnotify slab cache");
