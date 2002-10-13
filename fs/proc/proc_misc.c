75a08275da11f7ab4573a83754b1459976f2d08b
Andrew Morton <akpm@digeo.com>
2002-10-12 19:32:40 -0700

    [PATCH] /proc/meminfo alterations for hugetlbpages
    The patch from Rohit and David M-T changes the hugetlb page info in
    /proc/meminfo slightly.
    
    It makes the identifiers a little clearer while ensuring that we don't
    add any identifiers which have whitespace.  glibc is/shall be parsing
    this information to determine the size and alignment requirements of
    the hugetlb pages.
    
    This basically means that procfs is a requirement for successful
    hugetlb page usage.  Not very nice, but I suspect real-world userspace
    fails without procfs anyway.

--- fs/proc/proc_misc.c
+++ fs/proc/proc_misc.c
@@ -202,9 +202,9 @@ static int meminfo_read_proc(char *page, char **start, off_t off,
 	{
 		extern unsigned long htlbpagemem, htlbzone_pages;
 		len += sprintf(page + len,
-				"HugePages:    %8lu\n"
-				"Available:    %8lu\n"
-				"Size:         %8lu kB\n",
+				"HugePages_Total: %5lu\n"
+				"HugePages_Free:  %5lu\n"
+				"Hugepagesize:    %5lu kB\n",
 				htlbzone_pages,
 				htlbpagemem,
 				HPAGE_SIZE/1024);
