8867e01d412c4da2145b95aa2667fd3a9e9cc4ed
Marcel Holtmann <marcel@holtmann.org>
2002-10-27 15:44:11 +0100

    [Bluetooth] Fix operator precedence for modem status
    This patch puts the needed parantheses around the modem status to
    make it work correctly.

--- net/bluetooth/rfcomm/tty.c
+++ net/bluetooth/rfcomm/tty.c
@@ -500,10 +500,10 @@ static void rfcomm_dev_modem_status(struct rfcomm_dlc *dlc, u8 v24_sig)
 	BT_DBG("dlc %p dev %p v24_sig 0x%02x", dlc, dev, v24_sig);
 
 	dev->modem_status = 
-		(v24_sig & RFCOMM_V24_RTC) ? (TIOCM_DSR | TIOCM_DTR) : 0 |
-		(v24_sig & RFCOMM_V24_RTR) ? (TIOCM_RTS | TIOCM_CTS) : 0 |
-		(v24_sig & RFCOMM_V24_IC)  ? TIOCM_RI : 0 |
-		(v24_sig & RFCOMM_V24_DV)  ? TIOCM_CD : 0;
+		((v24_sig & RFCOMM_V24_RTC) ? (TIOCM_DSR | TIOCM_DTR) : 0) |
+		((v24_sig & RFCOMM_V24_RTR) ? (TIOCM_RTS | TIOCM_CTS) : 0) |
+		((v24_sig & RFCOMM_V24_IC)  ? TIOCM_RI : 0) |
+		((v24_sig & RFCOMM_V24_DV)  ? TIOCM_CD : 0);
 }
 
 /* ---- TTY functions ---- */
