736c7efc7075a7b1a3ee31895af7347278d345e8
Patrick Mochel <mochel@osdl.org>
2002-10-29 01:19:51 -0800

    introduce struct kobject: simple, generic object for embedding in other structures.
    This is not meant to be fancy; just something simple for which we can 
    control the refcount and other common functionality using common code. 
       
    The basic operations for registration and reference count manipulation are
    included.

---
+++ include/linux/kobject.h
@@ -0,0 +1,29 @@
+/*
+ * kobject.h - generic kernel object infrastructure.
+ *
+ */
+
+#ifndef _KOBJECT_H_
+#define _KOBJECT_H_
+
+#include <linux/types.h>
+#include <linux/list.h>
+#include <linux/sysfs.h>
+#include <asm/atomic.h>
+
+struct kobject {
+	char			name[16];
+	atomic_t		refcount;
+	struct list_head	entry;
+	struct kobject		* parent;
+};
+
+extern void kobject_init(struct kobject *);
+
+extern int kobject_register(struct kobject *);
+extern void kobject_unregister(struct kobject *);
+
+extern struct kobject * kobject_get(struct kobject *);
+extern void kobject_put(struct kobject *);
+
+#endif /* _KOBJECT_H_ */
