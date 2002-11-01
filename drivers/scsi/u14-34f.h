ea10cfeb379780079b666655bbafcb64fdaee7f3
Alan Cox <alan@lxorguk.ukuu.org.uk>
2002-10-31 19:41:24 -0800

    [PATCH] update the u14-34f driver to maintainer updates
    (With bios changes merged)

--- drivers/scsi/u14-34f.h
+++ drivers/scsi/u14-34f.h
@@ -1,20 +1,16 @@
 /*
  *   u14-34f.h - used by the low-level driver for UltraStor 14F/34F
  */
-#ifndef _U14_34F_H
-#define _U14_34F_H
 
-#include <scsi/scsicam.h>
+static int u14_34f_detect(Scsi_Host_Template *);
+static int u14_34f_release(struct Scsi_Host *);
+static int u14_34f_queuecommand(Scsi_Cmnd *, void (*done)(Scsi_Cmnd *));
+static int u14_34f_eh_abort(Scsi_Cmnd *);
+static int u14_34f_eh_host_reset(Scsi_Cmnd *);
+static int u14_34f_bios_param(Disk *, struct block_device *, int *);
+static int u14_34f_slave_attach(Scsi_Device *);
 
-int u14_34f_detect(Scsi_Host_Template *);
-int u14_34f_release(struct Scsi_Host *);
-int u14_34f_queuecommand(Scsi_Cmnd *, void (*done)(Scsi_Cmnd *));
-int u14_34f_abort(Scsi_Cmnd *);
-int u14_34f_reset(Scsi_Cmnd *);
-int u14_34f_biosparam(struct scsi_device *, struct block_device *,
-		sector_t, int *);
-
-#define U14_34F_VERSION "7.70.00"
+#define U14_34F_VERSION "8.00.00"
 
 #define ULTRASTOR_14_34F {                                                   \
                 name:         "UltraStor 14F/34F rev. " U14_34F_VERSION " ", \
@@ -23,14 +19,13 @@ int u14_34f_biosparam(struct scsi_device *, struct block_device *,
                 queuecommand:            u14_34f_queuecommand,               \
                 abort:                   NULL,                               \
                 reset:                   NULL,                               \
-                eh_abort_handler:        u14_34f_abort,                      \
+                eh_abort_handler:        u14_34f_eh_abort,                   \
                 eh_device_reset_handler: NULL,                               \
                 eh_bus_reset_handler:    NULL,                               \
-                eh_host_reset_handler:   u14_34f_reset,                      \
-                bios_param:              u14_34f_biosparam,                  \
+                eh_host_reset_handler:   u14_34f_eh_host_reset,              \
+                bios_param:              u14_34f_bios_param,                 \
+                slave_attach:            u14_34f_slave_attach,               \
                 this_id:                 7,                                  \
                 unchecked_isa_dma:       1,                                  \
-                use_clustering:          ENABLE_CLUSTERING,                  \
+                use_clustering:          ENABLE_CLUSTERING                   \
                 }
-
-#endif
