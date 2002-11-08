ae24f9c0e2165b95aff70cfb8ff87403fedfe208
Rusty Russell <rusty@rustcorp.com.au>
2002-11-08 01:09:24 -0500

    [PATCH] [Trivial Patch] scsi_register-004
    From:  Michael Still <mikal@stillhq.com>
    
    
      This patch has been generated as part of my scsi_register audit of the 2.5.45 kernel tree. scsi_register() can fail, returning a NULL pointer, and the failure case was not being handled here...

--- drivers/scsi/gdth.c
+++ drivers/scsi/gdth.c
@@ -3991,6 +3991,9 @@ GDTH_INITFUNC(int, gdth_detect(Scsi_Host_Template *shtp))
             break;
         if (gdth_search_isa(isa_bios)) {        /* controller found */
             shp = scsi_register(shtp,sizeof(gdth_ext_str));
+	    if(shp == NULL)
+		    continue;
+
             ha = HADATA(shp);
             if (!gdth_init_isa(isa_bios,ha)) {
                 scsi_unregister(shp);
