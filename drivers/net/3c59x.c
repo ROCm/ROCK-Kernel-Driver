e300f70b13ec4c9bafa638f40da81676966efacb
Andrew Morton <akpm@digeo.com>
2002-11-10 02:01:22 -0800

    [PATCH] misc fixes
    - Revert the 3c59x.c compile warning fixes.  The return type of inl()
      was reverted back to the correct 32 bits.
    
    - Fix an uninitialised timer in ext3 (JBD debug mode only) - run
      setup_ro_after() during initialisation.
    
    - Fix ifdef/endif imbalance in JFS

--- drivers/net/3c59x.c
+++ drivers/net/3c59x.c
@@ -1804,10 +1804,10 @@ static void vortex_tx_timeout(struct net_device *dev)
 		   inw(ioaddr + EL3_STATUS));
 	EL3WINDOW(4);
 	printk(KERN_ERR "  diagnostics: net %04x media %04x dma %08x fifo %04x\n",
-			(unsigned)inw(ioaddr + Wn4_NetDiag),
-			(unsigned)inw(ioaddr + Wn4_Media),
-			(unsigned)inl(ioaddr + PktStatus),
-			(unsigned)inw(ioaddr + Wn4_FIFODiag));
+			inw(ioaddr + Wn4_NetDiag),
+			inw(ioaddr + Wn4_Media),
+			inl(ioaddr + PktStatus),
+			inw(ioaddr + Wn4_FIFODiag));
 	/* Slight code bloat to be user friendly. */
 	if ((inb(ioaddr + TxStatus) & 0x88) == 0x88)
 		printk(KERN_ERR "%s: Transmitter encountered 16 collisions --"
@@ -2644,7 +2644,7 @@ dump_tx_ring(struct net_device *dev)
 					vp->dirty_tx, vp->dirty_tx % TX_RING_SIZE,
 					vp->cur_tx, vp->cur_tx % TX_RING_SIZE);
 			printk(KERN_ERR "  Transmit list %8.8x vs. %p.\n",
-				   (unsigned)inl(ioaddr + DownListPtr),
+				   inl(ioaddr + DownListPtr),
 				   &vp->tx_ring[vp->dirty_tx % TX_RING_SIZE]);
 			issue_and_wait(dev, DownStall);
 			for (i = 0; i < TX_RING_SIZE; i++) {
