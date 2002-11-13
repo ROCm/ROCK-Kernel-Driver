7bd4f8b456c19da7316137856a0b52fd529b9e09
David Mosberger <davidm@tiger.hpl.hp.com>
2002-11-13 04:49:11 -0800

    ia64: Make it easier to set a breakpoint in the Ski simulator right before 	starting the kernel (based on patch by Peter Chubb).

--- arch/ia64/boot/bootloader.c
+++ arch/ia64/boot/bootloader.c
@@ -53,6 +53,15 @@ struct disk_stat {
 
 #include "../kernel/fw-emu.c"
 
+/*
+ * Set a break point on this function so that symbols are available to set breakpoints in
+ * the kernel being debugged.
+ */
+static void
+debug_break (void)
+{
+}
+
 static void
 cons_write (const char *buf)
 {
@@ -187,6 +196,7 @@ _start (void)
 
 	ssc(0, (long) kpath, 0, 0, SSC_LOAD_SYMBOLS);
 
+	debug_break();
 	asm volatile ("mov sp=%2; mov r28=%1; br.sptk.few %0"
 		      :: "b"(e_entry), "r"(bp), "r"(__pa(&stack)));
 
