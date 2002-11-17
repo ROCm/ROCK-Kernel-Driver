bd51d9ad6761f096a0c8a301432a5cf80af8cef9
Russell King <rmk@flint.arm.linux.org.uk>
2002-11-17 16:21:13 +0000

    [ARM] Fix flush_dcache_page()
    Make flush_dcache_page() handle user space mappings correctly; with a
    VIVT cache, we need to make sure that any user space cache lines are
    coherent with the kernels view of the same page of memory.

--- arch/arm/mm/proc-syms.c
+++ arch/arm/mm/proc-syms.c
@@ -1,7 +1,7 @@
 /*
  *  linux/arch/arm/mm/proc-syms.c
  *
- *  Copyright (C) 2000 Russell King
+ *  Copyright (C) 2000-2002 Russell King
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -14,6 +14,8 @@
 #include <asm/pgalloc.h>
 #include <asm/proc-fns.h>
 #include <asm/tlbflush.h>
+
+EXPORT_SYMBOL(__flush_dcache_page);
 
 #ifndef MULTI_CPU
 EXPORT_SYMBOL(cpu_cache_clean_invalidate_all);
