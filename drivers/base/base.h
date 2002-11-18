f17e144440df970749df5b02247b8ae1ca56f89e
Patrick Mochel <mochel@osdl.org>
2002-11-18 06:28:37 -0600

    driver model: make sure driver is added to class it belongs to.
    devclass_{add,remove}_driver() had been implemented, but had never been
    called.

--- drivers/base/base.h
+++ drivers/base/base.h
@@ -19,6 +19,9 @@ extern void bus_remove_driver(struct device_driver *);
 extern int devclass_add_device(struct device *);
 extern void devclass_remove_device(struct device *);
 
+extern int devclass_add_driver(struct device_driver *);
+extern void devclass_remove_driver(struct device_driver *);
+
 extern int interface_add(struct device_class *, struct device *);
 extern void interface_remove(struct device_class *, struct device *);
 
