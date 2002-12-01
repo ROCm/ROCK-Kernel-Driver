423830367c25e5a00f9a3df3fd1f456de51f83db
Alan Cox <alan@lxorguk.ukuu.org.uk>
2002-12-01 02:27:07 -0800

    [PATCH] mmuless no longer needs this ifdef

--- fs/locks.c
+++ fs/locks.c
@@ -1412,7 +1412,6 @@ int fcntl_setlk(struct file *filp, unsigned int cmd, struct flock *l)
 
 	inode = filp->f_dentry->d_inode;
 
-#ifdef CONFIG_MMU
 	/* Don't allow mandatory locks on files that may be memory mapped
 	 * and shared.
 	 */
@@ -1425,7 +1424,6 @@ int fcntl_setlk(struct file *filp, unsigned int cmd, struct flock *l)
 			goto out;
 		}
 	}
-#endif
 
 	error = flock_to_posix_lock(filp, file_lock, &flock);
 	if (error)
