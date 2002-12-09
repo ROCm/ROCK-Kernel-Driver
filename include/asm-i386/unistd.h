ebdef6d5bec6fd340ee07c8b888e2bb4d97797c4
Davide Libenzi <davidel@xmailserver.org>
2002-12-09 01:31:50 -0800

    [PATCH] epoll bits 0.59 ...
    - Finalized the interface by :
    
            * Having an epoll_event structure instead of using the pollfd
            * Adding a 64 bit opaque data member to the epoll_event structure
            * Removing the "fd" member from the epoll_event structure
            * Removing the "revents" member to leave space for a unique 32 bit
                    "events" member
    
    - Fixes the problem where, due the new callback'd wake_up() mechanism
            loops might be generated by bringing deadlock or stack blow ups.
            In fact a user could create a cycle by adding epoll fds inside
            other epoll fds. The patch solves the problem by either :
    
            * Moving the wake_up() call done on the poll wait queue head,
                    outside the locked region
            * Implementing a new safe wake up function for the poll wait queue
                    head
    
    - Some variable renaming
    
    - Changed __NR_sys_epoll_* to __NR_epoll_* ( Hanna Linder )
    
    - Blocked the add operation of an epoll file descriptor inside itself
    
    - Comments added/fixed

--- include/asm-i386/unistd.h
+++ include/asm-i386/unistd.h
@@ -259,9 +259,9 @@
 #define __NR_free_hugepages	251
 #define __NR_exit_group		252
 #define __NR_lookup_dcookie	253
-#define __NR_sys_epoll_create	254
-#define __NR_sys_epoll_ctl	255
-#define __NR_sys_epoll_wait	256
+#define __NR_epoll_create	254
+#define __NR_epoll_ctl		255
+#define __NR_epoll_wait		256
 #define __NR_remap_file_pages	257
 #define __NR_set_tid_address	258
 
