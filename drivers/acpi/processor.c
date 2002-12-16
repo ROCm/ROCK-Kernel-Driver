70fd41994e58426678fec23ec86212c673f6c856
Andy Grover <agrover@groveronline.com>
2002-12-15 19:22:23 -0800

    ACPI: Fix oops on module insert/remove (Matthew Tippett)

--- drivers/acpi/processor.c
+++ drivers/acpi/processor.c
@@ -2348,8 +2348,10 @@ acpi_processor_remove_fs (
 {
 	ACPI_FUNCTION_TRACE("acpi_processor_remove_fs");
 
-	if (acpi_device_dir(device))
+	if (acpi_device_dir(device)) {
 		remove_proc_entry(acpi_device_bid(device), acpi_processor_dir);
+		acpi_device_dir(device) = NULL;
+	}
 
 	return_VALUE(0);
 }
