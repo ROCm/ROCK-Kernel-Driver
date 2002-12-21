4e375211e20eff9ad5382e3bfe2e7408907fd971
Linus Torvalds <torvalds@home.transmeta.com>
2002-12-21 06:42:37 -0800

    Remove old pci_dma_supported(), this is done by the generic device DMA now (see <linux/pci.h> for the compat wrapper).

--- include/asm-i386/pci.h
+++ include/asm-i386/pci.h
@@ -57,25 +57,6 @@ struct pci_dev;
 #define pci_unmap_len(PTR, LEN_NAME)		(0)
 #define pci_unmap_len_set(PTR, LEN_NAME, VAL)	do { } while (0)
 
-
-/* Return whether the given PCI device DMA address mask can
- * be supported properly.  For example, if your device can
- * only drive the low 24-bits during PCI bus mastering, then
- * you would pass 0x00ffffff as the mask to this function.
- */
-static inline int pci_dma_supported(struct pci_dev *hwdev, u64 mask)
-{
-        /*
-         * we fall back to GFP_DMA when the mask isn't all 1s,
-         * so we can't guarantee allocations that must be
-         * within a tighter range than GFP_DMA..
-         */
-        if(mask < 0x00ffffff)
-                return 0;
-
-	return 1;
-}
-
 /* This is always fine. */
 #define pci_dac_dma_supported(pci_dev, mask)	(1)
 
