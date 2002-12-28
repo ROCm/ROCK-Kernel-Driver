72a4ba8f53900e5d87c10824caf49dd6f843c8b6
Geert Uytterhoeven <geert@linux-m68k.org>
2002-12-27 19:13:08 -0800

    [PATCH] M68k fbdev local_irq*() updates
    Convert m68k fbdev drivers to new local_irq*() framework:
      - Atari builtin graphics

--- drivers/video/atafb.c
+++ drivers/video/atafb.c
@@ -2050,8 +2050,7 @@ static void st_ovsc_switch(void)
 
     if (!(atari_switches & ATARI_SWITCH_OVSC_MASK))
 	return;
-    save_flags(flags);
-    cli();
+    local_irq_save(flags);
 
     mfp.tim_ct_b = 0x10;
     mfp.active_edge |= 8;
@@ -2079,7 +2078,7 @@ static void st_ovsc_switch(void)
 			   ((atari_switches&ATARI_SWITCH_OVSC_SND6) ? 0x40:0) |
 			   ((atari_switches&ATARI_SWITCH_OVSC_SND7) ? 0x80:0);
     }
-    restore_flags(flags);
+    local_irq_restore(flags);
 }
 
 /* ------------------- External Video ---------------------- */
