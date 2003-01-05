f9cc7a8340d124ea237a3e5c7804b509ff2c9a11
Bob Miller <rem@osdl.org>
2003-01-05 04:57:04 -0800

    [PATCH] Remove unused function from radeon_mem.c

--- drivers/char/drm/radeon_mem.c
+++ drivers/char/drm/radeon_mem.c
@@ -130,16 +130,6 @@ static void free_block( struct mem_block *p )
 	}
 }
 
-static void print_heap( struct mem_block *heap )
-{
-	struct mem_block *p;
-
-	for (p = heap->next ; p != heap ; p = p->next) 
-		DRM_DEBUG("0x%x..0x%x (0x%x) -- owner %d\n",
-			  p->start, p->start + p->size,
-			  p->size, p->pid);
-}
-
 /* Initialize.  How to check for an uninitialized heap?
  */
 static int init_heap(struct mem_block **heap, int start, int size)
