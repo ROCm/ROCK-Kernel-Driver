73ef10724699cdbf4df056af38862727af9d6711
Chris Wilson <chris@qwirx.com>
2003-01-05 04:56:37 -0800

    [PATCH] ip27-rtc.c create_proc_read_entry patch
      As part of my work on the Linux Kernel Janitors project, cleaning up on
      functions which call create_proc_*_entry and don't check for an error
      return, I would like to submit my patch to arch/mips64/sgi-ip27/ip27-rtc.c.
    
      This patch adds a printk() if misc_register fails (since that causes the
      driver to fail to load, it should be logged somewhere), and checks that
      create_proc_read_entry returned a valid handle. ENOENT seems to me like a
      good error code to return in this case, although EBUSY is a candidate too.

--- arch/mips64/sgi-ip27/ip27-rtc.c
+++ arch/mips64/sgi-ip27/ip27-rtc.c
@@ -202,9 +202,15 @@ static int __init rtc_init(void)
 	    KL_CONFIG_CH_CONS_INFO(nid)->memory_base + IOC3_BYTEBUS_DEV0;
 
 	printk(KERN_INFO "Real Time Clock Driver v%s\n", RTC_VERSION);
-	if (misc_register(&rtc_dev))
+	if (misc_register(&rtc_dev)) {
+		printk(KERN_ERR "rtc: cannot register misc device.\n");
 		return -ENODEV;
-	create_proc_read_entry ("rtc", 0, NULL, rtc_read_proc, NULL);
+	}
+	if (!create_proc_read_entry ("rtc", 0, NULL, rtc_read_proc, NULL)) {
+		printk(KERN_ERR "rtc: cannot create /proc/rtc.\n");
+		misc_deregister(&rtc_dev);
+		return -ENOENT;
+	}
 
 	save_flags(flags);
 	cli();
