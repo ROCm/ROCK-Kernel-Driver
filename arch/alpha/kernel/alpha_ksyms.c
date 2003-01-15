39bcaa1582d5be45169ada8afbec00b6b961c097
Ivan Kokshaysky <ink@jurassic.park.msu.ru>
2003-01-14 18:59:59 -0800

    [PATCH] alpha ksyms From Jeff.Wiedemeier@hp.com:
    Export proper functions when debugging is enabled.

--- arch/alpha/kernel/alpha_ksyms.c
+++ arch/alpha/kernel/alpha_ksyms.c
@@ -217,13 +217,13 @@ EXPORT_SYMBOL(smp_call_function);
 EXPORT_SYMBOL(smp_call_function_on_cpu);
 EXPORT_SYMBOL(atomic_dec_and_lock);
 #ifdef CONFIG_DEBUG_SPINLOCK
-EXPORT_SYMBOL(spin_unlock);
+EXPORT_SYMBOL(_raw_spin_unlock);
 EXPORT_SYMBOL(debug_spin_lock);
 EXPORT_SYMBOL(debug_spin_trylock);
 #endif
 #ifdef CONFIG_DEBUG_RWLOCK
-EXPORT_SYMBOL(write_lock);
-EXPORT_SYMBOL(read_lock);
+EXPORT_SYMBOL(_raw_write_lock);
+EXPORT_SYMBOL(_raw_read_lock);
 #endif
 EXPORT_SYMBOL(cpu_present_mask);
 #endif /* CONFIG_SMP */
