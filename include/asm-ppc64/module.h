251a12723f1854004dc1f2815fa37222288e99a1
Anton Blanchard <anton@samba.org>
2003-01-15 17:46:23 +1100

    ppc64: 2.5 module support, from Rusty

--- include/asm-ppc64/module.h
+++ include/asm-ppc64/module.h
@@ -1,18 +1,22 @@
 #ifndef _ASM_PPC64_MODULE_H
 #define _ASM_PPC64_MODULE_H
-/*
- * This file contains the PPC architecture specific module code.
- *
- * Copyright (C) 2001 PPC 64 Team, IBM Corp
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or (at your option) any later version.
- */
 
-#define module_map(x)		vmalloc(x)
-#define module_unmap(x)		vfree(x)
-#define arch_init_modules(x)	do { } while (0)
-#define module_arch_init(x)  (0)
+struct mod_arch_specific
+{
+	/* Index of stubs section within module. */
+	unsigned int stubs_section;
+
+	/* What section is the TOC? */
+	unsigned int toc_section;
+};
+
+#define Elf_Shdr Elf64_Shdr
+#define Elf_Sym Elf64_Sym
+#define Elf_Ehdr Elf64_Ehdr
+
+/* Make empty section for module_frob_arch_sections to expand. */
+#ifdef MODULE
+asm(".section .stubs,\"ax\",@nobits; .align 3; .previous");
+#endif
+
 #endif /* _ASM_PPC64_MODULE_H */
