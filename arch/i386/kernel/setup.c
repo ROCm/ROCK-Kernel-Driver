c501274adfe3fbec6d20385983fa6655bf0282f0
Matt Domsch <matt_domsch@dell.com>
2003-01-21 10:08:08 -0600

    EDD: don't over-allocate EDD data block
    Found by Kevin Lawton.

--- arch/i386/kernel/setup.c
+++ arch/i386/kernel/setup.c
@@ -474,7 +474,7 @@ static int __init copy_e820_map(struct e820entry * biosmap, int nr_map)
 
 #if defined(CONFIG_EDD) || defined(CONFIG_EDD_MODULE)
 unsigned char eddnr;
-struct edd_info edd[EDDNR];
+struct edd_info edd[EDDMAXNR];
 /**
  * copy_edd() - Copy the BIOS EDD information
  *              from empty_zero_page into a safe place.
