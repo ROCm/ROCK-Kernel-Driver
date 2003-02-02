4a3691214ab7f95b23909d899df65ad28c145209
Andrew Morton <akpm@digeo.com>
2003-02-02 06:07:57 -0800

    [PATCH] stack overflow checking fix
    Patch from William Lee Irwin III <wli@holomorphy.com>
    
    struct thread_info is shared with the stack, not struct task_struct.
    False positives have been seen.

--- arch/i386/kernel/irq.c
+++ arch/i386/kernel/irq.c
@@ -338,9 +338,9 @@ asmlinkage unsigned int do_IRQ(struct pt_regs regs)
 
 		__asm__ __volatile__("andl %%esp,%0" :
 					"=r" (esp) : "0" (8191));
-		if (unlikely(esp < (sizeof(struct task_struct) + 1024))) {
+		if (unlikely(esp < (sizeof(struct thread_info) + 1024))) {
 			printk("do_IRQ: stack overflow: %ld\n",
-				esp - sizeof(struct task_struct));
+				esp - sizeof(struct thread_info));
 			dump_stack();
 		}
 	}
