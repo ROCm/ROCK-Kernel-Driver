9e549588fdff1d6e6319e2778e921b5b70148836
Roland McGrath <roland@redhat.com>
2003-02-06 16:22:22 -0800

    [PATCH] SA_NOCLDWAIT now supported - update comments
    This patch removes all the comments on the SA_NOCLDWAIT definitions,
    since SA_NOCLDWAIT is fully supported now.

--- include/asm-sparc64/signal.h
+++ include/asm-sparc64/signal.h
@@ -145,7 +145,7 @@ struct sigstack {
 #define SA_INTERRUPT	0x10
 #define SA_NOMASK	0x20
 #define SA_SHIRQ	0x40
-#define SA_NOCLDWAIT    0x100 /* not supported yet */
+#define SA_NOCLDWAIT    0x100
 #define SA_SIGINFO      0x200
 
 
