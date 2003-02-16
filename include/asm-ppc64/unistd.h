2e6e0d096f65f8f608549bb8080bb27ad262391c
Anton Blanchard <anton@samba.org>
2003-02-16 15:36:31 +1100

    ppc64: add set_tid_address and fadvise64

--- include/asm-ppc64/unistd.h
+++ include/asm-ppc64/unistd.h
@@ -242,7 +242,8 @@
 #define __NR_io_getevents	229
 #define __NR_io_submit		230
 #define __NR_io_cancel		231
-/* 223 + 224 currently unused */
+#define __NR_set_tid_address	232
+#define __NR_fadvise64		233
 #define __NR_exit_group		234
 #define __NR_lookup_dcookie	235
 #define __NR_sys_epoll_create	236
