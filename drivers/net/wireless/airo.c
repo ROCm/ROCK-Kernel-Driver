804bae86b16bbed330ccc66e88f5fa13b5dbbc5c
Matthew Wilcox <willy@debian.org>
2003-02-17 08:46:23 -0500

    [wireless airo] call pci_enable_device, pci_set_master as needed

--- drivers/net/wireless/airo.c
+++ drivers/net/wireless/airo.c
@@ -4002,6 +4002,10 @@ static int __devinit airo_pci_probe(struct pci_dev *pdev,
 {
 	struct net_device *dev;
 
+	if (pci_enable_device(pdev))
+		return -ENODEV;
+	pci_set_master(pdev);
+
 	dev = init_airo_card(pdev->irq,	pdev->resource[2].start, 0);
 	if (!dev)
 		return -ENODEV;
