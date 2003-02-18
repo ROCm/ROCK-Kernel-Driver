fee3a34fca6701e2bd544ca818d571f33b7d9ef2
Andrew Morton <akpm@digeo.com>
2003-02-18 05:26:31 -0800

    [PATCH] visws: move header file into asm/arch-visws
    Patch from Andrey Panin <pazke@orbita1.ru>
    
    This trivial patch moves visws related header files into asm/mach-visws.

--- include/asm-i386/mach-visws/lithium.h
+++ include/asm-i386/mach-visws/lithium.h
@@ -1,10 +1,10 @@
-#ifndef __I386_LITHIUM_H
-#define __I386_LITHIUM_H
+#ifndef __I386_SGI_LITHIUM_H
+#define __I386_SGI_LITHIUM_H
 
-#include <linux/config.h>
+#include <asm/fixmap.h>
 
 /*
- * Lithium is the I/O ASIC on the SGI 320 and 540 Visual Workstations
+ * Lithium is the SGI Visual Workstation I/O ASIC
  */
 
 #define	LI_PCI_A_PHYS		0xfc000000	/* Enet is dev 3 */
@@ -18,28 +18,36 @@
 #define	LI_PCI_BUSNUM	0x44			/* lo8: primary, hi8: sub */
 #define LI_PCI_INTEN    0x46
 
-#ifdef CONFIG_X86_VISWS_APIC
+/* LI_PCI_INTENT bits */
+#define	LI_INTA_0	0x0001
+#define	LI_INTA_1	0x0002
+#define	LI_INTA_2	0x0004
+#define	LI_INTA_3	0x0008
+#define	LI_INTA_4	0x0010
+#define	LI_INTB		0x0020
+#define	LI_INTC		0x0040
+#define	LI_INTD		0x0080
+
 /* More special purpose macros... */
-static __inline void li_pcia_write16(unsigned long reg, unsigned short v)
+extern __inline void li_pcia_write16(unsigned long reg, unsigned short v)
 {
 	*((volatile unsigned short *)(LI_PCIA_VADDR+reg))=v;
 }
 
-static __inline unsigned short li_pcia_read16(unsigned long reg)
+extern __inline unsigned short li_pcia_read16(unsigned long reg)
 {
 	 return *((volatile unsigned short *)(LI_PCIA_VADDR+reg));
 }
 
-static __inline void li_pcib_write16(unsigned long reg, unsigned short v)
+extern __inline void li_pcib_write16(unsigned long reg, unsigned short v)
 {
 	*((volatile unsigned short *)(LI_PCIB_VADDR+reg))=v;
 }
 
-static __inline unsigned short li_pcib_read16(unsigned long reg)
+extern __inline unsigned short li_pcib_read16(unsigned long reg)
 {
 	return *((volatile unsigned short *)(LI_PCIB_VADDR+reg));
 }
-#endif
 
 #endif
 
