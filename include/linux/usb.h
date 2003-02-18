f5889474a290f8a3fb9cef91045cc69ad359937c
Greg Kroah-Hartman <greg@kroah.com>
2003-02-18 00:27:45 -0800

    [PATCH] USB: added sched.h to usb.h
    Thanks to Matt Wilcox for the info.

--- include/linux/usb.h
+++ include/linux/usb.h
@@ -17,6 +17,7 @@
 #include <linux/device.h>	/* for struct device */
 #include <linux/fs.h>		/* for struct file_operations */
 #include <linux/completion.h>	/* for struct completion */
+#include <linux/sched.h>	/* for current && schedule_timeout */
 
 
 static __inline__ void wait_ms(unsigned int ms)
