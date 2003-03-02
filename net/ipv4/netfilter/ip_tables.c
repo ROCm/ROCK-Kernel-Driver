1b31fcf3da6b5650cb3254edded6233f5c7d6a27
Harald Welte <harald@gnumonks.org>
2003-03-02 09:21:38 -0800

    [NETFILTER]: Fix icmp-type all problem in iptables.

--- net/ipv4/netfilter/ip_tables.c
+++ net/ipv4/netfilter/ip_tables.c
@@ -1611,7 +1611,7 @@ icmp_type_code_match(u_int8_t test_type, u_int8_t min_code, u_int8_t max_code,
 		     u_int8_t type, u_int8_t code,
 		     int invert)
 {
-	return (type == test_type && code >= min_code && code <= max_code)
+	return ((test_type == 0xFF) || (type == test_type && code >= min_code && code <= max_code))
 		^ invert;
 }
 
