06ad5d9405f0f3a778c2a92c3ef6f08640b3dfe1
Greg Kroah-Hartman <greg@kroah.com>
2003-03-05 23:47:22 -0800

    [PATCH] USB: unfortunatly, we can't call usb_unlink_urb() right now all of the time.
    The host controllers have to be fixed up before we can safely take
    out the check for dev->state.

--- drivers/usb/core/urb.c
+++ drivers/usb/core/urb.c
@@ -381,7 +381,16 @@ int usb_submit_urb(struct urb *urb, int mem_flags)
  */
 int usb_unlink_urb(struct urb *urb)
 {
-	if (urb && urb->dev && urb->dev->bus && urb->dev->bus->op)
+	/* FIXME
+	 * We should not care about the state here, but the host controllers
+	 * die a horrible death if we submit a urb for a device that has been
+	 * physically removed.
+	 */
+	if (urb &&
+	    urb->dev &&
+	    (urb->dev->state >= USB_STATE_DEFAULT) &&
+	    urb->dev->bus &&
+	    urb->dev->bus->op)
 		return urb->dev->bus->op->unlink_urb(urb);
 	else
 		return -ENODEV;
