5871d7a10005bab508cf1bea2891cde58bf9ee98
Andrew Morton <akpm@digeo.com>
2003-03-08 06:06:03 -0800

    [PATCH] fix a warning in eepro100.c
    Remove the unneeded forward declaration of eepro100_remove_one().
    
    It is defeating the __attribute__(unused) tag on the definition of
    eepro100_remove_one() and is causing a "defined but not used" warning.

--- drivers/net/eepro100.c
+++ drivers/net/eepro100.c
@@ -529,8 +529,6 @@ static const char is_mii[] = { 0, 1, 1, 0, 1, 1, 0, 1 };
 static int eepro100_init_one(struct pci_dev *pdev,
 		const struct pci_device_id *ent);
 
-static void eepro100_remove_one (struct pci_dev *pdev);
-
 static int do_eeprom_cmd(long ioaddr, int cmd, int cmd_len);
 static int mdio_read(struct net_device *dev, int phy_id, int location);
 static void mdio_write(struct net_device *dev, int phy_id, int location, int value);
