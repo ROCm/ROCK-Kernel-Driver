986815a1495b7dc88eaad350fd5421ca1f910d77
Geert Uytterhoeven <geert@linux-m68k.org>
2003-03-20 06:03:03 -0800

    [PATCH] M68k PAGE_SIZE warnings
    M68k: Make PAGE_SIZE unsigned long to kill more warnings (cfr. other
    architectures), except for asm, since gas cannot cope with the UL.

--- include/asm-m68k/page.h
+++ include/asm-m68k/page.h
@@ -6,10 +6,13 @@
 /* PAGE_SHIFT determines the page size */
 #ifndef CONFIG_SUN3
 #define PAGE_SHIFT	(12)
-#define PAGE_SIZE	(4096)
 #else
 #define PAGE_SHIFT	(13)
-#define PAGE_SIZE	(8192)
+#endif
+#ifdef __ASSEMBLY__
+#define PAGE_SIZE	(1 << PAGE_SHIFT)
+#else
+#define PAGE_SIZE	(1UL << PAGE_SHIFT)
 #endif
 #define PAGE_MASK	(~(PAGE_SIZE-1))
 
