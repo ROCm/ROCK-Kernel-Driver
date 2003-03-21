231704c959e5d12e765c8a6cf3c647efae66bd78
Alan Cox <alan@lxorguk.ukuu.org.uk>
2003-03-21 02:27:07 -0800

    [PATCH] remove __NO_VERSION__ in mtd

--- drivers/mtd/nftlmount.c
+++ drivers/mtd/nftlmount.c
@@ -21,7 +21,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#define __NO_VERSION__
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <asm/errno.h>
