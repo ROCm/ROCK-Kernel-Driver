65b9e49f97df9327bfd3f34cefac8a124bd3dc44
Alan Cox <alan@lxorguk.ukuu.org.uk>
2003-03-21 02:20:37 -0800

    [PATCH] unbreak the acquirewdt
    This puts back a MOD_INC_USE which leaves a warning but means that the
    driver doesnt now load/unload and disable the watchdog on a close

--- drivers/char/watchdog/acquirewdt.c
+++ drivers/char/watchdog/acquirewdt.c
@@ -141,6 +141,8 @@ static int acq_open(struct inode *inode, struct file *file)
 			spin_unlock(&acq_lock);
 			return -EBUSY;
 		}
+		if (nowayout)
+			MOD_INC_USE_COUNT;
 
 		/* Activate */
 		acq_is_open=1;
