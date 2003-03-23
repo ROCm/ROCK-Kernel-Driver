b2b1832b2f948d795403c9b2d746f091cfd8d3bc
David S. Miller <davem@nuts.ninka.net>
2003-03-23 06:15:55 -0800

    [SPARC64]: Fix typos in uptr changes.

--- include/asm-sparc64/compat.h
+++ include/asm-sparc64/compat.h
@@ -108,9 +108,9 @@ typedef u32		compat_sigset_word;
  */
 typedef	u32		compat_uptr_t;
 
-static inline void *compat_ptr(compat_ptr_t uptr)
+static inline void *compat_ptr(compat_uptr_t uptr)
 {
-	return (void *)uptr;
+	return (void *)(unsigned long)uptr;
 }
 
 #endif /* _ASM_SPARC64_COMPAT_H */
