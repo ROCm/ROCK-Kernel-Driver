14e514738a92b96826c1d6b7ac2966f7ac36ccbe
Anton Blanchard <anton@samba.org>
2003-03-29 15:56:19 +1100

    ppc64: clean up some cpu feature checks

--- include/asm-ppc64/processor.h
+++ include/asm-ppc64/processor.h
@@ -742,6 +742,15 @@ static inline void prefetchw(const void *x)
 #define cpu_has_noexecute()	(processor_type() == PV_POWER4 || \
 				 processor_type() == PV_POWER4p)
 
+/* XXX we have to call HV to set when in LPAR */
+#define cpu_has_dabr()		(1)
+
+#define cpu_has_iabr()		(processor_type() != PV_POWER4 && \
+				 processor_type() != PV_POWER4p)
+
+#define cpu_alignexc_sets_dsisr() (processor_type() != PV_POWER4 && \
+				 processor_type() != PV_POWER4p)
+
 #endif /* ASSEMBLY */
 
 #endif /* __ASM_PPC64_PROCESSOR_H */
