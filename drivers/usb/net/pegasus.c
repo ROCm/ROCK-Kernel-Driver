d33008a0dde7a809af8b3720301b62983c80b2df
Oliver Neukum <oliver@neukum.org>
2003-04-10 22:15:15 -0700

    [PATCH] USB: remove configuration change from pegasus.c
    the driver should not mess with configurations here.

--- drivers/usb/net/pegasus.c
+++ drivers/usb/net/pegasus.c
@@ -1171,10 +1171,6 @@ static int pegasus_probe(struct usb_interface *intf,
 	pegasus_t *pegasus;
 	int dev_index = id - pegasus_ids;
 
-	if (usb_set_configuration(dev, dev->config[0].desc.bConfigurationValue)) {
-		err("usb_set_configuration() failed");
-		return -ENODEV;
-	}
 	if (!(pegasus = kmalloc(sizeof (struct pegasus), GFP_KERNEL))) {
 		err("out of memory allocating device structure");
 		return -ENOMEM;
