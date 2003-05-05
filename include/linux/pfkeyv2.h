7f1648a622916edb3ca08fe67696757f0ee70171
David S. Miller <davem@nuts.ninka.net>
2003-05-05 10:14:29 -0700

    [IPSEC]: Fix SADB_EALG_{3,}DESCBC values.

--- include/linux/pfkeyv2.h
+++ include/linux/pfkeyv2.h
@@ -275,8 +275,8 @@ struct sadb_x_nat_t_port {
 
 /* Encryption algorithms */
 #define SADB_EALG_NONE			0
-#define SADB_EALG_DESCBC		1
-#define SADB_EALG_3DESCBC		2
+#define SADB_EALG_DESCBC		2
+#define SADB_EALG_3DESCBC		3
 #define SADB_X_EALG_CASTCBC		6
 #define SADB_X_EALG_BLOWFISHCBC		7
 #define SADB_EALG_NULL			11
