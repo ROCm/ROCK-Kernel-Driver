66eee7f90f1820d4cd14d7754658a8ba88b27a11
James Bottomley <jejb@raven.il.steeleye.com>
2003-05-11 04:26:23 -0500

    Fix thinko introduced into include/scsi/scsi.h
    SAM_STAT_* are return codes, not bitmaps

--- include/scsi/scsi.h
+++ include/scsi/scsi.h
@@ -137,11 +137,11 @@ static inline int scsi_status_is_good(int status)
 	
 	status &= 0xfe;
 
-	return ((status & SAM_STAT_GOOD)
-		|| (status & SAM_STAT_INTERMEDIATE)
-		|| (status & SAM_STAT_INTERMEDIATE_CONDITION_MET)
+	return ((status == SAM_STAT_GOOD)
+		|| (status == SAM_STAT_INTERMEDIATE)
+		|| (status == SAM_STAT_INTERMEDIATE_CONDITION_MET)
 		/* FIXME: this is obsolete in SAM-3 */
-		|| (status & SAM_STAT_COMMAND_TERMINATED));
+		|| (status == SAM_STAT_COMMAND_TERMINATED));
 }
 
 /*
