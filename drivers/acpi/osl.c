ab1bfa5cee11efebda5377fa72a9de4c015b9e0a
Andy Grover <agrover@groveronline.com>
2003-05-11 20:40:25 -0700

    ACPI: Allow ":" in OS override string (Ducrot Bruno)

--- drivers/acpi/osl.c
+++ drivers/acpi/osl.c
@@ -1006,7 +1006,7 @@ acpi_os_name_setup(char *str)
 		return 0;
 
 	for (; count-- && str && *str; str++) {
-		if (isalnum(*str) || *str == ' ')
+		if (isalnum(*str) || *str == ' ' || *str == ':')
 			*p++ = *str;
 		else if (*str == '\'' || *str == '"')
 			continue;
