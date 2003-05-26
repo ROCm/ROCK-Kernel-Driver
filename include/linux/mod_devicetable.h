674084941e3bdad3b414c492b78f44c861896d70
Martin Schwidefsky <schwidefsky@de.ibm.com>
2003-05-25 23:34:34 -0700

    [PATCH] s390: module alias support.
    Add module alias support for ccw devices.

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -130,4 +130,22 @@ struct usb_device_id {
 #define USB_DEVICE_ID_MATCH_INT_SUBCLASS	0x0100
 #define USB_DEVICE_ID_MATCH_INT_PROTOCOL	0x0200
 
+/* s390 CCW devices */
+struct ccw_device_id {
+	__u16	match_flags;	/* which fields to match against */
+
+	__u16	cu_type;	/* control unit type     */
+	__u16	dev_type;	/* device type           */
+	__u8	cu_model;	/* control unit model    */
+	__u8	dev_model;	/* device model          */
+
+	kernel_ulong_t driver_info;
+};
+
+#define CCW_DEVICE_ID_MATCH_CU_TYPE		0x01
+#define CCW_DEVICE_ID_MATCH_CU_MODEL		0x02
+#define CCW_DEVICE_ID_MATCH_DEVICE_TYPE		0x04
+#define CCW_DEVICE_ID_MATCH_DEVICE_MODEL	0x08
+
+
 #endif /* LINUX_MOD_DEVICETABLE_H */
