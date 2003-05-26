0b1a47c2d46b9539b5a29e9e88f3ae1f1acbd438
Martin Schwidefsky <schwidefsky@de.ibm.com>
2003-05-25 23:34:24 -0700

    [PATCH] s390: inline assemblies.
    Optimize s390 inline assemblies.

--- arch/s390/kernel/smp.c
+++ arch/s390/kernel/smp.c
@@ -513,10 +513,9 @@ int __cpu_up(unsigned int cpu)
 	cpu_lowcore->kernel_stack = (unsigned long)
 		idle->thread_info + (THREAD_SIZE);
 	__ctl_store(cpu_lowcore->cregs_save_area[0], 0, 15);
-	__asm__ __volatile__("la    1,%0\n\t"
-			     "stam  0,15,0(1)"
-			     : "=m" (cpu_lowcore->access_regs_save_area[0])
-			     : : "1", "memory");
+	__asm__ __volatile__("stam  0,15,0(%0)"
+			     : : "a" (&cpu_lowcore->access_regs_save_area)
+			     : "memory");
         eieio();
         signal_processor(cpu,sigp_restart);
 
