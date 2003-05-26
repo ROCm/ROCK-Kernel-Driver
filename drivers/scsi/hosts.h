3134118a9832aea4e1c03c73440dcda2313d4179
James Bottomley <james.bottomley@steeleye.com>
2003-05-26 02:14:38 -0500

    [PATCH] SCSI: Make sysfs attributes mutable
    The attached patch makes the sysfs attributes a property of the host
    template (I can't really see a reason why the actual attributes need to
    change per host, so I think the template is the better place for them).
    
    I've also provided helper functions to modify the attributes in the LLD
    init routines.

--- drivers/scsi/hosts.h
+++ drivers/scsi/hosts.h
@@ -356,6 +356,16 @@ typedef struct	SHT
      * FIXME: This should probably be a value in the template */
     #define SCSI_DEFAULT_HOST_BLOCKED	7
 
+    /*
+     * pointer to the sysfs class properties for this host
+     */
+    struct class_device_attribute **shost_attrs;
+
+    /*
+     * Pointer to the SCSI device properties for this host
+     */
+    struct device_attribute **sdev_attrs;
+
 } Scsi_Host_Template;
 
 /*
