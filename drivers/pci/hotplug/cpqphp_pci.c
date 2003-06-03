629453109b5972a55a9385b00d6ec98d864ee737
Greg Kroah-Hartman <greg@kroah.com>
2003-06-03 06:54:30 -0700

    PCI Hotplug: move drivers/hotplug/* to drivers/pci/hotplug/*
    This will let include/linux/pci.h get smaller, and is what I should 
    have done in the first place 2 years ago...

--- drivers/pci/hotplug/cpqphp_pci.c
+++ drivers/pci/hotplug/cpqphp_pci.c
@@ -36,7 +36,7 @@
 #include <linux/pci.h>
 #include "cpqphp.h"
 #include "cpqphp_nvram.h"
-#include "../../arch/i386/pci/pci.h"	/* horrible hack showing how processor dependent we are... */
+#include "../../../arch/i386/pci/pci.h"	/* horrible hack showing how processor dependent we are... */
 
 
 u8 cpqhp_nic_irq;
--- drivers/pci/hotplug/ibmphp_core.c
+++ drivers/pci/hotplug/ibmphp_core.c
@@ -35,7 +35,7 @@
 #include <linux/delay.h>
 #include <linux/wait.h>
 #include <linux/smp_lock.h>
-#include "../../arch/i386/pci/pci.h"	/* for struct irq_routing_table */
+#include "../../../arch/i386/pci/pci.h"	/* for struct irq_routing_table */
 #include "ibmphp.h"
 
 #define attn_on(sl)  ibmphp_hpc_writeslot (sl, HPC_SLOT_ATTNON)
