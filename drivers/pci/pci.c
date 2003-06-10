34f14bfb984326abbfdf1c0837376fb628f3dcbe
Greg Kroah-Hartman <greg@kroah.com>
2003-06-09 20:30:57 -0700

    [PATCH] PCI: remove pci_for_each_bus() usage from drivers/pci/pci.c

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -55,11 +55,11 @@ pci_bus_max_busnr(struct pci_bus* bus)
 unsigned char __devinit
 pci_max_busnr(void)
 {
-	struct pci_bus* bus;
+	struct pci_bus *bus = NULL;
 	unsigned char max, n;
 
 	max = 0;
-	pci_for_each_bus(bus) {
+	while ((bus = pci_find_next_bus(bus)) != NULL) {
 		n = pci_bus_max_busnr(bus);
 		if(n > max)
 			max = n;
