0230f93d22d3e8f5f621277110043a1c3d018793
Andrew Morton <akpm@digeo.com>
2003-06-14 07:19:59 -0700

    [PATCH] x86_64: fix do_settimeofday() for new API

--- arch/x86_64/kernel/time.c
+++ arch/x86_64/kernel/time.c
@@ -124,20 +124,23 @@ void do_gettimeofday(struct timeval *tv)
  * ages already.
  */
 
-void do_settimeofday(struct timeval *tv)
+int do_settimeofday(struct timespec *tv)
 {
+	if ((unsigned long)tv->tv_nsec >= NSEC_PER_SEC)
+		return -EINVAL;
+
 	write_seqlock_irq(&xtime_lock);
 
-	tv->tv_usec -= do_gettimeoffset() +
-		(jiffies - wall_jiffies) * (USEC_PER_SEC/HZ);
+	tv->tv_nsec -= do_gettimeoffset() * 1000 +
+		(jiffies - wall_jiffies) * (NSEC_PER_SEC/HZ);
 
-	while (tv->tv_usec < 0) {
-		tv->tv_usec += 1000000;
+	while (tv->tv_nsec < 0) {
+		tv->tv_nsec += NSEC_PER_SEC;
 		tv->tv_sec--;
 	}
 
 	xtime.tv_sec = tv->tv_sec;
-	xtime.tv_nsec = (tv->tv_usec * 1000);
+	xtime.tv_nsec = tv->tv_nsec;
 
 	time_adjust = 0;		/* stop active adjtime() */
 	time_status |= STA_UNSYNC;
@@ -146,6 +149,7 @@ void do_settimeofday(struct timeval *tv)
 
 	write_sequnlock_irq(&xtime_lock);
 	clock_was_set();
+	return 0;
 }
 
 /*
