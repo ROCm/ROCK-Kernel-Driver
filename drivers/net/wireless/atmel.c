c1a9fe6510903daf7c160e2639dc3573911f4464
Adrian Bunk <bunk@fs.tum.de>
2003-06-17 00:28:40 -0700

    [NET]: Fix namespace pollution in two wireless drivers.

--- drivers/net/wireless/atmel.c
+++ drivers/net/wireless/atmel.c
@@ -1903,7 +1903,7 @@ static int atmel_get_frag(struct net_device *dev,
 	return 0;
 }
 
-const long frequency_list[] = { 2412, 2417, 2422, 2427, 2432, 2437, 2442,
+static const long frequency_list[] = { 2412, 2417, 2422, 2427, 2432, 2437, 2442,
 				2447, 2452, 2457, 2462, 2467, 2472, 2484 };
 
 static int atmel_set_freq(struct net_device *dev,
