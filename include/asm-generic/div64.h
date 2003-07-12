ed08e6dfc28d810101c7665a72c73117888bc4a0
Bernardo Innocenti <bernie@develer.com>
2003-07-11 19:35:48 -0700

    [PATCH] asm-generic/div64.h breakage
     - __div64_32(): remove __attribute_pure__ qualifier from the prototype
       since this function obviously clobbers memory through &(n);
    
     - do_div(): add a check to ensure (n) is type-compatible with uint64_t;
    
     - as_update_iohist(): Use sector_div() instead of do_div().
       (Whether the result of the addition should always be stored in 64bits
       regardless of CONFIG_LBD is still being discussed, therefore it's
       unadderessed here);
    
     - Fix all places where do_div() was being called with a bad divisor argument.

--- include/asm-generic/div64.h
+++ include/asm-generic/div64.h
@@ -32,11 +32,15 @@
 
 #elif BITS_PER_LONG == 32
 
-extern uint32_t __div64_32(uint64_t *dividend, uint32_t divisor) __attribute_pure__;
+extern uint32_t __div64_32(uint64_t *dividend, uint32_t divisor);
 
+/* The unnecessary pointer compare is there
+ * to check for type safety (n must be 64bit)
+ */
 # define do_div(n,base) ({				\
 	uint32_t __base = (base);			\
 	uint32_t __rem;					\
+	(void)(((typeof((n)) *)0) == ((uint64_t *)0));	\
 	if (likely(((n) >> 32) == 0)) {			\
 		__rem = (uint32_t)(n) % __base;		\
 		(n) = (uint32_t)(n) / __base;		\
