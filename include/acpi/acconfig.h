dc595eb92d4491930b345b2b7ef930797b1bcb21
Andy Grover <agrover@groveronline.com>
2003-07-13 23:13:10 -0700

    ACPI: Update version, and other trivialities

--- include/acpi/acconfig.h
+++ include/acpi/acconfig.h
@@ -64,7 +64,7 @@
 
 /* Version string */
 
-#define ACPI_CA_VERSION                 0x20030619
+#define ACPI_CA_VERSION                 0x20030714
 
 /* Maximum objects in the various object caches */
 
