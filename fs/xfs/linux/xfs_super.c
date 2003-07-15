7e2f1d98a863c174f299d6e3b7b33b8e84530c47
Nathan Scott <nathans@sgi.com>
2003-07-15 03:37:11 -0500

    [XFS] Max file size tweak for LBD - if LBD enabled on 32 bit platforms slightly bigger files are possible
    SGI Modid: 2.5.x-xfs:slinx:153108a

--- fs/xfs/linux/xfs_super.c
+++ fs/xfs/linux/xfs_super.c
@@ -103,6 +103,7 @@ xfs_max_file_offset(
 	unsigned int		blockshift)
 {
 	unsigned int		pagefactor = 1;
+	unsigned int		bitshift = BITS_PER_LONG - 1;
 
 	/* Figure out maximum filesize, on Linux this can depend on
 	 * the filesystem blocksize (on 32 bit platforms).
@@ -118,14 +119,17 @@ xfs_max_file_offset(
 	 * an [unsigned] long long.
 	 */
 
-#if defined(HAVE_SECTOR_T)
+#if BITS_PER_LONG == 32
+# if defined(HAVE_SECTOR_T)
 	ASSERT(sizeof(sector_t) == 8);
 	pagefactor = PAGE_CACHE_SIZE;
-#elif BITS_PER_LONG == 32
+	bitshift = BITS_PER_LONG;
+# else
 	pagefactor = PAGE_CACHE_SIZE >> (PAGE_CACHE_SHIFT - blockshift);
+# endif
 #endif
 
-	return (((__uint64_t)pagefactor) << (BITS_PER_LONG - 1)) - 1;
+	return (((__uint64_t)pagefactor) << bitshift) - 1;
 }
 
 STATIC __inline__ void
