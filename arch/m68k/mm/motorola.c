5dd971667ba010e05a315fb0d9b231f79825452a
Geert Uytterhoeven <geert@linux-m68k.org>
2003-07-25 19:26:04 -0700

    [PATCH] m68k page
    m68k page updates (from Roman Zippel):
      - Set _PAGE_RONLY bit and clear _PAGE_PRESENT to mark PAGE_NONE pte entries
        (this also makes PAGE_NONE fixup unnecessary)
      - Move _PAGE_FILE to a lower, so pte_to_pgoff/pgoff_to_pte becomes simpler

--- arch/m68k/mm/motorola.c
+++ arch/m68k/mm/motorola.c
@@ -220,18 +220,6 @@ void __init paging_init(void)
 		for (i = 0; i < 16; i++)
 			pgprot_val(protection_map[i]) |= _PAGE_CACHE040;
 	}
-	/* Fix the PAGE_NONE value. */
-	if (CPU_IS_040_OR_060) {
-		/* On the 680[46]0 we can use the _PAGE_SUPER bit.  */
-		pgprot_val(protection_map[0]) |= _PAGE_SUPER;
-		pgprot_val(protection_map[VM_SHARED]) |= _PAGE_SUPER;
-	} else {
-		/* Otherwise we must fake it. */
-		pgprot_val(protection_map[0]) &= ~_PAGE_PRESENT;
-		pgprot_val(protection_map[0]) |= _PAGE_FAKE_SUPER;
-		pgprot_val(protection_map[VM_SHARED]) &= ~_PAGE_PRESENT;
-		pgprot_val(protection_map[VM_SHARED]) |= _PAGE_FAKE_SUPER;
-	}
 
 	/*
 	 * Map the physical memory available into the kernel virtual
