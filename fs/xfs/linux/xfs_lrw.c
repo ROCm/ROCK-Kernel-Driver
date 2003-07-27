1ca335e9de47c1ec376f2a540ecd8a481e5c0270
Stephen Lord <lord@laptop.americas.sgi.com>
2003-07-26 22:47:34 -0500

    Only add bytes read into the stats if we did not get an error

--- fs/xfs/linux/xfs_lrw.c
+++ fs/xfs/linux/xfs_lrw.c
@@ -230,7 +230,8 @@ xfs_read(
 	ret = __generic_file_aio_read(iocb, iovp, segs, offset);
 	xfs_iunlock(ip, XFS_IOLOCK_SHARED);
 
-	XFS_STATS_ADD(xfsstats.xs_read_bytes, ret);
+	if (ret > 0)
+		XFS_STATS_ADD(xfsstats.xs_read_bytes, ret);
 
 	if (!invisible)
 		xfs_ichgtime(ip, XFS_ICHGTIME_ACC);
