e2d69a3604c69af30719e89391634e59c3c134e4
David S. Miller <davem@nuts.ninka.net>
2003-07-29 09:35:30 -0700

    [NET]: Need to export secpath_dup to modules.

--- net/netsyms.c
+++ net/netsyms.c
@@ -322,6 +322,7 @@ EXPORT_SYMBOL(xfrm_replay_advance);
 EXPORT_SYMBOL(xfrm_check_selectors);
 EXPORT_SYMBOL(xfrm_check_output);
 EXPORT_SYMBOL(__secpath_destroy);
+EXPORT_SYMBOL(secpath_dup);
 EXPORT_SYMBOL(xfrm_get_acqseq);
 EXPORT_SYMBOL(xfrm_parse_spi);
 EXPORT_SYMBOL(xfrm4_rcv);
