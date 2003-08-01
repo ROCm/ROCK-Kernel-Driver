d49ceaba9a9b5a0797abd8fafaf014a201c44dc4
Andrew Morton <akpm@osdl.org>
2003-07-31 20:01:37 -0700

    [PATCH] fix bogus IO error messages
    Since Jens added the pagecache readahead support in the block layer we've
    been getting bogus IO error messages from buffer.c due to __make_request
    calling end_io against a non-uptodate buffer.
    
    We can just use PF_READAHEAD to shut that up.  But really, we shouldn't even
    have allocated all those pages and submittted the readahead IO if the queue
    was congested.   We have the infrastructure to do that now.

--- fs/buffer.c
+++ fs/buffer.c
@@ -506,7 +506,8 @@ static void end_buffer_async_read(struct buffer_head *bh, int uptodate)
 		set_buffer_uptodate(bh);
 	} else {
 		clear_buffer_uptodate(bh);
-		buffer_io_error(bh);
+		if (!(current->flags & PF_READAHEAD))
+			buffer_io_error(bh);
 		SetPageError(page);
 	}
 
