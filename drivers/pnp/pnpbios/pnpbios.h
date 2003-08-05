76d0b039d22dc9d62d2060f905315707834041e7
Adam Belay <ambx1@neo.rr.com>
2003-08-05 20:34:05 +0000

    [PNPBIOS] Move low level code into a separate file
    This patch moves the low level bios calls to a separate file, 
    "bioscalls.c".  It is a cleanup that will improve organization of the
    pnpbios driver code.

--- drivers/pnp/pnpbios/pnpbios.h
+++ drivers/pnp/pnpbios/pnpbios.h
@@ -6,3 +6,6 @@ extern int pnpbios_parse_data_stream(struct pnp_dev *dev, struct pnp_bios_node *
 extern int pnpbios_read_resources_from_node(struct pnp_resource_table *res, struct pnp_bios_node * node);
 extern int pnpbios_write_resources_to_node(struct pnp_resource_table *res, struct pnp_bios_node * node);
 extern void pnpid32_to_pnpid(u32 id, char *str);
+
+extern void pnpbios_print_status(const char * module, u16 status);
+extern int pnpbios_probe_installation(void);
