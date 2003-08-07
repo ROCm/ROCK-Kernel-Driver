c987cb10a5f9a323b392a9883355a5f22f5585de
Andrew Morton <akpm@osdl.org>
2003-08-06 21:15:16 -0700

    [PATCH] initialise page->private
    From: Nathan Scott <nathans@sgi.com>
    
    XFS wants to use page->private as a bitmap of uptodate indicators for
    sub-page-sized blocks (which is one of the things ->provate was intended
    for).
    
    But someone needs to initialise ->private somewhere.  best to do it in the
    page allocator, so the zeroness of a new page's ->private becomes a
    system-wide thing.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -331,6 +331,7 @@ static void prep_new_page(struct page *page, int order)
 	page->flags &= ~(1 << PG_uptodate | 1 << PG_error |
 			1 << PG_referenced | 1 << PG_arch_1 |
 			1 << PG_checked | 1 << PG_mappedtodisk);
+	page->private = 0;
 	set_page_refs(page, order);
 }
 
