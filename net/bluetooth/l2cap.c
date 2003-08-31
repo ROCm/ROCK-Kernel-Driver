23d77265461b9493c5060945f3cd324ebc5b6421
Andrew Morton <akpm@osdl.org>
2003-08-31 04:32:26 -0700

    [PATCH] Fix bluetooth compile warnings

--- net/bluetooth/l2cap.c
+++ net/bluetooth/l2cap.c
@@ -200,7 +200,7 @@ found:
  */
 static struct sock *__l2cap_get_sock_by_psm(int state, u16 psm, bdaddr_t *src)
 {
-	struct sock *sk, *sk1 = NULL;
+	struct sock *sk = NULL, *sk1 = NULL;
 	struct hlist_node *node;
 
 	sk_for_each(sk, node, &l2cap_sk_list.head) {
