038921ab5cdb266b24ec390eeb82aff06681075b
Herbert Xu <herbert@gondor.apana.org.au>
2003-08-31 01:41:25 -0700

    [PATCH] free_netdev typo
    The free_netdev fixes in 2.6.0-test4 broke drivers/net/wan/cosa.c.
    This fixes it.

--- drivers/net/wan/cosa.c
+++ drivers/net/wan/cosa.c
@@ -632,7 +632,7 @@ static void sppp_channel_delete(struct channel_data *chan)
 {
 	sppp_detach(chan->pppdev.dev);
 	unregister_netdev(chan->pppdev.dev);
-	free_netdev(chan->ppp.dev);
+	free_netdev(chan->pppdev.dev);
 }
 
 static int cosa_sppp_open(struct net_device *d)
