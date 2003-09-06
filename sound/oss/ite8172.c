673ea6e36c81434da9713845dbecf04e8244f11f
Randy Dunlap <rddunlap@osdl.org>
2003-09-06 00:34:36 -0700

    [PATCH] janitor: oss/ite8172 copy*user fixes
    This patch catches insures proper return values from
    copy_to/from_user calls.
    
    From: Ron Gage <ron@rongage.org>

--- sound/oss/ite8172.c
+++ sound/oss/ite8172.c
@@ -1655,7 +1655,7 @@ static int it8172_ioctl(struct inode *inode, struct file *file,
 		if (count < 0)
 			count = 0;
 		cinfo.blocks = count >> s->dma_adc.fragshift;
-		return copy_to_user((void *)arg, &cinfo, sizeof(cinfo));
+		return copy_to_user((void *)arg, &cinfo, sizeof(cinfo)) ? -EFAULT : 0;
 
 	case SNDCTL_DSP_GETOPTR:
 		if (!(file->f_mode & FMODE_READ))
@@ -1678,7 +1678,7 @@ static int it8172_ioctl(struct inode *inode, struct file *file,
 		if (count < 0)
 			count = 0;
 		cinfo.blocks = count >> s->dma_dac.fragshift;
-		return copy_to_user((void *)arg, &cinfo, sizeof(cinfo));
+		return copy_to_user((void *)arg, &cinfo, sizeof(cinfo)) ? -EFAULT : 0;
 
 	case SNDCTL_DSP_GETBLKSIZE:
 		if (file->f_mode & FMODE_WRITE)
