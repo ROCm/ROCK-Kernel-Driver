5a44e1e30568ec45210f1e87e236ead11abf47dd
Randy Dunlap <rddunlap@osdl.org>
2003-09-12 01:32:38 -0400

    [PATCH] enable aha152x to build when AHA152X_DEBUG is defined
    patch_name:	aha152x_debugon.patch
    patch_version:	2003-09-10.11:02:09
    author:		Randy.Dunlap <rddunlap@osdl.org>
    description:	enable aha152x to build when AHA152X_DEBUG is defined;
    product:	Linux
    product_versions: 260-test5
    maintainer:	James Bottomley
    diffstat:	=
     drivers/scsi/aha152x.c |    2 +-
     1 files changed, 1 insertion(+), 1 deletion(-)

--- drivers/scsi/aha152x.c
+++ drivers/scsi/aha152x.c
@@ -1761,7 +1761,7 @@ static void reset_ports(struct Scsi_Host *shpnt)
 int aha152x_host_reset(Scsi_Cmnd * SCpnt)
 {
 #if defined(AHA152X_DEBUG)
-	struct Scsi_Host *shpnt = SCpnt->host;
+	struct Scsi_Host *shpnt = SCpnt->device->host;
 #endif
 
 	DPRINTK(debug_eh, DEBUG_LEAD "aha152x_host_reset(%p)\n", CMDINFO(SCpnt), SCpnt);
