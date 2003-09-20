9e79d82f4f404c66cdc2eb7b4a5752cbfa8baa3c
Randy Dunlap <rddunlap@osdl.org>
2003-09-20 05:55:45 -0400

    [PATCH] janitor: remove (or add) unneeded includes
    From: Randy Hron <rwhron@earthlink.net>

--- drivers/net/wan/comx-hw-munich.c
+++ drivers/net/wan/comx-hw-munich.c
@@ -30,7 +30,6 @@
 #include <linux/config.h>
 #include <linux/ctype.h>
 #include <linux/module.h>
-#include <linux/version.h>
 #include <linux/types.h>
 #include <linux/netdevice.h>
 #include <linux/proc_fs.h>
