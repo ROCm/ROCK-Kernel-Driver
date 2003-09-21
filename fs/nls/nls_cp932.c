1dffaaf7677d6e7e7e38f71451c6cdf0721d157e
Andrew Morton <akpm@osdl.org>
2003-09-21 01:37:51 -0700

    [PATCH] NLS: remove emacs metadata
    From: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    
    This elisp was obsolete on recently emacs's cc-mode. And this should
    be personally set.

--- fs/nls/nls_cp932.c
+++ fs/nls/nls_cp932.c
@@ -7908,21 +7908,3 @@ module_exit(exit_nls_cp932)
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_ALIAS_NLS(sjis);
-
-/*
- * Overrides for Emacs so that we follow Linus's tabbing style.
- * Emacs will notice this stuff at the end of the file and automatically
- * adjust the settings for this buffer only.  This must remain at the end
- * of the file.
- *
----------------------------------------------------------------------------
- * Local variables:
- * c-indent-level: 8
- * c-brace-imaginary-offset: 0
- * c-brace-offset: -8
- * c-argdecl-indent: 8
- * c-label-offset: -8
- * c-continued-statement-offset: 8
- * c-continued-brace-offset: 0
- * End:
- */
--- fs/nls/nls_cp950.c
+++ fs/nls/nls_cp950.c
@@ -9484,21 +9484,3 @@ module_exit(exit_nls_cp950)
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_ALIAS_NLS(big5);
-
-/*
- * Overrides for Emacs so that we follow Linus's tabbing style.
- * Emacs will notice this stuff at the end of the file and automatically
- * adjust the settings for this buffer only.  This must remain at the end
- * of the file.
- *
----------------------------------------------------------------------------
- * Local variables:
- * c-indent-level: 8
- * c-brace-imaginary-offset: 0
- * c-brace-offset: -8
- * c-argdecl-indent: 8
- * c-label-offset: -8
- * c-continued-statement-offset: 8
- * c-continued-brace-offset: 0
- * End:
- */
