66aa68ba6eb25e41c7910a4bd681080e3735db10
Greg Kroah-Hartman <greg@kroah.com>
2003-09-22 23:54:31 -0700

    [PATCH] I2C: remove the isa address check alltogether.

--- drivers/i2c/i2c-sensor.c
+++ drivers/i2c/i2c-sensor.c
@@ -50,10 +50,7 @@ int i2c_detect(struct i2c_adapter *adapter,
 		return -1;
 
 	for (addr = 0x00; addr <= (is_isa ? 0xffff : 0x7f); addr++) {
-		void *region_used = request_region(addr, 1, "foo");
-		release_region(addr, 1);
-		if ((is_isa && (region_used == NULL)) ||
-		    (!is_isa && i2c_check_addr(adapter, addr)))
+		if (!is_isa && i2c_check_addr(adapter, addr))
 			continue;
 
 		/* If it is in one of the force entries, we don't do any
