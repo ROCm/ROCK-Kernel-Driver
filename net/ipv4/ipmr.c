a552375fc9aae4af3652846b842f2bf972bfa966
David S. Miller <davem@nuts.ninka.net>
2003-09-25 13:35:51 -0700

    [IPV4]: Use correct ptrdiff_t printf format in ipmr.c

--- net/ipv4/ipmr.c
+++ net/ipv4/ipmr.c
@@ -1657,7 +1657,7 @@ static int ipmr_vif_seq_show(struct seq_file *seq, void *v)
 		const char *name =  vif->dev ? vif->dev->name : "none";
 
 		seq_printf(seq,
-			   "%2d %-10s %8ld %7ld  %8ld %7ld %05X %08X %08X\n",
+			   "%2Zd %-10s %8ld %7ld  %8ld %7ld %05X %08X %08X\n",
 			   vif - vif_table,
 			   name, vif->bytes_in, vif->pkt_in, 
 			   vif->bytes_out, vif->pkt_out,
