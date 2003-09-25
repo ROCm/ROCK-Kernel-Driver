b4b6051ab1c26f3cc46410b40677b21c189a6d0d
Linus Torvalds <torvalds@home.osdl.org>
2003-09-24 18:30:29 -0700

    DRI CVS merge: SiS driver updates from Eric Anholt.
    Major whitespace cleanups and fix a potential oops.
    
    The driver can now run with or without FB support.

--- drivers/char/drm/sis_drm.h
+++ drivers/char/drm/sis_drm.h
@@ -1,46 +1,33 @@
 
-#ifndef _sis_drm_public_h_
-#define _sis_drm_public_h_
+#ifndef __SIS_DRM_H__
+#define __SIS_DRM_H__
 
 /* SiS specific ioctls */
-#define SIS_IOCTL_FB_ALLOC		DRM_IOWR(0x44, drm_sis_mem_t)
-#define SIS_IOCTL_FB_FREE		DRM_IOW( 0x45, drm_sis_mem_t)
-#define SIS_IOCTL_AGP_INIT		DRM_IOWR(0x53, drm_sis_agp_t)
-#define SIS_IOCTL_AGP_ALLOC		DRM_IOWR(0x54, drm_sis_mem_t)
-#define SIS_IOCTL_AGP_FREE		DRM_IOW( 0x55, drm_sis_mem_t)
-#define SIS_IOCTL_FLIP			DRM_IOW( 0x48, drm_sis_flip_t)
-#define SIS_IOCTL_FLIP_INIT		DRM_IO(  0x49)
-#define SIS_IOCTL_FLIP_FINAL		DRM_IO(  0x50)
+#define DRM_IOCTL_SIS_FB_ALLOC		DRM_IOWR(0x44, drm_sis_mem_t)
+#define DRM_IOCTL_SIS_FB_FREE		DRM_IOW( 0x45, drm_sis_mem_t)
+#define DRM_IOCTL_SIS_AGP_INIT		DRM_IOWR(0x53, drm_sis_agp_t)
+#define DRM_IOCTL_SIS_AGP_ALLOC		DRM_IOWR(0x54, drm_sis_mem_t)
+#define DRM_IOCTL_SIS_AGP_FREE		DRM_IOW( 0x55, drm_sis_mem_t)
+#define DRM_IOCTL_SIS_FB_INIT		DRM_IOW( 0x56, drm_sis_fb_t)
+/*
+#define DRM_IOCTL_SIS_FLIP		DRM_IOW( 0x48, drm_sis_flip_t)
+#define DRM_IOCTL_SIS_FLIP_INIT		DRM_IO(  0x49)
+#define DRM_IOCTL_SIS_FLIP_FINAL	DRM_IO(  0x50)
+*/
 
 typedef struct {
-  int context;
-  unsigned int offset;
-  unsigned int size;
-  unsigned int free;
+	int context;
+	unsigned int offset;
+	unsigned int size;
+	unsigned long free;
 } drm_sis_mem_t;
 
 typedef struct {
-  unsigned int offset, size;
+	unsigned int offset, size;
 } drm_sis_agp_t;
 
 typedef struct {
-  unsigned int left, right;
-} drm_sis_flip_t;
+	unsigned int offset, size;
+} drm_sis_fb_t;
 
-#ifdef __KERNEL__
-
-int sis_fb_alloc(struct inode *inode, struct file *filp, unsigned int cmd,
-		  unsigned long arg);
-int sis_fb_free(struct inode *inode, struct file *filp, unsigned int cmd,
-		  unsigned long arg);
-
-int sisp_agp_init(struct inode *inode, struct file *filp, unsigned int cmd,
-		  unsigned long arg);
-int sisp_agp_alloc(struct inode *inode, struct file *filp, unsigned int cmd,
-		  unsigned long arg);
-int sisp_agp_free(struct inode *inode, struct file *filp, unsigned int cmd,
-		  unsigned long arg);
-
-#endif
-
-#endif
+#endif /* __SIS_DRM_H__ */
