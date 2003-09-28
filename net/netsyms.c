27ae266e7031b590a67ec2ecae1942ff3b456c2b
Arnaldo Carvalho de Melo <acme@conectiva.com.br>
2003-09-28 17:58:51 -0300

    o NET: move fs/file_table.c EXPORT_SYMBOLs from net/netsyms.c

--- net/netsyms.c
+++ net/netsyms.c
@@ -4,10 +4,3 @@
  *  Symbol table for the linux networking subsystem. Moved here to
  *  make life simpler in ksyms.c.
  */
-
-#include <linux/config.h>
-#include <linux/fs.h>
-#include <linux/module.h>
-
-/* Needed by unix.o */
-EXPORT_SYMBOL(files_stat);
