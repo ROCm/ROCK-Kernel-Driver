5e0f970385d7be6fa211ff1ef36529d03b9b7cb6
Arnaldo Carvalho de Melo <acme@conectiva.com.br>
2003-09-28 12:38:21 -0300

    o NET: move net/ipv4/tcp_minisocks.c EXPORT_SYMBOLs from net/netsyms.c

--- net/netsyms.c
+++ net/netsyms.c
@@ -168,17 +168,12 @@ EXPORT_SYMBOL(ip_queue_xmit);
 EXPORT_SYMBOL(memcpy_fromiovecend);
 EXPORT_SYMBOL(csum_partial_copy_fromiovecend);
 /* UDP/TCP exported functions for TCPv6 */
-EXPORT_SYMBOL(tcp_check_req);
-EXPORT_SYMBOL(tcp_child_process);
 EXPORT_SYMBOL(tcp_parse_options);
 EXPORT_SYMBOL(tcp_rcv_established);
 EXPORT_SYMBOL(tcp_init_xmit_timers);
 EXPORT_SYMBOL(tcp_clear_xmit_timers);
 EXPORT_SYMBOL(tcp_statistics);
 EXPORT_SYMBOL(tcp_rcv_state_process);
-EXPORT_SYMBOL(tcp_timewait_state_process);
-EXPORT_SYMBOL(tcp_create_openreq_child);
-EXPORT_SYMBOL(tcp_tw_deschedule);
 EXPORT_SYMBOL(tcp_delete_keepalive_timer);
 EXPORT_SYMBOL(tcp_reset_keepalive_timer);
 EXPORT_SYMBOL(net_statistics); 
@@ -188,7 +183,6 @@ EXPORT_SYMBOL(tcp_cwnd_application_limited);
 
 #ifdef CONFIG_SYSCTL
 extern int sysctl_tcp_tw_recycle;
-EXPORT_SYMBOL(sysctl_tcp_tw_recycle); 
 #endif
 
 EXPORT_SYMBOL(ip_generic_getfrag);
