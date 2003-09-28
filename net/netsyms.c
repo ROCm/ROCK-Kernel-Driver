e1e1fc67b25b36a22183d18dd6bf353312628196
Arnaldo Carvalho de Melo <acme@conectiva.com.br>
2003-09-28 17:24:16 -0300

    o NET: move net/802/*.c EXPORT_SYMBOLs from net/netsyms.c
    Also add #include <linux/netdevice.h> to include/net/pkt_sched.h, as it uses
    functions defined on this include (netif_queue_stopped, etc). Ah, the other
    datalink stuff was moved as well (eth.c, pe2.c)

--- net/netsyms.c
+++ net/netsyms.c
@@ -8,14 +8,6 @@
 #include <linux/config.h>
 #include <linux/module.h>
 
-#include <linux/netdevice.h>
-#include <linux/fddidevice.h>
-#include <linux/trdevice.h>
-#include <linux/fcdevice.h>
-#include <linux/etherdevice.h>
-#ifdef CONFIG_HIPPI
-#include <linux/hippidevice.h>
-#endif
 #include <net/pkt_sched.h>
 
 #ifdef CONFIG_INET
@@ -27,22 +19,9 @@
 #endif
 #endif
 
-#ifdef CONFIG_IPX_MODULE
-extern struct datalink_proto   *make_EII_client(void);
-extern struct datalink_proto   *make_8023_client(void);
-extern void destroy_EII_client(struct datalink_proto *);
-extern void destroy_8023_client(struct datalink_proto *);
-#endif
-
 /* Needed by unix.o */
 EXPORT_SYMBOL(files_stat);
 
-#ifdef CONFIG_IPX_MODULE
-EXPORT_SYMBOL(make_8023_client);
-EXPORT_SYMBOL(destroy_8023_client);
-EXPORT_SYMBOL(make_EII_client);
-EXPORT_SYMBOL(destroy_EII_client);
-#endif
 
 #ifdef CONFIG_INET
 #if defined(CONFIG_INET_ESP) || defined(CONFIG_INET_ESP_MODULE) || defined(CONFIG_INET6_ESP) || defined(CONFIG_INET6_ESP_MODULE)
@@ -52,25 +31,13 @@ EXPORT_SYMBOL_GPL(skb_to_sgvec);
 #endif
 #endif  /* CONFIG_INET */
 
-#ifdef CONFIG_TR
-EXPORT_SYMBOL(tr_source_route);
-EXPORT_SYMBOL(tr_type_trans);
-#endif
 
 /* support for loadable net drivers */
 #ifdef CONFIG_NET
 EXPORT_SYMBOL(loopback_dev);
-EXPORT_SYMBOL(eth_type_trans);
-#ifdef CONFIG_FDDI
-EXPORT_SYMBOL(fddi_type_trans);
-#endif /* CONFIG_FDDI */
 EXPORT_SYMBOL(dev_base);
 EXPORT_SYMBOL(dev_base_lock);
 EXPORT_SYMBOL(__kill_fasync);
-
-#ifdef CONFIG_HIPPI
-EXPORT_SYMBOL(hippi_type_trans);
-#endif
 
 #ifdef CONFIG_NET_SCHED
 EXPORT_SYMBOL(pfifo_qdisc_ops);
