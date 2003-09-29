f3f72ed268942e8eb19a98f6e8df3e8c1ffe7dfb
Geert Uytterhoeven <geert@linux-m68k.org>
2003-09-28 20:03:47 -0700

    [PATCH] Amiga GVP-II SCSI fix
    GVP-II SCSI: Fix compilation by getting rid of the obsolete SCSI host instance
    loop and using per-card interrupt handlers instead.

--- drivers/scsi/gvp11.c
+++ drivers/scsi/gvp11.c
@@ -25,29 +25,20 @@
 #define DMA(ptr) ((gvp11_scsiregs *)((ptr)->base))
 #define HDATA(ptr) ((struct WD33C93_hostdata *)((ptr)->hostdata))
 
-static struct Scsi_Host *first_instance = NULL;
-static Scsi_Host_Template *gvp11_template;
-
-static irqreturn_t gvp11_intr (int irq, void *dummy, struct pt_regs *fp)
+static irqreturn_t gvp11_intr (int irq, void *_instance, struct pt_regs *fp)
 {
     unsigned long flags;
     unsigned int status;
-    struct Scsi_Host *instance;
-    int handled = 0;
+    struct Scsi_Host *instance = (struct Scsi_Host *)_instance;
 
-    for (instance = first_instance; instance &&
-	 instance->hostt == gvp11_template; instance = instance->next)
-    {
-	status = DMA(instance)->CNTR;
-	if (!(status & GVP11_DMAC_INT_PENDING))
-	    continue;
+    status = DMA(instance)->CNTR;
+    if (!(status & GVP11_DMAC_INT_PENDING))
+	return IRQ_NONE;
 
-	spin_lock_irqsave(instance->host_lock, flags);
-	wd33c93_intr (instance);
-	spin_unlock_irqrestore(instance->host_lock, flags);
-	handled = 1;
-    }
-    return IRQ_RETVAL(handled);
+    spin_lock_irqsave(instance->host_lock, flags);
+    wd33c93_intr(instance);
+    spin_unlock_irqrestore(instance->host_lock, flags);
+    return IRQ_HANDLED;
 }
 
 static int gvp11_xfer_mask = 0;
@@ -177,8 +168,6 @@ static void dma_stop (struct Scsi_Host *instance, Scsi_Cmnd *SCpnt,
     }
 }
 
-static int num_gvp11 = 0;
-
 #define CHECK_WD33C93
 
 int __init gvp11_detect(Scsi_Host_Template *tpnt)
@@ -190,6 +179,7 @@ int __init gvp11_detect(Scsi_Host_Template *tpnt)
     struct zorro_dev *z = NULL;
     unsigned int default_dma_xfer_mask;
     wd33c93_regs regs;
+    int num_gvp11 = 0;
 #ifdef CHECK_WD33C93
     volatile unsigned char *sasr_3393, *scmd_3393;
     unsigned char save_sasr;
@@ -339,13 +329,10 @@ int __init gvp11_detect(Scsi_Host_Template *tpnt)
 		     (epc & GVP_SCSICLKMASK) ? WD33C93_FS_8_10
 					     : WD33C93_FS_12_15);
 
-	if (num_gvp11++ == 0) {
-		first_instance = instance;
-		gvp11_template = instance->hostt;
-		request_irq(IRQ_AMIGA_PORTS, gvp11_intr, SA_SHIRQ,
-			    "GVP11 SCSI", gvp11_intr);
-	}
+	request_irq(IRQ_AMIGA_PORTS, gvp11_intr, SA_SHIRQ, "GVP11 SCSI",
+		    instance);
 	DMA(instance)->CNTR = GVP11_DMAC_INT_ENABLE;
+	num_gvp11++;
 	continue;
 
 release:
@@ -391,8 +378,7 @@ int gvp11_release(struct Scsi_Host *instance)
 #ifdef MODULE
     DMA(instance)->CNTR = 0;
     release_mem_region(ZTWO_PADDR(instance->base), 256);
-    if (--num_gvp11 == 0)
-	    free_irq(IRQ_AMIGA_PORTS, gvp11_intr);
+    free_irq(IRQ_AMIGA_PORTS, instance);
     wd33c93_release();
 #endif
     return 1;
