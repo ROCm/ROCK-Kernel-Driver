b5e489808a42b03c37f5a75d086fc26d93bbe64a
Len Brown <len.brown@intel.com>
2003-09-29 16:53:46 -0400

    [ACPI] acpi_pci_link_allocate() should stick with irq.active if set. (Andrew de Quincey) Fixes OSDL #1186 "broken USB" and others

--- drivers/acpi/pci_link.c
+++ drivers/acpi/pci_link.c
@@ -500,15 +500,15 @@ static int acpi_pci_link_allocate(struct acpi_pci_link* link) {
 		irq = link->irq.active;
 	} else {
 		irq = link->irq.possible[0];
-	}
 
-	/* 
-	 * Select the best IRQ.  This is done in reverse to promote 
-	 * the use of IRQs 9, 10, 11, and >15.
-	 */
-	for (i=(link->irq.possible_count-1); i>0; i--) {
-		if (acpi_irq_penalty[irq] > acpi_irq_penalty[link->irq.possible[i]])
-			irq = link->irq.possible[i];
+		/*
+		 * Select the best IRQ.  This is done in reverse to promote
+		 * the use of IRQs 9, 10, 11, and >15.
+		 */
+		for (i=(link->irq.possible_count-1); i>0; i--) {
+			if (acpi_irq_penalty[irq] > acpi_irq_penalty[link->irq.possible[i]])
+				irq = link->irq.possible[i];
+		}
 	}
 
 	/* Attempt to enable the link device at this IRQ. */
