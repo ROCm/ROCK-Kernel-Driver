0b8c03e488d0a5ff989f8d9f757e87cb0f1fd85b
David S. Miller <davem@nuts.ninka.net>
2003-10-20 08:34:44 -0700

    [LLC]: Make LLC2 compile with PROC_FS=n

--- include/net/llc.h
+++ include/net/llc.h
@@ -88,4 +88,12 @@ extern int llc_build_and_send_ui_pkt(struct llc_sap *sap, struct sk_buff *skb,
 
 extern int llc_station_init(void);
 extern void llc_station_exit(void);
+
+#ifdef CONFIG_PROC_FS
+extern int llc_proc_init(void);
+extern void llc_proc_exit(void);
+#else
+#define llc_proc_init()	(0)
+#define llc_proc_exit()	do { } while(0)
+#endif /* CONFIG_PROC_FS */
 #endif /* LLC_H */
