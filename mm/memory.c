453dc491ce6396a40854a2229a1a483b0d285e0e
Gerd Hoffmann <kraxel@suse.de>
2003-10-27 15:01:59 +0000

    fixup UML fixaddr stuff.
    suse-commit: 858ecdc975209c3514cbf6844dec55851fd9b26d

--- mm/memory.c
+++ mm/memory.c
@@ -682,7 +682,7 @@ static inline struct page *get_page_map(struct page *page)
 	return page;
 }
 
-
+#ifdef FIXADDR_START
 static struct vm_area_struct fixmap_vma = {
 	/* Catch users - if there are any valid
 	   ones, we can make this be "&init_mm" or
@@ -700,6 +700,7 @@ static int init_fixmap_vma(void)
 }
 
 __initcall(init_fixmap_vma);
+#endif
 
 int get_user_pages(struct task_struct *tsk, struct mm_struct *mm,
 		unsigned long start, int len, int write, int force,
