1fb13dc1b2806a56e99435dff596fd2f2d01836c
Dave Jones <davej@redhat.com>
2003-11-21 12:38:12 +0000

    [AGPGART] Mask memory after allocation We missed a few cases where we need to do this. Fix from Alan Hourihane.

--- drivers/char/agp/generic.c
+++ drivers/char/agp/generic.c
@@ -180,7 +180,8 @@ struct agp_memory *agp_allocate_memory(size_t page_count, u32 type)
 			agp_free_memory(new);
 			return NULL;
 		}
-		new->memory[i] = virt_to_phys(addr);
+		new->memory[i] =
+			agp_bridge->driver->mask_memory(virt_to_phys(addr), type);
 		new->page_count++;
 	}
 
