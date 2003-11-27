83bd35ae50eef86b3e62c80d32a1a074450b3503
Jaroslav Kysela <perex@suse.cz>
2003-11-27 01:30:41 +0100

    ALSA CVS update - Takashi Iwai <tiwai@suse.de> VIA82xx driver - fixed the (syntax) description of dxs_support module option.

--- sound/pci/via82xx.c
+++ sound/pci/via82xx.c
@@ -106,7 +106,7 @@ MODULE_PARM_DESC(ac97_clock, "AC'97 codec clock (default 48000Hz).");
 MODULE_PARM_SYNTAX(ac97_clock, SNDRV_ENABLED ",default:48000");
 MODULE_PARM(dxs_support, "1-" __MODULE_STRING(SNDRV_CARDS) "i");
 MODULE_PARM_DESC(dxs_support, "Support for DXS channels (0 = auto, 1 = enable, 2 = disable, 3 = 48k only, 4 = no VRA)");
-MODULE_PARM_SYNTAX(dxs_support, SNDRV_ENABLED ",allows:{{0,3}},dialog:list");
+MODULE_PARM_SYNTAX(dxs_support, SNDRV_ENABLED ",allows:{{0,4}},dialog:list");
 
 
 /* pci ids */
