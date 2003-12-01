fbbcc35c0acf482e59847e9f5ba1877ba2db9930
Jaroslav Kysela <perex@suse.cz>
2003-12-01 11:52:23 +0100

    ALSA CVS update - Takashi Iwai <tiwai@suse.de> AMD InterWave driver - fixed the detection of STB board via pnp.

--- sound/isa/gus/interwave.c
+++ sound/isa/gus/interwave.c
@@ -229,12 +229,12 @@ static int __devinit snd_interwave_detect_stb(struct snd_interwave *iwcard,
 				break;
 			port += 0x10;
 		}
-		if (port > 0x380)
-			return -ENODEV;
 	} else {
-		if ((iwcard->i2c_res = request_region(port, 1, "InterWave (I2C bus)")) != NULL)
-			return -ENODEV;
+		iwcard->i2c_res = request_region(port, 1, "InterWave (I2C bus)");
 	}
+	if (iwcard->i2c_res == NULL)
+		return -ENODEV;
+
 	sprintf(name, "InterWave-%i", card->number);
 	if ((err = snd_i2c_bus_create(card, name, NULL, &bus)) < 0)
 		return err;
