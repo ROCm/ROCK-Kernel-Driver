3cd9860993d079686ff0025ef7e9e2f289bd2a8f
Jaroslav Kysela <perex@suse.cz>
2003-12-01 13:02:07 +0100

    ALSA 1.0.0rc1

--- include/sound/version.h
+++ include/sound/version.h
@@ -1,3 +1,3 @@
 /* include/version.h.  Generated automatically by configure.  */
-#define CONFIG_SND_VERSION "1.0.0pre3"
-#define CONFIG_SND_DATE " (Mon Dec 01 08:36:23 2003 UTC)"
+#define CONFIG_SND_VERSION "1.0.0rc1"
+#define CONFIG_SND_DATE " (Mon Dec 01 09:52:55 2003 UTC)"
