422cbad199e8de456b3f8a0f18f8ae62da661c2e
Henning Meier-Geinitz <henning@meier-geinitz.de>
2003-12-08 01:25:34 -0800

    [PATCH] USB scanner driver: new device ids
    Added vendor/product ids for Epson, Genius, Microtek, Plustek,
    Reflecta, and Visioneer scanners. Removed ids for HP PSC devices as
    these are supported by the hpoj userspace driver.

--- drivers/usb/image/scanner.c
+++ drivers/usb/image/scanner.c
@@ -380,6 +380,10 @@
  *      Visioneer scanners.
  *    - Added test for USB_CLASS_CDC_DATA which is used by some fingerprint scanners.
  *
+ * 0.4.16  2003-11-04
+ *    - Added vendor/product ids for Epson, Genius, Microtek, Plustek, Reflecta, and
+ *      Visioneer scanners. Removed ids for HP PSC devices as these are supported by
+ *      the hpoj userspace driver.
  *
  * TODO
  *    - Performance
