cec9104e5b7a3a866efb6df6c601940694a3db88
Alexander Viro <viro@parcelfarce.linux.theplanet.co.uk>
2004-01-10 05:31:52 -0500

    [netdrvr ether00] s/kfree/free_netdev/ ; remove redundant memset() calls

--- drivers/net/arm/ether00.c
+++ drivers/net/arm/ether00.c
@@ -923,8 +923,6 @@ static int ether00_add_device(struct pldhs_dev_info* dev_info,void* dev_ps_data)
 		result = -ENOMEM;
 		goto out_release;
 	}
-	memset(dev,0,sizeof(struct net_device));
-	memset(dev->priv, 0, sizeof(struct net_priv));
 	priv = dev->priv;
 
 	priv->tq_memupdate.routine=ether00_mem_update;
@@ -966,7 +964,7 @@ static int ether00_add_device(struct pldhs_dev_info* dev_info,void* dev_ps_data)
  out_unmap:
 	iounmap(map_addr);
  out_kfree:
-	kfree(dev);
+	free_netdev(dev);
  out_release:
 	release_mem_region(dev_info->base_addr, MAC_REG_SIZE);
 	return result;
