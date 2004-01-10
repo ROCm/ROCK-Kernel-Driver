5828f47dd2fd18341f4253e9294bf47b13a663d1
Stephen Hemminger <shemminger@osdl.org>
2004-01-10 05:08:56 -0500

    [PATCH] (32/42) mac8390
    NE57-mac8390
    	* switched mac8390 to dynamic allocation
    	* mac8390: fixed resource leaks on failure exits
    	* get rid of MOD_INC/DEC

--- drivers/net/Space.c
+++ drivers/net/Space.c
@@ -88,7 +88,7 @@ extern struct net_device *tc515_probe(int unit);
 extern struct net_device *lance_probe(int unit);
 extern int mace_probe(struct net_device *dev);
 extern int macsonic_probe(struct net_device *dev);
-extern int mac8390_probe(struct net_device *dev);
+extern struct net_device *mac8390_probe(int unit);
 extern struct net_device *mac89x0_probe(int unit);
 extern struct net_device *mc32_probe(int unit);
 extern struct net_device *cops_probe(int unit);
@@ -326,13 +326,13 @@ static struct devprobe m68k_probes[] __initdata = {
 #ifdef CONFIG_MACSONIC		/* Mac SONIC-based Ethernet of all sorts */ 
 	{macsonic_probe, 0},
 #endif
-#ifdef CONFIG_MAC8390           /* NuBus NS8390-based cards */
-	{mac8390_probe, 0},
-#endif
 	{NULL, 0},
 };
 
 static struct devprobe2 m68k_probes2[] __initdata = {
+#ifdef CONFIG_MAC8390           /* NuBus NS8390-based cards */
+	{mac8390_probe, 0},
+#endif
 #ifdef CONFIG_MAC89x0
  	{mac89x0_probe, 0},
 #endif
