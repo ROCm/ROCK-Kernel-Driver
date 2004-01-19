44a98b7218d9983886fb56059832f3ca5929f559
Jean Delvare <khali@linux-fr.org>
2004-01-18 20:49:57 -0800

    [PATCH] I2C: Fix i2c-core.c with DEBUG
    At the moment, i2c-core.c fails compiling with DEBUG. Following patch
    should fix that.

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -373,7 +373,7 @@ int i2c_attach_client(struct i2c_client *client)
 	}
 
 	DEB(dev_dbg(&adapter->dev, "client [%s] registered to adapter\n",
-			client->dev.name));
+			client->name));
 
 	if (client->flags & I2C_CLIENT_ALLOW_USE)
 		client->usage_count = 0;
