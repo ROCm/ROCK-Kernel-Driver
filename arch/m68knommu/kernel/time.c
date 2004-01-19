114d9308fb03a5fc8c9807f77c4cfce3bf616488
Andrew Morton <akpm@osdl.org>
2004-01-18 18:40:10 -0800

    [PATCH] sched_clock() for m68knommu architectures
    From: gerg@snapgear.com
    
    Implement the sched_clock() function for m68knommu architectures.

--- arch/m68knommu/kernel/time.c
+++ arch/m68knommu/kernel/time.c
@@ -202,4 +202,12 @@ int do_settimeofday(struct timespec *tv)
 	return 0;
 }
 
+/*
+ * Scheduler clock - returns current time in nanosec units.
+ */
+unsigned long long sched_clock(void)
+{
+	return (unsigned long long)jiffies * (1000000000 / HZ);
+}
+
 EXPORT_SYMBOL(do_settimeofday);
