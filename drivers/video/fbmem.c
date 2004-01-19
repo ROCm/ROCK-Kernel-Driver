d2d4d371639107c0121f05b295aeb2988d641c43
Andrew Morton <akpm@osdl.org>
2004-01-18 18:05:15 -0800

    [PATCH] kyrofb support
    From: Paul Mundt <lethal@linux-sh.org>
    
    This patch adds support for the Kyro graphics boards (STG4000/PowerVR
    3/etc= .) to 2.6.  This is a direct port and substantial cleanup / rewrite
    of the 2.4 driver that's available in the sh64 tree at linux-sh.bkbits.net.
    
    Some of the overlay code and the STG4000 bits are still a bit ugly, so be
    forewarned.

--- drivers/video/fbmem.c
+++ drivers/video/fbmem.c
@@ -159,6 +159,8 @@ extern int tcx_init(void);
 extern int tcx_setup(char*);
 extern int leo_init(void);
 extern int leo_setup(char*);
+extern int kyrofb_init(void);
+extern int kyrofb_setup(char*);
 
 static struct {
 	const char *name;
@@ -360,6 +362,10 @@ static struct {
 #ifdef CONFIG_FB_VOODOO1
 	{ "sstfb", sstfb_init, sstfb_setup },
 #endif
+#ifdef CONFIG_FB_KYRO
+	{ "kyrofb", kyrofb_init, kyrofb_setup },
+#endif
+
 	/*
 	 * Generic drivers that don't use resource management (yet)
 	 */
