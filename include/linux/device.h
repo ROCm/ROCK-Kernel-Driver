73009dd485707254f4ae4a59f242fa3e796041cb
Hollis Blanchard <hollisb@us.ibm.com>
2004-01-19 00:31:36 -0800

    [PATCH] Driver Core: add device_find() function
    Greg KH wrote:
    >
    > How about just adding a device_find() function to the driver core, where
    > you pass in a name and a type, so that others can use it?
    
    Something like this?

--- include/linux/device.h
+++ include/linux/device.h
@@ -354,6 +354,7 @@ extern int (*platform_notify_remove)(struct device * dev);
  */
 extern struct device * get_device(struct device * dev);
 extern void put_device(struct device * dev);
+extern struct device *device_find(const char *name, struct bus_type *bus);
 
 
 /* drivers/base/platform.c */
