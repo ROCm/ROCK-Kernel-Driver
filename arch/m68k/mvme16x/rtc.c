50b5381333091292faacf2ddacdb402b0058a768
Andrew Morton <akpm@osdl.org>
2004-01-18 18:33:22 -0800

    [PATCH] MVME16x RTC C99
    From: Geert Uytterhoeven <geert@linux-m68k.org>
    
    MVME16x RTC: Use C99 struct initializers

--- arch/m68k/mvme16x/rtc.c
+++ arch/m68k/mvme16x/rtc.c
@@ -155,9 +155,9 @@ static struct file_operations rtc_fops = {
 
 static struct miscdevice rtc_dev=
 {
-	RTC_MINOR,
-	"rtc",
-	&rtc_fops
+	.minor =	RTC_MINOR,
+	.name =		"rtc",
+	.fops =		&rtc_fops
 };
 
 int __init rtc_MK48T08_init(void)
