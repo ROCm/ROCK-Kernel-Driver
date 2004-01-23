d4becc82fb783de0c27a41bf7e6f8ee88159b7a2
Len Brown <len.brown@intel.com>
2004-01-23 13:48:11 -0500

    [ACPI] acpi_bus_add() ignored _STA's return value   from Bjorn Helgaas

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -706,11 +706,11 @@ acpi_bus_add (
 	switch (type) {
 	case ACPI_BUS_TYPE_DEVICE:
 		result = acpi_bus_get_status(device);
-		if (!result)
-			break;
-		if (!device->status.present) 
+		if (ACPI_FAILURE(result) || !device->status.present) {
 			result = -ENOENT;
-		goto end;
+			goto end;
+		}
+		break;
 	default:
 		STRUCT_TO_INT(device->status) = 0x0F;
 		break;
