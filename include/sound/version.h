d0f1fe70bc9c92d43cbe22b472a6b2ea04f0fc4e
Jaroslav Kysela <perex@suse.cz>
2004-01-25 12:48:45 +0100

    ALSA - updated date identification

--- include/sound/version.h
+++ include/sound/version.h
@@ -1,3 +1,3 @@
 /* include/version.h.  Generated automatically by configure.  */
 #define CONFIG_SND_VERSION "1.0.1"
-#define CONFIG_SND_DATE " (Tue Dec 30 10:04:14 2003 UTC)"
+#define CONFIG_SND_DATE " (Fri Jan 23 13:43:51 2004 UTC)"
