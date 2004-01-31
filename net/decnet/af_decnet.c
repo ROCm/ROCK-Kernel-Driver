01a9b9922833f97f6c892849290e15d28b2abdcc
Patrick Caulfield <pcaulfie@redhat.com>
2004-01-31 01:47:32 -0800

    [DECNET]: Made SDF_WILD sockets actually work.

--- net/decnet/af_decnet.c
+++ net/decnet/af_decnet.c
@@ -163,7 +163,7 @@ static struct hlist_head *dn_find_list(struct sock *sk)
 	struct dn_scp *scp = DN_SK(sk);
 
 	if (scp->addr.sdn_flags & SDF_WILD)
-		return hlist_empty(&dn_wild_sk) ? NULL : &dn_wild_sk;
+		return hlist_empty(&dn_wild_sk) ? &dn_wild_sk : NULL;
 
 	return &dn_sk_hash[scp->addrloc & DN_SK_HASH_MASK];
 }
