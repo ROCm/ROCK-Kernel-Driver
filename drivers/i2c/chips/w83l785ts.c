129ccc33c5d25feed3e1feabaaebcaf13a1ad58e
Jean Delvare <khali@linux-fr.org>
2004-02-01 22:51:45 -0800

    [PATCH] I2C: Bring w83l785ts in compliance with sysfs naming conventions
    Here is a patch that brings the w83l785ts driver in compliance with
    sysfs naming conventions. This is pretty much the same problem and
    solution that occured very recently with the lm75 and lm78 drivers.
    
    The patch was tested to work fine by James Bolt.

--- drivers/i2c/chips/w83l785ts.c
+++ drivers/i2c/chips/w83l785ts.c
@@ -137,8 +137,8 @@ static ssize_t show_temp_over(struct device *dev, char *buf)
 	return sprintf(buf, "%d\n", TEMP_FROM_REG(data->temp_over));
 }
 
-static DEVICE_ATTR(temp_input, S_IRUGO, show_temp, NULL)
-static DEVICE_ATTR(temp_max, S_IRUGO, show_temp_over, NULL)
+static DEVICE_ATTR(temp_input1, S_IRUGO, show_temp, NULL)
+static DEVICE_ATTR(temp_max1, S_IRUGO, show_temp_over, NULL)
 
 /*
  * Real code
@@ -249,8 +249,8 @@ static int w83l785ts_detect(struct i2c_adapter *adapter, int address, int kind)
 	 */
 
 	/* Register sysfs hooks */
-	device_create_file(&new_client->dev, &dev_attr_temp_input);
-	device_create_file(&new_client->dev, &dev_attr_temp_max);
+	device_create_file(&new_client->dev, &dev_attr_temp_input1);
+	device_create_file(&new_client->dev, &dev_attr_temp_max1);
 
 	return 0;
 
