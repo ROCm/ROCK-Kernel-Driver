70fa9749e4b3a2b0debef832b1ddadf612177a97
Andrew Morton <akpm@osdl.org>
2004-02-12 23:45:19 -0800

    [PATCH] sh: Wrap fb_read/writeX() to __raw_read/writeX()
    From: Paul Mundt <lethal@linux-sh.org>
    
    This adds sh to the list of platforms that wrap fb_readX/fb_writeX() to
    __raw_readX/__raw_writeX().  This is needed so that the generic fb read/write
    routines will wrap properly through the sh machvec and use the appropriate
    board-specific I/O routines.

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -476,7 +476,7 @@ struct fb_info {
 #define fb_writeq sbus_writeq
 #define fb_memset sbus_memset_io
 
-#elif defined(__i386__) || defined(__alpha__) || defined(__x86_64__) || defined(__hppa__)
+#elif defined(__i386__) || defined(__alpha__) || defined(__x86_64__) || defined(__hppa__) || defined(__sh__)
 
 #define fb_readb __raw_readb
 #define fb_readw __raw_readw
