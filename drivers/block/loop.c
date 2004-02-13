8c011a9974bbf219854d74eaa6ce904cda3ded34
Hubert Mantel <mantel@suse.de>
2004-02-13 15:14:54 +0000

    work around umount problem of loopback devices
    suse-commit: 738f97da47f0af9147dc798e32f54d650821be82

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1098,7 +1098,11 @@ static int lo_open(struct inode *inode, struct file *file)
 
 static int lo_release(struct inode *inode, struct file *file)
 {
-	struct loop_device *lo = inode->i_bdev->bd_disk->private_data;
+	struct block_device *bdev = inode->i_bdev;
+	struct loop_device *lo = bdev->bd_disk->private_data;
+
+	if (!(lo->lo_flags & LO_FLAGS_READ_ONLY))
+		fsync_bdev(bdev);
 
 	down(&lo->lo_ctl_mutex);
 	--lo->lo_refcnt;
