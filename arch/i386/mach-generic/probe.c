fac09b2a638b4050ac5dd2c8147b052a5ae1fe91
Andreas Gruenbacher <agruen@suse.de>
2004-02-25 17:49:16 +0000

    - patches.fixes/missing-exports.diff: bcm5700.ko requires the   genapic symbol. - patches.fixes/icn-strchr-bug.diff: strpbrk gets converted to   strchr from libgcc by the compiler, but there is no libgcc in   the kernel. The compieler does not realize that it should use   an inlined function instead.
    suse-commit: b43236e12d85a12f9ad41abcb1d8cfed8d6ba02e

--- arch/i386/mach-generic/probe.c
+++ arch/i386/mach-generic/probe.c
@@ -20,6 +20,7 @@ extern struct genapic apic_bigsmp;
 extern struct genapic apic_default;
 
 struct genapic *genapic = &apic_default;
+EXPORT_SYMBOL(genapic);
 
 struct genapic *apic_probe[] __initdata = { 
 	&apic_summit,
