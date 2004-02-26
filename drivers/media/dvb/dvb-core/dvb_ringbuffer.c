d7812a5441c699d2c1bbfb2e43746875e6e1ba57
Andrew Morton <akpm@osdl.org>
2004-02-25 16:22:01 -0800

    [PATCH] dvb: core update
    From: Michael Hunold <hunold@linuxtv.org>
    
    - dvb-core: replace usage of sleep_on_...() with
      wait_event_interruptible_timeout()
    
    - dvb-core: fix dvb_ringbuffer_read/write() buffer pointer bug
    
    - video: added VIDEO_EVENT_FRAME_RATE_CHANGED and VIDEO_GET_FRAME_RATE
      ioctl

--- drivers/media/dvb/dvb-core/dvb_ringbuffer.c
+++ drivers/media/dvb/dvb-core/dvb_ringbuffer.c
@@ -123,7 +123,7 @@ ssize_t dvb_ringbuffer_read(struct dvb_ringbuffer *rbuf, u8 *buf, size_t len, in
                 if (copy_to_user(buf, rbuf->data+rbuf->pread, todo))
                         return -EFAULT;
 
-        rbuf->pread = (rbuf->pread + len) % rbuf->size;
+        rbuf->pread = (rbuf->pread + todo) % rbuf->size;
 
         return len;
 }
@@ -155,7 +155,7 @@ ssize_t dvb_ringbuffer_write(struct dvb_ringbuffer *rbuf, const u8 *buf,
                 if (copy_from_user(rbuf->data+rbuf->pwrite, buf, todo)) 
                         return -EFAULT;
 
-        rbuf->pwrite = (rbuf->pwrite + len) % rbuf->size;
+        rbuf->pwrite = (rbuf->pwrite + todo) % rbuf->size;
 
 	return len;
 }
