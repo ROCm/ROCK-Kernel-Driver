b6b13f960e2766c2fe859af5e7b27073f257f7b2
Andrew Vasquez <andrew.vasquez@qlogic.com>
2004-03-16 08:49:59 -0500

    [PATCH] PATCH [1/15] qla2xxx:  Update copyright banner
    Update copyright banner for all source files.

--- drivers/scsi/qla2xxx/qla_os.h
+++ drivers/scsi/qla2xxx/qla_os.h
@@ -2,7 +2,7 @@
  *                  QLOGIC LINUX SOFTWARE
  *
  * QLogic ISP2x00 device driver for Linux 2.6.x
- * Copyright (C) 2003 QLogic Corporation
+ * Copyright (C) 2003-2004 QLogic Corporation
  * (www.qlogic.com)
  *
  * Portions (C) Arjan van de Ven <arjanv@redhat.com> for Red Hat, Inc.
