878a6c99f5a3e3c3f70471ce5e66427f3a5d9041
Andrew Vasquez <andrew.vasquez@qlogic.com>
2004-03-16 09:22:39 -0500

    [PATCH] PATCH [15/15] qla2xxx: Update driver version
    Update version number to 8.00.00b11-k (to indicate embedded driver).

--- drivers/scsi/qla2xxx/qla_version.h
+++ drivers/scsi/qla2xxx/qla_version.h
@@ -19,9 +19,9 @@
 /*
  * Driver version 
  */
-#define QLA2XXX_VERSION      "8.00.00b10"
+#define QLA2XXX_VERSION      "8.00.00b11-k"
 
 #define QLA_DRIVER_MAJOR_VER	8
 #define QLA_DRIVER_MINOR_VER	0
 #define QLA_DRIVER_PATCH_VER	0
-#define QLA_DRIVER_BETA_VER	10
+#define QLA_DRIVER_BETA_VER	11
