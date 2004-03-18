6fa4a50c4dab0067de5ce819c554e6ed21bce1b9
Andrew Morton <akpm@osdl.org>
2004-03-18 15:00:38 -0800

    [PATCH] config: persistent qconf configuration
    From: Roman Zippel <zippel@linux-m68k.org>
    
    This patch is by Andreas Fester <Andreas.Fester@gmx.de> and saves and
    restores various runtime options of qconf.

--- scripts/kconfig/qconf.h
+++ scripts/kconfig/qconf.h
@@ -4,17 +4,39 @@
  */
 
 #include <qlistview.h>
+#if QT_VERSION >= 300
+#include <qsettings.h>
+#else
+class QSettings { };
+#endif
 
 class ConfigList;
 class ConfigItem;
 class ConfigLineEdit;
 class ConfigMainWindow;
 
+
+class ConfigSettings : public QSettings {
+public:
+	ConfigSettings();
+
+#if QT_VERSION >= 300
+	void readListSettings();
+	QValueList<int> ConfigSettings::readSizes(const QString& key, bool *ok);
+	bool ConfigSettings::writeSizes(const QString& key, const QValueList<int>& value);
+#endif
+
+	bool showAll;
+	bool showName;
+	bool showRange;
+	bool showData;
+};
+
 class ConfigView : public QVBox {
 	Q_OBJECT
 	typedef class QVBox Parent;
 public:
-	ConfigView(QWidget* parent, ConfigMainWindow* cview);
+	ConfigView(QWidget* parent, ConfigMainWindow* cview, ConfigSettings* configSettings);
 	~ConfigView(void);
 	static void updateList(ConfigItem* item);
 	static void updateListAll(void);
@@ -38,7 +60,7 @@ class ConfigList : public QListView {
 	Q_OBJECT
 	typedef class QListView Parent;
 public:
-	ConfigList(ConfigView* p, ConfigMainWindow* cview);
+	ConfigList(ConfigView* p, ConfigMainWindow* cview, ConfigSettings *configSettings);
 	void reinit(void);
 	ConfigView* parent(void) const
 	{
@@ -222,6 +244,7 @@ public slots:
 	void setShowData(bool);
 	void showIntro(void);
 	void showAbout(void);
+	void saveSettings(void);
 
 protected:
 	void closeEvent(QCloseEvent *e);
@@ -233,6 +256,8 @@ protected:
 	QTextView *helpText;
 	QToolBar *toolBar;
 	QAction *backAction;
+	QSplitter* split1;
+	QSplitter* split2;
 
 	bool showDebug;
 };
