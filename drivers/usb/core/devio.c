0592ba9ac17e079c4406b41515e32a1751c56c24
Olaf Hering <olh@suse.de>
2004-03-21 08:56:47 +0000

    +- update patches.drivers/usb-eciadsl.patch +  better version from David Brownell
    suse-commit: 4150b91e9bc5aa76f2d4363364771ff63218e5a3

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -828,6 +828,9 @@ static int proc_submiturb(struct dev_state *ps, void __user *arg)
 		/* arbitrary limit */
 		if (uurb.number_of_packets < 1 || uurb.number_of_packets > 128)
 			return -EINVAL;
+		if (!(ep_desc = usb_epnum_to_ep_desc(ps->dev, uurb.endpoint)))
+			return -ENOENT;
+		interval = (1 << (min ((u8)16,ep_desc->bInterval) - 1));
 		isofrmlen = sizeof(struct usbdevfs_iso_packet_desc) * uurb.number_of_packets;
 		if (!(isopkt = kmalloc(isofrmlen, GFP_KERNEL)))
 			return -ENOMEM;
@@ -853,7 +856,10 @@ static int proc_submiturb(struct dev_state *ps, void __user *arg)
 		uurb.number_of_packets = 0;
 		if (!(ep_desc = usb_epnum_to_ep_desc(ps->dev, uurb.endpoint)))
 			return -ENOENT;
-		interval = (1 << (ep_desc->bInterval - 1));
+		if (ps->dev->speed == USB_SPEED_HIGH)
+			interval = (1 << (min ((u8)16,ep_desc->bInterval) - 1));
+		else
+			interval = ep_desc->bInterval;
 		if (uurb.buffer_length > 16384)
 			return -EINVAL;
 		if (!access_ok((uurb.endpoint & USB_DIR_IN) ? VERIFY_WRITE : VERIFY_READ, uurb.buffer, uurb.buffer_length))
