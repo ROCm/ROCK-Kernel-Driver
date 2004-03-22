b1776b04ae7a0623c5fa8611770593011fe3a01f
Jeff Garzik <jgarzik@redhat.com>
2004-03-22 14:15:58 -0500

    Create PCI_DMA_{64,32]BIT constants, for use in passing to pci_set_{consistent_}dma_mask().
    Use them in e1000 and ixgb.

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -362,6 +362,9 @@ enum pci_mmap_state {
 #define PCI_DMA_FROMDEVICE	2
 #define PCI_DMA_NONE		3
 
+#define PCI_DMA_64BIT	0xffffffffffffffffULL
+#define PCI_DMA_32BIT	0x00000000ffffffffULL
+
 #define DEVICE_COUNT_COMPATIBLE	4
 #define DEVICE_COUNT_RESOURCE	12
 
