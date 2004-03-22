f3d0c6a37d98dba6035021690b0ecb7323a54619
Andreas Schwab <schwab@suse.de>
2004-03-22 17:37:32 +0000

    Update ppc64 kdb patches.
    suse-commit: 79dbaae33d8b543f59745408f621975809f03a8e

--- include/asm-ppc64/bfd.h
+++ include/asm-ppc64/bfd.h
@@ -35,8 +35,8 @@
 extern "C" {
 #endif
 
-#include "ansidecl.h"
-#include "symcat.h"
+#include <asm/ansidecl.h>
+#include <asm/symcat.h>
 #if defined (__STDC__) || defined (ALMOST_STDC) || defined (HAVE_STRINGIZE)
 #ifndef SABER
 /* This hack is to avoid a problem with some strict ANSI C preprocessors.
