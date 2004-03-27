2b82353a0c1b3594cc902f477240602edec1712a
Andi Kleen <ak@suse.de>
2004-03-26 20:56:32 -0800

    [PATCH] Don't register disabled nodes
    Don't register disabled nodes.  Hits on x86-64 when there is a node with no memory.
    X86-64 shares this code with i386.

--- include/asm-i386/node.h
+++ include/asm-i386/node.h
@@ -12,8 +12,12 @@ struct i386_node {
 extern struct i386_node node_devices[MAX_NUMNODES];
 
 static inline int arch_register_node(int num){
-	int p_node = parent_node(num);
+	int p_node;
 	struct node *parent = NULL;
+
+	if (!node_online(num))
+		return 0;
+	p_node = parent_node(num);
 
 	if (p_node != num)
 		parent = &node_devices[p_node].node;
