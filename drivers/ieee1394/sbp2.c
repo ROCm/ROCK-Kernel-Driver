bd6b55b0d3a832a17de0dd65f348a3cb3b21b296
Ben Collins <bcollins@debian.org>
2004-04-09 02:06:41 -0400

    [SBP2]: Sync revision

--- drivers/ieee1394/sbp2.c
+++ drivers/ieee1394/sbp2.c
@@ -78,7 +78,7 @@
 #include "sbp2.h"
 
 static char version[] __devinitdata =
-	"$Rev: 1200 $ Ben Collins <bcollins@debian.org>";
+	"$Rev: 1205 $ Ben Collins <bcollins@debian.org>";
 
 /*
  * Module load parameter definitions
