d1e069e24b6da75b8177ca523916309e27cd08e3
Andrew Morton <akpm@osdl.org>
2004-04-11 23:19:47 -0700

    [PATCH] add stop_machine barriers
    From: Andrea Arcangeli <andrea@suse.de>
    
    We need a barrier before checking for kthread_should_stop in do_stop.

--- kernel/stop_machine.c
+++ kernel/stop_machine.c
@@ -149,10 +149,10 @@ static int do_stop(void *_smdata)
 	complete(&smdata->done);
 
 	/* Wait for kthread_stop */
-	__set_current_state(TASK_INTERRUPTIBLE);
+	set_current_state(TASK_INTERRUPTIBLE);
 	while (!kthread_should_stop()) {
 		schedule();
-		__set_current_state(TASK_INTERRUPTIBLE);
+		set_current_state(TASK_INTERRUPTIBLE);
 	}
 	__set_current_state(TASK_RUNNING);
 	return ret;
