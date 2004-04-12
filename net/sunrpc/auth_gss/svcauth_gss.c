5b2b9a8100c1125f075a824aebba06ffb5a4c2c2
Andrew Morton <akpm@osdl.org>
2004-04-11 22:52:19 -0700

    [PATCH] knfsd: fix a problem with incorrectly formatted auth_error returns.
    From: NeilBrown <neilb@cse.unsw.edu.au>
    
    From: Fred Isaman

--- net/sunrpc/auth_gss/svcauth_gss.c
+++ net/sunrpc/auth_gss/svcauth_gss.c
@@ -690,7 +690,7 @@ svcauth_gss_accept(struct svc_rqst *rqstp, u32 *authp)
 	struct rsc	*rsci = NULL;
 	struct rsi	*rsip, rsikey;
 	u32		*rpcstart;
-	u32		*reject_stat = resv->iov_base;
+	u32		*reject_stat = resv->iov_base + resv->iov_len;
 	int		ret;
 
 	dprintk("RPC: svcauth_gss: argv->iov_len = %zd\n",argv->iov_len);
