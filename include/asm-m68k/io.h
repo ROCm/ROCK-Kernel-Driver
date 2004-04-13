782cf01e7f6392c537c74297a0ee869f42234625
Geert Uytterhoeven <geert@linux-m68k.org>
2004-04-12 17:31:46 -0700

    [PATCH] m68k I/O
    m68k: Use explicit-sized types for I/O accesses

--- include/asm-m68k/io.h
+++ include/asm-m68k/io.h
@@ -120,66 +120,66 @@ extern int isa_sex;
  * be compiled in so the case statement will be optimised away
  */
 
-static inline unsigned char *isa_itb(long addr)
+static inline u8 *isa_itb(long addr)
 {
   switch(ISA_TYPE)
     {
 #ifdef CONFIG_Q40
-    case Q40_ISA: return (unsigned char *)Q40_ISA_IO_B(addr);
+    case Q40_ISA: return (u8 *)Q40_ISA_IO_B(addr);
 #endif
 #ifdef CONFIG_GG2
-    case GG2_ISA: return (unsigned char *)GG2_ISA_IO_B(addr);
+    case GG2_ISA: return (u8 *)GG2_ISA_IO_B(addr);
 #endif
 #ifdef CONFIG_AMIGA_PCMCIA
-    case AG_ISA: return (unsigned char *)AG_ISA_IO_B(addr);
+    case AG_ISA: return (u8 *)AG_ISA_IO_B(addr);
 #endif
     default: return 0; /* avoid warnings, just in case */
     }
 }
-static inline unsigned short *isa_itw(long addr)
+static inline u16 *isa_itw(long addr)
 {
   switch(ISA_TYPE)
     {
 #ifdef CONFIG_Q40
-    case Q40_ISA: return (unsigned short *)Q40_ISA_IO_W(addr);
+    case Q40_ISA: return (u16 *)Q40_ISA_IO_W(addr);
 #endif
 #ifdef CONFIG_GG2
-    case GG2_ISA: return (unsigned short *)GG2_ISA_IO_W(addr);
+    case GG2_ISA: return (u16 *)GG2_ISA_IO_W(addr);
 #endif
 #ifdef CONFIG_AMIGA_PCMCIA
-    case AG_ISA: return (unsigned short *)AG_ISA_IO_W(addr);
+    case AG_ISA: return (u16 *)AG_ISA_IO_W(addr);
 #endif
     default: return 0; /* avoid warnings, just in case */
     }
 }
-static inline unsigned char *isa_mtb(long addr)
+static inline u8 *isa_mtb(long addr)
 {
   switch(ISA_TYPE)
     {
 #ifdef CONFIG_Q40
-    case Q40_ISA: return (unsigned char *)Q40_ISA_MEM_B(addr);
+    case Q40_ISA: return (u8 *)Q40_ISA_MEM_B(addr);
 #endif
 #ifdef CONFIG_GG2
-    case GG2_ISA: return (unsigned char *)GG2_ISA_MEM_B(addr);
+    case GG2_ISA: return (u8 *)GG2_ISA_MEM_B(addr);
 #endif
 #ifdef CONFIG_AMIGA_PCMCIA
-    case AG_ISA: return (unsigned char *)addr;
+    case AG_ISA: return (u8 *)addr;
 #endif
     default: return 0; /* avoid warnings, just in case */
     }
 }
-static inline unsigned short *isa_mtw(long addr)
+static inline u16 *isa_mtw(long addr)
 {
   switch(ISA_TYPE)
     {
 #ifdef CONFIG_Q40
-    case Q40_ISA: return (unsigned short *)Q40_ISA_MEM_W(addr);
+    case Q40_ISA: return (u16 *)Q40_ISA_MEM_W(addr);
 #endif
 #ifdef CONFIG_GG2
-    case GG2_ISA: return (unsigned short *)GG2_ISA_MEM_W(addr);
+    case GG2_ISA: return (u16 *)GG2_ISA_MEM_W(addr);
 #endif
 #ifdef CONFIG_AMIGA_PCMCIA
-    case AG_ISA: return (unsigned short *)addr;
+    case AG_ISA: return (u16 *)addr;
 #endif
     default: return 0; /* avoid warnings, just in case */
     }
@@ -213,7 +213,7 @@ static inline void isa_delay(void)
     }
 }
 
-#define isa_inb_p(p)      ({unsigned char v=isa_inb(p);isa_delay();v;})
+#define isa_inb_p(p)      ({u8 v=isa_inb(p);isa_delay();v;})
 #define isa_outb_p(v,p)   ({isa_outb((v),(p));isa_delay();})
 
 #define isa_insb(port, buf, nr) raw_insb(isa_itb(port), (buf), (nr))
