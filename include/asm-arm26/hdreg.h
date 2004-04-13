76fff7dbcd13e7437f49db01b4807b34e8ccbb2d
Bartlomiej Zolnierkiewicz <b.zolnierkiewicz@elka.pw.edu.pl>
2004-04-13 04:28:56 -0700

    [PATCH] add asm-generic/hdreg.h
    Use it on all archs which define ide_ioreg_t to unsigned long.

--- include/asm-arm26/hdreg.h
+++ include/asm-arm26/hdreg.h
@@ -1,15 +1,1 @@
-/*
- *  linux/include/asm-arm26/hdreg.h
- *
- *  Used by include/linux/ide.h
- *
- *  Copyright (C) 1994-1996  Linus Torvalds & authors
- */
-
-#ifndef __ASMARM_HDREG_H
-#define __ASMARM_HDREG_H
-
-typedef unsigned long ide_ioreg_t;
-
-#endif /* __ASMARM_HDREG_H */
-
+#include <asm-generic/hdreg.h>
---
+++ include/asm-generic/hdreg.h
@@ -0,0 +1,6 @@
+#ifndef __ASM_GENERIC_HDREG_H
+#define __ASM_GENERIC_HDREG_H
+
+typedef unsigned long ide_ioreg_t;
+
+#endif /* __ASM_GENERIC_HDREG_H */
