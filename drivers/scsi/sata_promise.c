a9403f0ab02d745580df89865bdb07afa17b74a5
Jeff Garzik <jgarzik@redhat.com>
2004-04-14 13:04:48 -0400

    [libata sata_promise] fix taskfile delivery cases
    We should only be touching the ATA shadow registers if we are doing
    PIO.

--- drivers/scsi/sata_promise.c
+++ drivers/scsi/sata_promise.c
@@ -1180,14 +1180,14 @@ static void pdc_dma_start(struct ata_queued_cmd *qc)
 
 static void pdc_tf_load_mmio(struct ata_port *ap, struct ata_taskfile *tf)
 {
-	if (tf->protocol != ATA_PROT_DMA)
+	if (tf->protocol == ATA_PROT_PIO)
 		ata_tf_load_mmio(ap, tf);
 }
 
 
 static void pdc_exec_command_mmio(struct ata_port *ap, struct ata_taskfile *tf)
 {
-	if (tf->protocol != ATA_PROT_DMA)
+	if (tf->protocol == ATA_PROT_PIO)
 		ata_exec_command_mmio(ap, tf);
 }
 
