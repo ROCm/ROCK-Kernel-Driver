c608aedd5ae37592960476b2d96c0f2b8d719dcf
James Bottomley <james.bottomley@steeleye.com>
2004-04-14 19:20:00 -0700

    [PATCH] fix 4k irqstacks on x86 (and add voyager support)
    There's a bug in the x86 code in that it sets the boot CPU to zero.
    
    This isn't correct since some subarch's use physically indexed CPUs. 
    However, subarchs have either set the boot cpu before irq_INIT() (or
    just inherited the default zero from INIT_THREAD_INFO()), so it's safe
    to believe current_thread_info()->cpu about the boot cpu.

--- arch/i386/mach-voyager/voyager_thread.c
+++ arch/i386/mach-voyager/voyager_thread.c
@@ -135,7 +135,7 @@ thread(void *unused)
 	init_timer(&wakeup_timer);
 
 	sigfillset(&current->blocked);
-	current->tty = NULL;	/* get rid of controlling tty */
+	current->signal->tty = NULL;
 
 	printk(KERN_NOTICE "Voyager starting monitor thread\n");
 
