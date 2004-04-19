253472be42b55199b24b5779dfc8a562ffb02ebc
Olaf Kirch <okir@suse.de>
2004-04-19 15:21:11 +0000

    - nat-before-ipsec patches were lacking an EXPORT_SYMBOL(nf_rcv_postxfrm_local)
    suse-commit: eb00825ed367ed2de66c812c000a3a6f347087e2

--- net/core/netfilter.c
+++ net/core/netfilter.c
@@ -857,3 +857,4 @@ EXPORT_SYMBOL(nf_setsockopt);
 EXPORT_SYMBOL(nf_unregister_hook);
 EXPORT_SYMBOL(nf_unregister_queue_handler);
 EXPORT_SYMBOL(nf_unregister_sockopt);
+EXPORT_SYMBOL(nf_rcv_postxfrm_local)
