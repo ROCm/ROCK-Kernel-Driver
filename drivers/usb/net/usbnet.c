eb757e4f2218a4be2330519e53c6f0752ef14f55
David Brownell <david-b@pacbell.net>
2004-04-21 23:53:58 -0700

    [PATCH] USB: usbnet and pl2301/2302 reset
    Handle some PL-2301/2302 devices better.

--- drivers/usb/net/usbnet.c
+++ drivers/usb/net/usbnet.c
@@ -2107,8 +2107,12 @@ pl_set_QuickLink_features (struct usbnet *dev, int val)
 
 static int pl_reset (struct usbnet *dev)
 {
-	return pl_set_QuickLink_features (dev,
+	/* some units seem to need this reset, others reject it utterly.
+	 * FIXME be more like "naplink" or windows drivers.
+	 */
+	(void) pl_set_QuickLink_features (dev,
 		PL_S_EN|PL_RESET_OUT|PL_RESET_IN|PL_PEER_E);
+	return 0;
 }
 
 static const struct driver_info	prolific_info = {
