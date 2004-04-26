e836586f86e0397291c42e5c36c7d13b0e2434e8
Olaf Kirch <okir@suse.de>
2004-04-26 07:29:42 +0000

    - Merged patch from netdev to prevent oops when removing tulip module
    suse-commit: 265c099ba50d9bed3ecd1b1d476719314db4ff1d

--- drivers/net/tulip/tulip_core.c
+++ drivers/net/tulip/tulip_core.c
@@ -1761,11 +1761,11 @@ static void __devexit tulip_remove_one (struct pci_dev *pdev)
 		return;
 
 	tp = netdev_priv(dev);
+	unregister_netdev(dev);
 	pci_free_consistent (pdev,
 			     sizeof (struct tulip_rx_desc) * RX_RING_SIZE +
 			     sizeof (struct tulip_tx_desc) * TX_RING_SIZE,
 			     tp->rx_ring, tp->rx_ring_dma);
-	unregister_netdev (dev);
 	if (tp->mtable)
 		kfree (tp->mtable);
 #ifndef USE_IO_OPS
