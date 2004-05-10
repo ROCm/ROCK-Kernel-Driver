bf434bf2dc9f2b88a7ae47ffa54996240004199f
Andrew Morton <akpm@osdl.org>
2004-05-10 00:04:25 -0700

    [PATCH] gcc-3.4.0 fixes for 2.6.6-rc3 x86_64 kernel
    From: Mikael Pettersson <mikpe@csd.uu.se>
    
    Here are some patches to fix compilation warnings from
    gcc-3.4.0 in the 2.6.6-rc3 x86_64 kernel.
    
    - puts() type conflict in boot/compressed/misc.c:
      rename to putstr(), just like i386 did
    - cast-as-lvalue in ia32_copy_siginfo_from_user():
      use temporary
    - code before declaration in io_apic.c:
      move decl up
    - code before declaration in ioremap.c:
      move existing #ifndef up
    - cast-as-lvalue (tons of them) from UP version of per_cpu():
      merged asm-generic's version

--- arch/x86_64/mm/ioremap.c
+++ arch/x86_64/mm/ioremap.c
@@ -132,13 +132,13 @@ void * __ioremap(unsigned long phys_addr, unsigned long size, unsigned long flag
 	 * Don't allow anybody to remap normal RAM that we're using..
 	 */
 	if (phys_addr < virt_to_phys(high_memory)) {
+#ifndef CONFIG_DISCONTIGMEM
 		char *t_addr, *t_end;
+ 		struct page *page;
 
 		t_addr = __va(phys_addr);
 		t_end = t_addr + (size - 1);
 	   
-#ifndef CONFIG_DISCONTIGMEM	 
- 		struct page *page;
 		for(page = virt_to_page(t_addr); page <= virt_to_page(t_end); page++)
 			if(!PageReserved(page))
 				return NULL;
