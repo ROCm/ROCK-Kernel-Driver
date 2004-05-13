42705eb4fe4cb82f31e78fa869a8799e6e7eb118
David Mosberger <davidm@tiger.hpl.hp.com>
2004-05-13 01:35:41 -0700

    ia64: Correct atomic_inc_and_test() and atomic64_inc_and_test().

--- include/asm-ia64/atomic.h
+++ include/asm-ia64/atomic.h
@@ -159,10 +159,10 @@ atomic64_add_negative (__s64 i, atomic64_t *v)
 
 #define atomic_sub_and_test(i,v)	(atomic_sub_return((i), (v)) == 0)
 #define atomic_dec_and_test(v)		(atomic_sub_return(1, (v)) == 0)
-#define atomic_inc_and_test(v)		(atomic_add_return(1, (v)) != 0)
+#define atomic_inc_and_test(v)		(atomic_add_return(1, (v)) == 0)
 #define atomic64_sub_and_test(i,v)	(atomic64_sub_return((i), (v)) == 0)
 #define atomic64_dec_and_test(v)	(atomic64_sub_return(1, (v)) == 0)
-#define atomic64_inc_and_test(v)	(atomic64_add_return(1, (v)) != 0)
+#define atomic64_inc_and_test(v)	(atomic64_add_return(1, (v)) == 0)
 
 #define atomic_add(i,v)			atomic_add_return((i), (v))
 #define atomic_sub(i,v)			atomic_sub_return((i), (v))
