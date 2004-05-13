6d1a98255444c92124a0379a31b2331c192224ca
Jens Axboe <axboe@suse.de>
2004-05-13 08:22:13 +0000

    - barrier-core update. clear_buffer_write_io_error() on WRITE_BARRIER, not just   write (should just check direction). correctly check BIO_EOPNOTSUPP at   end_io time as well (for async notication of lacking barrier support)
    suse-commit: c49527cbf998465129b8e04cd53ac213eec241b3

--- fs/buffer.c
+++ fs/buffer.c
@@ -2774,6 +2774,9 @@ static int end_bio_bh_io_sync(struct bio *bio, unsigned int bytes_done, int err)
 	if (bio->bi_size)
 		return 1;
 
+	if (err == -EOPNOTSUPP)
+		set_bit(BIO_EOPNOTSUPP, &bio->bi_flags);
+
 	bh->b_end_io(bh, test_bit(BIO_UPTODATE, &bio->bi_flags));
 	bio_put(bio);
 	return 0;
@@ -2798,8 +2801,11 @@ int submit_bh(int rw, struct buffer_head * bh)
 	if (buffer_ordered(bh) && (rw == WRITE))
 		rw = WRITE_BARRIER;
 
-	/* Only clear out a write error when rewriting */
-	if (test_set_buffer_req(bh) && rw == WRITE)
+	/*
+	 * Only clear out a write error when rewriting, should this
+	 * include WRITE_SYNC as well?
+	 */
+	if (test_set_buffer_req(bh) && (rw == WRITE || rw == WRITE_BARRIER))
 		clear_buffer_write_io_error(bh);
 
 	/*
