784798168972a40d89096288ca9756b4468e84a1
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-05-14 21:13:22 -0700

    Fix gidsetsize == 0 for real this time.
    We need to always allocate at least one indirect block
    pointer, since we always fill out blocks[0] even if
    we don't have any groups.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1126,8 +1126,9 @@ struct group_info *groups_alloc(int gidsetsize)
 	int i;
 
 	nblocks = (gidsetsize + NGROUPS_PER_BLOCK - 1) / NGROUPS_PER_BLOCK;
-	group_info = kmalloc(sizeof(*group_info) + nblocks*sizeof(gid_t *),
-	    GFP_USER);
+	/* Make sure we always allocate at least one indirect block pointer */
+	nblocks = nblocks ? : 1;
+	group_info = kmalloc(sizeof(*group_info) + nblocks*sizeof(gid_t *), GFP_USER);
 	if (!group_info)
 		return NULL;
 	group_info->ngroups = gidsetsize;
