4a72e942ad937ed10566b87efd4bcb152c0f5b31
Andrew Morton <akpm@osdl.org>
2004-05-22 08:07:38 -0700

    [PATCH] rmap 23 empty flush_dcache_mmap_lock
    From: Hugh Dickins <hugh@veritas.com>
    
    Most architectures (like i386) do nothing in flush_dcache_page, or don't scan
    i_mmap in flush_dcache_page, so don't need flush_dcache_mmap_lock to do
    anything: define it and flush_dcache_mmap_unlock away.  Noticed arm26, cris,
    h8300 still defining flush_page_to_ram: delete it again.

--- include/asm-alpha/cacheflush.h
+++ include/asm-alpha/cacheflush.h
@@ -10,6 +10,8 @@
 #define flush_cache_range(vma, start, end)	do { } while (0)
 #define flush_cache_page(vma, vmaddr)		do { } while (0)
 #define flush_dcache_page(page)			do { } while (0)
+#define flush_dcache_mmap_lock(mapping)		do { } while (0)
+#define flush_dcache_mmap_unlock(mapping)	do { } while (0)
 #define flush_cache_vmap(start, end)		do { } while (0)
 #define flush_cache_vunmap(start, end)		do { } while (0)
 
--- include/asm-m68knommu/cacheflush.h
+++ include/asm-m68knommu/cacheflush.h
@@ -12,6 +12,8 @@
 #define flush_cache_page(vma, vmaddr)		do { } while (0)
 #define flush_dcache_range(start,len)		do { } while (0)
 #define flush_dcache_page(page)			do { } while (0)
+#define flush_dcache_mmap_lock(mapping)		do { } while (0)
+#define flush_dcache_mmap_unlock(mapping)	do { } while (0)
 #define flush_icache_range(start,len)		__flush_cache_all()
 #define flush_icache_page(vma,pg)		do { } while (0)
 #define flush_icache_user_range(vma,pg,adr,len)	do { } while (0)
