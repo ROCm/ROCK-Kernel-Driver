87050fae7607166d02c1eaf052f8a55d7eca8e5c
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-05-24 23:04:59 -0700

    Introduce architecture-specific "ptep_update_dirty_accessed()" helper function to write-back the dirty and accessed bits from ptep_establish().
    Right now this defaults to the same old "set_pte()" that we've
    always done, except for x86 where we now fix the (unlikely)
    race in updating accessed bits and dropping a concurrent dirty
    bit.

--- include/asm-generic/pgtable.h
+++ include/asm-generic/pgtable.h
@@ -2,6 +2,11 @@
 #define _ASM_GENERIC_PGTABLE_H
 
 #ifndef __HAVE_ARCH_PTEP_ESTABLISH
+
+#ifndef ptep_update_dirty_accessed
+#define ptep_update_dirty_accessed(__ptep, __entry, __dirty) set_pte(__ptep, __entry)
+#endif
+
 /*
  * Establish a new mapping:
  *  - flush the old one
@@ -12,7 +17,7 @@
  */
 #define ptep_establish(__vma, __address, __ptep, __entry, __dirty)	\
 do {									\
-	set_pte(__ptep, __entry);					\
+	ptep_update_dirty_accessed(__ptep, __entry, __dirty);		\
 	flush_tlb_page(__vma, __address);				\
 } while (0)
 #endif
