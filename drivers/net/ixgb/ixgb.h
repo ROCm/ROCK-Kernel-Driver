3c7fd9ca851da8057f37bf6854beef2ce8aa05d6
Jeff Garzik <jgarzik@redhat.com>
2004-05-27 08:22:20 -0400

    [netdrvr ixgb] massive update
    Since Intel agreed to submit further updates via broken-up patches
    like they do currently (and admirably) for e1000 and e100, I agreed
    to merge this update to bring the driver up to speed.
    
    Contributed by: Ayyappan.Veeraiyan@intel.com
    
    Changes:
    
    Features implemented
    
    *	Support for new 850nm adapters.
    *	Copyright notice updated to include year 2004.
    *	Fix for "ixgb does not maintain non-default MTU setting across a
    link loss" issue - When link loss happens in non-default MTU
    environment, driver will incorrectly operate with default 1500 MTU
    setting.
    *	Netpoll support added
    *	Ethtool support - status functionality, FlowControl interface,
    Checksum interface, TSO and Scatter Gather interfaces
    *	Race condition fix - Race condition (TX path) exists between
    ixgb_xmit_frame and clean_tx_irq routines in handling the queue.
    * 	Removed dead code segments (#if 0)
    
    
    Performance improvement features 
    *	Mod operator usage is removed - used to cause performance
    problems in non-IA architecture based machines
    *	Multiple ICR register read in ISR is avoided
    *	RS bit set on only 'end of packet' TX descriptors - to avoid
    multiple writebacks by controller for packets with multiple descriptors.
    *	RX descriptors prefetch is done - improved 1500 MTU TX
    performance
    
    
    All relevant e1000 driver cleanups ported to ixgb
    *	Valid error propagation in functions ixgb_up, ixgb_probe,
    ixgb_open. 
    * 	NAPI code cleanups
    * 	u8, u16 and u32 data types are changed to uint8_t, uint16_t and
    uint32_t respectively. We use these types on our shared code for
    multiple OSes and also to make it uniform with e1000 driver.
    *	Some functions in ixgb_main.c are moved to different location -
    This enables easy porting of e1000 bug fixes to ixgb

--- drivers/net/ixgb/ixgb.h
+++ drivers/net/ixgb/ixgb.h
@@ -1,7 +1,7 @@
 /*******************************************************************************
 
   
-  Copyright(c) 1999 - 2003 Intel Corporation. All rights reserved.
+  Copyright(c) 1999 - 2004 Intel Corporation. All rights reserved.
   
   This program is free software; you can redistribute it and/or modify it 
   under the terms of the GNU General Public License as published by the Free 
@@ -23,6 +23,7 @@
   Contact Information:
   Linux NICS <linux.nics@intel.com>
   Intel Corporation, 5200 N.E. Elam Young Parkway, Hillsboro, OR 97124-6497
+
 *******************************************************************************/
 
 #ifndef _IXGB_H_
@@ -34,39 +35,44 @@
 #include <linux/types.h>
 #include <asm/byteorder.h>
 #include <linux/init.h>
+#include <linux/mm.h>
+#include <linux/errno.h>
 #include <linux/ioport.h>
 #include <linux/pci.h>
+#include <linux/kernel.h>
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 #include <linux/skbuff.h>
 #include <linux/delay.h>
 #include <linux/timer.h>
+#include <linux/slab.h>
+#include <linux/interrupt.h>
 #include <linux/string.h>
 #include <linux/pagemap.h>
 #include <linux/dma-mapping.h>
-#include <linux/bitops.h>
+#include <asm/bitops.h>
+#include <asm/io.h>
+#include <asm/irq.h>
+#include <linux/capability.h>
 #include <linux/in.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
 #include <net/pkt_sched.h>
 #include <linux/list.h>
-#include <linux/workqueue.h>
 #include <linux/reboot.h>
 #ifdef NETIF_F_TSO
 #include <net/checksum.h>
 #endif
 
-/* ethtool support */
 #include <linux/ethtool.h>
 #include <linux/if_vlan.h>
 
+#define BAR_0		0
+#define BAR_1		1
+#define BAR_5		5
+
 struct ixgb_adapter;
-
-#define BAR_0           0
-#define BAR_1           1
-#define BAR_5           5
-
 #include "ixgb_hw.h"
 #include "ixgb_ee.h"
 #include "ixgb_ids.h"
@@ -89,7 +95,7 @@ struct ixgb_adapter;
 #define IXGB_TX_QUEUE_WAKE 16
 
 /* How many Rx Buffers do we bundle into one write to the hardware ? */
-#define IXGB_RX_BUFFER_WRITE    16
+#define IXGB_RX_BUFFER_WRITE	16	/* Must be power of 2 */
 
 /* only works for sizes that are powers of 2 */
 #define IXGB_ROUNDUP(i, size) ((i) = (((i) + (size) - 1) & ~((size) - 1)))
@@ -101,71 +107,66 @@ struct ixgb_buffer {
 	uint64_t dma;
 	unsigned long length;
 	unsigned long time_stamp;
+	unsigned int next_to_watch;
 };
 
 struct ixgb_desc_ring {
-	/* pointer to the descriptor ring memory  */
+	/* pointer to the descriptor ring memory */
 	void *desc;
-	/* physical address of the descriptor ring  */
+	/* physical address of the descriptor ring */
 	dma_addr_t dma;
-	/* length of descriptor ring in bytes  */
+	/* length of descriptor ring in bytes */
 	unsigned int size;
-	/* number of descriptors in the ring  */
+	/* number of descriptors in the ring */
 	unsigned int count;
-	/* next descriptor to associate a buffer with  */
+	/* next descriptor to associate a buffer with */
 	unsigned int next_to_use;
-	/* next descriptor to check for DD status bit  */
+	/* next descriptor to check for DD status bit */
 	unsigned int next_to_clean;
-	/* array of buffer information structs  */
+	/* array of buffer information structs */
 	struct ixgb_buffer *buffer_info;
 };
 
 #define IXGB_DESC_UNUSED(R) \
-((((R)->next_to_clean + (R)->count) - ((R)->next_to_use + 1)) % ((R)->count))
+	((((R)->next_to_clean > (R)->next_to_use) ? 0 : (R)->count) + \
+	(R)->next_to_clean - (R)->next_to_use - 1)
 
-#define IXGB_GET_DESC(R, i, type)       (&(((struct type *)((R).desc))[i]))
-#define IXGB_RX_DESC(R, i)              IXGB_GET_DESC(R, i, ixgb_rx_desc)
-#define IXGB_TX_DESC(R, i)              IXGB_GET_DESC(R, i, ixgb_tx_desc)
-#define IXGB_CONTEXT_DESC(R, i)         IXGB_GET_DESC(R, i, ixgb_context_desc)
+#define IXGB_GET_DESC(R, i, type)	(&(((struct type *)((R).desc))[i]))
+#define IXGB_RX_DESC(R, i)		IXGB_GET_DESC(R, i, ixgb_rx_desc)
+#define IXGB_TX_DESC(R, i)		IXGB_GET_DESC(R, i, ixgb_tx_desc)
+#define IXGB_CONTEXT_DESC(R, i)	IXGB_GET_DESC(R, i, ixgb_context_desc)
 
 /* board specific private data structure */
 
 struct ixgb_adapter {
 	struct timer_list watchdog_timer;
 	struct vlan_group *vlgrp;
-	char *id_string;
-	u32 bd_number;
-	u32 rx_buffer_len;
-	u32 part_num;
-	u16 link_speed;
-	u16 link_duplex;
+	uint32_t bd_number;
+	uint32_t rx_buffer_len;
+	uint32_t part_num;
+	uint16_t link_speed;
+	uint16_t link_duplex;
+	spinlock_t tx_lock;
 	atomic_t irq_sem;
 	struct work_struct tx_timeout_task;
 
-#ifdef ETHTOOL_PHYS_ID
 	struct timer_list blink_timer;
 	unsigned long led_status;
-#endif
-#ifdef _INTERNAL_LOOPBACK_DRIVER_
-	struct ixgb_desc_ring diag_tx_ring;
-	struct ixgb_desc_ring diag_rx_ring;
-#endif
+
 	/* TX */
 	struct ixgb_desc_ring tx_ring;
 	unsigned long timeo_start;
-	u32 tx_cmd_type;
-	int max_data_per_txd;
+	uint32_t tx_cmd_type;
 	uint64_t hw_csum_tx_good;
 	uint64_t hw_csum_tx_error;
-	boolean_t tx_csum;
-	u32 tx_int_delay;
+	uint32_t tx_int_delay;
 	boolean_t tx_int_delay_enable;
 
 	/* RX */
 	struct ixgb_desc_ring rx_ring;
 	uint64_t hw_csum_rx_error;
 	uint64_t hw_csum_rx_good;
-	u32 rx_int_delay;
+	uint32_t rx_int_delay;
 	boolean_t raidc;
 	boolean_t rx_csum;
 
@@ -177,8 +178,6 @@ struct ixgb_adapter {
 	/* structs defined in ixgb_hw.h */
 	struct ixgb_hw hw;
 	struct ixgb_hw_stats stats;
-	u32 pci_state[16];
-	char ifname[IFNAMSIZ];
+	uint32_t pci_state[16];
 };
-
 #endif				/* _IXGB_H_ */
