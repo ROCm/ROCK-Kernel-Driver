05800c021ee09e2d63f390859aa5972352649655
Steve French <stevef@smfhome1.smfsambadom>
2004-05-28 14:59:09 -0500

    Missing spin lock init

--- fs/cifs/misc.c
+++ fs/cifs/misc.c
@@ -123,6 +123,9 @@ tconInfoAlloc(void)
 		ret_buf->tidStatus = CifsNew;
 		INIT_LIST_HEAD(&ret_buf->openFileList);
 		init_MUTEX(&ret_buf->tconSem);
+#ifdef CONFIG_CIFS_STATS
+		ret_buf->stat_lock = SPIN_LOCK_UNLOCKED;
+#endif
 		write_unlock(&GlobalSMBSeslock);
 	}
 	return ret_buf;
