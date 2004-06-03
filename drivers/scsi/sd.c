587600a0f5e99574be7c9df1367d3f1270b6bdef
Christoph Hellwig <hch@lst.de>
2004-06-03 09:55:15 -0500

    [PATCH] handle NO_SENSE in sd
    Original patch from
    http://marc.theaimsgroup.com/?l=linux-scsi&m=107428662718579&w=2
    reworked to according to James' suggestion to handle NO_SENSE the same
    as RECOVERED_ERROR.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -763,11 +763,11 @@ static void sd_rw_intr(struct scsi_cmnd * SCpnt)
 				good_bytes = 0;
 			break;
 
-		case RECOVERED_ERROR:
+		case RECOVERED_ERROR: /* an error occurred, but it recovered */
+		case NO_SENSE: /* LLDD got sense data */
 			/*
-			 * An error occurred, but it recovered.  Inform the
-			 * user, but make sure that it's not treated as a
-			 * hard error.
+			 * Inform the user, but make sure that it's not treated
+			 * as a hard error.
 			 */
 			print_sense("sd", SCpnt);
 			SCpnt->result = 0;
