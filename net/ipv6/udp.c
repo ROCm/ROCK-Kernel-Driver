aa95033253d0f3ff0cf5e5776e8806530f1e8403
Hideaki Yoshifuji <yoshfuji@linux-ipv6.org>
2004-06-15 07:09:47 -0700

    [IPV6]: UDPv6 checksum.
    We always need to check UDPv6 checksum because it is mandatory.
    
    Author: Yoshifuji Hideaki <yoshfuji@linux-ipv6.org>
    Signed-off-by: David S. Miller <davem@redhat.com>

--- net/ipv6/udp.c
+++ net/ipv6/udp.c
@@ -507,7 +507,7 @@ static inline int udpv6_queue_rcv_skb(struct sock * sk, struct sk_buff *skb)
 		return -1;
 	}
 
-	if (sk->sk_filter && skb->ip_summed != CHECKSUM_UNNECESSARY) {
+	if (skb->ip_summed != CHECKSUM_UNNECESSARY) {
 		if ((unsigned short)csum_fold(skb_checksum(skb, 0, skb->len, skb->csum))) {
 			UDP6_INC_STATS_BH(UdpInErrors);
 			kfree_skb(skb);
