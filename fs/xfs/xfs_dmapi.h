ab96a93436e8846cd8626c58a0599886df094638
Dean Roehrich <roehrich@sgi.com>
2004-06-19 06:43:35 +1000

    [XFS] Fix non-dmapi build
    SGI Modid: xfs-linux:xfs-kern:173222a
    Signed-off-by: nathans@sgi.com

--- fs/xfs/xfs_dmapi.h
+++ fs/xfs/xfs_dmapi.h
@@ -205,8 +205,8 @@ void xfs_dm_exit(struct file_system_type *);
 #define XFS_DM_INIT(fstype)	xfs_dm_init(fstype)
 #define XFS_DM_EXIT(fstype)	xfs_dm_exit(fstype)
 #else
-#define XFS_DM_INIT()
-#define XFS_DM_EXIT()
+#define XFS_DM_INIT(fstype)
+#define XFS_DM_EXIT(fstype)
 #endif
 
 #endif  /* __XFS_DMAPI_H__ */
