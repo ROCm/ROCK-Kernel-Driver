86634e478c487331fd8b488068dc43643319f4eb
Nathan Scott <nathans@sgi.com>
2004-06-19 07:53:10 +1000

    [XFS] Fix a race condition in the undo-delayed-write buffer routine.
    SGI Modid: xfs-linux:xfs-kern:173564a
    Signed-off-by: nathans@sgi.com

--- fs/xfs/linux-2.6/xfs_buf.c
+++ fs/xfs/linux-2.6/xfs_buf.c
@@ -1566,11 +1566,20 @@ void
 pagebuf_delwri_dequeue(
 	xfs_buf_t		*pb)
 {
-	PB_TRACE(pb, "delwri_uq", 0);
+	int			dequeued = 0;
+
 	spin_lock(&pbd_delwrite_lock);
-	list_del_init(&pb->pb_list);
+	if ((pb->pb_flags & PBF_DELWRI) && !list_empty(&pb->pb_list)) {
+		list_del_init(&pb->pb_list);
+		dequeued = 1;
+	}
 	pb->pb_flags &= ~PBF_DELWRI;
 	spin_unlock(&pbd_delwrite_lock);
+
+	if (dequeued)
+		pagebuf_rele(pb);
+
+	PB_TRACE(pb, "delwri_dq", (long)dequeued);
 }
 
 STATIC void
