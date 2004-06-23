12e81b0ce5060a282e0801521f7e8d9f32f87860
Jaroslav Kysela <perex@suse.cz>
2004-06-23 16:37:24 +0200

    ALSA CVS update ATIIXP driver,VIA82xx driver Added the missing RESUME info bits to pcm.
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/atiixp.c
+++ sound/pci/atiixp.c
@@ -996,6 +996,7 @@ static snd_pcm_hardware_t snd_atiixp_pcm_hw =
 {
 	.info =			(SNDRV_PCM_INFO_MMAP | SNDRV_PCM_INFO_INTERLEAVED |
 				 SNDRV_PCM_INFO_BLOCK_TRANSFER |
+				 SNDRV_PCM_INFO_RESUME |
 				 SNDRV_PCM_INFO_MMAP_VALID),
 	.formats =		SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S32_LE,
 	.rates =		SNDRV_PCM_RATE_48000,
