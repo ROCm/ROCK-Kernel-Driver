a44115192f55f8b7a22d511cc194d7880a2ba553
Andrew Morton <akpm@osdl.org>
2004-06-23 18:54:04 -0700

    [PATCH] vm: vfs shrinkage tuning
    Some people want the dentry and inode caches shrink harder, others want them
    shrunk more reluctantly.
    
    The patch adds /proc/sys/vm/vfs_cache_pressure, which tunes the vfs cache
    versus pagecache scanning pressure.
    
    - at vfs_cache_pressure=0 we don't shrink dcache and icache at all.
    
    - at vfs_cache_pressure=100 there is no change in behaviour.
    
    - at vfs_cache_pressure > 100 we reclaim dentries and inodes harder.
    
    
    The number of megabytes of slab left after a slocate.cron on my 256MB test
    box:
    
    vfs_cache_pressure=100000   33480
    vfs_cache_pressure=10000    61996
    vfs_cache_pressure=1000     104056
    vfs_cache_pressure=200      166340
    vfs_cache_pressure=100      190200
    vfs_cache_pressure=50       206168
    
    Of course, this just left more directory and inode pagecache behind instead of
    vfs cache.  Interestingly, on this machine the entire slocate run fits into
    pagecache, but not into VFS caches.
    
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/dcache.c
+++ fs/dcache.c
@@ -32,8 +32,9 @@
 #include <linux/swap.h>
 #include <linux/bootmem.h>
 
-#define DCACHE_PARANOIA 1
 /* #define DCACHE_DEBUG 1 */
+
+int sysctl_vfs_cache_pressure = 100;
 
 spinlock_t dcache_lock __cacheline_aligned_in_smp = SPIN_LOCK_UNLOCKED;
 seqlock_t rename_lock __cacheline_aligned_in_smp = SEQLOCK_UNLOCKED;
@@ -664,7 +665,7 @@ static int shrink_dcache_memory(int nr, unsigned int gfp_mask)
 			return -1;
 		prune_dcache(nr);
 	}
-	return dentry_stat.nr_unused;
+	return (dentry_stat.nr_unused / 100) * sysctl_vfs_cache_pressure;
 }
 
 /**
