1dd040ff89736c120c593fceb2ae55e11d500a7c
Randy Dunlap <randy.dunlap@verizon.net>
2004-06-26 07:07:04 -0500

    [PATCH] fdomain screwup
    | From: Andrew Morton
    | To: linux-scsi@vger.kernel.org
    | Subject: fdomain screwup
    |
    | - fdomain_setup() is marked __init, but is called from non-__init
    |   fdomain_16x0_detect()
    |
    | - fdomain_setup() is declared in drivers/scsi/pcmcia/fdomain_stub.c as
    |   taking two arguments, but is implemented in fdomain_setup() taking a
    |   single argument.
    |
    |   Please, never ever ever put extern function declarations in .c files.
    |   Put it in a header file which is visible to the definition and to all
    |   callsites.
    |
    | - fdomain_setup() is declared static, hence the linkage fails.
    |
    | - fdomain_16x0_bus_reset() is implemented in drivers/scsi/fdomain.c but
    |   has static scope, so the call from drivers/scsi/pcmcia/fdomain_stub.c
    |   doesn't work.
    
    [I didn't find this to be the case, so I didn't fix it.]
    
    | - fdomain_16x0_bus_reset() has an extern declaration in
    |   drivers/scsi/pcmcia/fdomain_stub.c.  This should be moved to a header
    |   file which is visible to etc...
    
    Builds and loads.  No hardware to test.
    
    Signed-off-by: Randy Dunlap <rddunlap@osdl.org>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/pcmcia/fdomain_stub.c
+++ drivers/scsi/pcmcia/fdomain_stub.c
@@ -45,6 +45,7 @@
 
 #include "scsi.h"
 #include <scsi/scsi_host.h>
+#include "fdomain.h"
 
 #include <pcmcia/version.h>
 #include <pcmcia/cs_types.h>
@@ -84,10 +85,6 @@ typedef struct scsi_info_t {
     struct Scsi_Host	*host;
 } scsi_info_t;
 
-extern Scsi_Host_Template fdomain_driver_template;
-extern void fdomain_setup(char *str, int *ints);
-extern struct Scsi_Host *__fdomain_16x0_detect( Scsi_Host_Template *tpnt );
-extern int fdomain_16x0_bus_reset(Scsi_Cmnd *SCpnt);
 
 static void fdomain_release(dev_link_t *link);
 static int fdomain_event(event_t event, int priority,
@@ -189,7 +186,7 @@ static void fdomain_config(dev_link_t *link)
     scsi_info_t *info = link->priv;
     tuple_t tuple;
     cisparse_t parse;
-    int i, last_ret, last_fn, ints[3];
+    int i, last_ret, last_fn;
     u_char tuple_data[64];
     char str[16];
     struct Scsi_Host *host;
@@ -229,11 +226,8 @@ static void fdomain_config(dev_link_t *link)
     release_region(link->io.BasePort1, link->io.NumPorts1);
 
     /* Set configuration options for the fdomain driver */
-    ints[0] = 2;
-    ints[1] = link->io.BasePort1;
-    ints[2] = link->irq.AssignedIRQ;
     sprintf(str, "%d,%d", link->io.BasePort1, link->irq.AssignedIRQ);
-    fdomain_setup(str, ints);
+    fdomain_setup(str);
     
     host = __fdomain_16x0_detect(&fdomain_driver_template);
     if (!host) {
