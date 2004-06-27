7518f92d71e5c95d254cc04cb30cebbde6d783ea
David Howells <dhowells@redhat.com>
2004-06-26 20:53:26 -0700

    [PATCH] ppc64: fix usage of cpumask_t on iSeries
    This fixes a bit of iSeries code that hadn't been changed to reflect that
    cpumask_t is now a struct and not an integer type.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc64/kernel/irq.c
+++ arch/ppc64/kernel/irq.c
@@ -807,11 +807,10 @@ static int prof_cpu_mask_write_proc (struct file *file, const char __user *buffe
 	{
 		unsigned i;
 		for (i=0; i<NR_CPUS; ++i) {
-			if ( paca[i].prof_buffer && (new_value & 1) )
+			if ( paca[i].prof_buffer && cpu_isset(1, new_value) )
 				paca[i].prof_enabled = 1;
 			else
 				paca[i].prof_enabled = 0;
-			new_value >>= 1;
 		}
 	}
 #endif
