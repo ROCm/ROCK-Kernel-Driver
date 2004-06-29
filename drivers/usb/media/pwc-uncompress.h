60cfbb5503bf8979a8769d58d5bd976f9f1e12f9
Andrew Morton <akpm@osdl.org>
2004-06-28 19:48:16 -0700

    [PATCH] USB: pwc-uncompress.h
    For some reason, gcc-2.95.4 dies horridly on those asmlinkage
    declarations.
    
    I was unable to work out _why_ those functions have asmlinkage, as there
    seem to be no instances of them - nobody calls
    pwc_register_decompressor().
     What's up with that?
    
    
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/usb/media/pwc-uncompress.h
+++ drivers/usb/media/pwc-uncompress.h
@@ -48,9 +48,10 @@ struct pwc_decompressor
 	int  type;		/* type of camera (645, 680, etc) */
 	int  table_size;	/* memory needed */
 
-	asmlinkage void (* init)(int type, int release, void *buffer, void *table);	/* Initialization routine; should be called after each set_video_mode */
-	asmlinkage void (* exit)(void);	/* Cleanup routine */
-	asmlinkage void (* decompress)(struct pwc_coord *image, struct pwc_coord *view, struct pwc_coord *offset,
+	void (* init)(int type, int release, void *buffer, void *table);	/* Initialization routine; should be called after each set_video_mode */
+	void (* exit)(void);	/* Cleanup routine */
+	void (* decompress)(struct pwc_coord *image, struct pwc_coord *view,
+			    struct pwc_coord *offset,
                             void *src, void *dst, int flags,
 	                    void *table, int bandlength);
 	void (* lock)(void);	/* make sure module cannot be unloaded */
