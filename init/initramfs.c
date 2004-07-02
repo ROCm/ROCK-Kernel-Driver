0d39ac56a8686eb6cbb3cac06d5cb2ea6a2810b4
Mika Kukkonen <mika@osdl.org>
2004-07-01 20:36:36 -0700

    [PATCH] Remaining sparse warnings in allnoconfig
    Attached is a smallish patch for couple trivial sparse warnings in
    allnoconfig build and more importantly an "excuses" text file explaining
    why the rest have not been fixed.
    
    Basically all of them (with the exception of the one in Andrews tree) need
    some serious re-engineering.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- init/initramfs.c
+++ init/initramfs.c
@@ -169,7 +169,7 @@ static int __init do_collect(void)
 	memcpy(collect, victim, n);
 	eat(n);
 	collect += n;
-	if (remains -= n)
+	if ((remains -= n) != 0)
 		return 1;
 	state = next_state;
 	return 0;
