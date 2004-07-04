7403dd4558d2d3d9c446eb7a200fa961488f17a8
Jaroslav Kysela <perex@suse.cz>
2004-07-04 19:54:07 +0200

    ALSA CVS update EMU10K1/EMU10K2 driver Clean up the invalid (commented out) lines for emu10k1x.
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/emu10k1/emu10k1.c
+++ sound/pci/emu10k1/emu10k1.c
@@ -71,9 +71,6 @@ MODULE_PARM_DESC(enable_ir, "Enable IR.");
 
 static struct pci_device_id snd_emu10k1_ids[] = {
 	{ 0x1102, 0x0002, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0 },	/* EMU10K1 */
-#if 0 /* FIXME: not working! */
-	{ 0x1102, 0x0006, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0 },	/* Dell OEM version (EMU10K1) */
-#endif
 	{ 0x1102, 0x0004, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 1 },	/* Audigy */
 	{ 0, }
 };
