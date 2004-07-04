1dd7f7cab40e7e75f59e9fcff860b9bc65670e6e
Marcel Holtmann <marcel@holtmann.org>
2004-07-04 21:28:19 +0200

    [Bluetooth] Allocate protocol number for AVDTP support
    This patch allocates the next free protocol number for the upcoming
    AVDTP support.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -51,13 +51,13 @@
 #define BT_DBG(D...)
 #endif
 
-#define VERSION "2.5"
+#define VERSION "2.6"
 
 struct proc_dir_entry *proc_bt;
 EXPORT_SYMBOL(proc_bt);
 
 /* Bluetooth sockets */
-#define BT_MAX_PROTO	7
+#define BT_MAX_PROTO	8
 static struct net_proto_family *bt_proto[BT_MAX_PROTO];
 
 static kmem_cache_t *bt_sock_cache;
