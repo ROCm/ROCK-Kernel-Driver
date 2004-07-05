71fbaefdde14fc95df23816f10c674e9cbd481e6
David S. Miller <davem@nuts.davemloft.net>
2004-07-05 01:53:46 -0700

    [TG3]: Bump driver version and reldate.

--- drivers/net/tg3.c
+++ drivers/net/tg3.c
@@ -57,8 +57,8 @@
 
 #define DRV_MODULE_NAME		"tg3"
 #define PFX DRV_MODULE_NAME	": "
-#define DRV_MODULE_VERSION	"3.7"
-#define DRV_MODULE_RELDATE	"July 2, 2004"
+#define DRV_MODULE_VERSION	"3.8"
+#define DRV_MODULE_RELDATE	"July 5, 2004"
 
 #define TG3_DEF_MAC_MODE	0
 #define TG3_DEF_RX_MODE		0
