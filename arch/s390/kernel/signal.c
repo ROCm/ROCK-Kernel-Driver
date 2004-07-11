1a914247a359037d8bcb29b2e0a114e5cf39e2ae
Martin Schwidefsky <schwidefsky@de.ibm.com>
2004-07-10 19:38:31 -0700

    [PATCH] s390: signals & exceptions
    From: Martin Schwidefsky <schwidefsky@de.ibm.com>
    From: Ulrich Weigand <uweigand@de.ibm.com>
    
    s390 core changes:
     - Add signo between signal frame and the signal return instruction on the
       user stack for backtrace over signal handlers.
     - Add hfp floating point exceptions.
     - Use a single function for region, segment and page translation exceptions.
     - Discard SIGTRAP for single stepped instructions if the trapping instruction
       is repeated (normal memory faults) or if another signal is delivered anyway.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/s390/kernel/signal.c
+++ arch/s390/kernel/signal.c
@@ -39,6 +39,7 @@ typedef struct
 	__u8 callee_used_stack[__SIGNAL_FRAMESIZE];
 	struct sigcontext sc;
 	_sigregs sregs;
+	int signo;
 	__u8 retcode[S390_SYSCALL_SIZE];
 } sigframe;
 
@@ -350,6 +351,10 @@ static void setup_frame(int sig, struct k_sigaction *ka,
 	   To avoid breaking binary compatibility, they are passed as args. */
 	regs->gprs[4] = current->thread.trap_no;
 	regs->gprs[5] = current->thread.prot_addr;
+
+	/* Place signal number on stack to allow backtrace from handler.  */
+	if (__put_user(regs->gprs[2], (int __user *) &frame->signo))
+		goto give_sigsegv;
 	return;
 
 give_sigsegv:
