33ebcaa2dde062291064ef137264f94ddfba89d8
Alexander Viro <viro@parcelfarce.linux.theplanet.co.uk>
2004-07-12 21:02:10 -0700

    [PATCH] sparse: alpha sparse infrastructure
    	* added usual CHECK assignment in Makefile
    	* switched uaccess.h to __check_uptr()
    	* added L on long constants (ones missed earlier)
    	* added __user in osf_sys.c (duh - I've added __user to cast in
    the initializer, but forgot to add it in declaration)

--- arch/alpha/kernel/osf_sys.c
+++ arch/alpha/kernel/osf_sys.c
@@ -1303,7 +1303,7 @@ osf_fix_iov_len(const struct iovec __user *iov, unsigned long count)
 	unsigned long i;
 
 	for (i = 0 ; i < count ; i++) {
-		int *iov_len_high = (int __user *)&iov[i].iov_len + 1;
+		int __user *iov_len_high = (int __user *)&iov[i].iov_len + 1;
 
 		if (put_user(0, iov_len_high))
 			return -EFAULT;
