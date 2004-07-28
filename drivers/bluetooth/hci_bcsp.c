94ae4fb0cf2ac5fee353a5ab3cb2716d83ab9cfe
Marcel Holtmann <marcel@holtmann.org>
2004-07-28 03:42:21 +0200

    [Bluetooth] Replace BCSP retransmitting message with BT_DBG
    The message "Timeout, retransmitting 1 pkts" is not an error and so
    change this into a debug message.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_bcsp.c
+++ drivers/bluetooth/hci_bcsp.c
@@ -633,7 +633,8 @@ static void bcsp_timed_event(unsigned long arg)
 	struct sk_buff *skb;
 	unsigned long flags;
 
-	BT_ERR("Timeout, retransmitting %u pkts", bcsp->unack.qlen);
+	BT_DBG("hu %p retransmitting %u pkts", hu, bcsp->unack.qlen);
+
 	spin_lock_irqsave(&bcsp->unack.lock, flags);
 
 	while ((skb = __skb_dequeue_tail(&bcsp->unack)) != NULL) {
