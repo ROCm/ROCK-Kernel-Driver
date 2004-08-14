d91218ae7d44e6802c16a07a17ff7ec427354351
Tony Lindgren <tony@com.rmk.(none)>
2004-08-14 19:36:09 +0100

    [ARM PATCH] 2008/1: OMAP update 4/6: Include files
    Patch from Tony Lindgren
    
    This patch syncs the mainline kernel with the linux-omap tree.
    The highlights of the patch are:
    - DMA fixes from Samuel Ortiz
    - USB low level configuration from David Brownell
    - H3 support from Nishant Kamat
    - TPS65010 power management interface from Dirk Behme

--- include/asm-arm/arch-omap/tps65010.h
+++ include/asm-arm/arch-omap/tps65010.h
@@ -1,8 +1,8 @@
-/* linux/include/asm-arm/arch-omap/omap1510.h
+/* linux/include/asm-arm/arch-omap/tps65010.h
  *
- * Hardware definitions for TI OMAP1510 processor.
+ * Functions to access TPS65010 power management device.
  *
- * Cleanup for Linux-2.6 by Dirk Behme <dirk.behme@de.bosch.com>
+ * Copyright (C) 2004 Dirk Behme <dirk.behme@de.bosch.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -25,37 +25,56 @@
  * 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#ifndef __ASM_ARCH_OMAP1510_H
-#define __ASM_ARCH_OMAP1510_H
+#ifndef __ASM_ARCH_TPS65010_H
+#define __ASM_ARCH_TPS65010_H
 
 /*
  * ----------------------------------------------------------------------------
- * Base addresses
+ * Macros used by exported functions
  * ----------------------------------------------------------------------------
  */
 
-/* Syntax: XX_BASE = Virtual base address, XX_START = Physical base address */
-
-#define OMAP1510_SRAM_BASE	0xD0000000
-#define OMAP1510_SRAM_SIZE	(SZ_128K + SZ_64K)
-#define OMAP1510_SRAM_START	0x20000000
-
-#define OMAP1510_DSP_BASE	0xE0000000
-#define OMAP1510_DSP_SIZE	0x28000
-#define OMAP1510_DSP_START	0xE0000000
-
-#define OMAP1510_DSPREG_BASE	0xE1000000
-#define OMAP1510_DSPREG_SIZE	SZ_128K
-#define OMAP1510_DSPREG_START	0xE1000000
+#define LED1  1
+#define LED2  2
+#define OFF   0
+#define ON    1
+#define BLINK 2
+#define GPIO1 1
+#define GPIO2 2
+#define GPIO3 3
+#define GPIO4 4
+#define LOW   0
+#define HIGH  1
 
 /*
  * ----------------------------------------------------------------------------
- * Memory used by power management
+ * Exported functions
  * ----------------------------------------------------------------------------
  */
 
-#define OMAP1510_SRAM_IDLE_SUSPEND	(OMAP1510_SRAM_BASE + OMAP1510_SRAM_SIZE - 0x200)
-#define OMAP1510_SRAM_API_SUSPEND	(OMAP1510_SRAM_IDLE_SUSPEND + 0x100)
+/* Draw from VBUS:
+ *   0 mA -- DON'T DRAW (might supply power instead)
+ * 100 mA -- usb unit load (slowest charge rate)
+ * 500 mA -- usb high power (fast battery charge)
+ */
+extern int tps65010_set_vbus_draw(unsigned mA);
 
-#endif /*  __ASM_ARCH_OMAP1510_H */
+/* tps65010_set_gpio_out_value parameter:
+ * gpio:  GPIO1, GPIO2, GPIO3 or GPIO4
+ * value: LOW or HIGH
+ */
+extern int tps65010_set_gpio_out_value(unsigned gpio, unsigned value);
+
+/* tps65010_set_led parameter:
+ * led:  LED1 or LED2
+ * mode: ON, OFF or BLINK
+ */
+extern int tps65010_set_led(unsigned led, unsigned mode);
+
+/* tps65010_set_low_pwr parameter:
+ * mode: ON or OFF
+ */
+extern int tps65010_set_low_pwr(unsigned mode);
+
+#endif /*  __ASM_ARCH_TPS65010_H */
 
