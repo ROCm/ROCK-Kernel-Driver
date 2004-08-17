46e2fa2d4ff7a00927d49c28aa89d7f3520f75d8
Hubert Mantel <mantel@suse.de>
2004-08-17 15:42:50 +0000

    fix swsusp-platform.diff
    suse-commit: 328defc4a8640b94e5547e84bc7831dae195cf74

--- drivers/acpi/sleep/main.c
+++ drivers/acpi/sleep/main.c
@@ -216,7 +216,10 @@ static int __init acpi_sleep_init(void)
 			if (acpi_gbl_FACS->S4bios_f) {
 				sleep_states[i] = 1;
 				printk(" S4bios");
-			}
+				acpi_pm_ops.pm_disk_mode = PM_DISK_FIRMWARE;
+			} 
+			if (sleep_states[i])
+				acpi_pm_ops.pm_disk_mode = PM_DISK_PLATFORM;
 		}
 	}
 	printk(")\n");
