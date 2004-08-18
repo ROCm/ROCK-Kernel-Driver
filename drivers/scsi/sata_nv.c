b5ac4f2c850f82a1d271201a5a428be451630d8f
Andrew Chew <achew@nvidia.com>
2004-08-18 12:39:03 -0400

    [libata sata_nv] fix leak on error
    Spotted by Francois Romieu.

--- drivers/scsi/sata_nv.c
+++ drivers/scsi/sata_nv.c
@@ -372,7 +372,7 @@ static int nv_init_one (struct pci_dev *pdev, const struct pci_device_id *ent)
 		probe_ent->mmio_base = ioremap(pci_resource_start(pdev, 5),
 				pci_resource_len(pdev, 5));
 		if (probe_ent->mmio_base == NULL)
-			goto err_out_free_ent;
+			goto err_out_iounmap;
 
 		base = (unsigned long)probe_ent->mmio_base;
 
@@ -396,11 +396,15 @@ static int nv_init_one (struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	rc = ata_device_add(probe_ent);
 	if (rc != NV_PORTS)
-		goto err_out_free_ent;
+		goto err_out_iounmap;
 
 	kfree(probe_ent);
 
 	return 0;
+
+err_out_iounmap:
+	if (host->host_desc->host_flags & NV_HOST_FLAGS_SCR_MMIO)
+		iounmap(probe_ent->mmio_base);
 
 err_out_free_ent:
 	kfree(probe_ent);
