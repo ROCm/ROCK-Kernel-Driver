6b6bf0458d7718df85add476ca67eff7a0e5ec98
Ben Dooks <ben-linux@org.rmk.(none)>
2004-08-19 01:18:48 +0100

    [ARM PATCH] 2026/1: S3C2410 - header text for arch/arm/mach-s3c2410/s3c2410.h
    Patch from Ben Dooks
    
    Attached is a copyright / explanation header for the
    arch/arm/mach-s3c2410/s3c2410.h header files

--- arch/arm/mach-s3c2410/s3c2410.h
+++ arch/arm/mach-s3c2410/s3c2410.h
@@ -1,4 +1,18 @@
-
+/* arch/arm/mach-s3c2410/s3c2410.h
+ *
+ * Copyright (c) 2004 Simtec Electronics
+ * Ben Dooks <ben@simtec.co.uk>
+ *
+ * Header file for s3c2410 machine directory
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Modifications:
+ *     18-Aug-2004 BJD  Created initial version
+ *
+*/
 
 extern void s3c2410_map_io(struct map_desc *, int count);
 
