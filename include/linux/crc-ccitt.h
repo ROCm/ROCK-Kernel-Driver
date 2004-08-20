77a6ab5708bbbbe494991fb8acfa913790906f7c
Hubert Mantel <mantel@suse.de>
2004-08-20 14:41:59 +0000

    - use __u16 instead of u16 in crc-ccitt.h since this is exposed   to user space (#43884)
    suse-commit: 502b87f9f15dfc50c1e9c6fa4ea6a5cee2448732

--- include/linux/crc-ccitt.h
+++ include/linux/crc-ccitt.h
@@ -3,11 +3,11 @@
 
 #include <linux/types.h>
 
-extern u16 const crc_ccitt_table[256];
+extern __u16 const crc_ccitt_table[256];
 
-extern u16 crc_ccitt(u16 crc, const u8 *buffer, size_t len);
+extern __u16 crc_ccitt(__u16 crc, const __u8 *buffer, size_t len);
 
-static inline u16 crc_ccitt_byte(u16 crc, const u8 c)
+static inline __u16 crc_ccitt_byte(__u16 crc, const __u8 c)
 {
 	return (crc >> 8) ^ crc_ccitt_table[(crc ^ c) & 0xff];
 }
