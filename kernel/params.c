f1577452f18e893e59bba53b3bda4fab17cc66c6
Arnd Bergmann <arnd@arndb.de>
2004-08-22 22:26:41 -0700

    [PATCH] fix reading string module parameters in sysfs
    Reading the contents of a module_param_string through sysfs currently
    oopses because the param_get_charp() function cannot operate on a
    kparam_string struct.  This introduces the required param_get_string.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/params.c
+++ kernel/params.c
@@ -339,6 +339,12 @@ int param_set_copystring(const char *val, struct kernel_param *kp)
 	return 0;
 }
 
+int param_get_string(char *buffer, struct kernel_param *kp)
+{
+	struct kparam_string *kps = kp->arg;
+	return strlcpy(buffer, kps->string, kps->maxlen);
+}
+
 EXPORT_SYMBOL(param_set_short);
 EXPORT_SYMBOL(param_get_short);
 EXPORT_SYMBOL(param_set_ushort);
@@ -360,3 +366,4 @@ EXPORT_SYMBOL(param_get_invbool);
 EXPORT_SYMBOL(param_array_set);
 EXPORT_SYMBOL(param_array_get);
 EXPORT_SYMBOL(param_set_copystring);
+EXPORT_SYMBOL(param_get_string);
