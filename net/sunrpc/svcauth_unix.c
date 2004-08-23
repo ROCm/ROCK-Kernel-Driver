45bf77b483f2cf8e5aee5aff7d42b073977a7a1b
Neil Brown <neilb@cse.unsw.edu.au>
2004-08-22 23:01:08 -0700

    [PATCH] nfsd: fix ip_map cache reference count leak.
    Fix a leak: when auth_unix_lookup sets CACHE_NEGATIVE, it should also
    auth_domain_put() the reference it holds in ipm->m_client, since setting
    CACHE_NEGATIVE prevents ip_map_put() from putting the reference itself.
    
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>
    Signed-off-by: Neil Brown <neilb@cse.unsw.edu.au>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- net/sunrpc/svcauth_unix.c
+++ net/sunrpc/svcauth_unix.c
@@ -316,7 +316,8 @@ struct auth_domain *auth_unix_lookup(struct in_addr addr)
 		return NULL;
 
 	if ((ipm->m_client->addr_changes - ipm->m_add_change) >0) {
-		set_bit(CACHE_NEGATIVE, &ipm->h.flags);
+		if (test_and_set_bit(CACHE_NEGATIVE, &ipm->h.flags) == 0)
+			auth_domain_put(&ipm->m_client->h);
 		rv = NULL;
 	} else {
 		rv = &ipm->m_client->h;
