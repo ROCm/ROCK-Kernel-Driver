1fd6c4af84c9bb116c307127a60d8e3e9a989b0b
Ryan S. Arnold <rsa@us.ibm.com>
2004-08-30 20:39:20 -0700

    [PATCH] interrupt driven hvc_console as vio device
    This is an hvc_console patch which provides driver and ppc64 architecture
    fixes to enable the hvc_console driver to register itself as a vio device
    with the vio bus, provide hotplug add/remove for vty adapters, and act as
    an interrupt driven driver on Power-5 hardware or remain as a polling
    driver on Power-4 hardware.
    
    arch/ppc64/kernel/hvconsole.c
    =============================
    
    - Changed hvc_get_chars() and hvc_put_chars() api to take vtermno rather
      than index number.
    
    - Added hvc_find_vtys() function which walks the bus looking for
      vterm/vty devices to callback to the hvc_console driver.  This provides
      console output functionality prior to early console init (pre mem init
      and pre device probe).
    
    include/asm-ppc64/hvconsole.h
    =============================
    
    - Changed hvc_get_chars() and hvc_put_chars() api to take vtermno rather
      than index number.
    
    - Added hvc_find_vtys() function.
    
    - Added hvc_instantiate() function which is implemented by a console
      driver wanting to receive a callback of and early console init.
    
    drivers/char/hvc_console.c
    ==========================
    
    - Switch khvcd from kernel_threads to kthreads which got rid of
      deprecated daemonize().
    
    - Added module exit clause to be thorough (not terribly necessary with a
      console driver of course)
    
    - Added early discovery of vterm/vty adapters by doing a bus walk on
      early console init which results in hvc_instantiate() callback and
      addition of the vtermno into a static array of vtermnos supported as
      console adapters (meaning the console api's work against these vtermnos
      prior to full console initialization).
    
    - This driver is now registered as a vio driver which means that vty
      adapters are now managed via probe/remove.  This means hvc_console
      supports hotplug vty adapters.
    
    - Driver now requests more device nodes than what was found on the
      initial bus walk when registered as a tty driver to make room for hotplug
      vty adapters.  These secondary vty adapters provide a tty tunnel between
      partitions.
    
    - Removed static hvc_struct array and replaced with a linux list that has
      elements (hvc_struct instances) added/removed on probe/remove AFTER early
      console init.  This is important because kmalloc can't be done at early
      console init.
    
    - Driver now either runs in interrupt driven mode or in polling mode on
      older hardware.  The khvcd is smart enough to not 'schedule()' when there
      are no interrupts.
    
    - kobjects are now used for ref counting on the hvc_struct instances.
    
    - This driver puts the tty layer to sleep on hvc_close() if there are
      pending data writes being blocked by firmware.
    
    - Removed useless spinlocks in hvc_chars_in_buffer() and hvc_write_room.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Ryan S. Arnold <rsa@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc64/kernel/hvconsole.c
+++ arch/ppc64/kernel/hvconsole.c
@@ -1,6 +1,10 @@
 /*
  * hvconsole.c
  * Copyright (C) 2004 Hollis Blanchard, IBM Corporation
+ * Copyright (C) 2004 IBM Corporation
+ *
+ * Additional Author(s):
+ *  Ryan S. Arnold <rsa@us.ibm.com>
  *
  * LPAR console support.
  * 
@@ -22,14 +26,22 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <asm/hvcall.h>
-#include <asm/prom.h>
 #include <asm/hvconsole.h>
+#include <asm/prom.h>
 
-int hvc_get_chars(int index, char *buf, int count)
+/**
+ * hvc_get_chars - retrieve characters from firmware for denoted vterm adatper
+ * @vtermno: The vtermno or unit_address of the adapter from which to fetch the
+ *	data.
+ * @buf: The character buffer into which to put the character data fetched from
+ *	firmware.
+ * @count: not used?
+ */
+int hvc_get_chars(uint32_t vtermno, char *buf, int count)
 {
 	unsigned long got;
 
-	if (plpar_hcall(H_GET_TERM_CHAR, index, 0, 0, 0, &got,
+	if (plpar_hcall(H_GET_TERM_CHAR, vtermno, 0, 0, 0, &got,
 		(unsigned long *)buf, (unsigned long *)buf+1) == H_Success) {
 		/*
 		 * Work around a HV bug where it gives us a null
@@ -53,40 +65,56 @@ int hvc_get_chars(int index, char *buf, int count)
 
 EXPORT_SYMBOL(hvc_get_chars);
 
-int hvc_put_chars(int index, const char *buf, int count)
+/**
+ * hvc_put_chars: send characters to firmware for denoted vterm adapter
+ * @vtermno: The vtermno or unit_address of the adapter from which the data
+ *	originated.
+ * @buf: The character buffer that contains the character data to send to
+ *	firmware.
+ * @count: Send this number of characters.
+ */
+int hvc_put_chars(uint32_t vtermno, const char *buf, int count)
 {
 	unsigned long *lbuf = (unsigned long *) buf;
 	long ret;
 
-	ret = plpar_hcall_norets(H_PUT_TERM_CHAR, index, count, lbuf[0],
+	ret = plpar_hcall_norets(H_PUT_TERM_CHAR, vtermno, count, lbuf[0],
 				 lbuf[1]);
 	if (ret == H_Success)
 		return count;
 	if (ret == H_Busy)
 		return 0;
-	return -1;
+	return -EIO;
 }
 
 EXPORT_SYMBOL(hvc_put_chars);
 
-/* return the number of client vterms present */
-/* XXX this requires an interface change to handle multiple discontiguous
- * vterms */
-int hvc_count(int *start_termno)
+/*
+ * We hope/assume that the first vty found corresponds to the first console
+ * device.
+ */
+int hvc_find_vtys(void)
 {
 	struct device_node *vty;
 	int num_found = 0;
 
-	/* consider only the first vty node.
-	 * we should _always_ be able to find one. */
-	vty = of_find_node_by_name(NULL, "vty");
-	if (vty && device_is_compatible(vty, "hvterm1")) {
-		u32 *termno = (u32 *)get_property(vty, "reg", NULL);
+	for (vty = of_find_node_by_name(NULL, "vty"); vty != NULL;
+			vty = of_find_node_by_name(vty, "vty")) {
+		uint32_t *vtermno;
 
-		if (termno && start_termno)
-			*start_termno = *termno;
-		num_found = 1;
-		of_node_put(vty);
+		/* We have statically defined space for only a certain number of
+		 * console adapters. */
+		if (num_found >= MAX_NR_HVC_CONSOLES)
+			break;
+
+		vtermno = (uint32_t *)get_property(vty, "reg", NULL);
+		if (!vtermno)
+			continue;
+
+		if (device_is_compatible(vty, "hvterm1")) {
+			hvc_instantiate(*vtermno, num_found);
+			++num_found;
+		}
 	}
 
 	return num_found;
