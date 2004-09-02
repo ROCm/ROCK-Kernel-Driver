0974040725f2b14b4f638026299f3f6fd9d6f386
Olaf Hering <olh@suse.de>
2004-09-02 12:36:12 +0000

    R patches.fixes/ppc32-pmac_zilog-consoleinit
    suse-commit: 8e07363937776cca407c78ca86154d165bd6e247

--- drivers/serial/pmac_zilog.c
+++ drivers/serial/pmac_zilog.c
@@ -1960,9 +1960,6 @@ static int __init pmz_console_setup(struct console *co, char *options)
 	int flow = 'n';
 	unsigned long pwr_delay;
 
-	if(!(machine_is_compatible("MacRISC") || machine_is_compatible("MacRISC3")))
-			return -ENODEV;
-
 	/*
 	 * XServe's default to 57600 bps
 	 */
