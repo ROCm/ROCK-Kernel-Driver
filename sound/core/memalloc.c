d983846d061e7dc2c6d30a04bceab41697b1a1e4
Jaroslav Kysela <perex@suse.cz>
2004-09-07 00:14:51 +0200

    [ALSA]  Enable __GFP_NOWARN as default for buffer allocation
    Memalloc module
    __GFP_NOWARN is enabled for DMA buffer allocation regardless of
    its size.  The DMA buffer allocation is not a critical task.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/memalloc.c
+++ sound/core/memalloc.c
@@ -246,10 +246,9 @@ static void *snd_malloc_dev_pages(struct device *dev, size_t size, dma_addr_t *d
 	snd_assert(size > 0, return NULL);
 	snd_assert(dma != NULL, return NULL);
 	pg = get_order(size);
-	gfp_flags = GFP_KERNEL;
-	gfp_flags |= __GFP_NORETRY; /* don't trigger OOM-killer */
-	if (pg > 0)
-		gfp_flags |= __GFP_NOWARN;
+	gfp_flags = GFP_KERNEL
+		| __GFP_NORETRY /* don't trigger OOM-killer */
+		| __GFP_NOWARN; /* no stack trace print - this call is non-critical */
 	res = dma_alloc_coherent(dev, PAGE_SIZE << pg, dma, gfp_flags);
 	if (res != NULL) {
 #ifdef NEED_RESERVE_PAGES
