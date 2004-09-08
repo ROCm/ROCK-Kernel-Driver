765dad09b4ac101a32d87af2bb793c3060497d3c
Marcelo Tosatti <marcelo.tosatti@cyclades.com>
2004-09-07 17:51:17 -0700

    [PATCH] small wait_on_page_writeback_range() optimization
    filemap_fdatawait() calls wait_on_page_writeback_range() with -1 as "end"
    parameter.  This is not needed since we know the EOF from the inode.  Use
    that instead.
    
    Signed-off-by: Marcelo Tosatti <marcelo.tosatti@cyclades.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -287,7 +287,13 @@ EXPORT_SYMBOL(sync_page_range);
  */
 int filemap_fdatawait(struct address_space *mapping)
 {
-	return wait_on_page_writeback_range(mapping, 0, -1);
+	loff_t i_size = i_size_read(mapping->host);
+
+	if (i_size == 0)
+		return 0;
+
+	return wait_on_page_writeback_range(mapping, 0,
+				(i_size - 1) >> PAGE_CACHE_SHIFT);
 }
 EXPORT_SYMBOL(filemap_fdatawait);
 
