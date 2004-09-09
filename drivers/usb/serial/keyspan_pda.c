5426b1c448a21a259f545827f049f5edb23cb9d5
Greg Kroah-Hartman <greg@kroah.com>
2004-09-08 21:54:43 -0700

    USB: fix usb_unlink_urb() usage in keyspan_pda driver
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/usb/serial/keyspan_pda.c
+++ drivers/usb/serial/keyspan_pda.c
@@ -291,7 +291,7 @@ static void keyspan_pda_rx_throttle (struct usb_serial_port *port)
 	   upon the device too. */
 
 	dbg("keyspan_pda_rx_throttle port %d", port->number);
-	usb_unlink_urb(port->interrupt_in_urb);
+	usb_kill_urb(port->interrupt_in_urb);
 }
 
 
@@ -712,8 +712,8 @@ static void keyspan_pda_close(struct usb_serial_port *port, struct file *filp)
 			keyspan_pda_set_modem_info(serial, 0);
 
 		/* shutdown our bulk reads and writes */
-		usb_unlink_urb (port->write_urb);
-		usb_unlink_urb (port->interrupt_in_urb);
+		usb_kill_urb(port->write_urb);
+		usb_kill_urb(port->interrupt_in_urb);
 	}
 }
 
