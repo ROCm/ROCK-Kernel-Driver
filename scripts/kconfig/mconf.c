c794e99c91d095ad07349222ff7a6be1c52d9153
Roman Zippel <zippel@linux-m68k.org>
2004-09-13 19:33:17 -0700

    [PATCH] properly fix double current_menu
    The two current_menu variables are really two separate variables, so keep
    them separate.
    
    Signed-off-by: Roman Zippel <zippel@linux-m68k.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- scripts/kconfig/mconf.c
+++ scripts/kconfig/mconf.c
@@ -89,7 +89,7 @@ static char *args[1024], **argptr = args;
 static int indent;
 static struct termios ios_org;
 static int rows = 0, cols = 0;
-struct menu *current_menu;
+static struct menu *current_menu;
 static int child_count;
 static int do_resize;
 static int single_menu_mode;
