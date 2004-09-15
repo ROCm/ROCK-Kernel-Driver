2d4b17f535a6644b3c1352bec5387a6a271a6a7c
Jens Axboe <axboe@suse.de>
2004-09-15 07:29:37 +0000

    - Update the series of barrier patches, luckily most are merged in   mainline this time!
    suse-commit: d89a09ee2295654791bebf72c0be4cd33c30752e

--- fs/ext3/super.c
+++ fs/ext3/super.c
@@ -1294,6 +1294,9 @@ static int ext3_fill_super (struct super_block *sb, void *data, int silent)
 	sbi->s_resuid = le16_to_cpu(es->s_def_resuid);
 	sbi->s_resgid = le16_to_cpu(es->s_def_resgid);
 
+	/* enable barriers by default */
+	set_opt(sbi->s_mount_opt, BARRIER);
+
 	if (!parse_options ((char *) data, sb, &journal_inum, 0))
 		goto failed_mount;
 
