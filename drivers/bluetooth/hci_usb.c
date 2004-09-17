94035d04ec5750f93641385bb538089502eba809
Andreas Gruenbacher <agruen@suse.de>
2004-09-17 10:27:55 +0000

    - patches.fixes/bluetooth-bnep-check-checksums: Check checksums   for BNEP - patches.fixes/bluetooth-broadcom-hidproxy-no-isoc: Don't use   ISOC transfers for Broadcom dongle - patches.fixes/bluetooth-l2cap-cmd-reject-fix: Don't send L2CAP   reject command for bad responses
    suse-commit: 74701170daa8144de15e32ddf13e37256cc79170

--- drivers/bluetooth/hci_usb.c
+++ drivers/bluetooth/hci_usb.c
@@ -96,7 +96,7 @@ static struct usb_device_id blacklist_ids[] = {
 	{ USB_DEVICE(0x0a5c, 0x2033), .driver_info = HCI_IGNORE },
 
 	/* Broadcom BCM2035 */
-	{ USB_DEVICE(0x0a5c, 0x200a), .driver_info = HCI_RESET },
+	{ USB_DEVICE(0x0a5c, 0x200a), .driver_info = HCI_RESET | HCI_BROKEN_ISOC },
 
 	/* ISSC Bluetooth Adapter v3.1 */
 	{ USB_DEVICE(0x1131, 0x1001), .driver_info = HCI_RESET },
