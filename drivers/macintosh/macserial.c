7e86d18ba72d14a184d30195d040207480c8e443
Nishanth Aravamudan <nacc@us.ibm.com>
2004-09-22 04:20:34 -0700

    [PATCH] macintosh/macserial: replace schedule_timeout() with msleep_interruptible()
    Use msleep_interruptible() instead of schedule_timeout() to guarantee the
    task delays as expected.
    
    Signed-off-by: Nishanth Aravamudan <nacc@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/macintosh/macserial.c
+++ drivers/macintosh/macserial.c
@@ -2002,8 +2002,7 @@ static void rs_close(struct tty_struct *tty, struct file * filp)
 
 	if (info->blocked_open) {
 		if (info->close_delay) {
-			current->state = TASK_INTERRUPTIBLE;
-			schedule_timeout(info->close_delay);
+			msleep_interruptible(jiffies_to_msecs(info->close_delay));
 		}
 		wake_up_interruptible(&info->open_wait);
 	}
@@ -2048,8 +2047,7 @@ static void rs_wait_until_sent(struct tty_struct *tty, int timeout)
 	if (timeout)
 		char_time = min_t(unsigned long, char_time, timeout);
 	while ((read_zsreg(info->zs_channel, 1) & ALL_SNT) == 0) {
-		current->state = TASK_INTERRUPTIBLE;
-		schedule_timeout(char_time);
+		msleep_interruptible(jiffies_to_msecs(char_time));
 		if (signal_pending(current))
 			break;
 		if (timeout && time_after(jiffies, orig_jiffies + timeout))
