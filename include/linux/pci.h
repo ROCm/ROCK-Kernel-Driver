9ec20a24ed211fe9bbb0dce022dcf31a43629454
Greg Kroah-Hartman <greg@kroah.com>
2004-09-22 02:25:38 -0700

    [PATCH] PCI: add "struct module *" to struct pci_driver to show symlink in sysfs for pci drivers.
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -631,9 +631,11 @@ struct pci_dynids {
 	unsigned int use_driver_data:1; /* pci_driver->driver_data is used */
 };
 
+struct module;
 struct pci_driver {
 	struct list_head node;
 	char *name;
+	struct module *owner;
 	const struct pci_device_id *id_table;	/* must be non-NULL for probe to be called */
 	int  (*probe)  (struct pci_dev *dev, const struct pci_device_id *id);	/* New device inserted */
 	void (*remove) (struct pci_dev *dev);	/* Device removed (NULL if not a hot-plug capable driver) */
