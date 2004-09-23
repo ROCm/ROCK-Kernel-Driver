5b9af9b786887e62048e95799acad5554a7eec69
Olaf Hering <olh@suse.de>
2004-09-23 22:19:04 +0000

    +- remove patches.fixes/usb-ehci_urb_done-barrier.patch +  this patch is not needed, the inital bug was triggered by +  list corruption
    suse-commit: 1623c19c0334491b0d9f84033524efa2c95a40ed

--- drivers/usb/host/ehci-q.c
+++ drivers/usb/host/ehci-q.c
@@ -206,7 +206,6 @@ ehci_urb_done (struct ehci_hcd *ehci, struct urb *urb, struct pt_regs *regs)
 		COUNT (ehci->stats.unlink);
 		break;
 	}
-	mb();
 	spin_unlock (&urb->lock);
 
 #ifdef EHCI_URB_TRACE
