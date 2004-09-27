4e3e6e8a75059f94682e3ea2d28917167a410fb9
Matthew Wilcox <willy@debian.org>
2004-09-27 02:56:26 -0400

    [PATCH] Add SPI-5 constants to scsi.h
    On Tue, Sep 21, 2004 at 01:04:38PM +0100, Christoph Hellwig wrote:
    > On Tue, Sep 21, 2004 at 12:50:59PM +0100, Matthew Wilcox wrote:
    > >
    > > Add the new constant names from SPI-5 to <scsi/scsi.h>
    
    > What about having a section ala
    >
    > /*
    >  * Old SCSI2 names, don't use in new code
    >  */
    > #define ABORT               ABORT_TASK_SET
    > #define BUS_DEVICE_RESET    TARGET_RESET
    >
    > instead of intermixing them with the rest of the defines?
    
    Sure...
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- include/scsi/scsi.h
+++ include/scsi/scsi.h
@@ -246,24 +246,36 @@ struct scsi_lun {
 #define     EXTENDED_SDTR                   0x01
 #define     EXTENDED_EXTENDED_IDENTIFY      0x02    /* SCSI-I only */
 #define     EXTENDED_WDTR                   0x03
+#define     EXTENDED_PPR                    0x04
+#define     EXTENDED_MODIFY_BIDI_DATA_PTR   0x05
 #define SAVE_POINTERS       0x02
 #define RESTORE_POINTERS    0x03
 #define DISCONNECT          0x04
 #define INITIATOR_ERROR     0x05
-#define ABORT               0x06
+#define ABORT_TASK_SET      0x06
 #define MESSAGE_REJECT      0x07
 #define NOP                 0x08
 #define MSG_PARITY_ERROR    0x09
 #define LINKED_CMD_COMPLETE 0x0a
 #define LINKED_FLG_CMD_COMPLETE 0x0b
-#define BUS_DEVICE_RESET    0x0c
-
+#define TARGET_RESET        0x0c
+#define ABORT_TASK          0x0d
+#define CLEAR_TASK_SET      0x0e
 #define INITIATE_RECOVERY   0x0f            /* SCSI-II only */
 #define RELEASE_RECOVERY    0x10            /* SCSI-II only */
-
+#define CLEAR_ACA           0x16
+#define LOGICAL_UNIT_RESET  0x17
 #define SIMPLE_QUEUE_TAG    0x20
 #define HEAD_OF_QUEUE_TAG   0x21
 #define ORDERED_QUEUE_TAG   0x22
+#define IGNORE_WIDE_RESIDUE 0x23
+#define ACA                 0x24
+#define QAS_REQUEST         0x55
+#define IDENTIFY            0x80
+
+/* Old SCSI2 names, don't use in new code */
+#define BUS_DEVICE_RESET    TARGET_RESET
+#define ABORT               ABORT_TASK_SET
 
 /*
  * Host byte codes
