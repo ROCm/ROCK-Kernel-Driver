a94b1b9bc9c7504807f9484e7033cf4d5db8e3ca
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2004-09-29 00:09:09 -0700

    [PATCH] ppc/ppc64: Fix g5 access to PCI IO cycles
    Looks like we never needed them, since that bug has been there forever,
    I didn't get the right base for the IO cycles on the G5 host bridge
    in the first place (I probably misinterpreted some OF forth code or
    something like that).

--- arch/ppc64/kernel/pmac_pci.c
+++ arch/ppc64/kernel/pmac_pci.c
@@ -419,7 +419,7 @@ static void __init setup_u3_ht(struct pci_controller* hose)
 	 * properties or figuring out the U3 address space decoding logic and
 	 * then read it's configuration register (if any).
 	 */
-	hose->io_base_phys = 0xf4000000 + 0x00400000;
+	hose->io_base_phys = 0xf4000000;
 	hose->io_base_virt = ioremap(hose->io_base_phys, 0x00400000);
 	isa_io_base = pci_io_base = (unsigned long) hose->io_base_virt;
 	hose->io_resource.name = np->full_name;
