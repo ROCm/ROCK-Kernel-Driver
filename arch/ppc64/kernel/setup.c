0616d60931fdfea4265345c2729a12ab6c5a74e3
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2004-09-29 00:09:21 -0700

    [PATCH] ppc64: DART iommu allocation fix
    The current code allocating the DART has a couple of bugs, first
    it's called on all machines including the ones who have no DART
    (oops), and then it tries to access the device-tree using the
    "of_chosen" pointer before it was initialized.
    
    The enclosed patch fixes these.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc64/kernel/setup.c
+++ arch/ppc64/kernel/setup.c
@@ -406,16 +406,6 @@ void __init early_setup(unsigned long dt_ptr)
 
 	DBG("Found, Initializing memory management...\n");
 
-#ifdef CONFIG_U3_DART
-	/*
-	 * On U3, the DART (iommu) must be allocated now since it
-	 * has an impact on htab_initialize (due to the large page it
-	 * occupies having to be broken up so the DART itself is not
-	 * part of the cacheable linar mapping
-	 */
-	alloc_u3_dart_table();
-#endif /* CONFIG_U3_DART */
-
 	/*
 	 * Initialize stab / SLB management
 	 */
--- arch/ppc64/kernel/u3_iommu.c
+++ arch/ppc64/kernel/u3_iommu.c
@@ -47,6 +47,7 @@
 
 #include "pci.h"
 
+extern int iommu_force_on;
 
 /* physical base of DART registers */
 #define DART_BASE        0xf8033000UL
@@ -305,8 +306,7 @@ void __init alloc_u3_dart_table(void)
 	/* Only reserve DART space if machine has more than 2GB of RAM
 	 * or if requested with iommu=on on cmdline.
 	 */
-	if (lmb_end_of_DRAM() <= 0x80000000ull &&
-	    get_property(of_chosen, "linux,iommu-force-on", NULL) == NULL)
+	if (lmb_end_of_DRAM() <= 0x80000000ull && !iommu_force_on)
 		return;
 
 	/* 512 pages (2MB) is max DART tablesize. */
