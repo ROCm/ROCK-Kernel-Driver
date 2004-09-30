067dfdd61a153765d7fc4e412c09a88dbbd49e7f
Jeff Garzik <jgarzik@pobox.com>
2004-09-30 14:11:06 -0400

    [libata sata_uli] add dev_select hook

--- drivers/scsi/sata_uli.c
+++ drivers/scsi/sata_uli.c
@@ -88,6 +88,7 @@ static struct ata_port_operations uli_ops = {
 	.tf_read		= ata_tf_read,
 	.check_status		= ata_check_status,
 	.exec_command		= ata_exec_command,
+	.dev_select		= ata_std_dev_select,
 
 	.phy_reset		= sata_phy_reset,
 
