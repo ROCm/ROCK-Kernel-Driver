72b59f4ae8cb8a65d00e3d6ec1af27e0afefeafe
Jaroslav Kysela <perex@suse.cz>
2004-10-07 15:51:16 +0200

    [ALSA]  Fix the variable types in struct
    RME HDSP driver
    The variable types are declared explicitly like u32 and u64
    to avoid ambiguity.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/hdsp.h
+++ include/sound/hdsp.h
@@ -32,13 +32,13 @@ typedef enum {
 typedef struct _snd_hdsp_peak_rms hdsp_peak_rms_t;
 
 struct _snd_hdsp_peak_rms {
-	unsigned int input_peaks[26];
-	unsigned int playback_peaks[26];
-	unsigned int output_peaks[28];
-	unsigned long long input_rms[26];
-	unsigned long long playback_rms[26];
+	u32 input_peaks[26];
+	u32 playback_peaks[26];
+	u32 output_peaks[28];
+	u64 input_rms[26];
+	u64 playback_rms[26];
 	/* These are only used for H96xx cards */
-	unsigned long long output_rms[26];
+	u64 output_rms[26];
 };
 
 #define SNDRV_HDSP_IOCTL_GET_PEAK_RMS _IOR('H', 0x40, hdsp_peak_rms_t)
