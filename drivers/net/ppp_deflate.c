06a47cc0e389ca93be199083856588f2118033af
Maximilian Attems <janitor@sternwelten.at>
2004-10-18 18:21:02 -0700

    [PATCH] janitor: mark __init/__exit static drivers/net/ppp_deflate
    Signed-off-by: Maximilian Attems <janitor@sternwelten.at>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/net/ppp_deflate.c
+++ drivers/net/ppp_deflate.c
@@ -636,7 +636,7 @@ struct compressor ppp_deflate_draft = {
 	.owner =		THIS_MODULE
 };
 
-int __init deflate_init(void)
+static int __init deflate_init(void)
 {  
         int answer = ppp_register_compressor(&ppp_deflate);
         if (answer == 0)
@@ -646,7 +646,7 @@ int __init deflate_init(void)
         return answer;
 }
      
-void __exit deflate_cleanup(void)
+static void __exit deflate_cleanup(void)
 {
 	ppp_unregister_compressor(&ppp_deflate);
 	ppp_unregister_compressor(&ppp_deflate_draft);
