0c5a1299d6d8b244b7686417a858319f142df5c2
Greg Kroah-Hartman <greg@kroah.com>
2004-10-19 00:48:04 -0700

    PCI: fix up pci_save/restore_state in via-agp due to api change.
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/char/agp/via-agp.c
+++ drivers/char/agp/via-agp.c
@@ -442,7 +442,7 @@ static void __devexit agp_via_remove(struct pci_dev *pdev)
 
 static int agp_via_suspend(struct pci_dev *pdev, u32 state)
 {
-	pci_save_state (pdev, pdev->saved_config_space);
+	pci_save_state (pdev);
 	pci_set_power_state (pdev, 3);
 
 	return 0;
@@ -453,7 +453,7 @@ static int agp_via_resume(struct pci_dev *pdev)
 	struct agp_bridge_data *bridge = pci_get_drvdata(pdev);
 
 	pci_set_power_state (pdev, 0);
-	pci_restore_state(pdev, pdev->saved_config_space);
+	pci_restore_state(pdev);
 
 	if (bridge->driver == &via_agp3_driver)
 		return via_configure_agp3();
