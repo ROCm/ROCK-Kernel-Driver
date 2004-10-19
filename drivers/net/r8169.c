6842c6e1cbd7902fbb29e26929396c9ff2e39225
François Romieu <romieu@fr.zoreil.com>
2004-10-19 03:26:56 -0400

    [PATCH] r8169: always clean Tx desc
    rtl8169_unmap_tx_skb() can not assume that a Tx ring descriptor belongs
    to the host as it can be issued during rtl8169_tx_clear() as a part of
    a recovery process (during Tx timeout for instance).
    
    Simple fix: always clean the relevant descriptor entry.
    
    Acked-by: Francois Romieu <romieu@fr.zoreil.com>
    Signed-off-by: Jon Mason <jdmason@us.ibm.com>

--- drivers/net/r8169.c
+++ drivers/net/r8169.c
@@ -1681,6 +1681,7 @@ static void rtl8169_unmap_tx_skb(struct pci_dev *pdev, struct ring_info *tx_skb,
 	unsigned int len = tx_skb->len;
 
 	pci_unmap_single(pdev, le64_to_cpu(desc->addr), len, PCI_DMA_TODEVICE);
+	desc->opts1 = 0x00;
 	desc->opts2 = 0x00;
 	desc->addr = 0x00;
 	tx_skb->len = 0;
