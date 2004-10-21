646d13d8d585b8be035c6b2ae7f1cd57f5a58d48
Dmitry Torokhov <dtor_core@ameritech.net>
2004-10-21 13:53:52 -0500

    Input: i8042 - get rid of old style power management handler since        APM calls both pm_send and device_suspend.
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/serio/i8042.c
+++ drivers/input/serio/i8042.c
@@ -113,7 +113,6 @@ static unsigned char i8042_initial_ctr;
 static unsigned char i8042_ctr;
 static unsigned char i8042_mux_open;
 static unsigned char i8042_mux_present;
-static struct pm_dev *i8042_pm_dev;
 static struct timer_list i8042_timer;
 static struct platform_device *i8042_platform_device;
 
@@ -826,13 +825,36 @@ void i8042_controller_cleanup(void)
 
 
 /*
+ * We need to reset the 8042 back to original mode on system shutdown,
+ * because otherwise BIOSes will be confused.
+ */
+
+static int i8042_notify_sys(struct notifier_block *this, unsigned long code,
+        		    void *unused)
+{
+        if (code == SYS_DOWN || code == SYS_HALT)
+        	i8042_controller_cleanup();
+        return NOTIFY_DONE;
+}
+
+static struct notifier_block i8042_notifier =
+{
+        i8042_notify_sys,
+        NULL,
+        0
+};
+
+
+/*
  * Here we try to restore the original BIOS settings
  */
 
-static int i8042_controller_suspend(void)
+static int i8042_suspend(struct device *dev, u32 state, u32 level)
 {
-	del_timer_sync(&i8042_timer);
-	i8042_controller_reset();
+	if (level == SUSPEND_DISABLE) {
+		del_timer_sync(&i8042_timer);
+		i8042_controller_reset();
+	}
 
 	return 0;
 }
@@ -842,9 +864,12 @@ static int i8042_controller_suspend(void)
  * Here we try to reset everything back to a state in which suspended
  */
 
-static int i8042_controller_resume(void)
+static int i8042_resume(struct device *dev, u32 level)
 {
 	int i;
+
+	if (level != RESUME_ENABLE)
+		return 0;
 
 	if (i8042_controller_init()) {
 		printk(KERN_ERR "i8042: resume failed\n");
@@ -876,40 +901,7 @@ static int i8042_controller_resume(void)
 	mod_timer(&i8042_timer, jiffies + I8042_POLL_PERIOD);
 
 	return 0;
-}
 
-
-/*
- * We need to reset the 8042 back to original mode on system shutdown,
- * because otherwise BIOSes will be confused.
- */
-
-static int i8042_notify_sys(struct notifier_block *this, unsigned long code,
-        		    void *unused)
-{
-        if (code == SYS_DOWN || code == SYS_HALT)
-        	i8042_controller_cleanup();
-        return NOTIFY_DONE;
-}
-
-static struct notifier_block i8042_notifier =
-{
-        i8042_notify_sys,
-        NULL,
-        0
-};
-
-/*
- * Suspend/resume handlers for the new PM scheme (driver model)
- */
-static int i8042_suspend(struct device *dev, u32 state, u32 level)
-{
-	return level == SUSPEND_DISABLE ? i8042_controller_suspend() : 0;
-}
-
-static int i8042_resume(struct device *dev, u32 level)
-{
-	return level == RESUME_ENABLE ? i8042_controller_resume() : 0;
 }
 
 static void i8042_shutdown(struct device *dev)
@@ -924,22 +916,6 @@ static struct device_driver i8042_driver = {
 	.resume		= i8042_resume,
 	.shutdown	= i8042_shutdown,
 };
-
-/*
- * Suspend/resume handler for the old PM scheme (APM)
- */
-static int i8042_pm_callback(struct pm_dev *dev, pm_request_t request, void *dummy)
-{
-	switch (request) {
-		case PM_SUSPEND:
-			return i8042_controller_suspend();
-
-		case PM_RESUME:
-			return i8042_controller_resume();
-	}
-
-	return 0;
-}
 
 static struct serio * __init i8042_allocate_kbd_port(void)
 {
@@ -1055,8 +1031,6 @@ int __init i8042_init(void)
 
 	mod_timer(&i8042_timer, jiffies + I8042_POLL_PERIOD);
 
-	i8042_pm_dev = pm_register(PM_SYS_DEV, PM_SYS_UNKNOWN, i8042_pm_callback);
-
 	register_reboot_notifier(&i8042_notifier);
 
 	return 0;
@@ -1067,9 +1041,6 @@ void __exit i8042_exit(void)
 	int i;
 
 	unregister_reboot_notifier(&i8042_notifier);
-
-	if (i8042_pm_dev)
-		pm_unregister(i8042_pm_dev);
 
 	i8042_controller_cleanup();
 
