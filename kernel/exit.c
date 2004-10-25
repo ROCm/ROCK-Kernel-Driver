39221a1aec5ddb29d2b7b8622477086b4f45f4d0
Roland McGrath <roland@redhat.com>
2004-10-25 04:18:49 -0700

    [PATCH] session leader tty disassociation fix
    The session leader should disassociate from its controlling terminal and
    send SIGHUP signals only when the whole session leader process dies. 
    Currently, this gets done when any thread in that process dies, which is
    wrong.  This patch fixes it.  
    
    Signed-off-by: Roland McGrath <roland@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -783,6 +783,7 @@ static void exit_notify(struct task_struct *tsk)
 asmlinkage NORET_TYPE void do_exit(long code)
 {
 	struct task_struct *tsk = current;
+	int group_dead;
 
 	profile_task_exit(tsk);
 
@@ -807,7 +808,8 @@ asmlinkage NORET_TYPE void do_exit(long code)
 		ptrace_notify((PTRACE_EVENT_EXIT << 8) | SIGTRAP);
 	}
 
-	if (atomic_dec_and_test(&tsk->signal->live))
+	group_dead = atomic_dec_and_test(&tsk->signal->live);
+	if (group_dead)
 		acct_process(code);
 	__exit_mm(tsk);
 
@@ -818,7 +820,7 @@ asmlinkage NORET_TYPE void do_exit(long code)
 	exit_thread();
 	exit_keys(tsk);
 
-	if (tsk->signal->leader)
+	if (group_dead && tsk->signal->leader)
 		disassociate_ctty(1);
 
 	module_put(tsk->thread_info->exec_domain->module);
