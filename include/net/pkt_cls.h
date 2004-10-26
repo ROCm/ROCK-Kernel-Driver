bb3725344c0f8713240b7a22e156e11133634fd9
Thomas Graf <tgraf@suug.ch>
2004-10-26 01:58:53 -0700

    [PKT_SCHED]: Remove obsolete definitions in pkt_cls.h
    Removes all obsolete definitions in pkt_cls.h now in sch_generic.h
    and includes them via sch_generic.h.
    
    Signed-off-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/pkt_cls.h
+++ include/net/pkt_cls.h
@@ -1,35 +1,10 @@
 #ifndef __NET_PKT_CLS_H
 #define __NET_PKT_CLS_H
 
-
 #include <linux/pkt_cls.h>
-
-struct rtattr;
-struct tcmsg;
+#include <net/sch_generic.h>
 
 /* Basic packet classifier frontend definitions. */
-
-struct tcf_result
-{
-	unsigned long	class;
-	u32		classid;
-};
-
-struct tcf_proto
-{
-	/* Fast access part */
-	struct tcf_proto	*next;
-	void			*root;
-	int			(*classify)(struct sk_buff*, struct tcf_proto*, struct tcf_result *);
-	u32			protocol;
-
-	/* All the rest */
-	u32			prio;
-	u32			classid;
-	struct Qdisc		*q;
-	void			*data;
-	struct tcf_proto_ops	*ops;
-};
 
 struct tcf_walker
 {
@@ -37,29 +12,6 @@ struct tcf_walker
 	int	skip;
 	int	count;
 	int	(*fn)(struct tcf_proto *, unsigned long node, struct tcf_walker *);
-};
-
-struct module;
-
-struct tcf_proto_ops
-{
-	struct tcf_proto_ops	*next;
-	char			kind[IFNAMSIZ];
-
-	int			(*classify)(struct sk_buff*, struct tcf_proto*, struct tcf_result *);
-	int			(*init)(struct tcf_proto*);
-	void			(*destroy)(struct tcf_proto*);
-
-	unsigned long		(*get)(struct tcf_proto*, u32 handle);
-	void			(*put)(struct tcf_proto*, unsigned long);
-	int			(*change)(struct tcf_proto*, unsigned long, u32 handle, struct rtattr **, unsigned long *);
-	int			(*delete)(struct tcf_proto*, unsigned long);
-	void			(*walk)(struct tcf_proto*, struct tcf_walker *arg);
-
-	/* rtnetlink specific */
-	int			(*dump)(struct tcf_proto*, unsigned long, struct sk_buff *skb, struct tcmsg*);
-
-	struct module		*owner;
 };
 
 /* Main classifier routine: scans classifier chain attached
@@ -107,18 +59,8 @@ reclassify:
 	return -1;
 }
 
-static inline void tcf_destroy(struct tcf_proto *tp)
-{
-	tp->ops->destroy(tp);
-	module_put(tp->ops->owner);
-	kfree(tp);
-}
-
 extern int register_tcf_proto_ops(struct tcf_proto_ops *ops);
 extern int unregister_tcf_proto_ops(struct tcf_proto_ops *ops);
 extern int ing_filter(struct sk_buff *skb);
-
-
-
 
 #endif
