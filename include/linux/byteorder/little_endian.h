1fb62196a83398679dfe5dfa9120ad005d032377
Matthew Wilcox <matthew@wil.cx>
2004-10-31 05:50:14 -0800

    [PATCH] Use optimised byteswap again
    Looks like Al's patch to sparsify the byteorder code prevented use of the
    assembler-optimised code.  Not that I can blame him -- three underscores
    are C-coded and two underscores are assembly-optimised.

--- include/linux/byteorder/little_endian.h
+++ include/linux/byteorder/little_endian.h
@@ -33,12 +33,12 @@
 #define __le32_to_cpu(x) ((__force __u32)(__le32)(x))
 #define __cpu_to_le16(x) ((__force __le16)(__u16)(x))
 #define __le16_to_cpu(x) ((__force __u16)(__le16)(x))
-#define __cpu_to_be64(x) ((__force __be64)___swab64((x)))
-#define __be64_to_cpu(x) ___swab64((__force __u64)(__be64)(x))
-#define __cpu_to_be32(x) ((__force __be32)___swab32((x)))
-#define __be32_to_cpu(x) ___swab32((__force __u32)(__be32)(x))
-#define __cpu_to_be16(x) ((__force __be16)___swab16((x)))
-#define __be16_to_cpu(x) ___swab16((__force __u16)(__be16)(x))
+#define __cpu_to_be64(x) ((__force __be64)__swab64((x)))
+#define __be64_to_cpu(x) __swab64((__force __u64)(__be64)(x))
+#define __cpu_to_be32(x) ((__force __be32)__swab32((x)))
+#define __be32_to_cpu(x) __swab32((__force __u32)(__be32)(x))
+#define __cpu_to_be16(x) ((__force __be16)__swab16((x)))
+#define __be16_to_cpu(x) __swab16((__force __u16)(__be16)(x))
 
 static inline __le64 __cpu_to_le64p(const __u64 *p)
 {
