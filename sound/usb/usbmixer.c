dcd3942f498a6e4338109da123ee79be6b5a4ced
Jaroslav Kysela <perex@suse.cz>
2004-11-11 13:50:04 +0100

    [ALSA] read bmControls array in correct order
    USB generic driver
    The driver used some code from audio.c that reads the bmControl array
    backwards; this would not work here as we get a pointer to the
    beginning of the array.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>

--- sound/usb/usbmixer.c
+++ sound/usb/usbmixer.c
@@ -374,7 +374,7 @@ static int parse_audio_unit(mixer_build_t *state, int unitid);
 static int check_matrix_bitmap(unsigned char *bmap, int ich, int och, int num_outs)
 {
 	int idx = ich * num_outs + och;
-	return bmap[-(idx >> 3)] & (0x80 >> (idx & 7));
+	return bmap[idx >> 3] & (0x80 >> (idx & 7));
 }
 
 
