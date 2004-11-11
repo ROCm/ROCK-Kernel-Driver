435883d2c7e37c41826910f42d2d3b727280f0a0
Greg Kroah-Hartman <greg@kroah.com>
2004-11-10 23:40:24 -0800

    PCI Hotplug: fix up remaining MODULE_PARAM usage in pci hotplug drivers
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/pci/hotplug/fakephp.c
+++ drivers/pci/hotplug/fakephp.c
@@ -227,6 +227,6 @@ module_exit(dummyphp_exit);
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
-MODULE_PARM(debug, "i");
+module_param(debug, bool, S_IRUGO | S_IWUSR);
 MODULE_PARM_DESC(debug, "Debugging mode enabled or not");
 
