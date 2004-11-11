8d62a3ffeaebd0b3b234153d780251f6c0d815ae
Kumar Gala <galak@somerset.sps.mot.com>
2004-11-10 21:31:03 -0800

    [PATCH] ppc32: updated reporting of CPU rev & freq for e500 CPUs
    Fixes up the reporting of the e500 core revision.  Additionally, it changes
    the reporting of CPU frequency to match what other PPC systems do.
    
    Signed-off-by: Kumar Gala <kumar.gala@freescale.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc/platforms/85xx/mpc85xx_ads_common.c
+++ arch/ppc/platforms/85xx/mpc85xx_ads_common.c
@@ -142,8 +142,7 @@ mpc85xx_ads_show_cpuinfo(struct seq_file *m)
 		seq_printf(m, "Machine\t\t: unknown\n");
 		break;
 	}
-	seq_printf(m, "bus freq\t: %u.%.6u MHz\n", freq / 1000000,
-		   freq % 1000000);
+	seq_printf(m, "clock\t\t: %dMHz\n", freq / 1000000);
 	seq_printf(m, "PVR\t\t: 0x%x\n", pvid);
 	seq_printf(m, "SVR\t\t: 0x%x\n", svid);
 
