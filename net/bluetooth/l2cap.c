09f68b67f9dfccc1db8fcdc3275dc474fecdb40b
Marcel Holtmann <marcel@holtmann.org>
2004-11-17 12:30:12 +0100

    [Bluetooth] Correct locking for zero SCID responses
    The l2cap_get_chan_by_ident() function must lock the socket with the
    bh_lock_sock() function on success.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/l2cap.c
+++ net/bluetooth/l2cap.c
@@ -964,6 +964,7 @@ static inline struct sock *l2cap_get_chan_by_ident(struct l2cap_chan_list *l, u8
 	struct sock *s;
 	read_lock(&l->lock);
 	s = __l2cap_get_chan_by_ident(l, ident);
+	if (s) bh_lock_sock(s);
 	read_unlock(&l->lock);
 	return s;
 }
