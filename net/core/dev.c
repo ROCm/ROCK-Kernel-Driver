051adc81602dbc3aa84b3ba9622c5dca2d8e144f
Christoph Hellwig <hch@lst.de>
2004-11-17 22:13:50 -0800

    [NET]: Use local_softirq_pending instead of softirq_pending in netif_rx_ni
    Some architectures can optimize local_softirq_pending much better than
    softirq_pending(smp_processor_id()), and for all others the former is
    just a macro expanding to the later.
    
    Also this is the last use of softirq_pending() in common code, once this
    is in we can soon kill the notation of beeing able to query other cpus
    softirq pending count.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1493,7 +1493,7 @@ int netif_rx_ni(struct sk_buff *skb)
 
 	preempt_disable();
 	err = netif_rx(skb);
-	if (softirq_pending(smp_processor_id()))
+	if (local_softirq_pending())
 		do_softirq();
 	preempt_enable();
 
