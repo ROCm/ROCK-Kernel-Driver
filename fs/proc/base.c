4e7ef413ce126ec63ba30e802f175ffd941cc391
Prasanna Meda <pmeda@akamai.com>
2004-11-21 16:43:25 -0800

    [PATCH] /proc/cmdline missing mmput
    Fix the mmput bug introduced while fixing cmdline race.
    
    Signed-off-by: Prasanna Meda <pmeda@akamai.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -343,7 +343,7 @@ static int proc_pid_cmdline(struct task_struct *task, char * buffer)
 	if (!mm)
 		goto out;
 	if (!mm->arg_end)
-		goto out;	/* Shh! No looking before we're done */
+		goto out_mm;	/* Shh! No looking before we're done */
 
  	len = mm->arg_end - mm->arg_start;
  
@@ -366,8 +366,8 @@ static int proc_pid_cmdline(struct task_struct *task, char * buffer)
 			res = strnlen(buffer, res);
 		}
 	}
+out_mm:
 	mmput(mm);
-
 out:
 	return res;
 }
