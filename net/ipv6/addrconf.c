3bfb790cfebb74ef758c16b636587e201177bff4
Brian Haley <brian.haley@hp.com>
2004-11-24 21:34:53 +0900

    [IPV6] improve ipv6_ifa_notify() readability.

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -3017,10 +3017,8 @@ static void __ipv6_ifa_notify(int event, struct inet6_ifaddr *ifp)
 static void ipv6_ifa_notify(int event, struct inet6_ifaddr *ifp)
 {
 	read_lock_bh(&addrconf_lock);
-	if (ifp->idev->dead)
-		goto out;
-	__ipv6_ifa_notify(event, ifp);
-out:
+	if (likely(ifp->idev->dead == 0))
+		__ipv6_ifa_notify(event, ifp);
 	read_unlock_bh(&addrconf_lock);
 }
 
