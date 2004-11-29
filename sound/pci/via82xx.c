ccacb9be0c86893a6f7a1958b8a4bb06896a1216
Jaroslav Kysela <perex@suse.cz>
2004-11-29 11:12:32 +0100

    [ALSA] via82xx: Enable DXS on ABIT KV8 Pro
    VIA82xx driver
    From Marko Kreen <marko@l-t.ee>:
    
    Enable DXS for via audio chip on ABIT KV8 Pro.
    
    Works for me.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/via82xx.c
+++ sound/pci/via82xx.c
@@ -2108,6 +2108,7 @@ static int __devinit check_dxs_list(struct pci_dev *pci)
 		{ .vendor = 0x1297, .device = 0xc160, .action = VIA_DXS_ENABLE }, /* Shuttle SK41G */
 		{ .vendor = 0x1458, .device = 0xa002, .action = VIA_DXS_ENABLE }, /* Gigabyte GA-7VAXP */
 		{ .vendor = 0x147b, .device = 0x1401, .action = VIA_DXS_ENABLE }, /* ABIT KD7(-RAID) */
+		{ .vendor = 0x147b, .device = 0x1413, .action = VIA_DXS_ENABLE }, /* ABIT KV8 Pro */
 		{ .vendor = 0x14ff, .device = 0x0403, .action = VIA_DXS_ENABLE }, /* Twinhead mobo */
 		{ .vendor = 0x1462, .device = 0x3800, .action = VIA_DXS_ENABLE }, /* MSI KT266 */
 		{ .vendor = 0x1462, .device = 0x7120, .action = VIA_DXS_ENABLE }, /* MSI KT4V */
