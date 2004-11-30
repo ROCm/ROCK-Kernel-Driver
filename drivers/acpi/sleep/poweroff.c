7e225d7f4f10e8b9ba22c1ca5c78e44a9b4b9509
Len Brown <len.brown@intel.com>
2004-11-30 15:05:10 -0500

    [ACPI] fix reboot on poweroff regression due to enabled wakeup GPEs http://bugzilla.kernel.org/show_bug.cgi?id=3669
    Signed-off-by: David Shaohua Li <shaohua.li@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/sleep/poweroff.c
+++ drivers/acpi/sleep/poweroff.c
@@ -9,6 +9,7 @@
 #include <linux/init.h>
 #include <acpi/acpi_bus.h>
 #include <linux/sched.h>
+#include "sleep.h"
 
 static void
 acpi_power_off (void)
@@ -16,6 +17,7 @@ acpi_power_off (void)
 	printk("%s called\n",__FUNCTION__);
 	/* Some SMP machines only can poweroff in boot CPU */
 	set_cpus_allowed(current, cpumask_of_cpu(0));
+	acpi_wakeup_gpe_poweroff_prepare();
 	acpi_enter_sleep_state_prep(ACPI_STATE_S5);
 	ACPI_DISABLE_IRQS();
 	acpi_enter_sleep_state(ACPI_STATE_S5);
