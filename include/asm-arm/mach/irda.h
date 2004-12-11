a6eb52bcd4408e4559cc1f4c477200bf516151b1
Russell King <rmk@flint.arm.linux.org.uk>
2004-12-11 17:07:00 +0000

    [ARM] Fix compiler warning for set_speed irda method.

--- include/asm-arm/mach/irda.h
+++ include/asm-arm/mach/irda.h
@@ -14,7 +14,7 @@ struct irda_platform_data {
 	int (*startup)(struct device *);
 	void (*shutdown)(struct device *);
 	int (*set_power)(struct device *, unsigned int state);
-	void (*set_speed)(struct device *, int speed);
+	void (*set_speed)(struct device *, unsigned int speed);
 };
 
 #endif
