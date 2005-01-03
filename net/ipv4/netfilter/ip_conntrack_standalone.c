fbff1be6c2ba746649d5189522c30270bc086a28
Olaf Kirch <okir@suse.de>
2005-01-03 12:42:30 +0000

    - prevent oops when unloading netfilter
    suse-commit: 35cd25ef04c81f9166bc3c430de276c55b5d42a5

--- net/ipv4/netfilter/ip_conntrack_standalone.c
+++ net/ipv4/netfilter/ip_conntrack_standalone.c
@@ -820,7 +820,7 @@ static int init_or_cleanup(int init)
 	nf_unregister_hook(&ip_conntrack_defrag_ops);
  cleanup_proc_stat:
 #ifdef CONFIG_PROC_FS
-	proc_net_remove("ip_conntrack_stat");
+	remove_proc_entry("ip_conntrack", proc_net_stat);
 cleanup_proc_exp:
 	proc_net_remove("ip_conntrack_expect");
  cleanup_proc:
