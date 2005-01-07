cf12055b92d67b2f0f47a1829b37a3378f2cce07
Dave Jiang <dave.jiang@com.rmk.(none)>
2005-01-07 23:36:26 +0000

    [ARM PATCH] 2362/1: cleanup of PCI defines for IOP321 platforms
    Patch from Dave Jiang
    
    Signed-off-by: Dave Jiang
    
    Major cleanup of the 321 PCI defines to make them more coherent. Unified some groups that were per platform to common proc specific. Removed some magic numbers.
    Signed-off-by: Russell King

--- include/asm-arm/arch-iop3xx/iq31244.h
+++ include/asm-arm/arch-iop3xx/iq31244.h
@@ -7,8 +7,6 @@
 #ifndef _IQ31244_H_
 #define _IQ31244_H_
 
-#define IQ31244_RAMBASE		0xa0000000
-
 #define	IQ31244_FLASHBASE	0xf0000000	/* Flash */
 #define	IQ31244_FLASHSIZE	0x00800000
 #define	IQ31244_FLASHWIDTH	2
@@ -18,16 +16,6 @@
 #define IQ31244_7SEG_0		0xfe850000	/* 7-Segment LSB (WO) */
 #define IQ31244_ROTARY_SW	0xfe8d0000	/* Rotary Switch */
 #define IQ31244_BATT_STAT	0xfe8f0000	/* Battery Status */
-
-/*
- * IQ31244 PCI I/O and Mem space regions
- */
-#define IQ31244_PCI_IO_BASE	0x90000000
-#define IQ31244_PCI_IO_SIZE	0x00010000
-#define IQ31244_PCI_MEM_BASE	0x80000000
-//#define IQ31244_PCI_MEM_SIZE	0x04000000
-#define IQ31244_PCI_MEM_SIZE	0x08000000
-#define	IQ31244_PCI_IO_OFFSET	0x6e000000
 
 #ifndef __ASSEMBLY__
 extern void iq31244_map_io(void);
