b3aa40315f5f2eb84e9f53f87974e08c1343e416
Mark M. Hoffman <mhoffman@lightlink.com>
2005-01-06 19:14:41 -0800

    [PATCH] I2C: add new sensors driver: SMSC LPC47B397-NC
    This patch (3rd time's a charm) adds support for the SMSC LPC47B397-NC
    sensor chip.  It was sponsored by In-Store Broadcasting Network.
    
    Signed-off-by: Craig Kelly (In-Store Broadcasting Network)
    Signed-off-by: Glenn Ball (Utilitek Systems, Inc.)
    Signed-off-by: Mark M. Hoffman <mhoffman@lightlink.com>

--- include/linux/i2c-id.h
+++ include/linux/i2c-id.h
@@ -167,6 +167,7 @@
 #define I2C_DRIVERID_ASB100 1043
 #define I2C_DRIVERID_FSCHER 1046
 #define I2C_DRIVERID_W83L785TS 1047
+#define I2C_DRIVERID_SMSC47B397 1050
 
 /*
  * ---- Adapter types ----------------------------------------------------
