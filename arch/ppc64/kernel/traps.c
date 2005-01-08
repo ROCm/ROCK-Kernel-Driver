c6744c49fa35053db691c8440ef092107da8441e
David Gibson <david@gibson.dropbear.id.au>
2005-01-07 21:38:32 -0800

    [PATCH] ppc64: add performance monitor register information to processor.h
    Most special purpose registers on the ppc64 have both the SPR number, and
    the various fields within the register defined in asm-ppc64/processor.h.
    So far that's not true for the performance counter control registers, MMCR0
    and MMCRA.  They have the SPR numbers defined, but the internal fields are
    defined in the oprofile code and (just a few) in traps.c where they're
    actually used.
    
    This patch moves all the MMCR0 and MMCRA definitions, plus the MSR
    performance monitor bit, MSR_PMM, into processor.h.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc64/kernel/traps.c
+++ arch/ppc64/kernel/traps.c
@@ -549,9 +549,6 @@ void AltivecUnavailableException(struct pt_regs *regs)
 }
 
 /* Ensure exceptions are disabled */
-#define MMCR0_PMXE      (1UL << (31 - 5))
-#define MMCR0_PMAO      (1UL << (31 - 24))
-
 static void dummy_perf(struct pt_regs *regs)
 {
 	unsigned int mmcr0 = mfspr(SPRN_MMCR0);
