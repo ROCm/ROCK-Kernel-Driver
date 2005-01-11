a6ab0c16edd9bf13db04afc7cd88cb137fe770aa
Martin Schwidefsky <schwidefsky@de.ibm.com>
2005-01-11 01:40:53 -0800

    [PATCH] cputime: microsecond based cputime for s390
    This patch adds the architecture magic to replace the jiffies based cputime
    with microsecond based cputime and it adds code to calculate involuntary
    wait time.  With this patch the numbers reported by top and ps when running
    on LPAR or z/VM are finally not junk anymore.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/s390/kernel/binfmt_elf32.c
+++ arch/s390/kernel/binfmt_elf32.c
@@ -202,9 +202,8 @@ MODULE_AUTHOR("Gerhard Tonn <ton@de.ibm.com>");
 static __inline__ void
 cputime_to_compat_timeval(const cputime_t cputime, struct compat_timeval *value)
 {
-	unsigned long jiffies = cputime_to_jiffies(cputime);
-	value->tv_usec = (jiffies % HZ) * (1000000L / HZ);
-	value->tv_sec = jiffies / HZ;
+	value->tv_usec = cputime % 1000000;
+	value->tv_sec = cputime / 1000000;
 }
 
 #include "../../../fs/binfmt_elf.c"
