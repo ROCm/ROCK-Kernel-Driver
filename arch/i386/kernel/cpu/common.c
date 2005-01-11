b9662b21dd274ff9c9d1ddbc85b90f7cebf38d03
Andi Kleen <ak@suse.de>
2005-01-11 01:48:01 -0800

    [PATCH] i386: AMD dual core support for i386
    AMD dual core support for i386
    
    Run HT initialization on AMD dual core CPUs on i386.  They fake being HT CPUs.
     This patch makes the HT detection run on AMD CPUs too.  I moved the HT
    detection code into a common file from intel.c for that.
    
    It would be actually better to run HT detection always on all CPUs but this
    would need a second callback afterwards to AMD code, which I avoided for now.
    
    It adds a cpuinfo->x86_num_cores field.
    
    This sets up the phys_proc_id[] array.  This overloads this array with HT, but
    when smp_num_siblings is 1
    
    It is currently only used for /proc/cpuinfo printing.  The reason we want to
    behave this like SMT is that there are some license managers in user space
    that license according to number of physical CPUs, and when they handle HT
    they should handle CMP with this hack too.  Another reason we need this is
    that the powernow k8 driver needs this information to properly manage dual
    core CPUs.  
    
    When there are ever dual core HT CPUs this will need small changes in
    smpboot.c.  I didn't do this for now to keep the patch simple.
    
    Then we set smp_num_siblings to 1 on these systems again to prevent the
    scheduler from setting up HT scheduling (which is not a very good match for
    dual core). 
    
    This is a port of the CMP support code from x86-64 (minus the NUMA bits)
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/kernel/cpu/common.c
+++ arch/i386/kernel/cpu/common.c
@@ -10,6 +10,11 @@
 #include <asm/msr.h>
 #include <asm/io.h>
 #include <asm/mmu_context.h>
+#ifdef CONFIG_X86_LOCAL_APIC
+#include <asm/mpspec.h>
+#include <asm/apic.h>
+#include <mach_apic.h>
+#endif
 
 #include "cpu.h"
 
@@ -323,6 +328,7 @@ void __init identify_cpu(struct cpuinfo_x86 *c)
 	c->x86_model = c->x86_mask = 0;	/* So far unknown... */
 	c->x86_vendor_id[0] = '\0'; /* Unset */
 	c->x86_model_id[0] = '\0';  /* Unset */
+	c->x86_num_cores = 1;
 	memset(&c->x86_capability, 0, sizeof c->x86_capability);
 
 	if (!have_cpuid_p()) {
@@ -429,6 +435,48 @@ void __init dodgy_tsc(void)
 	if (( boot_cpu_data.x86_vendor == X86_VENDOR_CYRIX ) ||
 	    ( boot_cpu_data.x86_vendor == X86_VENDOR_NSC   ))
 		cpu_devs[X86_VENDOR_CYRIX]->c_init(&boot_cpu_data);
+}
+
+void __init detect_ht(struct cpuinfo_x86 *c)
+{
+	u32 	eax, ebx, ecx, edx;
+	int 	index_lsb, index_msb, tmp;
+	int 	cpu = smp_processor_id();
+
+	if (!cpu_has(c, X86_FEATURE_HT))
+		return;
+
+	cpuid(1, &eax, &ebx, &ecx, &edx);
+	smp_num_siblings = (ebx & 0xff0000) >> 16;
+
+	if (smp_num_siblings == 1) {
+		printk(KERN_INFO  "CPU: Hyper-Threading is disabled\n");
+	} else if (smp_num_siblings > 1 ) {
+		index_lsb = 0;
+		index_msb = 31;
+
+		if (smp_num_siblings > NR_CPUS) {
+			printk(KERN_WARNING "CPU: Unsupported number of the siblings %d", smp_num_siblings);
+			smp_num_siblings = 1;
+			return;
+		}
+		tmp = smp_num_siblings;
+		while ((tmp & 1) == 0) {
+			tmp >>=1 ;
+			index_lsb++;
+		}
+		tmp = smp_num_siblings;
+		while ((tmp & 0x80000000 ) == 0) {
+			tmp <<=1 ;
+			index_msb--;
+		}
+		if (index_lsb != index_msb )
+			index_msb++;
+		phys_proc_id[cpu] = phys_pkg_id((ebx >> 24) & 0xFF, index_msb);
+
+		printk(KERN_INFO  "CPU: Physical Processor ID: %d\n",
+		       phys_proc_id[cpu]);
+	}
 }
 
 void __init print_cpu_info(struct cpuinfo_x86 *c)
