df69f7acb11b7bd78fd5ec8bcb216e0d70728e03
Jeff Dike <jdike@addtoit.com>
2005-01-11 01:53:06 -0800

    [PATCH] UML: x86-64 core support
    This adds the new files from the x86_64 port which just drop in and don't
    require any work anywhere else.
    
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-um/vm-flags-x86_64.h
+++ include/asm-um/vm-flags-x86_64.h
@@ -17,11 +17,17 @@ extern unsigned long vm_stack_flags, vm_stack_flags32;
 extern unsigned long vm_data_default_flags, vm_data_default_flags32;
 extern unsigned long vm_force_exec32;
 
+#ifdef TIF_IA32
 #define VM_DATA_DEFAULT_FLAGS \
 	(test_thread_flag(TIF_IA32) ? vm_data_default_flags32 : \
 	  vm_data_default_flags)
 
 #define VM_STACK_DEFAULT_FLAGS \
 	(test_thread_flag(TIF_IA32) ? vm_stack_flags32 : vm_stack_flags)
+#endif
+
+#define VM_DATA_DEFAULT_FLAGS vm_data_default_flags
+
+#define VM_STACK_DEFAULT_FLAGS vm_stack_flags
 
 #endif
