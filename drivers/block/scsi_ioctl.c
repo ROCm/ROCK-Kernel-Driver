b9c68ab531efb01b86e72fd50287bf5b69e51e55
Jens Axboe <axboe@suse.de>
2005-01-12 18:15:18 +0000

    - patches.fixes/sg-ioctl-unsigned: sg_scsi_ioctl() sign issue (#49782)
    suse-commit: 66e9b2670c75733f3e0c05d597d5247e8b696765

--- drivers/block/scsi_ioctl.c
+++ drivers/block/scsi_ioctl.c
@@ -339,7 +339,8 @@ static int sg_scsi_ioctl(struct file *file, request_queue_t *q,
 			 struct gendisk *bd_disk, Scsi_Ioctl_Command __user *sic)
 {
 	struct request *rq;
-	int err, in_len, out_len, bytes, opcode, cmdlen;
+	unsigned int bytes, opcode, cmdlen, in_len, out_len;
+	int err;
 	char *buffer = NULL, sense[SCSI_SENSE_BUFFERSIZE];
 
 	/*
