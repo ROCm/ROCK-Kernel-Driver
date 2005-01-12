924382dd3248b71194ee7cb3529a270d07189614
Jaroslav Kysela <perex@suse.cz>
2005-01-12 13:19:00 +0100

    [ALSA] Adapt SPDIF Input selection for Realtek ALC658
    AC97 Codec Core
    This fixes the SPDIF Input selection for ALC658 as Realtek has
    changed the meaning betweenALC655 and ALC658.
    
    Signed-off-by: Stefan Macher <Stefan.Macher@web.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ac97/ac97_patch.c
+++ sound/pci/ac97/ac97_patch.c
@@ -1605,7 +1605,10 @@ int patch_alc655(ac97_t * ac97)
 
 	/* adjust default values */
 	val = snd_ac97_read(ac97, 0x7a); /* misc control */
-	val |= (1 << 1); /* spdif input pin */
+	if (ac97->id == 0x414c4780) /* ALC658 */
+		val &= ~(1 << 1); /* Pin 47 is spdif input pin */
+	else /* ALC655 */
+		val |= (1 << 1); /* Pin 47 is spdif input pin */
 	val &= ~(1 << 12); /* vref enable */
 	snd_ac97_write_cache(ac97, 0x7a, val);
 	/* set default: spdif-in enabled,
