80a47504df19efd401b1fb9f9e31f898efa45ac4
Bartlomiej Zolnierkiewicz <bzolnier@trik.(none)>
2005-01-15 04:06:28 +0100

    [ide] ide_dump_atapi_status() printk readability fix
    From: Gunther Mayer <gunther.mayer@gmx.net>
    
    this improves logic and readability:
    - remove blank from: AbortedCommand (as other flags)
    - add blank and {} to error= line
    - clean up: remove 2 lines and extra printk
    
    before:
      hdd: status error: status=0x7f { DriveReady DeviceFault SeekComplete 
    DataRequest CorrectedError Index Error }
      hdd: status error: error=0x7fIllegalLengthIndication EndOfMedia 
    Aborted Command MediaChangeRequested LastFailedSense 0x07
    
    after:
      hdd: status error: status=0x7f { DriveReady DeviceFault SeekComplete 
    DataRequest CorrectedError Index Error }
      hdd: status error: error=0x7f { IllegalLengthIndication EndOfMedia 
    AbortedCommand MediaChangeRequested LastFailedSense=0x07 }
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-lib.c
+++ drivers/ide/ide-lib.c
@@ -571,8 +571,7 @@ static u8 ide_dump_atapi_status(ide_drive_t *drive, const char *msg, u8 stat)
 
 	status.all = stat;
 	local_irq_set(flags);
-	printk("%s: %s: status=0x%02x", drive->name, msg, stat);
-	printk(" { ");
+	printk("%s: %s: status=0x%02x { ", drive->name, msg, stat);
 	if (status.b.bsy)
 		printk("Busy ");
 	else {
@@ -584,18 +583,17 @@ static u8 ide_dump_atapi_status(ide_drive_t *drive, const char *msg, u8 stat)
 		if (status.b.idx)	printk("Index ");
 		if (status.b.check)	printk("Error ");
 	}
-	printk("}");
-	printk("\n");
+	printk("}\n");
 	if ((status.all & (status.b.bsy|status.b.check)) == status.b.check) {
 		error.all = HWIF(drive)->INB(IDE_ERROR_REG);
-		printk("%s: %s: error=0x%02x", drive->name, msg, error.all);
+		printk("%s: %s: error=0x%02x { ", drive->name, msg, error.all);
 		if (error.b.ili)	printk("IllegalLengthIndication ");
 		if (error.b.eom)	printk("EndOfMedia ");
-		if (error.b.abrt)	printk("Aborted Command ");
+		if (error.b.abrt)	printk("AbortedCommand ");
 		if (error.b.mcr)	printk("MediaChangeRequested ");
-		if (error.b.sense_key)	printk("LastFailedSense 0x%02x ",
+		if (error.b.sense_key)	printk("LastFailedSense=0x%02x ",
 						error.b.sense_key);
-		printk("\n");
+		printk("}\n");
 	}
 	ide_dump_opcode(drive);
 	local_irq_restore(flags);
