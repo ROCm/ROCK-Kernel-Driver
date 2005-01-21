ac6895a3922882ea2f8c0fed89cd5e064bece5e0
Chris L Mason <mason@suse.de>
2005-01-21 17:17:44 +0000

    - patches.suse/legacy-remap_page_range: add back remap_page_range   until all modules are converted.
    suse-commit: 1d3d97e3253e90a7570a372a171723c4fd84ea96

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -812,9 +812,15 @@ extern struct page * vmalloc_to_page(void *addr);
 extern unsigned long vmalloc_to_pfn(void *addr);
 extern struct page * follow_page(struct mm_struct *mm, unsigned long address,
 		int write);
-extern int check_user_page_readable(struct mm_struct *mm, unsigned long address);
 int remap_pfn_range(struct vm_area_struct *, unsigned long,
 		unsigned long, unsigned long, pgprot_t);
+
+static inline __deprecated /* since 25 Sept 2004 -- wli */
+int remap_page_range(struct vm_area_struct *vma, unsigned long uvaddr,
+			unsigned long paddr, unsigned long size, pgprot_t prot)
+{
+	return remap_pfn_range(vma, uvaddr, paddr >> PAGE_SHIFT, size, prot);
+}
 
 #ifdef CONFIG_PROC_FS
 void __vm_stat_account(struct mm_struct *, unsigned long, struct file *, long);
