a0764fb0c36a44059ec1ba719aa4c10f2890f6fe
Thomas Gleixner <tglx@linutronix.de>
2005-01-20 16:20:17 -0800

    [PATCH] Lock initializer cleanup: Firewire
    Use the new lock initializers DEFINE_SPIN_LOCK and DEFINE_RW_LOCK
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/ieee1394/dv1394.c
+++ drivers/ieee1394/dv1394.c
@@ -177,7 +177,7 @@ static long dv1394_compat_ioctl(struct file *file, unsigned int cmd,
 
 /* list of all video_cards */
 static LIST_HEAD(dv1394_cards);
-static spinlock_t dv1394_cards_lock = SPIN_LOCK_UNLOCKED;
+static DEFINE_SPINLOCK(dv1394_cards_lock);
 
 /* translate from a struct file* to the corresponding struct video_card* */
 
