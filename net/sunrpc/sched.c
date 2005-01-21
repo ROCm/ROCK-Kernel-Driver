f9c456032fb5b9cd0dd631f7b58d977fcd9551a5
Olaf Kirch <okir@suse.de>
2005-01-21 10:12:37 +0000

    - removed patches.fixes/sunrpc-sched-deadlock
    suse-commit: c8a1dfad22aafad15fdfe4149607004c01c38758

--- net/sunrpc/sched.c
+++ net/sunrpc/sched.c
@@ -947,18 +947,14 @@ void rpc_killall_tasks(struct rpc_clnt *clnt)
 	/*
 	 * Spin lock all_tasks to prevent changes...
 	 */
-again:
 	spin_lock(&rpc_sched_lock);
 	alltask_for_each(rovr, le, &all_tasks) {
 		if (! RPC_IS_ACTIVATED(rovr))
 			continue;
-		if (!(rovr->tk_flags & RPC_TASK_KILLED) && 
-		    (!clnt || rovr->tk_client == clnt)) {
+		if (!clnt || rovr->tk_client == clnt) {
 			rovr->tk_flags |= RPC_TASK_KILLED;
 			rpc_exit(rovr, -EIO);
-			spin_unlock(&rpc_sched_lock);
 			rpc_wake_up_task(rovr);
-			goto again;
 		}
 	}
 	spin_unlock(&rpc_sched_lock);
