6c25c2655a163004a4b9f52b27cb433ffc5b76ab
David S. Miller <davem@nuts.davemloft.net>
2005-02-07 23:59:00 -0800

    [TG3]: Update driver version and reldate.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tg3.c
+++ drivers/net/tg3.c
@@ -60,8 +60,8 @@
 
 #define DRV_MODULE_NAME		"tg3"
 #define PFX DRV_MODULE_NAME	": "
-#define DRV_MODULE_VERSION	"3.20"
-#define DRV_MODULE_RELDATE	"February 2, 2005"
+#define DRV_MODULE_VERSION	"3.21"
+#define DRV_MODULE_RELDATE	"February 8, 2005"
 
 #define TG3_DEF_MAC_MODE	0
 #define TG3_DEF_RX_MODE		0
