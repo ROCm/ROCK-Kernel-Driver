39b56d902bf35241e7cba6cc30b828ed937175ad
Pavel Machek <pavel@ucw.cz>
2005-02-09 22:40:30 -0800

    [PATCH] binfmt_elf: clearing bss may fail
    So we discover that Borland's Kylix application builder emits weird elf
    files which describe a non-writeable bss segment.
    
    So remove the clear_user() check at the place where we zero out the bss.  I
    don't _think_ there are any security implications here (plus we've never
    checked that clear_user() return value, so whoops if it is a problem).
    
    Signed-off-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -803,11 +803,13 @@ static int load_elf_binary(struct linux_binprm * bprm, struct pt_regs * regs)
 				nbyte = ELF_MIN_ALIGN - nbyte;
 				if (nbyte > elf_brk - elf_bss)
 					nbyte = elf_brk - elf_bss;
-				if (clear_user((void __user *) elf_bss + load_bias, nbyte)) {
-					retval = -EFAULT;
-					send_sig(SIGKILL, current, 0);
-					goto out_free_dentry;
-				}
+				/*
+				 * This bss-zeroing can fail if the ELF file
+				 * specifies odd protections.  So we don't check
+				 * the return value
+				 */
+				(void)clear_user((void __user *)elf_bss +
+							load_bias, nbyte);
 			}
 		}
 
