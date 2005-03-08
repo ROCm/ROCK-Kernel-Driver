c4d2a95a6072e553ddd92d347d9f55a7582999b6
Olaf Kirch <okir@suse.de>
2005-03-08 09:29:56 +0000

    - patches.fixes/drm-setversion-missing-memset: Add missing memset   to drm_setversion (65926). (from eich@suse.de)
    suse-commit: a1d35ad7968bb729d21640a4417079f554a1de60

--- drivers/char/drm/drm_ioctl.c
+++ drivers/char/drm/drm_ioctl.c
@@ -326,6 +326,14 @@ int drm_setversion(DRM_IOCTL_ARGS)
 
 	DRM_COPY_FROM_USER_IOCTL(sv, argp, sizeof(sv));
 
+	/*
+	 * version.name etc need to be initialized to zero.
+	 * If we don't, driver->version() will poke random strings to
+	 * random locations in user space, causing X server segfaults
+	 * that are interesting to debug.   --eich
+	 */
+	memset(&version, 0, sizeof(version));
+
 	dev->driver->version(&version);
 	retv.drm_di_major = DRM_IF_MAJOR;
 	retv.drm_di_minor = DRM_IF_MINOR;
