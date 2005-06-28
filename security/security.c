45e4beb95e63a95d415b3194357a33aa6e04a369
Kurt Garloff <garloff@suse.de>
2005-06-28 21:19:08 +0000

    Commit missing file to CVS.
    suse-commit: 647b2caea0219fa7eb9531dede4c678b20fd6a4a

--- security/security.c
+++ security/security.c
@@ -26,6 +26,8 @@ extern void security_fixup_ops (struct security_operations *ops);
 extern struct security_operations capability_security_ops;
 
 struct security_operations *security_ops;	/* Initialized to NULL */
+int security_enabled;				/* ditto */
+EXPORT_SYMBOL(security_enabled);
 
 static inline int verify(struct security_operations *ops)
 {
@@ -62,6 +64,7 @@ int __init security_init(void)
 		return -EIO;
 	}
 
+	security_enabled = 0;
 	security_ops = &capability_security_ops;
 
 	/* Initialize compiled-in security modules */
@@ -94,6 +97,7 @@ int register_security(struct security_operations *ops)
 		return -EAGAIN;
 
 	security_ops = ops;
+	security_enabled = 1;
 
 	return 0;
 }
@@ -118,6 +122,7 @@ int unregister_security(struct security_operations *ops)
 		return -EINVAL;
 	}
 
+	security_enabled = 0;
 	security_ops = &capability_security_ops;
 
 	return 0;
