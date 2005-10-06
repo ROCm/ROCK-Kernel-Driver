3f7aaeb8f4f9faca4c1116b09587db887bd58f77
Olaf Kirch <okir@suse.de>
2005-10-06 11:35:17 +0000

    - patches.fixes/acpi-endless-loop: [ACPI] Fix ACPI device scan (116763).
    suse-commit: 5fdbce59ea908891a3dfa0b18a7a85b412e02b73

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -1152,7 +1152,7 @@ acpi_add_single_object (
 	 *
 	 * TBD: Assumes LDM provides driver hot-plug capability.
 	 */
-	result = acpi_bus_find_driver(device);
+	acpi_bus_find_driver(device);
 
 end:
 	if (!result)
