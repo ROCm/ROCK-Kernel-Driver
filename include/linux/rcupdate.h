293d85554d3d35234a9dc4cd1900b4fdd60372ba
Chris L Mason <mason@suse.de>
2006-03-21 02:22:22 +0000

    - patches.suse/rcu-remote: allow specific cpus to run rcu   (147431). rediff from sgi
    suse-commit: de443db83a18c8c7340220c35b6cc20ecd2e70b4

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -104,6 +104,7 @@ struct rcu_data {
 	struct rcu_head *donelist;
 	struct rcu_head **donetail;
 	long		blimit;		 /* Upper limit on a processed batch */
+	int doneself;
 	int cpu;
 	struct rcu_head barrier;
 #ifdef CONFIG_SMP
@@ -269,6 +270,8 @@ extern __deprecated_for_modules void synchronize_kernel(void);
 extern void synchronize_rcu(void);
 void synchronize_idle(void);
 extern void rcu_barrier(void);
+int rcu_set_remote_rcu(int cpu);
+void rcu_clear_remote_rcu(int cpu);
 
 #endif /* __KERNEL__ */
 #endif /* __LINUX_RCUPDATE_H */
