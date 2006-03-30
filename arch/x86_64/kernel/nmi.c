2e8901cafe4940bab4d3aeb04c98f95698e5c3dc
Andreas Kleen <ak@suse.de>
2006-03-30 14:49:55 +0000

    - patches.arch/x86_64-mce-nmi-watchdog: Don't run NMI watchdog   during machine checks (156566 155726). - patches.arch/i386-kexec-apic-ack: [PATCH] i386 kdump timer   vector lockup fix.
    suse-commit: fe046bb91bca5fb8134f72df63cfe8d7a741a7a0

--- arch/x86_64/kernel/nmi.c
+++ arch/x86_64/kernel/nmi.c
@@ -35,6 +35,7 @@
 #include <asm/proto.h>
 #include <asm/kdebug.h>
 #include <asm/local.h>
+#include <asm/mce.h>
 
 /*
  * lapic_nmi_owner tracks the ownership of the lapic NMI hardware:
@@ -483,6 +484,10 @@ void __kprobes nmi_watchdog_tick(struct pt_regs * regs, unsigned reason)
 		__get_cpu_var(nmi_touch) = 0;
 		touched = 1;
 	}
+	/* Could check oops_in_progress here too, but it's safer
+	   not too */
+	if (atomic_read(&mce_entry) > 0) 
+		touched = 1;
 	if (!touched && __get_cpu_var(last_irq_sum) == sum) {
 		/*
 		 * Ayiee, looks like this CPU is stuck ...
