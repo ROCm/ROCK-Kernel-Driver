399d8ac0ba1bff907391f9cf7fddedeb94cd1ddf
Olaf Hering <olh@suse.de>
2006-04-24 20:57:52 +0000

    +- add patches.fixes/tg3-selftest-phy_reset.patch +  workaround for lost carrier during ethtool -t (158568 - LTC22155)
    suse-commit: c18369d7b2e698938a6bf9cfe273b17d01d4b2cf

--- drivers/net/tg3.c
+++ drivers/net/tg3.c
@@ -140,6 +140,8 @@
 
 #define TG3_NUM_TEST		6
 
+static void tg3_link_report(struct tg3 *);
+
 static char version[] __devinitdata =
 	DRV_MODULE_NAME ".c:v" DRV_MODULE_VERSION " (" DRV_MODULE_RELDATE ")\n";
 
@@ -964,6 +966,11 @@ static int tg3_phy_reset(struct tg3 *tp)
 	err |= tg3_readphy(tp, MII_BMSR, &phy_status);
 	if (err != 0)
 		return -EBUSY;
+
+	if (netif_running(tp->dev) && netif_carrier_ok(tp->dev)) {
+		netif_carrier_off(tp->dev);
+		tg3_link_report(tp);
+	}
 
 	if (GET_ASIC_REV(tp->pci_chip_rev_id) == ASIC_REV_5703 ||
 	    GET_ASIC_REV(tp->pci_chip_rev_id) == ASIC_REV_5704 ||
