3b4e30ef1fa4f5227c280a0c9ea3e23db40cc8e9
Olaf Kirch <okir@suse.de>
2006-06-19 09:48:51 +0000

    - patches.fixes/ipv6-add-addr-install-dstentry: ipv6_add_addr   should install dstentry earlier (185506).
    suse-commit: b457c4796f2c6ec615c54666765bf426cd7232f6

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -549,6 +549,8 @@ ipv6_add_addr(struct inet6_dev *idev, const struct in6_addr *addr, int pfxlen,
 	ifa->flags = flags | IFA_F_TENTATIVE;
 	ifa->cstamp = ifa->tstamp = jiffies;
 
+	ifa->rt = rt;
+
 	ifa->idev = idev;
 	in6_dev_hold(idev);
 	/* For caller */
@@ -574,8 +576,6 @@ ipv6_add_addr(struct inet6_dev *idev, const struct in6_addr *addr, int pfxlen,
 		in6_ifa_hold(ifa);
 	}
 #endif
-
-	ifa->rt = rt;
 
 	in6_ifa_hold(ifa);
 	write_unlock(&idev->lock);
