d6cb9ae15b112ad00ca67341c050644409af4367
Greg Kroah-Hartman <gregkh@suse.de>
2006-08-01 01:27:07 +0000

    - fix build error in patches.fixes/export-symbols-gpl: export some symbols for   SGI GPL kernel modules (SUSE46082, 141533).
    suse-commit: 5e77ca095c1ea8f13e49950b355fd5837cff01dc

--- kernel/fork.c
+++ kernel/fork.c
@@ -121,7 +121,6 @@ void __put_task_struct(struct task_struct *tsk)
 	if (!profile_handoff_task(tsk))
 		free_task(tsk);
 }
-EXPORT_SYMBOL_GPL(__put_task_struct_cb);
 
 void __init fork_init(unsigned long mempages)
 {
