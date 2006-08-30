50ff945670ccb09f1b5d00d34f556022f39ba8b0
Greg Kroah-Hartman <gregkh@suse.de>
2006-08-30 00:23:08 +0000

    - Disable CONFIG_SMB_FS  CIFS is now the way to go.  The samba team is working   on the migration tools for the upgrade issues. - patches.drivers/smbfs-sendqueue-backoff: Delete. - patches.drivers/smbfs-request-counting: Delete.
    suse-commit: b73ba4813a87e145a3853e2534e0fdab4f7353dc

--- include/linux/smb_fs_sb.h
+++ include/linux/smb_fs_sb.h
@@ -66,10 +66,8 @@ struct smb_sb_info {
 	u32                smb_len;
 	u32                smb_read;
 
-        /* We use our own data_ready and write_space callbacks,
-	 * but need the original ones */
+        /* We use our own data_ready callback, but need the original one */
         void *data_ready;
-	void *write_space;
 
 	/* nls pointers for codepage conversions */
 	struct nls_table *remote_nls;
