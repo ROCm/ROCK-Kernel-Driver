d71d121c0091f662e4a2285eb9f2b462fb33ffb6
Olaf Hering <olh@suse.de>
2007-08-10 13:33:32 +0000

    +- add patches.arch/ppc-pegasos-keyboard-8042-check.patch +  fix Pegasos keyboard detection
    suse-commit: 7c1e4bb9d5dad0f4a5a64ee78136615102fb6d09

--- arch/powerpc/kernel/setup-common.c
+++ arch/powerpc/kernel/setup-common.c
@@ -487,6 +487,10 @@ int check_legacy_ioport(unsigned long base_port)
 	switch(base_port) {
 	case I8042_DATA_REG:
 		np = of_find_node_by_type(NULL, "8042");
+		/* Pegasos has no device_type on its 8042 node, look for the
+		 * name instead */
+		if (!np)
+			np = of_find_node_by_name(NULL, "8042");
 		break;
 	case FDC_BASE: /* FDC1 */
 		np = of_find_node_by_type(NULL, "fdc");
