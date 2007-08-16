f4707d38a88dd028faf3933dd9158441fc708dfa
Thomas Renninger <trenn@suse.de>
2007-08-16 17:11:45 +0000

    - patches.arch/acpi_autoloading_interface.patch:   Remove dummy in struct to avoid a lot compile warnings due to   wrong initialisations
    suse-commit: f57a224891e694d4c603e20e81a2d0071a64b731

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -159,12 +159,11 @@ struct ap_device_id {
 
 #define AP_DEVICE_ID_MATCH_DEVICE_TYPE		0x01
 
-#define ACPI_ID_LEN	9
-#define FILLUP_LEN     7 /* dirty fix for i386 -> 64bit cross-compilation */
+#define ACPI_ID_LEN	16 /* only 9 bytes needed here, 16 bytes are used */
+			   /* to workaround crosscompile issues */
 
 struct acpi_device_id {
 	__u8 id[ACPI_ID_LEN];
-	__u8 dummy[FILLUP_LEN];
 	kernel_ulong_t driver_data;
 };
 
