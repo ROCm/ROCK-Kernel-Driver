160f6a85fd60db490eed762fb9035ffe69d40012
Olaf Hering <olh@suse.de>
2008-04-24 12:28:43 +0000

    +- update patches.arch/ppc-efika-modalias.patch +  add devspec file for mac-io, it was created as a side effect +  in of/platform.c (bnc#374693)
    suse-commit: 48088c9a6e38524686954f61b3683c8cd4abf449

--- drivers/macintosh/macio_sysfs.c
+++ drivers/macintosh/macio_sysfs.c
@@ -52,6 +52,15 @@ static ssize_t modalias_show (struct device *dev, struct device_attribute *attr,
 	return len+1;
 }
 
+static ssize_t devspec_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	struct of_device *ofdev;
+
+	ofdev = to_of_device(dev);
+	return sprintf(buf, "%s", ofdev->node->full_name);
+}
+
 macio_config_of_attr (name, "%s\n");
 macio_config_of_attr (type, "%s\n");
 
@@ -60,5 +69,6 @@ struct device_attribute macio_dev_attrs[] = {
 	__ATTR_RO(type),
 	__ATTR_RO(compatible),
 	__ATTR_RO(modalias),
+	__ATTR_RO(devspec),
 	__ATTR_NULL
 };
