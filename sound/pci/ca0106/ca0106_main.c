321d528e45c4e2459c462660c0c2b53fcc2e80ee
Takashi Iwai <tiwai@suse.de>
2008-12-20 11:32:20 +0100

    - patches.drivers/alsa-ca0106-pm-support: More fixes (bnc#447624).
    suse-commit: facf0d76d88f1563096c1baf5f28a67a41c19a60

--- sound/pci/ca0106/ca0106_main.c
+++ sound/pci/ca0106/ca0106_main.c
@@ -1701,6 +1701,7 @@ static int __devinit snd_ca0106_probe(struct pci_dev *pci,
 	err = snd_ca0106_create(dev, card, pci, &chip);
 	if (err < 0)
 		goto error;
+	card->private_data = chip;
 
 	for (i = 0; i < 4; i++) {
 		err = snd_ca0106_pcm(chip, i);
@@ -1759,7 +1760,8 @@ static int snd_ca0106_suspend(struct pci_dev *pci, pm_message_t state)
 	snd_power_change_state(card, SNDRV_CTL_POWER_D3hot);
 	for (i = 0; i < 4; i++)
 		snd_pcm_suspend_all(chip->pcm[i]);
-	snd_ac97_suspend(chip->ac97);
+	if (chip->details->ac97)
+		snd_ac97_suspend(chip->ac97);
 	snd_ca0106_mixer_suspend(chip);
 
 	ca0106_stop_chip(chip);
@@ -1788,7 +1790,8 @@ static int snd_ca0106_resume(struct pci_dev *pci)
 
 	ca0106_init_chip(chip, 1);
 
-	snd_ac97_resume(chip->ac97);
+	if (chip->details->ac97)
+		snd_ac97_resume(chip->ac97);
 	snd_ca0106_mixer_resume(chip);
 	if (chip->details->spi_dac) {
 		for (i = 0; i < ARRAY_SIZE(chip->spi_dac_reg); i++)
