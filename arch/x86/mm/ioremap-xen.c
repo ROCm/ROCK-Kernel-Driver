a197b257fc5ea9b247249f543818a1d08594895a
Jan Beulich <jbeulich@novell.com>
2009-01-26 13:46:42 +0100

    - Just comment out patches.xen/xen3-e1000e_* (to address build error)   until disposition of their originals is known.
    suse-commit: 72ce311a54fa0df32ae5851b7db08d677f421316

--- arch/x86/mm/ioremap-xen.c
+++ arch/x86/mm/ioremap-xen.c
@@ -329,12 +329,6 @@ static void __iomem *__ioremap_caller(resource_size_t phys_addr,
 		return (__force void __iomem *)isa_bus_to_virt((unsigned long)phys_addr);
 
 	/*
-	 * Check if the request spans more than any BAR in the iomem resource
-	 * tree.
-	 */
-	WARN_ON(iomem_map_sanity_check(phys_addr, size));
-
-	/*
 	 * Don't allow anybody to remap normal RAM that we're using..
 	 */
 	for (mfn = PFN_DOWN(phys_addr); mfn < PFN_UP(last_addr); mfn++) {
