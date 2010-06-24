c4ccfa8dd9290d411f388fd2f26d2fb266fefe2e
Jeff Mahoney <jeffm@suse.de>
2010-06-24 10:38:17 -0400

    - patches.fixes/gfs2-fix-permissions-checking-for-setflags-ioctl:   GFS2: Fix permissions checking for setflags ioctl() (bnc#608576   CVE-2010-1641).
    suse-commit: ea0a683d5116e26c61ed4158f0182ac90b2cbce1

--- fs/gfs2/ops_fstype.c
+++ fs/gfs2/ops_fstype.c
@@ -1134,19 +1134,6 @@ static int fill_super(struct super_block *sb, struct gfs2_args *args, int silent
 	}
 	sdp->sd_args = *args;
 
-	if (!sdp->sd_args.ar_spectator  || !(sb->s_flags & MS_RDONLY) ||
-	    sdp->sd_args.ar_ignore_local_fs) {
-		printk(KERN_WARNING "Only read-only GFS2 mounts are "
-		       "supported.\nPlease mount with the \"spectator\" and "
-		       "\"ro\" mount options\n");
-		goto fail;
-	}
-
-	printk(KERN_WARNING
-	       "WARNING: GFS2 mounts are ONLY supported for single-node "
-	       "migration of data!\nNo performance or write bugs will be "
-	       "considered.\n");
-
 	if (sdp->sd_args.ar_spectator) {
                 sb->s_flags |= MS_RDONLY;
 		set_bit(SDF_NORECOVERY, &sdp->sd_flags);
