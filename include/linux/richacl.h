ac5c3471148b2a8c7412b948d56f6cbd5ce95db1
Andreas Gruenbacher <agruen@suse.de>
2010-06-24 20:51:10 +0200

    - patches.suse/0008-richacl-Permission-check-algorithm.patch:   richacl: Permission check algorithm.
    suse-commit: e1339c19c0ff41de4de6089d0d1cc0795dc5b896

--- include/linux/richacl.h
+++ include/linux/richacl.h
@@ -270,5 +270,7 @@ extern unsigned int richacl_mode_to_mask(mode_t);
 extern unsigned int richacl_want_to_mask(int);
 extern void richacl_compute_max_masks(struct richacl *);
 extern struct richacl *richacl_chmod(struct richacl *, mode_t);
+extern int richacl_permission(struct inode *, const struct richacl *,
+			      unsigned int);
 
 #endif /* __RICHACL_H */
