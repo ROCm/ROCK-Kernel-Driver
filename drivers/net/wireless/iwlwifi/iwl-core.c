0d3e2078291ddfee9cd8cb56416a06084d55de68
Jeff Mahoney <jeffm@suse.de>
2010-07-02 11:22:01 -0400

    - patches.fixes/iwlwifi-fix-tx-power-configuration-on-3945-and-4965-devices:   iwlwifi: fix TX power configuration on 3945 and 4965 devices   (bnc#619440 bnc#610421).
    suse-commit: f8d6b9bf7023bc7b66a088b299dc149891ba678d

--- drivers/net/wireless/iwlwifi/iwl-core.c
+++ drivers/net/wireless/iwlwifi/iwl-core.c
@@ -2826,13 +2826,6 @@ int iwl_mac_config(struct ieee80211_hw *hw, u32 changed)
 			IWL_DEBUG_MAC80211(priv, "Error setting sleep level\n");
 	}
 
-	if (changed & IEEE80211_CONF_CHANGE_POWER) {
-		IWL_DEBUG_MAC80211(priv, "TX Power old=%d new=%d\n",
-			priv->tx_power_user_lmt, conf->power_level);
-
-		iwl_set_tx_power(priv, conf->power_level, false);
-	}
-
 	if (!iwl_is_ready(priv)) {
 		IWL_DEBUG_MAC80211(priv, "leave - not ready\n");
 		goto out;
@@ -2846,6 +2839,14 @@ int iwl_mac_config(struct ieee80211_hw *hw, u32 changed)
 		iwlcore_commit_rxon(priv);
 	else
 		IWL_DEBUG_INFO(priv, "Not re-sending same RXON configuration.\n");
+
+	if (changed & IEEE80211_CONF_CHANGE_POWER) {
+		IWL_DEBUG_MAC80211(priv, "TX Power old=%d new=%d\n",
+			priv->tx_power_user_lmt, conf->power_level);
+
+		iwl_set_tx_power(priv, conf->power_level, false);
+	}
+
 
 
 out:
