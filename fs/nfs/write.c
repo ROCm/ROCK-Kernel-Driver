567c1cd4df3aa19626c4c713383bc981b6dc8a9d
Suresh Jayaraman <sjayaraman@suse.de>
2011-03-17 12:24:09 +0100

    - netvm: Do not mark requests for swapfile writes as dirty or   kswapd fails to free the page (bnc#678472).
    suse-commit: a798dd143b7ecef80bddef7806512093cecc63c6

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -745,11 +745,21 @@ static int nfs_writepage_setup(struct nfs_open_context *ctx, struct page *page,
 	req = nfs_setup_write_request(ctx, page, offset, count);
 	if (IS_ERR(req))
 		return PTR_ERR(req);
-	nfs_mark_request_dirty(req);
+
+	/*
+	 * There is no need to mark swapfile requests as dirty like normal
+	 * writepage requests as page dirtying and cleaning is managed
+	 * from the mm. If a PageSwapCache page is marked dirty like this,
+	 * it will still be dirty after kswapd calls writepage and may
+	 * never be released
+	 */
+	if (!PageSwapCache(page))
+		nfs_mark_request_dirty(req);
 	/* Update file length */
 	nfs_grow_file(page, offset, count);
 	nfs_mark_uptodate(page, req->wb_pgbase, req->wb_bytes);
-	nfs_mark_request_dirty(req);
+	if (!PageSwapCache(page))
+		nfs_mark_request_dirty(req);
 	nfs_clear_page_tag_locked(req);
 	return 0;
 }
