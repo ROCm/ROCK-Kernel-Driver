6695729990ba7f7dcbb3ddeb230c77570fececd3
Michal Hocko <mhocko@suse.cz>
2011-05-10 16:04:27 +0200

    - Delete patches.suse/mm-devzero-optimisation.patch: we do not need   it anymore as we have zero page back since 2.6.32
    suse-commit: 323a093b350666a635e65c544b4b947566a29dfb

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -964,8 +964,6 @@ void free_pgd_range(struct mmu_gather *tlb, unsigned long addr,
 		unsigned long end, unsigned long floor, unsigned long ceiling);
 int copy_page_range(struct mm_struct *dst, struct mm_struct *src,
 			struct vm_area_struct *vma);
-int zeromap_page_range(struct vm_area_struct *vma, unsigned long from,
-			unsigned long size, pgprot_t prot);
 void unmap_mapping_range(struct address_space *mapping,
 		loff_t const holebegin, loff_t const holelen, int even_cows);
 int follow_pfn(struct vm_area_struct *vma, unsigned long address,
