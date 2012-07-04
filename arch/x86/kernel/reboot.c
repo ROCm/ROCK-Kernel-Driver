788c746c23e3867e3de1c566429730cf2f015167
Thomas Renninger <trenn@suse.de>
2012-07-04 09:53:34 +0200

    - Delete patches.arch/dell_latitude_e6xxx_e5xxx__fix_reboot.patch. - Delete patches.drivers/apei_allow_drivers_access_nvs_ram.patch.
    suse-commit: c6e9e42b2f02f4cb9b1f1a50226dc981e6f1e73c

--- arch/x86/kernel/reboot.c
+++ arch/x86/kernel/reboot.c
@@ -419,20 +419,28 @@ static struct dmi_system_id __initdata reboot_dmi_table[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "iMac9,1"),
 		},
 	},
-	{
+	{	/* Handle problems with rebooting on the Latitude E6320. */
 		.callback = set_pci_reboot,
-		.ident = "Dell Latitude E5xxx",
+		.ident = "Dell Latitude E6320",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
-			DMI_MATCH(DMI_PRODUCT_NAME, "Latitude E5"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Latitude E6320"),
 		},
 	},
-	{
+	{	/* Handle problems with rebooting on the Latitude E5420. */
 		.callback = set_pci_reboot,
-		.ident = "Dell Latitude E6xxx",
+		.ident = "Dell Latitude E5420",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
-			DMI_MATCH(DMI_PRODUCT_NAME, "Latitude E6"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Latitude E5420"),
+		},
+	},
+	{	/* Handle problems with rebooting on the Latitude E6420. */
+		.callback = set_pci_reboot,
+		.ident = "Dell Latitude E6420",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Latitude E6420"),
 		},
 	},
 	{	/* Handle problems with rebooting on the OptiPlex 990. */
