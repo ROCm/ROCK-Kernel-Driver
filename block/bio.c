23f6d981952acdbf78d102ea28fe50f323185927
Jeff Mahoney <jeffm@suse.com>
2014-10-09 00:03:09 -0400

    block: copy bi_vcnt in __bio_clone_fast (boo#888259).
    suse-commit: 263fe495efd7a2cc0bdb2e0305f96cb60f71cd05

--- block/bio.c
+++ block/bio.c
@@ -565,6 +565,7 @@ void __bio_clone_fast(struct bio *bio, struct bio *bio_src)
 	bio->bi_rw = bio_src->bi_rw;
 	bio->bi_iter = bio_src->bi_iter;
 	bio->bi_io_vec = bio_src->bi_io_vec;
+	bio->bi_vcnt = bio_src->bi_vcnt;
 }
 EXPORT_SYMBOL(__bio_clone_fast);
 
