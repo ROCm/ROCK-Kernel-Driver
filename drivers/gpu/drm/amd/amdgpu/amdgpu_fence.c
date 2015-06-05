43c9900cace5c9829af2056de83b1ff2dd232c97
Chunming Zhou <david1.zhou@amd.com>
2015-06-05 22:13:55 +0800

    drm/amdgpu: fix merge error that result in compile failed
    Signed-off-by: Chunming Zhou <david1.zhou@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
@@ -129,9 +129,9 @@ int amdgpu_fence_emit(struct amdgpu_ring *ring, void *owner,
 		&adev->fence_queue.lock, adev->fence_context + ring->idx,
 		(*fence)->seq);
 	amdgpu_ring_emit_fence(ring, ring->fence_drv.gpu_addr,
-			       fence->seq,
+			       (*fence)->seq,
 			       AMDGPU_FENCE_FLAG_INT);
-	trace_amdgpu_fence_emit(ring->adev->ddev, ring->idx, fence->seq);
+	trace_amdgpu_fence_emit(ring->adev->ddev, ring->idx, (*fence)->seq);
 	return 0;
 }
 
