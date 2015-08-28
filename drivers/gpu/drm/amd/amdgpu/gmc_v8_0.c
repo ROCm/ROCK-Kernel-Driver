0bbac03b7d819f2c5e35b0dd0a61a721fab9f76e
Jay Cornwall <jay@jcornwall.me>
2015-08-28 11:25:39 -0500

    drm/amdgpu: Enable BIF 5.0 atomic promotion to PCIe 3.0 AtomicOps
    Atomic operations to system memory are normally dropped by the BIF.
    Some PCIe 3.0 platforms support atomic routing to the host for execution
    by the CPU. To use this feature, atomic requests must be enabled in the
    GPU's PCIe configuration space.
    
    This is a no-op on platforms which cannot route the atomic request to
    the host. In this instance, atomics would be dropped with or without
    this change.
    
    Signed-off-by: Jay Cornwall <jay@jcornwall.me>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Acked-by: John Bridgman <john.bridgman@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gmc_v8_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v8_0.c
@@ -1042,6 +1042,10 @@ static int gmc_v8_0_sw_init(void *handle)
 		adev->vm_manager.enabled = true;
 	}
 
+	/* Allow BIF to recode atomics to PCIe 3.0 AtomicOps.
+	 */
+	pci_enable_atomic_request(adev->pdev);
+
 	return r;
 }
 
