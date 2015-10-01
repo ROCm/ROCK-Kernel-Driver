d882bad42c3523bd153cc41cf4603262e0522fdd
Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>
2015-10-01 17:19:56 -0400

    drm/amdgpu: add members to struct amdgpu_cu_info
    For KFD topology these additional information is necessary.
    
    Change-Id: Ifd700b28cce7d38e41c1a168d64dfa2d992b150c

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -857,6 +857,10 @@ struct amdgpu_gfx_config {
 struct amdgpu_cu_info {
 	uint32_t number; /* total active CU number */
 	uint32_t ao_cu_mask;
+	uint32_t simd_per_cu;
+	uint32_t max_waves_per_simd;
+	uint32_t wave_front_size;
+	uint32_t max_scratch_slots_per_cu;
 	uint32_t bitmap[4][4];
 };
 
@@ -1196,6 +1200,7 @@ struct amdgpu_allowed_register_entry {
 	bool untouched;
 	bool grbm_indexed;
 };
+
 
 /*
  * ASIC specific functions.
