76936cc1f6a8b475b34a1d7723ba6def035e4881
Jiri Slaby <jslaby@suse.cz>
2015-11-14 23:36:45 +0100

    - Refresh patches.suse/crasher.patch
    See the patch to see what the update consists of.
    
    suse-commit: 57989db4ab061d1002892f2d57ff918bc116a0f7

--- drivers/char/crasher.c
+++ drivers/char/crasher.c
@@ -2,40 +2,40 @@
  * crasher.c, it breaks things
  */
 
-
-#include <linux/module.h>
-#include <linux/types.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/slab.h>
 #include <linux/completion.h>
+#include <linux/init.h>
 #include <linux/jiffies.h>
-#include <linux/sched.h>
-#include <linux/moduleparam.h>
+#include <linux/kernel.h>
 #include <linux/kthread.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/random.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/timer.h>
+#include <linux/types.h>
 
-static int module_exiting;
-static struct completion startup = COMPLETION_INITIALIZER(startup);
-static unsigned long rand_seed = 152L;
-static unsigned long seed = 152L;
-static int threads = 1;
+static DECLARE_BITMAP(module_exiting, 1);
+static DECLARE_COMPLETION(startup);
+static unsigned int threads = 1;
+static unsigned int timer;
 static bool call_panic, call_bug, call_warn;
 static bool trap_null, call_null, jump_null;
-static long trap_read, trap_write, call_bad, jump_bad;
+static unsigned long trap_read, trap_write, call_bad, jump_bad;
 
-module_param(seed, ulong, 0);
+module_param(timer, uint, 0);
 module_param(call_panic, bool, 0);
 module_param(call_bug, bool, 0);
 module_param(call_warn, bool, 0);
 module_param(trap_null, bool, 0);
-module_param(trap_read, long, 0);
-module_param(trap_write, long, 0);
+module_param(trap_read, ulong, 0);
+module_param(trap_write, ulong, 0);
 module_param(call_null, bool, 0);
-module_param(call_bad, long, 0);
+module_param(call_bad, ulong, 0);
 module_param(jump_null, bool, 0);
-module_param(jump_bad, long, 0);
-module_param(threads, int, 0);
-MODULE_PARM_DESC(seed, "random seed for memory tests");
+module_param(jump_bad, ulong, 0);
+module_param(threads, uint, 0);
+MODULE_PARM_DESC(timer, "perform the selected crash action from the timer context in the specified number of ms");
 MODULE_PARM_DESC(call_panic, "test option. call panic() and render the system unusable.");
 MODULE_PARM_DESC(call_bug, "test option. call BUG() and render the system unusable.");
 MODULE_PARM_DESC(call_warn, "test option. call WARN() and leave the system usable.");
@@ -50,86 +50,65 @@ MODULE_PARM_DESC(threads, "number of threads to run");
 MODULE_LICENSE("GPL");
 
 #define NUM_ALLOC 24
-#define NUM_SIZES 8
-static int sizes[]  = { 32, 64, 128, 192, 256, 1024, 2048, 4096 };
+static const unsigned int sizes[] = { 32, 64, 128, 192, 256, 1024, 2048, 4096 };
+#define NUM_SIZES ARRAY_SIZE(sizes)
 
 struct mem_buf {
 	char *buf;
-	int size;
+	unsigned int size;
 };
 
-static unsigned long crasher_random(void)
+static inline char crasher_hash(unsigned int i)
 {
-	rand_seed = rand_seed*69069L+1;
-	return rand_seed^jiffies;
+	return (i % 119) + 8;
 }
 
-void crasher_srandom(unsigned long entropy)
+static void mem_alloc(struct mem_buf *b)
 {
-	rand_seed ^= entropy;
-	crasher_random();
-}
+	unsigned int i, size = sizes[prandom_u32_max(NUM_SIZES)];
 
-static char *mem_alloc(int size) {
-	char *p = kmalloc(size, GFP_KERNEL);
-	int i;
-	if (!p)
-		return p;
-	for (i = 0 ; i < size; i++)
-		p[i] = (i % 119) + 8;
-	return p;
-}
-
-static void mem_check(char *p, int size) {
-	int i;
-	if (!p)
+	b->buf = kmalloc(size, GFP_KERNEL);
+	if (!b->buf)
 		return;
-	for (i = 0 ; i < size; i++) {
-		if (p[i] != ((i % 119) + 8)) {
-			printk(KERN_CRIT "verify error at %lX offset %d "
-			       " wanted %d found %d size %d\n",
-			       (unsigned long)(p + i), i, (i % 119) + 8,
-			       p[i], size);
-		}
-	}
-	// try and trigger slab poisoning for people using this buffer
-	// wrong
-	memset(p, 0, size);
+
+	b->size = size;
+
+	for (i = 0 ; i < size; i++)
+		b->buf[i] = crasher_hash(i);
 }
 
-static void mem_verify(void) {
-	struct mem_buf bufs[NUM_ALLOC];
-	struct mem_buf *b;
-	int index;
-	int size;
-	unsigned long sleep;
-	memset(bufs, 0, sizeof(struct mem_buf) * NUM_ALLOC);
-	while(!module_exiting) {
-		index = crasher_random() % NUM_ALLOC;
-		b = bufs + index;
-		if (b->size) {
-			mem_check(b->buf, b->size);
-			kfree(b->buf);
-			b->buf = NULL;
-			b->size = 0;
-		} else {
-			size = crasher_random() % NUM_SIZES;
-			size = sizes[size];
-			b->buf = mem_alloc(size);
-			b->size = size;
-		}
-		sleep = crasher_random() % (HZ / 10);
-		set_current_state(TASK_INTERRUPTIBLE);
-		schedule_timeout(sleep);
-		set_current_state(TASK_RUNNING);
-	}
-	for (index = 0 ; index < NUM_ALLOC ; index++) {
-		b = bufs + index;
-		if (b->size) {
-			mem_check(b->buf, b->size);
-			kfree(b->buf);
+static void mem_check_free(struct mem_buf *b)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < b->size; i++) {
+		if (b->buf[i] != crasher_hash(i)) {
+			pr_crit("crasher: verify error at %p, offset %u, wanted %d, found %d, size %u\n",
+					&b->buf[i], i, crasher_hash(i),
+					b->buf[i], b->size);
 		}
 	}
+	kfree(b->buf);
+	b->buf = NULL;
+	b->size = 0;
+}
+
+static void mem_verify(void)
+{
+	struct mem_buf *b, bufs[NUM_ALLOC] = {};
+
+	while (!test_bit(0, module_exiting)) {
+		b = &bufs[prandom_u32_max(NUM_ALLOC)];
+		if (b->size)
+			mem_check_free(b);
+		else
+			mem_alloc(b);
+		schedule_timeout_interruptible(prandom_u32_max(HZ / 10));
+	}
+
+	for (b = bufs; b < &bufs[NUM_ALLOC]; b++)
+		if (b->size)
+			mem_check_free(b);
 }
 
 static int crasher_thread(void *unused)
@@ -137,90 +116,119 @@ static int crasher_thread(void *unused)
 	complete(&startup);
 	mem_verify();
 	complete(&startup);
+
 	return 0;
 }
 
-static int __init crasher_init(void)
+static int crash_now(void)
 {
-	int i;
-	init_completion(&startup);
-	crasher_srandom(seed);
-
 	if (call_panic) {
-		panic("test panic from crasher module. Good Luck.\n");
+		panic("test panic from crasher module. Good luck!");
 		return -EFAULT;
 	}
 	if (call_bug) {
-		printk("triggering BUG\n");
-		BUG_ON(1);
+		pr_crit("crasher: triggering BUG\n");
+		BUG();
 		return -EFAULT;
 	}
-	if (WARN(call_warn, "triggering WARN\n"))
+	if (WARN(call_warn, "crasher: triggering WARN\n"))
 		return -EFAULT;
 
 	if (trap_null) {
 		volatile char *p = NULL;
-		printk("dereferencing NULL pointer.\n");
+		pr_crit("crasher: dereferencing NULL pointer\n");
 		p[0] = '\n';
 		return -EFAULT;
 	}
 	if (trap_read) {
 		const volatile char *p = (char *)trap_read;
-		printk("reading from invalid(?) address %p.\n", p);
+		pr_crit("crasher: reading from invalid(?) address %p\n", p);
 		return p[0] ? -EFAULT : -EACCES;
 	}
 	if (trap_write) {
 		volatile char *p = (char *)trap_write;
-		printk("writing to invalid(?) address %p.\n", p);
+		pr_crit("crasher: writing to invalid(?) address %p\n", p);
 		p[0] = ' ';
 		return -EFAULT;
 	}
 
 	if (call_null) {
-		void(*f)(void) = NULL;
-		printk("calling NULL pointer.\n");
+		void (*f)(void) = NULL;
+		pr_crit("crasher: calling NULL pointer\n");
 		f();
 		return -EFAULT;
 	}
 	if (call_bad) {
-		void(*f)(void) = (void(*)(void))call_bad;
-		printk("calling invalid(?) address %p.\n", f);
+		void (*f)(void) = (void(*)(void))call_bad;
+		pr_crit("crasher: calling invalid(?) address %p\n", f);
 		f();
 		return -EFAULT;
 	}
 
-	/* These two depend on the compiler doing tail call optimization. */
+	/* These two depend on the compiler doing tail call optimization */
 	if (jump_null) {
-		int(*f)(void) = NULL;
-		printk("jumping to NULL.\n");
+		int (*f)(void) = NULL;
+		pr_crit("crasher: jumping to NULL\n");
 		return f();
 	}
 	if (jump_bad) {
-		int(*f)(void) = (int(*)(void))jump_bad;
-		printk("jumping to invalid(?) address %p.\n", f);
+		int (*f)(void) = (int(*)(void))jump_bad;
+		pr_crit("crasher: jumping to invalid(?) address %p\n", f);
 		return f();
 	}
 
-	printk("crasher module (%d threads).  Testing sizes: ", threads);
-	for (i = 0 ; i < NUM_SIZES ; i++)
-		printk("%d ", sizes[i]);
-	printk("\n");
+	return 0;
+}
 
-	for (i = 0 ; i < threads ; i++)
-		kthread_run(crasher_thread, crasher_thread, "crasher");
-	for (i = 0 ; i < threads ; i++)
+static void crash_timer_cb(unsigned long unused)
+{
+	crash_now();
+}
+
+static DEFINE_TIMER(crash_timer, crash_timer_cb, 0, 0);
+
+static int __init crasher_init(void)
+{
+	int ret, i;
+
+	if (timer) {
+		pr_info("crasher: crashing in %u miliseconds...\n", timer);
+		mod_timer(&crash_timer, jiffies + msecs_to_jiffies(timer));
+		return 0;
+	}
+
+	ret = crash_now();
+	if (ret)
+		return ret;
+
+	pr_info("crasher: running %u threads. Testing sizes: ", threads);
+	for (i = 0; i < NUM_SIZES; i++)
+		pr_cont("%u ", sizes[i]);
+	pr_cont("\n");
+
+	for (i = 0; i < threads; i++)
+		kthread_run(crasher_thread, NULL, "crasher");
+	for (i = 0; i < threads; i++)
 		wait_for_completion(&startup);
+
 	return 0;
 }
 
 static void __exit crasher_exit(void)
 {
-	int i;
-	module_exiting = 1;
-	for (i = 0 ; i < threads ; i++)
+	unsigned int i;
+
+	if (timer) {
+		del_timer_sync(&crash_timer);
+		return;
+	}
+
+	set_bit(0, module_exiting);
+
+	for (i = 0; i < threads; i++)
 		wait_for_completion(&startup);
-	printk("all crasher threads done\n");
-	return;
+
+	pr_info("crasher: all threads done\n");
 }
 
 module_init(crasher_init);
