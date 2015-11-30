331e73d46e43728041f23a0a0e6a3e9cca7e48f3
Christian König <christian.koenig@amd.com>
2015-11-30 14:30:08 +0100

    drm/amdgpu: handle foreign BOs in the VM mapping code v2
    That should allow us using foreign BOs in VRAM.
    
    v2: use the correct device for the PTE creation.
    
    Change-Id: I2b4916332a3d93a15d8afadeaf6a2c2fd05b057a
    Signed-off-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
@@ -950,11 +950,14 @@ int amdgpu_vm_bo_update(struct amdgpu_device *adev,
 			struct ttm_mem_reg *mem)
 {
 	struct amdgpu_vm *vm = bo_va->vm;
+	struct amdgpu_bo *bo = bo_va->bo;
 	struct amdgpu_bo_va_mapping *mapping;
 	dma_addr_t *pages_addr = NULL;
 	uint32_t gtt_flags, flags;
 	uint64_t addr;
 	int r;
+
+	flags = amdgpu_ttm_tt_pte_flags(adev, bo->tbo.ttm, mem);
 
 	if (mem) {
 		struct ttm_dma_tt *ttm;
@@ -968,7 +971,11 @@ int amdgpu_vm_bo_update(struct amdgpu_device *adev,
 			break;
 
 		case TTM_PL_VRAM:
-			addr += adev->vm_manager.vram_base_offset;
+			if (bo->adev != adev) {
+				addr += bo->adev->mc.aper_base;
+				flags |= AMDGPU_PTE_SYSTEM;
+			} else
+				addr += adev->vm_manager.vram_base_offset;
 			break;
 
 		default:
@@ -978,7 +985,6 @@ int amdgpu_vm_bo_update(struct amdgpu_device *adev,
 		addr = 0;
 	}
 
-	flags = amdgpu_ttm_tt_pte_flags(adev, bo_va->bo->tbo.ttm, mem);
 	gtt_flags = (adev == bo_va->bo->adev) ? flags : 0;
 
 	spin_lock(&vm->status_lock);
