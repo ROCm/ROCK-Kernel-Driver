a7614ee6949acd0b63ff83d7bb3882e306b85622
Mykola Lysenko <Mykola.Lysenko@amd.com>
2015-12-09 20:18:00 +0800

    drm/amd/dal: do not remove MST connector until reset mode
    Before MST connector removal we need to make sure reset mode happened
    from user space, and all state is clear
    
    Change-Id: I5630ab576bb515fb9dc7ceea6a478b755e8d2271
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Acked-by: Harry Wentland<harry.wentland@amd.com>

--- drivers/gpu/drm/amd/dal/amdgpu_dm/amdgpu_dm_types.c
+++ drivers/gpu/drm/amd/dal/amdgpu_dm/amdgpu_dm_types.c
@@ -1856,6 +1856,7 @@ int amdgpu_dm_connector_init(
 			dm->backlight_link = link;
 	}
 #endif
+	sema_init(&aconnector->mst_sem, 1);
 
 	return 0;
 }
@@ -2173,6 +2174,7 @@ int amdgpu_dm_atomic_commit(
 				/* this is the update mode case */
 				dc_target_release(acrtc->target);
 				acrtc->target = NULL;
+				up(&aconnector->mst_sem);
 			}
 
 			/*
@@ -2186,10 +2188,12 @@ int amdgpu_dm_atomic_commit(
 			acrtc->target = new_target;
 			acrtc->enabled = true;
 			acrtc->base.enabled = true;
+
 			connector_funcs = aconnector->base.helper_private;
 			aconnector->base.encoder =
 				connector_funcs->best_encoder(
 					&aconnector->base);
+			down(&aconnector->mst_sem);
 			break;
 		}
 
@@ -2208,6 +2212,7 @@ int amdgpu_dm_atomic_commit(
 				acrtc->enabled = false;
 				acrtc->base.enabled = false;
 				aconnector->base.encoder = NULL;
+				up(&aconnector->mst_sem);
 			}
 			break;
 		} /* switch() */
