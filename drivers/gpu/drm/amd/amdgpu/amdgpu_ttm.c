59d9196a464476d5d3bb97facf416d61588b482e
Felix Kuehling <Felix.Kuehling@amd.com>
2016-01-15 16:19:12 -0500

    drm/amdgpu: Make amdgpu_ttm_tt_get_user_pages work in kernel thread
    For HSA userptr support, a kernel worker thread must be able to
    re-pin a userptr BO after an MMU notifier unpinned it. Store the
    user task_struct in amdgpu_ttm_tt and use that task and its
    mm_struct instead of the current thread's.
    
    Change-Id: I3ba20fc9c489ad598c9706cf0b64231a18a0ec9f
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -595,7 +595,7 @@ struct amdgpu_ttm_tt {
 	struct amdgpu_device	*adev;
 	u64			offset;
 	uint64_t		userptr;
-	struct mm_struct	*usermm;
+	struct task_struct	*usertask;
 	uint32_t		userflags;
 	spinlock_t              guptasklock;
 	struct list_head        guptasks;
@@ -619,7 +619,7 @@ int amdgpu_ttm_tt_get_user_pages(struct ttm_tt *ttm, struct page **pages)
 		unsigned long end = gtt->userptr + ttm->num_pages * PAGE_SIZE;
 		struct vm_area_struct *vma;
 
-		vma = find_vma(gtt->usermm, gtt->userptr);
+		vma = find_vma(gtt->usertask->mm, gtt->userptr);
 		if (!vma || vma->vm_file || vma->vm_end < end)
 			return -EPERM;
 	}
@@ -974,7 +974,7 @@ int amdgpu_ttm_tt_set_userptr(struct ttm_tt *ttm, uint64_t addr,
 		return -EINVAL;
 
 	gtt->userptr = addr;
-	gtt->usermm = current->mm;
+	gtt->usertask = current;
 	gtt->userflags = flags;
 	spin_lock_init(&gtt->guptasklock);
 	INIT_LIST_HEAD(&gtt->guptasks);
