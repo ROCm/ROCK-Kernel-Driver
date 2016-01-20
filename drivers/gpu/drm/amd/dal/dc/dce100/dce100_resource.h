03ad957b64ecfc6513435f1614425f49593a83e9
Eric Yang <eric.yang2@amd.com>
2016-01-20 17:43:06 -0500

    drm/amd/dal: create dce100 resource
    This has been verified by creating dce100 resouce
    in dce110_hwsequencer
    
    Signed-off-by: Eric Yang <eric.yang2@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>

---
+++ drivers/gpu/drm/amd/dal/dc/dce100/dce100_resource.h
@@ -0,0 +1,24 @@
+/*
+ * dce100_resource.h
+ *
+ *  Created on: 2016-01-20
+ *      Author: qyang
+ */
+
+#ifndef DCE100_RESOURCE_H_
+#define DCE100_RESOURCE_H_
+
+struct adapter_service;
+struct dc;
+struct resource_pool;
+struct dc_validation_set;
+
+
+bool dce100_construct_resource_pool(
+	struct adapter_service *adapter_serv,
+	struct dc *dc,
+	struct resource_pool *pool);
+
+void dce100_destruct_resource_pool(struct resource_pool *pool);
+
+#endif /* DCE100_RESOURCE_H_ */
