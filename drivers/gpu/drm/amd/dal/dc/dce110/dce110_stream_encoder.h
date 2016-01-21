23cf0162cbe36f973878cf526a7e2d01eb1b719d
Harry Wentland <harry.wentland@amd.com>
2016-01-21 16:38:09 -0500

    drm/amd/dal: Re-use stream encoder programming between DCEs
    Pass register values into stream_encoder HW programming code
    so we can reuse it between DCEs.
    
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/dal/dc/dce110/dce110_stream_encoder.h
+++ drivers/gpu/drm/amd/dal/dc/dce110/dce110_stream_encoder.h
@@ -31,15 +31,39 @@
 #define DCE110STRENC_FROM_STRENC(stream_encoder)\
 	container_of(stream_encoder, struct dce110_stream_encoder, base)
 
-struct dce110_stream_enc_offsets {
-	int32_t dig;
-	int32_t dp;
+struct dce110_stream_enc_registers {
+	uint32_t AFMT_AVI_INFO0;
+	uint32_t AFMT_AVI_INFO1;
+	uint32_t AFMT_AVI_INFO2;
+	uint32_t AFMT_AVI_INFO3;
+	uint32_t AFMT_GENERIC_0;
+	uint32_t AFMT_GENERIC_7;
+	uint32_t AFMT_GENERIC_HDR;
+	uint32_t AFMT_INFOFRAME_CONTROL0;
+	uint32_t AFMT_VBI_PACKET_CONTROL;
+	uint32_t DIG_FE_CNTL;
+	uint32_t DP_MSE_RATE_CNTL;
+	uint32_t DP_MSE_RATE_UPDATE;
+	uint32_t DP_PIXEL_FORMAT;
+	uint32_t DP_SEC_CNTL;
+	uint32_t DP_STEER_FIFO;
+	uint32_t DP_VID_M;
+	uint32_t DP_VID_N;
+	uint32_t DP_VID_STREAM_CNTL;
+	uint32_t DP_VID_TIMING;
+	uint32_t HDMI_CONTROL;
+	uint32_t HDMI_GC;
+	uint32_t HDMI_GENERIC_PACKET_CONTROL0;
+	uint32_t HDMI_GENERIC_PACKET_CONTROL1;
+	uint32_t HDMI_INFOFRAME_CONTROL0;
+	uint32_t HDMI_INFOFRAME_CONTROL1;
+	uint32_t HDMI_VBI_PACKET_CONTROL;
+	uint32_t TMDS_CNTL;
 };
 
 struct dce110_stream_encoder {
 	struct stream_encoder base;
-	struct dce110_stream_enc_offsets offsets;
-	struct dce110_stream_enc_offsets derived_offsets;
+	const struct dce110_stream_enc_registers *regs;
 };
 
 bool dce110_stream_encoder_construct(
@@ -47,7 +71,7 @@ bool dce110_stream_encoder_construct(
 	struct dc_context *ctx,
 	struct dc_bios *bp,
 	enum engine_id eng_id,
-	const struct dce110_stream_enc_offsets *offsets);
+	const struct dce110_stream_enc_registers *regs);
 
 /***** HW programming ***********/
 /* setup stream encoder in dp mode */
