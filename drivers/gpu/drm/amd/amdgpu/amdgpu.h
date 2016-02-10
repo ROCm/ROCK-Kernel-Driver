2738fffcc9a7578100b4a625f9d95cecf18b6d3d
Yair Shachar <Yair.Shachar@amd.com>
2016-02-10 20:47:59 +0200

    VRAM Memory Eviction: first phase:
    Getting the PDD from the BO.
    Adding  an interface to get mapped size-.
    
    Change-Id: I096c03c4454a83face9dd23595e3ddfa920dedaf
    Signed-off-by: Yair Shachar <Yair.Shachar@amd.com>
    Signed-off-by: Moses Reuben <moses.reuben@amd.com>
    
    Conflicts:
    	drivers/gpu/drm/amd/amdgpu/amdgpu.h
    	drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v8.c
    	drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v8_tonga.c
    	drivers/gpu/drm/amd/amdkfd/kfd_chardev.c
    	drivers/gpu/drm/amd/amdkfd/kfd_module.c
    	drivers/gpu/drm/amd/amdkfd/kfd_process.c
    	drivers/gpu/drm/amd/include/kgd_kfd_interface.h
    	drivers/gpu/drm/radeon/radeon_kfd.c

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -165,6 +165,7 @@ struct amdgpu_cs_parser;
 struct amdgpu_job;
 struct amdgpu_irq_src;
 struct amdgpu_fpriv;
+struct kfd_process_device;
 
 enum amdgpu_cp_irq {
 	AMDGPU_CP_IRQ_GFX_EOP = 0,
@@ -432,6 +433,7 @@ struct amdgpu_bo {
 	struct amdgpu_mn		*mn;
 	struct list_head		mn_list;
 	struct list_head		shadow_list;
+	struct kfd_process_device	*pdd;
 };
 #define gem_to_amdgpu_bo(gobj) container_of((gobj), struct amdgpu_bo, gem_base)
 
