821b9ba862b971ae7039aab9d6cf4b141b13b4c2
Christian König <christian.koenig@amd.com>
2016-02-11 09:56:44 +0100

    drm/amdgpu: nuke the kernel context
    Not used any more.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -976,10 +976,6 @@ struct amdgpu_ctx_mgr {
 	struct idr		ctx_handles;
 };
 
-int amdgpu_ctx_init(struct amdgpu_device *adev, enum amd_sched_priority pri,
-		    struct amdgpu_ctx *ctx);
-void amdgpu_ctx_fini(struct amdgpu_ctx *ctx);
-
 struct amdgpu_ctx *amdgpu_ctx_get(struct amdgpu_fpriv *fpriv, uint32_t id);
 int amdgpu_ctx_put(struct amdgpu_ctx *ctx);
 
@@ -2044,9 +2040,6 @@ struct amdgpu_device {
 
 	/* amdkfd interface */
 	struct kfd_dev          *kfd;
-
-	/* kernel conext for IB submission */
-	struct amdgpu_ctx	kernel_ctx;
 
 	struct amdgpu_virtualization virtualization;
 };
