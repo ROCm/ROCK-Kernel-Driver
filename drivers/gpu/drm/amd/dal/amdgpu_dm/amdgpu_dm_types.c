83a3523e6f5be45d9a976ae12991a96b15d06113
Jordan Lazare <Jordan.Lazare@amd.com>
2016-02-23 12:14:16 -0500

    drm/amd/dal: Remove wait_for_vblanks call on atomic_commit
    It appears to be superfluous, and having the call was causing a performance regression
    
    Signed-off-by: Jordan Lazare <Jordan.Lazare@amd.com>
    Reviewed-by: Tony Cheng <tony.cheng@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/dal/amdgpu_dm/amdgpu_dm_types.c
+++ drivers/gpu/drm/amd/dal/amdgpu_dm/amdgpu_dm_types.c
@@ -2391,8 +2391,6 @@ int amdgpu_dm_atomic_commit(
 				0);
 	}
 
-	drm_atomic_helper_wait_for_vblanks(dev, state);
-
 	/* In this state all old framebuffers would be unpinned */
 
 	drm_atomic_helper_cleanup_planes(dev, state);
