86b74ba6bd43cfb29fac5383043581490b7d3c34
Harry Wentland <harry.wentland@amd.com>
2016-02-27 15:43:04 -0500

    drm/amd/dal: Remove delay_in_nanoseconds
    Change-Id: I96d27123f47a0d81109cfdcd4690afb77720f26b
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/dal/amdgpu_dm/amdgpu_dm_services.c
+++ drivers/gpu/drm/amd/dal/amdgpu_dm/amdgpu_dm_services.c
@@ -48,11 +48,6 @@ void dm_sleep_in_milliseconds(struct dc_context *ctx, uint32_t milliseconds)
 		usleep_range(milliseconds*1000, milliseconds*1000+1);
 }
 
-void dal_delay_in_nanoseconds(uint32_t nanoseconds)
-{
-	ndelay(nanoseconds);
-}
-
 void dm_delay_in_microseconds(struct dc_context *ctx, uint32_t microseconds)
 {
 	udelay(microseconds);
