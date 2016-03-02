5505427f426f742af233f00bb167bf5f69b03dbf
Olaf Hering <ohering@suse.de>
2016-03-02 15:31:03 +0100

    Disable access to lowmem in a Xen PV guest (bnc#964342).
    suse-commit: e7a0da213cba6b5fd022c7b6bab48a800d69ffa8

--- arch/x86/mm/init.c
+++ arch/x86/mm/init.c
@@ -637,7 +637,7 @@ void __init init_mem_mapping(void)
 int devmem_is_allowed(unsigned long pagenr)
 {
 	if (pagenr < 256)
-		return 1;
+		return !xen_pv_domain();
 	if (iomem_is_exclusive(pagenr << PAGE_SHIFT))
 		return 0;
 	if (!page_is_ram(pagenr))
