d18f3ba97551067eb53848caf85738c3e471ce4d
David Ogbeide <Davidboyowa.Ogbeide@amd.com>
2016-03-07 13:37:51 -0500

    drm/amdgpu: implement scalable VM page tables
    GPUVM table and GART sizes are currently
    initialized to default fixed constants.
    
    Sizes should be determined by the amount of physical
    RAM to optimize memory usage on smaller systems and
    prevent mapping constraints on larger systems.
    
    BUG: KFD-224
    
    Signed-off-by: David Ogbeide <davidboyowa.ogbeide@amd.com>
    Change-Id: Ia33d5234c3e1186a0ca0711a173dad308e0c6a30

--- drivers/gpu/drm/amd/amdgpu/gmc_v7_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v7_0.c
@@ -359,6 +359,8 @@ static int gmc_v7_0_mc_init(struct amdgpu_device *adev)
 {
 	u32 tmp;
 	int chansize, numchan;
+	uint64_t gart_size_aligned;
+	struct sysinfo si;
 
 	/* Get VRAM informations */
 	tmp = RREG32(mmMC_ARB_RAMCFG);
@@ -411,11 +413,31 @@ static int gmc_v7_0_mc_init(struct amdgpu_device *adev)
 	if (adev->mc.visible_vram_size > adev->mc.real_vram_size)
 		adev->mc.visible_vram_size = adev->mc.real_vram_size;
 
-	/* unless the user had overridden it, set the gart
-	 * size equal to the 1024 or vram, whichever is larger.
-	 */
-	if (amdgpu_gart_size == -1)
-		adev->mc.gtt_size = max((1024ULL << 20), adev->mc.mc_vram_size);
+	/* Unless the user has overridden it, compute the GART size */
+	if (amdgpu_gart_size == -1) {
+		si_meminfo(&si);
+		/* Set the GART to map the largest size between either
+		 * VRAM capacity or double the available physical RAM,
+		 * but no larger than 512GB.
+		 */
+		adev->mc.gtt_size = min(
+			max(
+				((uint64_t)si.totalram * si.mem_unit * 2),
+				adev->mc.mc_vram_size
+			),
+			1024ULL << 29
+		);
+
+		/* GART sizes computed from physical RAM capacity
+		 * may not always be perfect powers of two.
+		 * Round up starting from the minimum size of 1GB.
+		 */
+		gart_size_aligned = 1024ULL << 20;
+		while (adev->mc.gtt_size > gart_size_aligned)
+			gart_size_aligned <<= 1;
+
+		adev->mc.gtt_size = gart_size_aligned;
+	}
 	else
 		adev->mc.gtt_size = (uint64_t)amdgpu_gart_size << 20;
 
