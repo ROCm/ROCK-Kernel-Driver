34ea164abb19126f79002f908d3f4fbf628f1ffb
Eric Yang <eric.yang2@amd.com>
2016-03-09 17:09:53 -0500

    drm/amd/dal: allocate structures in temp_params separately
    The temp_params structure was very big (117648 bytes) and fails
    to allocate on some systems in suspend/resume. This causes gamma
    to not be programmed and color corruption seen on resume.
    
    Signed-off-by: Eric Yang <eric.yang2@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/dal/dc/inc/gamma_calcs.h
+++ drivers/gpu/drm/amd/dal/dc/inc/gamma_calcs.h
@@ -12,19 +12,7 @@
 #include "core_types.h"
 #include "dc.h"
 
-struct temp_params {
-	struct hw_x_point coordinates_x[256 + 3];
-	struct pwl_float_data rgb_user[FLOAT_GAMMA_RAMP_MAX + 3];
-	struct pwl_float_data_ex rgb_regamma[256 + 3];
-	struct pwl_float_data rgb_oem[FLOAT_GAMMA_RAMP_MAX + 3];
-	struct gamma_pixel axix_x_256[256];
-	struct pixel_gamma_point coeff128_oem[256 + 3];
-	struct pixel_gamma_point coeff128[256 + 3];
-
-};
-
-void calculate_regamma_params(struct pwl_params *params,
-		struct temp_params *temp_params,
+bool calculate_regamma_params(struct pwl_params *params,
 		const struct core_gamma *ramp,
 		const struct core_surface *surface);
 
