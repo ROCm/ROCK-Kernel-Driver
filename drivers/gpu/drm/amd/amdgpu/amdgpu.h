9096116ccee6992a25d8ea5ac72e9a269b3a7689
shaoyunl <Shaoyun.Liu@amd.com>
2016-03-14 15:26:09 -0400

    drm/amdgpu: get_vm_fault implementation on amdgpu side
    amdgpu save the vm fault related information for KFD usage and keep the
    copy until KFD read it.
    
    Change-Id: Ib7a65dfa062e5cdd243beeefc3890409c0bfe90f
    Signed-off-by: shaoyun liu <shaoyun.liu@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -155,6 +155,7 @@ struct amdgpu_job;
 struct amdgpu_irq_src;
 struct amdgpu_fpriv;
 struct kfd_process_device;
+struct kfd_vm_fault_info;
 
 enum amdgpu_cp_irq {
 	AMDGPU_CP_IRQ_GFX_EOP = 0,
@@ -694,6 +695,8 @@ struct amdgpu_mc {
 	uint32_t		vram_type;
 	uint32_t                srbm_soft_reset;
 	struct amdgpu_mode_mc_save save;
+	struct kfd_vm_fault_info *vm_fault_info;
+	atomic_t		vm_fault_info_updated;
 };
 
 /*
