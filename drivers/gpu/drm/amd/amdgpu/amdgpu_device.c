39a7fce2c404dd443c1ebdc0539710b1cfe15254
Alex Deucher <alexander.deucher@amd.com>
2016-03-15 12:28:29 -0400

    drm/amdgpu: remove duplicated error message
    rebasing cruft.
    
    Reviewed-by: Harry Wentland <Harry.Wentland@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -1812,9 +1812,6 @@ int amdgpu_resume_kms(struct drm_device *dev, bool resume, bool fbcon)
 	if (r)
 		DRM_ERROR("amdgpu_resume failed (%d).\n", r);
 
-	if (r)
-		DRM_ERROR("amdgpu_resume failed (%d).\n", r);
-
 	amdgpu_fence_driver_resume(adev);
 
 	if (resume) {
