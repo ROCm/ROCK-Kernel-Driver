8ecac8c33adb039b36b1816598bdb696595afb83
yanyang1 <Young.Yang@amd.com>
2016-04-22 17:14:06 +0800

    drm/amdkcl: [4.5] fix amdgpu framebuffer functions
    Change-Id: I47f6f69585891467aed6eec709f496d8f2e4f965
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>
    
    Conflicts:
          drivers/gpu/drm/amd/amdgpu/amdgpu_display.h

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.h
@@ -42,6 +42,11 @@ uint32_t amdgpu_display_supported_domains(struct amdgpu_device *adev);
 struct drm_framebuffer *
 amdgpu_display_user_framebuffer_create(struct drm_device *dev,
 				       struct drm_file *file_priv,
+#if DRM_VERSION_CODE < DRM_VERSION(4, 5, 0)
+				       struct drm_mode_fb_cmd2 *mode_cmd)
+#else
 				       const struct drm_mode_fb_cmd2 *mode_cmd);
+#endif
+void amdgpu_output_poll_changed(struct drm_device *dev);
 
 #endif
