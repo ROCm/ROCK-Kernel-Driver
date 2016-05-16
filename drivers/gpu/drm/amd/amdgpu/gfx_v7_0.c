f6e5cf609ca4647021fefa39bd803729558ab96b
Lan Xiao <Lan.Xiao@amd.com>
2016-05-16 15:59:53 -0400

    drm/amdgpu: Implement get_cu_info for Hawaii
    fix the problem LDS segment is 0
    
    BUG:KFD-240
    Change-Id: I94b3b982851360ee5b3790e6e155f6d0ae3a64dc
    Signed-off-by: Lan Xiao <Lan.Xiao@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
@@ -5383,11 +5383,18 @@ static void gfx_v7_0_get_cu_info(struct amdgpu_device *adev)
 	cu_info->number = active_cu_number;
 	cu_info->ao_cu_mask = ao_cu_mask;
 	cu_info->simd_per_cu = NUM_SIMD_PER_CU;
-	if (adev->asic_type == CHIP_KAVERI) {
+	switch (adev->asic_type) {
+	case CHIP_KAVERI:
+	case CHIP_HAWAII:
 		cu_info->max_waves_per_simd = 10;
 		cu_info->max_scratch_slots_per_cu = 32;
 		cu_info->wave_front_size = 64;
 		cu_info->lds_size = 64;
+		break;
+	default:
+		dev_warn(adev->dev, "CU info asic_type [0x%x] not supported\n",
+				adev->asic_type);
+		break;
 	}
 }
 
