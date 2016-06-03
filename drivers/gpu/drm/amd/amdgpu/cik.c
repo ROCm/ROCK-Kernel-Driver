d76d3c3ec7b27371855598d4ed281920a2e0c0cf
Andres Rodriguez <andres.rodriguez@amd.com>
2016-06-03 16:57:23 -0400

    amdgpu: fix asic initialization for virtualized environments
    When executing in a PCI passthrough based virtualization environment, the
    hypervisor will usually attempt to send a PCIe bus reset signal to the
    ASIC when the VM reboots. In this scenario, the card is not correctly
    initialized, but we still consider it to be posted. Therefore, in a
    passthrough based environment we should always post the card to guarantee
    that it is in a good state for driver initialization.
    
    However, if we are operating in SR-IOV mode it is up to the GIM driver
    to manage the asic state, therefore we should not post the card (and
    shouldn't be able to do it either).
    
    Signed-off-by: Andres Rodriguez <andres.rodriguez@amd.com>
    Change-Id: I08d8c5a59f474d904ce963a3403a978baeba3848

--- drivers/gpu/drm/amd/amdgpu/cik.c
+++ drivers/gpu/drm/amd/amdgpu/cik.c
@@ -963,6 +963,12 @@ static bool cik_read_bios_from_rom(struct amdgpu_device *adev,
 	return true;
 }
 
+static u32 cik_get_virtual_caps(struct amdgpu_device *adev)
+{
+	/* CIK does not support SR-IOV */
+	return 0;
+}
+
 static const struct amdgpu_allowed_register_entry cik_allowed_read_registers[] = {
 	{mmGRBM_STATUS, false},
 	{mmGB_ADDR_CONFIG, false},
@@ -2168,6 +2174,7 @@ static const struct amdgpu_asic_funcs cik_asic_funcs =
 	.get_xclk = &cik_get_xclk,
 	.set_uvd_clocks = &cik_set_uvd_clocks,
 	.set_vce_clocks = &cik_set_vce_clocks,
+	.get_virtual_caps = &cik_get_virtual_caps,
 	.get_cu_info = &gfx_v7_0_get_cu_info,
 	/* these should be moved to their own ip modules */
 	.get_gpu_clock_counter = &gfx_v7_0_get_gpu_clock_counter,
