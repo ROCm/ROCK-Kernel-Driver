bbe2c0cc5a5d4e582463d6f03b4ad7c082f3cb5d
Rex Zhu <Rex.Zhu@amd.com>
2016-06-16 15:49:50 +0800

    drm/amdgpu: fix flickering on screen on bonaire with DAL enable.
    when mode change, dpm didn't catch the change of crtc's enabled state
    As dal notify dpm when crtc in state (not active and enabled)  and in state
    (active and enabled). So in dpm, check the amdgpu_crtc's enabled state.
    
    Signed-off-by: Rex Zhu <Rex.Zhu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c
@@ -1060,7 +1060,6 @@ force:
 		printk("switching to power state:\n");
 		amdgpu_dpm_print_power_state(adev, adev->pm.dpm.requested_ps);
 	}
-
 	/* update whether vce is active */
 	ps->vce_active = adev->pm.dpm.vce_active;
 
@@ -1312,7 +1311,7 @@ void amdgpu_pm_compute_clocks(struct amdgpu_device *adev)
 			list_for_each_entry(crtc,
 					    &ddev->mode_config.crtc_list, head) {
 				amdgpu_crtc = to_amdgpu_crtc(crtc);
-				if (crtc->enabled) {
+				if (amdgpu_crtc->enabled) {
 					adev->pm.dpm.new_active_crtcs |= (1 << amdgpu_crtc->crtc_id);
 					adev->pm.dpm.new_active_crtc_count++;
 				}
