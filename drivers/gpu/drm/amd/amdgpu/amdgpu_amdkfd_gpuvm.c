c93bf9483b016ec0363e62ded30827df1e058b28
Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>
2016-06-27 12:20:18 -0400

    drm/amdgpu: Introduce amdkfd_vm structure
    For KFD process use amdkfd_vm instead of amdgpu_vm but keep pointer
    compatability between these two structures.
    
    struct amdkfd_vm will aid KGD in keeping track of all KFD BOs and VMs
    belonging to a KFD process. This is useful for KFD memory eviction and
    restore since KFD eviction / restore is done per process.
    
    Change-Id: I83383a183c7f0c6b95d46c72b37dbaaaaa1e93f8
    Signed-off-by: Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -27,7 +27,6 @@
 #include <linux/list.h>
 #include <drm/drmP.h>
 #include <linux/dma-buf.h>
-#include "amdgpu.h"
 #include "amdgpu_amdkfd.h"
 #include "amdgpu_ucode.h"
 #include "gca/gfx_8_0_sh_mask.h"
@@ -1038,14 +1037,14 @@ bo_reserve_failed:
 int amdgpu_amdkfd_gpuvm_create_process_vm(struct kgd_dev *kgd, void **vm)
 {
 	int ret;
-	struct amdgpu_vm *new_vm;
+	struct amdkfd_vm *new_vm;
 	struct amdgpu_bo *pd;
 	struct amdgpu_device *adev = get_amdgpu_device(kgd);
 
 	BUG_ON(kgd == NULL);
 	BUG_ON(vm == NULL);
 
-	new_vm = kzalloc(sizeof(struct amdgpu_vm), GFP_KERNEL);
+	new_vm = kzalloc(sizeof(struct amdkfd_vm), GFP_KERNEL);
 	if (new_vm == NULL)
 		return -ENOMEM;
 
@@ -1054,11 +1053,14 @@ int amdgpu_amdkfd_gpuvm_create_process_vm(struct kgd_dev *kgd, void **vm)
 	if (ret != 0) {
 		pr_err("amdgpu: failed init vm ret %d\n", ret);
 		/* Undo everything related to the new VM context */
-		amdgpu_vm_fini(adev, new_vm);
+		amdgpu_vm_fini(adev, &new_vm->base);
 		kfree(new_vm);
 		new_vm = NULL;
 	}
-
+	new_vm->adev = adev;
+	mutex_init(&new_vm->lock);
+	INIT_LIST_HEAD(&new_vm->kfd_bo_list);
+	INIT_LIST_HEAD(&new_vm->kfd_vm_list);
 	*vm = (void *) new_vm;
 
 	/*
@@ -1066,12 +1068,12 @@ int amdgpu_amdkfd_gpuvm_create_process_vm(struct kgd_dev *kgd, void **vm)
 	 * removed from PT. This function is called here because it requires
 	 * the radeon_vm::mutex to be locked and PT to be reserved
 	 */
-	ret = amdgpu_vm_clear_freed(adev, new_vm);
+	ret = amdgpu_vm_clear_freed(adev, &new_vm->base);
 	if (ret != 0)
 		pr_err("amdgpu: Failed to amdgpu_vm_clear_freed\n");
 
 	/* Pin the PD directory */
-	pd = new_vm->page_directory;
+	pd = new_vm->base.page_directory;
 	amdgpu_bo_reserve(pd, true);
 	ret = try_pin_bo(pd, AMDGPU_GEM_DOMAIN_VRAM);
 	amdgpu_bo_unreserve(pd);
@@ -1079,7 +1081,7 @@ int amdgpu_amdkfd_gpuvm_create_process_vm(struct kgd_dev *kgd, void **vm)
 		pr_err("amdkfd: Failed to pin PD\n");
 
 	pr_debug("amdgpu: created process vm with address 0x%llx\n",
-			amdgpu_bo_gpu_offset(new_vm->page_directory));
+			amdgpu_bo_gpu_offset(new_vm->base.page_directory));
 
 	return ret;
 }
