694a93c07f6a1c3a2af6ec72777080b18e3727ea
Harish Kasiviswanathan <hkasivis@fnode2-6.pub.pic2.ibm.com>
2016-06-30 10:56:52 -0400

    drm/amdgpu: Fix compile error for power arch
    Change-Id: Iaebf56d01521faa56eabdfe609240169b91d1737
    Signed-off-by: Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -30,6 +30,7 @@
 #include <linux/sched/mm.h>
 #include <drm/drmP.h>
 #include <linux/dma-buf.h>
+#include <linux/pagemap.h>
 #include "amdgpu_amdkfd.h"
 #include "amdgpu_ucode.h"
 #include "gca/gfx_8_0_sh_mask.h"
