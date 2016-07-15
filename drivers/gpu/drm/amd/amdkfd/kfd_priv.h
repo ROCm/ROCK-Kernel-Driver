290c6c300897f731876dfdbadfdc76d14a6a9ad9
Yong Zhao <yong.zhao@amd.com>
2016-07-15 18:05:59 -0400

    drm/amdkfd: Simplify kfd_map_memory_to_gpu function
    Change-Id: I3b64feb67c30f4e58bce3ced782c5838128088f4
    Signed-off-by: Yong Zhao <yong.zhao@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_priv.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_priv.h
@@ -717,8 +717,7 @@ void run_rdma_free_callback(struct kfd_bo *buf_obj);
 struct kfd_process *kfd_lookup_process_by_pid(struct pid *pid);
 
 /* kfd dgpu memory */
-int kfd_map_memory_to_gpu(struct kfd_dev *dev, void *mem,
-		struct kfd_process *p, struct kfd_process_device *pdd);
+int kfd_map_memory_to_gpu(void *mem, struct kfd_process_device *pdd);
 
 /* Process device data iterator */
 struct kfd_process_device *kfd_get_first_process_device_data(struct kfd_process *p);
