9232dd74ec9939a4ad622a73117668a02ffdec7f
Amber Lin <Amber.Lin@amd.com>
2016-07-22 14:56:10 -0400

    drm/amdgpu: Get user pages in non-current task
    get_user_pages is changed to be called from current task where mm is being
    operated on. get_user_pages_remote is introduced when the target mm is not
    the current task's. Adjust amdgpu_ttm_tt_get_user_pages so it'll get user
    pages in both cases.
    
    Change-Id: If0e306c107c3663e2eb0d900009021265e97e04d
    Signed-off-by: Amber Lin <Amber.Lin@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -578,7 +578,13 @@ int amdgpu_ttm_tt_get_user_pages(struct ttm_tt *ttm, struct page **pages)
 		list_add(&guptask.list, &gtt->guptasks);
 		spin_unlock(&gtt->guptasklock);
 
-		r = get_user_pages(userptr, num_pages, write, 0, p, NULL);
+		if (gtt->usertask->mm == current->mm)
+			r = get_user_pages(userptr, num_pages, write, 0, p,
+					NULL);
+		else
+			r = get_user_pages_remote(gtt->usertask,
+					gtt->usertask->mm, userptr, num_pages,
+					write, 0, p, NULL);
 
 		spin_lock(&gtt->guptasklock);
 		list_del(&guptask.list);
