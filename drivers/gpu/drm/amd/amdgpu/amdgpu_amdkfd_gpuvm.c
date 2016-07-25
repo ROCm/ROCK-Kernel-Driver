ef56c3c7fcb3de7eb1c20025f02cb714f3883fdd
Amber Lin <Amber.Lin@amd.com>
2016-07-25 10:09:20 -0400

    drm/amdgpu: Polaris10/11 support in KFD interface
    Add Polaris10 and Polaris11 to amdgpu-amdkfd interface
    
    Change-Id: I464618fd352af44880333ec472e18483387584e1
    Signed-off-by: Amber Lin <Amber.Lin@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -319,7 +319,10 @@ static int __alloc_memory_of_gpu(struct kgd_dev *kgd, uint64_t va,
 
 	adev = get_amdgpu_device(kgd);
 	byte_align = (adev->family == AMDGPU_FAMILY_VI &&
-		      adev->asic_type != CHIP_FIJI) ? VI_BO_SIZE_ALIGN : 1;
+			adev->asic_type != CHIP_FIJI &&
+			adev->asic_type != CHIP_POLARIS10 &&
+			adev->asic_type != CHIP_POLARIS11) ?
+			VI_BO_SIZE_ALIGN : 1;
 
 	*mem = kzalloc(sizeof(struct kgd_mem), GFP_KERNEL);
 	if (*mem == NULL) {
