d7ae06a191278c4490f09a3f92ecb63efedadc56
annwang <Annie.Wang@amd.com>
2016-08-23 20:25:13 +0800

    drm/amd/amdkcl: [4.7] fix down_write() in amdgpu_mn_get()
    Change-Id: I8e3cec1a9a6ffde5b29ded930b2b58713897faa9
    Signed-off-by: Hongcheng Wang <Annie.Wang@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_mn.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_mn.c
@@ -232,10 +232,14 @@ static struct amdgpu_mn *amdgpu_mn_get(struct amdgpu_device *adev)
 	int r;
 
 	mutex_lock(&adev->mn_lock);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 7, 0)
+	down_write(&mm->mmap_sem);
+#else
 	if (down_write_killable(&mm->mmap_sem)) {
 		mutex_unlock(&adev->mn_lock);
 		return ERR_PTR(-EINTR);
 	}
+#endif
 
 	hash_for_each_possible(adev->mn_hash, rmn, node, (unsigned long)mm)
 		if (rmn->mm == mm)
