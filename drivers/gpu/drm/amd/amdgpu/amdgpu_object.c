46b12ba025f9a107c71a8078e4d70a11a0a8f859
Flora Cui <Flora.Cui@amd.com>
2016-09-19 17:26:27 +0800

    drm/amdgpu: unbind gart when release direct gma imported bo
    Change-Id: Ie8db112202ad2b11a5ec1294dc9b4163eaa0a542
    Signed-off-by: Flora Cui <Flora.Cui@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    
    Conflicts:
    	drivers/gpu/drm/amd/amdgpu/amdgpu_object.c

--- drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
@@ -102,9 +102,15 @@ static void amdgpu_ttm_bo_destroy(struct ttm_buffer_object *tbo)
 {
 	struct amdgpu_device *adev = amdgpu_ttm_adev(tbo->bdev);
 	struct amdgpu_bo *bo;
+	u64 offset;
 
 	bo = container_of(tbo, struct amdgpu_bo, tbo);
 
+	if (bo->tbo.mem.mem_type == AMDGPU_PL_DGMA_IMPORT) {
+		offset = amdgpu_bo_gpu_offset(bo);
+		offset -= adev->mman.bdev.man[TTM_PL_TT].gpu_offset;
+		amdgpu_gart_unbind(adev, offset, bo->tbo.num_pages);
+	}
 	amdgpu_update_memory_usage(adev, &bo->tbo.mem, NULL);
 
 	drm_gem_object_release(&bo->gem_base);
