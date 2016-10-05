936c85085e489a70f0c9d554000aec7e67a51777
Michal Kubecek <mkubecek@suse.cz>
2016-10-05 15:04:10 +0200

    net: add recursion limit to GRO (CVE-2016-7039 bsc#1001486).
    suse-commit: 0a6dcd2887f5902e12f72cce67b660953f75196e

--- net/ipv4/udp_offload.c
+++ net/ipv4/udp_offload.c
@@ -293,7 +293,13 @@ unflush:
 
 	skb_gro_pull(skb, sizeof(struct udphdr)); /* pull encapsulating udp header */
 	skb_gro_postpull_rcsum(skb, uh, sizeof(struct udphdr));
-	pp = udp_sk(sk)->gro_receive(sk, head, skb);
+
+	if (gro_recursion_inc_test(skb)) {
+		flush = 1;
+		pp = NULL;
+	} else {
+		pp = udp_sk(sk)->gro_receive(sk, head, skb);
+	}
 
 out_unlock:
 	rcu_read_unlock();
