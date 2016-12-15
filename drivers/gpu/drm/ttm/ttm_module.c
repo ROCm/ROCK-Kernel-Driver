9a402c686d0bd795a09e62afa808660839a2ea8f
Qiang Yu <Qiang.Yu@amd.com>
2016-12-15 11:52:56 +0800

    drm/amdkcl[ttm]: rename the device name to amdttm
    Change-Id: I5db8b0bdaffde9f0c516684093d8eed3c3720895
    Signed-off-by: Qiang Yu <Qiang.Yu@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/ttm/ttm_module.c
+++ drivers/gpu/drm/ttm/ttm_module.c
@@ -39,7 +39,7 @@ static DECLARE_WAIT_QUEUE_HEAD(exit_q);
 static atomic_t device_released;
 
 static struct device_type ttm_drm_class_type = {
-	.name = "ttm",
+	.name = "amdttm",
 	/**
 	 * Add pm ops here.
 	 */
@@ -67,7 +67,7 @@ static int __init ttm_init(void)
 {
 	int ret;
 
-	ret = dev_set_name(&ttm_drm_class_device, "ttm");
+	ret = dev_set_name(&ttm_drm_class_device, "amdttm");
 	if (unlikely(ret != 0))
 		return ret;
 
