e0f21823094412f027035b903233082390493859
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-23 17:42:52 +0800

    drm/amdkcl: [4.6] fix kcl_get_user_pages()
    Change-Id: I9042cf3e16f03f97a1ce2a11773c4dfb7fa5a9b2
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>
    
    Conflicts:
          drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c

---
+++ include/kcl/kcl_mm.h
@@ -0,0 +1,69 @@
+#ifndef AMDKCL_MM_H
+#define AMDKCL_MM_H
+
+#include <linux/mm.h>
+
+static inline int kcl_get_user_pages(struct task_struct *tsk, struct mm_struct *mm,
+				unsigned long start, unsigned long nr_pages,
+				int write, int force, struct page **pages,
+				struct vm_area_struct **vmas, int *locked)
+{
+#if	LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0) || \
+	defined(OS_NAME_SLE_12_3) || \
+	defined(OS_NAME_SUSE_42_3) || \
+	defined(OS_NAME_RHEL_7_4_5)
+	if (mm == current->mm)
+		return get_user_pages(start, nr_pages, write, pages, vmas);
+	else
+#if	LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0) || \
+	defined(OS_NAME_SLE_12_3) || \
+	defined(OS_NAME_SUSE_42_3) || \
+	defined(OS_NAME_RHEL_7_4_5)
+		return get_user_pages_remote(tsk, mm, start, nr_pages,
+				write, pages, vmas, locked);
+#else
+		return get_user_pages_remote(tsk, mm, start, nr_pages,
+				write, pages, vmas);
+#endif
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0) || \
+    defined(OS_NAME_SLE_12_3) || \
+    defined(OS_NAME_SUSE_42_3)
+	if (mm == current->mm)
+		return get_user_pages(start, nr_pages, write, force, pages,
+				vmas);
+	else
+		return get_user_pages_remote(tsk, mm, start, nr_pages,
+				write, force, pages, vmas);
+#else
+	write = !!(write & FOLL_WRITE);
+	return get_user_pages(tsk, mm, start, nr_pages,
+			write, force, pages, vmas);
+#endif
+}
+
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+static inline unsigned int memalloc_nofs_save(void)
+{
+		return current->flags;
+}
+
+static inline void memalloc_nofs_restore(unsigned int flags)
+{
+}
+
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+#define kvzalloc kzalloc
+#define kvfree kfree
+#define kvmalloc kzalloc
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 8, 0)
+#define GFP_TRANSHUGE_LIGHT	((GFP_HIGHUSER_MOVABLE | __GFP_COMP | \
+			 __GFP_NOMEMALLOC | __GFP_NOWARN) & ~__GFP_RECLAIM)
+#endif
+
+
+#endif /* AMDKCL_MM_H */
