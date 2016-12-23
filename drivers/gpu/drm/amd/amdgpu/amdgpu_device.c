e65330e51e242c2f67587a4c56bd159949843f35
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-23 17:49:54 +0800

    drm/amdkcl: [4.4] fix vga_switcheroo_register_handler()
    Change-Id: Iff4e3628c93842f53c06af42b641f8017c5a3e07
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2459,7 +2459,7 @@ int amdgpu_device_init(struct amdgpu_device *adev,
 	if (amdgpu_device_is_px(ddev))
 		runtime = true;
 	if (!pci_is_thunderbolt_attached(adev->pdev))
-		vga_switcheroo_register_client(adev->pdev,
+		kcl_vga_switcheroo_register_client(adev->pdev,
 					       &amdgpu_switcheroo_ops, runtime);
 	if (runtime)
 		vga_switcheroo_init_domain_pm_ops(adev->dev, &adev->vga_pm_domain);
