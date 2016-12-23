fb976158af7d7f6284b0806cb1cee41c18a197eb
Hongbo.He <Hongbo.He@amd.com>
2016-12-23 14:48:10 +0800

    drm/amd/amdkcl: [RHEL 7.2] fix mmu_notifier_unregister_no_release conflict
    Change-Id: Ibb1b3dc5e608308cf087759e0971a06f9768596f
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>
    Signed-off-by: Hongbo.He <Hongbo.He@amd.com>
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_mn.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_mn.c
@@ -3,7 +3,7 @@
 #include <kcl/kcl_mn.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 17, 0) && \
-	!defined(OS_NAME_RHEL_7_3)
+	!defined(OS_NAME_RHEL_7_3) && !defined(OS_NAME_RHEL_7_2)
 void mmu_notifier_unregister_no_release(struct mmu_notifier *mn,
 					struct mm_struct *mm)
 {
