718b088e095702acb4d07f56f8144a15c48e7d04
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-23 18:32:20 +0800

    drm/amdkcl: Test whether kthread_{park/unpark/parkme/should_park}() is available
    History:
    Introduced by kernel v3.7-rc1~37^2~2^2~1^2~8
    Exported by kernel v4.2-rc6~15^2~9
    
    v2: drm/amdkcl: fix kthread functions
    v3: drm/amdkcl: Test whether kthread_{park/unpark/parkme/should_park}()
    is available (v2)
    v4: drm/amdkcl: drop kcl_kthread_xxx
    v5: drm/amdkcl: update test for kthread_xxx
    v6: drm/amdkcl: fix license for kcl part
    
    Change-Id: I4dfe3fe981c7d4f5f60ae0a862b2c3c52bf3fca9
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

---
+++ include/kcl/kcl_kthread.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef AMDKCL_KTHREAD_H
+#define AMDKCL_KTHREAD_H
+
+#include <linux/sched.h>
+#include <linux/kthread.h>
+
+#if !defined(HAVE_KTHREAD_PARK_XX)
+extern void (*_kcl_kthread_parkme)(void);
+extern void (*_kcl_kthread_unpark)(struct task_struct *k);
+extern int (*_kcl_kthread_park)(struct task_struct *k);
+extern bool (*_kcl_kthread_should_park)(void);
+#endif
+#endif /* AMDKCL_KTHREAD_H */
