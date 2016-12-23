fb1f299d7d929d5069bbcdff59df002a6e917307
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-23 18:32:20 +0800

    drm/amdkcl: Test whether kthread_{park/unpark/parkme/should_park}() is available
    History:
    Introduced by kernel v3.7-rc1~37^2~2^2~1^2~8
    Exported by kernel v4.2-rc6~15^2~9
    
    v2: drm/amdkcl: fix kthread functions
    v3: drm/amdkcl: Test whether kthread_{park/unpark/parkme/should_park}()
    is available (v2)
    v4: drm/amdkcl: drop kcl_kthread_xxx
    v5: drm/amdkcl: update test for kthread_xxx
    v6: drm/amdkcl: fix license for kcl part
    
    Change-Id: I4dfe3fe981c7d4f5f60ae0a862b2c3c52bf3fca9
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/kcl_kthread.c
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Kernel thread helper functions.
+ *   Copyright (C) 2004 IBM Corporation, Rusty Russell.
+ *   Copyright (C) 2009 Red Hat, Inc.
+ *
+ * Creation is done via kthreadd, so that we get a clean environment
+ * even if we're invoked from userspace (think modprobe, hotplug cpu,
+ * etc.).
+ */
+
+/*
+* FIXME: implement below API when kernel version < 4.2
+*/
+#include <linux/printk.h>
+#include <linux/version.h>
+#include <kcl/kcl_kthread.h>
+#include "kcl_common.h"
+
+#if !defined(HAVE_KTHREAD_PARK_XX)
+bool (*_kcl_kthread_should_park)(void);
+EXPORT_SYMBOL(_kcl_kthread_should_park);
+
+void (*_kcl_kthread_parkme)(void);
+EXPORT_SYMBOL(_kcl_kthread_parkme);
+
+void (*_kcl_kthread_unpark)(struct task_struct *k);
+EXPORT_SYMBOL(_kcl_kthread_unpark);
+
+int (*_kcl_kthread_park)(struct task_struct *k);
+EXPORT_SYMBOL(_kcl_kthread_park);
+
+static bool _kcl_kthread_should_park_stub(void)
+{
+	printk_once(KERN_WARNING "This kernel version not support API: kthread_should_park!\n");
+	return false;
+}
+
+static void _kcl_kthread_parkme_stub(void)
+{
+	printk_once(KERN_WARNING "This kernel version not support API: kthread_parkme!\n");
+}
+
+static void _kcl_kthread_unpark_stub(struct task_struct *k)
+{
+	printk_once(KERN_WARNING "This kernel version not support API: kthread_unpark!\n");
+}
+
+static int _kcl_kthread_park_stub(struct task_struct *k)
+{
+	printk_once(KERN_WARNING "This kernel version not support API: kthread_park!\n");
+	return 0;
+}
+#endif
+
+void amdkcl_kthread_init(void)
+{
+#if !defined(HAVE_KTHREAD_PARK_XX)
+	_kcl_kthread_should_park = amdkcl_fp_setup("kthread_should_park",
+						_kcl_kthread_should_park_stub);
+	_kcl_kthread_parkme = amdkcl_fp_setup("kthread_parkme",
+						_kcl_kthread_parkme_stub);
+	_kcl_kthread_unpark = amdkcl_fp_setup("kthread_unpark",
+						_kcl_kthread_unpark_stub);
+	_kcl_kthread_park = amdkcl_fp_setup("kthread_park",
+						_kcl_kthread_park_stub);
+#endif
+}
