60e042fed439f125dec920e12e468903563625bf
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-23 15:10:08 +0800

    drm/amd/amdkcl: [4.4] update display functions
    Change-Id: Ia2245ce0ec29adf98de9cab3c77abde748d973fd
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -814,7 +814,11 @@ int amdgpu_get_crtc_scanoutpos(struct drm_device *dev, unsigned int pipe,
 	}
 	else {
 		/* No: Fake something reasonable which gives at least ok results. */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0)
+		vbl_start = adev->mode_info.crtcs[pipe]->base.hwmode.crtc_vdisplay;
+#else
 		vbl_start = mode->crtc_vdisplay;
+#endif
 		vbl_end = 0;
 	}
 
@@ -860,7 +864,11 @@ int amdgpu_get_crtc_scanoutpos(struct drm_device *dev, unsigned int pipe,
 
 	/* Inside "upper part" of vblank area? Apply corrective offset if so: */
 	if (in_vbl && (*vpos >= vbl_start)) {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0)
+		vtotal = adev->mode_info.crtcs[pipe]->base.hwmode.crtc_vtotal;
+#else
 		vtotal = mode->crtc_vtotal;
+#endif
 		*vpos = *vpos - vtotal;
 	}
 
