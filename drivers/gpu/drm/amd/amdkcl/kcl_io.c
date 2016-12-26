f8e60cf252e6a26bc49048fbc6055ff379410979
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-26 13:42:16 +0800

    drm/amdkcl: [4.9] add x86 io memtype_wc functions
    Change-Id: Ia197a439a01edf261a6187e0a99831cc19254e87
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/kcl_io.c
@@ -0,0 +1,45 @@
+#include <linux/module.h>
+#include <kcl/kcl_io.h>
+#include "kcl_common.h"
+
+#if DRM_VERSION_CODE < DRM_VERSION(4, 9, 0) && \
+	defined(CONFIG_X86_PAT)
+#include <asm/pgtable_types.h>
+
+static int (*_kcl_io_reserve_memtype)(resource_size_t start, resource_size_t end,
+			enum page_cache_mode *type);
+static void (*_kcl_io_free_memtype)(resource_size_t start, resource_size_t end);
+
+int arch_io_reserve_memtype_wc(resource_size_t start, resource_size_t size)
+{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
+	unsigned long type = _PAGE_CACHE_WC;
+#else
+	enum page_cache_mode type = _PAGE_CACHE_MODE_WC;
+#endif
+
+	return _kcl_io_reserve_memtype(start, start + size, &type);
+}
+#if !defined(OS_NAME_SLE_12_3) && !defined(OS_NAME_SUSE_42_3)
+EXPORT_SYMBOL(arch_io_reserve_memtype_wc);
+#endif
+
+void arch_io_free_memtype_wc(resource_size_t start, resource_size_t size)
+{
+	_kcl_io_free_memtype(start, start + size);
+}
+#if !defined(OS_NAME_SLE_12_3) && !defined(OS_NAME_SUSE_42_3)
+EXPORT_SYMBOL(arch_io_free_memtype_wc);
+#endif
+
+void amdkcl_io_init(void)
+{
+	_kcl_io_reserve_memtype = amdkcl_fp_setup("io_reserve_memtype", NULL);
+	_kcl_io_free_memtype = amdkcl_fp_setup("io_free_memtype", NULL);
+}
+#else
+void amdkcl_io_init(void)
+{
+
+}
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0) */
