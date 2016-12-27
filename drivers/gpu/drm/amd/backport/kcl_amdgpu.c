b6bdf8c92d4432e4c418c16ff6760a8109dcd76b
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-27 13:31:13 +0800

    drm/amd/backport: [RHEL 6.8] fix amdgpu functions
    Change-Id: I2bff346494327fa7b24e25c548c36692db8f83fa
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>

--- drivers/gpu/drm/amd/backport/kcl_amdgpu.c
+++ drivers/gpu/drm/amd/backport/kcl_amdgpu.c
@@ -1,6 +1,7 @@
 #include <kcl/kcl_amdgpu.h>
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0) || \
+		defined(OS_NAME_RHEL_6)
 u32 kcl_amdgpu_get_vblank_counter_kms(struct drm_device *dev, unsigned int crtc)
 {
 	return amdgpu_get_vblank_counter_kms(dev, crtc);
