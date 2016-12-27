fa9eb92e03908e6ffecf5d2618375d9c1f54396f
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-27 09:50:00 +0800

    drm/amd/amdkcl: [RHEL 6.8] fix UVD unit test fail
    This is due to the RHEL6.8's buggy implementation of
    reservation_object_test_signaled_single
    
    The UVD unit test on CIK card will move BO from GTT
    to VRAM, then parse the data. But the RHEL6.8's
    reservation_object_test_signaled_single
    always return true even the move fence has not been
    finished which causes the following parse fail.
    
    The move is not needed on VI card, so the UVD unit
    test fail is not aboserved.
    
    This commit should be reverted after the following
    bugzilla is fixed on the RHEL side.
    
    Change-Id: I26ace5cfa52aed3298365b1a5beff8a315ccd253
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1398084
    Signed-off-by: Qiang Yu <Qiang.Yu@amd.com>
    Reviewed-by: Hongbo.He <Hongbo.He@amd.com>
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    
     Conflicts:
    	drivers/gpu/drm/amd/amdkcl/Makefile
    	drivers/gpu/drm/amd/amdkcl/kcl_reservation.c
    	drivers/gpu/drm/amd/backport/backport.h
    	drivers/gpu/drm/ttm/backport/backport.h
    	include/kcl/kcl_reservation.h

--- include/kcl/kcl_reservation.h
+++ include/kcl/kcl_reservation.h
@@ -96,4 +96,20 @@ kcl_reservation_object_trylock(struct reservation_object *obj)
 #endif
 }
 
+#ifdef OS_NAME_RHEL_6
+bool _kcl_reservation_object_test_signaled_rcu(struct reservation_object *obj,
+					       bool test_all);
+#endif
+
+static inline bool
+kcl_reservation_object_test_signaled_rcu(struct reservation_object *obj,
+					 bool test_all)
+{
+#ifdef OS_NAME_RHEL_6
+	return _kcl_reservation_object_test_signaled_rcu(obj, test_all);
+#else
+	return reservation_object_test_signaled_rcu(obj, test_all);
+#endif
+}
+
 #endif /* AMDKCL_RESERVATION_H */
