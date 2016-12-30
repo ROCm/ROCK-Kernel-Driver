590fb3df0c9773befd7d7595fc489eecca5b7e28
Junwei Zhang <Jerry.Zhang@amd.com>
2016-12-30 10:09:10 +0800

    drm/amd/amdkcl: [RHEL 6.8] fix arch_io_reserve_memtype_wc()
    Change-Id: I10a3f0e37f253a927ae8c2cc3739d9e63c8c2d3a
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_io.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_io.c
@@ -9,7 +9,11 @@ static void (*_kcl_io_free_memtype)(resource_size_t start, resource_size_t end);
 
 int arch_io_reserve_memtype_wc(resource_size_t start, resource_size_t size)
 {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
+	unsigned long type = _PAGE_CACHE_WC;
+#else
 	enum page_cache_mode type = _PAGE_CACHE_MODE_WC;
+#endif
 
 	return _kcl_io_reserve_memtype(start, start + size, &type);
 }
