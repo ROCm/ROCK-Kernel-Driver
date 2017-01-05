483e999854180eec452a54453ff52d7a34ae07c4
Andrey Grodzovsky <Andrey.Grodzovsky@amd.com>
2017-01-05 11:24:31 -0500

    drm/amd/display: Fix vblank IRQ refcount in DM.
    Temporary fix follwoing the introduction of page_flip_target
    hook in drm.
    
    Change-Id: Iebb736fd861358cbf1ed4b0b37134dae1fd68c63
    Signed-off-by: Andrey Grodzovsky <Andrey.Grodzovsky@amd.com>
    Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.c
@@ -1095,6 +1095,10 @@ static int amdgpu_atomic_helper_page_flip(struct drm_crtc *crtc,
 	if (!state)
 		return -ENOMEM;
 
+	ret = drm_crtc_vblank_get(crtc);
+	if (ret)
+		return ret;
+
 	state->acquire_ctx = drm_modeset_legacy_acquire_ctx(crtc);
 retry:
 	crtc_state = drm_atomic_get_crtc_state(state, crtc);
@@ -1137,6 +1141,9 @@ retry:
 fail:
 	if (ret == -EDEADLK)
 		goto backoff;
+
+	if (ret)
+		drm_crtc_vblank_put(crtc);
 
 	drm_atomic_state_free(state);
 
