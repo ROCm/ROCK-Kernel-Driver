4354edc950d119b40dbf7660bb140793d7ce3ac5
Jay Cornwall <Jay.Cornwall@amd.com>
2017-01-17 12:08:50 -0600

    drm/amdkfd: Use volatile MTYPE in default/alternate apertures
    MTYPE_NC_NV (0) marks scalar/vector L1 cache lines as non-volatile.
    Cache lines loaded through these apertures are intended to be
    invalidated before (and sometimes during) a dispatch. The non-volatile
    qualifier prevents these cache lines from being distinguished from
    those loaded through the private aperture.
    
    Use MTYPE_NC (1) instead on both Gfx7 and Gfx8. This allows the
    compiler to use the BUFFER_WBINVL1_VOL instruction and is a precursor
    to automatic per-dispatch scalar/vector L1 volatile invalidation.
    
    Change-Id: Ic366945bfa50034935adf42009ea70cb1fce99c3
    Signed-off-by: Jay Cornwall <Jay.Cornwall@amd.com>

--- drivers/gpu/drm/amd/amdkfd/cik_regs.h
+++ drivers/gpu/drm/amd/amdkfd/cik_regs.h
@@ -55,7 +55,8 @@
 #define	APE1_MTYPE(x)					((x) << 7)
 
 /* valid for both DEFAULT_MTYPE and APE1_MTYPE */
-#define	MTYPE_CACHED					0
+#define	MTYPE_CACHED_NV					0
+#define	MTYPE_CACHED					1
 #define	MTYPE_NONCACHED					3
 
 
