8779e3716326831e744c0e61daf11e385e54dda2
Yongqiang Sun <yongqiang.sun@amd.com>
2017-01-27 16:24:27 -0500

    drm/amd/display: Changes for enable dcc mode.
    Change-Id: I7b027fbf1680c359f724347844258296ff688cda
    Signed-off-by: Yongqiang Sun <yongqiang.sun@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce/dce_dmcu.h
+++ drivers/gpu/drm/amd/display/dc/dce/dce_dmcu.h
@@ -30,7 +30,6 @@
 #include "dmcu.h"
 
 #define DMCU_COMMON_REG_LIST_DCE_BASE() \
-	SR(DMCU_CTRL), \
 	SR(DMCU_RAM_ACCESS_CTRL), \
 	SR(DMCU_IRAM_WR_CTRL), \
 	SR(DMCU_IRAM_WR_DATA)
@@ -43,8 +42,6 @@
 	.field_name = reg_name ## __ ## field_name ## post_fix
 
 #define DMCU_COMMON_MASK_SH_LIST_DCE_COMMON_BASE(mask_sh) \
-	DMCU_SF(DMCU_CTRL, \
-			DMCU_ENABLE, mask_sh), \
 	DMCU_SF(DMCU_RAM_ACCESS_CTRL, \
 			IRAM_HOST_ACCESS_EN, mask_sh), \
 	DMCU_SF(DMCU_RAM_ACCESS_CTRL, \
@@ -58,8 +55,7 @@
 #define DMCU_REG_FIELD_LIST(type) \
 	type DMCU_IRAM_MEM_PWR_STATE; \
 	type IRAM_HOST_ACCESS_EN; \
-	type IRAM_WR_ADDR_AUTO_INC; \
-	type DMCU_ENABLE
+	type IRAM_WR_ADDR_AUTO_INC
 
 struct dce_dmcu_shift {
 	DMCU_REG_FIELD_LIST(uint8_t);
@@ -70,7 +66,6 @@ struct dce_dmcu_mask {
 };
 
 struct dce_dmcu_registers {
-	uint32_t DMCU_CTRL;
 	uint32_t DMCU_RAM_ACCESS_CTRL;
 	uint32_t DCI_MEM_PWR_STATUS;
 	uint32_t DMU_MEM_PWR_CNTL;
