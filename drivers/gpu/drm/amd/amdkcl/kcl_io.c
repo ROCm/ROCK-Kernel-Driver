89d9ecea1eeb15fda22c4b8502e91296214e7921
Qiang Yu <Qiang.Yu@amd.com>
2017-02-22 17:09:04 +0800

    drm/amd/amdkcl: fix arch spec code when compile on ARM64
    <asm/pgtable_types.h> is x86 spec headers
    
    Change-Id: Ife242e982346b7b6db1895a6efb8d7d4028adad5
    Signed-off-by: Qiang Yu <Qiang.Yu@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_io.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_io.c
@@ -1,7 +1,10 @@
+#include <linux/module.h>
 #include <kcl/kcl_io.h>
 #include "kcl_common.h"
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0) && defined(CONFIG_X86_PAT)
+#include <asm/pgtable_types.h>
+
 static int (*_kcl_io_reserve_memtype)(resource_size_t start, resource_size_t end,
 			enum page_cache_mode *type);
 static void (*_kcl_io_free_memtype)(resource_size_t start, resource_size_t end);
