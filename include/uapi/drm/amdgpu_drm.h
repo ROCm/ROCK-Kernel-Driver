6296ef327ff17b0c14397cc9b55d1908693b27f1
Alex Deucher <alexander.deucher@amd.com>
2017-03-03 15:54:06 -0500

    drm/amdgpu: Add asic family for vega10
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- include/uapi/drm/amdgpu_drm.h
+++ include/uapi/drm/amdgpu_drm.h
@@ -887,6 +887,7 @@ struct drm_amdgpu_info_vce_clock_table {
 #define AMDGPU_FAMILY_KV			125 /* Kaveri, Kabini, Mullins */
 #define AMDGPU_FAMILY_VI			130 /* Iceland, Tonga */
 #define AMDGPU_FAMILY_CZ			135 /* Carrizo, Stoney */
+#define AMDGPU_FAMILY_AI			141 /* Vega10 */
 
 /**
  *  Definition of System Unified Address (SUA) apertures
