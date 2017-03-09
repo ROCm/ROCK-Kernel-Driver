6273797843505fcb93ed9e74bd4aa5d723729fe7
Junwei Zhang <Jerry.Zhang@amd.com>
2017-03-09 14:33:01 +0800

    drm/amd/amdkcl: [RHEL 6.8] add bitmap_find_next_zero_area_off()
    Change-Id: I2e8d88bf9a47d4200a682c1b971f2aecb07568e6
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>
    
     Conflicts:
    	drivers/gpu/drm/amd/amdkcl/Makefile
    	drivers/gpu/drm/amd/backport/backport.h

---
+++ drivers/gpu/drm/amd/amdkcl/kcl_bitmap.c
@@ -0,0 +1,29 @@
+#include <kcl/kcl_bitmap.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
+unsigned long bitmap_find_next_zero_area_off(unsigned long *map,
+					     unsigned long size,
+					     unsigned long start,
+					     unsigned int nr,
+					     unsigned long align_mask,
+					     unsigned long align_offset)
+{
+	unsigned long index, end, i;
+again:
+	index = find_next_zero_bit(map, size, start);
+
+	/* Align allocation */
+	index = __ALIGN_MASK(index + align_offset, align_mask) - align_offset;
+
+	end = index + nr;
+	if (end > size)
+		return end;
+	i = find_next_bit(map, end, index);
+	if (i < end) {
+		start = i + 1;
+		goto again;
+	}
+	return index;
+}
+EXPORT_SYMBOL(bitmap_find_next_zero_area_off);
+#endif
