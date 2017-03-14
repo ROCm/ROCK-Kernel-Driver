f273331fd2c005d8dbab0ef1de33f5858e25b647
Shaoyun Liu <Shaoyun.Liu@amd.com>
2017-03-14 18:07:28 -0400

    drm/amdkfd: Serialize tlb flush on KFD side
    Change-Id: Id78f764a66c8a16d8c983afd3913d4ef9d06e014
    Signed-off-by: Shaoyun Liu <Shaoyun.Liu@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_device.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device.c
@@ -562,6 +562,8 @@ bool kgd2kfd_device_init(struct kfd_dev *kfd,
 
 	kfd->dbgmgr = NULL;
 
+	spin_lock_init(&kfd->tlb_invalidation_lock);
+
 	kfd->init_complete = true;
 	dev_info(kfd_device, "added device (%x:%x)\n", kfd->pdev->vendor,
 		 kfd->pdev->device);
