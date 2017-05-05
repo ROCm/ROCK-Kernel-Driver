1be7b69f925417c19e31b9ae22dd1e4ba32ff205
Indrajit Das <indrajit-kumar.das@amd.com>
2017-05-05 14:28:12 +0530

    drm/amd/display: enable static audio routing
    Signed-off-by: Indrajit Das <indrajit-kumar.das@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_resource.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_resource.c
@@ -1278,7 +1278,7 @@ static struct audio *find_first_free_audio(
 {
 	int i;
 	for (i = 0; i < pool->audio_count; i++) {
-		if (res_ctx->is_audio_acquired[i] == false) {
+		if ((res_ctx->is_audio_acquired[i] == false) && (res_ctx->is_stream_enc_acquired[i] == true)) {
 			return pool->audios[i];
 		}
 	}
