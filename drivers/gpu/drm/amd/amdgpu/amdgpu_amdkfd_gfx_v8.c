17e8f1ce7de4422d170b5aac674ea22fcd4ae9f6
Evan Quan <evan.quan@amd.com>
2017-05-11 09:14:08 +0800

    drm/amdkfd: imported the change brought by kfd merge commit c94f9eb
    Change-Id: If11b5ec3b47f9275ae4321b66711a9776a8f8ed8
    Signed-off-by: Evan Quan <evan.quan@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v8.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v8.c
@@ -771,7 +771,7 @@ static int invalidate_tlbs_with_kiq(struct amdgpu_device *adev, uint16_t pasid)
 	struct amdgpu_ring *ring = &adev->gfx.kiq.ring;
 	struct amdgpu_kiq *kiq = &adev->gfx.kiq;
 
-	mutex_lock(&kiq->ring_mutex);
+	mutex_lock(&adev->gfx.kiq.ring_mutex);
 	amdgpu_ring_alloc(ring, 12); /* fence + invalidate_tlbs package*/
 	amdgpu_ring_write(ring, PACKET3(PACKET3_INVALIDATE_TLBS, 0));
 	amdgpu_ring_write(ring,
@@ -779,7 +779,7 @@ static int invalidate_tlbs_with_kiq(struct amdgpu_device *adev, uint16_t pasid)
 			PACKET3_INVALIDATE_TLBS_PASID(pasid));
 	amdgpu_fence_emit(ring, &f);
 	amdgpu_ring_commit(ring);
-	mutex_unlock(&kiq->ring_mutex);
+	mutex_unlock(&adev->gfx.kiq.ring_mutex);
 
 	r = fence_wait(f, false);
 	if (r)
