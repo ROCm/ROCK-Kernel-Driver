872a40bf6d2de4a05a8f4017b7990a8dd3009c19
Evan Quan <evan.quan@amd.com>
2017-05-11 16:20:30 +0800

    drm/amd/amdkcl: [4.11] fix for drm debug
    This patch merges 4 commits below in hybrid 4.12
    
      - daa16a3 drm/ttm: Include <linux/version.h> in ttm_bo_driver.h
      - 175974b drm/amdkcl: [4.11] kcl for print message
      - 9179a51 drm/amdkcl: [4.11] kcl for ttm debug API below:
      - c48e813 drm/amd/amdkcl: [4.11] fix for drm_mm_print
    
    Change-Id: I9863fd1507c252be311c2ab83214e92a50749c50
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    
     Conflicts:
    	drivers/gpu/drm/amd/amdgpu/amdgpu_gtt_mgr.c
    	drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
    	drivers/gpu/drm/amd/amdgpu/amdgpu_vram_mgr.c
    	drivers/gpu/drm/ttm/ttm_bo_manager.c

--- drivers/gpu/drm/ttm/ttm_bo_manager.c
+++ drivers/gpu/drm/ttm/ttm_bo_manager.c
@@ -181,12 +181,20 @@ static int ttm_bo_man_takedown(struct ttm_mem_type_manager *man)
 }
 
 static void ttm_bo_man_debug(struct ttm_mem_type_manager *man,
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
+			     const char *prefix)
+#else
 			     struct drm_printer *printer)
+#endif
 {
 	struct ttm_range_manager *rman = (struct ttm_range_manager *) man->priv;
 
 	spin_lock(&rman->lock);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
+	drm_mm_debug_table(&rman->mm, prefix);
+#else
 	drm_mm_print(&rman->mm, printer);
+#endif
 	spin_unlock(&rman->lock);
 }
 
