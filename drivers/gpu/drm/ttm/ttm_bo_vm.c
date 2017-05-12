ad11ad62a8d019e0fd410831edd2ca26845cca80
Evan Quan <evan.quan@amd.com>
2017-05-12 09:54:48 +0800

    drm/amd/amdkcl: [4.10] fix for (struct vm_fault *vmf)->address
    Change-Id: If406fe5e17055f181ba734bd56a14403bae276fc
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/ttm/ttm_bo_vm.c
+++ drivers/gpu/drm/ttm/ttm_bo_vm.c
@@ -120,7 +120,12 @@ static int ttm_bo_vm_fault(struct vm_fault *vmf)
 	struct page *page;
 	int ret;
 	int i;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
+	unsigned long address = (unsigned long)vmf->virtual_address;
+#else
 	unsigned long address = vmf->address;
+#endif
+
 	struct ttm_mem_type_manager *man =
 		&bdev->man[bo->mem.mem_type];
 	struct vm_area_struct cvma;
