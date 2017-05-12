2880a38f4d3cf2f8f6e297a03e90589c6518a6a3
Evan Quan <evan.quan@amd.com>
2017-05-12 09:41:15 +0800

    drm/amdkcl: [4.11] fix for uapi/linux/sched/types.h
    Change-Id: I6dbfce2211f2fa40d2746ac536d7ac07ab03c8ee
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/ttm/ttm_bo_vm.c
+++ drivers/gpu/drm/ttm/ttm_bo_vm.c
@@ -43,11 +43,14 @@
 #include <linux/rbtree.h>
 #include <linux/module.h>
 #include <linux/uaccess.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)
 #include <linux/mem_encrypt.h>
+#endif
 
 #define TTM_BO_VM_NUM_PREFAULT 16
 
 static vm_fault_t ttm_bo_vm_fault_idle(struct ttm_buffer_object *bo,
+				struct vm_area_struct *vma,
 				struct vm_fault *vmf)
 {
 	vm_fault_t ret = 0;
@@ -68,8 +71,10 @@ static vm_fault_t ttm_bo_vm_fault_idle(struct ttm_buffer_object *bo,
 	 */
 	if (vmf->flags & FAULT_FLAG_ALLOW_RETRY) {
 		ret = VM_FAULT_RETRY;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39)
 		if (vmf->flags & FAULT_FLAG_RETRY_NOWAIT)
 			goto out_unlock;
+#endif
 
 		ttm_bo_get(bo);
 		up_read(&vmf->vma->vm_mm->mmap_sem);
@@ -108,10 +113,15 @@ static unsigned long ttm_bo_io_mem_pfn(struct ttm_buffer_object *bo,
 	return ((bo->mem.bus.base + bo->mem.bus.offset) >> PAGE_SHIFT)
 		+ page_offset;
 }
-
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
+static vm_fault_t ttm_bo_vm_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
+#else
 static vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
+#endif
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
 	struct vm_area_struct *vma = vmf->vma;
+#endif
 	struct ttm_buffer_object *bo = (struct ttm_buffer_object *)
 	    vma->vm_private_data;
 	struct ttm_bo_device *bdev = bo->bdev;
@@ -123,7 +133,11 @@ static vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
 	int err;
 	int i;
 	vm_fault_t ret = VM_FAULT_NOPAGE;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
+	unsigned long address = (unsigned long)vmf->virtual_address;
+#else
 	unsigned long address = vmf->address;
+#endif
 	struct ttm_mem_type_manager *man =
 		&bdev->man[bo->mem.mem_type];
 	struct vm_area_struct cvma;
@@ -136,12 +150,16 @@ static vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
 	 */
 	if (unlikely(!reservation_object_trylock(bo->resv))) {
 		if (vmf->flags & FAULT_FLAG_ALLOW_RETRY) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39)
 			if (!(vmf->flags & FAULT_FLAG_RETRY_NOWAIT)) {
 				ttm_bo_get(bo);
-				up_read(&vmf->vma->vm_mm->mmap_sem);
+				up_read(&vma->vm_mm->mmap_sem);
 				(void) ttm_bo_wait_unreserved(bo);
 				ttm_bo_put(bo);
 			}
+#else
+			up_read(&vma->vm_mm->mmap_sem);
+#endif
 
 			return VM_FAULT_RETRY;
 		}
@@ -191,10 +209,14 @@ static vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
 	 * Wait for buffer data in transit, due to a pipelined
 	 * move.
 	 */
-	ret = ttm_bo_vm_fault_idle(bo, vmf);
+	ret = ttm_bo_vm_fault_idle(bo, vma, vmf);
 	if (unlikely(ret != 0)) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39)
 		if (ret == VM_FAULT_RETRY &&
 		    !(vmf->flags & FAULT_FLAG_RETRY_NOWAIT)) {
+#else
+		if (ret == VM_FAULT_RETRY) {
+#endif
 			/* The BO has already been unreserved. */
 			return ret;
 		}
@@ -259,8 +281,10 @@ static vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
 	 */
 	for (i = 0; i < TTM_BO_VM_NUM_PREFAULT; ++i) {
 		if (bo->mem.bus.is_iomem) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)
 			/* Iomem should not be marked encrypted */
 			cvma.vm_page_prot = pgprot_decrypted(cvma.vm_page_prot);
+#endif
 			pfn = ttm_bo_io_mem_pfn(bo, page_offset);
 		} else {
 			page = ttm->pages[page_offset];
@@ -280,7 +304,6 @@ static vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
 					__pfn_to_pfn_t(pfn, PFN_DEV | (bo->ssg_can_map ? PFN_MAP : 0)));
 		else
 			ret = vmf_insert_pfn(&cvma, address, pfn);
-
 		/*
 		 * Somebody beat us to this PTE or prefaulting to
 		 * an already populated PTE, or prefaulting error.
