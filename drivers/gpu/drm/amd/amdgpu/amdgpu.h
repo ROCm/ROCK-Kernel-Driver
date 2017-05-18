f2cff5a30c221dce57a16a75053a378b1c7aef0f
Chunming Zhou <David1.Zhou@amd.com>
2017-05-18 15:19:03 +0800

    drm/amdgpu: add dep_sync for amdgpu job
    The fence in dep_sync cannot be optimized.
    
    Signed-off-by: Chunming Zhou <David1.Zhou@amd.com>
    Tested and Reviewed-by Roger.He <Hongbo.He@amd.com>
    
    Change-Id: I2c9989d681690a339457b7762404fa062d16c299

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -1161,6 +1161,7 @@ struct amdgpu_job {
 	struct amdgpu_vm	*vm;
 	struct amdgpu_ring	*ring;
 	struct amdgpu_sync	sync;
+	struct amdgpu_sync	dep_sync;
 	struct amdgpu_sync	sched_sync;
 	struct amdgpu_ib	*ibs;
 	struct fence		*fence; /* the hw fence */
