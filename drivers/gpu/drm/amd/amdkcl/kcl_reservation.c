b6cdd894a71090d09327d4d4ee7e33d9e086cdad
Evan Quan <evan.quan@amd.com>
2017-05-24 14:21:29 +0800

    drm/amdkcl: [4.10][ubuntu17.04] kcl update for reservation_object_wait_timeout_rcu
    Change-Id: Ia513331c2c418078a03d6f16c2c497423f94d663
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    
     Conflicts:
    	drivers/gpu/drm/amd/amdkcl/kcl_reservation.c
    	include/kcl/kcl_reservation.h

--- drivers/gpu/drm/amd/amdkcl/kcl_reservation.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_reservation.c
@@ -1,6 +1,7 @@
 #include <kcl/kcl_fence.h>
 #include <kcl/kcl_reservation.h>
 
+#if defined(BUILD_AS_DKMS) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
 long _kcl_reservation_object_wait_timeout_rcu(struct reservation_object *obj,
 					 bool wait_all, bool intr,
 					 unsigned long timeout)
@@ -76,3 +77,4 @@ unlock_retry:
 	goto retry;
 }
 EXPORT_SYMBOL(_kcl_reservation_object_wait_timeout_rcu);
+#endif
--- include/kcl/kcl_reservation.h
+++ include/kcl/kcl_reservation.h
@@ -3,7 +3,7 @@
 
 #include <linux/reservation.h>
 
-#if defined(BUILD_AS_DKMS)
+#if defined(BUILD_AS_DKMS) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
 extern long _kcl_reservation_object_wait_timeout_rcu(struct reservation_object *obj,
 					 bool wait_all, bool intr,
 					 unsigned long timeout);
@@ -14,7 +14,7 @@ kcl_reservation_object_wait_timeout_rcu(struct reservation_object *obj,
 					 bool wait_all, bool intr,
 					 unsigned long timeout)
 {
-#if defined(BUILD_AS_DKMS)
+#if defined(BUILD_AS_DKMS) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
 	return _kcl_reservation_object_wait_timeout_rcu(obj,
 					wait_all, intr, timeout);
 #else
