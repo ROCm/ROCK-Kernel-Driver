3259098d3f4b859c9ac4445dfa3d35902a638b25
Evan Quan <evan.quan@amd.com>
2017-06-21 15:33:40 +0800

    drm/amdkcl: [4.11] kcl for macro DW_I2S_QUIRK_16BIT_IDX_OVERRIDE
    Change-Id: I11099c47601a377ac1cb889bac4c0e2c20d82976
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_acp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_acp.c
@@ -71,6 +71,10 @@
 #define ACP_DEVS			3
 #define ACP_SRC_ID			162
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
+#define DW_I2S_QUIRK_16BIT_IDX_OVERRIDE (1 << 2)
+#endif
+
 enum {
 	ACP_TILE_P1 = 0,
 	ACP_TILE_P2,
