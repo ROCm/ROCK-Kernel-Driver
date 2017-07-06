2a7193d20f308005b491c8acf51dd9c5f937bd4d
Roger.He <Hongbo.He@amd.com>
2017-07-06 13:00:02 +0800

    drm/amd/amdkcl: [RHEL 7.4] fix for get_user_pages
    Change-Id: If665a5dca4f537b2ee474da95086d2f38c745a59
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Roger.He <Hongbo.He@amd.com>

--- include/kcl/kcl_mm.h
+++ include/kcl/kcl_mm.h
@@ -9,9 +9,23 @@ static inline int kcl_get_user_pages(struct task_struct *tsk, struct mm_struct *
 				struct vm_area_struct **vmas)
 {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0) || defined(OS_NAME_RHEL_7_4)
-	return get_user_pages(start, nr_pages, write, pages, vmas);
+	if (mm == current->mm)
+		return get_user_pages(start, nr_pages, write, pages, vmas);
+	else
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0) || defined(OS_NAME_RHEL_7_4)
+		return get_user_pages_remote(tsk, mm, start, nr_pages,
+				write, pages, vmas, NULL);
+#else
+		return get_user_pages_remote(tsk, mm, start, nr_pages,
+				write, pages, vmas);
+#endif
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)
-	return get_user_pages(start, nr_pages, write, force, pages, vmas);
+	if (mm == current->mm)
+		return get_user_pages(start, nr_pages, write, force, pages,
+				vmas);
+	else
+		return get_user_pages_remote(tsk, mm, start, nr_pages,
+				write, force, pages, vmas);
 #else
 	write = !!(write & FOLL_WRITE);
 	return get_user_pages(tsk, mm, start, nr_pages,
