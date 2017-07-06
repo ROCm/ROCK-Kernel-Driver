d9722eee3e2f85acfd9acbbfd0d4b368afe8bc9b
Roger.He <Hongbo.He@amd.com>
2017-07-06 15:09:41 +0800

    drm/amd/amdkcl: [RHEL 7.4] use dma-fence on RHEL7.4
    Change-Id: I42d38941d71dcfe9bd5d0176e3989a1f5bc75140
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Roger.He <Hongbo.He@amd.com>
    
     Conflicts:
    	drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
    	drivers/gpu/drm/amd/amdkcl/kcl_fence.c
    	drivers/gpu/drm/amd/amdkcl/main.c
    	drivers/gpu/drm/amd/scheduler/sched_fence.c
    	include/kcl/kcl_fence.h

--- drivers/gpu/drm/amd/amdkcl/main.c
+++ drivers/gpu/drm/amd/amdkcl/main.c
@@ -5,14 +5,18 @@
 
 extern void amdkcl_kthread_init(void);
 extern void amdkcl_drm_init(void);
+#if !defined(OS_NAME_RHEL_7_4)
 extern void amdkcl_fence_init(void);
+#endif
 extern void amdkcl_io_init(void);
 
 int __init amdkcl_init(void)
 {
 	amdkcl_kthread_init();
 	amdkcl_drm_init();
+#if !defined(OS_NAME_RHEL_7_4)
 	amdkcl_fence_init();
+#endif
 	amdkcl_io_init();
 	kcl_drm_global_init();
 	return 0;
