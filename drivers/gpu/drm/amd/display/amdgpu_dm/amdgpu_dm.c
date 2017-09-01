843113861da807d77332cdcea522ed36468e935f
Le.Ma <Le.Ma@amd.com>
2017-09-01 15:09:41 +0800

    drm/amdkcl: [4.11] Add own page_flip interface for amdgpu_dm
    This is a squash of:
    drm/amdkcl: Test whether drm_crtc_state->async_flip is available
    drm/amdkcl: refactor test for pageflip flag in struct drm-crtc-state
        amdgpu_atomic_helper_page_flip() is dropped in commit
        32283f8a28c7("drm/amd/display: Drop our own page_flip_helper") since
        commit 6cbe5c466d73("drm/atomic: Save flip flags in drm_crtc_state") is
        upstreamed into drm_atomic_helper_page_flip().
    
        this commit rework the legacy kcl DRM_VERSION check one
        84fdb338a6b2("drm/amdkcl: [4.11] Add own page_flip interface for
    		    amdgpu_dm")
    
    The page_flip in amdgpu has been dropped and replaced with
    drm_atomic_helper_page_flip. We still need it for kernel < 4.11.
    
    This commit merges
      - part of "3e32027 drm/amd/amdkcl: [4.12] kcl fix for acquire ctx added since 4.12"
      - 21ca6be drm/amdkcl: [4.11] Fix our own page_flip_helper dropped
    
    Change-Id: I02a63606f8b8824ccf4d6045cfa432c167fb7f17
    Signed-off-by: Le.Ma <Le.Ma@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yang Xiong <Yang.Xiong@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6352,6 +6352,84 @@ static int dm_crtc_funcs_atomic_get_property(struct drm_crtc *crtc,
 }
 #endif
 
+#ifndef HAVE_STRUCT_DRM_CRTC_STATE_FLIP_FLAG
+static int amdgpu_atomic_helper_page_flip(struct drm_crtc *crtc,
+				struct drm_framebuffer *fb,
+				struct drm_pending_vblank_event *event,
+				uint32_t flags)
+{
+	struct drm_plane *plane = crtc->primary;
+	struct amdgpu_crtc *acrtc = to_amdgpu_crtc(crtc);
+	struct drm_atomic_state *state;
+	struct drm_plane_state *plane_state;
+	struct drm_crtc_state *crtc_state;
+	int ret = 0;
+
+	state = drm_atomic_state_alloc(plane->dev);
+	if (!state)
+		return -ENOMEM;
+
+	state->acquire_ctx = drm_modeset_legacy_acquire_ctx(crtc);
+retry:
+	crtc_state = drm_atomic_get_crtc_state(state, crtc);
+	if (IS_ERR(crtc_state)) {
+		ret = PTR_ERR(crtc_state);
+		goto fail;
+	}
+	crtc_state->event = event;
+
+	plane_state = drm_atomic_get_plane_state(state, plane);
+	if (IS_ERR(plane_state)) {
+		ret = PTR_ERR(plane_state);
+		goto fail;
+	}
+
+	ret = drm_atomic_set_crtc_for_plane(plane_state, crtc);
+	if (ret != 0)
+		goto fail;
+	drm_atomic_set_fb_for_plane(plane_state, fb);
+
+	/* Make sure we don't accidentally do a full modeset. */
+	state->allow_modeset = false;
+	if (!crtc_state->active) {
+		DRM_DEBUG_ATOMIC("[CRTC:%d] disabled, rejecting legacy flip\n",
+				 crtc->base.id);
+		ret = -EINVAL;
+		goto fail;
+	}
+	acrtc->flip_flags = flags;
+#if !defined(HAVE_DRM_ATOMIC_NONBLOCKING_COMMIT)
+	ret = drm_atomic_async_commit(state);
+#else
+	ret = drm_atomic_nonblocking_commit(state);
+#endif
+	if (ret != 0)
+		goto fail;
+
+	/* Driver takes ownership of state on successful async commit. */
+	return 0;
+fail:
+	if (ret == -EDEADLK)
+		goto backoff;
+
+	drm_atomic_state_put(state);
+
+	return ret;
+backoff:
+	drm_atomic_state_clear(state);
+	drm_atomic_legacy_backoff(state);
+
+	/*
+	 * Someone might have exchanged the framebuffer while we dropped locks
+	 * in the backoff code. We need to fix up the fb refcount tracking the
+	 * core does for us.
+	 */
+	plane->old_fb = plane->fb;
+
+	goto retry;
+}
+#endif
+
 static void dm_crtc_destroy_state(struct drm_crtc *crtc,
 				  struct drm_crtc_state *state)
 {
@@ -6511,7 +6589,11 @@ static const struct drm_crtc_funcs amdgpu_dm_crtc_funcs = {
 #ifdef HAVE_DRM_ATOMIC_HELPER_XXX_SET_PROPERTY
 	.set_property = drm_atomic_helper_crtc_set_property,
 #endif
+#ifndef HAVE_STRUCT_DRM_CRTC_STATE_FLIP_FLAG
+	.page_flip = amdgpu_atomic_helper_page_flip,
+#else
 	.page_flip = drm_atomic_helper_page_flip,
+#endif
 	.atomic_duplicate_state = dm_crtc_duplicate_state,
 	.atomic_destroy_state = dm_crtc_destroy_state,
 #if defined(HAVE_STRUCT_DRM_CRTC_FUNCS_SET_CRC_SOURCE)
