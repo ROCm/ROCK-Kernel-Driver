3f645c79b9c4e89a6b2845dd98874dca5c9c2aa0
Le.Ma <Le.Ma@amd.com>
2017-09-14 14:02:35 +0800

    drm/amdkcl: Include <kcl/kcl_amd_asic_type.h> in amdkfd backport.h
    Change-Id: I04d47a1886c93e3988481173214e904de636df0b
    Signed-off-by: Le.Ma <Le.Ma@amd.com>
    Acked-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdkfd/backport/backport.h
+++ drivers/gpu/drm/amd/amdkfd/backport/backport.h
@@ -2,6 +2,9 @@
 #define AMDKFD_BACKPORT_H
 
 #include <linux/version.h>
+#if defined(BUILD_AS_DKMS)
+#include <kcl/kcl_amd_asic_type.h>
+#endif
 #include <kcl/kcl_compat.h>
 #include <kcl/kcl_pci.h>
 #include <kcl/kcl_mn.h>
