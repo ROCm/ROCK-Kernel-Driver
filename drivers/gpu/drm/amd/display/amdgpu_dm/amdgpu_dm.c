fe4ca209ce90e4a8b03e2b85ef797b03f750c853
Le.Ma <Le.Ma@amd.com>
2017-09-26 11:13:00 +0800

    drm/amdkcl: [4.12] Reserve flip_flags for display usage
    Change-Id: I9205bb7f5f5df8b1483648044cb305348e0a91b3
    Signed-off-by: Le.Ma <Le.Ma@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -4152,7 +4152,11 @@ static void amdgpu_dm_do_flip(
 	struct amdgpu_framebuffer *afb = to_amdgpu_framebuffer(fb);
 	struct amdgpu_bo *abo = gem_to_amdgpu_bo(afb->obj);
 	struct amdgpu_device *adev = crtc->dev->dev_private;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+	bool async_flip = (acrtc->flip_flags & DRM_MODE_PAGE_FLIP_ASYNC) != 0;
+#else
 	bool async_flip = (crtc->state->pageflip_flags & DRM_MODE_PAGE_FLIP_ASYNC) != 0;
+#endif
 	struct dc_flip_addrs addr = { {0} };
 	/* TODO eliminate or rename surface_update */
 	struct dc_surface_update surface_updates[1] = { {0} };
@@ -4284,7 +4288,11 @@ static void amdgpu_dm_commit_planes(struct drm_atomic_state *state,
 			 * TODO Check if it's correct
 			 */
 			*wait_for_vblank =
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+					acrtc_attach->flip_flags & DRM_MODE_PAGE_FLIP_ASYNC ?
+#else
 					pcrtc->state->pageflip_flags & DRM_MODE_PAGE_FLIP_ASYNC ?
+#endif
 				false : true;
 
 			/* TODO: Needs rework for multiplane flip */
@@ -4295,6 +4303,9 @@ static void amdgpu_dm_commit_planes(struct drm_atomic_state *state,
 				crtc,
 				fb,
 				drm_crtc_vblank_count(crtc) + *wait_for_vblank);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+			acrtc_attach->flip_flags = 0;
+#endif
 		}
 
 	}
