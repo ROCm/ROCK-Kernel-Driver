456fafe93e0f4b9b5b0b4bf29c1803ab2377e6ba
Le.Ma <Le.Ma@amd.com>
2017-09-26 11:13:00 +0800

    drm/amdkcl: [4.12] Reserve flip_flags for display usage
    Change-Id: Ia7396521f5e11468e83cf2cabac1b6cbbcda21cd
    Signed-off-by: Le.Ma <Le.Ma@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -4,5 +4,6 @@
 #include <linux/version.h>
 #include <kcl/kcl_drm.h>
 #include <kcl/kcl_amdgpu.h>
+#include <kcl/kcl_bitops.h>
 
 #endif /* AMDGPU_BACKPORT_H */
---
+++ include/kcl/kcl_bitops.h
@@ -0,0 +1,15 @@
+#ifndef AMDKCL_BITOPS_H
+#define AMDKCL_BITOPS_H
+#include <linux/version.h>
+
+#ifndef _BITOPS_LONG_SHIFT
+#if BITS_PER_LONG == 32
+# define _BITOPS_LONG_SHIFT 5
+#elif BITS_PER_LONG == 64
+# define _BITOPS_LONG_SHIFT 6
+#else
+# error "Unexpected BITS_PER_LONG"
+#endif
+#endif
+
+#endif /* AMDKCL_BITOPS_H */
