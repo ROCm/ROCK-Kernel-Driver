bd08c1729ac2cb89035eca631b2ad522cd0148ec
Yongqiang Sun <yongqiang.sun@amd.com>
2017-09-28 17:18:27 -0400

    drm/amd/display: Fixed extend to second screen mode hang
    	1. Fixed acquire free split pipe bug.
    	2. Change return value for dc_add_stream_to_ctx
    	   from bool to enum.
    	4. Remove redundant apply_ctx_for_surface calling
    	5. Unlock pipe after back end programming.
    
    Signed-off-by: Yongqiang Sun <yongqiang.sun@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -4725,10 +4725,10 @@ static int dm_update_crtcs_state(struct dc *dc,
 				DRM_DEBUG_DRIVER("Enabling DRM crtc: %d\n",
 							crtc->base.id);
 
-				if (!dc_add_stream_to_ctx(
+				if (dc_add_stream_to_ctx(
 						dc,
 						dm_state->context,
-						new_acrtc_state->stream)) {
+						new_acrtc_state->stream) != DC_OK) {
 					ret = -EINVAL;
 					goto fail;
 				}
