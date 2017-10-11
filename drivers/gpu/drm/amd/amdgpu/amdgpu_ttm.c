aa4d7e6a04143bd211772e76491215266bc4538d
Le.Ma <Le.Ma@amd.com>
2017-10-11 16:00:21 +0800

    drm/amdkcl: [RHEL 6] Skip iova_to_phys support for rhel6 due to no iommu API
    Change-Id: Ia31f2e1dc8c355ba26d131bacfe13753974081a1
    Signed-off-by: Le.Ma <Le.Ma@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -2166,6 +2166,8 @@ static const struct file_operations amdgpu_ttm_gtt_fops = {
 
 #endif
 
+#if !defined(OS_NAME_RHEL_6)
+
 static ssize_t amdgpu_iova_to_phys_read(struct file *f, char __user *buf,
 				   size_t size, loff_t *pos)
 {
@@ -2201,6 +2203,8 @@ static const struct file_operations amdgpu_ttm_iova_fops = {
 	.llseek = default_llseek
 };
 
+#endif
+
 static const struct {
 	char *name;
 	const struct file_operations *fops;
@@ -2210,7 +2214,9 @@ static const struct {
 #ifdef CONFIG_DRM_AMDGPU_GART_DEBUGFS
 	{ "amdgpu_gtt", &amdgpu_ttm_gtt_fops, TTM_PL_TT },
 #endif
+#if !defined(OS_NAME_RHEL_6)
 	{ "amdgpu_iova", &amdgpu_ttm_iova_fops, TTM_PL_SYSTEM },
+#endif
 };
 
 #endif
