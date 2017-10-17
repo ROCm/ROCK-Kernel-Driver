a380a8dea3f64bd5561aa1112558583e26f9c20b
Tom St Denis <tom.stdenis@amd.com>
2017-10-17 08:49:13 -0400

    drm/amd/display: Fix indentation in dm_resume()
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>
    Acked-by: Andrey Grodzovsky  <andey.grodzovsky@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -638,10 +638,7 @@ static int dm_resume(void *handle)
 	struct amdgpu_display_manager *dm = &adev->dm;
 
 	/* power on hardware */
-	dc_set_power_state(
-		dm->dc,
-		DC_ACPI_CM_POWER_STATE_D0
-		);
+	dc_set_power_state(dm->dc, DC_ACPI_CM_POWER_STATE_D0);
 
 	return 0;
 }
