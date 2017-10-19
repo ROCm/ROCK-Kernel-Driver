6a03c7e27fb0349bbb5aa4517c51ef54a56206fe
Le.Ma <Le.Ma@amd.com>
2017-10-19 13:09:54 +0800

    drm/amdkcl: [4.8] Add kcl for drm_is_current_master()
    Change-Id: I7d539a0c4a179bc24f51d04366ffbeaaf0e8b60f
    Signed-off-by: Le.Ma <Le.Ma@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    
    Conflicts:
          include/kcl/kcl_drm.h

--- drivers/gpu/drm/amd/amdkcl/kcl_drm.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm.c
@@ -335,8 +335,7 @@ _kcl_drm_atomic_helper_update_legacy_modeset_state_stub(struct drm_device *dev,
 	}
 }
 
-#if DRM_VERSION_CODE < DRM_VERSION(4, 5, 0) && \
-	!defined(OS_NAME_UBUNTU)
+#if DRM_VERSION_CODE < DRM_VERSION(4, 5, 0) && !(defined(OS_NAME_UBUNTU) || defined(OS_NAME_SLE))
 int drm_modeset_lock_all_ctx(struct drm_device *dev,
 			     struct drm_modeset_acquire_ctx *ctx)
 {
@@ -537,3 +536,11 @@ void amdkcl_drm_init(void)
 					"drm_atomic_helper_update_legacy_modeset_state",
 					_kcl_drm_atomic_helper_update_legacy_modeset_state_stub);
 }
+
+#if DRM_VERSION_CODE < DRM_VERSION(4, 8, 0)
+bool drm_is_current_master(struct drm_file *fpriv)
+{
+	return fpriv->is_master && fpriv->master == fpriv->minor->master;
+}
+EXPORT_SYMBOL(drm_is_current_master);
+#endif
