b204abe324338b3e9effd8b03b22f91d9e2cd60a
Le.Ma <Le.Ma@amd.com>
2017-10-20 09:41:33 +0800

    drm/amdkcl: [4.11] Kcl for fence->error and dma_fence_set_error
    Change-Id: I53307623b960b6edbb807f947ec18805daaec284
    Signed-off-by: Le.Ma <Le.Ma@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
@@ -1360,8 +1360,13 @@ int amdgpu_cs_wait_ioctl(struct drm_device *dev, void *data,
 		r = PTR_ERR(fence);
 	else if (fence) {
 		r = kcl_fence_wait_timeout(fence, true, timeout);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
+		if (r > 0 && fence->status)
+			r = fence->status;
+#else
 		if (r > 0 && fence->error)
 			r = fence->error;
+#endif
 		dma_fence_put(fence);
 	} else
 		r = 1;
@@ -1509,8 +1514,13 @@ static int amdgpu_cs_wait_all_fences(struct amdgpu_device *adev,
 		if (r == 0)
 			break;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
+		if (r > 0 && fence->status)
+			r = fence->status;
+#else
 		if (fence->error)
 			return fence->error;
+#endif
 	}
 
 	memset(wait, 0, sizeof(*wait));
@@ -1572,7 +1582,11 @@ out:
 	wait->out.first_signaled = first;
 
 	if (first < fence_count && array[first])
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
+		r = array[first]->status;
+#else
 		r = array[first]->error;
+#endif
 	else
 		r = 0;
 
