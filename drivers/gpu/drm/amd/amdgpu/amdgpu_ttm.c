4ca927c31e2ccefe079cef1994a10135e54fae12
Monk Liu <Monk.Liu@amd.com>
2017-11-13 20:41:56 +0800

    drm/amdgpu:cleanup stolen vga memory finish
    Change-Id: Id641e125f3dafc54223e49ee444ab64249e7e3a1
    Signed-off-by: Monk Liu <Monk.Liu@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -1644,16 +1644,11 @@ void amdgpu_ttm_fini(struct amdgpu_device *adev)
 
 	if (!adev->mman.initialized)
 		return;
+
 	amdgpu_ttm_debugfs_fini(adev);
 
-	if (adev->stolen_vga_memory) {
-		r = amdgpu_bo_reserve(adev->stolen_vga_memory, true);
-		if (r == 0) {
-			amdgpu_bo_unpin(adev->stolen_vga_memory);
-			amdgpu_bo_unreserve(adev->stolen_vga_memory);
-		}
-		amdgpu_bo_unref(&adev->stolen_vga_memory);
-	}
+	amdgpu_bo_free_kernel(&adev->stolen_vga_memory, NULL, NULL);
+
 	amdgpu_ssg_fini(adev);
 	amdgpu_direct_gma_fini(adev);
 
