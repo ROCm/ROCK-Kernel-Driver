04a5629adf7b43f67f2552c98e109b311a35f263
Feifei Xu <Feifei.Xu@amd.com>
2017-11-27 16:48:06 +0800

    drm/amd/include:cleanup raven1 dcn header files.
    Cleanup asic_reg/raven1/DCN folder.Remove unused dcn_1_0_default.h.
    
    Change-Id: I9956270e18d62bca97d3cae40a56530972eda109
    Signed-off-by: Feifei Xu <Feifei.Xu@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hw_sequencer.c
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hw_sequencer.c
@@ -38,8 +38,8 @@
 #include "opp.h"
 #include "ipp.h"
 #include "mpc.h"
-#include "raven1/DCN/dcn_1_0_offset.h"
-#include "raven1/DCN/dcn_1_0_sh_mask.h"
+#include "dcn/dcn_1_0_offset.h"
+#include "dcn/dcn_1_0_sh_mask.h"
 #include "soc15ip.h"
 #include "reg_helper.h"
 
