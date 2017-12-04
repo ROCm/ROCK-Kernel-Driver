bittencourtjulival/eclipse_kernel_xiaomi_stone

drm/amd/display: Refactor dc_state creation into a function.
For less repetition and easy debugging.

Signed-off-by: Leo (Sunpeng) Li <sunpeng.li@amd.com>
Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -497,14 +497,12 @@ static bool construct(struct dc *dc,
 		goto fail;
 	}
 
-	dc->current_context = dm_alloc(sizeof(*dc->current_context));
+	dc->current_context = dc_create_state();
 
 	if (!dc->current_context) {
 		dm_error("%s: failed to create validate ctx\n", __func__);
 		goto fail;
 	}
-
-	atomic_inc(&dc->current_context->ref_count);
 
 	dc_ctx->cgs_device = init_params->cgs_device;
 	dc_ctx->driver_context = init_params->driver;
@@ -1162,6 +1160,17 @@ bool dc_commit_planes_to_stream(
 	return true;
 }
 
+struct validate_context *dc_create_state(void)
+{
+	struct validate_context *context = dm_alloc(sizeof(struct validate_context));
+
+	if (!context)
+		return NULL;
+
+	atomic_inc(&context->ref_count);
+	return context;
+}
+
 void dc_retain_validate_context(struct validate_context *context)
 {
 	ASSERT(atomic_read(&context->ref_count) > 0);
@@ -1442,11 +1451,9 @@ void dc_update_planes_and_stream(struct dc *dc,
 			new_planes[i] = srf_updates[i].surface;
 
 		/* initialize scratch memory for building context */
-		context = dm_alloc(sizeof(*context));
+		context = dc_create_state();
 		if (context == NULL)
 				goto context_alloc_fail;
-
-		atomic_inc(&context->ref_count);
 
 		dc_resource_validate_ctx_copy_construct(
 				core_dc->current_context, context);

drivers/gpu/drm/amd/display/dc/dc.h
@@ -732,6 +732,7 @@ enum surface_update_type dc_check_update_surfaces_for_stream(
 		const struct dc_stream_status *stream_status);
 
 
+struct validate_context *dc_create_state(void);
 void dc_retain_validate_context(struct validate_context *context);
 void dc_release_validate_context(struct validate_context *context);
 

ROCm/ROCK-Kernel-Driver

Reviewed-by: Harry Wentland <harry.wentland@amd.com>

drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -480,14 +480,12 @@ static bool construct(struct dc *dc,
 		goto fail;
 	}
 
-	dc->current_state = dm_alloc(sizeof(*dc->current_state));
+	dc->current_state = dc_create_state();
 
 	if (!dc->current_state) {
 		dm_error("%s: failed to create validate ctx\n", __func__);
 		goto fail;
 	}
-
-	atomic_inc(&dc->current_state->ref_count);
 
 	dc_ctx->cgs_device = init_params->cgs_device;
 	dc_ctx->driver_context = init_params->driver;
@@ -663,13 +661,11 @@ bool dc_validate_guaranteed(
 	if (!dc_validate_stream(dc, stream))
 		return false;
 
-	context = dm_alloc(sizeof(struct dc_state));
+	context = dc_create_state();
 	if (context == NULL)
 		goto context_alloc_fail;
 
 	dc_resource_state_construct(dc, dc->current_state);
-
-	atomic_inc(&context->ref_count);
 
 	result = dc->res_pool->funcs->validate_guaranteed(
 					dc, stream, context);
@@ -991,6 +987,17 @@ bool dc_commit_planes_to_stream(
 	return true;
 }
 
+struct dc_state *dc_create_state(void)
+{
+	struct dc_state *context = dm_alloc(sizeof(struct dc_state));
+
+	if (!context)
+		return NULL;
+
+	atomic_inc(&context->ref_count);
+	return context;
+}
+
 void dc_retain_state(struct dc_state *context)
 {
 	ASSERT(atomic_read(&context->ref_count) > 0);
@@ -1269,13 +1276,11 @@ void dc_update_planes_and_stream(struct dc *dc,
 			new_planes[i] = srf_updates[i].surface;
 
 		/* initialize scratch memory for building context */
-		context = dm_alloc(sizeof(*context));
+		context = dc_create_state();
 		if (context == NULL) {
 			DC_ERROR("Failed to allocate new validate context!\n");
 			return;
 		}
-
-		atomic_inc(&context->ref_count);
 
 		dc_resource_state_copy_construct(
 				dc->current_state, context);

drivers/gpu/drm/amd/display/dc/dc.h
@@ -754,6 +754,7 @@ enum surface_update_type dc_check_update_surfaces_for_stream(
 		const struct dc_stream_status *stream_status);
 
 
+struct dc_state *dc_create_state(void);
 void dc_retain_state(struct dc_state *context);
 void dc_release_state(struct dc_state *context);
 