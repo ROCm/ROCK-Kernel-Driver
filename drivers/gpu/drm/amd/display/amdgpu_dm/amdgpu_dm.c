94cfc05270f2735ebc73af0a6b33e387fbc6b75d
Harry Wentland <harry.wentland@amd.com>
2017-12-18 12:01:30 -0500

    drm/amd/display: Call update_stream_signal directly from amdgpu_dm
    There's no good place in DC to cover all place where stream signal should
    be updated. update_stream_signal depends on timing which comes from DM.
    
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -2443,6 +2443,7 @@ create_stream_for_sink(struct amdgpu_dm_connector *aconnector,
 				dm_state ? (dm_state->scaling != RMX_OFF) : false);
 	}
 
+	drm_mode_set_crtcinfo(&mode, 0);
 	fill_stream_properties_from_drm_display_mode(stream,
 			&mode, &aconnector->base);
 	update_stream_scaling_settings(&mode, dm_state, stream);
@@ -2451,6 +2452,8 @@ create_stream_for_sink(struct amdgpu_dm_connector *aconnector,
 		&stream->audio_info,
 		drm_connector,
 		aconnector->dc_sink);
+
+	update_stream_signal(stream);
 
 	return stream;
 }
@@ -2855,13 +2858,6 @@ int amdgpu_dm_connector_mode_valid(struct drm_connector *connector,
 		DRM_ERROR("Failed to create stream for sink!\n");
 		goto fail;
 	}
-
-	drm_mode_set_crtcinfo(mode, 0);
-	fill_stream_properties_from_drm_display_mode(stream, mode, connector);
-
-	stream->src.width = mode->hdisplay;
-	stream->src.height = mode->vdisplay;
-	stream->dst = stream->src;
 
 	dc_result = dc_validate_stream(adev->dm.dc, stream);
 
