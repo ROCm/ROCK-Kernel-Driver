4307dcb4fff604f60131d85141048928d94d5256
Jiri Slaby <jslaby@suse.cz>
2018-01-02 15:14:16 +0100

    bpf: prevent speculative execution in eBPF interpreter (bnc#1068032 CVE-2017-5753).
    suse-commit: 8464d17b0992c0a45986fc025c3b5e0ea2f6a8f1

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -33,6 +33,7 @@
 #include <linux/rcupdate.h>
 
 #include <asm/unaligned.h>
+#include <asm/barrier.h>
 
 /* Registers */
 #define BPF_R0	regs[BPF_REG_0]
@@ -939,6 +940,7 @@ select_insn:
 		DST = IMM;
 		CONT;
 	LD_IMM_DW:
+		osb();
 		DST = (u64) (u32) insn[0].imm | ((u64) (u32) insn[1].imm) << 32;
 		insn++;
 		CONT;
@@ -1200,6 +1202,7 @@ out:
 		*(SIZE *)(unsigned long) (DST + insn->off) = IMM;	\
 		CONT;							\
 	LDX_MEM_##SIZEOP:						\
+		osb();							\
 		DST = *(SIZE *)(unsigned long) (SRC + insn->off);	\
 		CONT;
 
