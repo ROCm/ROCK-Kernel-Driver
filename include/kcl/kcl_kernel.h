d1f6302cc0085bfe0358b3497b04bf9117b7a256
Kevin Wang <Kevin1.Wang@amd.com>
2018-01-23 15:33:23 +0800

    drm/amdkcl: check whether __GFP_RETRY_MAYFAIL is available
    kernel 4.13: rename __GFP_REPEAT to __GFP_RETRY_MAYFAIL
    
    Reviewed-by: Le Ma <Le.Ma@amd.com>
    Reviewed-by: Roger He <Hongbo.He@amd.com>
    Signed-Off-by: Kevin Wang <Kevin1.Wang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_kernel.h
+++ include/kcl/kcl_kernel.h
@@ -3,6 +3,7 @@
 #define AMDKCL_KERNEL_H
 
 #include <linux/kernel.h>
+#include <linux/gfp.h>
 
 /* Copied from include/linux/kernel.h */
 #ifndef u64_to_user_ptr
@@ -12,6 +13,10 @@
 	(void __user *)(uintptr_t)x;	\
 }					\
 )
+#endif
+
+#ifndef __GFP_RETRY_MAYFAIL
+#define __GFP_RETRY_MAYFAIL __GFP_REPEAT
 #endif
 
 #endif /* AMDKCL_KERNEL_H */
