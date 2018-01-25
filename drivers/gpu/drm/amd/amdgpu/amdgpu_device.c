7150e9386114afb9ba10539fbbbef425b7d7bf0d
Alex Deucher <alexander.deucher@amd.com>
2018-01-25 16:53:25 -0500

    drm/amdgpu: remove DC special casing for KB/ML
    It seems to be working now.
    
    Bug: https://bugs.freedesktop.org/show_bug.cgi?id=102372
    Reviewed-by: Mike Lothian <mike@fireburn.co.uk>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    
    Conflicts:
          drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
    
    Change-Id: I75ea89ea1751c6e533e849dd035382d0c8ecd1d5

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -1735,6 +1735,8 @@ bool amdgpu_device_asic_has_dc_support(enum amd_asic_type asic_type)
 	case CHIP_BONAIRE:
 	case CHIP_HAWAII:
 	case CHIP_KAVERI:
+	case CHIP_KABINI:
+	case CHIP_MULLINS:
 	case CHIP_CARRIZO:
 	case CHIP_STONEY:
 	case CHIP_POLARIS11:
@@ -1747,9 +1749,6 @@ bool amdgpu_device_asic_has_dc_support(enum amd_asic_type asic_type)
 #endif
 	case CHIP_VEGA10:
 		return amdgpu_dc != 0;
-	case CHIP_KABINI:
-	case CHIP_MULLINS:
-		return amdgpu_dc > 0;
 #endif
 #if defined(CONFIG_DRM_AMD_DC) && defined(CONFIG_DRM_AMD_DC_DCN1_0)
 	case CHIP_RAVEN:
