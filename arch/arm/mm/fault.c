fb576cf7e62d80623feac3b5aca8b278f236dbaa
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2018-01-26 14:08:19 +0100

    arm: Invalidate icache on prefetch abort outside of user mapping on Cortex-A15 (bsc#1068032).
    suse-commit: e591499c93291e12abc08a2b3bad3c067353dfc2

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -410,6 +410,10 @@ do_pabt_page_fault(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 		case ARM_CPU_PART_CORTEX_A17:
 			write_sysreg(0, BPIALL);
 			break;
+
+		case ARM_CPU_PART_CORTEX_A15:
+			write_sysreg(0, ICIALLU);
+			break;
 		}
 	}
 
