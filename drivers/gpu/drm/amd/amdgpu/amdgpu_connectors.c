23a77b2aa98545acfef693ceb7b108dc7e41c213
Junwei Zhang <Jerry.Zhang@amd.com>
2018-02-06 16:04:36 +0800

    drm/amdkcl: [4.15] fix drm_encoder_find()
    Change-Id: Ia57c22d973df13022f1445510376861404c38137
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    
    Conflicts:
          drivers/gpu/drm/amd/amdgpu/dce_virtual.c

--- drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c
@@ -233,7 +233,12 @@ amdgpu_connector_update_scratch_regs(struct drm_connector *connector,
 		if (connector->encoder_ids[i] == 0)
 			break;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	!defined(OS_NAME_SUSE_15)
+		encoder = drm_encoder_find(connector->dev,
+#else
 		encoder = drm_encoder_find(connector->dev, NULL,
+#endif
 					connector->encoder_ids[i]);
 		if (!encoder)
 			continue;
@@ -258,7 +263,12 @@ amdgpu_connector_find_encoder(struct drm_connector *connector,
 	for (i = 0; i < DRM_CONNECTOR_MAX_ENCODER; i++) {
 		if (connector->encoder_ids[i] == 0)
 			break;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	!defined(OS_NAME_SUSE_15)
+		encoder = drm_encoder_find(connector->dev,
+#else
 		encoder = drm_encoder_find(connector->dev, NULL,
+#endif
 					connector->encoder_ids[i]);
 		if (!encoder)
 			continue;
@@ -373,7 +383,12 @@ amdgpu_connector_best_single_encoder(struct drm_connector *connector)
 
 	/* pick the encoder ids */
 	if (enc_id)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	!defined(OS_NAME_SUSE_15)
+		return drm_encoder_find(connector->dev, enc_id);
+#else
 		return drm_encoder_find(connector->dev, NULL, enc_id);
+#endif
 	return NULL;
 }
 
@@ -1096,7 +1111,12 @@ amdgpu_connector_dvi_detect(struct drm_connector *connector, bool force)
 			if (connector->encoder_ids[i] == 0)
 				break;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	!defined(OS_NAME_SUSE_15)
+			encoder = drm_encoder_find(connector->dev, connector->encoder_ids[i]);
+#else
 			encoder = drm_encoder_find(connector->dev, NULL, connector->encoder_ids[i]);
+#endif
 			if (!encoder)
 				continue;
 
@@ -1155,7 +1175,12 @@ amdgpu_connector_dvi_encoder(struct drm_connector *connector)
 		if (connector->encoder_ids[i] == 0)
 			break;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	!defined(OS_NAME_SUSE_15)
+		encoder = drm_encoder_find(connector->dev, connector->encoder_ids[i]);
+#else
 		encoder = drm_encoder_find(connector->dev, NULL, connector->encoder_ids[i]);
+#endif
 		if (!encoder)
 			continue;
 
@@ -1174,7 +1199,12 @@ amdgpu_connector_dvi_encoder(struct drm_connector *connector)
 	/* then check use digitial */
 	/* pick the first one */
 	if (enc_id)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	!defined(OS_NAME_SUSE_15)
+		return drm_encoder_find(connector->dev, enc_id);
+#else
 		return drm_encoder_find(connector->dev, NULL, enc_id);
+#endif
 	return NULL;
 }
 
@@ -1317,7 +1347,12 @@ u16 amdgpu_connector_encoder_get_dp_bridge_encoder_id(struct drm_connector *conn
 		if (connector->encoder_ids[i] == 0)
 			break;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	!defined(OS_NAME_SUSE_15)
+		encoder = drm_encoder_find(connector->dev,
+#else
 		encoder = drm_encoder_find(connector->dev, NULL,
+#endif
 					connector->encoder_ids[i]);
 		if (!encoder)
 			continue;
@@ -1346,7 +1381,12 @@ static bool amdgpu_connector_encoder_is_hbr2(struct drm_connector *connector)
 	for (i = 0; i < DRM_CONNECTOR_MAX_ENCODER; i++) {
 		if (connector->encoder_ids[i] == 0)
 			break;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	!defined(OS_NAME_SUSE_15)
+		encoder = drm_encoder_find(connector->dev,
+#else
 		encoder = drm_encoder_find(connector->dev, NULL,
+#endif
 					connector->encoder_ids[i]);
 		if (!encoder)
 			continue;
