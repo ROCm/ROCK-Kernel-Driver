742d132e57093da2366ebdf68b5bfa89ad553090
Junwei Zhang <Jerry.Zhang@amd.com>
2018-02-06 16:23:33 +0800

    drm/amdkcl: [4.14] drm_hdmi_avi_infoframe_from_display_mode()
    Change-Id: I30975e98c1f94ab82008f6198e3a55edea8d5ae9
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
@@ -1487,7 +1487,11 @@ static void dce_v6_0_audio_set_avi_infoframe(struct drm_encoder *encoder,
 	ssize_t err;
 	u32 tmp;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0)
+	err = drm_hdmi_avi_infoframe_from_display_mode(&frame, mode);
+#else
 	err = drm_hdmi_avi_infoframe_from_display_mode(&frame, mode, false);
+#endif
 	if (err < 0) {
 		DRM_ERROR("failed to setup AVI infoframe: %zd\n", err);
 		return;
--- drivers/gpu/drm/amd/amdgpu/dce_v8_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v8_0.c
@@ -1675,7 +1675,11 @@ static void dce_v8_0_afmt_setmode(struct drm_encoder *encoder,
 	dce_v8_0_audio_write_sad_regs(encoder);
 	dce_v8_0_audio_write_latency_fields(encoder, mode);
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0)
+	err = drm_hdmi_avi_infoframe_from_display_mode(&frame, mode);
+#else
 	err = drm_hdmi_avi_infoframe_from_display_mode(&frame, mode, false);
+#endif
 	if (err < 0) {
 		DRM_ERROR("failed to setup AVI infoframe: %zd\n", err);
 		return;
