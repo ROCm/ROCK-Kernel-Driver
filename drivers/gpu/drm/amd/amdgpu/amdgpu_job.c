0580458096eecbaab2adffa45c7530f432c31f56
Junwei Zhang <Jerry.Zhang@amd.com>
2018-02-06 16:29:03 +0800

    drm/amdkcl: fix missing kcl fence funcs
    Change-Id: I7e1e17e5d9f4c51e8b2317c4275bbab184972f36
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_job.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_job.c
@@ -193,7 +193,7 @@ static struct dma_fence *amdgpu_job_run(struct drm_sched_job *sched_job)
 	trace_amdgpu_sched_run_job(job);
 
 	if (job->vram_lost_counter != atomic_read(&adev->vram_lost_counter))
-		dma_fence_set_error(finished, -ECANCELED);/* skip IB as well if VRAM lost */
+		kcl_dma_fence_set_error(finished, -ECANCELED);/* skip IB as well if VRAM lost */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
 	if (finished->status < 0) {
