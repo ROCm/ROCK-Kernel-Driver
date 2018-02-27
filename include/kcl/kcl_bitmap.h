1e21b4222f8931c05f3018578b7f7cd047049a1d
Roger He <Hongbo.He@amd.com>
2018-02-27 16:28:52 +0800

    drm/amd/amdkcl: [RHEL 7.5] fix bitmap_find_next_zero_area_off
    Signed-off-by: Roger He <Hongbo.He@amd.com>

--- include/kcl/kcl_bitmap.h
+++ include/kcl/kcl_bitmap.h
@@ -3,7 +3,8 @@
 
 #include <linux/version.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0) && \
+	!defined(OS_NAME_RHEL_7_5)
 unsigned long bitmap_find_next_zero_area_off(unsigned long *map,
 					     unsigned long size,
 					     unsigned long start,
