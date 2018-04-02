078fe9f0f8ffc2e4ea2107ac7492946cf705a02a
Jim Qu <Jim.Qu@amd.com>
2018-04-02 16:27:07 +0800

    drm/amdgpu: fix building error in RHEL7.5
    Signed-off-by: Jim Qu <Jim.Qu@amd.com>
    Reviewed-by: Xiaojie Yuan <Xiaojie.Yuan@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -593,7 +593,7 @@ static int init_user_pages(struct kgd_mem *mem, struct mm_struct *mm,
 	 */
 	WARN(mem->user_pages, "Leaking user_pages array");
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && !defined(OS_NAME_RHEL_7_5)
 	mem->user_pages = drm_calloc_large(bo->tbo.ttm->num_pages,
 					   sizeof(struct page *));
 #else
@@ -630,7 +630,7 @@ release_out:
 	if (ret)
 		release_pages(mem->user_pages, bo->tbo.ttm->num_pages, 0);
 free_out:
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && !defined(OS_NAME_RHEL_7_5)
 	drm_free_large(mem->user_pages);
 #else
 	kvfree(mem->user_pages);
@@ -1363,7 +1363,7 @@ int amdgpu_amdkfd_gpuvm_free_memory_of_gpu(
 		if (mem->user_pages[0])
 			release_pages(mem->user_pages,
 				      mem->bo->tbo.ttm->num_pages, 0);
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && !defined(OS_NAME_RHEL_7_5)
 		drm_free_large(mem->user_pages);
 #else
 		kvfree(mem->user_pages);
@@ -2016,7 +2016,7 @@ static int update_invalid_user_pages(struct amdkfd_process_info *process_info,
 		bo = mem->bo;
 
 		if (!mem->user_pages) {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && !defined(OS_NAME_RHEL_7_5)
 			mem->user_pages =
 				drm_calloc_large(bo->tbo.ttm->num_pages,
 						 sizeof(struct page *));
@@ -2150,7 +2150,7 @@ static int validate_invalid_user_pages(struct amdkfd_process_info *process_info)
 		 * the userptr_valid_list. If we need to revalidate
 		 * it, we need to start from scratch.
 		 */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && !defined(OS_NAME_RHEL_7_5)
 		drm_free_large(mem->user_pages);
 #else
 		kvfree(mem->user_pages);
