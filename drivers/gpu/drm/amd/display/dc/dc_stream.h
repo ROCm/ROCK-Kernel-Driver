d2f7e51f84f345d4eba39b0be26c30c6787422c5
Krunoslav Kovac <Krunoslav.Kovac@amd.com>
2018-05-08 16:03:58 -0400

    drm/amd/display: Dynamic HDR metadata mem buffer
    Basic framework:
    - caps for reporting dynamic HDR metadata support
    - allocation of frame buffer memory and storage
    
    Signed-off-by: Krunoslav Kovac <Krunoslav.Kovac@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Reviewed-by: Anthony Koo <Anthony.Koo@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc_stream.h
+++ drivers/gpu/drm/amd/display/dc/dc_stream.h
@@ -65,6 +65,8 @@ struct dc_stream_state {
 	struct audio_info audio_info;
 
 	struct dc_info_packet hdr_static_metadata;
+	PHYSICAL_ADDRESS_LOC dmdata_address;
+
 	struct dc_transfer_func *out_transfer_func;
 	struct colorspace_transform gamut_remap_matrix;
 	struct dc_csc_transform csc_color_matrix;
