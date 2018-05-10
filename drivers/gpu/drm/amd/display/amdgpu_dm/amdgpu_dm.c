08e36e959895e48d408be7e927a89efc6872fe57
Jerry (Fangzhi) Zuo <Jerry.Zuo@amd.com>
2018-05-10 13:05:05 -0400

    drm/amd/display: [rhel6.9] Fix fading gui issue
    Default ITF type is set by default to degamma bypass which doesn't
    support by RHEL6 leading to screwed up gamma setting.
    
    Pre-programmed SRGB degamma should be configured to RHEL6 for
    proper gamma setup.
    
    Signed-off-by: Jerry (Fangzhi) Zuo <Jerry.Zuo@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Acked-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -2279,6 +2279,10 @@ static int fill_plane_attributes(struct amdgpu_device *adev,
 		dc_plane_state->in_transfer_func = NULL;
 	}
 #else
+#if defined(OS_NAME_RHEL_6)
+	dc_plane_state->in_transfer_func->type = TF_TYPE_PREDEFINED;
+	dc_plane_state->in_transfer_func->tf = TRANSFER_FUNCTION_SRGB;
+#endif
 	if (crtc->mode.private_flags & AMDGPU_CRTC_MODE_PRIVATE_FLAGS_GAMMASET) {
 		fill_gamma_from_crtc(crtc, dc_plane_state);
 		/* reset trigger of gamma */
