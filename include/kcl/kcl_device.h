f28a70a9b2319f6020473f2ed7c67ebe0380e406
Slava Grigorev <slava.grigorev@amd.com>
2018-06-22 12:48:34 -0400

    drm/amdkcl: Test whether kobj_to_dev() is available
    Change-Id: I7b78fd51bf160a0ae9cca1900c1e0f3a2270b357
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- include/kcl/kcl_device.h
+++ include/kcl/kcl_device.h
@@ -1,7 +1,7 @@
 #ifndef AMDKCL_DEVICE_H
 #define AMDKCL_DEVICE_H
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 6, 0)
+#if !defined(HAVE_KOBJ_TO_DEV)
 static inline struct device *kobj_to_dev(struct kobject *kobj)
 {
 	return container_of(kobj, struct device, kobj);
