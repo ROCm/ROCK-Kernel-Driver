1b21182b9a75b72b41b9eba4da776286ae6d8585
Slava Grigorev <slava.grigorev@amd.com>
2018-06-25 17:19:50 -0400

    drm/amdkcl: Test whether drm_atomic_helper_disable_all() is available
    v1: drm/amdkcl: Test whether drm_helper_force_disable_all() is defined
    v2: drm/amdkcl: Test whether drm_helper_force_disable_all() is defined
    v3: drm/amdkcl: fix drm_helper_force_disable_all
    
    Change-Id: I39de6588b082d9b33ce52c46fc3a0ac9405a511e
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Adam Yang <Adam1.Yang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_drm.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm.c
@@ -30,3 +30,43 @@ int drm_modeset_lock_all_ctx(struct drm_device *dev,
 EXPORT_SYMBOL(drm_modeset_lock_all_ctx);
 #endif
 
+#if !defined(HAVE_DRM_ATOMIC_HELPER_DISABLE_ALL)
+int drm_atomic_helper_disable_all(struct drm_device *dev,
+				  struct drm_modeset_acquire_ctx *ctx)
+{
+	struct drm_atomic_state *state;
+	struct drm_connector *conn;
+	int err;
+
+	state = drm_atomic_state_alloc(dev);
+	if (!state)
+		return -ENOMEM;
+
+	state->acquire_ctx = ctx;
+
+	drm_for_each_connector(conn, dev) {
+		struct drm_crtc *crtc = conn->state->crtc;
+		struct drm_crtc_state *crtc_state;
+
+		if (!crtc || conn->dpms != DRM_MODE_DPMS_ON)
+			continue;
+
+		crtc_state = drm_atomic_get_crtc_state(state, crtc);
+		if (IS_ERR(crtc_state)) {
+			err = PTR_ERR(crtc_state);
+			goto free;
+		}
+
+		crtc_state->active = false;
+	}
+
+	err = drm_atomic_commit(state);
+
+free:
+	if (err < 0)
+		drm_atomic_state_free(state);
+
+	return err;
+}
+EXPORT_SYMBOL(drm_atomic_helper_disable_all);
+#endif
