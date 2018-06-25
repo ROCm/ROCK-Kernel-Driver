f11c9b61fbfc615daba7c1474074f59c8fc954d7
Slava Grigorev <slava.grigorev@amd.com>
2018-06-25 17:19:50 -0400

    drm/amdkcl: Test whether drm_atomic_helper_disable_all() is available
    v1: drm/amdkcl: Test whether drm_helper_force_disable_all() is defined
    v2: drm/amdkcl: Test whether drm_helper_force_disable_all() is defined
    v3: drm/amdkcl: fix drm_helper_force_disable_all
    v4: split to kcl_drm_atomic_helper.[ch]
    
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Adam Yang <Adam1.Yang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

---
+++ include/kcl/kcl_drm_atomic_helper.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef AMDKCL_DRM_ATOMIC_HELPER_H
+#define AMDKCL_DRM_ATOMIC_HELPER_H
+
+#include <drm/drm_atomic_helper.h>
+#include <drm/drm_atomic.h>
+#include <drm/drm_plane_helper.h>
+#include <kcl/kcl_drm_crtc.h>
+
+#if !defined(HAVE_DRM_ATOMIC_HELPER_DISABLE_ALL)
+int drm_atomic_helper_disable_all(struct drm_device *dev,
+				  struct drm_modeset_acquire_ctx *ctx);
+#endif
+
+#endif
