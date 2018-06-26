71cde2ce3da62dd177347634a5299b251280a1cc
Prike Liang <Prike.Liang@amd.com>
2018-06-26 10:31:19 +0800

    drm/amdkcl: Test whether vmf_insert_*() functions are available
    drm/amdkcl: [4.16] add kcl/kcl_mm_types.h file for ttm compatibility
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Reviewed-by: Tao Zhou <Tao.Zhou1@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: Test whether vmf_insert_*() functions are available
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amd/autoconf: refactor kcl_mm_types.h
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: drop HAVE_PFN_T check outside of kcl
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    
    drm/amdkcl: move kcl copy of vmf_* to kcl_mm.h
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: fix license for kcl part
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/ttm/backport/backport.h
+++ drivers/gpu/drm/ttm/backport/backport.h
@@ -9,4 +9,5 @@
 #include <kcl/backport/kcl_fence_backport.h>
 #include <kcl/backport/kcl_drm_vma_manager_backport.h>
 #include <kcl/kcl_drm_print.h>
+#include <kcl/backport/kcl_mm_backport.h>
 #endif
