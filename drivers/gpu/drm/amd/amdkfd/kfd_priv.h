b1ea4c33cd21f23d45d87a88c45c620c0176e915
Yong Zhao <Yong.Zhao@amd.com>
2018-08-03 15:59:40 -0400

    drm/amdkfd: Fix ATS capablity was not reported correctly on some APUs
    Because CRAT_CU_FLAGS_IOMMU_PRESENT was not set in some BIOS crat, we
    need to workaround this.
    
    For future compatibility, we also overwrite the bit in capability according
    to the value of needs_iommu_device.
    
    Change-Id: I4f5c8fe745222904013ca1dbfb8c851c0d5d791b
    Signed-off-by: Yong Zhao <Yong.Zhao@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_priv.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_priv.h
@@ -949,6 +949,7 @@ int kfd_topology_add_device(struct kfd_dev *gpu);
 int kfd_topology_remove_device(struct kfd_dev *gpu);
 struct kfd_topology_device *kfd_topology_device_by_proximity_domain(
 						uint32_t proximity_domain);
+struct kfd_topology_device *kfd_topology_device_by_id(uint32_t gpu_id);
 struct kfd_dev *kfd_device_by_id(uint32_t gpu_id);
 struct kfd_dev *kfd_device_by_pci_dev(const struct pci_dev *pdev);
 struct kfd_dev *kfd_device_by_kgd(const struct kgd_dev *kgd);
