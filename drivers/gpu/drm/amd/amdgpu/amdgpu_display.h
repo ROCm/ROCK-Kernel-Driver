dd190ca49fd84829582ceb8ed0bab8d560ad17fa
Kevin Wang <Kevin1.Wang@amd.com>
2018-08-13 17:21:10 +0800

    drm/amdkcl: [4.16] fix drm .last code, .output_poll_changed conflict
    Change-Id: I34c9e4815e34d037163e6f420812dd46f124839a

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.h
@@ -44,6 +44,10 @@ struct drm_framebuffer *
 amdgpu_display_user_framebuffer_create(struct drm_device *dev,
 				       struct drm_file *file_priv,
 				       const struct drm_mode_fb_cmd2 *mode_cmd);
+#if DRM_VERSION_CODE < DRM_VERSION(4, 16, 0)
+void amdgpu_output_poll_changed(struct drm_device *dev);
+#endif
+
 int amdgpu_sem_ioctl(struct drm_device *dev, void *data,
 		     struct drm_file *filp);
 
