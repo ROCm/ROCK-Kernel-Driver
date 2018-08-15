74549552214b71779935f3f821369158acf6e1d6
Kevin Wang <Kevin1.Wang@amd.com>
2018-08-15 17:21:02 +0800

    drm/amdkcl: fake drm_gem_fb_get_obj & kcl_drm_gem_fb_set_obj
    drm_gem_fb_get_obj() is introdued in v4.13-rc2-421-g4c3dbb2c312c. same
    with include/drm/drm_gem_framebuffer_helper.h
    fake a kcl copy for legacy kernel.
    
    Squash of
    e1357d7a01b8 drm/amdkcl: fake drm_gem_fb_get_obj &
    kcl_drm_gem_fb_set_obj
    c5ec212d2f24 drm/amdkcl: [4.14] fix drm_gem_object compile error
    
    Reviewed-by: Kevin Wang <Kevin1.Wang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yang Xiong <Yang.Xiong@amd.com>
    Change-Id: I2c087d77e289caabc6f5215f2bd54dc0e33e634e
    Signed-off-by: Kevin Wang <Kevin1.Wang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -1029,7 +1029,7 @@ static void amdgpu_dm_plane_helper_cleanup_fb(struct drm_plane *plane,
 	if (!old_state->fb)
 		return;
 
-	rbo = gem_to_amdgpu_bo(old_state->fb->obj[0]);
+	rbo = gem_to_amdgpu_bo(drm_gem_fb_get_obj(old_state->fb, 0));
 	r = amdgpu_bo_reserve(rbo, false);
 	if (unlikely(r)) {
 		DRM_ERROR("failed to reserve rbo before unpin\n");
