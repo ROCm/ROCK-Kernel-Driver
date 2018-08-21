ec3a6e27e5356899bddd0a885a89d7ec91d82e5e
Leslie Shi <Yuliang.Shi@amd.com>
2018-08-21 17:35:56 +0800

    drm/amdgpu: [hybrid] add direct gma(dgma) support
    v2: rebase on linux 4.18 and cleanup
    v3: 3f8d459ca0f1("drm/amdgpu: [hybrid] fix DGMA buffer info loss issue
    		(v2)")
    
    Signed-off-by: Flora Cui <Flora.Cui@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com> (v2)
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    
    Signed-off-by: Tianci.Yin <tianci.yin@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_object.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_object.h
@@ -111,6 +111,10 @@ struct amdgpu_bo {
 #endif
 	struct kgd_mem                  *kfd_bo;
 
+	/* DGMA imported buffer info */
+	void				*dgma_addr;
+	phys_addr_t			dgma_import_base;
+
 	/*
 	 * For GPUs with spatial partitioning, xcp partition number, -1 means
 	 * any partition. For other ASICs without spatial partition, always 0
@@ -125,7 +129,6 @@ struct amdgpu_bo_user {
 	u64				metadata_flags;
 	void				*metadata;
 	u32				metadata_size;
-
 };
 
 struct amdgpu_bo_vm {
@@ -182,6 +185,10 @@ static inline unsigned amdgpu_mem_type_to_domain(u32 mem_type)
 		return AMDGPU_GEM_DOMAIN_GWS;
 	case AMDGPU_PL_OA:
 		return AMDGPU_GEM_DOMAIN_OA;
+	case AMDGPU_PL_DGMA:
+		return AMDGPU_GEM_DOMAIN_DGMA;
+	case AMDGPU_PL_DGMA_IMPORT:
+		return AMDGPU_GEM_DOMAIN_DGMA_IMPORT;
 	default:
 		break;
 	}
