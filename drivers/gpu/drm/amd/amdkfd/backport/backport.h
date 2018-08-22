651df255e3a32a3808e8d927a2a58e0e6e952985
Kevin Wang <Kevin1.Wang@amd.com>
2018-08-22 12:42:06 +0800

    drm/amdkcl: [KFD] add kfd dkms support
    Change-Id: I169acf15f7b22178c9c8a75e7670494bfa8025c7

---
+++ drivers/gpu/drm/amd/amdkfd/backport/backport.h
@@ -0,0 +1,16 @@
+#ifndef AMDKFD_BACKPORT_H
+#define AMDKFD_BACKPORT_H
+
+#include <linux/version.h>
+#if defined(BUILD_AS_DKMS)
+#include <kcl/kcl_amd_asic_type.h>
+#endif
+#include <kcl/kcl_compat.h>
+#include <kcl/kcl_pci.h>
+#include <kcl/kcl_mn.h>
+#include <kcl/kcl_fence.h>
+#include <kcl/kcl_wait.h>
+#include <kcl/kcl_idr.h>
+#include <kcl/kcl_mm.h>
+
+#endif
