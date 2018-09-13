33a44fd5e8df6832400eb8b92e563932eb1e0f90
Flora Cui <Flora.Cui@amd.com>
2018-09-13 15:01:36 +0800

    drm/amdkcl: Enable PCIe Extended Tags if supported
    commit 60db3a4d8cc9 ("PCI: Enable PCIe Extended Tags if supported") fix rocm bandwidth test H2D case.
    add the change for kernel < 4.11
    
    v2: export _kcl_pci_configure_extended_tags for dkms support.
    v3: amd/amdkcl: make sure to enable PCIe Extended Tags if supported
    
    Signed-off-by: Flora Cui <Flora.Cui@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -1301,6 +1301,7 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 	if (!supports_atomic)
 		ddev->driver_features &= ~DRIVER_ATOMIC;
 
+	kcl_pci_configure_extended_tags(pdev);
 	ret = pci_enable_device(pdev);
 	if (ret)
 		return ret;
