d80799caab840714474c8a957ef813ebc3de3285
Anatoli Antonovitch <anatoli.antonovitch@amd.com>
2018-09-25 12:50:23 -0400

    drm/amdkcl: Test whether drm_is_current_master() is available
    v1: drm/amdkcl: refactor check for drm_is_current_master() & drm_auth.h
    v2: drm/amdkcl: accommodate to drmP.h removal for drm-is-current-master.m4
    v3: drm/amdkcl: mv drm_auth related stuff to kcl_drm_auth
        and fix license
    
    Change-Id: I3b93e57e0da514a64e60b3e8f691ee7200f42db6
    Signed-off-by: Anatoli Antonovitch <anatoli.antonovitch@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -35,6 +35,7 @@
 #include <kcl/backport/kcl_ttm_tt_backport.h>
 #include <kcl/backport/kcl_drm_cache_backport.h>
 #include <kcl/kcl_drm_print.h>
+#include <kcl/kcl_drm_auth.h>
 #include <kcl/backport/kcl_drm_crtc.h>
 #include <kcl/backport/kcl_drm_plane.h>
 #include <kcl/backport/kcl_drm_backport.h>
