193529cc07c7ce64e389c0f901b2eb149ac74a1d
Christian König <christian.koenig@amd.com>
2018-09-26 13:45:38 +0200

    drm/amdgpu: add support for processing IH ring 1 & 2
    Previously we only added the ring buffer memory, now add the handling as
    well.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_irq.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_irq.h
@@ -91,7 +91,9 @@ struct amdgpu_irq {
 
 	/* interrupt rings */
 	struct amdgpu_ih_ring		ih, ih1, ih2;
-	const struct amdgpu_ih_funcs	*ih_funcs;
+	const struct amdgpu_ih_funcs    *ih_funcs;
+	struct work_struct		ih1_work, ih2_work;
+	struct amdgpu_irq_src		self_irq;
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 1, 0)
 	/* gen irq stuff */
