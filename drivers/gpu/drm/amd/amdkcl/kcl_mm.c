6268e7207c8eda67e7243485507c46b0b79561a8
Christian König <christian.koenig@amd.com>
2018-09-26 11:50:09 +0200

    drm/amdgpu: move IV prescreening into the GMC code
    The GMC/VM subsystem is causing the faults, so move the handling here as
    well.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/kcl_mm.c
@@ -0,0 +1,16 @@
+#include <linux/version.h>
+#include "kcl_common.h"
+
+struct mm_struct* (*_kcl_mm_access)(struct task_struct *task, unsigned int mode);
+EXPORT_SYMBOL(_kcl_mm_access);
+
+static struct mm_struct * __kcl_mm_access_stub(struct task_struct *task, unsigned int mode)
+{
+	printk_once(KERN_WARNING "This kernel version not support API: mm_access !\n");
+	return NULL;
+}
+
+void kcl_mm_init()
+{
+	_kcl_mm_access = amdkcl_fp_setup("mm_access", __kcl_mm_access_stub);
+}
