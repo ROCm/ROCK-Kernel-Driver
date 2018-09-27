07dbecbfad5d105282d0fe79300f0a0f3c3577e9
Kevin Wang <Kevin1.Wang@amd.com>
2018-09-27 14:32:27 +0800

    drm/amdkcl: Test whether drm_encoder_find() wants file_priv
    v1: drm/amdkcl: [4.15] fix drm_encoder_find error
    v2: drm/amdkcl: [4.7] Adapt drm_atomic_helper_best_encoder for common use
    v3: split to kcl_drm_encoder.h
    v4: fix license for kcl drm part
    
    Signed-off-by: Anatoli Antonovitch <anatoli.antonovitch@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Kevin Wang <Kevin1.Wang@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -40,5 +40,6 @@
 #include <kcl/backport/kcl_drm_vma_manager_backport.h>
 #include <kcl/backport/kcl_drm_fb.h>
 #include <kcl/kcl_drm_modes.h>
+#include <kcl/backport/kcl_drm_encoder.h>
 
 #endif /* AMDGPU_BACKPORT_H */
