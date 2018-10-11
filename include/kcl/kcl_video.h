0ceb7ca88a1913daf63a87a7d006b465b7e16c3a
Prike Liang <Prike.Liang@amd.com>
2018-10-11 13:46:20 +0800

    drm/amdkcl: [4.10] Add ACPI_VIDEO_NOTIFY_PROBE define
    Change-Id: I1238e5b5c75851a156674c87f3a15e0a70cef3ca
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

---
+++ include/kcl/kcl_video.h
@@ -0,0 +1,10 @@
+#ifndef AMDKCL_VIDEO_H
+#define AMDKCL_VIDEO_H
+
+#include <acpi/video.h>
+
+#ifndef ACPI_VIDEO_NOTIFY_PROBE
+#define ACPI_VIDEO_NOTIFY_PROBE		0x81
+#endif
+
+#endif/*AMDKCL_VIDEO_H*/
