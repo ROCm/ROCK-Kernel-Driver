cbdb90c482e4a6df7f749b7f8981c22be5d66e87
Prike Liang <Prike.Liang@amd.com>
2018-10-11 12:59:05 +0800

    drm/amdkcl: [4.18] Add list_bulk_move_tail for comnon use
    Change-Id: Id0dbd31b5869bae48137a11466375da2157b4b68
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>

---
+++ include/kcl/kcl_list.h
@@ -0,0 +1,21 @@
+#ifndef AMDKCL_LIST_H
+#define AMDKCL_LIST_H
+
+#if defined(BUILD_AS_DKMS) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 18, 0)
+#include <linux/list.h>
+static inline void list_bulk_move_tail(struct list_head *head,
+				       struct list_head *first,
+				       struct list_head *last)
+{
+		first->prev->next = last->next;
+		last->next->prev = first->prev;
+
+		head->prev->next = first;
+		first->prev = head->prev;
+
+		last->next = head;
+		head->prev = last;
+}
+#endif
+
+#endif /*AMDKCL_LIST_H*/
