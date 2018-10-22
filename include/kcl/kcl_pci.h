fccc6dd24acc49e77373063f80ffe3b889166bba
Prike Liang <Prike.Liang@amd.com>
2018-10-22 17:08:04 +0800

    drm/amdkcl: [4.13] Add sysfs node max_link_speed/width, current_link_speed/width
    For SWDEV-165476 require and implement the feature inquire node.
    KCL originally implemented as commit 02805f8d0d76 to add sysfs node for
    max_link_speed, max_link_width, current_link_speed, current_link_width
    proposed in commit 56c1af4606f0.
    Since most of the attributes introduced in the commit 56c1af4606f0 are static
    and only visible within the PCI compile unit, we cannot use autoconf to
    check if they are defined. In this commit, we use the macro
    PCI_EXP_LNKCAP_SLS_8_0GB checked-in along with these attributes as a
    workaround and further check the return value after calling
    device_create_file() by deliberately ignoring the -EEXIST error code to
    make sure the error log is not mis-reported.
    
    v1: drm/amdkcl: Add sysfs node max_link_speed/width, current_link_speed/width
    v2: drm/amdkcl: add macros in kcl_pci.h if checked undefinded
    v3: add macro undefined check for remove BUILD_AS_DKMS macro
    v4: drm/amdkcl: remove sysfs entry on exit
    
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Reviewed-by: xinhui pan <xinhui.pan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Adam Yang <Adam1.Yang@amd.com>
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- include/kcl/kcl_pci.h
+++ include/kcl/kcl_pci.h
@@ -26,6 +26,58 @@
 #include <linux/pci.h>
 #include <linux/version.h>
 
+#ifndef PCI_EXP_DEVCAP2_ATOMIC_ROUTE
+#define PCI_EXP_DEVCAP2_ATOMIC_ROUTE	0x00000040 /* Atomic Op routing */
+#endif
+#ifndef PCI_EXP_DEVCAP2_ATOMIC_COMP32
+#define PCI_EXP_DEVCAP2_ATOMIC_COMP32	0x00000080 /* 32b AtomicOp completion */
+#endif
+#ifndef PCI_EXP_DEVCAP2_ATOMIC_COMP64
+#define PCI_EXP_DEVCAP2_ATOMIC_COMP64	0x00000100 /* 64b AtomicOp completion*/
+#endif
+#ifndef PCI_EXP_DEVCAP2_ATOMIC_COMP128
+#define PCI_EXP_DEVCAP2_ATOMIC_COMP128	0x00000200 /* 128b AtomicOp completion*/
+#endif
+#ifndef PCI_EXP_DEVCTL2_ATOMIC_REQ
+#define PCI_EXP_DEVCTL2_ATOMIC_REQ	0x0040	/* Set Atomic requests */
+#endif
+#ifndef PCI_EXP_DEVCTL2_ATOMIC_BLOCK
+#define PCI_EXP_DEVCTL2_ATOMIC_BLOCK	0x0040	/* Block AtomicOp on egress */
+#endif
+
+#ifndef PCIE_SPEED_16_0GT
+#define PCIE_SPEED_16_0GT		0x17
+#endif
+#ifndef PCI_EXP_LNKCAP2_SLS_16_0GB
+#define PCI_EXP_LNKCAP2_SLS_16_0GB	0x00000010	/* Supported Speed 16GT/s */
+#endif
+#ifndef PCI_EXP_LNKCAP_SLS_16_0GB
+#define PCI_EXP_LNKCAP_SLS_16_0GB	0x00000004	/* LNKCAP2 SLS Vector bit 3 */
+#endif
+#ifndef PCI_EXP_LNKSTA_CLS_16_0GB
+#define PCI_EXP_LNKSTA_CLS_16_0GB	0x0004		/* Current Link Speed 16.0GT/s */
+#endif
+
+/* PCIe link information */
+#ifndef PCIE_SPEED2STR
+#define PCIE_SPEED2STR(speed) \
+	((speed) == PCIE_SPEED_16_0GT ? "16 GT/s" : \
+	(speed) == PCIE_SPEED_8_0GT ? "8 GT/s" : \
+	(speed) == PCIE_SPEED_5_0GT ? "5 GT/s" : \
+	(speed) == PCIE_SPEED_2_5GT ? "2.5 GT/s" : \
+	"Unknown speed")
+#endif
+
+/* PCIe speed to Mb/s reduced by encoding overhead */
+#ifndef PCIE_SPEED2MBS_ENC
+#define PCIE_SPEED2MBS_ENC(speed) \
+	((speed) == PCIE_SPEED_16_0GT ? 16000*128/130 : \
+	(speed) == PCIE_SPEED_8_0GT  ?  8000*128/130 : \
+	(speed) == PCIE_SPEED_5_0GT  ?  5000*8/10 : \
+	(speed) == PCIE_SPEED_2_5GT  ?  2500*8/10 : \
+	0)
+#endif
+
 #if !defined(HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP)
 extern enum pci_bus_speed (*_kcl_pcie_get_speed_cap)(struct pci_dev *dev);
 extern enum pcie_link_width (*_kcl_pcie_get_width_cap)(struct pci_dev *dev);
@@ -118,5 +170,28 @@ static inline bool pci_pr3_present(struct pci_dev *pdev)
 static inline bool pci_pr3_present(struct pci_dev *pdev) { return false; }
 #endif
 #endif /* HAVE_PCI_PR3_PRESENT */
+
+#ifndef PCI_EXP_LNKCAP_SLS_8_0GB
+#define AMDKCL_CREATE_MEASURE_FILE
+#define  PCI_EXP_LNKCAP_SLS_8_0GB 0x00000003 /* LNKCAP2 SLS Vector bit 2 */
+int  _kcl_pci_create_measure_file(struct pci_dev *pdev);
+void _kcl_pci_remove_measure_file(struct pci_dev *pdev);
+#endif
+
+static inline int kcl_pci_create_measure_file(struct pci_dev *pdev)
+{
+#ifdef AMDKCL_CREATE_MEASURE_FILE
+	return _kcl_pci_create_measure_file(pdev);
+#else
+	return 0;
+#endif
+}
+
+static inline void kcl_pci_remove_measure_file(struct pci_dev *pdev)
+{
+#ifdef AMDKCL_CREATE_MEASURE_FILE
+	_kcl_pci_remove_measure_file(pdev);
+#endif
+}
 
 #endif /* AMDKCL_PCI_H */
