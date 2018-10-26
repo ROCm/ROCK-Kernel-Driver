e75ca4e9eed7a08ac06cb9da1105a465ef9abbd6
Christian König <christian.koenig@amd.com>
2018-10-26 15:59:05 +0200

    drm/amdgpu: wait for IB test on first device open
    Instead of delaying that to the first query. Otherwise we could try to use the
    SDMA for VM updates before the IB tests are done.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Chunming Zhou <david1.zhou@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -479,9 +479,6 @@ static int amdgpu_info_ioctl(struct drm_device *dev, void *data, struct drm_file
 	if (!info->return_size || !info->return_pointer)
 		return -EINVAL;
 
-	/* Ensure IB tests are run on ring */
-	flush_delayed_work(&adev->late_init_work);
-
 	switch (info->query) {
 	case AMDGPU_INFO_VIRTUAL_RANGE: {
 		struct drm_amdgpu_virtual_range range_info;
@@ -998,6 +995,9 @@ int amdgpu_driver_open_kms(struct drm_device *dev, struct drm_file *file_priv)
 	struct amdgpu_device *adev = dev->dev_private;
 	struct amdgpu_fpriv *fpriv;
 	int r, pasid;
+
+	/* Ensure IB tests are run on ring */
+	flush_delayed_work(&adev->late_init_work);
 
 	file_priv->driver_priv = NULL;
 
