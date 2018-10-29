252e726c5a8d50d59f0d8f72e8a919adbc5606d9
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2018-10-29 12:23:06 +0100

    drm/amdgpu: print an error when the parser can't be initialized
    Similar to other error messages, might help for tracking down
    issues.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
@@ -1355,7 +1355,7 @@ int amdgpu_cs_ioctl(struct drm_device *dev, void *data, struct drm_file *filp)
 
 	r = amdgpu_cs_parser_init(&parser, data);
 	if (r) {
-		DRM_ERROR("Failed to initialize parser !\n");
+		DRM_ERROR("Failed to initialize parser %d!\n", r);
 		goto out;
 	}
 
