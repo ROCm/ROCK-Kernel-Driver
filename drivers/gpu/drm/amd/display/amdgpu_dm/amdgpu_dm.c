be78cdf2dde64dd4f5d8f62efb05a2df0a3d4d34
changzhu <Changfeng.Zhu@amd.com>
2018-11-14 18:40:36 +0800

    drm/amdkcl: [4.2] fix build error in amdgpu_dm.c
    Change-Id: If6e2942353278b141a0f0de46386f08993694f9d
    
    This commit is used to fix build [4.2] error in amdgpu_dm.c in redhat 7.2
    
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Amber Lin <Amber.Lin@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -1023,12 +1023,15 @@ dm_atomic_state_alloc(struct drm_device *dev)
 
 	if (!state)
 		return NULL;
-
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 2, 0)
 	if (drm_atomic_state_init(dev, &state->base) < 0)
 		goto fail;
 
 	return &state->base;
-
+#else
+	DRM_DEBUG_ATOMIC("Allocate atomic state %p\n", state);
+	return (struct drm_atomic_state *)state;
+#endif
 fail:
 	kfree(state);
 	return NULL;
@@ -1043,15 +1046,22 @@ dm_atomic_state_clear(struct drm_atomic_state *state)
 		dc_release_state(dm_state->context);
 		dm_state->context = NULL;
 	}
-
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 2, 0)
 	drm_atomic_state_default_clear(state);
+#else
+	drm_atomic_state_clear(state);
+#endif
 }
 
 static void
 dm_atomic_state_alloc_free(struct drm_atomic_state *state)
 {
 	struct dm_atomic_state *dm_state = to_dm_atomic_state(state);
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 2, 0)
 	drm_atomic_state_default_release(state);
+#else
+	drm_atomic_state_free(state);
+#endif
 	kfree(dm_state);
 }
 
@@ -2022,11 +2032,12 @@ retry:
 		ret = drm_atomic_add_affected_connectors(state, crtc);
 		if (ret)
 			goto fail;
-
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 2, 0)
 		/* TODO rework amdgpu_dm_commit_planes so we don't need this */
 		ret = drm_atomic_add_affected_planes(state, crtc);
 		if (ret)
 			goto fail;
+#endif
 	}
 
 #if DRM_VERSION_CODE < DRM_VERSION(4, 12, 0)
@@ -2219,6 +2230,14 @@ static int dm_early_init(void *handle)
 
 #if DRM_VERSION_CODE < DRM_VERSION(4, 6, 0)
 #define AMDGPU_CRTC_MODE_PRIVATE_FLAGS_GAMMASET 1
+#endif
+
+#if DRM_VERSION_CODE < DRM_VERSION(4, 2, 0)
+static inline bool
+drm_atomic_crtc_needs_modeset(struct drm_crtc_state *state)
+{
+	return state->mode_changed || state->active_changed;
+}
 #endif
 
 static bool modeset_required(struct drm_crtc_state *crtc_state,
@@ -6118,10 +6137,11 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 		ret = drm_atomic_add_affected_connectors(state, crtc);
 		if (ret)
 			return ret;
-
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 2, 0)
 		ret = drm_atomic_add_affected_planes(state, crtc);
 		if (ret)
 			goto fail;
+#endif
 	}
 
 	dm_state->context = dc_create_state();
