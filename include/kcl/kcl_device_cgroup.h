7d070bf0e9ab6e00c9f0eeb96481f59b90e8cda9
changzhu <Changfeng.Zhu@amd.com>
2018-11-20 14:07:20 +0800

    drm/amdkcl: fix devcgroup_check_permission modprobe error on 4.18.0
    Change-Id: I0b846fc7031da8ed84144cbe6927815489cb9c66
    
    This commit is used to fix unknown symbol __devcgroup_check_permission
    when modprobe amdgpu driver on 4.18.0
    
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>

--- include/kcl/kcl_device_cgroup.h
+++ include/kcl/kcl_device_cgroup.h
@@ -1,8 +1,11 @@
 #ifndef AMDKCL_DEVICE_CGROUP_H
 #define AMDKCL_DEVICE_CGROUP_H
-
 #include <linux/version.h>
 #include <linux/types.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0)
+#include <linux/bpf-cgroup.h>
+#endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)
 #define DEVCG_DEV_CHAR  2
@@ -13,20 +16,22 @@
 extern int (*__kcl_devcgroup_check_permission)(short type, u32 major, u32 minor,
 				short access);
 
-#if defined(BUILD_AS_DKMS) && defined(CONFIG_CGROUP_DEVICE) && \
-                 (LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0))
-static inline int devcgroup_check_permission(short type, u32 major, u32 minor,
-					short access)
-{
-	return __kcl_devcgroup_check_permission(type, major, minor, access);
-}
-#elif !defined(CONFIG_CGROUP_DEVICE) && !defined(BUILD_AS_DKMS)
-static inline int devcgroup_check_permission(short type, u32 major, u32 minor,
-					short access)
-{
-	return 0;
-}
 
+static inline int kcl_devcgroup_check_permission(short type, u32 major, u32 minor,
+					short access)
+{
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0)
+	int rc = BPF_CGROUP_RUN_PROG_DEVICE_CGROUP(type, major, minor, access);
+        
+	if (rc)
+		return -EPERM;
 #endif
+
+#if defined(CONFIG_CGROUP_DEVICE)	
+	return __kcl_devcgroup_check_permission(type, major, minor, access);
+#else
+	return 0;
+#endif
+}
 
 #endif /*AMDKCL_DEVICE_CGROUP_H*/
