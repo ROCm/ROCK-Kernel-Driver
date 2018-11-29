c89cd96a19fdcbdd9512670ff23a8ac0158c1f33
tianci yin <tianci.yin@amd.com>
2018-11-29 21:34:56 +0800

    drm/amdkcl: [4.15] remove redundant __devcgroup_check_permission defination
    Change-Id: I71ced3f012356bd92ff02f66d2d4ae9c571f473b
    Reviewed-by:Kevin Wang <Kevin1.Wang@amd.com>
    Signed-off-by: Tianci Yin <tianci.yin@amd.com>

--- include/kcl/kcl_device_cgroup.h
+++ include/kcl/kcl_device_cgroup.h
@@ -12,15 +12,9 @@
  * the code segment copy from include/linux/device_cgroup.h,
  * directly include the header result build failure since device_cgroup.h has no protection like DEVICE_CGROUP_H.
  */
-#if !defined(BUILD_AS_DKMS)
-#ifdef CONFIG_CGROUP_DEVICE
+ #if defined(CONFIG_CGROUP_DEVICE) && !defined(BUILD_AS_DKMS)
 extern int __devcgroup_check_permission(short type, u32 major, u32 minor,
 					short access);
-#else
-static inline int __devcgroup_check_permission(short type, u32 major, u32 minor,
-					       short access)
-{ return 0; }
-#endif
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)
