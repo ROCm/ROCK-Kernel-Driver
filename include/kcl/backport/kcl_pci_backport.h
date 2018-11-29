b0f42c37cfc805fecdc7e71df3dd9997d9f66e3c
tianci yin <tianci.yin@amd.com>
2018-11-29 13:14:08 +0800

    drm/amdkcl: Test whether pcie_get_{speed/width}_cap() are available
    v2: drm/amdkcl: [4.18] fix pcie speed and width relevant build err
    v3: drm/amdkcl: [4.17] fix pcie_get_speed_cap and pcie_get_width_cap
    v4: drm/amdkcl: refactor HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP
    v5: drm/amdkcl: drop kcl_pcie_get_speed_cap
    
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Reviewed-by:Kevin Wang <Kevin1.Wang@amd.com>
    Signed-off-by: Tianci Yin <tianci.yin@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    
    drm/amdkcl: update check for pcie_get_{speed,width}_cap
    
    check symbols exported is enough
    
    Change-Id: I13b0ecc561a5651af73e22b7294d93eb629062df
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    
    drm/amdkcl: fix license for kcl part
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

---
+++ include/kcl/backport/kcl_pci_backport.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef AMDKCL_PCI_BACKPORT_H
+#define AMDKCL_PCI_BACKPORT_H
+
+#include <linux/pci.h>
+#include <linux/version.h>
+#include <kcl/kcl_pci.h>
+
+#if !defined(HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP)
+#define pcie_get_speed_cap _kcl_pcie_get_speed_cap
+#endif
+#endif
---
+++ include/kcl/kcl_pci.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *	pci.h
+ *
+ *	PCI defines and function prototypes
+ *	Copyright 1994, Drew Eckhardt
+ *	Copyright 1997--1999 Martin Mares <mj@ucw.cz>
+ *
+ *	PCI Express ASPM defines and function prototypes
+ *	Copyright (c) 2007 Intel Corp.
+ *		Zhang Yanmin (yanmin.zhang@intel.com)
+ *		Shaohua Li (shaohua.li@intel.com)
+ *
+ *	For more information, please consult the following manuals (look at
+ *	http://www.pcisig.com/ for how to get them):
+ *
+ *	PCI BIOS Specification
+ *	PCI Local Bus Specification
+ *	PCI to PCI Bridge Specification
+ *	PCI Express Specification
+ *	PCI System Design Guide
+ */
+#ifndef AMDKCL_PCI_H
+#define AMDKCL_PCI_H
+
+#include <linux/pci.h>
+#include <linux/version.h>
+
+#if !defined(HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP)
+extern enum pci_bus_speed (*_kcl_pcie_get_speed_cap)(struct pci_dev *dev);
+extern enum pcie_link_width (*_kcl_pcie_get_width_cap)(struct pci_dev *dev);
+#endif
+
+static inline enum pci_bus_speed kcl_pcie_get_speed_cap(struct pci_dev *dev)
+{
+#if defined(HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP)
+	return pcie_get_speed_cap(dev);
+#else
+	return _kcl_pcie_get_speed_cap(dev);
+#endif
+}
+
+static inline enum pcie_link_width kcl_pcie_get_width_cap(struct pci_dev *dev)
+{
+#if defined(HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP)
+	return pcie_get_width_cap(dev);
+#else
+	return _kcl_pcie_get_width_cap(dev);
+#endif
+}
+
+#endif /* AMDKCL_PCI_H */
