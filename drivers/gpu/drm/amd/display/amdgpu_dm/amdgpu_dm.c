22df32215af01deec2e335ec8f8041906516ae36
tianci yin <tianci.yin@amd.com>
2018-12-13 19:12:32 +0800

    drm/amdkcl: [4.14] add kcl for "add fast path for cursor plane updates"
    Add kcl for below commit:
    drm/amd/display: Add fast path for cursor plane updates
    
    [why]
    Old kernel has no below file and functions:
    drm_atomic_uapi.h
    drm_atomic_get_old_plane_state
    drm_atomic_helper_async_check
    And asynchronous plane update feature is introduced from DRM 4.14.
    It'd difficult for KCL patch to implement this feature for
    DRM version < 4.14.
    
    Reviewed-by: Prike Liang <Prike.Liang@amd.com>
    Signed-off-by: Tianci Yin <tianci.yin@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6202,6 +6202,7 @@ static int dm_plane_atomic_check(struct drm_plane *plane,
 	return -EINVAL;
 }
 
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 14, 0)
 static int dm_plane_atomic_async_check(struct drm_plane *plane,
 				       struct drm_plane_state *new_plane_state)
 {
@@ -6231,13 +6232,16 @@ static void dm_plane_atomic_async_update(struct drm_plane *plane,
 
 	handle_cursor_update(plane, old_state);
 }
+#endif
 
 static const struct drm_plane_helper_funcs dm_plane_helper_funcs = {
 	.prepare_fb = dm_plane_helper_prepare_fb,
 	.cleanup_fb = dm_plane_helper_cleanup_fb,
 	.atomic_check = dm_plane_atomic_check,
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 14, 0)
 	.atomic_async_check = dm_plane_atomic_async_check,
 	.atomic_async_update = dm_plane_atomic_async_update
+#endif
 };
 
 /*
