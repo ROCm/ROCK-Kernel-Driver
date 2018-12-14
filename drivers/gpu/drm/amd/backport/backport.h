b45fc6257ae22ee326524b7cf3fc42d85439c02f
Anatoli Antonovitch <anatoli.antonovitch@amd.com>
2018-12-14 16:11:31 -0500

    drm/amdkcl: Test whether drm_atomic_helper_best_encoder() is available
    Signed-off-by: Anatoli Antonovitch <anatoli.antonovitch@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: fix compile warning for drm_atomic_helper_best_encoder()
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: fix drm_atomic_helper_best_encoder
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -7,6 +7,7 @@
 #include <kcl/kcl_bitops.h>
 #include <kcl/kcl_drm_connector.h>
 #include <kcl/kcl_fence.h>
+#include <kcl/kcl_drm_atomic_helper.h>
 #include <kcl/kcl_mm.h>
 #include <kcl/kcl_vga_switcheroo.h>
 #include <kcl/kcl_fence_array.h>
--- include/kcl/kcl_drm_atomic_helper.h
+++ include/kcl/kcl_drm_atomic_helper.h
@@ -3,14 +3,15 @@
 
 #include <drm/drm_atomic_helper.h>
 
-
-#if !defined(HAVE_DRM_ENCODER_FIND_VALID_WITH_FILE)
+#if (DRM_VERSION_CODE < DRM_VERSION(5, 0, 0)) && defined(BUILD_AS_DKMS)
+#if !defined(HAVE_DRM_ATOMIC_HELPER_BEST_ENCODER)
 static struct drm_encoder *
 drm_atomic_helper_best_encoder(struct drm_connector *connector)
 {
             WARN_ON(connector->encoder_ids[1]);
             return drm_encoder_find(connector->dev, NULL, connector->encoder_ids[0]);
 }
+#endif
 #endif
 
 #endif /* AMDKCL_DRM_ATOMIC_HELPERH */
