aaa05d69bfc491bcef91157015cde973736bab4d
tianci yin <tianci.yin@amd.com>
2019-01-11 15:39:47 +0800

    drm/amdkcl: Test whether drm_dp_cec_xxx functions are available
    v1: drm/amdkcl: [4.19] kcl for drm_dp_cec_* functions
    v2: drm/amdkcl: Test whether drm_dp_cec_xxx functions are available
    v3: drm/amdkcl: fix kcl_drm_dp_cec_xxx
    v4: drm/amdkcl: fix drm_dp_cec_xxx check
    v5: drm_dp_cec_xxx are inlines with CONFIG_DRM_DP_CEC undefined.
    v6: drm/amdkcl: drop kcl_drm_dp_cec_xxx
    v7: drm/amdkcl: refactor kcl check for drm_dp_cec_irq
    v8: drm/amdkcl: split drm_dp_cec related stuff
    
    [why]
    commit "drm: add support for DisplayPort CEC-Tunneling-over-AUX" introduced
    drm_dp_cec_* functions.
    commit "drm/amdgpu: add DisplayPort CEC-Tunneling-over-AUX suppor" introduced
    the reference of drm_dp_cec_* functions.
    but for kernel < 4.19, drm_dp_cec_* functions are not available.
    [how]
    "DisplayPort CEC-Tunneling-over-AUX" is a new feature, for old kernel, we can
    just skip it, so we just define empty drm_dp_cec* functions like
    CONFIG_DRM_DP_CEC is not set.
    
    Reviewed-by: changzhu <Changfeng.Zhu@amd.com>
    Signed-off-by: Tianci Yin <tianci.yin@amd.com>
    Signed-off-by: chen gong <curry.gong@amd.com>
    Reviewed-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -38,6 +38,7 @@
 #include <kcl/backport/kcl_drm_gem.h>
 #include <kcl/backport/kcl_drm_fb.h>
 #include <kcl/kcl_drm_connector.h>
+#include <kcl/backport/kcl_drm_dp_helper_backport.h>
 #include <kcl/backport/kcl_drm_dp_mst_helper_backport.h>
 
 #endif /* AMDGPU_BACKPORT_H */
