c26c997e908af706502ba0603cd450e947fd178f
tianci yin <tianci.yin@amd.com>
2019-01-11 13:26:58 +0800

    drm/amdkcl: [4.19] kcl for drm_connector_* functions
    [why]
    drm connector functions like drm_mode_connector_update_edid_property
    were renamed to drm_connector_update_edid_property from v4.19-rc1.
    so, for kernel < 4.19, functions like
    drm_mode_connector_update_edid_property are not available.
    
    [how]
    kcl define drm_mode_connector_update_edid_property for kernel < 4.19
    
    Change-Id: I86563034b1a841991ad8e6e53c4293029fd5b17e
    Reviewed-by: Kevin Wang <Kevin1.Wang@amd.com>
    Signed-off-by: Tianci Yin <tianci.yin@amd.com>

--- include/kcl/kcl_drm_connector.h
+++ include/kcl/kcl_drm_connector.h
@@ -1,11 +1,16 @@
 #ifndef AMDKCL_DRM_CONNECTOR_H
 #define AMDKCL_DRM_CONNECTOR_H
 
-
-#if DRM_VERSION_CODE < DRM_VERSION(4, 12, 0)
 #include <drm/drm_crtc.h>
 #include <drm/drmP.h>
 
+#if DRM_VERSION_CODE < DRM_VERSION(4, 19, 0)
+#define drm_connector_update_edid_property drm_mode_connector_update_edid_property
+#define drm_connector_attach_encoder drm_mode_connector_attach_encoder
+#define drm_connector_set_path_property drm_mode_connector_set_path_property
+#endif
+
+#if DRM_VERSION_CODE < DRM_VERSION(4, 12, 0)
 static inline void drm_connector_put(struct drm_connector *connector)
 {
 #if DRM_VERSION_CODE >= DRM_VERSION(4, 7, 0)
