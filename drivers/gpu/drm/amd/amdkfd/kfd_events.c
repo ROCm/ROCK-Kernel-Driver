a348d0979460e0837b02c0b50d29ea9c8303d0a3
Anatoli Antonovitch <anatoli.antonovitch@amd.com>
2019-01-28 14:16:44 -0500

    drm/amdkcl: Test whether wait_queue_entry_t exists
    v2: drm/amdkcl: drop kcl_wait.h
    
    Signed-off-by: Anatoli Antonovitch <anatoli.antonovitch@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack Gui@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jiansong Chen <Jiansong.Chen@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_events.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_events.c
@@ -38,7 +38,11 @@
  * Wrapper around wait_queue_entry_t
  */
 struct kfd_event_waiter {
+#if defined(HAVE_WAIT_QUEUE_ENTRY)
 	wait_queue_entry_t wait;
+#else
+	wait_queue_t wait;
+#endif
 	struct kfd_event *event; /* Event to wait for */
 	bool activated;		 /* Becomes true when event is signaled */
 };
@@ -251,7 +255,11 @@ static void destroy_event(struct kfd_process *p, struct kfd_event *ev)
 	struct kfd_event_waiter *waiter;
 
 	/* Wake up pending waiters. They will return failure */
+#if !defined(HAVE_WAIT_QUEUE_ENTRY)
+	list_for_each_entry(waiter, &ev->wq.task_list, wait.task_list)
+#else
 	list_for_each_entry(waiter, &ev->wq.head, wait.entry)
+#endif
 		waiter->event = NULL;
 	wake_up_all(&ev->wq);
 
@@ -614,7 +622,11 @@ static void set_event(struct kfd_event *ev)
 	 */
 	ev->signaled = !ev->auto_reset || !waitqueue_active(&ev->wq);
 
+#if !defined(HAVE_WAIT_QUEUE_ENTRY)
+	list_for_each_entry(waiter, &ev->wq.task_list, wait.task_list)
+#else
 	list_for_each_entry(waiter, &ev->wq.head, wait.entry)
+#endif
 		waiter->activated = true;
 
 	wake_up_all(&ev->wq);
