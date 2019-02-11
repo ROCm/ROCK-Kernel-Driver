17decffc9732de93feac6d6a499f635989a72226
changzhu <Changfeng.Zhu@amd.com>
2019-02-11 18:10:17 +0800

    drm/amdkcl: [3.13] fix implicit declaration of pci_upstream_bridge error
    [Why]
    pci_upstream_bridge is not defined before kernel_version(3,13,0). So
    there is build error when using pci_upstream_bridge in function
    amdgpu_device_get_min_pci_speed_width(amdgpu_device.c) on redhat 6.10.
    This kcl patch is for patch:
    drm/amdgpu: Fix pci platform speed and width
    
    [How]
    Define pci_upstream_bridge in kcl_pci.h when
    kernel_version<3,13,0. Besides,pci_upstream_bridge is defined on redhat
    7.6,although the kernel_version of redha 7.6 is 3.10.0. So we need to use
    to complete the definition of pci_upstream_bridge.
    
    Change-Id: I686d3ad551159a23b28dfc02ed7a4781b053770a
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Rui Teng <rui.teng@amd.com>

--- include/kcl/kcl_pci.h
+++ include/kcl/kcl_pci.h
@@ -104,4 +104,14 @@ static inline int kcl_pci_create_measure_file(struct pci_dev *pdev)
 #endif
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 13, 0) && !defined(OS_NAME_RHEL_7_6)
+static inline struct pci_dev *pci_upstream_bridge(struct pci_dev *dev)
+{
+	dev = pci_physfn(dev);
+	if (pci_is_root_bus(dev->bus))
+		return NULL;
+
+	return dev->bus->self;
+}
+#endif
 #endif /* AMDKCL_PCI_H */
