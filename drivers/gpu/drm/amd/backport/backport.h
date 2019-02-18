de898aa5fa0be334801630f7b1890ef019cacde0
Le.Ma <Le.Ma@amd.com>
2019-02-18 10:40:35 +0800

    drm/amdkcl: check whether u64_to_user_ptr is available
    v1: drm/amdkcl: [4.7] kcl for u64_to_user_ptr()
    v2: drm/amdkcl: drop kcl_u64_to_user_ptr
    
    Signed-off-by: Le.Ma <Le.Ma@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>/
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -9,6 +9,7 @@
 #include <kcl/kcl_overflow.h>
 #include <kcl/kcl_idr.h>
 #include <kcl/kcl_kref.h>
+#include <kcl/kcl_kernel.h>
 #include <kcl/kcl_dma_mapping.h>
 #include <kcl/kcl_i2c.h>
 #include <kcl/kcl_firmware.h>
