fa1e21fa0441bd82653f0162f1f0fd2a1763b4e9
Evan Quan <evan.quan@amd.com>
2019-02-18 10:25:19 +0800

    drm/amdkcl: [3.7] kcl for INTERVAL_TREE_DEFINE
    Change-Id: I9ef495ae93946fd06f5ee5cf879e507d38a8e016
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -9,5 +9,8 @@
 #include <kcl/kcl_fence_array.h>
 #include <kcl/kcl_kthread.h>
 #include <kcl/kcl_io.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 7, 0)
+#include <kcl/kcl_interval_tree_generic.h>
+#endif
 
 #endif /* AMDGPU_BACKPORT_H */
