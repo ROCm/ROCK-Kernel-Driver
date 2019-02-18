9f7a83e2ca4af1d67921d48bbdf7bb7901406563
Prike Liang <Prike.Liang@amd.com>
2019-02-18 10:56:56 +0800

    drm/amdkcl: Test whether __devcgroup_check_permission() is available
    v1: drm/amdkcl: [4.15] Export __devcgroup_check_permission base on DKMS
    V2: EXport __devcgroup_check_permission by looking up the kallsys.
    v3: drm/amdkcl: Test whether __devcgroup_check_permission() is available
    v4: drm/amdkcl: fix devcgroup_check_permission() check
    v5: update for commit d16020d7429fffd47cfb2f3ab3b6b5b362108a6e :
    v6: drm/amdkcl: drop kcl_devcgroup_check_permission
    
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Rui Teng <rui.teng@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/main.c
@@ -0,0 +1,23 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+extern void amdkcl_dev_cgroup_init(void);
+
+int __init amdkcl_init(void)
+{
+	amdkcl_dev_cgroup_init();
+	return 0;
+}
+module_init(amdkcl_init);
+
+void __exit amdkcl_exit(void)
+{
+
+}
+
+module_exit(amdkcl_exit);
+
+MODULE_AUTHOR("AMD linux driver team");
+MODULE_DESCRIPTION("Module for OS kernel compatible layer");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("1.0");
