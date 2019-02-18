cd7272e600e4ccfef87f64b0d5532cb9cbf1ff7a
Evan Quan <evan.quan@amd.com>
2019-02-18 10:35:14 +0800

    drm/amdkcl: [4.10] use dma_fence for 4.10 kernel
    Change-Id: Ic4504be08cddce2820202fe178167a44d5fd2d2c
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
@@ -274,6 +274,14 @@ bool amdgpu_fence_process(struct amdgpu_ring *ring)
  *
  * Checks for fence activity.
  */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0)
+static void amdgpu_fence_fallback(unsigned long arg)
+{
+	struct amdgpu_ring *ring = (void *)arg;
+
+	amdgpu_fence_process(ring);
+}
+#else
 static void amdgpu_fence_fallback(struct timer_list *t)
 {
 	struct amdgpu_ring *ring = from_timer(ring, t,
@@ -282,6 +290,7 @@ static void amdgpu_fence_fallback(struct timer_list *t)
 	if (amdgpu_fence_process(ring))
 		DRM_WARN("Fence fallback timer expired on ring %s\n", ring->name);
 }
+#endif
 
 /**
  * amdgpu_fence_wait_empty - wait for all fences to signal
@@ -430,7 +439,12 @@ int amdgpu_fence_driver_init_ring(struct amdgpu_ring *ring,
 	atomic_set(&ring->fence_drv.last_seq, 0);
 	ring->fence_drv.initialized = false;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0)
+	setup_timer(&ring->fence_drv.fallback_timer, amdgpu_fence_fallback,
+		    (unsigned long)ring);
+#else
 	timer_setup(&ring->fence_drv.fallback_timer, amdgpu_fence_fallback, 0);
+#endif
 
 	ring->fence_drv.num_fences_mask = num_hw_submission * 2 - 1;
 	spin_lock_init(&ring->fence_drv.lock);
@@ -653,7 +667,7 @@ static const struct dma_fence_ops amdgpu_fence_ops = {
 	.get_timeline_name = amdgpu_fence_get_timeline_name,
 	.enable_signaling = amdgpu_fence_enable_signaling,
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 0)
-#if defined(BUILD_AS_DKMS)
+#if defined(BUILD_AS_DKMS) && !defined(OS_NAME_RHEL_7_4_5)
 	.wait = kcl_fence_default_wait,
 #else
 	.wait = dma_fence_default_wait,
--- drivers/gpu/drm/amd/amdkcl/main.c
+++ drivers/gpu/drm/amd/amdkcl/main.c
@@ -1,16 +1,21 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/version.h>
 
 extern void amdkcl_kthread_init(void);
 extern void amdkcl_drm_init(void);
+#if !defined(OS_NAME_RHEL_7_4_5)
 extern void amdkcl_fence_init(void);
+#endif
 extern void amdkcl_io_init(void);
 
 int __init amdkcl_init(void)
 {
 	amdkcl_kthread_init();
 	amdkcl_drm_init();
+#if !defined(OS_NAME_RHEL_7_4_5)
 	amdkcl_fence_init();
+#endif
 	amdkcl_io_init();
 	return 0;
 }
@@ -20,6 +25,7 @@ void __exit amdkcl_exit(void)
 {
 
 }
+module_exit(amdkcl_exit);
 
 MODULE_AUTHOR("AMD linux driver team");
 MODULE_DESCRIPTION("Module for OS kernel compatible layer");
