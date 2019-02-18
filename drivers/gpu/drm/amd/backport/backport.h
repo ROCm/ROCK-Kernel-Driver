16f5bce89d11cb381f58ca49ad402d1708aaca2a
Evan Quan <evan.quan@amd.com>
2019-02-18 10:33:40 +0800

    drm/amdkcl: [4.10] fix for dma_fence structures and apis
      - v2: define dma_fence* as fence* on oldler kernel
    
    Change-Id: Ie8eb4e66f22459d8de74cdc42fe534e95b84c0da
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -2,9 +2,9 @@
 #define AMDGPU_BACKPORT_H
 
 #include <kcl/kcl_kref.h>
+#include <kcl/kcl_fence.h>
 #include <kcl/kcl_drm.h>
 #include <kcl/kcl_amdgpu.h>
-#include <kcl/kcl_fence.h>
 #include <kcl/kcl_mm.h>
 #include <kcl/kcl_vga_switcheroo.h>
 #include <kcl/kcl_fence_array.h>
