ba0601c0877f42ed27e8857a3d94ea735af86a1a
Kevin Wang <Kevin1.Wang@amd.com>
2019-02-18 11:00:12 +0800

    drm/amdkcl: Test whether mm_access is available
    v1: 183d99136d50 - drm/amdkcl: add kcl mm_access funtions <Kevin Wang>
    v2: 1ef72c2b349f - drm/amd/autoconf: Test whether mm_access is available
    <Chengming Gui>
    
    Signed-off-by: Kevin Wang <Kevin1.Wang@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: refactor mm_access() check
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei Xu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: drop kcl_mm_access
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: fix license for kcl part
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

---
+++ include/kcl/backport/kcl_mm_backport.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef AMDKCL_MM_BACKPORT_H
+#define AMDKCL_MM_BACKPORT_H
+#include <kcl/kcl_mm.h>
+#include <linux/sched/mm.h>
+#include <linux/mm.h>
+
+#endif
