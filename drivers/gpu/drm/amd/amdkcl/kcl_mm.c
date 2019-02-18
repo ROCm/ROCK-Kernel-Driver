8a51e3784f9dc88e45e266b19a1378cdbfad1383
Kevin Wang <Kevin1.Wang@amd.com>
2019-02-18 11:00:12 +0800

    drm/amdkcl: Test whether mm_access is available
    v1: 183d99136d50 - drm/amdkcl: add kcl mm_access funtions <Kevin Wang>
    v2: 1ef72c2b349f - drm/amd/autoconf: Test whether mm_access is available
    <Chengming Gui>
    
    Signed-off-by: Kevin Wang <Kevin1.Wang@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: refactor mm_access() check
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei Xu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: drop kcl_mm_access
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: fix license for kcl part
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/kcl_mm.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *  linux/kernel/fork.c
+ *
+ *  Copyright (C) 1991, 1992  Linus Torvalds
+ */
+#include <linux/sched.h>
+
+void amdkcl_mm_init(void)
+{
+}
