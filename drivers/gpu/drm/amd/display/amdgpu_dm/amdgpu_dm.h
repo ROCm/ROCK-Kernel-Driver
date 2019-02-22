91cc78f567e8d159cf5a2fe7f8270ce14e78014e
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2019-02-22 13:56:25 -0500

    drm/amdkcl: [5.0] Add kcl for upstream DRM VRR properties(v2)
    [Why]
    Some userspace now expects to use vrr_capable and vrr_enabled
    properties for kernels < 5.0, but the DKMS build only exposes support
    for the legacy IOCTL for those kernels.
    
    [How]
    Expose the DRM properties for the connector and CRTC. The DRM functions
    and interface can't really be stubbed directly since they deal with
    state on the connector and CRTC that aren't available.
    
    Tested as passing the igt@kms_vrr tests for 4.16, and for 4.15 with
    a modified debugfs entry for vrr_range.
    
    4.21 kernel doesn't exist,so it need to replace DRM_VERSION(4, 21,0)
    with DRM_VERSION(5, 0, 0)
    
    Change-Id: I32544ba087c09024738adf8f1297ce580c6aabb3
    Cc: Liang, Prike <Prike.Liang@amd.com>
    Cc: Yin, Tianci (Rico) <Tianci.Yin@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Tested-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: changzhu <Changfeng.Zhu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -346,7 +346,7 @@ struct dm_crtc_state {
 	int crc_skip_count;
 	enum amdgpu_dm_pipe_crc_source crc_src;
 
-#if DRM_VERSION_CODE < DRM_VERSION(4, 21, 0)
+#if DRM_VERSION_CODE < DRM_VERSION(5, 0, 0)
 	bool base_vrr_enabled;
 #endif
 
