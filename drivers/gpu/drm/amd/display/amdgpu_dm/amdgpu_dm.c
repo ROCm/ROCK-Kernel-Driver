c5b45b2dd89c6832939d9d0c18ef0f32d7a37b4c
changzhu <Changfeng.Zhu@amd.com>
2019-03-19 11:26:06 +0800

    drm/amdkcl: [5.0] fix __drm_atomic_helper_plane_reset build error
    [Why]
    __drm_atomic_helper_plane_reset is not defined before drm
    version(4,19,0). However,its definition in drm version(4,19,0) lacks
    state->alpha = DRM_BLEND_ALPHA_OPAQUE;
    state->pixel_blend_mode = DRM_MODE_BLEND_PREMULTI;
    So there will be build error when using __drm_atomic_helper_plane_reset
    befor drm version 5.0
    DRM_BLEND_ALPHA_OPAQUE is not defined before drm version(4.18.0)
    DRM_MODE_BLEND_PREMULTI is not defined before drm version(4.20.0)
    So there will be build error when using them before drm version(4.18.0)
    
    [How]
    Use
    if (amdgpu_state) {
    	plane->state = &amdgpu_state->base;
    	plane->state->plane = plane;
    	plane->state->rotation = DRM_MODE_ROTATE_0;
    	plane->state->alpha = DRM_BLEND_ALPHA_OPAQUE;
    	plane->state->pixel_blend_mode = DRM_MODE_BLEND_PREMULTI;
    }
    before drm version 5.0
    Avoid using DRM_BLEND_ALPHA_OPAQUE before drm version(4.18.0)
    Avoid using DRM_MODE_BLEND_PREMULTI before drm version(4.20.0)
    
    Change-Id: I8c078ac3bb1e4ef207e521794b01766a77a1d822
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -5993,8 +5993,22 @@ static void dm_drm_plane_reset(struct drm_plane *plane)
 	amdgpu_state = kzalloc(sizeof(*amdgpu_state), GFP_KERNEL);
 	WARN_ON(amdgpu_state == NULL);
 
+#if DRM_VERSION_CODE < DRM_VERSION(5, 0, 0)
+	if (amdgpu_state) {
+		plane->state = &amdgpu_state->base;
+		plane->state->plane = plane;
+		plane->state->rotation = DRM_MODE_ROTATE_0;
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 18, 0)
+		plane->state->alpha = DRM_BLEND_ALPHA_OPAQUE;
+#endif
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 20, 0)
+		plane->state->pixel_blend_mode = DRM_MODE_BLEND_PREMULTI;
+#endif
+	}
+#else
 	if (amdgpu_state)
 		__drm_atomic_helper_plane_reset(plane, &amdgpu_state->base);
+#endif
 }
 
 static struct drm_plane_state *
