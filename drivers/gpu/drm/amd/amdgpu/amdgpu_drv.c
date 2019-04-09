945c3779410beeb9e51d6673d731dd16b68bb96f
Rui Teng <rui.teng@amd.com>
2019-04-09 11:13:22 +0800

    Hybrid Version: 5.0.19.20.14
    Change-Id: I7ece706ce980dd092de6a1d6bf05a9eba7119fe2
    Signed-off-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -80,7 +80,7 @@
 #define KMS_DRIVER_MINOR	31
 #define KMS_DRIVER_PATCHLEVEL	0
 
-#define AMDGPU_VERSION		"5.0.19.20.7"
+#define AMDGPU_VERSION		"5.0.19.20.14"
 
 int amdgpu_vram_limit = 0;
 int amdgpu_vis_vram_limit = 0;
