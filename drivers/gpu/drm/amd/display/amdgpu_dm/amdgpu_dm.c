95b0c7548489a011149db69b89196038b82315b4
changzhu <Changfeng.Zhu@amd.com>
2019-04-15 17:07:49 +0800

    drm/amdkcl: [4.17] fix fill_plane_color_attributes color_range and color_encoding build error
    [Why]
    enum drm_color_encoding and enum drm_color_range are not defined before
    drm version(4.17.0). They are also not defined in drm_plane_state before
    drm version(4.17.0). So there will be build error when using them in
    fill_plane_color_attributes.
    
    [How]
    Avoid using color_encoding and color_range before drm version(4.17.0)
    This kcl patch is for patch:
    drm/amd/display: Set surface color space from DRM plane state
    
    Change-Id: I2d732ad34ecaabd37c7dcdc818c46e5ec6aa5a11
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: [5.0] Move dc_plane_state->format check out of DRM check
    [Why]
    dc_plane_state->format check is a global thing. We need this check
    within all the versions of kernels.
    [How]
    Moving dc_plane_state->format check out of the DRM check macro
    Signed-off-by: Zhan liu <zhan.liu@amd.com>
    Reviewed-by: Changfeng Zhu <changfeng.zhu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -3668,6 +3668,7 @@ fill_plane_color_attributes(const struct drm_plane_state *plane_state,
 	if (format < SURFACE_PIXEL_FORMAT_VIDEO_BEGIN)
 		return 0;
 
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 17, 0)
 	full_range = (plane_state->color_range == DRM_COLOR_YCBCR_FULL_RANGE);
 
 	switch (plane_state->color_encoding) {
@@ -3695,7 +3696,11 @@ fill_plane_color_attributes(const struct drm_plane_state *plane_state,
 	default:
 		return -EINVAL;
 	}
-
+#else
+	/* Assume 709 full range for YUV formats when not given color space on plane. */
+	full_range = true;
+	*color_space = COLOR_SPACE_YCBCR709;
+#endif
 	return 0;
 }
 
