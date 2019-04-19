dff606eb8bb7e22e5daf1edb5079300290fcaf6e
changzhu <Changfeng.Zhu@amd.com>
2019-04-19 13:58:42 +0800

    drm/amdkcl: test whether struct drm_dp_mst_topology_cbs has hotplug
    drm/amdkcl: [5.1] Fix multiple MST daisy chain issues within DM
    [Why]
    This patch fixes the following issues:
    - Null pointer dereference was found with MST monitor attached
    - MST daisy chain hotplugging doesn't work
    [How]
    Until DRM Ver.5.0, we need a MST hotplug function to deal
    with MST hotplug operations. (this function is not needed since
    DRV Ver.5.1 rc1). Hence a MST hotplug function is added to avoid
    hotplug failure. (the old hotplug structure was replaced within
    DRV Ver.5.1)
    Signed-off-by: Zhan Liu <zhan.liu@amd.com>
    Reviewed-by: Xiaojie Yuan <xiaojie.yuan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    
    drm/amdkcl: fix dm_dp_mst_hotplug custom kernel build error
    [Why]
    There is build error when using dm_dp_mst_hotplug to build custom kernel
    on dkms-5.0 branch
    [How]
    Avoid using dm_dp_mst_hotplug when building custom kernel on dkms-5.0 branch.
    This kcl patch is supplement for kcl patch:
    drm/amdkcl: [5.1] Fix multiple MST daisy chain issues within DM
    Change-Id: I827c06eee369f0b5468e8298376f8aea344c5533
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Zhan Liu <zhan.liu@amd.com>
    
    drm/amdkcl: test whether struct drm_dp_mst_topology_cbs has hotplug
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -425,6 +425,16 @@ static void dm_dp_destroy_mst_connector(struct drm_dp_mst_topology_mgr *mgr,
 #endif
 }
 
+#if defined(HAVE_DRM_DP_MST_TOPOLOGY_CBS_HOTPLUG)
+static void dm_dp_mst_hotplug(struct drm_dp_mst_topology_mgr *mgr)
+{
+	struct amdgpu_dm_connector *master = container_of(mgr, struct amdgpu_dm_connector, mst_mgr);
+	struct drm_device *dev = master->base.dev;
+
+	drm_kms_helper_hotplug_event(dev);
+}
+#endif
+
 static void dm_dp_mst_register_connector(struct drm_connector *connector)
 {
 	struct drm_device *dev = connector->dev;
@@ -448,6 +458,9 @@ static void dm_dp_mst_register_connector(struct drm_connector *connector)
 static const struct drm_dp_mst_topology_cbs dm_mst_cbs = {
 	.add_connector = dm_dp_add_mst_connector,
 	.destroy_connector = dm_dp_destroy_mst_connector,
+#if defined(HAVE_DRM_DP_MST_TOPOLOGY_CBS_HOTPLUG)
+	.hotplug = dm_dp_mst_hotplug,
+#endif
 #if DRM_VERSION_CODE >= DRM_VERSION(4, 3, 0)
 	.register_connector = dm_dp_mst_register_connector
 #endif
