2bc3e12abe88404d03593d850cb4b113f5230b84
Slava Grigorev <slava.grigorev@amd.com>
2019-05-09 17:20:26 -0400

    drm/amdkcl: Test whether format in struct drm_framebuffer is available
    v1: drm/amdkcl: Test whether drm_framebuffer structure contains format
    v2: drm/amd/autoconf: test whether struct drm_framebuffer have format
    v3: drm/amdkcl: fix for HAVE_DRM_FRAMEBUFFER_FORMAT
    v4: drm/amdkcl: accommodate to drmP.h removal for drm-framebuffer-format.m4
    
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: chen gong <curry.gong@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
@@ -191,14 +191,18 @@ static void dce_v6_0_page_flip(struct amdgpu_device *adev,
 			       int crtc_id, u64 crtc_base, bool async)
 {
 	struct amdgpu_crtc *amdgpu_crtc = adev->mode_info.crtcs[crtc_id];
+#if defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
 	struct drm_framebuffer *fb = amdgpu_crtc->base.primary->fb;
+#endif
 
 	/* flip at hsync for async, default is vsync */
 	WREG32(mmGRPH_FLIP_CONTROL + amdgpu_crtc->crtc_offset, async ?
 	       GRPH_FLIP_CONTROL__GRPH_SURFACE_UPDATE_H_RETRACE_EN_MASK : 0);
+#if defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
 	/* update pitch */
 	WREG32(mmGRPH_PITCH + amdgpu_crtc->crtc_offset,
 	       fb->pitches[0] / fb->format->cpp[0]);
+#endif
 	/* update the scanout addresses */
 	WREG32(mmGRPH_PRIMARY_SURFACE_ADDRESS_HIGH + amdgpu_crtc->crtc_offset,
 	       upper_32_bits(crtc_base));
@@ -1854,7 +1858,11 @@ static int dce_v6_0_crtc_do_set_base(struct drm_crtc *crtc,
 	amdgpu_bo_get_tiling_flags(abo, &tiling_flags);
 	amdgpu_bo_unreserve(abo);
 
+#if !defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
+	switch (target_fb->pixel_format) {
+#else
 	switch (target_fb->format->format) {
+#endif
 	case DRM_FORMAT_C8:
 		fb_format = (GRPH_DEPTH(GRPH_DEPTH_8BPP) |
 			     GRPH_FORMAT(GRPH_FORMAT_INDEXED));
@@ -1930,7 +1938,11 @@ static int dce_v6_0_crtc_do_set_base(struct drm_crtc *crtc,
 		break;
 	default:
 		DRM_ERROR("Unsupported screen format %s\n",
+#if !defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
+		          drm_get_format_name(target_fb->pixel_format, &format_name));
+#else
 		          drm_get_format_name(target_fb->format->format, &format_name));
+#endif
 		return -EINVAL;
 	}
 
@@ -1993,7 +2005,11 @@ static int dce_v6_0_crtc_do_set_base(struct drm_crtc *crtc,
 	WREG32(mmGRPH_X_END + amdgpu_crtc->crtc_offset, target_fb->width);
 	WREG32(mmGRPH_Y_END + amdgpu_crtc->crtc_offset, target_fb->height);
 
+#if !defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
+	fb_pitch_pixels = target_fb->pitches[0] / (target_fb->bits_per_pixel / 8);
+#else
 	fb_pitch_pixels = target_fb->pitches[0] / target_fb->format->cpp[0];
+#endif
 	WREG32(mmGRPH_PITCH + amdgpu_crtc->crtc_offset, fb_pitch_pixels);
 
 	dce_v6_0_grph_enable(crtc, true);
