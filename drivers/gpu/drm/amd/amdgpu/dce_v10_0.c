d3270bcf21a13f20cfcdad8c0b973e0e39b40ab3
Slava Grigorev <slava.grigorev@amd.com>
2019-05-09 17:20:26 -0400

    drm/amdkcl: Test whether format in struct drm_framebuffer is available
    v1: drm/amdkcl: Test whether drm_framebuffer structure contains format
    v2: drm/amd/autoconf: test whether struct drm_framebuffer have format
    v3: drm/amdkcl: fix for HAVE_DRM_FRAMEBUFFER_FORMAT
    v4: drm/amdkcl: accommodate to drmP.h removal for drm-framebuffer-format.m4
    v5: drm/amdkcl: fix pitch setting on leacy kernel (Flora Cui)
    update pitch register as is done by commit 965ebe3d5d641("drm/amdgpu:
    		Update pitch on page flips without DC as well")
    
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: chen gong <curry.gong@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yang Xiong <Yang.Xiong@amd.com>

--- drivers/gpu/drm/amd/amdgpu/dce_v10_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v10_0.c
@@ -246,8 +246,13 @@ static void dce_v10_0_page_flip(struct amdgpu_device *adev,
 			    GRPH_SURFACE_UPDATE_H_RETRACE_EN, async ? 1 : 0);
 	WREG32(mmGRPH_FLIP_CONTROL + amdgpu_crtc->crtc_offset, tmp);
 	/* update pitch */
+#if defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
 	WREG32(mmGRPH_PITCH + amdgpu_crtc->crtc_offset,
 	       fb->pitches[0] / fb->format->cpp[0]);
+#else
+	WREG32(mmGRPH_PITCH + amdgpu_crtc->crtc_offset,
+	       fb->pitches[0] / (fb->bits_per_pixel / 8));
+#endif
 	/* update the primary scanout address */
 	WREG32(mmGRPH_PRIMARY_SURFACE_ADDRESS_HIGH + amdgpu_crtc->crtc_offset,
 	       upper_32_bits(crtc_base));
@@ -1897,7 +1902,11 @@ static int dce_v10_0_crtc_do_set_base(struct drm_crtc *crtc,
 
 	pipe_config = AMDGPU_TILING_GET(tiling_flags, PIPE_CONFIG);
 
+#if !defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
+	switch (target_fb->pixel_format) {
+#else
 	switch (target_fb->format->format) {
+#endif
 	case DRM_FORMAT_C8:
 		fb_format = REG_SET_FIELD(0, GRPH_CONTROL, GRPH_DEPTH, 0);
 		fb_format = REG_SET_FIELD(fb_format, GRPH_CONTROL, GRPH_FORMAT, 0);
@@ -1981,7 +1990,11 @@ static int dce_v10_0_crtc_do_set_base(struct drm_crtc *crtc,
 		break;
 	default:
 		DRM_ERROR("Unsupported screen format %p4cc\n",
+#if defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
 			  &target_fb->format->format);
+#else
+			  &target_fb->pixel_format);
+#endif
 		return -EINVAL;
 	}
 
@@ -2056,7 +2069,11 @@ static int dce_v10_0_crtc_do_set_base(struct drm_crtc *crtc,
 	WREG32(mmGRPH_X_END + amdgpu_crtc->crtc_offset, target_fb->width);
 	WREG32(mmGRPH_Y_END + amdgpu_crtc->crtc_offset, target_fb->height);
 
+#if !defined(HAVE_DRM_FRAMEBUFFER_FORMAT)
+	fb_pitch_pixels = target_fb->pitches[0] / (target_fb->bits_per_pixel / 8);
+#else
 	fb_pitch_pixels = target_fb->pitches[0] / target_fb->format->cpp[0];
+#endif
 	WREG32(mmGRPH_PITCH + amdgpu_crtc->crtc_offset, fb_pitch_pixels);
 
 	dce_v10_0_grph_enable(crtc, true);
