d7f426ca8c146d3663fc967fa0363664a1e05bbf
Christian König <christian.koenig@amd.com>
2019-05-21 15:07:04 +0200

    drm/scheduler: fix am issue with last patch
    Somehow that got mangled.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -631,7 +631,7 @@ static void drm_sched_cleanup_jobs(struct drm_gpu_scheduler *sched)
 	unsigned long flags;
 
 	/* Don't destroy jobs while the timeout worker is running */
-	if (sched->timeout !=3D MAX_SCHEDULE_TIMEOUT &&
+	if (sched->timeout != MAX_SCHEDULE_TIMEOUT &&
 	    !cancel_delayed_work(&sched->work_tdr))
 		return;
 
