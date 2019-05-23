81c5e432a0c4c4cbf4976643f353f3939f38ef55
chen gong <curry.gong@amd.com>
2019-05-23 18:07:33 +0800

    Test whether for_each_oldnew_connector_in_state() is defined
    Change-Id: I7b2847ca6fc7e847ba9e06bc2e2e9173ef26e8cf
    Signed-off-by: chen gong <curry.gong@amd.com>
    Reviewed-by: Anatoli Antonovitch <anatoli.antonovitch@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -2665,7 +2665,7 @@ retry:
 #endif
 	}
 
-#if DRM_VERSION_CODE < DRM_VERSION(4, 12, 0)
+#if !defined(HAVE_FOR_EACH_OLDNEW_CONNECTOR_IN_STATE)
 	for_each_connector_in_state(state, connector, new_con_state, i) {
 		old_con_state = connector->state;
 #else
@@ -6914,7 +6914,7 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 	}
 
 	/* Handle connector state changes */
-#if DRM_VERSION_CODE < DRM_VERSION(4, 12, 0)
+#if !defined(HAVE_FOR_EACH_OLDNEW_CONNECTOR_IN_STATE)
 	for_each_connector_in_state(state, connector, old_con_state, i) {
 		new_con_state = connector->state;
 #else
@@ -8126,7 +8126,7 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 	 * new stream into context w\o causing full reset. Need to
 	 * decide how to handle.
 	 */
-#if DRM_VERSION_CODE < DRM_VERSION(4, 12, 0)
+#if !defined(HAVE_FOR_EACH_OLDNEW_CONNECTOR_IN_STATE)
 	for_each_connector_in_state(state, connector, new_con_state, i) {
 		old_con_state = connector->state;
 #else
