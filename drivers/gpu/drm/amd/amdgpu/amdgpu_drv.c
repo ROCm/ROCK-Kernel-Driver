25057d41732ddfd7d30d2a9a6572e15966a2c053
changzhu <Changfeng.Zhu@amd.com>
2019-05-30 10:48:31 +0800

    drm/amdkcl: [5.2] fix DRIVER_SYNCOBJ_TIMELINE undeclared build error
    [Why]
    DRIVER_SYNCOBJ_TIMELINE is not definied in drm_drv.h until drm version
    5.2. So there will be build error when using it before drm version 5.2.
    
    [How]
    Avoid using DRIVER_SYNCOBJ_TIMELINE before drm version 5.2.
    This kcl patch is for patch:
    drm/amdgpu: add DRIVER_SYNCOBJ_TIMELINE to amdgpu
    
    Change-Id: Ia69fa648b595414d6a458e2a97acfa145dd953b0
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -1401,10 +1401,12 @@ static struct drm_driver kms_driver = {
 #endif
 	    DRIVER_HAVE_IRQ | DRIVER_IRQ_SHARED | DRIVER_GEM |
 	    DRIVER_PRIME | DRIVER_RENDER | DRIVER_MODESET |
-#if DRM_VERSION_CODE >= DRM_VERSION(4, 13, 0)
-	    DRIVER_SYNCOBJ |
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 13, 0) && DRM_VERSION_CODE < DRM_VERSION(5, 2, 0)
+		DRIVER_SYNCOBJ,
 #endif
-		DRIVER_SYNCOBJ_TIMELINE,
+#if DRM_VERSION_CODE >= DRM_VERSION(5, 2, 0)
+		RIVER_SYNCOBJ | DRIVER_SYNCOBJ_TIMELINE,
+#endif
 	.load = amdgpu_driver_load_kms,
 	.open = amdgpu_driver_open_kms,
 	.postclose = amdgpu_driver_postclose_kms,
