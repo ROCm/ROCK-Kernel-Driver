b5fee6ccb146754033613ad4a030d657272c563c
chen gong <curry.gong@amd.com>
2019-06-06 16:24:01 +0800

    drm/amdkcl: Test whether dev_pm_set_driver_flags() is available
    v2: fake dev_pm_set_driver_flags()
    
    Signed-off-by: chen gong <curry.gong@amd.com>
    Reviewed-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jiansong Chen <Jiansong.Chen@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- include/kcl/kcl_device.h
+++ include/kcl/kcl_device.h
@@ -3,6 +3,7 @@
 #define AMDKCL_DEVICE_H
 
 #include <linux/ratelimit.h>
+#include <linux/device.h>
 
 #if !defined(dev_err_once)
 #ifdef CONFIG_PRINTK
@@ -40,4 +41,18 @@ do {									\
 #define dev_err_ratelimited(dev, fmt, ...)				\
 	dev_level_ratelimited(dev_err, dev, fmt, ##__VA_ARGS__)
 #endif
+
+#if !defined(HAVE_DEV_PM_SET_DRIVER_FLAGS)
+/* rhel7.7 wrap macro dev_pm_set_driver_flags in drm/drm_backport.h */
+#ifdef dev_pm_set_driver_flags
+#undef dev_pm_set_driver_flags
+#endif
+#define DPM_FLAG_NEVER_SKIP    BIT(0)
+#define DPM_FLAG_SMART_PREPARE BIT(1)
+static inline void dev_pm_set_driver_flags(struct device *dev, u32 flags)
+{
+	printk_once(KERN_WARNING "%s is not available\n", __func__);
+}
+#endif
+
 #endif /* AMDKCL_DEVICE_H */
