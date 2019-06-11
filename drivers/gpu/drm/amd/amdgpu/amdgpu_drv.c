38c15f2b2a051b17e042e1b29f338be006506f38
Rui Teng <rui.teng@amd.com>
2019-06-11 21:13:36 +0800

    Bump AMDGPU version to 5.0.64
    Change-Id: Ia2b250708c6c51cf5e9c6e9e8cdaf25eaed75923
    Signed-off-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -81,7 +81,7 @@
 #define KMS_DRIVER_MINOR	32
 #define KMS_DRIVER_PATCHLEVEL	0
 
-#define AMDGPU_VERSION		"5.0.45"
+#define AMDGPU_VERSION		"5.0.64"
 #define AMDGPU_MAX_TIMEOUT_PARAM_LENTH	256
 
 int amdgpu_vram_limit = 0;
