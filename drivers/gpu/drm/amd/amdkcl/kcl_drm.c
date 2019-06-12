d545ebdd328328e1424c4495d6bb10b4fa67d1bb
Slava Grigorev <slava.grigorev@amd.com>
2019-06-12 11:03:57 -0400

    drm/amdkcl: Test whether drm_printf() function is available
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm.c
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: MIT */
+#include <kcl/kcl_drm.h>
+#include "kcl_common.h"
+
+#if !defined(HAVE_DRM_DRM_PRINT_H)
+void drm_printf(struct drm_printer *p, const char *f, ...)
+{
+	struct va_format vaf;
+	va_list args;
+
+	va_start(args, f);
+	vaf.fmt = f;
+	vaf.va = &args;
+	p->printfn(p, &vaf);
+	va_end(args);
+}
+EXPORT_SYMBOL(drm_printf);
+#endif
--- include/kcl/kcl_drm.h
+++ include/kcl/kcl_drm.h
@@ -2,4 +2,16 @@
 #ifndef AMDKCL_DRM_H
 #define AMDKCL_DRM_H
 
+#include <kcl/header/kcl_drm_print_h.h>
+
+#if !defined(HAVE_DRM_DRM_PRINT_H)
+struct drm_printer {
+	void (*printfn)(struct drm_printer *p, struct va_format *vaf);
+	void *arg;
+	const char *prefix;
+};
+
+void drm_printf(struct drm_printer *p, const char *f, ...);
+#endif
+
 #endif /* AMDKCL_DRM_H */
