e193b55607a8e61653261b47417627c4c5de2b5f
Slava Grigorev <slava.grigorev@amd.com>
2019-06-12 11:03:57 -0400

    drm/amdkcl: Test whether drm_printf() function is available
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: split drm_print stuff to kcl_drm_print.[ch]
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/scheduler/backport/backport.h
+++ drivers/gpu/drm/scheduler/backport/backport.h
@@ -5,6 +5,5 @@
 #include <linux/version.h>
 #include <kcl/backport/kcl_kthread_backport.h>
 #include <kcl/backport/kcl_fence_backport.h>
-#include <kcl/backport/kcl_drm_backport.h>
-
+#include <kcl/kcl_drm_print.h>
 #endif
