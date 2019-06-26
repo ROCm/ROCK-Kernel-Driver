990869ee50cdd8c24ca0a6de682e6b455e874e97
Anatoli Antonovitch <anatoli.antonovitch@amd.com>
2019-06-26 15:20:19 -0400

    drm/amdkcl: Test whether ttm_bo_vm_fault() wants 2 args
    v2: Test whether vm_fault->{address/vma} is available
    v3: fix intree failure due to vmf->vma kcl wrapper
    v4: refactor test for vm_operations_struct->fault()
    
    Signed-off-by: Anatoli Antonovitch <anatoli.antonovitch@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    Reviewed-by: Jiansong Chen <Jiansong.Chen@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_doorbell.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_doorbell.c
@@ -148,9 +148,15 @@ static void kfd_doorbell_close(struct vm_area_struct *vma)
 	mutex_unlock(&pdd->qpd.doorbell_lock);
 }
 
+#ifdef HAVE_VM_OPERATIONS_STRUCT_FAULT_1ARG
 static vm_fault_t kfd_doorbell_vm_fault(struct vm_fault *vmf)
 {
-	struct kfd_process_device *pdd = vmf->vma->vm_private_data;
+	struct vm_area_struct *vma = vmf->vma;
+#else
+static int kfd_doorbell_vm_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
+{
+#endif
+	struct kfd_process_device *pdd = vma->vm_private_data;
 
 	if (!pdd)
 		return VM_FAULT_SIGBUS;
