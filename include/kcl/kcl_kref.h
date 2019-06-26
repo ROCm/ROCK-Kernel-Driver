ec77faa10f6f55bbfaf6039fa9776dff26c8360a
Slava Grigorev <slava.grigorev@amd.com>
2019-06-26 10:37:11 -0400

    Test whether kref_read() function is available
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>

--- include/kcl/kcl_kref.h
+++ include/kcl/kcl_kref.h
@@ -1,10 +1,9 @@
 #ifndef AMDKCL_KREF_H
 #define AMDKCL_KREF_H
 
-#include <linux/version.h>
 #include <linux/kref.h>
 
-#if DRM_VERSION_CODE < DRM_VERSION(4, 11, 0)
+#if !defined(HAVE_KREF_READ)
 static inline unsigned int kref_read(const struct kref *kref)
 {
 	return atomic_read(&kref->refcount);
