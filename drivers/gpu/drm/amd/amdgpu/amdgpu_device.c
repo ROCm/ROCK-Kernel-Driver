57b4c9d19ba6736610e3d38305c28fd50599545b
Arnd Bergmann <arnd@arndb.de>
2019-07-08 16:41:43 +0200

    amdgpu: make pmu support optional
    When CONFIG_PERF_EVENTS is disabled, we cannot compile the pmu
    portion of the amdgpu driver:
    
    drivers/gpu/drm/amd/amdgpu/amdgpu_pmu.c:48:38: error: no member named 'hw' in 'struct perf_event'
            struct hw_perf_event *hwc = &event->hw;
                                         ~~~~~  ^
    drivers/gpu/drm/amd/amdgpu/amdgpu_pmu.c:51:13: error: no member named 'attr' in 'struct perf_event'
            if (event->attr.type != event->pmu->type)
                ~~~~~  ^
    ...
    
    Use conditional compilation for this file.
    
    Fixes: 9c7c85f7ea1f ("drm/amdgpu: add pmu counters")
    Change-Id: I775a250da228c450f21ab0d37c4810b4439452f0
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2821,7 +2821,8 @@ fence_driver_init:
 		return r;
 	}
 
-	r = amdgpu_pmu_init(adev);
+	if (IS_ENABLED(CONFIG_PERF_EVENTS))
+		r = amdgpu_pmu_init(adev);
 	if (r)
 		dev_err(adev->dev, "amdgpu_pmu_init failed\n");
 
@@ -2898,12 +2899,11 @@ void amdgpu_device_fini(struct amdgpu_device *adev)
 	amdgpu_debugfs_regs_cleanup(adev);
 	device_remove_file(adev->dev, &dev_attr_pcie_replay_count);
 	amdgpu_ucode_sysfs_fini(adev);
-
+	if (IS_ENABLED(CONFIG_PERF_EVENTS))
+		amdgpu_pmu_fini(adev);
 	amdgpu_debugfs_preempt_cleanup(adev);
 	if (amdgpu_discovery)
 		amdgpu_discovery_fini(adev);
-
-	amdgpu_pmu_fini(adev);
 }
 
 
