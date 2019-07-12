227e9fc2843054c24e59855c9c24b90a97b55cdb
changzhu <Changfeng.Zhu@amd.com>
2019-07-12 14:49:19 +0800

    drm/amdkcl: add AC_AMDGPU_DRM_AUDIO_COMPONENT_HEADER
    it is a squash of:
    
    drm/amdkcl: test whether drm/drm_audio_component.h is available
    
    [why]
    drm_audio_component.h is not defined before drm version 4.19.0. So there
    will be build error when using them before drm version 4.19.0.
    
    [How]
    Use autoconf way to check drm_audio_component_ops,
    drm_audio_component_audio_ops,drm_audio_component in kernel. If they are
    not defined,avoid to use them in driver code.
    This autoconf patch is caused by patch:
    drm/amd/display: Add drm_audio_component support to amdgpu_dm
    
    Change-Id: Ic75928406a042a0e08f5b06cf64543555c62e476
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: correct drm_audio_component_header.m4 patten
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: fix check for drm/drm_audio_component.h
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Change-Id: I7717d2f117361cfa3fa7a0f941e88ac3d99327b8

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -73,7 +73,10 @@
 #include <linux/pm_runtime.h>
 #include <linux/pci.h>
 #include <linux/firmware.h>
+
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 #include <linux/component.h>
+#endif
 #include <linux/dmi.h>
 
 #include <drm/display/drm_dp_mst_helper.h>
@@ -86,7 +89,9 @@
 #include <drm/drm_fourcc.h>
 #include <drm/drm_edid.h>
 #include <drm/drm_vblank.h>
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 #include <drm/drm_audio_component.h>
+#endif
 #include <drm/drm_gem_atomic_helper.h>
 #include <drm/drm_plane_helper.h>
 #ifdef CONFIG_DRM_AMD_DC_HDCP
@@ -883,6 +888,7 @@ static void amdgpu_dm_fbc_init(struct drm_connector *connector)
 
 }
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 static int amdgpu_dm_audio_component_get_eld(struct device *kdev, int port,
 					  int pipe, bool *enabled,
 					  unsigned char *buf, int max_bytes)
@@ -1015,6 +1021,7 @@ static  void amdgpu_dm_audio_eld_notify(struct amdgpu_device *adev, int pin)
 						 pin, -1);
 	}
 }
+#endif
 
 static int dm_dmub_hw_init(struct amdgpu_device *adev)
 {
@@ -1409,7 +1416,10 @@ static int amdgpu_dm_init(struct amdgpu_device *adev)
 #endif
 
 	mutex_init(&adev->dm.dc_lock);
+
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	mutex_init(&adev->dm.audio_lock);
+#endif
 
 	if(amdgpu_dm_irq_init(adev)) {
 		DRM_ERROR("amdgpu: failed to initialize DM IRQ support.\n");
@@ -1770,7 +1780,9 @@ static void amdgpu_dm_fini(struct amdgpu_device *adev)
 		adev->dm.freesync_module = NULL;
 	}
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	mutex_destroy(&adev->dm.audio_lock);
+#endif
 	mutex_destroy(&adev->dm.dc_lock);
 
 	return;
@@ -3855,12 +3867,14 @@ static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 		return r;
 	}
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	r = amdgpu_dm_audio_init(adev);
 	if (r) {
 		dc_release_state(state->context);
 		kfree(state);
 		return r;
 	}
+#endif
 
 	return 0;
 }
@@ -6984,7 +6998,9 @@ void amdgpu_dm_connector_init_helper(struct amdgpu_display_manager *dm,
 	aconnector->base.stereo_allowed = false;
 	aconnector->base.dpms = DRM_MODE_DPMS_OFF;
 	aconnector->hpd.hpd = AMDGPU_HPD_NONE; /* not used */
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	aconnector->audio_inst = -1;
+#endif
 	mutex_init(&aconnector->hpd_lock);
 
 	/*
@@ -7958,6 +7974,7 @@ cleanup:
 	kfree(bundle);
 }
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 static void amdgpu_dm_commit_audio(struct drm_device *dev,
 				   struct drm_atomic_state *state)
 {
@@ -8032,6 +8049,7 @@ static void amdgpu_dm_commit_audio(struct drm_device *dev,
 		amdgpu_dm_audio_eld_notify(adev, inst);
 	}
 }
+#endif
 
 /*
  * amdgpu_dm_crtc_copy_transient_flags - copy mirrored flags from DRM to DC
@@ -8427,8 +8445,10 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 						dm, crtc, wait_for_vblank);
 	}
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	/* Update audio instances for each connector. */
 	amdgpu_dm_commit_audio(dev, state);
+#endif
 
 	/* restore the backlight level */
 	for (i = 0; i < dm->num_of_edps; i++) {
--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -352,6 +352,7 @@ struct amdgpu_display_manager {
 	 */
 	struct mutex dc_lock;
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	/**
 	 * @audio_lock:
 	 *
@@ -373,6 +374,7 @@ struct amdgpu_display_manager {
 	 * successfully, false otherwise.
 	 */
 	bool audio_registered;
+#endif
 
 	/**
 	 * @irq_handler_list_low_tab:
@@ -614,8 +616,10 @@ struct amdgpu_dm_connector {
 	int max_vfreq ;
 	int pixel_clock_mhz;
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	/* Audio instance - protected by audio_lock. */
 	int audio_inst;
+#endif
 
 	struct mutex hpd_lock;
 
