639722d7b98f7c44a8342f84a2278701583a5441
Yifan Zhang <yifan1.zhang@amd.com>
2019-07-12 14:07:38 +0800

    drm/amd/autoconf: Test whether in_compat_syscall is defined.
    Test whether in_compat_syscall is defined.
    
    Change-Id: Ibe2206ffb3574ceb63249894eb0d7c077e46057b
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_compat.h
+++ include/kcl/kcl_compat.h
@@ -1,11 +1,10 @@
 #ifndef AMDKCL_COMPATE_H
 #define AMDKCL_COMPATE_H
 
-#include <linux/version.h>
 #include <linux/compat.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 6, 0)
-#define in_compat_syscall is_compat_task
+#ifndef in_compat_syscall
+static inline bool in_compat_syscall(void) { return is_compat_task(); }
 #endif
 
 #endif /* AMDKCL_COMPATE_H */
