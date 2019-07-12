72d8d2056c14a0c4c6e41fb8683361cdd3f544ad
changzhu <Changfeng.Zhu@amd.com>
2019-07-12 14:49:19 +0800

    drm/amdkcl: add AC_AMDGPU_DRM_AUDIO_COMPONENT_HEADER
    it is a squash of:
    
    drm/amdkcl: test whether drm/drm_audio_component.h is available
    
    [why]
    drm_audio_component.h is not defined before drm version 4.19.0. So there
    will be build error when using them before drm version 4.19.0.
    
    [How]
    Use autoconf way to check drm_audio_component_ops,
    drm_audio_component_audio_ops,drm_audio_component in kernel. If they are
    not defined,avoid to use them in driver code.
    This autoconf patch is caused by patch:
    drm/amd/display: Add drm_audio_component support to amdgpu_dm
    
    Change-Id: Ic75928406a042a0e08f5b06cf64543555c62e476
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: correct drm_audio_component_header.m4 patten
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: fix check for drm/drm_audio_component.h
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -64,7 +64,10 @@
 #include <linux/pm_runtime.h>
 #include <linux/pci.h>
 #include <linux/firmware.h>
+
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 #include <linux/component.h>
+#endif
 
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_uapi.h>
@@ -74,7 +77,9 @@
 #include <drm/drm_fourcc.h>
 #include <drm/drm_edid.h>
 #include <drm/drm_vblank.h>
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 #include <drm/drm_audio_component.h>
+#endif
 #include <drm/drm_hdcp.h>
 
 #if defined(CONFIG_DRM_AMD_DC_DCN1_0)
@@ -581,6 +586,7 @@ static void amdgpu_dm_fbc_init(struct drm_connector *connector)
 
 }
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 static int amdgpu_dm_audio_component_get_eld(struct device *kdev, int port,
 					  int pipe, bool *enabled,
 					  unsigned char *buf, int max_bytes)
@@ -713,6 +719,7 @@ static  void amdgpu_dm_audio_eld_notify(struct amdgpu_device *adev, int pin)
 						 pin, -1);
 	}
 }
+#endif
 
 static int dm_dmub_hw_init(struct amdgpu_device *adev)
 {
@@ -860,7 +867,10 @@ static int amdgpu_dm_init(struct amdgpu_device *adev)
 #endif
 
 	mutex_init(&adev->dm.dc_lock);
+
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	mutex_init(&adev->dm.audio_lock);
+#endif
 
 	if(amdgpu_dm_irq_init(adev)) {
 		DRM_ERROR("amdgpu: failed to initialize DM IRQ support.\n");
@@ -1014,7 +1024,9 @@ static void amdgpu_dm_fini(struct amdgpu_device *adev)
 		drm_encoder_cleanup(&adev->dm.mst_encoders[i].base);
 	}
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	amdgpu_dm_audio_fini(adev);
+#endif
 
 	amdgpu_dm_destroy_drm_device(&adev->dm);
 
@@ -1055,7 +1067,9 @@ static void amdgpu_dm_fini(struct amdgpu_device *adev)
 		adev->dm.freesync_module = NULL;
 	}
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	mutex_destroy(&adev->dm.audio_lock);
+#endif
 	mutex_destroy(&adev->dm.dc_lock);
 
 	return;
@@ -2913,12 +2927,14 @@ static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 		return r;
 	}
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	r = amdgpu_dm_audio_init(adev);
 	if (r) {
 		dc_release_state(state->context);
 		kfree(state);
 		return r;
 	}
+#endif
 
 	return 0;
 }
@@ -6434,7 +6450,9 @@ void amdgpu_dm_connector_init_helper(struct amdgpu_display_manager *dm,
 	aconnector->base.stereo_allowed = false;
 	aconnector->base.dpms = DRM_MODE_DPMS_OFF;
 	aconnector->hpd.hpd = AMDGPU_HPD_NONE; /* not used */
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	aconnector->audio_inst = -1;
+#endif
 	mutex_init(&aconnector->hpd_lock);
 
 	/*
@@ -7423,6 +7441,7 @@ cleanup:
 	kfree(bundle);
 }
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 static void amdgpu_dm_commit_audio(struct drm_device *dev,
 				   struct drm_atomic_state *state)
 {
@@ -7497,6 +7516,7 @@ static void amdgpu_dm_commit_audio(struct drm_device *dev,
 		amdgpu_dm_audio_eld_notify(adev, inst);
 	}
 }
+#endif
 
 /*
  * amdgpu_dm_crtc_copy_transient_flags - copy mirrored flags from DRM to DC
@@ -7864,8 +7884,10 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 						dm, crtc, wait_for_vblank);
 	}
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	/* Update audio instances for each connector. */
 	amdgpu_dm_commit_audio(dev, state);
+#endif
 
 	/*
 	 * send vblank event on all events not handled in flip and
