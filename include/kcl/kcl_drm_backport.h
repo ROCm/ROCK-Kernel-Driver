34d0b269c4bb561c0aa641b6d16ff32a7670a65b
Yifan Zhang <yifan1.zhang@amd.com>
2019-07-16 16:36:25 +0800

    drm/amdkcl: Test whether drm_get_format_name() is available
    v2: drm/amdkcl: drop kcl_drm_get_format_name
    
    Change-Id: I35b35a5e91dff3e32ab21deac46767cb7767aabe
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>/
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- include/kcl/kcl_drm_backport.h
+++ include/kcl/kcl_drm_backport.h
@@ -80,4 +80,34 @@ static inline int _kcl_drm_universal_plane_init(struct drm_device *dev, struct d
 #define drm_universal_plane_init _kcl_drm_universal_plane_init
 #endif
 
+#if !defined(HAVE_DRM_GET_FORMAT_NAME_I_P)
+/**
+ * struct drm_format_name_buf - name of a DRM format
+ * @str: string buffer containing the format name
+ */
+struct drm_format_name_buf {
+	char str[32];
+};
+
+static char printable_char(int c)
+{
+	return isascii(c) && isprint(c) ? c : '?';
+}
+
+static inline const char *_kcl_drm_get_format_name(uint32_t format, struct drm_format_name_buf *buf)
+{
+	snprintf(buf->str, sizeof(buf->str),
+		 "%c%c%c%c %s-endian (0x%08x)",
+		 printable_char(format & 0xff),
+		 printable_char((format >> 8) & 0xff),
+		 printable_char((format >> 16) & 0xff),
+		 printable_char((format >> 24) & 0x7f),
+		 format & DRM_FORMAT_BIG_ENDIAN ? "big" : "little",
+		 format);
+
+	return buf->str;
+}
+#define drm_get_format_name _kcl_drm_get_format_name
+#endif
+
 #endif /* AMDKCL_DRM_BACKPORT_H */
