d5a0e8817dacf2f26504ec37af8bdff3ff77d072
Yifan Zhang <yifan1.zhang@amd.com>
2019-07-22 10:35:21 +0800

    change dma_fence_get_rcu_safe to kcl_fence_get_rcu_safe
    dma_fence_get_rcu_safe doesn't exist in lower version kernel.
    
    Change-Id: I0f1bfd60451a3bea33b0c5d0b2e09836d3fffaee
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
@@ -158,7 +158,7 @@ int amdgpu_fence_emit(struct amdgpu_ring *ring, struct dma_fence **f,
 		struct dma_fence *old;
 
 		rcu_read_lock();
-		old = dma_fence_get_rcu_safe(ptr);
+		old = kcl_fence_get_rcu_safe(ptr);
 		rcu_read_unlock();
 
 		if (old) {
