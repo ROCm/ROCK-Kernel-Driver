88fcc6d0298b0a889ed2030ea1fc3b499365b74c
Kenneth Feng <kenneth.feng@amd.com>
2019-07-23 12:16:25 +0800

    drm/amd/powerplay: change smu_read_sensor sequence in smu
    change the smu_read_sensor sequence to:
    
    asic specific sensor read -> smu v11 specific sensor read -> smu v11 common sensor read
    
    Signed-off-by: Kenneth Feng <kenneth.feng@amd.com>

--- drivers/gpu/drm/amd/powerplay/navi10_ppt.c
+++ drivers/gpu/drm/amd/powerplay/navi10_ppt.c
@@ -1385,6 +1385,9 @@ static int navi10_read_sensor(struct smu_context *smu,
 	struct smu_table_context *table_context = &smu->smu_table;
 	PPTable_t *pptable = table_context->driver_pptable;
 
+	if(!data || !size)
+		return -EINVAL;
+
 	switch (sensor) {
 	case AMDGPU_PP_SENSOR_MAX_FAN_RPM:
 		*(uint32_t *)data = pptable->FanMaximumRpm;
@@ -1406,7 +1409,7 @@ static int navi10_read_sensor(struct smu_context *smu,
 		*size = 4;
 		break;
 	default:
-		return -EINVAL;
+		ret = smu_smc_read_sensor(smu, sensor, data, size);
 	}
 
 	return ret;
