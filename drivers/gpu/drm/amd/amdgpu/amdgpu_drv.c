9c232e5754db1386cf2b95fd9fb9099ec02ce8c3
Rui Teng <rui.teng@amd.com>
2019-07-26 08:58:16 +0800

    Bump AMDGPU version to 5.0.77
    Change-Id: Ida06835457a901310e60b44da3fd2c7c5334980b
    Signed-off-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -82,7 +82,7 @@
 #define KMS_DRIVER_MINOR	33
 #define KMS_DRIVER_PATCHLEVEL	0
 
-#define AMDGPU_VERSION		"5.0.76"
+#define AMDGPU_VERSION		"5.0.77"
 #define AMDGPU_MAX_TIMEOUT_PARAM_LENTH	256
 
 int amdgpu_vram_limit = 0;
