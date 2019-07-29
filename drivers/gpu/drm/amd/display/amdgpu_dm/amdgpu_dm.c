c256c069c00708d4bd23d84946c2a310db166c21
Yifan Zhang <yifan1.zhang@amd.com>
2019-07-29 10:54:51 +0800

    drm/amd/autoconf: test for_each_new_crtc_in_state directly
    for_each_new_crtc_in_state is a macro which can be tested directly
    with #ifdef, save a m4 macro
    
    Change-Id: I747b753244b1b514ef4a2ad4c1e0cc8bd8fb1314
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -1270,7 +1270,7 @@ static int dm_resume(void *handle)
 	}
 
 	/* Force mode set in atomic commit */
-#if !defined(HAVE_FOR_EACH_NEW_CRTC_IN_STATE)
+#if !defined(for_each_new_crtc_in_state)
 	for_each_crtc_in_state(dm->cached_state, crtc, new_crtc_state, i)
 #else
 	for_each_new_crtc_in_state(dm->cached_state, crtc, new_crtc_state, i)
@@ -1282,7 +1282,7 @@ static int dm_resume(void *handle)
 	 * them here, since they were duplicated as part of the suspend
 	 * procedure.
 	 */
-#if !defined(HAVE_FOR_EACH_NEW_CRTC_IN_STATE)
+#if !defined(for_each_new_crtc_in_state)
 	for_each_crtc_in_state(adev->dm.cached_state, crtc, new_crtc_state, i) {
 #else
 	for_each_new_crtc_in_state(dm->cached_state, crtc, new_crtc_state, i) {
@@ -6890,7 +6890,7 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 		mutex_unlock(&dm->dc_lock);
 	}
 
-#if !defined(HAVE_FOR_EACH_NEW_CRTC_IN_STATE)
+#if !defined(for_each_new_crtc_in_state)
 	list_for_each_entry(crtc, &dev->mode_config.crtc_list, head) {
 		new_crtc_state = crtc->state;
 #else
@@ -7009,7 +7009,7 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 	/* Enable interrupts for CRTCs going through a modeset. */
 	amdgpu_dm_enable_crtc_interrupts(dev, state, true);
 
-#if !defined(HAVE_FOR_EACH_NEW_CRTC_IN_STATE)
+#if !defined(for_each_new_crtc_in_state)
 	for_each_crtc_in_state(state, crtc, old_crtc_state, i) {
 		new_crtc_state = crtc->state;
 #else
@@ -7027,7 +7027,7 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 	}
 
 	/* update planes when needed per crtc*/
-#if !defined(HAVE_FOR_EACH_NEW_CRTC_IN_STATE)
+#if !defined(for_each_new_crtc_in_state)
 	for_each_crtc_in_state(state, crtc, old_crtc_state, i) {
 		new_crtc_state = crtc->state;
 #else
@@ -7055,7 +7055,7 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 	 * mark consumed event for drm_atomic_helper_commit_hw_done
 	 */
 	spin_lock_irqsave(&adev->ddev->event_lock, flags);
-#if !defined(HAVE_FOR_EACH_NEW_CRTC_IN_STATE)
+#if !defined(for_each_new_crtc_in_state)
 	for_each_crtc_in_state(state, crtc, old_crtc_state, i) {
 		new_crtc_state = crtc->state;
 #else
