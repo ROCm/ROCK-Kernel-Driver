144b580a1a14edb68bae45982a2c0bfb70866aa9
Colin Ian King <colin.king@canonical.com>
2019-08-01 09:39:41 +0100

    drm/amd/powerplay: fix a few spelling mistakes
    There are a few spelling mistakes "unknow" -> "unknown" and
    "enabeld" -> "enabled". Fix these.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/amdgpu_smu.c
+++ drivers/gpu/drm/amd/powerplay/amdgpu_smu.c
@@ -38,7 +38,7 @@ static const char* __smu_message_names[] = {
 const char *smu_get_message_name(struct smu_context *smu, enum smu_message_type type)
 {
 	if (type < 0 || type >= SMU_MSG_MAX_COUNT)
-		return "unknow smu message";
+		return "unknown smu message";
 	return __smu_message_names[type];
 }
 
@@ -51,7 +51,7 @@ static const char* __smu_feature_names[] = {
 const char *smu_get_feature_name(struct smu_context *smu, enum smu_feature_mask feature)
 {
 	if (feature < 0 || feature >= SMU_FEATURE_COUNT)
-		return "unknow smu feature";
+		return "unknown smu feature";
 	return __smu_feature_names[feature];
 }
 
@@ -86,7 +86,7 @@ size_t smu_sys_get_pp_feature_mask(struct smu_context *smu, char *buf)
 			       smu_get_feature_name(smu, sort_feature[i]),
 			       i,
 			       !!smu_feature_is_enabled(smu, sort_feature[i]) ?
-			       "enabeld" : "disabled");
+			       "enabled" : "disabled");
 	}
 
 failed:
