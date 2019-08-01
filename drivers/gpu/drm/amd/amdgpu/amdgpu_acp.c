ade8fe53998a454cbefcdd7aad6c1472e0aa71c5
changzhu <Changfeng.Zhu@amd.com>
2019-08-01 16:26:59 +0800

    drm/amdkcl: Test whether pm_genpd_remove_device wants two args
    Change-Id: I8106ddc74a281d5aafb4883b49f329cc93c67769
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Rui Teng <rui.teng@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: drop symbol check for pm_genpd_remove_device
    it's not a symbol with CONFIG_PM_GENERIC_DOMAINS undefined.
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_acp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_acp.c
@@ -463,7 +463,11 @@ static int acp_hw_fini(void *handle)
 
 	for (i = 0; i < ACP_DEVS ; i++) {
 		dev = get_mfd_cell_dev(adev->acp.acp_cell[i].name, i);
+#ifndef HAVE_PM_GENPD_REMOVE_DEVICE_2ARGS
 		ret = pm_genpd_remove_device(dev);
+#else
+		ret = pm_genpd_remove_device(&adev->acp.acp_genpd->gpd, dev);
+#endif
 		/* If removal fails, dont giveup and try rest */
 		if (ret)
 			dev_err(dev, "remove dev from genpd failed\n");
