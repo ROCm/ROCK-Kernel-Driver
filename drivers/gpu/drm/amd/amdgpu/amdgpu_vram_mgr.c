6704ddd44adbb26865404136925cfa354e7e0b9d
Chengming Gui <Jack.Gui@amd.com>
2019-08-05 10:10:54 +0800

    drm/amdkcl: Test whether drm_mm_insert_mode is available
    drm_mm_search_flags was replaced with drm_mm_insert_mode
    when kernel 4.11 introduced
    
    v1: drm/amdkcl: fix missing HAVE_DRM_MM_INSERT_MODE check (v2)
    v2: clean code
    v3: drm/amdkcl: accommodate to drmP.h removal for drm-mm-insert-mode.m4
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vram_mgr.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vram_mgr.c
@@ -317,7 +317,12 @@ static int amdgpu_vram_mgr_new(struct ttm_mem_type_manager *man,
 	struct amdgpu_vram_mgr *mgr = man->priv;
 	struct drm_mm *mm = &mgr->mm;
 	struct drm_mm_node *nodes;
+#ifndef HAVE_DRM_MM_INSERT_MODE
+	enum drm_mm_search_flags sflags = DRM_MM_SEARCH_DEFAULT;
+	enum drm_mm_allocator_flags aflags = DRM_MM_CREATE_DEFAULT;
+#else
 	enum drm_mm_insert_mode mode;
+#endif
 	unsigned long lpfn, num_nodes, pages_per_node, pages_left;
 	uint64_t vis_usage = 0, mem_bytes, max_bytes;
 	unsigned i;
@@ -360,9 +365,16 @@ static int amdgpu_vram_mgr_new(struct ttm_mem_type_manager *man,
 		return -ENOMEM;
 	}
 
+#ifndef HAVE_DRM_MM_INSERT_MODE
+	if (place->flags & TTM_PL_FLAG_TOPDOWN) {
+		sflags = DRM_MM_SEARCH_BELOW;
+		aflags = DRM_MM_CREATE_TOP;
+	}
+#else
 	mode = DRM_MM_INSERT_BEST;
 	if (place->flags & TTM_PL_FLAG_TOPDOWN)
 		mode = DRM_MM_INSERT_HIGH;
+#endif
 
 	mem->start = 0;
 	pages_left = mem->num_pages;
@@ -371,10 +383,19 @@ static int amdgpu_vram_mgr_new(struct ttm_mem_type_manager *man,
 	for (i = 0; pages_left >= pages_per_node; ++i) {
 		unsigned long pages = rounddown_pow_of_two(pages_left);
 
+#ifndef HAVE_DRM_MM_INSERT_MODE
+		sflags |= DRM_MM_SEARCH_BEST;
+		uint32_t alignment = mem->page_alignment;
+		r = drm_mm_insert_node_in_range_generic(mm, &nodes[i], pages,
+					alignment, 0,
+					place->fpfn, lpfn,
+					sflags, aflags);
+#else
 		r = drm_mm_insert_node_in_range(mm, &nodes[i], pages,
 						pages_per_node, 0,
 						place->fpfn, lpfn,
 						mode);
+#endif
 		if (unlikely(r))
 			break;
 
@@ -389,11 +410,20 @@ static int amdgpu_vram_mgr_new(struct ttm_mem_type_manager *man,
 
 		if (pages == pages_per_node)
 			alignment = pages_per_node;
+#ifndef HAVE_DRM_MM_INSERT_MODE
+		else
+			sflags |= DRM_MM_SEARCH_BEST;
 
+		r = drm_mm_insert_node_in_range_generic(mm, &nodes[i], pages,
+							alignment, 0,
+							place->fpfn, lpfn,
+							sflags, aflags);
+#else
 		r = drm_mm_insert_node_in_range(mm, &nodes[i],
 						pages, alignment, 0,
 						place->fpfn, lpfn,
 						mode);
+#endif
 		if (unlikely(r))
 			goto error;
 
