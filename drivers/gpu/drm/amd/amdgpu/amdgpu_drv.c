6cbfc7dc9ba9f3c0d07dc6c3d54c4ced3ec183c6
Flora Cui <flora.cui@amd.com>
2019-08-06 20:15:47 -0300

    drm/amdkcl: Test whether mmu_notifier_synchronize is available
    introduced by kernel v5.3-rc1-29-g2c7933f53f6b
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    Change-Id: I0a46e15c16ec5d3e79687a1a33e50069ba956244

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -3138,7 +3138,9 @@ static void __exit amdgpu_exit(void)
 	amdgpu_sync_fini();
 	amdgpu_fence_slab_fini();
 	amdgpu_userq_fence_slab_fini();
+#ifdef HAVE_MMU_NOTIFIER_SYNCHRONIZE
 	mmu_notifier_synchronize();
+#endif
 	amdgpu_xcp_drv_release();
 }
 
