bde93f5517a67ad1e0e675127edafa13dd114ff8
Chengming Gui <Jack.Gui@amd.com>
2019-08-09 14:29:29 +0800

    drm/amdkcl: Test whether amd_iommu_pc_supported is available (v2)
    amd_iommu_pc_xxx introduced by kernel v3.11-rc1~144^2~20
    v2: fix typo and correct header file included
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amd/autoconf: fix missing HAVE_AMD_IOMMU_PC_SUPPORTED
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amd/autoconf: fix missing HAVE_AMD_IOMMU_PC_SUPPORTED
    this break rhel7.6
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    
    drm/amd/autoconf: check CONFIG_AMD_IOMMU enabled for amd_iommu_pc_*
    or else the symbols are missing.
    This is for in-tree build.
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: Fix kfd_iommu dkms install error
    Issue introduced by commit: 4b38b7a521e3aa401f986483722154b3ba701538
    Fix RHEL7.6 with 3.10 kernel kfd_iommu dkms install error
    some legacy kcl path miss replaced with autoconf macro
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_topology.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_topology.c
@@ -132,7 +132,9 @@ static void kfd_release_topology_device(struct kfd_topology_device *dev)
 	struct kfd_cache_properties *cache;
 	struct kfd_iolink_properties *iolink;
 	struct kfd_iolink_properties *p2plink;
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	struct kfd_perf_properties *perf;
+#endif
 
 	list_del(&dev->list);
 
@@ -164,12 +166,14 @@ static void kfd_release_topology_device(struct kfd_topology_device *dev)
 		kfree(p2plink);
 	}
 
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	while (dev->perf_props.next != &dev->perf_props) {
 		perf = container_of(dev->perf_props.next,
 				struct kfd_perf_properties, list);
 		list_del(&perf->list);
 		kfree(perf);
 	}
+#endif
 
 	kfree(dev);
 }
@@ -206,7 +210,9 @@ struct kfd_topology_device *kfd_create_topology_device(
 	INIT_LIST_HEAD(&dev->cache_props);
 	INIT_LIST_HEAD(&dev->io_link_props);
 	INIT_LIST_HEAD(&dev->p2p_link_props);
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	INIT_LIST_HEAD(&dev->perf_props);
+#endif
 
 	list_add_tail(&dev->list, device_list);
 
@@ -398,6 +404,7 @@ static const struct kobj_type cache_type = {
 	.sysfs_ops = &cache_ops,
 };
 
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 /****** Sysfs of Performance Counters ******/
 
 struct kfd_perf_attr {
@@ -431,6 +438,7 @@ static struct kfd_perf_attr perf_attr_iommu[] = {
 	KFD_PERF_DESC(counter_ids, 0),
 };
 /****************************************/
+#endif
 
 static ssize_t node_show(struct kobject *kobj, struct attribute *attr,
 		char *buffer)
@@ -586,7 +594,9 @@ static void kfd_remove_sysfs_node_entry(struct kfd_topology_device *dev)
 	struct kfd_iolink_properties *iolink;
 	struct kfd_cache_properties *cache;
 	struct kfd_mem_properties *mem;
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	struct kfd_perf_properties *perf;
+#endif
 
 	if (dev->kobj_p2plink) {
 		list_for_each_entry(p2plink, &dev->p2p_link_props, list)
@@ -652,6 +662,7 @@ static void kfd_remove_sysfs_node_entry(struct kfd_topology_device *dev)
 		dev->kobj_mem = NULL;
 	}
 
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	if (dev->kobj_perf) {
 		list_for_each_entry(perf, &dev->perf_props, list) {
 			kfree(perf->attr_group);
@@ -661,6 +672,7 @@ static void kfd_remove_sysfs_node_entry(struct kfd_topology_device *dev)
 		kobject_put(dev->kobj_perf);
 		dev->kobj_perf = NULL;
 	}
+#endif
 
 	if (dev->kobj_node) {
 		sysfs_remove_file(dev->kobj_node, &dev->attr_gpuid);
@@ -679,10 +691,13 @@ static int kfd_build_sysfs_node_entry(struct kfd_topology_device *dev,
 	struct kfd_iolink_properties *iolink;
 	struct kfd_cache_properties *cache;
 	struct kfd_mem_properties *mem;
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	struct kfd_perf_properties *perf;
-	int ret;
-	uint32_t i, num_attrs;
+	uint32_t num_attrs;
 	struct attribute **attrs;
+#endif
+	int ret;
+	uint32_t i;
 
 	if (WARN_ON(dev->kobj_node))
 		return -EEXIST;
@@ -717,9 +732,11 @@ static int kfd_build_sysfs_node_entry(struct kfd_topology_device *dev,
 	if (!dev->kobj_p2plink)
 		return -ENOMEM;
 
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	dev->kobj_perf = kobject_create_and_add("perf", dev->kobj_node);
 	if (!dev->kobj_perf)
 		return -ENOMEM;
+#endif
 
 	/*
 	 * Creating sysfs files for node properties
@@ -838,6 +855,7 @@ static int kfd_build_sysfs_node_entry(struct kfd_topology_device *dev,
 		i++;
 	}
 
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	/* All hardware blocks have the same number of attributes. */
 	num_attrs = ARRAY_SIZE(perf_attr_iommu);
 	list_for_each_entry(perf, &dev->perf_props, list) {
@@ -863,6 +881,7 @@ static int kfd_build_sysfs_node_entry(struct kfd_topology_device *dev,
 		if (ret < 0)
 			return ret;
 	}
+#endif
 
 	return 0;
 }
@@ -1092,8 +1111,10 @@ int kfd_topology_init(void)
 		goto err;
 	}
 
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	kdev = list_first_entry(&temp_topology_device_list,
 				struct kfd_topology_device, list);
+#endif
 
 	down_write(&topology_lock);
 	kfd_topology_update_device_list(&temp_topology_device_list,
