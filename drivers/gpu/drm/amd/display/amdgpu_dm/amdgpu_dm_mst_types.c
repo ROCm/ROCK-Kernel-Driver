c4784f008725fcb9ee0b1d47cc11efa31403ab63
changzhu <Changfeng.Zhu@amd.com>
2019-08-09 11:06:58 +0800

    drm/amdkcl: Test whether whether drm_dp_mst_connector_{early_unregister,late_register} are available
    Rebase 5.3 squash:
    * 600691173f87 drm/amd/autoconf: Test whether drm_dp_mst_connector_late_register is available
    * 8baafc6b96b7 drm/amd/autoconf: Test whether whether drm_dp_mst_connector_early_unregister is available
    
    Change-Id: I8d5367bf7adf47083cb0c050a9b01b1b5145614f
    Signed-off-by: Adam Yang <Adam1.Yang@amd.com>
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Leo Li <sunpeng.li@amd.com>
    Reviewed-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -110,6 +110,7 @@ dm_dp_mst_connector_destroy(struct drm_connector *connector)
 	kfree(amdgpu_dm_connector);
 }
 
+#if defined(HAVE_DRM_DP_MST_CONNECTOR_LATE_REGISTER)
 static int
 amdgpu_dm_mst_connector_late_register(struct drm_connector *connector)
 {
@@ -128,7 +129,9 @@ amdgpu_dm_mst_connector_late_register(struct drm_connector *connector)
 
 	return r;
 }
+#endif /* HAVE_DRM_DP_MST_CONNECTOR_LATE_REGISTER */
 
+#if defined(HAVE_DRM_DP_MST_CONNECTOR_EARLY_UNREGISTER)
 static void
 amdgpu_dm_mst_connector_early_unregister(struct drm_connector *connector)
 {
@@ -138,6 +141,7 @@ amdgpu_dm_mst_connector_early_unregister(struct drm_connector *connector)
 
 	drm_dp_mst_connector_early_unregister(connector, port);
 }
+#endif /* HAVE_DRM_DP_MST_CONNECTOR_EARLY_UNREGISTER */
 
 static const struct drm_connector_funcs dm_dp_mst_connector_funcs = {
 	.fill_modes = drm_helper_probe_single_connector_modes,
@@ -147,8 +151,12 @@ static const struct drm_connector_funcs dm_dp_mst_connector_funcs = {
 	.atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
 	.atomic_set_property = amdgpu_dm_connector_atomic_set_property,
 	.atomic_get_property = amdgpu_dm_connector_atomic_get_property,
+#if defined(HAVE_DRM_DP_MST_CONNECTOR_LATE_REGISTER)
 	.late_register = amdgpu_dm_mst_connector_late_register,
+#endif /* HAVE_DRM_DP_MST_CONNECTOR_LATE_REGISTER */
+#if defined(HAVE_DRM_DP_MST_CONNECTOR_EARLY_UNREGISTER)
 	.early_unregister = amdgpu_dm_mst_connector_early_unregister,
+#endif /* HAVE_DRM_DP_MST_CONNECTOR_EARLY_UNREGISTER */
 };
 
 #if defined(CONFIG_DRM_AMD_DC_DCN1_0)
