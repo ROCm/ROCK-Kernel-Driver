fd34f41f3fcaac13d5b3aaad04004b862fd9a5eb
Chengming Gui <Jack.Gui@amd.com>
2019-08-13 14:21:43 +0800

    drm/amd/autoconf: Fix kfd_iommu dkms install error
    Issue introduced by commit: 4b38b7a521e3aa401f986483722154b3ba701538
    Fix RHEL7.6 with 3.10 kernel kfd_iommu dkms install error
    some legacy kcl path miss replaced with autoconf macro
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_topology.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_topology.h
@@ -155,7 +155,7 @@ struct kfd_iolink_properties {
 	struct attribute	attr;
 };
 
-#if LINUX_VERSION_CODE > KERNEL_VERSION(3, 10, 0)
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 struct kfd_perf_properties {
 	struct list_head	list;
 	char			block_name[16];
@@ -174,7 +174,7 @@ struct kfd_topology_device {
 	struct list_head		cache_props;
 	uint32_t			io_link_count;
 	struct list_head		io_link_props;
-#if LINUX_VERSION_CODE > KERNEL_VERSION(3, 10, 0)
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	struct list_head		perf_props;
 #endif
 	struct kfd_dev			*gpu;
@@ -182,7 +182,7 @@ struct kfd_topology_device {
 	struct kobject			*kobj_mem;
 	struct kobject			*kobj_cache;
 	struct kobject			*kobj_iolink;
-#if LINUX_VERSION_CODE > KERNEL_VERSION(3, 10, 0)
+#ifdef HAVE_AMD_IOMMU_PC_SUPPORTED
 	struct kobject			*kobj_perf;
 #endif
 	struct attribute		attr_gpuid;
