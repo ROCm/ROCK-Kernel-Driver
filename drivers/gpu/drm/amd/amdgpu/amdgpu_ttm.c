4b4b519db026efb3fa6281f3b2198efe6c9fbde8
Yifan Zhang <yifan1.zhang@amd.com>
2019-08-14 12:58:05 +0800

    drm/amdkcl: Test whether drm_mm_print is available
    v1: drm/amdkcl: Test whether drm_debug_printer() function is available
    v2: drm/amdkcl: fix the struct drm_printer error.
    v3: drm/amdkcl: fix drm_printer related checks
    v4: drm/amdkcl: accommodate to drmP.h removal for drm-mm-print.m4
    
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Change-Id: I66988af2c3ae0af9782f278edebaaafbccd3fa30

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -2594,7 +2594,6 @@ int amdgpu_ttm_evict_resources(struct amdgpu_device *adev, int mem_type)
 }
 
 #if defined(CONFIG_DEBUG_FS)
-
 static int amdgpu_ttm_page_pool_show(struct seq_file *m, void *unused)
 {
 	struct amdgpu_device *adev = m->private;
