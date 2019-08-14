b3f0282c70f9b9d8a4b82e8da20a7ebde61929ad
Yifan Zhang <yifan1.zhang@amd.com>
2019-08-14 12:58:05 +0800

    drm/amdkcl: Test whether drm_debug_printer is available
    v1: drm/amdkcl: Test whether drm_debug_printer() function is available
    v2: drm/amdkcl: fix the struct drm_printer error.
    v3: drm/amdkcl: fix drm_printer related checks
    v4: drm/amdkcl: accommodate to drmP.h removal for drm-mm-print.m4
    
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- include/kcl/kcl_drm.h
+++ include/kcl/kcl_drm.h
@@ -14,4 +14,26 @@ struct drm_printer {
 void drm_printf(struct drm_printer *p, const char *f, ...);
 #endif
 
+/**
+ * drm_debug_printer - construct a &drm_printer that outputs to pr_debug()
+ * @prefix: debug output prefix
+ *
+ * RETURNS:
+ * The &drm_printer object
+ */
+#if !defined(HAVE_DRM_DEBUG_PRINTER)
+extern void __drm_printfn_debug(struct drm_printer *p, struct va_format *vaf);
+
+static inline struct drm_printer drm_debug_printer(const char *prefix)
+{
+	struct drm_printer p = {
+		.printfn = __drm_printfn_debug,
+#if !defined(HAVE_DRM_DRM_PRINT_H)
+		.prefix = prefix
+#endif
+	};
+	return p;
+}
+#endif
+
 #endif /* AMDKCL_DRM_H */
