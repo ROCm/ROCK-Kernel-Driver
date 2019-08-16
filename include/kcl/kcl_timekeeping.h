7d3b640de59d60fb6dcceb07f26db8cffa906341
Chengming Gui <Jack.Gui@amd.com>
2019-08-16 14:45:16 +0800

    drm/amd/autoconf: Test whether ktime_get_ns() is available
    ktime_get_ns introduced by kernel v3.17-rc1~109^2~41
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_timekeeping.h
+++ include/kcl/kcl_timekeeping.h
@@ -1,7 +1,7 @@
 #ifndef _KCL_LINUX_TIMEKEEPING_H
 #define _KCL_LINUX_TIMEKEEPING_H
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 17, 0)) && !defined(OS_NAME_RHEL_7_X)
+#ifndef HAVE_KTIME_GET_NS
 static inline u64 ktime_get_ns(void)
 {
 	return ktime_to_ns(ktime_get());
