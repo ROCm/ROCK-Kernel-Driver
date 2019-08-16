95a69dd92f487a97ed505ebb9f19e35ee454ae34
Flora Cui <flora.cui@amd.com>
2019-08-16 17:00:31 +0800

    drm/amd/autoconf: fix compile warning for drm_atomic_helper_best_encoder()
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- include/kcl/kcl_drm_atomic_helper.h
+++ include/kcl/kcl_drm_atomic_helper.h
@@ -3,7 +3,7 @@
 
 #include <drm/drm_atomic_helper.h>
 
-
+#if (DRM_VERSION_CODE < DRM_VERSION(5, 0, 0)) && defined(BUILD_AS_DKMS)
 #if !defined(HAVE_DRM_ATOMIC_HELPER_BEST_ENCODER)
 static struct drm_encoder *
 drm_atomic_helper_best_encoder(struct drm_connector *connector)
@@ -11,6 +11,7 @@ drm_atomic_helper_best_encoder(struct drm_connector *connector)
             WARN_ON(connector->encoder_ids[1]);
             return drm_encoder_find(connector->dev, NULL, connector->encoder_ids[0]);
 }
+#endif
 #endif
 
 #endif /* AMDKCL_DRM_ATOMIC_HELPERH */
