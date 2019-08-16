a8d5ea6d9fa44b637e08f7d34b65e0ec3797b8ad
changzhu <Changfeng.Zhu@amd.com>
2019-08-16 12:18:26 +0800

    drm/amdkcl: Test whether type __poll_t is available
    [Why]
    __poll_t is not defined until patch:
    define __poll_t, annotate constants
    So there will be build error when using it in kfd_debug_events.c
    This problem is caused by patch:
    drm/amdkfd: add debug notification
    
    [How]
    Use autoconf patch to define __poll_t if it's not defined.
    
    Change-Id: I56bbeea7c27eb2974f224e8bccafb8bd97c794c1
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amd/autoconf: Test whether type __poll_t is available(v2)
    Change-Id: I397a3403223f9cbcfc29d36d63544ce4ca7ed4c6
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: fix macro define for __POLL_T
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    
    drm/amdkcl: drop HAVE_TYPE__POLL_T check outside of kcl
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -4,6 +4,7 @@
 
 #include <linux/version.h>
 #include <linux/kernel.h>
+#include <kcl/kcl_types.h>
 #include <kcl/kcl_hashtable.h>
 #include <kcl/kcl_overflow.h>
 #endif /* AMDGPU_BACKPORT_H */
---
+++ include/kcl/kcl_types.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef AMDKCL_TYPES_H
+#define AMDKCL_TYPES_H
+
+#ifndef HAVE_TYPE__POLL_T
+#ifdef __CHECK_POLL
+typedef unsigned __bitwise __poll_t;
+#else
+typedef unsigned __poll_t;
+#endif
+#endif
+#endif
