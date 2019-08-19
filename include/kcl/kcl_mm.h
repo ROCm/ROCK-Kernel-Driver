0cb9874a57c992eccb6cda37e9b0a5bafa21508c
Flora Cui <flora.cui@amd.com>
2019-08-19 11:47:41 +0800

    drm/amdkcl: test whether memory alloc and free functions are available
    checked functions:
    * drm_calloc_large()
    * drm_free_large()
    * drm_malloc_ab()
    * kvcalloc()
    * kvfree()
    * kvmalloc()
    * kvmalloc_array()
    * kvzalloc()
    
    v1: Rebase 5.2 squash:
    -01: d7eca10154b1 drm/amdkcl: [4.6] fix kcl_get_user_pages() (Partial)
    -02: bf34bf33eed4 drm/amdgpu: Fix version-dependent use of kvmalloc/kvfree
    -03: 24e56b34a857 Test whether drm_free_large() is available
    -04: 1dfc948d0ea3 Test whether drm_calloc_large() is available
    -05: 649125416af3 Test whether kv[mz]alloc() functions are available
    -06: 324f72f649bf Test whether kvfree() function is available
    -07: 68ef73a82c8d Test whether kvmalloc_array() function is available
    -08: fd898718a34e Test whether kvcalloc() function is available
    -09: b5a1effd0f4f drm/amd/autoconf: test whether drm_malloc_ab() is available
    -10: 858564e0ea8c drm/amd/autoconf: refactor HAVE_DRM_FREE_LARGE check
    -11: 02919b0c2ecc drm/amd/autoconf: refactorHAVE_DRM_CALLOC_LARGE check
    -12: 45a7c3ecde4a drm/amd/autoconf: fix kv[mz]alloc/kvfree/kvmalloc_array
    v2: drm/amdkcl: refactor kcl copy of kvfree & kvmalloc_array
    v3: drm/amdkcl: accommodate to drmP.h removal for drm-malloc-ab.m4
    v4: drm/amdkcl: refactor kvfree/kvmalloc series
    
    Signed-off-by: Adam Yang <Adam1.Yang@amd.com>
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Tianci Yin <tianci.yin@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: chen gong <curry.gong@amd.com>
    Reviewed-by: Anatoli Antonovitch <anatoli.antonovitch@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>
    Reviewed-by: Xiaojie Yuan <xiaojie.yuan@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yang Xiong <Yang.Xiong@amd.com>

--- include/kcl/kcl_mm.h
+++ include/kcl/kcl_mm.h
@@ -26,4 +26,48 @@ static inline bool fault_flag_allow_retry_first(unsigned int flags)
 }
 #endif
 
+#ifndef HAVE_KVFREE
+static inline void kvfree(const void *addr)
+{
+	if (is_vmalloc_addr(addr))
+		vfree(addr);
+	else
+		kfree(addr);
+}
+#endif
+
+#ifndef HAVE_KVZALLOC_KVMALLOC
+static inline void *kvmalloc(size_t size, gfp_t flags)
+{
+	void *out;
+
+	if (size > PAGE_SIZE)
+		out = __vmalloc(size, flags, PAGE_KERNEL);
+	else
+		out = kmalloc(size, flags);
+	return out;
+}
+static inline void *kvzalloc(size_t size, gfp_t flags)
+{
+	return kvmalloc(size, flags | __GFP_ZERO);
+}
+#endif /* HAVE_KVZALLOC_KVMALLOC */
+
+#ifndef HAVE_KVMALLOC_ARRAY
+static inline void *kvmalloc_array(size_t n, size_t size, gfp_t flags)
+{
+	if (size != 0 && n > SIZE_MAX / size)
+		return NULL;
+
+	return kvmalloc(n * size, flags);
+}
+#endif /* HAVE_KVMALLOC_ARRAY */
+
+#ifndef HAVE_KVCALLOC
+static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
+{
+	return kvmalloc_array(n, size, flags | __GFP_ZERO);
+}
+#endif /* HAVE_KVCALLOC */
+
 #endif /* AMDKCL_MM_H */
