ad0d2e4865f3651d9d79c2dcacd5e7a0065ab763
Chengming Gui <Jack.Gui@amd.com>
2019-08-22 09:52:00 +0800

    drm/amdkcl: Test whehter drm_fb_helper_set_suspend_unlocked() is available
    drm_fb_helper_set_suspend_unlocked() introduced
    and exported by kernel v4.9-rc1~41^2~39^2~4
    
    v1: drm/amdkcl: drop kcl_drm_fb_helper_set_suspend_unlocked
    v2: drm/amdkcl: drop symbol check for drm_fb_helper_set_suspend_unlocked
    v3: drm/amdkcl: accommodate to drmP.h removal for drm-fb-helper-set-suspend-unlocked.m4
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>/
    Signed-off-by: Jack Gui <Jack Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jiansong Chen <Jiansong.Chen@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_drm.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm.c
@@ -466,3 +466,23 @@ void _kcl_drm_fb_helper_unregister_fbi(struct drm_fb_helper *fb_helper)
 }
 EXPORT_SYMBOL(_kcl_drm_fb_helper_unregister_fbi);
 #endif
+
+#ifndef HAVE_DRM_FB_HELPER_SET_SUSPEND_UNLOCKED
+/**
+ * _kcl_drm_fb_helper_set_suspend_stub - wrapper around fb_set_suspend
+ * @fb_helper: driver-allocated fbdev helper
+ * @state: desired state, zero to resume, non-zero to suspend
+ *
+ * A wrapper around fb_set_suspend implemented by fbdev core
+ */
+void _kcl_drm_fb_helper_set_suspend_unlocked(struct drm_fb_helper *fb_helper, int state)
+{
+	if (!fb_helper || !fb_helper->fbdev)
+		return;
+
+	console_lock();
+	fb_set_suspend(fb_helper->fbdev, state);
+	console_unlock();
+}
+EXPORT_SYMBOL(_kcl_drm_fb_helper_set_suspend_unlocked);
+#endif
