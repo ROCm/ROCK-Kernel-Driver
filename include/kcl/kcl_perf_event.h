8ed5a1c8d87f972325b2c831458dac756956e95e
Chengming Gui <Jack.Gui@amd.com>
2019-08-26 09:43:41 +0800

    drm/amd/autoconf: fix symbol undefined error
    16:38:15  ERROR: "_kcl_perf_event_update_userpage"
    [drivers/gpu/drm/amd/amdgpu/amdgpu.ko] undefined!
    16:38:15  ERROR: "_kcl_drm_atomic_helper_update_legacy_modeset_state"
    [drivers/gpu/drm/amd/amdgpu/amdgpu.ko] undefined!
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>

--- include/kcl/kcl_perf_event.h
+++ include/kcl/kcl_perf_event.h
@@ -8,7 +8,7 @@ extern void (*_kcl_perf_event_update_userpage)(struct perf_event *event);
 
 static inline void kcl_perf_event_update_userpage(struct perf_event *event)
 {
-#if !defined(HAVE_PERF_EVENT_UPDATE_USERPAGE)
+#if !defined(HAVE_PERF_EVENT_UPDATE_USERPAGE) && defined(BUILD_AS_DKMS)
 	return _kcl_perf_event_update_userpage(event);
 #else
 	return perf_event_update_userpage(event);
