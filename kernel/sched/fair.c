5f3cb425b59a7b0a08e539f0207d6979bceb4ac2
Mel Gorman <mgorman@suse.de>
2019-08-26 15:30:47 +0100

    - sched: Further improve spurious CPU_IDLE active migrations   (bnc# 986383). - Update   patches.suse/cpufreq-ondemand-set-default-up_threshold-to-30-on-multi-core-systems.patch   (bnc#464461,bnc#981838,bnc#1064414,bnc#1144943). - Refresh   patches.suse/mm-Warn-users-of-node-memory-hot-remove-if-the-memory-ratio-is-a-high-risk.patch. - Update   patches.suse/sched-Fix-up-proc-sched_debug-to-print-only-runnable-tasks-again.patch   (none, should be posted upstream). - Update   patches.suse/sched-fair-Enable-SIS_AVG_CPU-by-default.patch   (bsc#1061593,bsc#1148030). - Refresh   patches.suse/sched-topology-Improve-load-balancing-on-AMD-EPYC.patch. - Delete   patches.suse/0001-Revert-mm-vmacache-optimize-overflow-system-wide-flu.patch. - Delete   patches.suse/0001-sched-wake_q-Reduce-reference-counting-for-special-u.patch. - Delete   patches.suse/cpufreq-intel_pstate-Ramp-up-frequency-faster-when-utilisation-reaches-setpoint.patch. - Delete   patches.suse/cpufreq-intel_pstate-Temporarily-boost-P-state-when-exiting-from-idle.patch. - Delete   patches.suse/cpufreq-intel_pstate-Update-setpoint-for-unspecified-ACPI-profile.patch. - Delete   patches.suse/cpufreq-intel_pstate-Use-load-based-policy-by-default-but-allow-PID-to-be-used.patch. - Delete   patches.suse/mm-Adjust-watermark-boost-factor-tunable.patch. - Delete   patches.suse/mm-migrate-Fix-reference-check-race-between-__find_get_block-and-migration.patch. - Delete   patches.suse/mm-page_alloc.c-use-a-single-function-to-free-page.patch.
    suse-commit: 9de058dd5943c563c73aa39d7d799149dcf57f6c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -56,15 +56,15 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_L
 /*
  * Minimal preemption granularity for CPU-bound tasks:
  *
- * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 2 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity			= 750000ULL;
-static unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
+unsigned int sysctl_sched_min_granularity			= 2000000ULL;
+static unsigned int normalized_sysctl_sched_min_granularity	= 2000000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 8;
+static unsigned int sched_nr_latency = 3;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -79,10 +79,10 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  *
- * (default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 2.5 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_wakeup_granularity			= 1000000UL;
-static unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
+unsigned int sysctl_sched_wakeup_granularity			= 2500000UL;
+static unsigned int normalized_sysctl_sched_wakeup_granularity	= 2500000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
 
--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -54,7 +54,7 @@ SCHED_FEAT(TTWU_QUEUE, true)
 /*
  * When doing wakeups, attempt to limit superfluous scans of the LLC domain.
  */
-SCHED_FEAT(SIS_AVG_CPU, false)
+SCHED_FEAT(SIS_AVG_CPU, true)
 SCHED_FEAT(SIS_PROP, true)
 
 /*
