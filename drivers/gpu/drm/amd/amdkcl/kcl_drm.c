1c574179dfe28c8190c03641ae000192008f9180
Chengming Gui <Jack.Gui@amd.com>
2019-08-27 16:17:55 +0800

    drm/amd/autoconf: Test whether fb_info->apertures is available
    fb_info->apertures introduced by kernel v2.6.35-rc1~18^2~65
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_drm.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm.c
@@ -121,7 +121,7 @@ struct fb_info *_kcl_drm_fb_helper_alloc_fbi_stub(struct drm_fb_helper *fb_helpe
 	if (ret)
 		goto err_release;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
+#ifdef HAVE_FB_INFO_APERTURES
 	info->apertures = alloc_apertures(1);
 	if (!info->apertures) {
 		ret = -ENOMEM;
