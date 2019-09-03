3e64de251a1e901bdae1dcb2ae742fb000e6dc04
NeilBrown <neilb@suse.com>
2019-09-03 13:41:20 +1000

    - Refresh   patches.suse/0001-NFS-flush-out-dirty-data-on-file-fput.patch. - Refresh   patches.suse/0001-NFSv4-don-t-let-hanging-mounts-block-other-mounts.patch. - NFS: Allow multiple connections to NFSv4.0 servers   (fate#322786). - Refresh patches.suse/nfs-set-acl-perm.patch. - Delete   patches.suse/0001-NFSv4-handle-EINVAL-from-EXCHANGE_ID-better.patch. - Delete   patches.suse/0001-SUNRPC-Allow-creation-of-RPC-clients-with-multiple-c.patch. - Delete   patches.suse/0001-sunrpc-include-sup-groups-in-hash.patch. - Delete   patches.suse/0002-NFS-Add-a-mount-option-to-specify-number-of-TCP-conn.patch. - Delete   patches.suse/0003-NFSv4-Allow-multiple-connections-to-NFSv4.x-x-0-serv.patch. - Delete   patches.suse/0004-pNFS-Allow-multiple-connections-to-the-DS.patch. - Delete   patches.suse/0005-NFS-Display-the-nconnect-mount-option-if-it-is-set.patch. - Delete   patches.suse/0007-SUNRPC-Mask-XIDs-to-prevent-replay-cache-collision.patch. - Delete patches.suse/NFS-optional-NFSv4_2-fix.patch. - Delete patches.suse/NFS-optional-NFSv4_2.patch. - Delete patches.suse/d-lookup-fairness.fix. - Delete   patches.suse/getcwd-close-race-with-d_move-called-by-lustre.patch. - Delete   patches.suse/sunrpc-cache-handle-missing-listeners-better.patch. - Delete   patches.suse/vfs-use-synchronize_rcu_expedited-in-namespace_unlock.patch.
    suse-commit: 4685373d5b214e85e838177b5f67dc69c775daf1

--- fs/nfs/file.c
+++ fs/nfs/file.c
@@ -82,6 +82,12 @@ nfs_file_release(struct inode *inode, struct file *filp)
 {
 	dprintk("NFS: release(%pD2)\n", filp);
 
+	if (filp->f_mode & FMODE_WRITE)
+		/* Ensure dirty mmapped pages are flushed
+		 * so there will be no dirty pages to
+		 * prevent an unmount from completing.
+		 */
+		vfs_fsync(filp, 0);
 	nfs_inc_stats(inode, NFSIOS_VFSRELEASE);
 	nfs_file_clear_open_context(filp);
 	return 0;
