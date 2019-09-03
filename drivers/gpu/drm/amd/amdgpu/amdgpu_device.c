fe44f62c8a4894aadee273e83292a8b30a409f8c
Chengming Gui <Jack.Gui@amd.com>
2019-09-03 10:46:33 +0800

    drm/amdkcl: Test whether drm_atomic_helper_shutdown() is available
    drm_atomic_helper_shutdown() introduced by kernel v4.12~21^2~24^2~38
    
    Change-Id: Ibb37772095493efff1d65de1ffb95f39c3f2595e
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -3323,8 +3323,10 @@ void amdgpu_device_fini(struct amdgpu_device *adev)
 	if (adev->mode_info.mode_config_initialized){
 		if (!amdgpu_device_has_dc_support(adev))
 			drm_helper_force_disable_all(adev->ddev);
+#ifdef HAVE_DRM_ATOMIC_HELPER_SHUTDOWN
 		else
 			drm_atomic_helper_shutdown(adev->ddev);
+#endif
 	}
 	amdgpu_fence_driver_fini(adev);
 	if (adev->pm_sysfs_en)
