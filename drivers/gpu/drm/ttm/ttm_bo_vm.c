508661198ef64b9b30d9dcb6d493f6454cedf100
Flora Cui <flora.cui@amd.com>
2019-09-05 14:42:02 +0800

    Revert "Test whether __pfn_to_pfn_t() function is available"
    This reverts commit e9ee6338948385368cf1c92f94ecba03c58ef2e4.
    not needed any more
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/ttm/ttm_bo_vm.c
+++ drivers/gpu/drm/ttm/ttm_bo_vm.c
@@ -37,7 +37,10 @@
 #include <drm/ttm/ttm_placement.h>
 #include <drm/drm_vma_manager.h>
 #include <linux/mm.h>
-#if defined(HAVE_PFN_TO_PFN_T)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 5, 0) || \
+	defined(OS_NAME_RHEL_7_3) || \
+	defined(OS_NAME_RHEL_7_X) || \
+	defined(OS_NAME_SLE)
 #include <linux/pfn_t.h>
 #endif
 #include <linux/rbtree.h>
@@ -314,7 +317,10 @@ static vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
 
 		if (vma->vm_flags & VM_MIXEDMAP)
 			ret = vmf_insert_mixed(&cvma, address,
-#if defined(HAVE_PFN_TO_PFN_T)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 5, 0) || \
+	defined(OS_NAME_RHEL_7_3) || \
+	defined(OS_NAME_RHEL_7_X) || \
+	defined(OS_NAME_SLE)
 			__pfn_to_pfn_t(pfn, PFN_DEV | (bo->ssg_can_map ? PFN_MAP : 0)));
 #else
 					pfn);
