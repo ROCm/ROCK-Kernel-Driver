22932f3007858acc502b5d048b71a690226d3116
Adam Yang <Adam1.Yang@amd.com>
2019-09-18 16:50:24 +0800

    drm/amdkcl: fix pr_fmt() macro is redefined warning
    Signed-off-by: Adam Yang <Adam1.Yang@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/vmwgfx/ttm_memory.c
+++ drivers/gpu/drm/vmwgfx/ttm_memory.c
@@ -26,6 +26,9 @@
  *
  **************************************************************************/
 
+#ifdef pr_fmt
+#undef pr_fmt
+#endif /* pr_fmt */
 #define pr_fmt(fmt) "[TTM] " fmt
 
 #include <linux/spinlock.h>
