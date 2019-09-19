a6f6e29571a33d06de59512ad2f57789c5734380
Chengming Gui <Jack.Gui@amd.com>
2019-09-19 13:44:02 +0800

    drm/amdkcl: Test whether drm_fb_helper_fill_info() is available
    Introduced by kernel v5.2-rc1~48^2~37^2~7
    v2: implement drm_fb_helper_fill_info() for kernel don't have it.
    v3: drm/amdkcl: drop symbol check for drm_fb_helper_fill_info
    v4: drm/amdkcl: accommodate to drmP.h removal for drm-fb-helper-fill-info.m4
    v5: move to kcl_drm_fb.h
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_drm_fb.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm_fb.c
@@ -1,4 +1,9 @@
 /* SPDX-License-Identifier: MIT */
+#include <kcl/header/kcl_drm_device_h.h>
+#include <kcl/header/kcl_drm_drv_h.h>
+#include <drm/drm_crtc.h>
+#include <drm/drm_crtc_helper.h>
+#include <drm/drm_fourcc.h>
 #include <kcl/kcl_drm_fb.h>
 #include "kcl_common.h"
 
@@ -28,4 +33,27 @@ int remove_conflicting_pci_framebuffers(struct pci_dev *pdev, int res_id, const 
 	return err;
 }
 EXPORT_SYMBOL(remove_conflicting_pci_framebuffers);
+#endif
+
+#ifndef HAVE_DRM_FB_HELPER_FILL_INFO
+void drm_fb_helper_fill_info(struct fb_info *info,
+			     struct drm_fb_helper *fb_helper,
+			     struct drm_fb_helper_surface_size *sizes)
+{
+	struct drm_framebuffer *fb = fb_helper->fb;
+
+#ifdef HAVE_DRM_FRAMEBUFFER_FORMAT
+	drm_fb_helper_fill_fix(info, fb->pitches[0], fb->format->depth);
+#else
+	drm_fb_helper_fill_fix(info, fb->pitches[0], fb->depth);
+#endif
+	drm_fb_helper_fill_var(info, fb_helper,
+			       sizes->fb_width, sizes->fb_height);
+
+	info->par = fb_helper;
+	snprintf(info->fix.id, sizeof(info->fix.id), "%sdrmfb",
+		 fb_helper->dev->driver->name);
+
+}
+EXPORT_SYMBOL(drm_fb_helper_fill_info);
 #endif
