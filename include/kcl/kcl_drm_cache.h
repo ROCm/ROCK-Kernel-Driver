3460df2b95759b7efcb5611ae31eac7f025cbfef
Adam Yang <Adam1.Yang@amd.com>
2019-09-23 16:44:20 +0800

    drm/amdkcl: Test whether drm_need_swiotlb() is available
    Signed-off-by: Adam Yang <Adam1.Yang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Change-Id: I14e2af5f5522e3ab8398f137f9d4af1cb144498f

---
+++ include/kcl/kcl_drm_cache.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef AMDKCL_DRM_CACHE_H
+#define AMDKCL_DRM_CACHE_H
+#include <linux/types.h>
+#include <drm/drm_cache.h>
+
+#if !defined(HAVE_DRM_NEED_SWIOTLB)
+bool drm_need_swiotlb(int dma_bits);
+#endif /* HAVE_DRM_NEED_SWIOTLB */
+
+#endif /* AMDKCL_DRM_CACHE_H */
