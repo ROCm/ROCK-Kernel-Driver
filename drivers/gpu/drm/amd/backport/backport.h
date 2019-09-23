a9b9cb1406aec90560fa732287c33c76bcfd2b24
Adam Yang <Adam1.Yang@amd.com>
2019-09-23 19:59:17 +0800

    drm/amdkcl: Test whether mmu_notifier_range_blockable() is available
    v2: drm/amdkcl: refactor kcl copy of mmu_notifier_range_blockable
    v3: drm/amdkcl: refactor kcl copy of mmu_notifier_range_blockable
    
    Signed-off-by: Adam Yang <Adam1.Yang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -30,6 +30,7 @@
 #include <kcl/backport/kcl_perf_event_backport.h>
 #include <kcl/kcl_ptrace.h>
 #include <kcl/kcl_seq_file.h>
+#include <kcl/kcl_mmu_notifier.h>
 #include <kcl/kcl_preempt.h>
 #include <kcl/kcl_suspend.h>
 #include <kcl/backport/kcl_pci_backport.h>
