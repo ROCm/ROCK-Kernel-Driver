3c7a2b2e2e6673fcb2303288f28f8af84f3332f0
Flora Cui <flora.cui@amd.com>
2019-09-24 13:30:02 +0800

    drm/amdkcl: Test whether drm_mode_is_420_xxx() is available
    v2: drm/amdkcl: refactor kcl copy rm_mode_is_420_xxx
    
    Change-Id: I3b71e9df855e8996d76a040ac6b650fdb52702fa
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei Xu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- include/kcl/kcl_drm_modes.h
+++ include/kcl/kcl_drm_modes.h
@@ -7,4 +7,12 @@
 #ifndef HAVE_DRM_MODE_GET_HV_TIMING
 #define drm_mode_get_hv_timing drm_crtc_get_hv_timing
 #endif
+
+#ifndef HAVE_DRM_MODE_IS_420_XXX
+bool drm_mode_is_420_only(const struct drm_display_info *display,
+		const struct drm_display_mode *mode);
+bool drm_mode_is_420_also(const struct drm_display_info *display,
+		const struct drm_display_mode *mode);
+#endif
+
 #endif
