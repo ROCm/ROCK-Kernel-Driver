fd272cb7586fbf7c6599bfe12b9222b2f0567a89
Arnd Bergmann <arnd@arndb.de>
2019-10-02 14:01:27 +0200

    drm/amdgpu: work around llvm bug #42576
    Code in the amdgpu driver triggers a bug when using clang to build
    an arm64 kernel:
    
    /tmp/sdma_v4_0-f95fd3.s: Assembler messages:
    /tmp/sdma_v4_0-f95fd3.s:44: Error: selected processor does not support `bfc w0,#1,#5'
    
    I expect this to be fixed in llvm soon, but we can also work around
    it by inserting a barrier() that prevents the optimization.
    
    Link: https://bugs.llvm.org/show_bug.cgi?id=42576
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/sdma_v4_0.c
+++ drivers/gpu/drm/amd/amdgpu/sdma_v4_0.c
@@ -1150,6 +1150,7 @@ static uint32_t sdma_v4_0_rb_cntl(struct amdgpu_ring *ring, uint32_t rb_cntl)
 	/* Set ring buffer size in dwords */
 	uint32_t rb_bufsz = order_base_2(ring->ring_size / 4);
 
+	barrier(); /* work around https://bugs.llvm.org/show_bug.cgi?id=42576 */
 	rb_cntl = REG_SET_FIELD(rb_cntl, SDMA0_GFX_RB_CNTL, RB_SIZE, rb_bufsz);
 #ifdef __BIG_ENDIAN
 	rb_cntl = REG_SET_FIELD(rb_cntl, SDMA0_GFX_RB_CNTL, RB_SWAP_ENABLE, 1);
