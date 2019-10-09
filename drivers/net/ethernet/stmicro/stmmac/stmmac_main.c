f37e3b496cdaddc050e6cb5d5de643e3b2411cdf
Jiri Slaby <jslaby@suse.cz>
2019-10-09 10:33:59 +0200

    net: stmmac: Avoid deadlock on suspend/resume (git-fixes).
    suse-commit: 9c4865d0c374ac90d3298eac44247c7c7baca0ed

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -4453,11 +4453,11 @@ int stmmac_suspend(struct device *dev)
 	if (!ndev || !netif_running(ndev))
 		return 0;
 
-	mutex_lock(&priv->lock);
-
 	rtnl_lock();
 	phylink_stop(priv->phylink);
 	rtnl_unlock();
+
+	mutex_lock(&priv->lock);
 
 	netif_device_detach(ndev);
 	stmmac_stop_all_queues(priv);
@@ -4562,11 +4562,11 @@ int stmmac_resume(struct device *dev)
 
 	stmmac_start_all_queues(priv);
 
+	mutex_unlock(&priv->lock);
+
 	rtnl_lock();
 	phylink_start(priv->phylink);
 	rtnl_unlock();
-
-	mutex_unlock(&priv->lock);
 
 	return 0;
 }
