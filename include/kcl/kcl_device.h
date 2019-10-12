554633dd2b300dc893c5dc7b5323056b1b4ba3e3
Flora Cui <flora.cui@amd.com>
2019-10-12 10:34:57 +0800

    drm/amdkcl: check whether dev_err_ratelimited() is available
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- include/kcl/kcl_device.h
+++ include/kcl/kcl_device.h
@@ -1,6 +1,8 @@
 #ifndef AMDKCL_DEVICE_H
 #define AMDKCL_DEVICE_H
 
+#include <linux/ratelimit.h>
+
 #if !defined(HAVE_KOBJ_TO_DEV)
 static inline struct device *kobj_to_dev(struct kobject *kobj)
 {
@@ -29,5 +31,19 @@ do {									\
 
 #define dev_err_once(dev, fmt, ...)					\
 	dev_level_once(dev_err, dev, fmt, ##__VA_ARGS__)
+#endif
+
+#if !defined(dev_err_ratelimited)
+#define dev_level_ratelimited(dev_level, dev, fmt, ...)			\
+do {									\
+	static DEFINE_RATELIMIT_STATE(_rs,				\
+				      DEFAULT_RATELIMIT_INTERVAL,	\
+				      DEFAULT_RATELIMIT_BURST);		\
+	if (__ratelimit(&_rs))						\
+		dev_level(dev, fmt, ##__VA_ARGS__);			\
+} while (0)
+
+#define dev_err_ratelimited(dev, fmt, ...)				\
+	dev_level_ratelimited(dev_err, dev, fmt, ##__VA_ARGS__)
 #endif
 #endif /* AMDKCL_DEVICE_H */
