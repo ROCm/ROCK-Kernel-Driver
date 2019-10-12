80615d46aa087e9d3c2f4094e8532c359f7db87c
Flora Cui <flora.cui@amd.com>
2019-10-12 10:26:05 +0800

    drm/amdkcl: add dev_err_once()
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- include/kcl/kcl_device.h
+++ include/kcl/kcl_device.h
@@ -8,4 +8,26 @@ static inline struct device *kobj_to_dev(struct kobject *kobj)
 }
 #endif
 
+#if !defined(dev_err_once)
+#ifdef CONFIG_PRINTK
+#define dev_level_once(dev_level, dev, fmt, ...)			\
+do {									\
+	static bool __print_once __read_mostly;				\
+									\
+	if (!__print_once) {						\
+		__print_once = true;					\
+		dev_level(dev, fmt, ##__VA_ARGS__);			\
+	}								\
+} while (0)
+#else
+#define dev_level_once(dev_level, dev, fmt, ...)			\
+do {									\
+	if (0)								\
+		dev_level(dev, fmt, ##__VA_ARGS__);			\
+} while (0)
+#endif
+
+#define dev_err_once(dev, fmt, ...)					\
+	dev_level_once(dev_err, dev, fmt, ##__VA_ARGS__)
+#endif
 #endif /* AMDKCL_DEVICE_H */
