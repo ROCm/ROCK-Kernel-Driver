4ad76f6504e3da09dfca6961c6b56841637d6a52
Flora Cui <flora.cui@amd.com>
2019-10-22 14:16:22 +0800

    drm/amdkcl: fix drm_add_edid_modes & drm_edid_to_eld
    drm_edid_to_eld() is moved to drm_add_edid_modes() in commit
    v4.14-rc3-592-gc945b8c14bb7
    and is set to static in commit v4.14-rc3-594-g79436a1c9bcc
    
    HAVE_DRM_EDID_TO_ELD check could help to avoid duplicated
    drm_edid_to_eld() in most cases.
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Jiansong Chen <Jiansong.Chen@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    Change-Id: Id7c442179d03f891274ecfa549ea26c16e68fa4e

--- include/kcl/backport/kcl_drm_encoder.h
+++ include/kcl/backport/kcl_drm_encoder.h
@@ -35,9 +35,26 @@
 #define KCL_BACKPORT_KCL_DRM_ENCODER_H
 
 #include <drm/drm_encoder.h>
+#include <drm/drm_edid.h>
 
 #if !defined(HAVE_DRM_ENCODER_FIND_VALID_WITH_FILE)
 #define drm_encoder_find(dev, file, id) drm_encoder_find(dev, id)
+#endif
+
+#if defined(HAVE_DRM_EDID_TO_ELD)
+static inline
+int _kcl_drm_add_edid_modes(struct drm_connector *connector, struct edid *edid)
+{
+	int ret;
+
+	ret = drm_add_edid_modes(connector, edid);
+
+	if (drm_edid_is_valid(edid))
+		drm_edid_to_eld(connector, edid);
+
+	return ret;
+}
+#define drm_add_edid_modes _kcl_drm_add_edid_modes
 #endif
 
 #endif
