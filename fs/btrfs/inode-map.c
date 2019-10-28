b2a0fe7040a5f41c50742fce5c067d07543820cd
Qu Wenruo <wqu@suse.com>
2019-10-28 17:30:54 +0800

    btrfs: qgroup: Always free PREALLOC META reserve in btrfs_delalloc_release_extents() (bsc#1155179).
    suse-commit: 6d163676a11bc52bd5d18c7dc1018ad059578e62

--- fs/btrfs/inode-map.c
+++ fs/btrfs/inode-map.c
@@ -484,12 +484,12 @@ again:
 	ret = btrfs_prealloc_file_range_trans(inode, trans, 0, 0, prealloc,
 					      prealloc, prealloc, &alloc_hint);
 	if (ret) {
-		btrfs_delalloc_release_extents(BTRFS_I(inode), prealloc, true);
+		btrfs_delalloc_release_extents(BTRFS_I(inode), prealloc);
 		goto out_put;
 	}
 
 	ret = btrfs_write_out_ino_cache(root, trans, path, inode);
-	btrfs_delalloc_release_extents(BTRFS_I(inode), prealloc, false);
+	btrfs_delalloc_release_extents(BTRFS_I(inode), prealloc);
 out_put:
 	iput(inode);
 out_release:
