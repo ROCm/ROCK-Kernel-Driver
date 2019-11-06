7535d0d35a9c779ded227261d1c4806f5120feb5
Flora Cui <flora.cui@amd.com>
2019-11-06 13:03:11 +0800

    drm/amdkcl: add AMDKCL_AMDGPU_DMABUF_OPS
    v2:missing gem_prime_pin pointer
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Jack Gui <Jack Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -1676,7 +1676,13 @@ static const struct drm_driver amdgpu_kms_driver = {
 
 	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
 	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
+#if defined(AMDKCL_AMDGPU_DMABUF_OPS)
 	.gem_prime_import = amdgpu_gem_prime_import,
+#else
+	.gem_prime_import = drm_gem_prime_import,
+	.gem_prime_pin = amdgpu_gem_prime_pin,
+	.gem_prime_unpin = amdgpu_gem_prime_unpin,
+#endif
 #ifdef HAVE_DRM_DRIVER_GEM_PRIME_RES_OBJ
 	.gem_prime_res_obj = amdgpu_gem_prime_res_obj,
 #endif
