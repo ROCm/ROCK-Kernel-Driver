6f78b7d546baba2618d491dd1e0888194af83591
Flora Cui <flora.cui@amd.com>
2019-11-06 11:37:29 +0800

    drm/amdkcl: add AMDKCL_AMDGPU_DEBUGFS_CLEANUP
    for debugfs_cleanup. macro AMDKCL_AMDGPU_DEBUGFS_CLEANUP would be more
    friendly for hybrid branch maintaining.
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    Change-Id: I006ec85bf1e697d4fb5173023f4f4ee9e81bf286

--- include/kcl/backport/kcl_drm_backport.h
+++ include/kcl/backport/kcl_drm_backport.h
@@ -2,6 +2,14 @@
 #ifndef AMDKCL_DRM_BACKPORT_H
 #define AMDKCL_DRM_BACKPORT_H
 
+/*
+ * commit v4.10-rc3-539-g086f2e5cde74
+ * drm: debugfs: Remove all files automatically on cleanup
+ */
+#if DRM_VERSION_CODE < DRM_VERSION(4, 11, 0)
+#define AMDKCL_AMDGPU_DEBUGFS_CLEANUP
+#endif
+
 #if DRM_VERSION_CODE >= DRM_VERSION(4, 17, 0)
 #define AMDKCL_AMDGPU_DMABUF_OPS
 #endif
