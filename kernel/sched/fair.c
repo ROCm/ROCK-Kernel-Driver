3a99e82ce953e23346bcf28356826d725fcc2f08
Mel Gorman <mgorman@suse.de>
2019-11-06 16:44:56 +0000

    sched: Rework pick_next_task() slow-path (bnc#1155798 (CPU scheduler functional and performance backports)).
    suse-commit: d16b90370664186d25a699217a720a3ede303d84

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6783,7 +6783,7 @@ again:
 		goto idle;
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	if (prev->sched_class != &fair_sched_class)
+	if (!prev || prev->sched_class != &fair_sched_class)
 		goto simple;
 
 	/*
@@ -6860,8 +6860,8 @@ again:
 	goto done;
 simple:
 #endif
-
-	put_prev_task(rq, prev);
+	if (prev)
+		put_prev_task(rq, prev);
 
 	do {
 		se = pick_next_entity(cfs_rq, NULL);
@@ -6889,6 +6889,9 @@ done: __maybe_unused;
 	return p;
 
 idle:
+	if (!rf)
+		return NULL;
+
 	new_tasks = newidle_balance(rq, rf);
 
 	/*
