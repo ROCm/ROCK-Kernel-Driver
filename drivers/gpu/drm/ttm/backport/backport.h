d414c435099c193300787429073125278f50635c
Flora Cui <flora.cui@amd.com>
2019-11-07 10:37:13 +0800

    drm/amdkcl: adapt drm_vma_node_verify_access() prototype change
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Jiansong Chen <Jiansong.Chen@amd.com>
    
    69a65a9c531c drm/amdkcl: fix license for kcl drm part
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/ttm/backport/backport.h
+++ drivers/gpu/drm/ttm/backport/backport.h
@@ -7,5 +7,6 @@
 #include <kcl/kcl_dma_mapping.h>
 #include <kcl/kcl_list.h>
 #include <kcl/backport/kcl_fence_backport.h>
+#include <kcl/backport/kcl_drm_vma_manager_backport.h>
 #include <kcl/backport/kcl_drm_backport.h>
 #endif
