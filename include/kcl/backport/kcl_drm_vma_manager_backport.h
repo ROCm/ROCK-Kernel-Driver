051d0da0f34171ca8d034d984565d59bc1ac416c
Flora Cui <flora.cui@amd.com>
2019-11-07 10:37:13 +0800

    drm/amdkcl: increase drm vma offset size limit
    v2: increase drm vma offset size limit
    For 8 processes share 256GB system memory application case, 1T Bytes drm
    vma offset limit is not big enough. Increase the limit to 16TB, for max
    44bits address space because the upper bit is encoded with gpu_id.
    
    Remove the 64GB size condition check to handle all kernels, 64GB is
    Ubuntu kernel default setting, CentOS, Redhat kernel default is 1TB.
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Jiansong Chen <Jiansong.Chen@amd.com>
    
    Signed-off-by: Philip Yang <Philip.Yang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/backport/kcl_drm_vma_manager_backport.h
+++ include/kcl/backport/kcl_drm_vma_manager_backport.h
@@ -22,8 +22,38 @@
 #ifndef AMDKCL_DRM_VMA_MANAGER_H
 #define AMDKCL_DRM_VMA_MANAGER_H
 
+/* We make up offsets for buffer objects so we can recognize them at
+ * mmap time. pgoff in mmap is an unsigned long, so we need to make sure
+ * that the faked up offset will fit
+ */
 #include <drm/drm_vma_manager.h>
 #include <drm/drm_file.h>
+
+#if (BITS_PER_LONG == 64)
+#ifdef DRM_FILE_PAGE_OFFSET_START
+#undef DRM_FILE_PAGE_OFFSET_START
+#endif
+#ifdef DRM_FILE_PAGE_OFFSET_SIZE
+#undef DRM_FILE_PAGE_OFFSET_SIZE
+#endif
+
+#define DRM_FILE_PAGE_OFFSET_START ((0xFFFFFFFFULL >> PAGE_SHIFT) + 1)
+#define DRM_FILE_PAGE_OFFSET_SIZE ((0xFFFFFFFFULL >> PAGE_SHIFT) * 4096)
+
+static inline void
+kcl_drm_vma_offset_manager_init(struct drm_vma_offset_manager *mgr)
+{
+	drm_vma_offset_manager_destroy(mgr);
+	drm_vma_offset_manager_init(mgr,
+		DRM_FILE_PAGE_OFFSET_START,
+		DRM_FILE_PAGE_OFFSET_SIZE);
+}
+#else
+static inline void
+kcl_drm_vma_offset_manager_init(struct drm_vma_offset_manager *mgr)
+{
+}
+#endif
 
 #ifndef HAVE_DRM_VMA_NODE_VERIFY_ACCESS_HAS_DRM_FILE
 static inline int _kcl_drm_vma_node_verify_access(struct drm_vma_offset_node *node,
