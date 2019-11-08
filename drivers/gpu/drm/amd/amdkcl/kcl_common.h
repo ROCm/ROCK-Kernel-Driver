df11f27e05c49a9e32efcf09ba368e17860e775f
Flora Cui <flora.cui@amd.com>
2019-11-08 12:14:03 +0800

    drm/amdkcl: add lookpup unexported symbol support
    Rebase: cherry-picked from cd47554bb08a
    
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/kcl_common.h
@@ -0,0 +1,42 @@
+#ifndef AMDKCL_COMMON_H
+#define AMDKCL_COMMON_H
+
+/*
+ * kallsyms_lookup_name has been exported in version 2.6.33
+ */
+#include <linux/version.h>
+#include <linux/kallsyms.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+extern unsigned long (*_kcl_kallsyms_lookup_name)(const char *name);
+#endif
+static inline unsigned long kcl_kallsyms_lookup_name(const char *name)
+{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+	return _kcl_kallsyms_lookup_name(name);
+#else
+	return kallsyms_lookup_name(name);
+#endif
+}
+
+static inline void *amdkcl_fp_setup(const char *symbol, void *fp_stup)
+{
+	unsigned long addr;
+	void *fp = NULL;
+
+	addr = kcl_kallsyms_lookup_name(symbol);
+	if (addr == 0) {
+		fp = fp_stup;
+		if (fp != NULL)
+			printk_once(KERN_WARNING "Warning: fail to get symbol %s, replace it with kcl stub\n", symbol);
+		else {
+			printk_once(KERN_ERR "Error: fail to get symbol %s\n", symbol);
+			BUG();
+		}
+	} else {
+		fp = (void *)addr;
+	}
+
+	return fp;
+}
+#endif
