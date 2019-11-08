079a1dd78a428159d3f05578ffbf66874c225a3c
Junwei Zhang <Jerry.Zhang@amd.com>
2019-11-08 12:13:34 +0800

    drm/amdkcl: add amdkcl support
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/main.c
@@ -0,0 +1,19 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+int __init amdkcl_init(void)
+{
+	return 0;
+}
+module_init(amdkcl_init);
+
+void __exit amdkcl_exit(void)
+{
+}
+
+module_exit(amdkcl_exit);
+
+MODULE_AUTHOR("AMD linux driver team");
+MODULE_DESCRIPTION("Module for OS kernel compatible layer");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("1.0");
