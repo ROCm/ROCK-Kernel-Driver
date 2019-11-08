94657783d950741a53964154d7847a14903236b0
Junwei Zhang <Jerry.Zhang@amd.com>
2019-11-08 12:14:03 +0800

    drm/amdkcl: add lookpup unexported symbol support
    v2: test kallsyms_lookup_name() is exported.
    v3: fix awk syntax to find kallsyms_lookup_name() addr.
    v4: use kprobe to find out kallsyms_lookup_name()
    
    Signed-off-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Qiang Yu <Qiang.Yu@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_common.h
+++ drivers/gpu/drm/amd/amdkcl/kcl_common.h
@@ -2,9 +2,14 @@
 #ifndef AMDKCL_COMMON_H
 #define AMDKCL_COMMON_H
 
+#include <linux/kernel.h>
+#include <linux/version.h>
+
 #ifdef pr_fmt
 #undef pr_fmt
 #endif
 #define pr_fmt(fmt) "amdkcl: " fmt
+
+void *amdkcl_fp_setup(const char *symbol, void *dummy);
 
 #endif
