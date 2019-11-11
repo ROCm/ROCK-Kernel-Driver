67c9f8982314355c2e72c746614e0477eb36da1f
Hannes Reinecke <hare@suse.de>
2019-11-11 12:47:59 +0100

    block: add helper for checking if queue is registered (bsc#1156419,jsc#SLE-8281).
    suse-commit: 7041d4410c5a13fafe981bac6465a987b35c6b65

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -945,7 +945,7 @@ int blk_register_queue(struct gendisk *disk)
 	if (WARN_ON(!q))
 		return -ENXIO;
 
-	WARN_ONCE(test_bit(QUEUE_FLAG_REGISTERED, &q->queue_flags),
+	WARN_ONCE(blk_queue_registered(q),
 		  "%s is registering an already registered queue\n",
 		  kobject_name(&dev->kobj));
 	blk_queue_flag_set(QUEUE_FLAG_REGISTERED, q);
@@ -1029,7 +1029,7 @@ void blk_unregister_queue(struct gendisk *disk)
 		return;
 
 	/* Return early if disk->queue was never registered. */
-	if (!test_bit(QUEUE_FLAG_REGISTERED, &q->queue_flags))
+	if (!blk_queue_registered(q))
 		return;
 
 	/*
