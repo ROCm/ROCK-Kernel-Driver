2b49a89fe9b6e7c8fdc06210843ad9b471afff70
Jiri Slaby <jslaby@suse.cz>
2019-11-13 10:51:45 +0100

    drm/i915/gen8+: Add RC6 CTX corruption WA (bnc#1151927 5.3.11).
    suse-commit: 7e4f031c3a14d13ba762c1df8b9e33f84d916b7b

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -2157,6 +2157,8 @@ static int i915_drm_suspend_late(struct drm_device *dev, bool hibernation)
 
 	i915_gem_suspend_late(dev_priv);
 
+	intel_rc6_ctx_wa_suspend(dev_priv);
+
 	intel_uncore_suspend(&dev_priv->uncore);
 
 	intel_power_domains_suspend(dev_priv,
@@ -2372,6 +2374,8 @@ static int i915_drm_resume_early(struct drm_device *dev)
 	intel_uncore_sanitize(dev_priv);
 
 	intel_power_domains_resume(dev_priv);
+
+	intel_rc6_ctx_wa_resume(dev_priv);
 
 	intel_gt_sanitize(dev_priv, true);
 
