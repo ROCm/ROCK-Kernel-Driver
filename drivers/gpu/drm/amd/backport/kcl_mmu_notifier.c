dfca3981b8c151b66a903f19fcd2df46d7b9221a
Flora Cui <flora.cui@amd.com>
2019-11-20 15:44:15 +0800

    drm/amdkcl: add kcl copy of mmu_notifier_unregister_no_release()
    Signed-off-by: Flora Cui <flora.cui@amd.com>

---
+++ drivers/gpu/drm/amd/backport/kcl_mmu_notifier.c
@@ -0,0 +1,18 @@
+#include <linux/sched/mm.h>
+
+#if !defined(HAVE_MMU_NOTIFIER_CALL_SRCU)
+void mmu_notifier_unregister_no_release(struct mmu_notifier *mn,
+					struct mm_struct *mm)
+{
+	spin_lock(&mm->mmu_notifier_mm->lock);
+	/*
+	 * Can not use list_del_rcu() since __mmu_notifier_release
+	 * can delete it before we hold the lock.
+	 */
+	hlist_del_init_rcu(&mn->hlist);
+	spin_unlock(&mm->mmu_notifier_mm->lock);
+
+	BUG_ON(atomic_read(&mm->mm_count) <= 0);
+	mmdrop(mm);
+}
+#endif
