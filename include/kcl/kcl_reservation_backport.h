b63618c647925cf64c3bfff1ade4a6f271f2794d
Yifan Zhang <yifan1.zhang@amd.com>
2019-11-21 17:28:41 +0800

    drm/amdkcl: check whether smp_store_mb is available
    Change-Id: I93b7fba7ecbb9ff092775d752cef3e1fcf906c3b
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amd/autoconf: add barrier.h in kcl_reservation_backport.h
    Change-Id: I55df29f1a881dd81f0b338287396a627bdf0c912
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>

--- include/kcl/kcl_reservation_backport.h
+++ include/kcl/kcl_reservation_backport.h
@@ -2,8 +2,14 @@
 #define AMDKCL_RESERVATION_BACKPORT_H
 
 #include <kcl/kcl_reservation.h>
+#include <asm/barrier.h>
 
 #define reservation_ww_class (*_kcl_reservation_ww_class)
 #define reservation_seqcount_class (*_kcl_reservation_seqcount_class)
 #define reservation_seqcount_string (_kcl_reservation_seqcount_string)
+
+#if !defined(smp_store_mb)
+#define smp_store_mb set_mb
+#endif
+
 #endif
