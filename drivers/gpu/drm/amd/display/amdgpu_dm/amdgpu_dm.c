f6e9fdedf006a9af3bb4ae0f29a336f2d466ab5a
Roman Li <roman.li@amd.com>
2019-11-22 10:58:10 -0500

    drm/amd/display: Default max bpc to 16 for eDP
    [Why]
    Some 10bit eDP panels don't lightup after we cap bpc to 8.
    
    [How]
    Set default max_bpc to 16 for edp connector type.
    
    Change-Id: I3837da1389a39e159600418f2027a7b5b9e7a40e
    Signed-off-by: Roman Li <roman.li@amd.com>
    Reviewed-by: Nicholas Kazlauskas <Nicholas.Kazlauskas@amd.com>
    Acked-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6340,9 +6340,9 @@ void amdgpu_dm_connector_init_helper(struct amdgpu_display_manager *dm,
 #if DRM_VERSION_CODE >= DRM_VERSION(5, 0, 0)
 	drm_connector_attach_max_bpc_property(&aconnector->base, 8, 16);
 
-	/* This defaults to the max in the range, but we want 8bpc. */
-	aconnector->base.state->max_bpc = 8;
-	aconnector->base.state->max_requested_bpc = 8;
+	/* This defaults to the max in the range, but we want 8bpc for non-edp. */
+	aconnector->base.state->max_bpc = (connector_type == DRM_MODE_CONNECTOR_eDP) ? 16 : 8;
+	aconnector->base.state->max_requested_bpc = aconnector->base.state->max_bpc;
 #else
 	drm_object_attach_property(&aconnector->base.base,
 				adev->mode_info.max_bpc_property,
