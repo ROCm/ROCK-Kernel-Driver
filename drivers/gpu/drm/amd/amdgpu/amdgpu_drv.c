0c966afe637aa7e9e81ba729cd5a3d1eaaf7652f
Rui Teng <rui.teng@amd.com>
2019-11-26 19:29:27 +0800

    Bump AMDGPU version to 5.2.4
    Change-Id: I4b5498637b70c0da717e7fa6f094672c59dd0af3
    Signed-off-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -91,7 +91,7 @@
 #define KMS_DRIVER_MINOR	36
 #define KMS_DRIVER_PATCHLEVEL	0
 
-#define AMDGPU_VERSION		"5.2.3"
+#define AMDGPU_VERSION		"5.2.4"
 
 int amdgpu_vram_limit = 0;
 int amdgpu_vis_vram_limit = 0;
