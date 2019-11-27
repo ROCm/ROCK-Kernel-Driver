9a8cd424fbec346ebfb38a9c31c8b2543fafb63c
Chengming Gui <Jack.Gui@amd.com>
2019-11-27 11:10:30 +0800

    drm/amdkcl: [4.12] fix drm_atomic_plane_disabling in dm_update_plane_state
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -8922,7 +8922,11 @@ static int dm_update_plane_state(struct dc *dc,
 	} else { /* Add new planes */
 		struct dc_plane_state *dc_new_plane_state;
 
+#if DRM_VERSION_CODE < DRM_VERSION(4, 12, 0)
+		if (drm_atomic_plane_disabling(plane, old_plane_state))
+#else
 		if (drm_atomic_plane_disabling(plane->state, new_plane_state))
+#endif
 			return 0;
 
 		if (!new_plane_crtc)
