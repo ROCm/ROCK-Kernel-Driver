7a595f957b131a2062ef1fb2ad89650ac8e19c87
Chengming Gui <Jack.Gui@amd.com>
2019-11-28 20:54:34 +0800

    drm/amdkcl: [5.3] Test whether drm_probe_help.h is available
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/kcl_drm_probe_helper_h.h
@@ -0,0 +1,9 @@
+#ifndef AMDKCL_DRM_PROBE_HELPER_H_H
+#define AMDKCL_DRM_PROBE_HELPER_H_H
+
+#ifdef HAVE_DRM_PROBE_HELPER_H
+#include <drm/drm_probe_helper.h>
+#else
+#include <drm/drm_crtc_helper.h>
+#endif
+#endif/*AMDKCL_DRM_PROBE_HELPER_H_H*/
