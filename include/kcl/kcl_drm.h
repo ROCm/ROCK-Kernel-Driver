8b6d974e1420227044b2a168d3c48a32d570486f
Chengming Gui <Jack.Gui@amd.com>
2019-11-29 09:36:13 +0800

    drm/amdkcl: fix missing header file needed by kcl_drm.h
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- include/kcl/kcl_drm.h
+++ include/kcl/kcl_drm.h
@@ -1,6 +1,16 @@
 #ifndef AMDKCL_DRM_H
 #define AMDKCL_DRM_H
 
+#include <linux/kconfig.h>
+#include <linux/ctype.h>
+#include <linux/console.h>
+
+#include <drm/drm_fb_helper.h>
+#include <drm/drm_atomic.h>
+#include <drm/drm_gem.h>
+#include <drm/drm_fourcc.h>
+#include <drm/drm_rect.h>
+#include <drm/drm_atomic_helper.h>
 #include <drm/drm_dp_helper.h>
 #include <drm/drm_modes.h>
 #if defined(HAVE_DRM_PRINTER)
