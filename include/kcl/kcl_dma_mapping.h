6f86d3d7c74c1c7ab3cc46db6fb4e936c621caa9
Flora Cui <flora.cui@amd.com>
2019-12-05 16:10:38 +0800

    drm/amdkcl: [5.4] fix dma_addressing_limited
    introduced by kernel v5.3-rc1-57-g06532750010e
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>

--- include/kcl/kcl_dma_mapping.h
+++ include/kcl/kcl_dma_mapping.h
@@ -2,6 +2,7 @@
 #define AMDKCL_DMA_MAPPING_H
 
 #include <linux/dma-mapping.h>
+#include <linux/version.h>
 
 /*
  * commit v4.8-11962-ga9a62c938441
@@ -9,5 +10,18 @@
  */
 #ifndef DMA_ATTR_NO_WARN
 #define DMA_ATTR_NO_WARN (0UL)
+#endif
+
+/*
+* commit v5.3-rc1-57-g06532750010e
+* dma-mapping: use dma_get_mask in dma_addressing_limited
+ */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 0)
+static inline bool _kcl_dma_addressing_limited(struct device *dev)
+{
+	return min_not_zero(dma_get_mask(dev), dev->bus_dma_mask) <
+			dma_get_required_mask(dev);
+}
+#define dma_addressing_limited _kcl_dma_addressing_limited
 #endif
 #endif
