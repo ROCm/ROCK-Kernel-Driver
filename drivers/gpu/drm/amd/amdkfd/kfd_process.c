c76baf0ca1f79f3862e1ed8c9f9fafa047be3f0e
Flora Cui <flora.cui@amd.com>
2019-12-06 22:15:18 +0800

    drm/amdkcl: Test whether mmu_notifier_put is available
    Change-Id: I6a1119a60093799f5560d72ea00b8bf44bc2b773
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack.Gui <Jack.Gui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_process.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -648,7 +648,7 @@ static void kfd_process_ref_release(struct kref *ref)
 	queue_work(kfd_process_wq, &p->release_work);
 }
 
-#ifdef HAVE_MMU_NOTIFIER_SYNCHRONIZE
+#ifdef HAVE_MMU_NOTIFIER_PUT
 static void kfd_process_free_notifier(struct mmu_notifier *mn)
 {
 	kfd_unref_process(container_of(mn, struct kfd_process, mmu_notifier));
@@ -739,7 +739,7 @@ static void kfd_process_notifier_release(struct mmu_notifier *mn,
 
 	mutex_unlock(&p->mutex);
 
-#ifdef HAVE_MMU_NOTIFIER_SYNCHRONIZE
+#ifdef HAVE_MMU_NOTIFIER_PUT
 	mmu_notifier_put(&p->mmu_notifier);
 #else
 	mmu_notifier_unregister_no_release(&p->mmu_notifier, mm);
@@ -749,7 +749,7 @@ static void kfd_process_notifier_release(struct mmu_notifier *mn,
 
 static const struct mmu_notifier_ops kfd_process_mmu_notifier_ops = {
 	.release = kfd_process_notifier_release,
-#ifdef HAVE_MMU_NOTIFIER_SYNCHRONIZE
+#ifdef HAVE_MMU_NOTIFIER_PUT
 	.free_notifier = kfd_process_free_notifier,
 #endif
 };
