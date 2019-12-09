83b9fd380761730b31f75c3fc497c0d1fc7f2d98
Chengming Gui <Jack.Gui@amd.com>
2019-12-09 18:16:29 +0800

    drm/amdkcl: add missing file header for amd/backport/backport.h
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -38,4 +38,11 @@
 #include <kcl/kcl_types.h>
 #include <kcl/kcl_amdgpu.h>
 #include <kcl/kcl_drm_dp_mst_helper_backport.h>
+#include <kcl/kcl_overflow.h>
+#include <kcl/kcl_seq_file.h>
+#include <kcl/kcl_ptrace.h>
+#include <kcl/kcl_workqueue.h>
+#include <kcl/kcl_preempt.h>
+#include <kcl/kcl_video.h>
+#include <kcl/kcl_idr.h>
 #endif /* AMDGPU_BACKPORT_H */
