6307602613ffa178981d7437261da2277d265dca
Takashi Iwai <tiwai@suse.de>
2019-12-17 14:48:03 +0100

    drm/i915/tgl: Add mipi dsi support for TGL (jsc#SLE-7953).
    suse-commit: 1c686cade7ccd32cff1c8a043eec8b2795d32455

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -15346,6 +15346,7 @@ static void intel_setup_outputs(struct drm_i915_private *dev_priv)
 		intel_ddi_init(dev_priv, PORT_A);
 		intel_ddi_init(dev_priv, PORT_B);
 		intel_ddi_init(dev_priv, PORT_C);
+		icl_dsi_init(dev_priv);
 	} else if (IS_ELKHARTLAKE(dev_priv)) {
 		intel_ddi_init(dev_priv, PORT_A);
 		intel_ddi_init(dev_priv, PORT_B);
