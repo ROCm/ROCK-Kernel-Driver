23962331571b471bbb87e5e33b660097346d46a8
Takashi Iwai <tiwai@suse.de>
2019-12-17 14:48:03 +0100

    drm/i915/gt: Hook up intel_context_fini() (jsc#SLE-7953).
    suse-commit: f549ff4837e359643490d130e7f2802967e8e4d5

--- drivers/gpu/drm/i915/gt/intel_lrc.c
+++ drivers/gpu/drm/i915/gt/intel_lrc.c
@@ -1566,6 +1566,7 @@ static void execlists_context_destroy(struct kref *kref)
 	if (ce->state)
 		__execlists_context_fini(ce);
 
+	intel_context_fini(ce);
 	intel_context_free(ce);
 }
 
@@ -3188,6 +3189,7 @@ static void virtual_context_destroy(struct kref *kref)
 
 	if (ve->context.state)
 		__execlists_context_fini(&ve->context);
+	intel_context_fini(&ve->context);
 
 	kfree(ve->bonds);
 	kfree(ve);
