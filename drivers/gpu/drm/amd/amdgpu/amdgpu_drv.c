186a7d525b3f247572fb9713127905739132334e
Zhifeng Liu <Zhifeng.Liu@amd.com>
2019-12-17 13:40:59 +0000

    Bump AMDGPU version to 5.4.0
    Signed-off-by: Zhifeng Liu <Zhifeng.Liu@amd.com>
    Reviewed-by: Alvin Huan <Alvin.huan@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -91,7 +91,7 @@
 #define KMS_DRIVER_MINOR	37
 #define KMS_DRIVER_PATCHLEVEL	0
 
-#define AMDGPU_VERSION		"19.10.9.418"
+#define AMDGPU_VERSION		"5.4.0"
 
 int amdgpu_vram_limit = 0;
 int amdgpu_vis_vram_limit = 0;
