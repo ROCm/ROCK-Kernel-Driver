02ed471f8e585deb91df6ea5a8fd5a58f778acfb
Flora Cui <flora.cui@amd.com>
2019-12-19 13:00:20 +0800

    drm/amdkcl: enlarge vma manager size
    since ttm remove embedded vma_offset_manager and amdgpu init
    ttm_bo_device_init() with the gem one.
    it's to fix SWDEV-216777
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Kevin Wang <kevin1.wang@amd.com>

--- include/kcl/kcl_drm_vma_manager_backport.h
+++ include/kcl/kcl_drm_vma_manager_backport.h
@@ -8,6 +8,9 @@
 #include <drm/drm_vma_manager.h>
 #include <kcl/kcl_drmP_h.h>
 
+#if (BITS_PER_LONG == 64) && \
+	(!defined(DRM_FILE_PAGE_OFFSET_SIZE) || \
+	(DRM_FILE_PAGE_OFFSET_SIZE == ((0xFFFFFFFUL >> PAGE_SHIFT) * 16)))
 #ifdef DRM_FILE_PAGE_OFFSET_START
 #undef DRM_FILE_PAGE_OFFSET_START
 #endif
@@ -15,12 +18,22 @@
 #undef DRM_FILE_PAGE_OFFSET_SIZE
 #endif
 
-#if BITS_PER_LONG == 64
 #define DRM_FILE_PAGE_OFFSET_START ((0xFFFFFFFFUL >> PAGE_SHIFT) + 1)
 #define DRM_FILE_PAGE_OFFSET_SIZE ((0xFFFFFFFFUL >> PAGE_SHIFT) * 256)
+
+static inline void
+kcl_drm_vma_offset_manager_init(struct drm_vma_offset_manager *mgr)
+{
+	drm_vma_offset_manager_destroy(mgr);
+	drm_vma_offset_manager_init(mgr,
+		DRM_FILE_PAGE_OFFSET_START,
+		DRM_FILE_PAGE_OFFSET_SIZE);
+}
 #else
-#define DRM_FILE_PAGE_OFFSET_START ((0xFFFFFFFUL >> PAGE_SHIFT) + 1)
-#define DRM_FILE_PAGE_OFFSET_SIZE ((0xFFFFFFFUL >> PAGE_SHIFT) * 16)
+static inline void
+kcl_drm_vma_offset_manager_init(struct drm_vma_offset_manager *mgr)
+{
+}
 #endif
 
 #ifndef HAVE_DRM_VMA_NODE_VERIFY_ACCESS_HAS_DRM_FILE
