f11e44993c0db7e033ebc8968b67f01220c4a560
Jiansong Chen <Jiansong.Chen@amd.com>
2019-12-24 20:23:33 +0800

    drm/amdkcl: add guarding for amdgpu_dm_crtc_handle_crc_irq call
    to match with amdgpu_dm_crtc_handle_crc_irq definition,
    otherwise lead to undefined symbol.
    
    Change-Id: I99c2b6c05f199ef84c18e58b8048230d1f7044d5
    Signed-off-by: Jiansong Chen <Jiansong.Chen@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -497,7 +497,9 @@ static void dm_crtc_high_irq(void *interrupt_params)
 		/* Following stuff must happen at start of vblank, for crc
 		 * computation and below-the-range btr support in vrr mode.
 		 */
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 10, 0)
 		amdgpu_dm_crtc_handle_crc_irq(&acrtc->base);
+#endif
 
 		if (acrtc_state->stream && adev->family >= AMDGPU_FAMILY_AI &&
 		    acrtc_state->vrr_params.supported &&
@@ -550,7 +552,9 @@ static void dm_dcn_crtc_high_irq(void *interrupt_params)
 	DRM_DEBUG_DRIVER("crtc:%d, vupdate-vrr:%d\n", acrtc->crtc_id,
 				amdgpu_dm_vrr_active(acrtc_state));
 
+#if DRM_VERSION_CODE >= DRM_VERSION(4, 10, 0)
 	amdgpu_dm_crtc_handle_crc_irq(&acrtc->base);
+#endif
 	drm_crtc_handle_vblank(&acrtc->base);
 
 	spin_lock_irqsave(&adev->ddev->event_lock, flags);
