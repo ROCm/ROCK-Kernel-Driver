8a039dfaa94bf847a3751b3979f1f2b50dee7408
Stanley.Yang <Stanley.Yang@amd.com>
2019-12-26 17:15:39 +0800

    drm/amdkcl: Test whether drm_dp_mst_dsc_aux_for_port is available
    Change-Id: If098c1c8474741511e7ae338be90c1a382e3f5cc
    Signed-off-by: Stanley.Yang <Stanley.Yang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -192,7 +192,9 @@ static bool validate_dsc_caps_on_connector(struct amdgpu_dm_connector *aconnecto
 	struct drm_dp_mst_port *port = aconnector->port;
 	u8 dsc_caps[16] = { 0 };
 
+#if defined(HAVE_DRM_DP_MST_DSC_AUX_FOR_PORT)
 	aconnector->dsc_aux = drm_dp_mst_dsc_aux_for_port(port);
+#endif
 
 	if (!aconnector->dsc_aux)
 		return false;
@@ -259,6 +261,7 @@ static int dm_dp_mst_get_modes(struct drm_connector *connector)
 
 #ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 #if defined(CONFIG_DRM_AMD_DC_DCN1_0)
+#if defined(HAVE_DRM_DP_MST_DSC_AUX_FOR_PORT)
 			if (!validate_dsc_caps_on_connector(aconnector))
 				memset(&aconnector->dc_sink->dsc_caps,
 				       0, sizeof(aconnector->dc_sink->dsc_caps));
