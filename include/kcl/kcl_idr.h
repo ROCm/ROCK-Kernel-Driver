c26047e5837ec40d89619ea75c82864200fa8e24
Flora Cui <flora.cui@amd.com>
2020-01-02 16:05:26 +0800

    drm/amdkcl: fake idr_remove()
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jiansong Chen <Jiansong.Chen@amd.com>

--- include/kcl/kcl_idr.h
+++ include/kcl/kcl_idr.h
@@ -10,4 +10,18 @@
             ++id, (entry) = idr_get_next((idr), &(id)))
 #endif
 
+#ifndef HAVE_IDR_REMOVE_RETURN_VOID_POINTER
+static inline void *_kcl_idr_remove(struct idr *idr, int id)
+{
+	void *ptr;
+
+	ptr = idr_find(idr, id);
+	if (ptr)
+		idr_remove(idr, id);
+
+	return ptr;
+}
+#define idr_remove _kcl_idr_remove
+#endif /* HAVE_IDR_REMOVE_RETURN_VOID_POINTER */
+
 #endif /* AMDKCL_IDR_H */
