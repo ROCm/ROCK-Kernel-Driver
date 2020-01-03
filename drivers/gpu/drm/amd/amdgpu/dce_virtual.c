ce0e18f4e15f9be13b573f4f02f7fe0ce13338d8
Flora Cui <flora.cui@amd.com>
2020-01-03 12:39:41 +0800

    drm/amdkcl: drop test for ktime_set
    Change-Id: I68aa1979aea44b4525c62b1a6274ef0598fdc037
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Jiansong Chen <Jiansong.Chen@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/dce_virtual.c
+++ drivers/gpu/drm/amd/amdgpu/dce_virtual.c
@@ -749,11 +749,7 @@ static enum hrtimer_restart dce_virtual_vblank_timer_handle(struct hrtimer *vbla
 
 	drm_handle_vblank(ddev, amdgpu_crtc->crtc_id);
 	dce_virtual_pageflip(adev, amdgpu_crtc->crtc_id);
-#if defined(HAVE_KTIME_SET)
 	hrtimer_start(vblank_timer, ktime_set(0, DCE_VIRTUAL_VBLANK_PERIOD),
-#else
-	hrtimer_start(vblank_timer, DCE_VIRTUAL_VBLANK_PERIOD,
-#endif
 		      HRTIMER_MODE_REL);
 
 	return HRTIMER_NORESTART;
@@ -773,19 +769,11 @@ static void dce_virtual_set_crtc_vblank_interrupt_state(struct amdgpu_device *ad
 		hrtimer_init(&adev->mode_info.crtcs[crtc]->vblank_timer,
 			     CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 		hrtimer_set_expires(&adev->mode_info.crtcs[crtc]->vblank_timer,
-#if defined(HAVE_KTIME_SET)
 				    ktime_set(0, DCE_VIRTUAL_VBLANK_PERIOD));
-#else
-				    DCE_VIRTUAL_VBLANK_PERIOD);
-#endif
 		adev->mode_info.crtcs[crtc]->vblank_timer.function =
 			dce_virtual_vblank_timer_handle;
 		hrtimer_start(&adev->mode_info.crtcs[crtc]->vblank_timer,
-#if defined(HAVE_KTIME_SET)
 			      ktime_set(0, DCE_VIRTUAL_VBLANK_PERIOD), HRTIMER_MODE_REL);
-#else
-			      DCE_VIRTUAL_VBLANK_PERIOD, HRTIMER_MODE_REL);
-#endif
 	} else if (!state && adev->mode_info.crtcs[crtc]->vsync_timer_enabled) {
 		DRM_DEBUG("Disable software vsync timer\n");
 		hrtimer_cancel(&adev->mode_info.crtcs[crtc]->vblank_timer);
