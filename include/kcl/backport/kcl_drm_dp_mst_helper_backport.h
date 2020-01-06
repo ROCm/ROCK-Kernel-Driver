e37257505f87b554b8884777f7037ae3fe799504
Stanley.Yang <Stanley.Yang@amd.com>
2020-01-06 14:59:32 +0800

    drm/amdkcl: Test whether drm_dp_calc_pbn_mode() wants three arguments
    v2: drm/amdkcl: Test whether mul_u32_u32 is available
    v3: drm/amdkcl: move kcl copy for drm_dp_mst_helper into backport part
    the macros definition should be in backport to warn amdkcl to NOT
    include it.
    
    It is not necessary to wrap a mul_u32_u32() funcion,
    because function mul_u32_u32() is introdued into kernel before
    function drm_dp_calc_pbn_mode(int clock, int bpp, bool dsc).
    
    Signed-off-by: Stanley.Yang <Stanley.Yang@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    
    Signed-off-by: Stanley.Yang <Stanley.Yang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    
    Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/backport/kcl_drm_dp_mst_helper_backport.h
+++ include/kcl/backport/kcl_drm_dp_mst_helper_backport.h
@@ -24,6 +24,22 @@
 
 #include <drm/drm_dp_mst_helper.h>
 
+/* Copied from drivers/gpu/drm/drm_dp_mst_topology.c and modified for KCL */
+#if !defined(HAVE_DRM_DP_CALC_PBN_MODE_3ARGS)
+static inline
+int _kcl_drm_dp_calc_pbn_mode(int clock, int bpp, bool dsc)
+{
+#if defined(HAVE_MUL_U32_U32)
+	if (dsc)
+		return DIV_ROUND_UP_ULL(mul_u32_u32(clock * (bpp / 16), 64 * 1006),
+				8 * 54 * 1000 * 1000);
+#endif
+
+	return drm_dp_calc_pbn_mode(clock, bpp);
+}
+#define drm_dp_calc_pbn_mode _kcl_drm_dp_calc_pbn_mode
+#endif
+
 #if defined(HAVE_DRM_DP_ATOMIC_FIND_VCPI_SLOTS)
 #if !defined(HAVE_DRM_DP_ATOMIC_FIND_VCPI_SLOTS_5ARGS)
 static inline
