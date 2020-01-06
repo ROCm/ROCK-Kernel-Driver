b87a21b9caec98ee04bed8e6ee59c4709a1288b1
Goldwyn Rodrigues <rgoldwyn@suse.com>
2020-01-06 08:16:47 -0600

    - vfs: don't allow writes to swap files  (bsc#1158633). - Refresh patches.suse/vfs-add-super_operations-get_inode_dev.
    suse-commit: 4523846a103dc916c5ced249199252a29bc2774b

--- mm/filemap.c
+++ mm/filemap.c
@@ -2971,6 +2971,9 @@ inline ssize_t generic_write_checks(struct kiocb *iocb, struct iov_iter *from)
 	loff_t count;
 	int ret;
 
+	if (IS_SWAPFILE(inode))
+		return -ETXTBSY;
+
 	if (!iov_iter_count(from))
 		return 0;
 
