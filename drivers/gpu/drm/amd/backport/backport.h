9cd951318c4d980e62846d33970c0da7873668e6
Stanley.Yang <Stanley.Yang@amd.com>
2020-01-07 11:41:45 +0800

    drm/amdkcl: Test whether drm_dp_atomic_find_vcpi_slots() wants five arguments
    it is a squash of:
    
    commit 60952ca580bfaa25cb57731bd7803bae5ec5293d
    Author: Stanley.Yang <Stanley.Yang@amd.com>
    Date:   Tue Jan 7 11:41:45 2020 +0800
    
        drm/amdkcl: Test whether drm_dp_atomic_find_vcpi_slots() wants five
        arguments
    
        Signed-off-by: Stanley.Yang <Stanley.Yang@amd.com>
    
    commit 6bbc6aadcb87e88d0f7976230b8292674b835785
    Author: Slava Grigorev <slava.grigorev@amd.com>
    Date:   Thu Jan 23 17:08:59 2020 -0500
    
        drm/amdkcl: fix AC_AMDGPU_DRM_DP_ATOMIC_FIND_VCPI_SLOTS test
    
        - a logical error in the test
        - compiler warning about ingnoring return value
    
        Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    
    commit 655092d31b7e37020ef98bda5a0401f6a13f603c
    Author: Flora Cui <flora.cui@amd.com>
    Date:   Mon Nov 16 16:34:47 2020 +0800
    
        drm/amdkcl: move kcl copy for drm_dp_mst_helper into backport part
    
        the macros definition should be in backport to warn amdkcl to
        NOT include it.
    
        Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>
        Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -38,5 +38,6 @@
 #include <kcl/backport/kcl_drm_gem.h>
 #include <kcl/backport/kcl_drm_fb.h>
 #include <kcl/kcl_drm_connector.h>
+#include <kcl/backport/kcl_drm_dp_mst_helper_backport.h>
 
 #endif /* AMDGPU_BACKPORT_H */
