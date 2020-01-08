1634038ae555224f705dc2d3661b64ac04685052
Stanley.Yang <Stanley.Yang@amd.com>
2020-01-08 14:23:18 +0800

    drm/amdkcl: Test whether drm_dp_mst_atomic_enable_dsc() is available
    Change-Id: If8f9e171797152ab6a10c4bc05b1e8cf014f0d98
    Signed-off-by: Stanley.Yang <Stanley.Yang@amd.com>
    Acked-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6793,6 +6793,7 @@ const struct drm_encoder_helper_funcs amdgpu_dm_encoder_helper_funcs = {
 
 #ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 #if defined(CONFIG_DRM_AMD_DC_DCN1_0)
+#if defined(HAVE_DRM_DP_MST_ATOMIC_ENABLE_DSC)
 static int dm_update_mst_vcpi_slots_for_dsc(struct drm_atomic_state *state,
 					    struct dc_state *dc_state)
 {
@@ -6855,6 +6856,7 @@ static int dm_update_mst_vcpi_slots_for_dsc(struct drm_atomic_state *state,
 	}
 	return 0;
 }
+#endif
 #endif
 #endif
 
@@ -10412,9 +10414,11 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 		if (!compute_mst_dsc_configs_for_state(state, dm_state->context))
 			goto fail;
 
+#if defined(HAVE_DRM_DP_MST_ATOMIC_ENABLE_DSC)
 		ret = dm_update_mst_vcpi_slots_for_dsc(state, dm_state->context);
 		if (ret)
 			goto fail;
+#endif
 #endif
 
 		/*
@@ -10424,9 +10428,11 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 		 * to get stuck in an infinite loop and hang eventually.
 		 */
 #ifdef HAVE_DRM_DP_MST_ATOMIC_CHECK
+#if defined(HAVE_DRM_DP_MST_ATOMIC_ENABLE_DSC)
 		ret = drm_dp_mst_atomic_check(state);
 		if (ret)
 			goto fail;
+#endif
 #endif
 		status = dc_validate_global_state(dc, dm_state->context, false);
 		if (status != DC_OK) {
