17bc54b0cac5f99836bd958c88cf7ebc0ca7f1bc
Joerg Roedel <jroedel@suse.de>
2020-01-13 10:14:21 +0100

    iommu/vt-d: Unlink device if failed to add to group (bsc#1160752).
    suse-commit: 0ea0858f6be9b7de4d68155fd89a6269b3b1a8fe

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -5326,8 +5326,10 @@ static int intel_iommu_add_device(struct device *dev)
 
 	group = iommu_group_get_for_dev(dev);
 
-	if (IS_ERR(group))
-		return PTR_ERR(group);
+	if (IS_ERR(group)) {
+		ret = PTR_ERR(group);
+		goto unlink;
+	}
 
 	iommu_group_put(group);
 
@@ -5353,7 +5355,8 @@ static int intel_iommu_add_device(struct device *dev)
 				if (!get_private_domain_for_dev(dev)) {
 					dev_warn(dev,
 						 "Failed to get a private domain.\n");
-					return -ENOMEM;
+					ret = -ENOMEM;
+					goto unlink;
 				}
 
 				dev_info(dev,
@@ -5363,6 +5366,10 @@ static int intel_iommu_add_device(struct device *dev)
 	}
 
 	return 0;
+
+unlink:
+	iommu_device_unlink(&iommu->iommu, dev);
+	return ret;
 }
 
 static void intel_iommu_remove_device(struct device *dev)
