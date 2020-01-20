0bfff2b0b47ad1b24f8701a1ec55d97004c934c0
Nirmoy Das <nirmoy.das@amd.com>
2020-01-20 13:54:30 +0100

    drm/amdgpu:  remove unnecessary conversion to bool
    Better clean that up before some automation starts to complain about it
    
    Signed-off-by: Nirmoy Das <nirmoy.das@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v10_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v10_0.c
@@ -4246,7 +4246,7 @@ static int gfx_v10_0_set_powergating_state(void *handle,
 					  enum amd_powergating_state state)
 {
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
-	bool enable = (state == AMD_PG_STATE_GATE) ? true : false;
+	bool enable = (state == AMD_PG_STATE_GATE);
 	switch (adev->asic_type) {
 	case CHIP_NAVI10:
 	case CHIP_NAVI12:
@@ -4273,7 +4273,7 @@ static int gfx_v10_0_set_clockgating_state(void *handle,
 	case CHIP_NAVI14:
 	case CHIP_NAVI12:
 		gfx_v10_0_update_gfx_clock_gating(adev,
-						 state == AMD_CG_STATE_GATE ? true : false);
+						 state == AMD_CG_STATE_GATE);
 		break;
 	default:
 		break;
