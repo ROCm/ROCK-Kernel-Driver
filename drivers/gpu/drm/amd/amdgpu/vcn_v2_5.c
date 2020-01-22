16242cc03848de5453c0a7e51b8ab4313798fa4c
Nirmoy Das <nirmoy.das@amd.com>
2020-01-22 10:43:10 +0100

    drm/amdgpu: remove unused label
    Fixes: 445a7d54 (drm/amdgpu/vcn2.5: fix a bug for the 2nd vcn instance)
    
    Change-Id: Ic16883768b2c916cbf83e219ffd8a34e16502f26
    Signed-off-by: Nirmoy Das <nirmoy.das@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/vcn_v2_5.c
+++ drivers/gpu/drm/amd/amdgpu/vcn_v2_5.c
@@ -1353,7 +1353,6 @@ static int vcn_v2_5_stop(struct amdgpu_device *adev)
 			~UVD_POWER_STATUS__UVD_POWER_STATUS_MASK);
 	}
 
-power_off:
 	if (adev->pm.dpm_enabled)
 		amdgpu_dpm_enable_uvd(adev, false);
 
