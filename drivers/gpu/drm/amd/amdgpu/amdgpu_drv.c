4d97e8260de8153ac5de500c65da135ae4ce2805
Rui Teng <rui.teng@amd.com>
2020-02-12 19:53:29 +0800

    Bump AMDGPU version to 5.4.6
    Change-Id: Ib38f084f82b1fef50bf9215dd1aec4a6f21328bb
    Signed-off-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -90,7 +90,7 @@
 #define KMS_DRIVER_MINOR	36
 #define KMS_DRIVER_PATCHLEVEL	0
 
-#define AMDGPU_VERSION		"5.4.5"
+#define AMDGPU_VERSION		"5.4.6"
 
 int amdgpu_vram_limit = 0;
 int amdgpu_vis_vram_limit = 0;
