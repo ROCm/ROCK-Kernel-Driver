396908f44e9b5f633a3e3785d5e6807585858636
Mel Gorman <mgorman@suse.de>
2020-02-15 21:44:34 +0000

    sched/pelt: Remove unused runnable load average (bnc#1155798 (CPU scheduler functional and performance backports)).
    suse-commit: 99634f9954014cfc449d3b1a96c29683fad5f492

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -755,7 +755,6 @@ static void set_load_weight(struct task_struct *p, bool update_load)
 	if (task_has_idle_policy(p)) {
 		load->weight = scale_load(WEIGHT_IDLEPRIO);
 		load->inv_weight = WMULT_IDLEPRIO;
-		p->se.runnable_weight = load->weight;
 		return;
 	}
 
@@ -768,7 +767,6 @@ static void set_load_weight(struct task_struct *p, bool update_load)
 	} else {
 		load->weight = scale_load(sched_prio_to_weight[prio]);
 		load->inv_weight = sched_prio_to_wmult[prio];
-		p->se.runnable_weight = load->weight;
 	}
 }
 
