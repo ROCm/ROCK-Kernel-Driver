2dc0a0c98c563b8314be9176c9be7eb39068c50c
Jan Kara <jack@suse.cz>
2020-02-17 13:46:43 +0100

    - locks: print unsigned ino in /proc/locks (bsc#1163905). - blacklist.conf: blacklist e39e773ad100
    suse-commit: 611e661d2530d419ccfb5be0ee15694020ab9a65

--- fs/locks.c
+++ fs/locks.c
@@ -2791,7 +2791,7 @@ static void lock_get_status(struct seq_file *f, struct file_lock *fl,
 	}
 	if (inode) {
 		/* userspace relies on this representation of dev_t */
-		seq_printf(f, "%d %02x:%02x:%ld ", fl_pid,
+		seq_printf(f, "%d %02x:%02x:%lu ", fl_pid,
 				MAJOR(inode_get_dev(inode)),
 				MINOR(inode_get_dev(inode)), inode->i_ino);
 	} else {
