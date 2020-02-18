083ce6e435a090d127276ed165e591f4c8ccc6b7
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2020-02-18 15:50:52 +0100

    RDMA/uverbs: Verify MR access flags (jsc#SLE-8449).
    suse-commit: fdd4a34c214960fe09078413eefb5e43c7e622a4

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -4321,6 +4321,9 @@ static inline int ib_check_mr_access(int flags)
 	    !(flags & IB_ACCESS_LOCAL_WRITE))
 		return -EINVAL;
 
+	if (flags & ~IB_ACCESS_SUPPORTED)
+		return -EINVAL;
+
 	return 0;
 }
 
