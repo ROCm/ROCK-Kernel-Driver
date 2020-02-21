ba6c02645ecca0f14f98e764ef4316ae52762bf2
Hannes Reinecke <hare@suse.de>
2020-02-21 08:22:43 +0100

    Disable 'megaraid_sas-fixup-MSIx-interrupt-setup-during-resume'
    suse-commit: e492554de5cc2c234568bc49a9ccdb16b6b8af44

--- drivers/scsi/megaraid/megaraid_sas_base.c
+++ drivers/scsi/megaraid/megaraid_sas_base.c
@@ -7592,6 +7592,7 @@ megasas_resume(struct pci_dev *pdev)
 	int rval;
 	struct Scsi_Host *host;
 	struct megasas_instance *instance;
+	int irq_flags = PCI_IRQ_LEGACY;
 
 	instance = pci_get_drvdata(pdev);
 
@@ -7633,15 +7634,16 @@ megasas_resume(struct pci_dev *pdev)
 	atomic_set(&instance->ldio_outstanding, 0);
 
 	/* Now re-enable MSI-X */
-	if (instance->msix_vectors)
-		megasas_alloc_irq_vectors(instance);
-
-	if (!instance->msix_vectors) {
-		rval = pci_alloc_irq_vectors(instance->pdev, 1, 1,
-					     PCI_IRQ_LEGACY);
-		if (rval < 0)
-			goto fail_reenable_msix;
+	if (instance->msix_vectors) {
+		irq_flags = PCI_IRQ_MSIX;
+		if (instance->smp_affinity_enable)
+			irq_flags |= PCI_IRQ_AFFINITY;
 	}
+	rval = pci_alloc_irq_vectors(instance->pdev, 1,
+				     instance->msix_vectors ?
+				     instance->msix_vectors : 1, irq_flags);
+	if (rval < 0)
+		goto fail_reenable_msix;
 
 	megasas_setup_reply_map(instance);
 
