045254115f184c2fbaa87e1580254cf514a17761
Alex Deucher <alexander.deucher@amd.com>
2020-02-27 09:59:01 -0500

    drm/amdgpu: don't enable runtime pm on navi12
    BACO for power savings is not supported on navi12.
    
    Change-Id: I04a86cc4680ef5cc695cc49e7ebc3ed7d73f7a9d
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Tested-by: Gang Long <gang.long@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -185,7 +185,8 @@ int amdgpu_driver_load_kms(struct drm_device *dev, unsigned long flags)
 		 (adev->asic_type >= CHIP_TOPAZ) &&
 		 (adev->asic_type != CHIP_VEGA10) &&
 		 (adev->asic_type != CHIP_VEGA20) &&
-		 (adev->asic_type != CHIP_ARCTURUS)) /* enable runpm on VI+ */
+		 (adev->asic_type != CHIP_ARCTURUS) &&
+		 (adev->asic_type != CHIP_NAVI12)) /* enable runpm on VI+ */
 		adev->runpm = true;
 	else if (amdgpu_device_supports_baco(dev) &&
 		 (amdgpu_runtime_pm > 0))  /* enable runpm if runpm=1 on CI */
