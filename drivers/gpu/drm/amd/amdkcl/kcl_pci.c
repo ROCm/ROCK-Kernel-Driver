ddbb152dab86b5940791c5efc19071a664815851
Flora Cui <flora.cui@amd.com>
2020-02-28 17:22:21 +0800

    drm/amdkcl: update _kcl_pci_create_measure_file()
    IIRC some OS has the sysfs entry and return -EEXIST.
    this patch is desired to fail that case and wait for sanity test to
    figuire out the specified distro.
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_pci.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_pci.c
@@ -343,42 +343,33 @@ static struct attribute *pcie_dev_attrs[] = {
 	NULL,
 };
 
-static const struct attribute_group pcie_dev_group = {
-	.attrs = pcie_dev_attrs,
-};
-
-const struct attribute_group *pcie_dev_groups[] = {
-	&pcie_dev_group,
-	NULL,
-};
-
 int _kcl_pci_create_measure_file(struct pci_dev *pdev)
 {
 	int ret = 0;
 
 	ret = device_create_file(&pdev->dev, &dev_attr_current_link_speed);
-	if (ret != 0 && ret != -EEXIST) {
+	if (ret) {
 		dev_err(&pdev->dev,
 				"Failed to create current_link_speed sysfs files: %d\n", ret);
 		return ret;
 	}
 
 	ret = device_create_file(&pdev->dev, &dev_attr_current_link_width);
-	if (ret != 0 && ret != -EEXIST) {
+	if (ret) {
 		dev_err(&pdev->dev,
 				"Failed to create current_link_width sysfs files: %d\n", ret);
 		return ret;
 	}
 
 	ret = device_create_file(&pdev->dev, &dev_attr_max_link_width);
-	if (ret != 0 && ret != -EEXIST) {
+	if (ret) {
 		dev_err(&pdev->dev,
 				"Failed to create max_link_width sysfs files: %d\n", ret);
 		return ret;
 	}
 
 	ret = device_create_file(&pdev->dev, &dev_attr_max_link_speed);
-	if (ret != 0 && ret != -EEXIST) {
+	if (ret) {
 		dev_err(&pdev->dev,
 				"Failed to create max_link_speed sysfs files: %d\n", ret);
 		return ret;
