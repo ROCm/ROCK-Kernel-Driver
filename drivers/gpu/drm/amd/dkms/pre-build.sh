c1b19bb1c3cd989a1d69f1d90baca8afd158c76f
Slava Grigorev <slava.grigorev@amd.com>
2020-02-29 19:23:40 -0500

    drm/amdkcl: fix SUSE DKMS build
    v2: drm/amdkcl: fix header files location when build DKMS on SUSE
    
    Change-Id: I6129a3ad889bbf1d4b4f0a1c4711eb15d414d918
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>

--- drivers/gpu/drm/amd/dkms/pre-build.sh
+++ drivers/gpu/drm/amd/dkms/pre-build.sh
@@ -6,7 +6,16 @@ SRC="amd/dkms"
 
 KERNELVER=$1
 KERNELVER_BASE=${KERNELVER%%-*}
-SRCTREE=/lib/modules/$KERNELVER/build
+SRCTREE=/lib/modules/$KERNELVER
+
+if [ -L $SRCTREE/source ]; then
+	BLDTREE="$SRCTREE/build"
+	SRCTREE="$SRCTREE/source"
+else
+	SRCTREE="$SRCTREE/build"
+	BLDTREE="$SRCTREE"
+fi
+
 SRCARCH=$(uname -m | sed -e "s/i.86/x86/" -e "s/x86_64/x86/" \
         -e "s/sun4u/sparc64/" -e "s/arm.*/arm/" -e "s/sa110/arm/" \
         -e "s/s390x/s390/" -e "s/parisc64/parisc/" \
@@ -74,8 +83,9 @@ done
 
 export KERNELVER
 CPPFLAGS="-I$SRCTREE/arch/$SRCARCH/include \
-	-I$SRCTREE/arch/$SRCARCH/include/generated \
+	-I$BLDTREE/arch/$SRCARCH/include/generated \
 	-I$SRCTREE/include \
+	-I$BLDTREE/include \
 	-I$SRCTREE/include/uapi \
 	-include linux/kconfig.h" \
 	./configure
