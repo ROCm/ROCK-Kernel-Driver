9825ef389252773223ac01c943e6d7740650a0ea
Feifei Xu <Feifei.Xu@amd.com>
2020-03-02 14:51:40 +0800

    drm/amdgpu/runpm: disable runpm on Vega10
    Some framework test will fail if enable runpm on Vega10.
    Disable it untill issue fixed.
    
    Signed-off-by: Feifei Xu <Feifei.Xu@amd.com>
    Tested-by: Kyle Chen <Kyle.Chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -186,6 +186,7 @@ int amdgpu_driver_load_kms(struct drm_device *dev, unsigned long flags)
 	else if (amdgpu_device_supports_baco(dev) &&
 		 (amdgpu_runtime_pm != 0) &&
 		 (adev->asic_type >= CHIP_TOPAZ) &&
+		 (adev->asic_type != CHIP_VEGA10) &&
 		 (adev->asic_type != CHIP_VEGA20) &&
 		 (adev->asic_type != CHIP_ARCTURUS) &&
 		 (adev->asic_type != CHIP_NAVI12)) /* enable runpm on VI+ */
