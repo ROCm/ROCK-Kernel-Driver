63d8dc38a3f15015c02daa1f0972c16db977db24
Yifan Zhang <yifan1.zhang@amd.com>
2020-03-04 15:46:02 +0800

    drm/amdkcl: add HAVE_HDR_SINK_METADATA macro v3
    Change-Id: Ifbf6c42b1b46065c6e7836e80bb505e14dfdf76c
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -143,6 +143,7 @@ struct vblank_control_work {
  * Describe the backlight support for ACPI or eDP AUX.
  */
 struct amdgpu_dm_backlight_caps {
+#ifdef HAVE_HDR_SINK_METADATA
 	/**
 	 * @ext_caps: Keep the data struct with all the information about the
 	 * display support for HDR.
@@ -157,6 +158,7 @@ struct amdgpu_dm_backlight_caps {
 	 * in nits.
 	 */
 	u32 aux_max_input_signal;
+#endif
 	/**
 	 * @min_input_signal: minimum possible input in range 0-255.
 	 */
@@ -172,7 +174,9 @@ struct amdgpu_dm_backlight_caps {
 	/**
 	 * @aux_support: Describes if the display supports AUX backlight.
 	 */
+#ifdef HAVE_HDR_SINK_METADATA
 	bool aux_support;
+#endif
 };
 
 /**
