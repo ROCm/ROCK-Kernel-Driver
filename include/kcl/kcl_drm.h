089d975be8a8973b3d9afdf34d849c873b91997c
Flora Cui <flora.cui@amd.com>
2020-03-06 16:46:07 +0800

    drm/amdkcl: fake kcl copy of drm_dev_get
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- include/kcl/kcl_drm.h
+++ include/kcl/kcl_drm.h
@@ -332,6 +332,11 @@ void drm_fb_helper_fill_info(struct fb_info *info,
 #endif
 
 #ifndef HAVE_DRM_DEV_PUT
+static inline void drm_dev_get(struct drm_device *dev)
+{
+	drm_dev_ref(dev);
+}
+
 static inline void drm_dev_put(struct drm_device *dev)
 {
 	return drm_dev_unref(dev);
