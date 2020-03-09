2594a33c092ad8a39fd727e2ad24d6cd45e81e06
Anthony Iliopoulos <ailiopoulos@suse.com>
2020-03-09 23:47:15 +0100

    xfs: attach dquots before performing xfs_swap_extents (bsc#1166161).
    suse-commit: 1da5af8f7baa067f7886449102bd48c41482cd16

--- fs/xfs/xfs_bmap_util.c
+++ fs/xfs/xfs_bmap_util.c
@@ -1752,6 +1752,14 @@ xfs_swap_extents(
 		goto out_unlock;
 	}
 
+	error = xfs_qm_dqattach(ip);
+	if (error)
+		goto out_unlock;
+
+	error = xfs_qm_dqattach(tip);
+	if (error)
+		goto out_unlock;
+
 	error = xfs_swap_extent_flush(ip);
 	if (error)
 		goto out_unlock;
