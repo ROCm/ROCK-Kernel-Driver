ca83f59c984071e829394c8d81ee7a7dcca2d58c
Jonathan Kim <jonathan.kim@amd.com>
2020-03-10 14:59:19 -0400

    drm/amdkfd: fill in data for control stack header for gfx10
    The debugger requires the control stack header to be filled in to
    update_waves.
    
    Signed-off-by: Jonathan Kim <Jonathan.Kim@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_mqd_manager_v9.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_mqd_manager_v9.c
@@ -366,38 +366,13 @@ static bool is_occupied(struct mqd_manager *mm, void *mqd,
 		pipe_id, queue_id);
 }
 
-struct user_context_save_area_header {
-	/* Byte offset from start of user context
-	 * save area to the last saved top (lowest
-	 * address) of control stack data. Must be
-	 * 4 byte aligned.
-	 */
-	uint32_t control_stack_offset;
-
-	/* Byte size of the last saved control stack
-	 * data. Must be 4 byte aligned.
-	 */
-	uint32_t control_stack_size;
-
-	/* Byte offset from start of user context save
-	 * area to the last saved base (lowest address)
-	 * of wave state data. Must be 4 byte aligned.
-	 */
-	uint32_t wave_state_offset;
-
-	/* Byte size of the last saved wave state data.
-	 * Must be 4 byte aligned.
-	 */
-	uint32_t wave_state_size;
-};
-
 static int get_wave_state(struct mqd_manager *mm, void *mqd,
 			  void __user *ctl_stack,
 			  u32 *ctl_stack_used_size,
 			  u32 *save_area_used_size)
 {
 	struct v9_mqd *m;
-	struct user_context_save_area_header header;
+	struct mqd_user_context_save_area_header header;
 
 	/* Control stack is located one page after MQD. */
 	void *mqd_ctl_stack = (void *)((uintptr_t)mqd + PAGE_SIZE);
