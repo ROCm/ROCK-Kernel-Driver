8078baaa4354fd76c0e54211f0e058ccf574170a
Mel Gorman <mgorman@suse.de>
2020-03-11 10:24:10 +0000

    sched/numa: Acquire RCU lock for checking idle cores during NUMA balancing (bnc#1155798 (CPU scheduler functional and performance backports)).
    suse-commit: 6ce2abfff608ca6338492257c3bd11434181d9bf

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1596,6 +1596,7 @@ static void update_numa_stats(struct task_numa_env *env,
 	memset(ns, 0, sizeof(*ns));
 	ns->idle_cpu = -1;
 
+	rcu_read_lock();
 	for_each_cpu(cpu, cpumask_of_node(nid)) {
 		struct rq *rq = cpu_rq(cpu);
 
@@ -1615,6 +1616,7 @@ static void update_numa_stats(struct task_numa_env *env,
 			idle_core = numa_idle_core(idle_core, cpu);
 		}
 	}
+	rcu_read_unlock();
 
 	ns->weight = cpumask_weight(cpumask_of_node(nid));
 
