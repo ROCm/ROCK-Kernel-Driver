91663278f9544ee9aeba52f306b6f0c04b1f8fb0
Mel Gorman <mgorman@suse.de>
2020-03-11 10:24:10 +0000

    sched/fair: Fix runnable_avg for throttled cfs (bnc#1155798 (CPU scheduler functional and performance backports)).
    suse-commit: 5652c540c63ff519923b4659b76364999d83858e

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4704,8 +4704,13 @@ static void throttle_cfs_rq(struct cfs_rq *cfs_rq)
 		if (!se->on_rq)
 			break;
 
-		if (dequeue)
+		if (dequeue) {
 			dequeue_entity(qcfs_rq, se, DEQUEUE_SLEEP);
+		} else {
+			update_load_avg(qcfs_rq, se, 0);
+			se_update_runnable(se);
+		}
+
 		qcfs_rq->h_nr_running -= task_delta;
 		qcfs_rq->idle_h_nr_running -= idle_task_delta;
 
@@ -4773,8 +4778,13 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 			enqueue = 0;
 
 		cfs_rq = cfs_rq_of(se);
-		if (enqueue)
+		if (enqueue) {
 			enqueue_entity(cfs_rq, se, ENQUEUE_WAKEUP);
+		} else {
+			update_load_avg(cfs_rq, se, 0);
+			se_update_runnable(se);
+		}
+
 		cfs_rq->h_nr_running += task_delta;
 		cfs_rq->idle_h_nr_running += idle_task_delta;
 
