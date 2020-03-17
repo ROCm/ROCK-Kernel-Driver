b4242db816b374a208417e0541e658fdaec1b370
Jan Kara <jack@suse.cz>
2020-03-17 17:24:41 +0100

    io_uring: prevent sq_thread from spinning when it should stop (bsc#1166875).
    suse-commit: 59a5904da24c8324424c5d3fec4af5f1ec3d5777

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2421,16 +2421,6 @@ static int io_sq_thread(void *data)
 		to_submit = io_sqring_entries(ctx);
 		if (!to_submit) {
 			/*
-			 * We're polling. If we're within the defined idle
-			 * period, then let us spin without work before going
-			 * to sleep.
-			 */
-			if (inflight || !time_after(jiffies, timeout)) {
-				cpu_relax();
-				continue;
-			}
-
-			/*
 			 * Drop cur_mm before scheduling, we can't hold it for
 			 * long periods (or over schedule()). Do this before
 			 * adding ourselves to the waitqueue, as the unuse/drop
@@ -2440,6 +2430,16 @@ static int io_sq_thread(void *data)
 				unuse_mm(cur_mm);
 				mmput(cur_mm);
 				cur_mm = NULL;
+			}
+
+			/*
+			 * We're polling. If we're within the defined idle
+			 * period, then let us spin without work before going
+			 * to sleep.
+			 */
+			if (inflight || !time_after(jiffies, timeout)) {
+				cpu_relax();
+				continue;
 			}
 
 			prepare_to_wait(&ctx->sqo_wait, &wait,
