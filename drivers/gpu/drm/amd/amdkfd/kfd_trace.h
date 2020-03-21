92bb89fa1c947194646c203760bc27520a4213b6
Slava Grigorev <slava.grigorev@amd.com>
2020-03-21 19:19:35 -0400

    amd/amdkcl: drop use of BUILD_AS_DKMS in Makefile files
    Change-Id: I53c86b8f52b8cabe35cc7b0a63f49dea585590a1
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_trace.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_trace.h
@@ -147,9 +147,5 @@ TRACE_EVENT(kfd_restore_process_worker_end,
 
 /* This part must be outside protection */
 #undef TRACE_INCLUDE_PATH
-#ifdef BUILD_AS_DKMS
-#define TRACE_INCLUDE_PATH ./
-#else
-#define TRACE_INCLUDE_PATH ../../drivers/gpu/drm/amd/amdkfd
-#endif
+#define TRACE_INCLUDE_PATH .
 #include <trace/define_trace.h>
