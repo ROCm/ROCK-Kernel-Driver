e246d91ec83ae24f846eb0f6f6cc0feecd98ad19
Andrey Grodzovsky <andrey.grodzovsky@amd.com>
2020-03-24 16:46:05 -0400

    drm/amdgpu: Add a UAPI flag for user to call mem_sync
    When this flag is set in the CS IB flags, it causes
    a memory cache flush of the GFX.
    
    v2:
    Move new flag to drm_amdgpu_cs_chunk_ib.flags
    Bump up UAPI version
    Remove condition on job != null to emit mem_sync
    
    Signed-off-by: Andrey Grodzovsky <andrey.grodzovsky@amd.com>
    Reviewed-by: Luben Tuikov <luben.tuikov@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -85,9 +85,10 @@
  * - 3.34.0 - Non-DC can flip correctly between buffers with different pitches
  * - 3.35.0 - Add drm_amdgpu_info_device::tcc_disabled_mask
  * - 3.36.0 - Allow reading more status registers on si/cik
+ * - 3.37.0 - Add AMDGPU_IB_FLAG_EMIT_MEM_SYNC
  */
 #define KMS_DRIVER_MAJOR	3
-#define KMS_DRIVER_MINOR	36
+#define KMS_DRIVER_MINOR	37
 #define KMS_DRIVER_PATCHLEVEL	0
 
 int amdgpu_vram_limit = 0;
