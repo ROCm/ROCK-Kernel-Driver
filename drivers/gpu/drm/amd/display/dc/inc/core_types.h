6b7eaf24478fa87854321be80176517ac48828ee
Anthony Koo <Anthony.Koo@amd.com>
2020-03-28 01:44:14 -0400

    drm/amd/display: move panel power seq to new panel struct
    [Why]
    panel power sequencer is currently just sitting in hwseq but it really
    it tied to internal panels
    
    [How]
    make a new panel struct to contain power sequencer code
    
    Signed-off-by: Anthony Koo <Anthony.Koo@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>

--- drivers/gpu/drm/amd/display/dc/inc/core_types.h
+++ drivers/gpu/drm/amd/display/dc/inc/core_types.h
@@ -40,6 +40,7 @@
 #include "dwb.h"
 #include "mcif_wb.h"
 #endif
+#include "panel.h"
 
 #define MAX_CLOCK_SOURCES 7
 
@@ -96,6 +97,8 @@ struct clk_bw_params;
 struct resource_funcs {
 	void (*destroy)(struct resource_pool **pool);
 	void (*link_init)(struct dc_link *link);
+	struct panel*(*panel_create)(
+		const struct panel_init_data *panel_init_data);
 	struct link_encoder *(*link_enc_create)(
 			const struct encoder_init_data *init);
 	bool (*validate_bandwidth)(
