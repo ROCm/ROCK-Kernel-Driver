e3a026d823636c0bc2c2476a040758d9779cf35d
Emily Deng <Emily.Deng@amd.com>
2020-04-02 15:06:51 +0800

    amd/amdkcl: Fix the assert report
    Signed-off-by: Emily Deng <Emily.Deng@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Change-Id: If2f264ccc7b2f0e73952fd88b45a23174d22ea61
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/dce_v8_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v8_0.c
@@ -291,7 +291,7 @@ static void dce_v8_0_hpd_init(struct amdgpu_device *adev)
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
 #else
-	drm_for_each_connector(connector, dev) {
+	list_for_each_entry(connector, &(dev)->mode_config.connector_list, head) {
 #endif
 		struct amdgpu_connector *amdgpu_connector = to_amdgpu_connector(connector);
 
@@ -344,7 +344,7 @@ static void dce_v8_0_hpd_fini(struct amdgpu_device *adev)
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
 #else
-	drm_for_each_connector(connector, dev) {
+	list_for_each_entry(connector, &(dev)->mode_config.connector_list, head) {
 #endif
 		struct amdgpu_connector *amdgpu_connector = to_amdgpu_connector(connector);
 
@@ -1206,7 +1206,7 @@ static void dce_v8_0_audio_write_latency_fields(struct drm_encoder *encoder,
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
 #else
-	drm_for_each_connector(connector, dev) {
+	list_for_each_entry(connector, &(dev)->mode_config.connector_list, head) {
 #endif
 		if (connector->encoder == encoder) {
 			amdgpu_connector = to_amdgpu_connector(connector);
@@ -1277,7 +1277,7 @@ static void dce_v8_0_audio_write_speaker_allocation(struct drm_encoder *encoder)
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
 #else
-	drm_for_each_connector(connector, dev) {
+	list_for_each_entry(connector, &(dev)->mode_config.connector_list, head) {
 #endif
 		if (connector->encoder == encoder) {
 			amdgpu_connector = to_amdgpu_connector(connector);
@@ -1353,7 +1353,7 @@ static void dce_v8_0_audio_write_sad_regs(struct drm_encoder *encoder)
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
 #else
-	drm_for_each_connector(connector, dev) {
+	list_for_each_entry(connector, &(dev)->mode_config.connector_list, head) {
 #endif
 		if (connector->encoder == encoder) {
 			amdgpu_connector = to_amdgpu_connector(connector);
