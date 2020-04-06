2037c614b1bcd82cf0e90026b8354f4b4eb270b0
Qu Wenruo <wqu@suse.com>
2020-04-06 14:29:03 +0800

    - btrfs: qgroup: ensure qgroup_rescan_running is only set when   the worker is at least queued (bsc#1168670). - Delete   patches.suse/btrfs-qgroups-fix-rescan-worker-running-races.patch.
    suse-commit: acca6e8ebbfe77d72f117f32dcfb9a05ca04087a

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -886,8 +886,6 @@ struct btrfs_fs_info {
 	struct btrfs_workqueue *qgroup_rescan_workers;
 	struct completion qgroup_rescan_completion;
 	struct btrfs_work qgroup_rescan_work;
-	/* qgroup rescan worker is running or queued to run */
-	bool qgroup_rescan_ready;
 	bool qgroup_rescan_running;	/* protected by qgroup_rescan_lock */
 
 	/* filesystem state */
