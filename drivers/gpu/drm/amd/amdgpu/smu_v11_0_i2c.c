e69fb82aeab8523dd8c1c4a41e5f6d6989044a98
Flora Cui <flora.cui@amd.com>
2020-04-13 06:37:24 +0800

    drm/amdkcl: Test whether struct i2c_lock_operations is defined
    void (*lock_bus)(struct i2c_adapter *, unsigned int flags);
    int (*trylock_bus)(struct i2c_adapter *, unsigned int flags);
    void (*unlock_bus)(struct i2c_adapter *, unsigned int flags);
    are not defined in struct i2c_adapter until the following patch:
    	commit 8320f495cf441d593f7cd4f30e6b63455be71a2c
    	Author: Peter Rosin <peda@axentia.se>
    	Date:   Wed May 4 22:15:27 2016 +0200
    	i2c: allow adapter drivers to override the adapter locking
    
    It's not defined until kernel version 4.7.0.
    So it's hard to support lock_bus,trylock_bus and unlock_bus on
    redhat
    7.7 whose kernel version is 3.10.0.
    
    v2: 31fbd646eca3 drm/amdgpu: add lock for i2c bus
    (andrey.grodzovsky@amd.com)
    
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Adam Yang <Adam1.Yang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    
    Signed-off-by: Andrey Grodzovsky <andrey.grodzovsky@amd.com>
    Reviewed-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/smu_v11_0_i2c.c
+++ drivers/gpu/drm/amd/amdgpu/smu_v11_0_i2c.c
@@ -610,17 +610,23 @@ static void unlock_bus(struct i2c_adapter *i2c, unsigned int flags)
 	adev->pm.bus_locked = false;
 }
 
+#if defined(HAVE_I2C_LOCK_OPERATIONS_STRUCT)
 static const struct i2c_lock_operations smu_v11_0_i2c_i2c_lock_ops = {
 	.lock_bus = lock_bus,
 	.trylock_bus = trylock_bus,
 	.unlock_bus = unlock_bus,
 };
+#endif
 
 static int smu_v11_0_i2c_xfer(struct i2c_adapter *i2c_adap,
 			      struct i2c_msg *msgs, int num)
 {
 	int i, ret;
 	struct amdgpu_device *adev = to_amdgpu_device(i2c_adap);
+
+#if !defined(HAVE_I2C_LOCK_OPERATIONS_STRUCT)
+	lock_bus(i2c_adap, 0);
+#endif
 
 	if (!adev->pm.bus_locked) {
 		DRM_ERROR("I2C bus unlocked, stopping transaction!");
@@ -646,6 +652,10 @@ static int smu_v11_0_i2c_xfer(struct i2c_adapter *i2c_adap,
 	}
 
 	smu_v11_0_i2c_fini(i2c_adap);
+
+#if !defined(HAVE_I2C_LOCK_OPERATIONS_STRUCT)
+	unlock_bus(i2c_adap, 0);
+#endif
 	return num;
 }
 
@@ -670,7 +680,9 @@ int smu_v11_0_i2c_control_init(struct i2c_adapter *control)
 	control->dev.parent = &adev->pdev->dev;
 	control->algo = &smu_v11_0_i2c_algo;
 	snprintf(control->name, sizeof(control->name), "AMDGPU SMU");
+#if defined(HAVE_I2C_LOCK_OPERATIONS_STRUCT)
 	control->lock_ops = &smu_v11_0_i2c_i2c_lock_ops;
+#endif
 
 	res = i2c_add_adapter(control);
 	if (res)
