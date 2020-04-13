3aedd6f31577cd59ab06524525de9eee00c6b97e
Yifan Zhang <yifan1.zhang@amd.com>
2020-04-13 16:51:26 +0800

    drm/amdkcl: add AC_AMDGPU_DMA_FENCE_HEADERS
    it is a squash of:
    
    commit e1d2e9c077849c7aa9732709870762117e6c435a
    Author: Slava Grigorev <slava.grigorev@amd.com>
    Date:   Tue Feb 25 21:30:21 2020 -0500
    
        drm/amdkcl: Fix in-tree build if locate of output files (O=) specified
    
        Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    
    commit 671508e545e8b7e6fda2209dfb2aae21e9b7ae54
    Author: Slava Grigorev <slava.grigorev@amd.com>
    Date:   Thu Feb 20 15:57:12 2020 -0500
    
        drm/amdkcl: drop AC_KERNEL_TEST_HEADER_FILE_EXIST macro
    
        remove AC_KERNEL_TEST_HEADER_FILE_EXIST macro from AC_AMDGPU_DRM_DEV_SUPPORTED test
    
        Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
        Reviewed-by: Flora Cui <Flora.Cui@amd.com>
    
    commit 5ebd41454f8a12b84f590c3c68e4333246ce1b53
    Author: Slava Grigorev <slava.grigorev@amd.com>
    Date:   Thu Feb 20 14:55:08 2020 -0500
    
        drm/amdkcl: modify dma-fence-headers test to use AC_KERENEL_CHECK_HERDERS macro
    
        Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
        Reviewed-by: Flora Cui <Flora.Cui@amd.com>
    
    commit c1f004d6344bd8c1c04343526b033c2768adfce7
    Author: Flora Cui <flora.cui@amd.com>
    Date:   Mon Nov 16 10:46:04 2020 +0800
    
        drm/amdkcl: fix license for kcl part
    
        Signed-off-by: Flora Cui <flora.cui@amd.com>
        Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Change-Id: I2ceeb8dc72f5ed53ebdd507b71faab67c59b56ab

--- drivers/gpu/drm/amd/amdkcl/main.c
+++ drivers/gpu/drm/amd/amdkcl/main.c
@@ -3,6 +3,7 @@
 #include <linux/module.h>
 
 extern void amdkcl_symbol_init(void);
+extern void amdkcl_fence_init(void);
 extern void amdkcl_io_init(void);
 extern void amdkcl_kthread_init(void);
 extern void amdkcl_mm_init(void);
@@ -13,6 +14,7 @@ extern void amdkcl_suspend_init(void);
 int __init amdkcl_init(void)
 {
 	amdkcl_symbol_init();
+	amdkcl_fence_init();
 	amdkcl_io_init();
 	amdkcl_kthread_init();
 	amdkcl_mm_init();
