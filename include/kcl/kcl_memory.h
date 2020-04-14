ba6c89e9fb5e31ea64cc3c5fc314b4ad34d6f9ae
Flora Cui <flora.cui@amd.com>
2020-04-14 18:18:15 +0800

    drm/amdkcl: test whether vmf_insert_mixed_prot() is available
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_memory.h
+++ include/kcl/kcl_memory.h
@@ -36,4 +36,15 @@ static inline vm_fault_t vmf_insert_pfn(struct vm_area_struct *vma,
 
 #endif /* HAVE_VMF_INSERT */
 
+#ifndef HAVE_VMF_INSERT_MIXED_PROT
+vm_fault_t _kcl_vmf_insert_mixed_prot(struct vm_area_struct *vma, unsigned long addr,
+				      pfn_t pfn, pgprot_t pgprot);
+static inline
+vm_fault_t vmf_insert_mixed_prot(struct vm_area_struct *vma, unsigned long addr,
+			pfn_t pfn, pgprot_t pgprot)
+{
+	return _kcl_vmf_insert_mixed_prot(vma, addr, pfn, pgprot);
+}
+#endif /* HAVE_VMF_INSERT_MIXED_PROT */
+
 #endif
