c227d3c75dfe3a7d3f7a3ad9a139338ab3d38b62
Flora Cui <flora.cui@amd.com>
2020-04-14 22:41:34 +0800

    drm/amdkcl: test whether drm_debug_enabled() is available
    Signed-off-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/kcl_drm_print.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef AMDKCL_DRM_PRINT_H
+#define AMDKCL_DRM_PRINT_H
+
+#include <kcl/header/kcl_drm_print_h.h>
+#include <kcl/header/kcl_drm_drv_h.h>
+
+#ifndef HAVE_DRM_DEBUG_ENABLED
+static  inline bool drm_debug_enabled(unsigned int category)
+{
+	return unlikely(drm_debug & category);
+}
+#endif /* HAVE_DRM_DEBUG_ENABLED */
+#endif
