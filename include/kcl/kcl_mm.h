9b95a49e8d987f5ebcbc87b7c40409db103e7443
Flora Cui <flora.cui@amd.com>
2020-04-15 18:07:17 +0800

    drm/amdkcl: fake a kcl copy of vmf_insert_pfn_prot()
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_mm.h
+++ include/kcl/kcl_mm.h
@@ -3,6 +3,7 @@
 
 #include <linux/mm.h>
 #include <asm/page.h>
+#include <linux/mm_types.h>
 
 #ifndef HAVE_VMF_INSERT_MIXED_PROT
 vm_fault_t _kcl_vmf_insert_mixed_prot(struct vm_area_struct *vma, unsigned long addr,
@@ -14,4 +15,14 @@ vm_fault_t vmf_insert_mixed_prot(struct vm_area_struct *vma, unsigned long addr,
 	return _kcl_vmf_insert_mixed_prot(vma, addr, pfn, pgprot);
 }
 #endif /* HAVE_VMF_INSERT_MIXED_PROT */
+
+#ifndef HAVE_VMF_INSERT_PFN_PROT
+vm_fault_t _kcl_vmf_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
+					unsigned long pfn, pgprot_t pgprot);
+vm_fault_t vmf_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
+					unsigned long pfn, pgprot_t pgprot)
+{
+	return _kcl_vmf_insert_pfn_prot(vma, addr, pfn, pgprot);
+}
+#endif /* HAVE_VMF_INSERT_PFN_PROT */
 #endif
