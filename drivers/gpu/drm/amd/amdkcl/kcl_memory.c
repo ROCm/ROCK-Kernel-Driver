f90f816b5af4d85217a7792af1f73b8efc2d4768
Flora Cui <flora.cui@amd.com>
2020-04-15 18:07:17 +0800

    drm/amdkcl: fake a kcl copy of vmf_insert_pfn_prot()
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_memory.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_memory.c
@@ -13,3 +13,31 @@ vm_fault_t _kcl_vmf_insert_mixed_prot(struct vm_area_struct *vma, unsigned long 
 }
 EXPORT_SYMBOL(_kcl_vmf_insert_mixed_prot);
 #endif
+
+#ifndef HAVE_VMF_INSERT_PFN_PROT
+#ifndef HAVE_VM_INSERT_PFN_PROT
+int vm_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
+                        unsigned long pfn, pgprot_t pgprot)
+{
+	struct vm_area_struct cvma = *vma;
+
+	cvma.vm_page_prot = pgprot;
+
+	return vm_insert_pfn(&cvma, addr, pfn);
+}
+#endif
+
+vm_fault_t _kcl_vmf_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
+		unsigned long pfn, pgprot_t pgprot)
+{
+	int err = vm_insert_pfn_prot(vma, addr, pfn, pgprot);
+
+	if (err == -ENOMEM)
+		return VM_FAULT_OOM;
+	if (err < 0 && err != -EBUSY)
+		return VM_FAULT_SIGBUS;
+
+	return VM_FAULT_NOPAGE;
+}
+EXPORT_SYMBOL(_kcl_vmf_insert_pfn_prot);
+#endif
