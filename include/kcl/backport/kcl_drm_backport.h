0701802a233e067d47262e58cf03a8e723ec049e
Flora Cui <flora.cui@amd.com>
2020-04-18 19:46:03 +0800

    drm/amdkcl: workaroud for shared address space with dma bufs
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- include/kcl/backport/kcl_drm_backport.h
+++ include/kcl/backport/kcl_drm_backport.h
@@ -80,4 +80,12 @@ int _kcl_drm_syncobj_find_fence(struct drm_file *file_private,
 #define AMDKCL_AMDGPU_DMABUF_OPS
 #endif
 
+/*
+ * commit v5.4-rc4-1120-gb3fac52c5193
+ * drm: share address space for dma bufs
+ */
+#if DRM_VERSION_CODE < DRM_VERSION(5, 5, 0)
+#define AMDKCL_DMA_BUF_SHARE_ADDR_SPACE
+#endif
+
 #endif/*AMDKCL_DRM_BACKPORT_H*/
