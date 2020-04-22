b7d2a6ef7a1becd6871d2c4df6e636a78e126558
Yifan Zhang <Yifan1.Zhang@amd.com>
2020-04-22 21:48:44 +0800

    drm/amdkcl: add HAVE_VM_INSERT_PFN_PROT
    Change-Id: I926c12f32886deda7969dfdf8263637137db4dce
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_memory.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_memory.c
@@ -13,7 +13,19 @@ vm_fault_t _kcl_vmf_insert_mixed_prot(struct vm_area_struct *vma, unsigned long 
 EXPORT_SYMBOL(_kcl_vmf_insert_mixed_prot);
 #endif
 
-#ifndef
+#ifndef HAVE_VMF_INSERT_PFN_PROT
+#ifndef HAVE_VM_INSERT_PFN_PROT
+int vm_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
+                        unsigned long pfn, pgprot_t pgprot)
+{
+	struct vm_area_struct cvma = *vma;
+
+	cvma.vm_page_prot = pgprot;
+
+	return vm_insert_pfn(&cvma, addr, pfn);
+}
+#endif
+
 vm_fault_t _kcl_vmf_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
 		unsigned long pfn, pgprot_t pgprot)
 {
