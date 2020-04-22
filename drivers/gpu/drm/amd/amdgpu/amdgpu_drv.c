de9e6f8ebe7ff15be17950a87ee319e139f4e558
Yifan Zhang <Yifan1.Zhang@amd.com>
2020-04-22 21:26:01 +0800

    drm/amdkcl: test drm_device->driver_features
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -1196,8 +1196,17 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 
 	kcl_drm_vma_offset_manager_init(ddev->vma_offset_manager);
 
+#ifdef HAVE_DRM_DEVICE_DRIVER_FEATURES
 	if (!supports_atomic)
 		ddev->driver_features &= ~DRIVER_ATOMIC;
+#else
+	/* warn the user if they mix atomic and non-atomic capable GPUs */
+	if ((kms_driver.driver_features & DRIVER_ATOMIC) && !supports_atomic)
+		DRM_ERROR("Mixing atomic and non-atomic capable GPUs!\n");
+	/* support atomic early so the atomic debugfs stuff gets created */
+	if (supports_atomic)
+		kms_driver.driver_features |= DRIVER_ATOMIC;
+#endif
 
 	kcl_pci_create_measure_file(pdev);
 	kcl_pci_configure_extended_tags(pdev);
@@ -1546,9 +1555,12 @@ int amdgpu_file_to_fpriv(struct file *filp, struct amdgpu_fpriv **fpriv)
 
 static struct drm_driver kms_driver = {
 	.driver_features =
-	    DRIVER_ATOMIC |
-	    DRIVER_GEM |
-	    DRIVER_RENDER | DRIVER_MODESET
+	    0
+#ifdef HAVE_DRM_DEVICE_DRIVER_FEATURES
+	    | DRIVER_ATOMIC
+#endif /* HAVE_DRM_DEVICE_DRIVER_FEATURES */
+	    | DRIVER_GEM
+	    | DRIVER_RENDER | DRIVER_MODESET
 #if defined(HAVE_CHUNK_ID_SYNOBJ_IN_OUT)
 	    | DRIVER_SYNCOBJ
 #endif
