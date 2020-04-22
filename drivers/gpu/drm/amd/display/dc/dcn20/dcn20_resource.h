bf81e4d66db10667f139d0a7b6724311595e41da
Sung Lee <sung.lee@amd.com>
2020-04-22 18:07:54 -0400

    drm/amd/display: Fail validation if building scaling params fails
    [WHY & HOW]
    If building scaling parameters fails, validation
    should also fail.
    
    Signed-off-by: Sung Lee <sung.lee@amd.com>
    Reviewed-by: Dmytro Laktyushkin <Dmytro.Laktyushkin@amd.com>
    Acked-by: Aurabindo Pillai <aurabindo.pillai@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn20/dcn20_resource.h
+++ drivers/gpu/drm/amd/display/dc/dcn20/dcn20_resource.h
@@ -131,7 +131,7 @@ void dcn20_release_dsc(struct resource_context *res_ctx,
 #ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 bool dcn20_validate_dsc(struct dc *dc, struct dc_state *new_ctx);
 #endif
-void dcn20_split_stream_for_mpc(
+bool dcn20_split_stream_for_mpc(
 		struct resource_context *res_ctx,
 		const struct resource_pool *pool,
 		struct pipe_ctx *primary_pipe,
