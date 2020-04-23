27f0f8e68f3b11175fbcbf6e6ef3f86e8236d3c1
Yifan Zhang <Yifan1.Zhang@amd.com>
2020-04-23 10:24:15 +0800

    drm/amdkcl: adjust the order of headers to be compatible with distros
    Change-Id: If34938b16a9a68bb3019656e3bd27c97c53f4842
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -2,24 +2,24 @@
 #define AMDGPU_BACKPORT_H
 
 #include <linux/version.h>
-#include "kcl/kcl_backport_amdgpu.h"
-#include <kcl/kcl_mn.h>
-#include <kcl/reservation.h>
-#include <kcl/kcl_drm_print.h>
-#include <kcl/kcl_fs.h>
-#include <kcl/kcl_amdgpu.h>
-#include <kcl/kcl_drm_backport.h>
-#include <kcl/kcl_acpi.h>
-#include <kcl/kcl_bitops.h>
-#include <kcl/kcl_device.h>
-#include <kcl/kcl_device_cgroup_backport.h>
-#include <kcl/kcl_kref.h>
-#include <kcl/kcl_dma_mapping.h>
 /*
  * linux/reservation.h must before all drm headers,
  * otherwise struct reservation_object is undefined.
  */
+#include <kcl/reservation.h>
 #include <kcl/kcl_fence_backport.h>
+#include <kcl/kcl_drm_backport.h>
+#include <kcl/kcl_mn.h>
+#include <kcl/kcl_drm_print.h>
+#include <kcl/kcl_fs.h>
+#include <kcl/kcl_mm_backport.h>
+#include <kcl/kcl_kref.h>
+#include <kcl/kcl_amdgpu.h>
+#include <kcl/kcl_acpi.h>
+#include <kcl/kcl_bitops.h>
+#include <kcl/kcl_device.h>
+#include <kcl/kcl_device_cgroup_backport.h>
+#include <kcl/kcl_dma_mapping.h>
 #include <kcl/kcl_drm_dp_mst_helper.h>
 #include <kcl/kcl_drm_cache_backport.h>
 #include <kcl/kcl_drm_connector.h>
@@ -31,7 +31,6 @@
 #include <kcl/kcl_io.h>
 #include <kcl/kcl_kernel.h>
 #include <kcl/kcl_kthread_backport.h>
-#include <kcl/kcl_mm_backport.h>
 #include <kcl/kcl_mmu_notifier.h>
 #include <kcl/kcl_suspend.h>
 #include <kcl/kcl_pagemap.h>
@@ -42,7 +41,9 @@
 #include <kcl/kcl_uaccess_backport.h>
 #include <kcl/kcl_vga_switcheroo_backport.h>
 #include <kcl/kcl_types.h>
+#include <kcl/kcl_amdgpu.h>
 #include <kcl/kcl_drm_dp_mst_helper_backport.h>
+#include "kcl/kcl_backport_amdgpu.h"
 #include <kcl/kcl_overflow.h>
 #include <kcl/kcl_seq_file.h>
 #include <kcl/kcl_ptrace.h>
