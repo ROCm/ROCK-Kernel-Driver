efaf1e870b4393c3053f610e41b4c06a98aea296
Felix Kuehling <Felix.Kuehling@amd.com>
2020-04-23 02:23:36 -0400

    drm/amdkfd: Add RDMA and PeerDirect support
    Both are interfaces between kernel drivers for direct peer-memory
    access between different devices, e.g. NICs and GPUs. The PeerDirect
    API is defined by Mellanox and supported by their non-upstream NIC
    driver. RDMA is an AMD interface and provides the implementation
    underneath the PeerDirect wrapper. It currently has no external users.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_priv.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_priv.h
@@ -382,7 +382,6 @@ struct kfd_bo {
 	void *mem;
 	struct interval_tree_node it;
 	struct kfd_dev *dev;
-	struct list_head cb_data_head;
 	struct kfd_ipc_obj *kfd_ipc_obj;
 	/* page-aligned VA address */
 	uint64_t cpuva;
@@ -1540,6 +1539,10 @@ int kfd_send_exception_to_runtime(struct kfd_process *p,
 				unsigned int queue_id,
 				uint64_t error_reason);
 bool kfd_is_locked(void);
+
+/* PeerDirect support */
+void kfd_init_peer_direct(void);
+void kfd_close_peer_direct(void);
 
 /* IPC Support */
 int kfd_ipc_init(void);
