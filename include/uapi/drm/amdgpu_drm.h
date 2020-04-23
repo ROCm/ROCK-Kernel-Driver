aa31b0992a3dd409a6c288b6ffc3fca22e2e95d0
Felix Kuehling <Felix.Kuehling@amd.com>
2020-04-23 03:06:45 -0400

    drm/amdgpu: Workaround incorrect VRAM width reported on Fiji
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- include/uapi/drm/amdgpu_drm.h
+++ include/uapi/drm/amdgpu_drm.h
@@ -1021,6 +1021,8 @@ struct drm_amdgpu_info_firmware {
 #define AMDGPU_VRAM_TYPE_DDR4  8
 #define AMDGPU_VRAM_TYPE_GDDR6 9
 
+#define AMDGPU_VRAM_TYPE_HBM_WIDTH 4096
+
 struct drm_amdgpu_info_device {
 	/** PCI Device ID */
 	__u32 device_id;
