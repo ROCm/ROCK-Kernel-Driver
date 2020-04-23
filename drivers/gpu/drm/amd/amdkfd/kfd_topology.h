cb058cc5e6961d60332a2a3a2096efe77cafbb5c
Felix Kuehling <Felix.Kuehling@amd.com>
2020-04-23 04:07:48 -0400

    drm/amdkfd: Report used GPU memory
    This change has been rejected upstream. A better solution is needed.
    Used memory being dynamic doesn't belong in the topology tree, which
    contains otherwise static information.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_topology.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_topology.h
@@ -83,7 +83,8 @@ struct kfd_mem_properties {
 	uint32_t		mem_clk_max;
 	struct kfd_node		*gpu;
 	struct kobject		*kobj;
-	struct attribute	attr;
+	struct attribute	attr_props;
+	struct attribute	attr_used;
 };
 
 #define CACHE_SIBLINGMAP_SIZE 64
