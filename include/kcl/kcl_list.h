f41d64a506dd936dfb9ba37efba41fd614786495
Yang Xiong <Yang.Xiong@amd.com>
2020-04-28 23:22:10 +0800

    drm/amdkcl: Test whether list_rotate_to_front() and list_is_first() is available
    This is a squash of:
    b6026ffc104a drm/amdkcl: minor refactor for indent and comment style
    588246ddef5a drm/amdkcl: Test whether list_rotate_to_front() and
    list_is_first() is available
    
    Signed-off-by: Yang Xiong <Yang.Xiong@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    
    Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_list.h
+++ include/kcl/kcl_list.h
@@ -2,20 +2,38 @@
 #ifndef AMDKCL_LIST_H
 #define AMDKCL_LIST_H
 
-#if !defined(HAVE_LIST_BULK_MOVE_TAIL)
 #include <linux/list.h>
+
+/* Copied from include/linux/list.h */
+#if !defined(HAVE_LIST_BULK_MOVE_TAIL)
 static inline void list_bulk_move_tail(struct list_head *head,
 				       struct list_head *first,
 				       struct list_head *last)
 {
-		first->prev->next = last->next;
-		last->next->prev = first->prev;
+	first->prev->next = last->next;
+	last->next->prev = first->prev;
 
-		head->prev->next = first;
-		first->prev = head->prev;
+	head->prev->next = first;
+	first->prev = head->prev;
 
-		last->next = head;
-		head->prev = last;
+	last->next = head;
+	head->prev = last;
+}
+#endif
+
+#if !defined(HAVE_LIST_ROTATE_TO_FRONT)
+static inline void list_rotate_to_front(struct list_head *list,
+					struct list_head *head)
+{
+	list_move_tail(head, list);
+}
+#endif
+
+#if !defined(HAVE_LIST_IS_FIRST)
+static inline int list_is_first(const struct list_head *list,
+					const struct list_head *head)
+{
+	return list->prev == head;
 }
 #endif
 
