ed044aace1918ed57fafe7f6c84572151183955a
Flora Cui <flora.cui@amd.com>
2020-05-06 15:17:01 +0800

    drm/amdkcl: drop legacy test for kallsyms_lookup_name()
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_common.h
+++ drivers/gpu/drm/amd/amdkcl/kcl_common.h
@@ -8,24 +8,12 @@
 #include <linux/kallsyms.h>
 #include <linux/bug.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
-extern unsigned long (*_kcl_kallsyms_lookup_name)(const char *name);
-#endif
-static inline unsigned long kcl_kallsyms_lookup_name(const char *name)
-{
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
-	return _kcl_kallsyms_lookup_name(name);
-#else
-	return kallsyms_lookup_name(name);
-#endif
-}
-
 static inline void *amdkcl_fp_setup(const char *symbol, void *fp_stup)
 {
 	unsigned long addr;
 	void *fp = NULL;
 
-	addr = kcl_kallsyms_lookup_name(symbol);
+	addr = kallsyms_lookup_name(symbol);
 	if (addr == 0) {
 		fp = fp_stup;
 		if (fp != NULL)
