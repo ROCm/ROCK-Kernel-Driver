90dc124b25d2b1f5da211b35d09f8f130b84e9ba
Evan Quan <evan.quan@amd.com>
2020-05-26 16:50:55 +0800

    drm/amd/powerplay: stop thermal IRQs on suspend
    Added missing thermal IRQs disablement on suspend.
    
    Change-Id: I959a1d56930de434cc8534334220d3faeadf79f8
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/amdgpu_smu.c
+++ drivers/gpu/drm/amd/powerplay/amdgpu_smu.c
@@ -1537,6 +1537,12 @@ static int smu_suspend(void *handle)
 
 	smu_i2c_eeprom_fini(smu, &adev->pm.smu_i2c);
 
+	ret = smu_stop_thermal_control(smu);
+	if (ret) {
+		pr_warn("Fail to stop thermal control!\n");
+		return ret;
+	}
+
 	ret = smu_disable_dpm(smu);
 	if (ret)
 		return ret;
