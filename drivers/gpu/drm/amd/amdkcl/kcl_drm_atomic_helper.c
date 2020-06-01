fa349f4b76c08262a51e3b206f5c1a7538716cf1
Flora Cui <flora.cui@amd.com>
2020-06-01 12:48:26 +0800

    drm/amdkcl: fake drm_atomic_helper_plane_reset
    to commit v4.19-rc1-206-ge267364a6e1b.
    
    v2: 69a65a9c531c drm/amdkcl: fix license for kcl drm part
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

---
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm_atomic_helper.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2018 Intel Corp.
+ * Copyright (C) 2014 Red Hat
+ * Copyright (C) 2014 Intel Corp.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER(S) OR AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Authors:
+ * Rob Clark <robdclark@gmail.com>
+ * Daniel Vetter <daniel.vetter@ffwll.ch>
+ */
+#include <kcl/kcl_drm_atomic_helper.h>
+#include <kcl/kcl_drm.h>
+
+#ifdef AMDKCL__DRM_ATOMIC_HELPER_PLANE_RESET
+void _kcl__drm_atomic_helper_plane_reset(struct drm_plane *plane,
+							struct drm_plane_state *state)
+{
+	state->plane = plane;
+	state->rotation = DRM_MODE_ROTATE_0;
+
+#ifdef DRM_BLEND_ALPHA_OPAQUE
+	state->alpha = DRM_BLEND_ALPHA_OPAQUE;
+#endif
+#ifdef DRM_MODE_BLEND_PREMULTI
+	state->pixel_blend_mode = DRM_MODE_BLEND_PREMULTI;
+#endif
+
+	plane->state = state;
+}
+EXPORT_SYMBOL(_kcl__drm_atomic_helper_plane_reset);
+#endif
