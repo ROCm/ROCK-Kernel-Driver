79ee29e2e3253d9a3cf42e9ec4ba49fb584c7eda
Coly Li <colyli@suse.de>
2020-06-08 15:42:44 +0800

    raid5: remove gfp flags from scribble_alloc() (bsc#1166985).
    suse-commit: d35a2bbbf9421c27a33d2eb9d11d4b4f399a6802

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -46,6 +46,7 @@
 #include <linux/slab.h>
 #include <linux/ratelimit.h>
 #include <linux/nodemask.h>
+#include <linux/sched/mm.h>
 
 #include <trace/events/block.h>
 #include <linux/list_sort.h>
@@ -2228,14 +2229,18 @@ static int grow_stripes(struct r5conf *conf, int num)
  * of the P and Q blocks.
  */
 static int scribble_alloc(struct raid5_percpu *percpu,
-			  int num, int cnt, gfp_t flags)
+			  int num, int cnt)
 {
 	size_t obj_size =
 		sizeof(struct page *) * (num+2) +
 		sizeof(addr_conv_t) * (num+2);
 	void *scribble;
+	unsigned int noio_flag;
 
-	scribble = kvmalloc_array(cnt, obj_size, flags);
+	/* use local noio_flag to avoid kabi breaking */
+	noio_flag = memalloc_noio_save();
+	scribble = kvmalloc_array(cnt, obj_size, GFP_KERNEL);
+	memalloc_noio_restore(noio_flag);
 	if (!scribble)
 		return -ENOMEM;
 
@@ -2267,8 +2272,7 @@ static int resize_chunks(struct r5conf *conf, int new_disks, int new_sectors)
 
 		percpu = per_cpu_ptr(conf->percpu, cpu);
 		err = scribble_alloc(percpu, new_disks,
-				     new_sectors / STRIPE_SECTORS,
-				     GFP_NOIO);
+				     new_sectors / STRIPE_SECTORS);
 		if (err)
 			break;
 	}
@@ -6762,8 +6766,7 @@ static int alloc_scratch_buffer(struct r5conf *conf, struct raid5_percpu *percpu
 			       conf->previous_raid_disks),
 			   max(conf->chunk_sectors,
 			       conf->prev_chunk_sectors)
-			   / STRIPE_SECTORS,
-			   GFP_KERNEL)) {
+			   / STRIPE_SECTORS)) {
 		free_scratch_buffer(conf, percpu);
 		return -ENOMEM;
 	}
