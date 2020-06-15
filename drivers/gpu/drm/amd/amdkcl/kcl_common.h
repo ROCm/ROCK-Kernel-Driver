26b52ab746a3a313eb1af2b750bf61e6b74d97e9
Andreas Gravgaard Andersen <andreasga22@gmail.com>
2020-06-15 12:54:45 +0200

    amd/amdkcl: avoid kallsyms_lookup_name for 5.7+

--- drivers/gpu/drm/amd/amdkcl/kcl_common.h
+++ drivers/gpu/drm/amd/amdkcl/kcl_common.h
@@ -8,12 +8,14 @@
 #include <linux/kallsyms.h>
 #include <linux/bug.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33) &&                           \
+	LINUX_VERSION_CODE > KERNEL_VERSION(5, 7, 0)
 extern unsigned long (*_kcl_kallsyms_lookup_name)(const char *name);
 #endif
 static inline unsigned long kcl_kallsyms_lookup_name(const char *name)
 {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33) &&                           \
+	LINUX_VERSION_CODE > KERNEL_VERSION(5, 7, 0)
 	return _kcl_kallsyms_lookup_name(name);
 #else
 	return kallsyms_lookup_name(name);
@@ -29,9 +31,13 @@ static inline void *amdkcl_fp_setup(const char *symbol, void *fp_stup)
 	if (addr == 0) {
 		fp = fp_stup;
 		if (fp != NULL)
-			printk_once(KERN_WARNING "Warning: fail to get symbol %s, replace it with kcl stub\n", symbol);
+			printk_once(
+				KERN_WARNING
+				"Warning: fail to get symbol %s, replace it with kcl stub\n",
+				symbol);
 		else {
-			printk_once(KERN_ERR "Error: fail to get symbol %s\n", symbol);
+			printk_once(KERN_ERR "Error: fail to get symbol %s\n",
+				    symbol);
 			BUG();
 		}
 	} else {
