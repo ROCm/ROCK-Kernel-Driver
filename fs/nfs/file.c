a1442aac6e7a31ecbe6eaac82e89e205addbb193
NeilBrown <neilb@suse.de>
2020-06-15 15:28:59 +1000

    work around mvfs bug (bsc#1162063).
    suse-commit: 5a1f922986741270c5f13f1c68e43e44c67c6862

--- fs/nfs/file.c
+++ fs/nfs/file.c
@@ -80,6 +80,11 @@ nfs_file_open(struct inode *inode, struct file *filp)
 int
 nfs_file_release(struct inode *inode, struct file *filp)
 {
+	if (!nfs_file_open_context(filp)) {
+		dprintk("NFS: buggy mvfs module called fput after failed open\n");
+		return 0;
+	}
+
 	dprintk("NFS: release(%pD2)\n", filp);
 
 	if (filp->f_mode & FMODE_WRITE)
