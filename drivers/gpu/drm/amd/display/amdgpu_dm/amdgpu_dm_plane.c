976a79f7fac04148e725e860a8eddb4f3f0c5d5c
Flora Cui <flora.cui@amd.com>
2020-06-16 10:55:00 +0800

    drm/amdkcl: add test for drm_plane_helper_funcs->atomic_async_check
    the 2 callbacks are introduced in commit
    v4.12-rc7-1335-gfef9df8b5945("drm/atomic: initial support for
    asynchronous plane update")
    
    It's a squash of
    drm/amdkcl: [4.14] add kcl for "add fast path for cursor plane updates"
        Reviewed-by: Prike Liang <Prike.Liang@amd.com>
        Signed-off-by: Tianci Yin <tianci.yin@amd.com>
        Signed-off-by: Jack Gui <Jack.Gui@amd.com>
        Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yang Xiong <Yang.Xiong@amd.com>
    Change-Id: Iec3f145247132537e016e6895ade88eedbfb0ec2
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -1178,6 +1178,7 @@ static int dm_plane_atomic_check(struct drm_plane *plane,
 	return -EINVAL;
 }
 
+#ifdef HAVE_STRUCT_DRM_PLANE_HELPER_FUNCS_ATOMIC_ASYNC_CHECK
 static int dm_plane_atomic_async_check(struct drm_plane *plane,
 				       struct drm_atomic_state *state)
 {
@@ -1187,6 +1188,7 @@ static int dm_plane_atomic_async_check(struct drm_plane *plane,
 
 	return 0;
 }
+#endif
 
 static int get_cursor_position(struct drm_plane *plane, struct drm_crtc *crtc,
 			       struct dc_cursor_position *position)
@@ -1324,8 +1326,10 @@ static const struct drm_plane_helper_funcs dm_plane_helper_funcs = {
 	.prepare_fb = dm_plane_helper_prepare_fb,
 	.cleanup_fb = dm_plane_helper_cleanup_fb,
 	.atomic_check = dm_plane_atomic_check,
+#ifdef HAVE_STRUCT_DRM_PLANE_HELPER_FUNCS_ATOMIC_ASYNC_CHECK
 	.atomic_async_check = dm_plane_atomic_async_check,
 	.atomic_async_update = dm_plane_atomic_async_update
+#endif
 };
 
 static void dm_drm_plane_reset(struct drm_plane *plane)
