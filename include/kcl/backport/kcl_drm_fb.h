0a971924cf67758393c681ff55b5876a0f5c1d31
Flora Cui <flora.cui@amd.com>
2020-06-17 13:08:42 +0800

    drm/amdkcl: fake drm_helper_mode_fill_fb_struct()
    drm_helper_mode_fill_fb_struct() prototype change in commit
    v4.9-rc8-1643-ga3f913ca9892
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yang Xiong <Yang.Xiong@amd.com>

--- include/kcl/backport/kcl_drm_fb.h
+++ include/kcl/backport/kcl_drm_fb.h
@@ -28,4 +28,9 @@
 #if !defined(HAVE_DRM_FB_HELPER_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_PP)
 #define drm_fb_helper_remove_conflicting_pci_framebuffers _kcl_drm_fb_helper_remove_conflicting_pci_framebuffers
 #endif
+
+#ifndef HAVE_DRM_HELPER_MODE_FILL_FB_STRUCT_DEV
+#define drm_helper_mode_fill_fb_struct _kcl_drm_helper_mode_fill_fb_struct
+#endif
+
 #endif
