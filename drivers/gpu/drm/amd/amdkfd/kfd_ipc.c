bb52f7e99da1a50cc7303b101177962947c16f78
Felix Kuehling <Felix.Kuehling@amd.com>
2020-06-26 22:32:01 -0400

    drm/amdkfd: Move ipc_obj from kfd_bo to kgd_mem
    This should make IPC easier to upstream. struct kfd_bo does not exist
    upstream and its main purpose is to support RDMA, which is not
    upstreamable.
    
    struct kgd_mem exists upstream and is a KFD-specific buffer object
    structure that can be used to store a pointer to the ipc_obj. Adding
    another one layer of tracking in KFD just for IPC cannot be justified
    when upstreaming IPC.
    
    With this change, some of the IPC functionality moves into
    amdgpu_amdkfd_gpuvm.c with some calls back to kfd_ipc.c. Cleaned up and
    exported kfd_ipc_obj_put and kfd_ipc_store_insert.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Acked-and-tested-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_ipc.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_ipc.c
@@ -20,7 +20,6 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include <linux/dma-buf.h>
 #include <linux/slab.h>
 #include <linux/random.h>
 
@@ -42,7 +41,7 @@ static struct kfd_ipc_handles {
  */
 #define HANDLE_TO_KEY(sh) ((*(uint64_t *)sh) & KFD_IPC_HASH_TABLE_SIZE_MASK)
 
-static int ipc_store_insert(void *val, void *sh, struct kfd_ipc_obj **ipc_obj)
+int kfd_ipc_store_insert(struct dma_buf *dmabuf, struct kfd_ipc_obj **ipc_obj)
 {
 	struct kfd_ipc_obj *obj;
 
@@ -58,10 +57,8 @@ static int ipc_store_insert(void *val, void *sh, struct kfd_ipc_obj **ipc_obj)
 	 * storage happens at ipc_obj release time.
 	 */
 	kref_init(&obj->ref);
-	obj->data = val;
+	obj->dmabuf = dmabuf;
 	get_random_bytes(obj->share_handle, sizeof(obj->share_handle));
-
-	memcpy(sh, obj->share_handle, sizeof(obj->share_handle));
 
 	mutex_lock(&kfd_ipc_handles.lock);
 	hlist_add_head(&obj->node,
@@ -84,21 +81,23 @@ static void ipc_obj_release(struct kref *r)
 	hash_del(&obj->node);
 	mutex_unlock(&kfd_ipc_handles.lock);
 
-	dma_buf_put(obj->data);
+	dma_buf_put(obj->dmabuf);
 	kfree(obj);
 }
 
-struct kfd_ipc_obj *ipc_obj_get(struct kfd_ipc_obj *obj)
+static struct kfd_ipc_obj *ipc_obj_get(struct kfd_ipc_obj *obj)
 {
 	if (kref_get_unless_zero(&obj->ref))
 		return obj;
 	return NULL;
 }
 
-void ipc_obj_put(struct kfd_ipc_obj **obj)
+void kfd_ipc_obj_put(struct kfd_ipc_obj **obj)
 {
-	kref_put(&(*obj)->ref, ipc_obj_release);
-	*obj = NULL;
+	if (*obj) {
+		kref_put(&(*obj)->ref, ipc_obj_release);
+		*obj = NULL;
+	}
 }
 
 int kfd_ipc_init(void)
@@ -110,10 +109,10 @@ int kfd_ipc_init(void)
 
 static int kfd_import_dmabuf_create_kfd_bo(struct kfd_dev *dev,
 			  struct kfd_process *p,
-			  uint32_t gpu_id, struct dma_buf *dmabuf,
+			  uint32_t gpu_id,
+			  struct dma_buf *dmabuf, struct kfd_ipc_obj *ipc_obj,
 			  uint64_t va_addr, uint64_t *handle,
-			  uint64_t *mmap_offset,
-			  struct kfd_ipc_obj *ipc_obj)
+			  uint64_t *mmap_offset)
 {
 	int r;
 	void *mem;
@@ -135,7 +134,7 @@ static int kfd_import_dmabuf_create_kfd_bo(struct kfd_dev *dev,
 		goto err_unlock;
 	}
 
-	r = amdgpu_amdkfd_gpuvm_import_dmabuf(dev->kgd, dmabuf,
+	r = amdgpu_amdkfd_gpuvm_import_dmabuf(dev->kgd, dmabuf, ipc_obj,
 					va_addr, pdd->vm,
 					(struct kgd_mem **)&mem, &size,
 					mmap_offset);
@@ -143,8 +142,7 @@ static int kfd_import_dmabuf_create_kfd_bo(struct kfd_dev *dev,
 		goto err_unlock;
 
 	idr_handle = kfd_process_device_create_obj_handle(pdd, mem,
-							  va_addr, size, 0, 0,
-							  ipc_obj);
+							  va_addr, size, 0, 0);
 	if (idr_handle < 0) {
 		r = -EFAULT;
 		goto err_free;
@@ -175,9 +173,8 @@ int kfd_ipc_import_dmabuf(struct kfd_dev *dev,
 	if (!dmabuf)
 		return -EINVAL;
 
-	r = kfd_import_dmabuf_create_kfd_bo(dev, p, gpu_id, dmabuf,
-					    va_addr, handle, mmap_offset,
-					    NULL);
+	r = kfd_import_dmabuf_create_kfd_bo(dev, p, gpu_id, dmabuf, NULL,
+					    va_addr, handle, mmap_offset);
 	dma_buf_put(dmabuf);
 	return r;
 }
@@ -207,18 +204,18 @@ int kfd_ipc_import_handle(struct kfd_dev *dev, struct kfd_process *p,
 	if (!found)
 		return -EINVAL;
 
-	pr_debug("Found ipc_dma_buf: %p\n", found->data);
+	pr_debug("Found ipc_dma_buf: %p\n", found->dmabuf);
 
-	r = kfd_import_dmabuf_create_kfd_bo(dev, p, gpu_id, found->data,
-					    va_addr, handle, mmap_offset,
-					    found);
+	r = kfd_import_dmabuf_create_kfd_bo(dev, p, gpu_id,
+					    found->dmabuf, found,
+					    va_addr, handle, mmap_offset);
 	if (r)
 		goto error_unref;
 
 	return r;
 
 error_unref:
-	ipc_obj_put(&found);
+	kfd_ipc_obj_put(&found);
 	return r;
 }
 
@@ -226,9 +223,9 @@ int kfd_ipc_export_as_handle(struct kfd_dev *dev, struct kfd_process *p,
 			     uint64_t handle, uint32_t *ipc_handle)
 {
 	struct kfd_process_device *pdd = NULL;
-	struct kfd_ipc_obj *obj;
+	struct kfd_ipc_obj *ipc_obj;
 	struct kfd_bo *kfd_bo = NULL;
-	struct dma_buf *dmabuf;
+	struct kgd_mem *mem;
 	int r;
 
 	if (!dev || !ipc_handle)
@@ -249,23 +246,15 @@ int kfd_ipc_export_as_handle(struct kfd_dev *dev, struct kfd_process *p,
 		pr_err("Failed to get bo");
 		return -EINVAL;
 	}
-	if (kfd_bo->kfd_ipc_obj) {
-		memcpy(ipc_handle, kfd_bo->kfd_ipc_obj->share_handle,
-		       sizeof(kfd_bo->kfd_ipc_obj->share_handle));
-		return 0;
-	}
+	mem = (struct kgd_mem *)kfd_bo->mem;
 
-	r = amdgpu_amdkfd_gpuvm_export_dmabuf(dev->kgd, pdd->vm,
-					(struct kgd_mem *)kfd_bo->mem,
-					&dmabuf);
+	r = amdgpu_amdkfd_gpuvm_export_ipc_obj(dev->kgd, pdd->vm, mem,
+					       &ipc_obj);
 	if (r)
 		return r;
 
-	r = ipc_store_insert(dmabuf, ipc_handle, &obj);
-	if (r)
-		return r;
-
-	kfd_bo->kfd_ipc_obj = obj;
+	memcpy(ipc_handle, ipc_obj->share_handle,
+	       sizeof(ipc_obj->share_handle));
 
 	return r;
 }
