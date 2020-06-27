1915c61ab49fbceddfb9cc302207d0031e94571e
Felix Kuehling <Felix.Kuehling@amd.com>
2020-06-26 22:32:01 -0400

    drm/amdkfd: Move ipc_obj from kfd_bo to kgd_mem
    This should make IPC easier to upstream. struct kfd_bo does not exist
    upstream and its main purpose is to support RDMA, which is not
    upstreamable.
    
    struct kgd_mem exists upstream and is a KFD-specific buffer object
    structure that can be used to store a pointer to the ipc_obj. Adding
    another one layer of tracking in KFD just for IPC cannot be justified
    when upstreaming IPC.
    
    With this change, some of the IPC functionality moves into
    amdgpu_amdkfd_gpuvm.c with some calls back to kfd_ipc.c. Cleaned up and
    exported kfd_ipc_obj_put and kfd_ipc_store_insert.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Acked-and-tested-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -29,6 +29,7 @@
 #include "amdgpu_vm.h"
 #include "amdgpu_amdkfd.h"
 #include "amdgpu_dma_buf.h"
+#include "kfd_ipc.h"
 #include <uapi/linux/kfd_ioctl.h>
 
 /* BO flag to indicate a KFD userptr BO */
@@ -1373,6 +1374,9 @@ int amdgpu_amdkfd_gpuvm_free_memory_of_gpu(
 			*size = 0;
 	}
 
+	/* Unreference the ipc_obj if applicable */
+	kfd_ipc_obj_put(&mem->ipc_obj);
+
 	/* Free the BO*/
 	drm_gem_object_put(&mem->bo->tbo.base);
 	mutex_destroy(&mem->lock);
@@ -1831,6 +1835,7 @@ void amdgpu_amdkfd_gpuvm_unpin_put_sg_table(
 
 int amdgpu_amdkfd_gpuvm_import_dmabuf(struct kgd_dev *kgd,
 				      struct dma_buf *dma_buf,
+				      struct kfd_ipc_obj *ipc_obj,
 				      uint64_t va, void *vm,
 				      struct kgd_mem **mem, uint64_t *size,
 				      uint64_t *mmap_offset)
@@ -1878,6 +1883,7 @@ int amdgpu_amdkfd_gpuvm_import_dmabuf(struct kgd_dev *kgd,
 
 	drm_gem_object_get(&bo->tbo.base);
 	(*mem)->bo = bo;
+	(*mem)->ipc_obj = ipc_obj;
 	(*mem)->va = va;
 	(*mem)->domain = (bo->preferred_domains & AMDGPU_GEM_DOMAIN_VRAM) ?
 		AMDGPU_GEM_DOMAIN_VRAM : AMDGPU_GEM_DOMAIN_GTT;
@@ -1890,22 +1896,40 @@ int amdgpu_amdkfd_gpuvm_import_dmabuf(struct kgd_dev *kgd,
 	return 0;
 }
 
-int amdgpu_amdkfd_gpuvm_export_dmabuf(struct kgd_dev *kgd, void *vm,
-				      struct kgd_mem *mem,
-				      struct dma_buf **dmabuf)
+int amdgpu_amdkfd_gpuvm_export_ipc_obj(struct kgd_dev *kgd, void *vm,
+				       struct kgd_mem *mem,
+				       struct kfd_ipc_obj **ipc_obj)
 {
 	struct amdgpu_device *adev = NULL;
+	struct dma_buf *dmabuf;
+	int r = 0;
 
-	if (!dmabuf || !kgd || !vm || !mem)
+	if (!kgd || !vm || !mem)
 		return -EINVAL;
 
 	adev = get_amdgpu_device(kgd);
+	mutex_lock(&mem->lock);
 
-	*dmabuf = amdgpu_gem_prime_export(&mem->bo->tbo.base, 0);
-	if (IS_ERR(*dmabuf))
-		return -EINVAL;
+	if (mem->ipc_obj) {
+		*ipc_obj = mem->ipc_obj;
+		goto unlock_out;
+	}
 
-	return 0;
+	dmabuf = amdgpu_gem_prime_export(&mem->bo->tbo.base, 0);
+	if (IS_ERR(dmabuf)) {
+		r = PTR_ERR(dmabuf);
+		goto unlock_out;
+	}
+
+	r = kfd_ipc_store_insert(dmabuf, &mem->ipc_obj);
+	if (r)
+		dma_buf_put(dmabuf);
+	else
+		*ipc_obj = mem->ipc_obj;
+
+unlock_out:
+	mutex_unlock(&mem->lock);
+	return r;
 }
 
 /* Evict a userptr BO by stopping the queues if necessary
--- drivers/gpu/drm/amd/amdkfd/kfd_process.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -32,7 +32,6 @@
 #include <linux/compat.h>
 #include <linux/mman.h>
 #include <linux/file.h>
-#include "kfd_ipc.h"
 #include <linux/pm_runtime.h>
 #include "amdgpu_amdkfd.h"
 #include "amdgpu.h"
@@ -544,7 +543,7 @@ static int kfd_process_alloc_gpuvm(struct kfd_process_device *pdd,
 	 * created and the ioctls have not had the chance to run.
 	 */
 	handle = kfd_process_device_create_obj_handle(
-			pdd, mem, gpu_va, size, 0, mem_type, NULL);
+			pdd, mem, gpu_va, size, 0, mem_type);
 
 	if (handle < 0) {
 		err = handle;
@@ -1357,8 +1356,7 @@ bool kfd_has_process_device_data(struct kfd_process *p)
 int kfd_process_device_create_obj_handle(struct kfd_process_device *pdd,
 					void *mem, uint64_t start,
 					uint64_t length, uint64_t cpuva,
-					unsigned int mem_type,
-					struct kfd_ipc_obj *ipc_obj)
+					unsigned int mem_type)
 {
 	int handle;
 	struct kfd_bo *buf_obj;
@@ -1377,7 +1375,6 @@ int kfd_process_device_create_obj_handle(struct kfd_process_device *pdd,
 
 	buf_obj->mem = mem;
 	buf_obj->dev = pdd->dev;
-	buf_obj->kfd_ipc_obj = ipc_obj;
 	buf_obj->cpuva = cpuva;
 	buf_obj->mem_type = mem_type;
 
@@ -1454,9 +1451,6 @@ void kfd_process_device_remove_obj_handle(struct kfd_process_device *pdd,
 		return;
 
 	buf_obj = kfd_process_device_find_bo(pdd, handle);
-
-	if (buf_obj->kfd_ipc_obj)
-		ipc_obj_put(&buf_obj->kfd_ipc_obj);
 
 	idr_remove(&pdd->alloc_idr, handle);
 
