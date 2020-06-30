563c3157fe505cdbf99959b25297db1c7f976c9c
Joseph Greathouse <Joseph.Greathouse@amd.com>
2020-06-30 18:40:37 -0500

    drm/amdkfd: Prevent GWS+debugger on buggy firmware
    Some versions of MEC2 firmware do not correctly handle simultaneously
    setting up debugging on a process while having a GWS-enabled queue
    in that process. This could lead to CWSR failure and crashing the
    kernel driver.
    
    Prevent this situation from happening when we are on an affected
    firmware version by preventing both debugging and GWS-enabled queue
    from both being enabled at the same time. If the process has a GWS
    enabled queue, attempting to turn on debugging will fail with -EBUSY.
    If debugging is already enabled, attempting to create a GWS-enabled
    queue will fail with -EBUSY.
    
    v2: Use qpd.num_gws to track GWS usage, even with suspended queues.
    
    Signed-off-by: Joseph Greathouse <Joseph.Greathouse@amd.com>
    Reviewed-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Change-Id: Id4f1d12665fa6ac5d5402637cf9361bcb2dbfbc6

--- drivers/gpu/drm/amd/amdkfd/kfd_chardev.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_chardev.c
@@ -1491,6 +1491,7 @@ static int kfd_ioctl_alloc_queue_gws(struct file *filep,
 	struct kfd_ioctl_alloc_queue_gws_args *args = data;
 	struct queue *q;
 	struct kfd_dev *dev;
+	struct kfd_process_device *pdd;
 
 	mutex_lock(&p->mutex);
 	q = pqm_get_user_queue(&p->pqm, args->queue_id);
@@ -1502,6 +1503,12 @@ static int kfd_ioctl_alloc_queue_gws(struct file *filep,
 		goto out_unlock;
 	}
 
+	pdd = kfd_bind_process_to_device(dev, p);
+	if (IS_ERR(pdd)) {
+		retval = -ESRCH;
+		goto out_unlock;
+	}
+
 	if (!dev->gws) {
 		retval = -ENODEV;
 		goto out_unlock;
@@ -1509,6 +1516,11 @@ static int kfd_ioctl_alloc_queue_gws(struct file *filep,
 
 	if (dev->dqm->sched_policy == KFD_SCHED_POLICY_NO_HWS) {
 		retval = -ENODEV;
+		goto out_unlock;
+	}
+
+	if (dev->gws_debug_workaround && pdd->is_debugging_enabled) {
+		retval = -EBUSY;
 		goto out_unlock;
 	}
 
@@ -1790,6 +1802,11 @@ static int kfd_ioctl_dbg_set_debug_trap(struct file *filep,
 
 		if (!pdd) {
 			r = -EINVAL;
+			goto unlock_out;
+		}
+
+		if (dev->gws_debug_workaround && pdd->qpd.num_gws) {
+			r = -EBUSY;
 			goto unlock_out;
 		}
 
