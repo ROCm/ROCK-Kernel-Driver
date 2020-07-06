1e49bc44712946e390de66760f8b45ede193ac93
Yang Xiong <Yang.Xiong@amd.com>
2020-07-06 17:20:41 +0800

    drm/amdkcl: fix build error of drm_atomic_get_new_crtc_state undefined
    This kcl patch is caused by patch:
    drm/amd/display: clip plane rects in DM before passing into DC
    
    Change-Id: I0932a38c7d28db411b721fcb6303cefed51d8674
    Signed-off-by: Yang Xiong <Yang.Xiong@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -1180,9 +1180,9 @@ static int dm_plane_atomic_check(struct drm_plane *plane,
 	if (!dm_plane_state->dc_state)
 		return 0;
 
-	new_crtc_state =
-		drm_atomic_get_new_crtc_state(state,
-					      new_plane_state->crtc);
+	new_crtc_state = kcl_drm_atomic_get_new_crtc_state_before_commit(
+					state, new_plane_state->crtc);
+
 	if (!new_crtc_state)
 		return -EINVAL;
 
