73d96d265a8e10d1aff445ad86a4c5e3b9d65204
Filipe Manana <fdmanana@suse.com>
2020-07-23 16:41:41 +0100

    btrfs: drop argument tree from btrfs_lock_and_flush_ordered_range (bsc#1174438).
    suse-commit: fddc7a9e70d2327323f7418beb27cca00a9ab6d8

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -3276,7 +3276,7 @@ static inline void contiguous_readpages(struct extent_io_tree *tree,
 
 	ASSERT(tree == &inode->io_tree);
 
-	btrfs_lock_and_flush_ordered_range(tree, inode, start, end, NULL);
+	btrfs_lock_and_flush_ordered_range(inode, start, end, NULL);
 
 	for (index = 0; index < nr_pages; index++) {
 		__do_readpage(tree, pages[index], btrfs_get_extent, em_cached,
@@ -3299,7 +3299,7 @@ static int __extent_read_full_page(struct extent_io_tree *tree,
 
 	ASSERT(tree == &inode->io_tree);
 
-	btrfs_lock_and_flush_ordered_range(tree, inode, start, end, NULL);
+	btrfs_lock_and_flush_ordered_range(inode, start, end, NULL);
 
 	ret = __do_readpage(tree, page, get_extent, NULL, bio, mirror_num,
 			    bio_flags, read_flags, NULL);
