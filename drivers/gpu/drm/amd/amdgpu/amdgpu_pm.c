901386b10241d91fae22020a181a61e04e793e4a
Paweł Gronowski <me@woland.xyz>
2020-07-30 01:11:06 +0200

    drm/amdgpu: Fix regression in adjusting power table/profile
    Regression was introduced in commit 38e0c89a19fd
    ("drm/amdgpu: Fix NULL dereference in dpm sysfs handlers") which
    made the set_pp_od_clk_voltage and set_pp_power_profile_mode return
    -EINVAL for previously valid input. This was caused by an empty
    string (starting at the \0 character) being passed to the kstrtol.
    
    Signed-off-by: Paweł Gronowski <me@woland.xyz>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c
@@ -842,12 +842,14 @@ static ssize_t amdgpu_set_pp_od_clk_voltage(struct device *dev,
 		tmp_str++;
 	while (isspace(*++tmp_str));
 
-	while ((sub_str = strsep(&tmp_str, delimiter)) != NULL) {
+	while ((sub_str = strsep(&tmp_str, delimiter)) && *sub_str) {
 		ret = kstrtol(sub_str, 0, &parameter[parameter_size]);
 		if (ret)
 			return -EINVAL;
 		parameter_size++;
 
+		if (!tmp_str)
+			break;
 		while (isspace(*tmp_str))
 			tmp_str++;
 	}
@@ -1741,11 +1743,14 @@ static ssize_t amdgpu_set_pp_power_profile_mode(struct device *dev,
 			i++;
 		memcpy(buf_cpy, buf, count-i);
 		tmp_str = buf_cpy;
-		while ((sub_str = strsep(&tmp_str, delimiter)) != NULL) {
+		while ((sub_str = strsep(&tmp_str, delimiter)) && *sub_str) {
 			ret = kstrtol(sub_str, 0, &parameter[parameter_size]);
 			if (ret)
 				return -EINVAL;
 			parameter_size++;
+
+			if (!tmp_str)
+				break;
 			while (isspace(*tmp_str))
 				tmp_str++;
 		}
