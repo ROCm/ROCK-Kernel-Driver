78c5511d5b38069ffe66f1c27beaf4e866df9d2a
Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
2020-07-31 17:56:07 -0400

    drm/amdkcl: Enable HDCP Build by default
    Add HDCP config flag to the makefile
    
    It's a squash of
    drm/amdkcl: test whether drm_hdcp_update_content_protection is available
    drm/amdkcl: add kcl/kcl_drm_hdcp.h
    drm/amdkcl: test drm_hdcp.h for enabling hdcp
        Signed-off-by: Yang Xiong <Yang.Xiong@amd.com>
        Reviewed-by: Flora Cui <flora.cui@amd.com>
        Acked-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    drm/amdkcl: fix missing CONFIG_DRM_AMD_DC_HDCP check
        Signed-off-by: Rui Teng <rui.teng@amd.com>
        Reviewed-by: Jack Gui <Jack.Gui@amd.com>
    drm/amdkcl: move hdcp related stuff to kcl_drm_hdcp.c
    	and fix license
        Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>
        Signed-off-by: Flora Cui <flora.cui@amd.com>
        Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    
    Signed-off-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    Signed-off-by: Yang Xiong <Yang.Xiong@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Change-Id: Iecbc2d3cefce447cbb2a1cc10703fea226a469e5

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -54,5 +54,6 @@
 #include <kcl/backport/kcl_drm_dp_mst_helper_backport.h>
 #include <kcl/backport/kcl_drm_atomic_helper_backport.h>
 #include <kcl/kcl_drm_atomic.h>
+#include <kcl/kcl_drm_hdcp.h>
 
 #endif /* AMDGPU_BACKPORT_H */
