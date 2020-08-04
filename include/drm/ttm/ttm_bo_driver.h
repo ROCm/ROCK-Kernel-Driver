3df1ae3d7ab263487f6a6db8e0d093af97c369cb
Dave Airlie <airlied@redhat.com>
2020-08-04 12:56:24 +1000

    drm/ttm: drop priv pointer in memory manager
    This isn't needed anymore by any drivers.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Ben Skeggs <bskeggs@redhat.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200804025632.3868079-52-airlied@gmail.com

--- include/drm/ttm/ttm_bo_driver.h
+++ include/drm/ttm/ttm_bo_driver.h
@@ -130,7 +130,6 @@ struct ttm_resource_manager_func {
  * @default_caching: The default caching policy used for a buffer object
  * placed in this memory type if the user doesn't provide one.
  * @func: structure pointer implementing the range manager. See above
- * @priv: Driver private closure for @func.
  * @io_reserve_mutex: Mutex optionally protecting shared io_reserve structures
  * @use_io_reserve_lru: Use an lru list to try to unreserve io_mem_regions
  * reserved by the TTM vm system.
@@ -155,7 +154,6 @@ struct ttm_resource_manager {
 	uint32_t available_caching;
 	uint32_t default_caching;
 	const struct ttm_resource_manager_func *func;
-	void *priv;
 	struct mutex io_reserve_mutex;
 	bool use_io_reserve_lru;
 	spinlock_t move_lock;
