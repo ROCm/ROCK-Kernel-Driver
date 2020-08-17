c56d0ef755693596d4dfbc3ed123233c1540d0be
Stanley.Yang <Stanley.Yang@amd.com>
2020-08-17 15:48:21 +0800

    drm/amdkfd: fix set kfd node ras properties value
    The ctx->features are new RAS implementation which
    is only available for Vega20 and onwards, it is not
    available for vega10, vega10 should follow legacy
    ECC implementation.
    
    Changed from V1:
        wrap function to initialize kfd node properties
    
    Changed from V2:
        remove wrap function and SDMA SRAM ECC check
    
    Change-Id: I1e3ff899bf066611fe5775e67104ce2e0bf8b7d0
    Signed-off-by: Stanley.Yang <Stanley.Yang@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -1037,6 +1037,7 @@ struct amdgpu_device {
 
 	atomic_t			throttling_logging_enabled;
 	struct ratelimit_state		throttling_logging_rs;
+	uint32_t			ras_features;
 };
 
 static inline struct amdgpu_device *amdgpu_ttm_adev(struct ttm_bo_device *bdev)
