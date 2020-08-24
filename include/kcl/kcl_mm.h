bec74def457c61dc4bdc726b60c54ab15966dab2
Flora Cui <flora.cui@amd.com>
2020-08-24 15:35:12 +0800

    drm/amdkcl: add kcl copy of untagged_addr
    Signed-off-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/kcl_mm.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef AMDKCL_MM_H
+#define AMDKCL_MM_H
+
+#include <linux/mm.h>
+
+#ifndef untagged_addr
+#define untagged_addr(addr) (addr)
+#endif
+
+#endif /* AMDKCL_MM_H */
