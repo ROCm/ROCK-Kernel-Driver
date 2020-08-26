c1de4176b4b5a31b2832e4f2ae7c5c07acf9c189
Flora Cui <flora.cui@amd.com>
2020-08-26 15:35:29 +0800

    drm/amdkcl: test kthread_{use,unuse}_mm()
    introduced in commit f5678e7f2ac3 ("kernel: better document the
    use_mm/unuse_mm API contract")
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_kthread.h
+++ include/kcl/kcl_kthread.h
@@ -4,6 +4,9 @@
 
 #include <linux/sched.h>
 #include <linux/kthread.h>
+#ifndef HAVE_KTHREAD_USE_MM
+#include <linux/mmu_context.h>
+#endif
 
 #if !defined(HAVE___KTHREAD_SHOULD_PATK)
 extern bool __kcl_kthread_should_park(struct task_struct *k);
@@ -15,4 +18,18 @@ extern void (*_kcl_kthread_unpark)(struct task_struct *k);
 extern int (*_kcl_kthread_park)(struct task_struct *k);
 extern bool (*_kcl_kthread_should_park)(void);
 #endif
+
+#ifndef HAVE_KTHREAD_USE_MM
+static inline
+void kthread_use_mm(struct mm_struct *mm)
+{
+	use_mm(mm);
+}
+static inline
+void kthread_unuse_mm(struct mm_struct *mm)
+{
+	unuse_mm(mm);
+}
+#endif
+
 #endif /* AMDKCL_KTHREAD_H */
