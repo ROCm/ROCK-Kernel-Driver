aa1bcaa46aa20c2bdb69d0cf376f009bd0672e80
Evan Quan <evan.quan@amd.com>
2020-08-26 10:06:31 +0800

    drm/amdkcl: add macro DRM_MODE_ROTATE_xxx
    v2: move to kcl_drm_crtc.h
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/kcl_drm_crtc.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef KCL_KCL_DRM_CRTC_H
+#define KCL_KCL_DRM_CRTC_H
+
+#include <drm/drm_crtc.h>
+#include <drm/drm_crtc_helper.h>
+
+#ifndef DRM_MODE_ROTATE_0
+#define DRM_MODE_ROTATE_0       (1<<0)
+#endif
+#ifndef DRM_MODE_ROTATE_90
+#define DRM_MODE_ROTATE_90      (1<<1)
+#endif
+#ifndef DRM_MODE_ROTATE_180
+#define DRM_MODE_ROTATE_180     (1<<2)
+#endif
+#ifndef DRM_MODE_ROTATE_270
+#define DRM_MODE_ROTATE_270     (1<<3)
+#endif
+
+#ifndef DRM_MODE_ROTATE_MASK
+#define DRM_MODE_ROTATE_MASK (\
+		DRM_MODE_ROTATE_0  | \
+		DRM_MODE_ROTATE_90  | \
+		DRM_MODE_ROTATE_180 | \
+		DRM_MODE_ROTATE_270)
+#endif
+
+#endif
