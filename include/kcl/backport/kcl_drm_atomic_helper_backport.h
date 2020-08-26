4276239c962cc85ee8dfc3597beb2f171585069e
Flora Cui <flora.cui@amd.com>
2020-08-26 16:57:05 +0800

    drm/amdkcl: fake kcl copy of drm_atomic_helper_resume
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/backport/kcl_drm_atomic_helper_backport.h
+++ include/kcl/backport/kcl_drm_atomic_helper_backport.h
@@ -4,6 +4,34 @@
 
 #include <kcl/kcl_drm_atomic_helper.h>
 
+/*
+ * commit v4.14-rc4-1-g78279127253a
+ * drm/atomic: Unref duplicated drm_atomic_state in drm_atomic_helper_resume()
+ */
+#if DRM_VERSION_CODE < DRM_VERSION(4, 15, 0) && \
+	defined(HAVE_DRM_ATOMIC_STATE_PUT)
+static inline
+int _kcl_drm_atomic_helper_resume(struct drm_device *dev,
+					 struct drm_atomic_state *state)
+{
+	unsigned int prev, after;
+	int ret;
+
+	prev = kref_read(&state->ref);
+
+	drm_atomic_state_get(state);
+	ret = drm_atomic_helper_resume(dev, state);
+
+	after = kref_read(&state->ref);
+	drm_atomic_state_put(state);
+	if (prev != after)
+		drm_atomic_state_put(state);
+
+	return ret;
+}
+#define drm_atomic_helper_resume _kcl_drm_atomic_helper_resume
+#endif
+
 #ifdef AMDKCL__DRM_ATOMIC_HELPER_PLANE_RESET
 #define __drm_atomic_helper_plane_reset _kcl__drm_atomic_helper_plane_reset
 #endif /* AMDKCL__DRM_ATOMIC_HELPER_PLANE_RESET */
