4b8e3e66ce1794fe53ea93289f88b5152e83df78
Joshua Aberback <joshua.aberback@amd.com>
2020-08-31 01:58:03 -0400

    drm/amd/display: Update idle optimization handling
    [How]
     - use dc interface instead of hwss interface in cursor functions, to keep
    dc->idle_optimizations_allowed updated
     - add dc interface to check if idle optimizations might apply to a plane
    
    Signed-off-by: Joshua Aberback <joshua.aberback@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>

--- drivers/gpu/drm/amd/display/dc/inc/hw_sequencer.h
+++ drivers/gpu/drm/amd/display/dc/inc/hw_sequencer.h
@@ -237,6 +237,8 @@ struct hw_sequencer_funcs {
 	bool (*apply_idle_power_optimizations)(struct dc *dc, bool enable);
 #endif
 
+	bool (*does_plane_fit_in_mall)(struct dc *dc, struct dc_plane_state *plane);
+
 	bool (*is_abm_supported)(struct dc *dc,
 			struct dc_state *context, struct dc_stream_state *stream);
 
