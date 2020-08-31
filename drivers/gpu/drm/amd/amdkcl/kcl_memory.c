2be9aef88882395999ba7e5f5c8fff79543257d4
Flora Cui <flora.cui@amd.com>
2020-08-31 11:30:23 +0800

    drm/amdkcl: test vmf_insert_pfn_xxx_prot()
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_memory.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_memory.c
@@ -41,3 +41,60 @@ vm_fault_t _kcl_vmf_insert_pfn_prot(struct vm_area_struct *vma, unsigned long ad
 }
 EXPORT_SYMBOL(_kcl_vmf_insert_pfn_prot);
 #endif
+
+#ifndef HAVE_VMF_INSERT_PFN_PMD_PROT
+vm_fault_t vmf_insert_pfn_pmd_prot(struct vm_fault *vmf, pfn_t pfn,
+				   pgprot_t pgprot, bool write)
+{
+#if !defined(HAVE_VM_FAULT_ADDRESS_VMA)
+	pr_warn_once("%s is not available\n", __func__);
+	return (vm_fault_t)0;
+#else
+	struct vm_fault cvmf = *vmf;
+	struct vm_area_struct cvma = *vmf->vma;
+
+	cvmf.vma = &cvma;
+	cvma.vm_page_prot = pgprot;
+#if defined(HAVE_VMF_INSERT_PFN_PMD_3ARGS)
+	return vmf_insert_pfn_pmd(&cvmf, pfn, write);
+#elif defined(HAVE_VM_FAULT_ADDRESS_VMA)
+	return vmf_insert_pfn_pmd(&cvma, cvmf.address & PMD_MASK, cvmf.pmd, pfn, write);
+#endif /* HAVE_VMF_INSERT_PFN_PMD_3ARGS */
+#endif /* HAVE_VM_FAULT_ADDRESS_VMA */
+}
+EXPORT_SYMBOL_GPL(vmf_insert_pfn_pmd_prot);
+
+#ifdef CONFIG_HAVE_ARCH_TRANSPARENT_HUGEPAGE_PUD
+vm_fault_t vmf_insert_pfn_pud_prot(struct vm_fault *vmf, pfn_t pfn,
+				   pgprot_t pgprot, bool write)
+{
+#if defined(HAVE_VMF_INSERT_PFN_PMD_3ARGS)
+	struct vm_fault cvmf = *vmf;
+	struct vm_area_struct cvma = *vmf->vma;
+
+	cvmf.vma = &cvma;
+	cvma.vm_page_prot = pgprot;
+
+	return vmf_insert_pfn_pud(&cvmf, pfn, write);
+#elif defined(HAVE_VMF_INSERT_PFN_PUD)
+	struct vm_fault cvmf = *vmf;
+	struct vm_area_struct cvma = *vmf->vma;
+#ifdef HAVE_VM_FAULT_ADDRESS_VMA
+	unsigned long addr = vmf->address & PUD_MASK;
+#else
+	unsigned long addr = (unsigned long)vmf->virtual_address & PUD_MASK;
+#endif
+
+	cvmf.vma = &cvma;
+	cvma.vm_page_prot = pgprot;
+
+	return vmf_insert_pfn_pud(&cvma, addr, cvmf.pud, pfn, write);
+#else
+	pr_warn_once("% is not available\n", __func__);
+	return (vm_fault_t)0;
+#endif
+}
+EXPORT_SYMBOL_GPL(vmf_insert_pfn_pud_prot);
+#endif /* CONFIG_HAVE_ARCH_TRANSPARENT_HUGEPAGE_PUD */
+
+#endif /* HAVE_VMF_INSERT_PFN_PMD_PROT */
