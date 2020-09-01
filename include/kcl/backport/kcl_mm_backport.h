b4b240f3a4b7654fabc691a8242dfcd24f5865ab
Slava Grigorev <slava.grigorev@amd.com>
2020-09-01 11:19:53 +0800

    drm/amdkcl: Test whether get_user_{pages/pages_remote}() wants {5/6,8} args
    v2: rework get_user_pages() & get_user_pages_remote() test
    v3: adapt to get_user_pages_remote() prototype change (drop task_struct)
    
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Jack Gui <Jack.Gui@amd.com>
    Reviewed-by: Feifei Xu <Feifei.Xu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yang Xiong <Yang.Xiong@amd.com>

--- include/kcl/backport/kcl_mm_backport.h
+++ include/kcl/backport/kcl_mm_backport.h
@@ -9,4 +9,49 @@
 #define mm_access _kcl_mm_access
 #endif
 
+#ifdef get_user_pages_remote
+#undef get_user_pages_remote
+#endif
+#ifdef get_user_pages
+#undef get_user_pages
+#endif
+
+static inline
+long kcl_get_user_pages_remote(struct task_struct *tsk, struct mm_struct *mm,
+		unsigned long start, unsigned long nr_pages,
+		unsigned int gup_flags, struct page **pages,
+		struct vm_area_struct **vmas, int *locked)
+{
+#if defined(HAVE_GET_USER_PAGES_REMOTE_REMOVE_TASK_STRUCT)
+	return get_user_pages_remote(mm, start, nr_pages, gup_flags, pages, vmas, locked);
+#elif defined(HAVE_GET_USER_PAGES_REMOTE_LOCKED)
+	return get_user_pages_remote(tsk, mm, start, nr_pages, gup_flags, pages, vmas, locked);
+#elif defined(HAVE_GET_USER_PAGES_REMOTE_GUP_FLAGS)
+	return get_user_pages_remote(tsk, mm, start, nr_pages, gup_flags, pages, vmas);
+#elif defined(HAVE_GET_USER_PAGES_REMOTE_INTRODUCED)
+	return get_user_pages_remote(tsk, mm, start, nr_pages, !!(gup_flags & FOLL_WRITE),
+				     !!(gup_flags & FOLL_FORCE), pages, vmas);
+#else
+	return get_user_pages(tsk, mm, start, nr_pages, !!(gup_flags & FOLL_WRITE),
+			      !!(gup_flags & FOLL_FORCE), pages, vmas);
+#endif
+}
+
+#ifndef HAVE_GET_USER_PAGES_GUP_FLAGS
+static inline
+long _kcl_get_user_pages(unsigned long start, unsigned long nr_pages,
+		unsigned int gup_flags, struct page **pages,
+		struct vm_area_struct **vmas)
+{
+#if defined(HAVE_GET_USER_PAGES_6ARGS)
+	return get_user_pages(start, nr_pages, !!(gup_flags & FOLL_WRITE),
+			      !!(gup_flags & FOLL_FORCE), pages, vmas);
+#else
+	return get_user_pages(current, current->mm, start, nr_pages, !!(gup_flags & FOLL_WRITE),
+			      !!(gup_flags & FOLL_FORCE), pages, vmas);
+#endif
+}
+#define get_user_pages _kcl_get_user_pages
+#endif /* HAVE_GET_USER_PAGES_GUP_FLAGS */
+
 #endif
