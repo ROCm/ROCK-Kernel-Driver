4c3ee8078afcff2946cc0b1954588aa4e6425002
Flora Cui <flora.cui@amd.com>
2020-09-03 17:40:25 +0800

    drm/amdkcl: test sched_set_fifo_low()
    v2: c1f004d6344b drm/amdkcl: fix license for kcl part
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkcl/main.c
+++ drivers/gpu/drm/amd/amdkcl/main.c
@@ -11,6 +11,7 @@ extern void amdkcl_mm_init(void);
 extern void amdkcl_perf_event_init(void);
 extern void amdkcl_pci_init(void);
 extern void amdkcl_suspend_init(void);
+extern void amdkcl_sched_init(void);
 
 int __init amdkcl_init(void)
 {
@@ -23,6 +24,7 @@ int __init amdkcl_init(void)
 	amdkcl_perf_event_init();
 	amdkcl_pci_init();
 	amdkcl_suspend_init();
+	amdkcl_sched_init();
 
 	return 0;
 }
