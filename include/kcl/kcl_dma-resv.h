df343e2ddf24e99463b0e2ab7f67b68e9744aedc
Flora Cui <flora.cui@amd.com>
2020-09-03 16:13:29 +0800

    drm/amdkcl: test seqcount_ww_mutex_init for dma_resv
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: include kcl_common.h in every .c
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>
    
    drm/amdkcl: fix license for kcl part
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- include/kcl/kcl_dma-resv.h
+++ include/kcl/kcl_dma-resv.h
@@ -48,10 +48,19 @@
 
 #include <asm/barrier.h>
 #include <kcl/backport/kcl_fence_backport.h>
+#include <kcl/kcl_seqlock.h>
 
 struct dma_resv_list;
 
-#if defined(HAVE_RESERVATION_OBJECT_STAGED)
+#if defined(HAVE_DMA_RESV_SEQCOUNT_WW_MUTEX_T)
+struct dma_resv {
+	struct ww_mutex lock;
+	seqcount_ww_mutex_t seq;
+
+	struct dma_fence __rcu *fence_excl;
+	struct dma_resv_list __rcu *fence;
+};
+#elif defined(HAVE_RESERVATION_OBJECT_STAGED)
 struct dma_resv {
 	struct ww_mutex lock;
 	seqcount_t seq;
