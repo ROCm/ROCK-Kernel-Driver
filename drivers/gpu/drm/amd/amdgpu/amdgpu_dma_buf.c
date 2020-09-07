b9a0979689e8f4d860dc89df482d0175bc76c9ba
Flora Cui <flora.cui@amd.com>
2020-09-07 14:04:55 +0800

    drm/amdkcl: test dma_buf_ops->allow_peer2peer
    introduced in commit v5.6-rc5-1663-g09606b5446c2 ("dma-buf: add
    		peer2peer flag")
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
@@ -57,8 +57,10 @@ static int amdgpu_dma_buf_attach(struct dma_buf *dmabuf,
 	struct amdgpu_bo *bo = gem_to_amdgpu_bo(obj);
 	struct amdgpu_device *adev = amdgpu_ttm_adev(bo->tbo.bdev);
 
+#ifdef HAVE_STRUCT_DMA_BUF_OPS_ALLOW_PEER2PEER
 	if (pci_p2pdma_distance(adev->pdev, attach->dev, false) < 0)
 		attach->peer2peer = false;
+#endif
 
 	return 0;
 }
@@ -122,11 +124,13 @@ static struct sg_table *amdgpu_dma_buf_map(struct dma_buf_attachment *attach,
 		struct ttm_operation_ctx ctx = { false, false };
 		unsigned int domains = AMDGPU_GEM_DOMAIN_GTT;
 
+#ifdef HAVE_STRUCT_DMA_BUF_OPS_ALLOW_PEER2PEER
 		if (bo->preferred_domains & AMDGPU_GEM_DOMAIN_VRAM &&
 		    attach->peer2peer) {
 			bo->flags |= AMDGPU_GEM_CREATE_CPU_ACCESS_REQUIRED;
 			domains |= AMDGPU_GEM_DOMAIN_VRAM;
 		}
+#endif
 		amdgpu_bo_placement_from_domain(bo, domains);
 		r = ttm_bo_validate(&bo->tbo, &bo->placement, &ctx);
 		if (r)
@@ -393,7 +397,9 @@ amdgpu_dma_buf_move_notify(struct dma_buf_attachment *attach)
 }
 
 static const struct dma_buf_attach_ops amdgpu_dma_buf_attach_ops = {
+#ifdef HAVE_STRUCT_DMA_BUF_OPS_ALLOW_PEER2PEER
 	.allow_peer2peer = true,
+#endif
 	.move_notify = amdgpu_dma_buf_move_notify
 };
 
