d10222d061983988efcca8e79eefd9de3893e7af
Petr Tesarik <ptesarik@suse.cz>
2020-09-08 16:34:49 +0200

    s390/qeth: integrate RX refill worker with NAPI (git-fixes).
    suse-commit: 16aaccb77be617e1776c39d3d93f298971560822

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -3197,20 +3197,15 @@ static int qeth_check_qdio_errors(struct qeth_card *card,
 	return 0;
 }
 
-static void qeth_queue_input_buffer(struct qeth_card *card, int index)
+static unsigned int qeth_rx_refill_queue(struct qeth_card *card,
+					 unsigned int count)
 {
 	struct qeth_qdio_q *queue = card->qdio.in_q;
 	struct list_head *lh;
-	int count;
 	int i;
 	int rc;
 	int newcount = 0;
 
-	count = (index < queue->next_buf_to_init)?
-		card->qdio.in_buf_pool.buf_count -
-		(queue->next_buf_to_init - index) :
-		card->qdio.in_buf_pool.buf_count -
-		(queue->next_buf_to_init + QDIO_MAX_BUFFERS_PER_Q - index);
 	/* only requeue at a certain threshold to avoid SIGAs */
 	if (count >= QETH_IN_BUF_REQUEUE_THRESHOLD(card)) {
 		for (i = queue->next_buf_to_init;
@@ -3238,12 +3233,11 @@ static void qeth_queue_input_buffer(struct qeth_card *card, int index)
 				i++;
 			if (i == card->qdio.in_buf_pool.buf_count) {
 				QETH_CARD_TEXT(card, 2, "qsarbw");
-				card->reclaim_index = index;
 				schedule_delayed_work(
 					&card->buffer_reclaim_work,
 					QETH_RECLAIM_WORK_TIME);
 			}
-			return;
+			return 0;
 		}
 
 		/*
@@ -3260,7 +3254,10 @@ static void qeth_queue_input_buffer(struct qeth_card *card, int index)
 		}
 		queue->next_buf_to_init = QDIO_BUFNR(queue->next_buf_to_init +
 						     count);
+		return count;
 	}
+
+	return 0;
 }
 
 static void qeth_buffer_reclaim_work(struct work_struct *work)
@@ -3268,8 +3265,10 @@ static void qeth_buffer_reclaim_work(struct work_struct *work)
 	struct qeth_card *card = container_of(work, struct qeth_card,
 		buffer_reclaim_work.work);
 
-	QETH_CARD_TEXT_(card, 2, "brw:%x", card->reclaim_index);
-	qeth_queue_input_buffer(card, card->reclaim_index);
+	local_bh_disable();
+	napi_schedule(&card->napi);
+	/* kick-start the NAPI softirq: */
+	local_bh_enable();
 }
 
 static void qeth_handle_send_error(struct qeth_card *card,
@@ -5292,6 +5291,7 @@ EXPORT_SYMBOL_GPL(qeth_core_get_next_skb);
 int qeth_poll(struct napi_struct *napi, int budget)
 {
 	struct qeth_card *card = container_of(napi, struct qeth_card, napi);
+	struct qeth_rx *ctx = &card->rx;
 	int work_done = 0;
 	struct qeth_qdio_buffer *buffer;
 	int done;
@@ -5329,8 +5329,11 @@ int qeth_poll(struct napi_struct *napi, int budget)
 				qeth_put_buffer_pool_entry(card,
 					buffer->pool_entry);
 				buffer->pool_entry = NULL;
-				qeth_queue_input_buffer(card, card->rx.b_index);
 				card->rx.b_count--;
+				ctx->bufs_refill++;
+				ctx->bufs_refill -=
+					qeth_rx_refill_queue(card,
+							     ctx->bufs_refill);
 				if (card->rx.b_count) {
 					card->rx.b_index =
 						QDIO_BUFNR(card->rx.b_index + 1);
@@ -5341,6 +5344,10 @@ int qeth_poll(struct napi_struct *napi, int budget)
 					card->rx.e_offset = 0;
 				}
 			}
+
+				if (budget)
+					/* Process any substantial refill backlog: */
+					ctx->bufs_refill -= qeth_rx_refill_queue(card, ctx->bufs_refill);
 
 			if (work_done >= budget)
 				goto out;
@@ -6456,6 +6463,7 @@ int qeth_stop(struct net_device *dev)
 	}
 
 	napi_disable(&card->napi);
+	cancel_delayed_work_sync(&card->buffer_reclaim_work);
 	return 0;
 }
 EXPORT_SYMBOL_GPL(qeth_stop);
