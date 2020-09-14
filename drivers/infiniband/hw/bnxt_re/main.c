ca4086b2827eae6827b61fd5b36139a5c2a2e22b
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2020-09-14 23:00:28 +0200

    RDMA/bnxt_re: Remove set but not used variable 'qplib_ctx' (bsc#1170774).
    suse-commit: d51761bd25ef1a1a9264359f9f6b77b194ccea87

--- drivers/infiniband/hw/bnxt_re/main.c
+++ drivers/infiniband/hw/bnxt_re/main.c
@@ -998,7 +998,6 @@ static void bnxt_re_free_res(struct bnxt_re_dev *rdev)
 static int bnxt_re_alloc_res(struct bnxt_re_dev *rdev)
 {
 	struct bnxt_re_ring_attr rattr = {};
-	struct bnxt_qplib_ctx *qplib_ctx;
 	int num_vec_created = 0;
 	int rc = 0, i;
 	u8 type;
@@ -1021,7 +1020,6 @@ static int bnxt_re_alloc_res(struct bnxt_re_dev *rdev)
 	if (rc)
 		goto dealloc_res;
 
-	qplib_ctx = &rdev->qplib_ctx;
 	for (i = 0; i < rdev->num_msix - 1; i++) {
 		struct bnxt_qplib_nq *nq;
 
