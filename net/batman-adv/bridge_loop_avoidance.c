ecde928f764e419931d89dc3a222367c65a5a6a0
Takashi Iwai <tiwai@suse.de>
2020-09-24 09:47:43 +0200

    batman-adv: bla: fix type misuse for backbone_gw hash indexing (git-fixes).
    suse-commit: 1f98d0282d474941cf5bd0c146f4ad1e2afa40ec

--- net/batman-adv/bridge_loop_avoidance.c
+++ net/batman-adv/bridge_loop_avoidance.c
@@ -65,11 +65,12 @@ batadv_bla_send_announce(struct batadv_priv *bat_priv,
  */
 static inline u32 batadv_choose_claim(const void *data, u32 size)
 {
-	struct batadv_bla_claim *claim = (struct batadv_bla_claim *)data;
+	const struct batadv_bla_backbone_gw *gw;
 	u32 hash = 0;
 
-	hash = jhash(&claim->addr, sizeof(claim->addr), hash);
-	hash = jhash(&claim->vid, sizeof(claim->vid), hash);
+	gw = (struct batadv_bla_backbone_gw *)data;
+	hash = jhash(&gw->orig, sizeof(gw->orig), hash);
+	hash = jhash(&gw->vid, sizeof(gw->vid), hash);
 
 	return hash % size;
 }
