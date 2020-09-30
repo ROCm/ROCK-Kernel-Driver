643e7dcaa74e2ffbaac1171e5d716d27d3f29ab7
Evan Quan <evan.quan@amd.com>
2020-09-30 14:20:38 +0800

    drm/amd/display: correct asic type check V2
    Check chip family also to avoid wrong identification.
    
    V2: use the correct macro without AMDGPU prefix
    
    Change-Id: I38e9a9ee70937db95fdd03fe6057310eabf5d547
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link.c
@@ -2739,7 +2739,8 @@ bool dc_link_setup_psr(struct dc_link *link,
 
 #if defined(CONFIG_DRM_AMD_DC_DCN1_0)
 	/*skip power down the single pipe since it blocks the cstate*/
-	if (ASICREV_IS_RAVEN(link->ctx->asic_id.hw_internal_rev))
+	if ((link->ctx->asic_id.chip_family == FAMILY_AI) &&
+	     ASICREV_IS_RAVEN(link->ctx->asic_id.hw_internal_rev))
 		psr_context->psr_level.bits.SKIP_CRTC_DISABLE = true;
 #endif
 
