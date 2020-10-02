b51d1abb7269e0811c83788442b76d7eca030723
Flora Cui <flora.cui@amd.com>
2020-10-02 12:48:30 +0800

    drm/amdkcl: drop test for DRIVER_ATOMIC
    DRIVER_ATOMIC is introduced in v3.18-1050-g88a48e297b3a
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -1291,7 +1291,6 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 	ddev = adev_to_drm(adev);
 	kcl_drm_vma_offset_manager_init(ddev->vma_offset_manager);
 
-#ifdef HAVE_DRM_DRV_DRIVER_ATOMIC
 #ifdef HAVE_DRM_DEVICE_DRIVER_FEATURES
 	if (!supports_atomic)
 		ddev->driver_features &= ~DRIVER_ATOMIC;
@@ -1302,7 +1301,6 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 	/* support atomic early so the atomic debugfs stuff gets created */
 	if (supports_atomic)
 		kms_driver.driver_features |= DRIVER_ATOMIC;
-#endif
 #endif
 
 	kcl_pci_configure_extended_tags(pdev);
