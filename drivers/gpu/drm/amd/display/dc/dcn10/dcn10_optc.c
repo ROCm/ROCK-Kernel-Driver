b63509acba25e3b94a50dba4df1c70574e83f9e2
Shiwu Zhang <shiwu.zhang@amd.com>
2020-10-13 18:49:35 +0800

    drm/amdkcl: wrap the change under CONFIG_DRM_AMD_DC_DCN2_0 for the centos phantom distro
    This is caused by "Fix OPTC_DATA_FORMAT programming"
    v5.9-rc2-592-g959adf55a8ae
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_optc.c
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_optc.c
@@ -287,7 +287,7 @@ void optc1_program_timing(
 
 	if (optc1_is_two_pixels_per_containter(&patched_crtc_timing) || optc1->opp_count == 2)
 		h_div = H_TIMING_DIV_BY2;
-
+#if defined(CONFIG_DRM_AMD_DC_DCN2_0)
 	if (REG(OPTC_DATA_FORMAT_CONTROL)) {
 		uint32_t data_fmt = 0;
 
@@ -298,6 +298,7 @@ void optc1_program_timing(
 
 		REG_UPDATE(OPTC_DATA_FORMAT_CONTROL, OPTC_DATA_FORMAT, data_fmt);
 	}
+#endif
 
 #if defined(CONFIG_DRM_AMD_DC_DCN3_0)
 	if (optc1->tg_mask->OTG_H_TIMING_DIV_MODE != 0) {
