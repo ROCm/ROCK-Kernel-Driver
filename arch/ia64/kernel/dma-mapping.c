0fa0f0c4bdb43cbf4da22d058972ce18ab606821
Joerg Roedel <jroedel@suse.de>
2020-10-13 16:57:37 +0200

    - dma-direct: provide mmap and get_sgtable method overrides (bsc#1175898, ECO-2743). - Update config files. - Refresh   patches.suse/dma-direct-relax-addressability-checks-in-dma_direct_supported.patch.
    suse-commit: 4e7185d125bd9c611b63d9b9e2abd4f55d1a06eb

--- arch/ia64/kernel/dma-mapping.c
+++ arch/ia64/kernel/dma-mapping.c
@@ -28,12 +28,6 @@ void arch_dma_free(struct device *dev, size_t size, void *cpu_addr,
 	dma_direct_free_pages(dev, size, cpu_addr, dma_addr, attrs);
 }
 
-long arch_dma_coherent_to_pfn(struct device *dev, void *cpu_addr,
-		dma_addr_t dma_addr)
-{
-	return page_to_pfn(virt_to_page(cpu_addr));
-}
-
 void __init swiotlb_dma_init(void)
 {
 	swiotlb_init(1);
