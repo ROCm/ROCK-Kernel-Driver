3043fabacca46dc8b9d8150ff0bc49fa130ff5b3
Shiwu Zhang <shiwu.zhang@amd.com>
2020-10-19 14:57:24 +0800

    drm/amdkcl: DCN302 backport for DC
    This is caused by "Add support for DCN302 (v2)"
    v5.9-rc2-689-gbe0899b1f9f0
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn302/dcn302_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/dcn302/dcn302_hwseq.c
@@ -156,6 +156,7 @@ void dcn302_hubp_pg_control(struct dce_hwseq *hws, unsigned int hubp_inst, bool 
 	}
 }
 
+#ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 void dcn302_dsc_pg_control(struct dce_hwseq *hws, unsigned int dsc_inst, bool power_on)
 {
 	uint32_t power_gate = power_on ? 0 : 1;
@@ -221,3 +222,4 @@ void dcn302_dsc_pg_control(struct dce_hwseq *hws, unsigned int dsc_inst, bool po
 	if (org_ip_request_cntl == 0)
 		REG_SET(DC_IP_REQUEST_CNTL, 0, IP_REQUEST_EN, 0);
 }
+#endif
