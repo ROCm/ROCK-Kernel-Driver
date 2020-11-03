8951913d967e6815f658c516c51fa32be0317400
Shiwu Zhang <shiwu.zhang@amd.com>
2020-11-03 15:47:44 +0800

    drm/amdkcl: use the kcl wrapper for get/set gem object
    This is caused by "Store tiling_flags in the framebuffer"
    v5.9-rc5-1363-gdd3cc8e45355
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -1153,7 +1153,7 @@ static int amdgpu_display_get_fb_info(const struct amdgpu_framebuffer *amdgpu_fb
 		return 0;
 	}
 
-	rbo = gem_to_amdgpu_bo(amdgpu_fb->base.obj[0]);
+	rbo = gem_to_amdgpu_bo(drm_gem_fb_get_obj(&amdgpu_fb->base, 0));
 	r = amdgpu_bo_reserve(rbo, false);
 
 	if (unlikely(r)) {
