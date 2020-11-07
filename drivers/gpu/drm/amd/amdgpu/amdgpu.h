628e2039460f2fd94731caf32c57e5ad921856fe
Hawking Zhang <Hawking.Zhang@amd.com>
2020-11-07 22:50:05 +0800

    drm/amdgpu: add amdgpu_smuio structure
    Add amdgpu_smuio structure in amdgpu_device to
    provide various callback functions to support
    smuio ip funcitonality
    
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: John Clements <john.clements@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -107,6 +107,7 @@
 #include "amdgpu_mmhub.h"
 #include "amdgpu_gfxhub.h"
 #include "amdgpu_df.h"
+#include "amdgpu_smuio.h"
 
 #define MAX_GPU_INSTANCE		16
 
@@ -975,6 +976,9 @@ struct amdgpu_device {
 
 	/* nbio */
 	struct amdgpu_nbio		nbio;
+
+	/* smuio */
+	struct amdgpu_smuio		smuio;
 
 	/* mmhub */
 	struct amdgpu_mmhub		mmhub;
