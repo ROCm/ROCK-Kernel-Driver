4806a4e9a999a09dbe59fc3bb80a9cc86793e088
Shiwu Zhang <shiwu.zhang@amd.com>
2020-11-17 16:48:12 +0800

    drm/amdkcl: disable modifier support for DRM before 5.0
    This is caused by "Add formats for DCC with 2/3 planes"
    and "Expose modifiers"
    v5.9-rc5-1367-g564b9f4c7cf9 and v5.9-rc5-1368-g86ce3ed796ff
    
    This is a squash of:
    
    drm/amdkcl: fix license for kcl drm part
        Signed-off-by: Flora Cui <flora.cui@amd.com>
        Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    drm/amdkcl: guard the modifier code for backport
        This is caused by "Store gem objects for planes 1-3"
        v5.9-rc5-1565-gbf01c77ad320
        Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
        Reviewed-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    drm/amdkcl: update the AMD modifer macros
        This is caused by "Fix modifier field mask for AMD modifiers",
        "add table describing AMD modifiers bit layout",
        "fix AMD modifiers PACKERS field doc"
        v5.9-rc5-1563-g80363c69ca44, v5.9-rc5-1564-gbac8bece9c55
        and
        v5.9-rc5-1594-g11e500d2892a
        Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    drm/amdkcl: fix the config.h mismatch for autotest
        and regenerate the config.h
        Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    drm/amdkcl: disable modifier feature if
        Floating point 64bpp RGB format is not defined
        This is to avoid warning printing during modprobe
        on the OS like rhel 7.9
        Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Change-Id: If9ccae068c5b8cf3f475fa8c178b5d3135a9b84a

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -3096,7 +3096,9 @@ dm_atomic_state_alloc_free(struct drm_atomic_state *state)
 
 static const struct drm_mode_config_funcs amdgpu_dm_mode_funcs = {
 	.fb_create = amdgpu_display_user_framebuffer_create,
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 	.get_format_info = amdgpu_dm_plane_get_format_info,
+#endif
 	.atomic_check = amdgpu_dm_atomic_check,
 	.atomic_commit = drm_atomic_helper_commit,
 #ifndef HAVE_DRM_ATOMIC_PRIVATE_OBJ_INIT
@@ -5724,7 +5726,6 @@ get_output_color_space(const struct dc_crtc_timing *dc_crtc_timing)
 	case PIXEL_ENCODING_RGB:
 		color_space = COLOR_SPACE_SRGB;
 		break;
-
 	default:
 		WARN_ON(1);
 		break;
--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -161,10 +161,12 @@ static void add_modifier(uint64_t **mods, uint64_t *size, uint64_t *cap, uint64_
 	*size += 1;
 }
 
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 static bool modifier_has_dcc(uint64_t modifier)
 {
 	return IS_AMD_FMT_MOD(modifier) && AMD_FMT_MOD_GET(DCC, modifier);
 }
+#endif
 
 static unsigned modifier_gfx9_swizzle_mode(uint64_t modifier)
 {
@@ -300,6 +302,7 @@ static int validate_dcc(struct amdgpu_device *adev,
 	return 0;
 }
 
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 static int fill_gfx9_plane_attributes_from_modifiers(struct amdgpu_device *adev,
 					  const struct amdgpu_framebuffer *afb,
 					  const enum surface_pixel_format format,
@@ -350,6 +353,7 @@ static int fill_gfx9_plane_attributes_from_modifiers(struct amdgpu_device *adev,
 
 	return ret;
 }
+#endif
 
 static void add_gfx10_1_modifiers(const struct amdgpu_device *adev,
 		      uint64_t **mods, uint64_t *size, uint64_t *capacity)
@@ -824,6 +828,7 @@ int amdgpu_dm_plane_fill_plane_buffer_attributes(struct amdgpu_device *adev,
 	}
 
 	if (adev->family >= AMDGPU_FAMILY_AI) {
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 		ret = fill_gfx9_plane_attributes_from_modifiers(adev, afb, format,
 								rotation, plane_size,
 								tiling_info, dcc,
@@ -831,6 +836,7 @@ int amdgpu_dm_plane_fill_plane_buffer_attributes(struct amdgpu_device *adev,
 								force_disable_dcc);
 		if (ret)
 			return ret;
+#endif
 	} else {
 		fill_gfx8_tiling_info_from_flags(tiling_info, tiling_flags);
 	}
@@ -1459,7 +1465,9 @@ static const struct drm_plane_funcs dm_plane_funcs = {
 	.reset = dm_drm_plane_reset,
 	.atomic_duplicate_state = dm_drm_plane_duplicate_state,
 	.atomic_destroy_state = dm_drm_plane_destroy_state,
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 	.format_mod_supported = dm_plane_format_mod_supported,
+#endif
 };
 
 int amdgpu_dm_plane_init(struct amdgpu_display_manager *dm,
@@ -1478,9 +1486,11 @@ int amdgpu_dm_plane_init(struct amdgpu_display_manager *dm,
 	num_formats = get_plane_formats(plane, plane_cap, formats,
 					ARRAY_SIZE(formats));
 
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 	res = get_plane_modifiers(dm->adev, plane->type, &modifiers);
 	if (res)
 		return res;
+#endif
 
 	if (modifiers == NULL)
 		adev_to_drm(dm->adev)->mode_config.fb_modifiers_not_supported = true;
