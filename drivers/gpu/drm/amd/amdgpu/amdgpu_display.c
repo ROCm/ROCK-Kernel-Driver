3bf958d987ee72269885e84cfdf2f7cf2f6dc19d
Shiwu Zhang <shiwu.zhang@amd.com>
2020-11-17 16:48:12 +0800

    drm/amdkcl: disable modifier support for DRM before 5.0
    This is caused by "Add formats for DCC with 2/3 planes"
    and "Expose modifiers"
    v5.9-rc5-1367-g564b9f4c7cf9 and v5.9-rc5-1368-g86ce3ed796ff
    
    This is a squash of:
    
    drm/amdkcl: fix license for kcl drm part
        Signed-off-by: Flora Cui <flora.cui@amd.com>
        Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    drm/amdkcl: guard the modifier code for backport
        This is caused by "Store gem objects for planes 1-3"
        v5.9-rc5-1565-gbf01c77ad320
        Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
        Reviewed-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    drm/amdkcl: update the AMD modifer macros
        This is caused by "Fix modifier field mask for AMD modifiers",
        "add table describing AMD modifiers bit layout",
        "fix AMD modifiers PACKERS field doc"
        v5.9-rc5-1563-g80363c69ca44, v5.9-rc5-1564-gbac8bece9c55
        and
        v5.9-rc5-1594-g11e500d2892a
        Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    drm/amdkcl: fix the config.h mismatch for autotest
        and regenerate the config.h
        Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    drm/amdkcl: disable modifier feature if
        Floating point 64bpp RGB format is not defined
        This is to avoid warning printing during modprobe
        on the OS like rhel 7.9
        Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -819,6 +819,7 @@ extract_render_dcc_offset(struct amdgpu_device *adev,
 	return 0;
 }
 
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 static int convert_tiling_flags_to_modifier(struct amdgpu_framebuffer *afb)
 {
 	struct amdgpu_device *adev = drm_to_adev(afb->base.dev);
@@ -973,6 +974,7 @@ static int convert_tiling_flags_to_modifier(struct amdgpu_framebuffer *afb)
 	afb->base.flags |= DRM_MODE_FB_MODIFIERS;
 	return 0;
 }
+#endif
 
 static int amdgpu_display_get_fb_info(const struct amdgpu_framebuffer *amdgpu_fb,
 				      uint64_t *tiling_flags, bool *tmz_surface)
@@ -1023,6 +1025,7 @@ int amdgpu_display_framebuffer_init(struct drm_device *dev,
 	 * This needs to happen before modifier conversion as that might change
 	 * the number of planes.
 	 */
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 	for (i = 1; i < rfb->base.format->num_planes; ++i) {
 		if (mode_cmd->handles[i] != mode_cmd->handles[0]) {
 			drm_dbg_kms(dev, "Plane 0 and %d have different BOs: %u vs. %u\n",
@@ -1031,11 +1034,13 @@ int amdgpu_display_framebuffer_init(struct drm_device *dev,
 			goto fail;
 		}
 	}
+#endif
 
 	ret = amdgpu_display_get_fb_info(rfb, &rfb->tiling_flags, &rfb->tmz_surface);
 	if (ret)
 		goto fail;
 
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 	if (dev->mode_config.allow_fb_modifiers &&
 	    !(rfb->base.flags & DRM_MODE_FB_MODIFIERS)) {
 		ret = convert_tiling_flags_to_modifier(rfb);
@@ -1050,6 +1055,7 @@ int amdgpu_display_framebuffer_init(struct drm_device *dev,
 		rfb->base.obj[i] = rfb->base.obj[0];
 		drm_gem_object_get(rfb->base.obj[i]);
 	}
+#endif
 
 	return 0;
 
