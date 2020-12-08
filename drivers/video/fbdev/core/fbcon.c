5157115a81c1e5b93e934e5f4a1d51593ee45d4f
Thomas Zimmermann <tzimmermann@suse.de>
2020-12-08 10:07:42 +0100

    fbcon: Remove the superfluous break (bsc#1152472)
    suse-commit: e00dc986b75e39f14cc39bcd1503510384c3f5d7

--- drivers/video/fbdev/core/fbcon.c
+++ drivers/video/fbdev/core/fbcon.c
@@ -1812,7 +1812,6 @@ static bool fbcon_scroll(struct vc_data *vc, unsigned int t, unsigned int b,
 				    vc->vc_video_erase_char,
 				    vc->vc_size_row * count);
 			return true;
-			break;
 
 		case SCROLL_WRAP_MOVE:
 			if (b - t - count > 3 * vc->vc_rows >> 2) {
