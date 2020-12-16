cdea2034f75d135c058de94f820eb7f8aa890583
Flora Cui <flora.cui@amd.com>
2020-12-16 16:57:36 +0800

    drm/amdkcl: split fbmem related stuff
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_drm_fb.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm_fb.c
@@ -56,34 +56,6 @@ void _kcl_drm_fb_helper_set_suspend_unlocked(struct drm_fb_helper *fb_helper, in
 EXPORT_SYMBOL(_kcl_drm_fb_helper_set_suspend_unlocked);
 #endif
 
-#if !defined(HAVE_DRM_FB_HELPER_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS)
-int remove_conflicting_pci_framebuffers(struct pci_dev *pdev, int res_id, const char *name)
-{
-	struct apertures_struct *ap;
-	bool primary = false;
-	int err = 0;
-
-	ap = alloc_apertures(1);
-	if (!ap)
-		return -ENOMEM;
-
-	ap->ranges[0].base = pci_resource_start(pdev, res_id);
-	ap->ranges[0].size = pci_resource_len(pdev, res_id);
-#ifdef CONFIG_X86
-	primary = pdev->resource[PCI_ROM_RESOURCE].flags &
-					IORESOURCE_ROM_SHADOW;
-#endif
-#ifdef HAVE_REMOVE_CONFLICTING_FRAMEBUFFERS_RETURNS_INT
-	err = remove_conflicting_framebuffers(ap, name, primary);
-#else
-	remove_conflicting_framebuffers(ap, name, primary);
-#endif
-	kfree(ap);
-	return err;
-}
-EXPORT_SYMBOL(remove_conflicting_pci_framebuffers);
-#endif
-
 #ifndef HAVE_DRM_FB_HELPER_FILL_INFO
 void drm_fb_helper_fill_info(struct fb_info *info,
 			     struct drm_fb_helper *fb_helper,
