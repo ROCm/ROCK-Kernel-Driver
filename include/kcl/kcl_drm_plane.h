c63241256d902e7204426d4778e1f9aff249f0ea
Flora Cui <flora.cui@amd.com>
2020-12-16 14:25:40 +0800

    drm/amdkcl: split drm_plane stuff
    try to keep backport part contains backport stuff only.
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- include/kcl/kcl_drm_plane.h
+++ include/kcl/kcl_drm_plane.h
@@ -19,30 +19,17 @@
  * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
  * OF THIS SOFTWARE.
  */
-#ifndef KCL_BACKPORT_KCL_DRM_PLANE_H
-#define KCL_BACKPORT_KCL_DRM_PLANE_H
+#ifndef __KCL_KCL_DRM_PLANE_H__
+#define __KCL_KCL_DRM_PLANE_H__
 
 #include <kcl/header/kcl_drm_plane_h.h>
-#include <kcl/kcl_drm_plane.h>
 
-#ifndef HAVE_DRM_UNIVERSAL_PLANE_INIT_9ARGS
-static inline int _kcl_drm_universal_plane_init(struct drm_device *dev, struct drm_plane *plane,
-			     unsigned long possible_crtcs,
-			     const struct drm_plane_funcs *funcs,
-			     const uint32_t *formats, unsigned int format_count,
-			     const uint64_t *format_modifiers,
-			     enum drm_plane_type type,
-			     const char *name, ...)
+#ifndef HAVE_DRM_PLANE_MASK
+/* Copied from include/drm/drm_plane.h */
+static inline u32 drm_plane_mask(const struct drm_plane *plane)
 {
-#if defined(HAVE_DRM_UNIVERSAL_PLANE_INIT_8ARGS)
-	return drm_universal_plane_init(dev, plane, possible_crtcs, funcs,
-			 formats, format_count, type, name);
-#else
-	return drm_universal_plane_init(dev, plane, possible_crtcs, funcs,
-			 formats, format_count, type);
-#endif
+	return 1 << drm_plane_index(plane);
 }
-#define drm_universal_plane_init _kcl_drm_universal_plane_init
-#endif /* HAVE_DRM_UNIVERSAL_PLANE_INIT_9ARGS */
+#endif
 
 #endif
