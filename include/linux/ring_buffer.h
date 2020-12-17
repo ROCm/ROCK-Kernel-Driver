cd10ec05162c32cd63ed33327364b927ab9f3499
Miroslav Benes <mbenes@suse.cz>
2020-12-17 16:17:46 +0100

    ring-buffer: speed up buffer resets by avoiding synchronize_rcu for each CPU (CVE-2020-27825 bsc#1179960).
    suse-commit: d3082786194b7b1d534ea35d673dea93e60738c7

--- include/linux/ring_buffer.h
+++ include/linux/ring_buffer.h
@@ -143,6 +143,7 @@ int ring_buffer_iter_empty(struct ring_buffer_iter *iter);
 unsigned long ring_buffer_size(struct ring_buffer *buffer, int cpu);
 
 void ring_buffer_reset_cpu(struct ring_buffer *buffer, int cpu);
+void ring_buffer_reset_online_cpus(struct ring_buffer *buffer);
 void ring_buffer_reset(struct ring_buffer *buffer);
 
 #ifdef CONFIG_RING_BUFFER_ALLOW_SWAP
--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -1875,7 +1875,6 @@ void tracing_reset(struct trace_buffer *buf, int cpu)
 void tracing_reset_online_cpus(struct trace_buffer *buf)
 {
 	struct ring_buffer *buffer = buf->buffer;
-	int cpu;
 
 	if (!buffer)
 		return;
@@ -1887,8 +1886,7 @@ void tracing_reset_online_cpus(struct trace_buffer *buf)
 
 	buf->time_start = buffer_ftrace_now(buf, buf->cpu);
 
-	for_each_online_cpu(cpu)
-		ring_buffer_reset_cpu(buffer, cpu);
+	ring_buffer_reset_online_cpus(buffer);
 
 	ring_buffer_record_enable(buffer);
 }
