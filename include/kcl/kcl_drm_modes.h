5908f02e026f810e8a4fc0ed5e9473ef0623b4d1
Flora Cui <flora.cui@amd.com>
2020-12-28 11:27:04 +0800

    drm/amdkcl: add faked drm_connector_list_update()
    leverage test for drm_connector_set_path_property()
    the 2 func changes are introduced in the same commit.
    
    Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_drm_modes.h
+++ include/kcl/kcl_drm_modes.h
@@ -33,6 +33,10 @@
 #define drm_mode_get_hv_timing drm_crtc_get_hv_timing
 #endif
 
+#ifndef HAVE_DRM_CONNECTOR_SET_PATH_PROPERTY
+#define drm_connector_list_update drm_mode_connector_list_update
+#endif
+
 #ifndef HAVE_DRM_MODE_IS_420_XXX
 bool drm_mode_is_420_only(const struct drm_display_info *display,
 		const struct drm_display_mode *mode);
