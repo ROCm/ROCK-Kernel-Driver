72bd2b8ca94aae9b0b0ff4feea26ef23f01baf96
Felix Kuehling <Felix.Kuehling@amd.com>
2021-01-20 14:29:34 +0800

    drm/amdkcl: Work around mmu_notifier_put issue on RHEL 8.3
    The DRM backport from kernel 5.6 includes some MMU notifier changes
    that cause problems with the mmu_notifier_put function. The
    free_notifier never gets called. This leads to a leak of kfd_process
    structures and their doorbells.
    
    Work around this by falling back to the old method of releasing the
    MMU notifier and destryoing the process structure.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- include/kcl/kcl_mn.h
+++ include/kcl/kcl_mn.h
@@ -4,6 +4,12 @@
 
 #include <linux/mmu_notifier.h>
 
+/* mmu_notifier_put in the RH DRM backport from 5.6 is broken */
+#if DRM_VER == 5 && DRM_PATCH == 6 && \
+	LINUX_VERSION_CODE == KERNEL_VERSION(4, 18, 0)
+#undef HAVE_MMU_NOTIFIER_PUT
+#endif
+
 /* Copied from v3.16-6588-gb972216e27d1 include/linux/mmu_notifier.h */
 #if !defined(HAVE_MMU_NOTIFIER_CALL_SRCU) && \
 	!defined(HAVE_MMU_NOTIFIER_PUT)
