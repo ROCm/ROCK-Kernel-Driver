5f28ed37e2d234db939cdbe07f5604e8debd488f
Rui Teng <rui.teng@amd.com>
2021-02-02 15:07:26 +0800

    Bump AMDGPU version to 5.9.18
    Change-Id: I3553a896e31e4cb1d7318e818ef96ee500e49ebe
    Signed-off-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -95,7 +95,7 @@
 #define KMS_DRIVER_MINOR	41
 #define KMS_DRIVER_PATCHLEVEL	0
 
-#define AMDGPU_VERSION		"5.9.17"
+#define AMDGPU_VERSION		"5.9.18"
 
 int amdgpu_vram_limit;
 int amdgpu_vis_vram_limit;
