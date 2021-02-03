e133edb0c4c89e4262b9ce390f79bd689930e944
Flora Cui <flora.cui@amd.com>
2021-02-03 16:22:49 +0800

    drm/amdkcl: fake drm_gem_ttm_{vmap,vunmap}
    Signed-off-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/header/drm/drm_gem_ttm_helper.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_DRM_VBLANK_H_H_
+#define _KCL_HEADER_DRM_VBLANK_H_H_
+
+#ifdef HAVE_DRM_DRM_DRM_GEM_TTM_HELPER_H
+#include_next <drm/drm_gem_ttm_helper.h>
+#endif
+
+#endif
