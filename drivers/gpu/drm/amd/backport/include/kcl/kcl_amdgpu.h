635093f6b705b60cd9ca8adc950df9246d248dcb
Flora Cui <flora.cui@amd.com>
2021-02-07 17:20:33 +0800

    drm/amdkcl: rework test for drm_calc_vbltimestamp_from_scanoutpos
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/backport/include/kcl/kcl_amdgpu.h
+++ drivers/gpu/drm/amd/backport/include/kcl/kcl_amdgpu.h
@@ -7,41 +7,28 @@
 #include <drm/drm_drv.h>
 
 #ifndef HAVE_STRUCT_DRM_CRTC_FUNCS_GET_VBLANK_TIMESTAMP
-
-#if defined(HAVE_VGA_USE_UNSIGNED_INT_PIPE)
 static inline u32 kcl_amdgpu_get_vblank_counter_kms(struct drm_device *dev, unsigned int crtc)
-#else
-static inline u32 kcl_amdgpu_get_vblank_counter_kms(struct drm_device *dev, int crtc)
-#endif
 {
 	struct drm_crtc *drm_crtc = drm_crtc_from_index(dev, crtc);
 
 	return amdgpu_get_vblank_counter_kms(drm_crtc);
 }
 
-#if defined(HAVE_VGA_USE_UNSIGNED_INT_PIPE)
 static inline int kcl_amdgpu_enable_vblank_kms(struct drm_device *dev, unsigned int crtc)
-#else
-static inline int kcl_amdgpu_enable_vblank_kms(struct drm_device *dev, int crtc)
-#endif
 {
 	struct drm_crtc *drm_crtc = drm_crtc_from_index(dev, crtc);
 
 	return amdgpu_enable_vblank_kms(drm_crtc);
 }
 
-#if defined(HAVE_VGA_USE_UNSIGNED_INT_PIPE)
 static inline void kcl_amdgpu_disable_vblank_kms(struct drm_device *dev, unsigned int crtc)
-#else
-static inline void kcl_amdgpu_disable_vblank_kms(struct drm_device *dev, int crtc)
-#endif
 {
 	struct drm_crtc *drm_crtc = drm_crtc_from_index(dev, crtc);
 
 	return amdgpu_disable_vblank_kms(drm_crtc);
 }
 
-#if defined(HAVE_GET_SCANOUT_POSITION_RETURN_BOOL)
+#if defined(HAVE_DRM_DRIVER_GET_SCANOUT_POSITION_RETURN_BOOL)
 static inline bool kcl_amdgpu_get_crtc_scanout_position(struct drm_device *dev, unsigned int pipe,
 				 bool in_vblank_irq, int *vpos, int *hpos,
 				 ktime_t *stime, ktime_t *etime,
@@ -49,75 +36,57 @@ static inline bool kcl_amdgpu_get_crtc_scanout_position(struct drm_device *dev, 
 {
 	return !!amdgpu_display_get_crtc_scanoutpos(dev, pipe, in_vblank_irq, vpos, hpos, stime, etime, mode);
 }
-#elif defined(HAVE_VGA_USE_UNSIGNED_INT_PIPE)
-static inline int kcl_amdgpu_get_crtc_scanout_position(struct drm_device *dev, unsigned int crtc,
+#else
+static inline int kcl_amdgpu_get_crtc_scanout_position(struct drm_device *dev, unsigned int pipe,
 				   unsigned int flags, int *vpos, int *hpos,
 				   ktime_t *stime, ktime_t *etime,
 				   const struct drm_display_mode *mode)
 {
-	return amdgpu_display_get_crtc_scanoutpos(dev, crtc, flags, vpos, hpos, stime, etime, mode);
-}
-#elif defined(HAVE_GET_SCANOUT_POSITION_HAS_DRM_DISPLAY_MODE_ARG)
-static inline int kcl_amdgpu_get_crtc_scanout_position(struct drm_device *dev, int crtc,
-					   unsigned int flags,
-					   int *vpos, int *hpos,
-					   ktime_t *stime, ktime_t *etime,
-					   const struct drm_display_mode *mode)
-{
-	return amdgpu_display_get_crtc_scanoutpos(dev, crtc, flags, vpos, hpos, stime, etime, mode);
-}
-#elif defined(HAVE_GET_SCANOUT_POSITION_HAS_TIMESTAMP_ARG)
-static inline int kcl_amdgpu_get_crtc_scanout_position(struct drm_device *dev, int crtc,
-					   int *vpos, int *hpos, ktime_t *stime,
-					   ktime_t *etime)
-{
-	return amdgpu_display_get_crtc_scanoutpos(dev, crtc, 0, vpos, hpos, stime, etime, NULL);
-}
-#else
-static inline int kcl_amdgpu_get_crtc_scanout_position(struct drm_device *dev, int crtc,
-					   int *vpos, int *hpos)
-{
-	return amdgpu_display_get_crtc_scanoutpos(dev, crtc, 0, vpos, hpos, NULL, NULL, NULL);
+	return amdgpu_display_get_crtc_scanoutpos(dev, pipe, flags, vpos, hpos, stime, etime, mode);
 }
 #endif
 
-#if defined(HAVE_GET_VBLANK_TIMESTAMP_IN_DRM_DRIVER_HAS_KTIME_T)
+#if defined(HAVE_DRM_DRIVER_GET_VBLANK_TIMESTAMP_USE_KTIMER_T_ARG)
 static inline bool kcl_amdgpu_get_vblank_timestamp_kms(struct drm_device *dev, unsigned int pipe,
 					int *max_error,	ktime_t *vblank_time,
 					bool in_vblank_irq)
 {
 	return drm_calc_vbltimestamp_from_scanoutpos(dev, pipe, max_error, vblank_time, in_vblank_irq);
 }
-#elif defined(HAVE_GET_VBLANK_TIMESTAMP_IN_DRM_DRIVER_HAS_BOOL_IN_VBLANK_IRQ)
+#elif defined(HAVE_DRM_DRIVER_GET_VBLANK_TIMESTAMP_RETURN_BOOL)
 static inline bool kcl_amdgpu_get_vblank_timestamp_kms(struct drm_device *dev, unsigned int pipe,
 						int *max_error, struct timeval *vblank_time,
 						bool in_vblank_irq)
 {
-	return !!amdgpu_get_vblank_timestamp_kms(dev, pipe, max_error, vblank_time, in_vblank_irq);
+	return drm_calc_vbltimestamp_from_scanoutpos(dev, pipe, max_error, vblank_time, in_vblank_irq);
 }
-#elif defined(HAVE_GET_VBLANK_TIMESTAMP_IN_DRM_DRIVER_RETURN_BOOL)
-static inline bool kcl_amdgpu_get_vblank_timestamp_kms(struct drm_device *dev, unsigned int pipe,
-						int *max_error, struct timeval *vblank_time,
-						unsigned flags)
-{
-	return !!amdgpu_get_vblank_timestamp_kms(dev, pipe, max_error, vblank_time, flags);
-}
-#elif defined(HAVE_VGA_USE_UNSIGNED_INT_PIPE)
+#else
 static inline int kcl_amdgpu_get_vblank_timestamp_kms(struct drm_device *dev, unsigned int pipe,
 					int *max_error,	struct timeval *vblank_time,
 					unsigned flags)
 {
-	return amdgpu_get_vblank_timestamp_kms(dev, pipe, max_error, vblank_time, flags);
+	struct drm_crtc *crtc;
+	struct amdgpu_device *adev = drm_to_adev(dev);
+
+	if (pipe >= dev->num_crtcs) {
+		DRM_ERROR("Invalid crtc %u\n", pipe);
+		return -EINVAL;
+	}
+
+	/* Get associated drm_crtc: */
+	crtc = &adev->mode_info.crtcs[pipe]->base;
+	if (!crtc) {
+		/* This can occur on driver load if some component fails to
+		 * initialize completely and driver is unloaded */
+		DRM_ERROR("Uninitialized crtc %d\n", pipe);
+		return -EINVAL;
+	}
+
+	return drm_calc_vbltimestamp_from_scanoutpos(dev, pipe, max_error,
+						vblank_time, flags,
+						&crtc->hwmode);
 }
-#else
-static inline int kcl_amdgpu_get_vblank_timestamp_kms(struct drm_device *dev, int crtc,
-					int *max_error,
-					struct timeval *vblank_time,
-					unsigned flags)
-{
-	return amdgpu_get_vblank_timestamp_kms(dev, crtc, max_error, vblank_time, flags);
-}
-#endif
+#endif /* HAVE_DRM_DRIVER_GET_VBLANK_TIMESTAMP_USE_KTIMER_T_ARG */
 #endif /* HAVE_STRUCT_DRM_CRTC_FUNCS_GET_VBLANK_TIMESTAMP */
 
 #endif /* AMDGPU_BACKPORT_KCL_AMDGPU_H */
