164d43082ea8193040a21350da50b64532b20293
Leslie Shi <Yuliang.Shi@amd.com>
2021-03-01 15:43:59 +0800

    drm/amdkcl: adapt for drm_mm_insert_node prototype change
    This is caused "drm/amdgpu: reserve backup pages for bad page retirment"
    v5.9-rc5-2461-g3a5cce0da738
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/backport/kcl_drm_mm_backport.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef AMDKCL_DRM_MM_H
+#define AMDKCL_DRM_MM_H
+
+/**
+ * interface change in mainline kernel 4.10
+ * v4.10-rc5-1060-g4e64e5539d15 drm: Improve drm_mm search (and fix topdown allocation) 
+ * with rbtrees
+ */
+
+#include <drm/drm_mm.h>
+
+#ifndef HAVE_DRM_MM_INSERT_NODE_THREE_PARAMETERS
+static inline int _kcl_drm_mm_insert_node(struct drm_mm *mm,
+				          struct drm_mm_node *node,
+				          u64 size)
+{
+        return drm_mm_insert_node(mm, node, size, 0, DRM_MM_SEARCH_DEFAULT);
+}
+#define drm_mm_insert_node _kcl_drm_mm_insert_node
+#endif /* HAVE_DRM_MM_INSERT_NODE_THREE_PARAMETERS */
+
+#endif /* AMDKCL_DRM_MM_H */
