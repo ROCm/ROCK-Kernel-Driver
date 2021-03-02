cac66db534543b9f6255d50e13ec1f00d623f38b
Leslie Shi <Yuliang.Shi@amd.com>
2021-03-02 15:22:04 +0800

    drm/amdkcl: Replace DEFINE_SIMPLE_ATTRIBUTE with DEFINE_DEBUGFS_ATTRIBUTE
    This is caused by "drm/amdgpu: Replace DEFINE_SIMPLE_ATTRIBUTE with DEFINE_DEBUGFS_ATTRIBUTE"
    v5.9-rc5-2487-gd7ce9ec795fb
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_debugfs.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_debugfs.c
@@ -1644,11 +1644,13 @@ out:
 	return ret;
 }
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 DEFINE_DEBUGFS_ATTRIBUTE(fops_ib_preempt, NULL,
 			amdgpu_debugfs_ib_preempt, "%llu\n");
 
 DEFINE_DEBUGFS_ATTRIBUTE(fops_sclk_set, NULL,
 			amdgpu_debugfs_sclk_set, "%llu\n");
+#endif
 
 int amdgpu_debugfs_init(struct amdgpu_device *adev)
 {
@@ -1659,6 +1661,7 @@ int amdgpu_debugfs_init(struct amdgpu_device *adev)
 	if (!debugfs_initialized())
 		return 0;
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 	debugfs_create_x32("amdgpu_smu_debug", 0600, root,
 			   &adev->pm.smu_debug_mask);
 
@@ -1675,6 +1678,7 @@ int amdgpu_debugfs_init(struct amdgpu_device *adev)
 		DRM_ERROR("unable to create amdgpu_set_sclk debugsfs file\n");
 		return PTR_ERR(ent);
 	}
+#endif
 
 	/* Register debugfs entries for amdgpu_ttm */
 	amdgpu_ttm_debugfs_init(adev);
