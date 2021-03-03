2222b6b13e8fc1cba93dd99dfbd48ef508e57c56
Leslie Shi <Yuliang.Shi@amd.com>
2021-03-03 15:25:31 +0800

    drm/amdkcl: wrapper the code of max bpc debugfs under HAVE_DRM_CONNECTOR_PROPERTY_MAX_BPC
    This patch is caused by 'drm/amd/display: Add max bpc debugfs'
    v5.9-rc5-2502-g33f048960308
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_debugfs.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_debugfs.c
@@ -2281,6 +2281,7 @@ static ssize_t dp_dsc_slice_bpg_offset_read(struct file *f, char __user *buf,
 }
 #endif
 
+#ifdef HAVE_DRM_CONNECTOR_PROPERTY_MAX_BPC
 /*
  * function description: Read max_requested_bpc property from the connector
  *
@@ -2422,6 +2423,7 @@ unlock:
 	kfree(wr_buf);
 	return size;
 }
+#endif /* HAVE_DRM_CONNECTOR_PROPERTY_MAX_BPC */
 
 /*
  * Backlight at this moment.  Read only.
@@ -2580,12 +2582,14 @@ static const struct file_operations dp_dpcd_data_debugfs_fops = {
 	.llseek = default_llseek
 };
 
+#ifdef HAVE_DRM_CONNECTOR_PROPERTY_MAX_BPC
 static const struct file_operations dp_max_bpc_debugfs_fops = {
 	.owner = THIS_MODULE,
 	.read = dp_max_bpc_read,
 	.write = dp_max_bpc_write,
 	.llseek = default_llseek
 };
+#endif
 
 static const struct file_operations dp_dsc_disable_passthrough_debugfs_fops = {
 	.owner = THIS_MODULE,
@@ -2623,8 +2627,12 @@ static const struct {
 		{"dsc_slice_bpg", &dp_dsc_slice_bpg_offset_debugfs_fops},
 		{"dp_dsc_fec_support", &dp_dsc_fec_support_fops},
 		{"max_bpc", &dp_max_bpc_debugfs_fops},
-		{"dsc_disable_passthrough", &dp_dsc_disable_passthrough_debugfs_fops},
 #endif
+
+#ifdef HAVE_DRM_CONNECTOR_PROPERTY_MAX_BPC
+		{"max_bpc", &dp_max_bpc_debugfs_fops},
+#endif
+		{"dsc_disable_passthrough", &dp_dsc_disable_passthrough_debugfs_fops},
 };
 
 #ifdef CONFIG_DRM_AMD_DC_HDCP
