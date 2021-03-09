d638cbf7514190139dee7ca4e1948d933f638dc5
Flora Cui <flora.cui@amd.com>
2021-03-09 10:14:58 +0800

    REWORKME: config.h
    Change-Id: I981875bb459ee1d29553b026e33b316e185250cb
    Signed-off-by: Flora Cui <flora.cui@amd.com>

---
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -0,0 +1,147 @@
+/* config/config.h.  Generated from config.h.in by configure.  */
+/* config/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* *FLAGS_<basetarget>.o support to take the path relative to $(obj) */
+#define HAVE_AMDKCL_FLAGS_TAKE_PATH 1
+
+/* Define to 1 if you have the <asm/fpu/api.h> header file. */
+#define HAVE_ASM_FPU_API_H 1
+
+/* Define to 1 if you have the <asm/set_memory.h> header file. */
+#define HAVE_ASM_SET_MEMORY_H 1
+
+/* Define to 1 if you have the <drm/amdgpu_pciid.h> header file. */
+/* #undef HAVE_DRM_AMDGPU_PCIID_H */
+
+/* Define to 1 if you have the <drm/drmP.h> header file. */
+/* #undef HAVE_DRM_DRMP_H */
+
+/* Define to 1 if you have the <drm/drm_atomic_uapi.h> header file. */
+#define HAVE_DRM_DRM_ATOMIC_UAPI_H 1
+
+/* Define to 1 if you have the <drm/drm_audio_component.h> header file. */
+#define HAVE_DRM_DRM_AUDIO_COMPONENT_H 1
+
+/* Define to 1 if you have the <drm/drm_auth.h> header file. */
+#define HAVE_DRM_DRM_AUTH_H 1
+
+/* Define to 1 if you have the <drm/drm_backport.h> header file. */
+/* #undef HAVE_DRM_DRM_BACKPORT_H */
+
+/* Define to 1 if you have the <drm/drm_connector.h> header file. */
+#define HAVE_DRM_DRM_CONNECTOR_H 1
+
+/* Define to 1 if you have the <drm/drm_debugfs.h> header file. */
+#define HAVE_DRM_DRM_DEBUGFS_H 1
+
+/* Define to 1 if you have the <drm/drm_device.h> header file. */
+#define HAVE_DRM_DRM_DEVICE_H 1
+
+/* Define to 1 if you have the <drm/drm_drv.h> header file. */
+#define HAVE_DRM_DRM_DRV_H 1
+
+/* Define to 1 if you have the <drm/drm_encoder.h> header file. */
+#define HAVE_DRM_DRM_ENCODER_H 1
+
+/* Define to 1 if you have the <drm/drm_file.h> header file. */
+#define HAVE_DRM_DRM_FILE_H 1
+
+/* Define to 1 if you have the <drm/drm_gem_framebuffer_helper.h> header file.
+   */
+#define HAVE_DRM_DRM_GEM_FRAMEBUFFER_HELPER_H 1
+
+/* Define to 1 if you have the <drm/drm_hdcp.h> header file. */
+#define HAVE_DRM_DRM_HDCP_H 1
+
+/* Define to 1 if you have the <drm/drm_ioctl.h> header file. */
+#define HAVE_DRM_DRM_IOCTL_H 1
+
+/* Define to 1 if you have the <drm/drm_irq.h> header file. */
+#define HAVE_DRM_DRM_IRQ_H 1
+
+/* Define to 1 if you have the <drm/drm_managed.h> header file. */
+#define HAVE_DRM_DRM_MANAGED_H 1
+
+/* Define to 1 if you have the <drm/drm_plane.h> header file. */
+#define HAVE_DRM_DRM_PLANE_H 1
+
+/* Define to 1 if you have the <drm/drm_print.h> header file. */
+#define HAVE_DRM_DRM_PRINT_H 1
+
+/* Define to 1 if you have the <drm/drm_probe_helper.h> header file. */
+#define HAVE_DRM_DRM_PROBE_HELPER_H 1
+
+/* Define to 1 if you have the <drm/drm_util.h> header file. */
+#define HAVE_DRM_DRM_UTIL_H 1
+
+/* Define to 1 if you have the <drm/drm_vblank.h> header file. */
+#define HAVE_DRM_DRM_VBLANK_H 1
+
+/* Define to 1 if you have the <drm/task_barrier.h> header file. */
+#define HAVE_DRM_TASK_BARRIER_H 1
+
+/* kallsyms_lookup_name is available */
+/* #undef HAVE_KALLSYMS_LOOKUP_NAME */
+
+/* Define to 1 if you have the <linux/bits.h> header file. */
+#define HAVE_LINUX_BITS_H 1
+
+/* Define to 1 if you have the <linux/compiler_attributes.h> header file. */
+#define HAVE_LINUX_COMPILER_ATTRIBUTES_H 1
+
+/* Define to 1 if you have the <linux/dma-attrs.h> header file. */
+/* #undef HAVE_LINUX_DMA_ATTRS_H */
+
+/* Define to 1 if you have the <linux/dma-fence.h> header file. */
+#define HAVE_LINUX_DMA_FENCE_H 1
+
+/* Define to 1 if you have the <linux/dma-resv.h> header file. */
+#define HAVE_LINUX_DMA_RESV_H 1
+
+/* Define to 1 if you have the <linux/io-64-nonatomic-lo-hi.h> header file. */
+#define HAVE_LINUX_IO_64_NONATOMIC_LO_HI_H 1
+
+/* Define to 1 if you have the <linux/mem_encrypt.h> header file. */
+#define HAVE_LINUX_MEM_ENCRYPT_H 1
+
+/* Define to 1 if you have the <linux/mmap_lock.h> header file. */
+#define HAVE_LINUX_MMAP_LOCK_H 1
+
+/* Define to 1 if you have the <linux/nospec.h> header file. */
+#define HAVE_LINUX_NOSPEC_H 1
+
+/* Define to 1 if you have the <linux/overflow.h> header file. */
+#define HAVE_LINUX_OVERFLOW_H 1
+
+/* Define to 1 if you have the <linux/pci-p2pdma.h> header file. */
+#define HAVE_LINUX_PCI_P2PDMA_H 1
+
+/* Define to 1 if you have the <linux/sched/mm.h> header file. */
+#define HAVE_LINUX_SCHED_MM_H 1
+
+/* Define to 1 if you have the <linux/sched/signal.h> header file. */
+#define HAVE_LINUX_SCHED_SIGNAL_H 1
+
+/* Define to 1 if you have the <linux/sched/task.h> header file. */
+#define HAVE_LINUX_SCHED_TASK_H 1
+
+/* Define to 1 if you have the <uapi/linux/sched/types.h> header file. */
+#define HAVE_UAPI_LINUX_SCHED_TYPES_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "amdgpu-dkms"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "amdgpu-dkms 19.40"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "amdgpu-dkms"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "19.40"
