ec870cabdfe139878c6204e2a37e9c03cd676720
Nirmoy Das <nirmoy.das@amd.com>
2021-03-15 16:02:37 +0100

    drm/amdgpu: make BO type check less restrictive
    BO with ttm_bo_type_sg type can also have tiling_flag and metadata.
    So so BO type check for only ttm_bo_type_kernel.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Nirmoy Das <nirmoy.das@amd.com>
    Reported-by: Tom StDenis <Tom.StDenis@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
@@ -1204,7 +1204,7 @@ int amdgpu_bo_set_tiling_flags(struct amdgpu_bo *bo, u64 tiling_flags)
 	struct amdgpu_device *adev = amdgpu_ttm_adev(bo->tbo.bdev);
 	struct amdgpu_bo_user *ubo;
 
-	BUG_ON(bo->tbo.type != ttm_bo_type_device);
+	BUG_ON(bo->tbo.type == ttm_bo_type_kernel);
 	if (adev->family <= AMDGPU_FAMILY_CZ &&
 	    AMDGPU_TILING_GET(tiling_flags, TILE_SPLIT) > 6)
 		return -EINVAL;
@@ -1226,7 +1226,7 @@ void amdgpu_bo_get_tiling_flags(struct amdgpu_bo *bo, u64 *tiling_flags)
 {
 	struct amdgpu_bo_user *ubo;
 
-	BUG_ON(bo->tbo.type != ttm_bo_type_device);
+	BUG_ON(bo->tbo.type == ttm_bo_type_kernel);
 	dma_resv_assert_held(amdkcl_ttm_resvp(&bo->tbo));
 	ubo = to_amdgpu_bo_user(bo);
 
@@ -1253,7 +1253,7 @@ int amdgpu_bo_set_metadata (struct amdgpu_bo *bo, void *metadata,
 	struct amdgpu_bo_user *ubo;
 	void *buffer;
 
-	BUG_ON(bo->tbo.type != ttm_bo_type_device);
+	BUG_ON(bo->tbo.type == ttm_bo_type_kernel);
 	ubo = to_amdgpu_bo_user(bo);
 	if (!metadata_size) {
 		if (ubo->metadata_size) {
@@ -1303,7 +1303,7 @@ int amdgpu_bo_get_metadata(struct amdgpu_bo *bo, void *buffer,
 	if (!buffer && !metadata_size)
 		return -EINVAL;
 
-	BUG_ON(bo->tbo.type != ttm_bo_type_device);
+	BUG_ON(bo->tbo.type == ttm_bo_type_kernel);
 	ubo = to_amdgpu_bo_user(bo);
 	if (buffer) {
 		if (buffer_size < ubo->metadata_size)
