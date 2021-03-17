e2503353086140a51c067a9ea0837025ee7ad581
Flora Cui <flora.cui@amd.com>
2021-03-17 15:07:31 +0800

    drm/amdkcl: cleanup HAVE_DRM_MM_INSERT_MODE
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/ttm/backport/backport.h
+++ drivers/gpu/drm/ttm/backport/backport.h
@@ -11,6 +11,7 @@
 #include <kcl/kcl_list.h>
 #include <kcl/backport/kcl_fence_backport.h>
 #include <kcl/backport/kcl_drm_vma_manager_backport.h>
+#include <kcl/backport/kcl_drm_mm_backport.h>
 #include <kcl/kcl_drm_print.h>
 #include <kcl/backport/kcl_mm_backport.h>
 #include <kcl/kcl_sched_mm.h>
