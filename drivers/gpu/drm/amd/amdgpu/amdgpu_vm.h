edace7ace372126540059575c7664299aa267233
Christian König <christian.koenig@amd.com>
2021-03-22 13:33:05 +0100

    drm/amdgpu: re-apply "use the new cursor in the VM code" v2
    Now that we found the underlying problem we can re-apply this patch.
    
    This reverts commit 867fee7f8821ff42e7308088cf0c3450ac49c17c.
    
    v2: rebase on KFD changes
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Acked-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Tested-by: Nirmoy Das <nirmoy.das@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vm.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vm.h
@@ -402,17 +402,15 @@ int amdgpu_vm_handle_moved(struct amdgpu_device *adev,
 			   struct amdgpu_vm *vm);
 
 int amdgpu_vm_bo_update_mapping(struct amdgpu_device *adev,
-                                       struct amdgpu_device *bo_adev,
-                                       struct amdgpu_vm *vm, bool immediate,
-                                       bool unlocked, struct dma_resv *resv,
-                                       uint64_t start, uint64_t last,
-                                       uint64_t flags, uint64_t offset,
-                                       struct ttm_resource *mem,
-                                       struct drm_mm_node *nodes,
-                                       dma_addr_t *pages_addr,
-                                       struct dma_fence **fence,
-                                       uint64_t vram_base_offset);
-
+				struct amdgpu_device *bo_adev,
+				struct amdgpu_vm *vm, bool immediate,
+				bool unlocked, struct dma_resv *resv,
+				uint64_t start, uint64_t last,
+				uint64_t flags, uint64_t offset,
+				struct ttm_resource *res,
+				dma_addr_t *pages_addr,
+				struct dma_fence **fence,
+				uint64_t vram_base_offset);
 int amdgpu_vm_bo_update(struct amdgpu_device *adev,
 			struct amdgpu_bo_va *bo_va,
 			bool clear);
