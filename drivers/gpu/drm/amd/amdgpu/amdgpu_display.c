79eab19997a2e2638925913b2b50daaa043a8f99
Leslie Shi <Yuliang.Shi@amd.com>
2021-03-23 15:41:51 +0800

    drm/amdkcl: adapt code using drm_gem_fb_get_obj for legacy os
    This patch is caused by following commit 'drm/amdgpu: clean up non-DC suspend/resume handling'
    v5.11-2764-ga7c22df2fd07
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -1949,10 +1949,10 @@ int amdgpu_display_suspend_helper(struct amdgpu_device *adev)
 			}
 		}
 
-		if (fb == NULL || fb->obj[0] == NULL) {
+		if (fb == NULL || drm_gem_fb_get_obj(fb, 0) == NULL) {
 			continue;
 		}
-		robj = gem_to_amdgpu_bo(fb->obj[0]);
+		robj = gem_to_amdgpu_bo(drm_gem_fb_get_obj(fb, 0));
 		if (!amdgpu_display_robj_is_fb(adev, robj)) {
 			r = amdgpu_bo_reserve(robj, true);
 			if (r == 0) {
