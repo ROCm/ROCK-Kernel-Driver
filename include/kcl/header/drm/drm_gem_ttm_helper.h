57affe9dc983f335da57db7d4d562a2c1ef49b78
Shiwu Zhang <shiwu.zhang@amd.com>
2021-03-29 20:24:42 +0800

    drm/amdkcl: fix the autotest of drm_gem_ttm_helper.h
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>

--- include/kcl/header/drm/drm_gem_ttm_helper.h
+++ include/kcl/header/drm/drm_gem_ttm_helper.h
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: MIT */
-#ifndef _KCL_HEADER_DRM_VBLANK_H_H_
-#define _KCL_HEADER_DRM_VBLANK_H_H_
+#ifndef _KCL_HEADER_DRM_GEM_TTM_HELPER_H_H_
+#define _KCL_HEADER_DRM_GEM_TTM_HELPER_H_H_
 
-#ifdef HAVE_DRM_DRM_DRM_GEM_TTM_HELPER_H
+#ifdef HAVE_DRM_DRM_GEM_TTM_HELPER_H
 #include_next <drm/drm_gem_ttm_helper.h>
 #endif
 
