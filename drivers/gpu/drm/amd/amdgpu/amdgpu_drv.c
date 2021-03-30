135fbb98fa59e3b013ac27d48947a58409c10f29
Rui Teng <rui.teng@amd.com>
2021-03-30 15:52:13 +0800

    Bump AMDGPU version to 5.11.1
    Change-Id: I5d907f26f10cd3765593cac4e9cc00fd1d4a38da
    Signed-off-by: Rui Teng <rui.teng@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -97,7 +97,7 @@
 #define KMS_DRIVER_MINOR	41
 #define KMS_DRIVER_PATCHLEVEL	0
 
-#define AMDGPU_VERSION		"5.11.0"
+#define AMDGPU_VERSION		"5.11.1"
 
 int amdgpu_vram_limit;
 int amdgpu_vis_vram_limit;
