da03c33d70234b08a59804e2338ed060bed3cc13
Leslie Shi <Yuliang.Shi@amd.com>
2021-04-13 13:10:01 +0800

    drm/amdkcl: wrap the code under DEFINE_DEBUGFS_ATTRIBUTE
    This is caused by "drm/amd/display: Add debugfs to repress HPD and HPR_RX IRQs"
    v5.11-2975-g14bc8d04dd21
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_debugfs.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_debugfs.c
@@ -3712,6 +3712,7 @@ DEFINE_DEBUGFS_ATTRIBUTE(force_timing_sync_ops, force_timing_sync_get,
 #endif
 
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 /*
  * Disables all HPD and HPD RX interrupt handling in the
  * driver when set to 1. Default is 0.
@@ -3741,6 +3742,7 @@ static int disable_hpd_get(void *data, u64 *val)
 
 DEFINE_DEBUGFS_ATTRIBUTE(disable_hpd_ops, disable_hpd_get,
 			 disable_hpd_set, "%llu\n");
+#endif
 
 /*
  * Prints hardware capabilities. These are used for IGT testing.
