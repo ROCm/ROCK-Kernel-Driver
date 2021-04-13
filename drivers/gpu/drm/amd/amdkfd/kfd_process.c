66002794a70544617cb2e98ad8ff4eda4d81d667
Shiwu Zhang <shiwu.zhang@amd.com>
2021-04-13 19:22:51 +0800

    drm/amdkfd: skip the interrupt drain processing for ASICs before V9
    As the interrupt drain feature is for the debugger, which is not
    supported before gfx v9. Then just skip it for ASICs before V9.
    
    Otherwise, the user process will hang on creating new compute queues.
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Fix-suggested-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_process.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -2272,6 +2272,47 @@ void kfd_flush_tlb(struct kfd_process_device *pdd, enum TLB_FLUSH_TYPE type)
 	}
 }
 
+/* assumes caller holds process lock. */
+void kfd_process_drain_interrupts(struct kfd_process_device *pdd)
+{
+	uint32_t irq_drain_fence[8];
+
+	if (pdd->dev->device_info->asic_family < CHIP_VEGA10)
+		return;
+
+	pdd->process->irq_drain_is_open = true;
+
+	memset(irq_drain_fence, 0, sizeof(irq_drain_fence));
+	irq_drain_fence[0] = (KFD_IRQ_FENCE_SOURCEID << 8) |
+							KFD_IRQ_FENCE_CLIENTID;
+	irq_drain_fence[3] = pdd->process->pasid;
+
+	/* ensure stale irqs scheduled KFD interrupts and send drain fence. */
+	if (amdgpu_amdkfd_send_close_event_drain_irq(pdd->dev->kgd,
+							irq_drain_fence)) {
+		pdd->process->irq_drain_is_open = false;
+		return;
+	}
+
+	if (wait_event_interruptible(pdd->process->wait_irq_drain,
+				!READ_ONCE(pdd->process->irq_drain_is_open)))
+		pdd->process->irq_drain_is_open = false;
+}
+
+void kfd_process_close_interrupt_drain(unsigned int pasid)
+{
+	struct kfd_process *p;
+
+	p = kfd_lookup_process_by_pasid(pasid);
+
+	if (!p)
+		return;
+
+	WRITE_ONCE(p->irq_drain_is_open, false);
+	wake_up_all(&p->wait_irq_drain);
+	kfd_unref_process(p);
+}
+
 struct kfd_process_device *kfd_process_device_data_by_id(struct kfd_process *p, uint32_t gpu_id)
 {
 	int i;
