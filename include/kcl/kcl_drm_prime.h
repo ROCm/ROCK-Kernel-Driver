80d9f35b242e367e166e7dd1777a0d19049cc138
Shiwu Zhang <shiwu.zhang@amd.com>
2021-04-29 16:58:17 +0800

    drm/amdkcl: fix sg to page arrays callback for legacy OS
    To call the legacy drm_prime_sg_to_page_addr_arrays, the 2nd parameter
    should be page array instead of NULL pointer
    
    Otherwise, the page fault will be triggered
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_drm_prime.h
+++ include/kcl/kcl_drm_prime.h
@@ -12,8 +12,19 @@ static inline
 int drm_prime_sg_to_dma_addr_array(struct sg_table *sgt, dma_addr_t *addrs,
 				   int max_entries)
 {
+#ifdef HAVE_TTM_SG_TT_INIT
 	return drm_prime_sg_to_page_addr_arrays(sgt, NULL, addrs, max_entries);
-
-}
+#else
+	/*
+	 * the page array stands right next to dma address array,
+	 * so get the page array pointer directly by max_entries offset
+	 * refer to ttm_sg_tt_init() for initial array allocation and
+	 * c67e62790f5c drm/prime: split array import functions v4 for
+	 * the change to drm_prime_sg_to_page_addr_arrays()
+	 */
+	struct page **pages = (void*)addrs - max_entries;
+	return drm_prime_sg_to_page_addr_arrays(sgt, pages, addrs, max_entries);
 #endif
+}
+#endif /* HAVE_DRM_PRIME_SG_TO_DMA_ADDR_ARRAY */
 #endif
