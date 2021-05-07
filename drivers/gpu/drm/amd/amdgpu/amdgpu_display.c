c8b6f9fa897e625785025e3582d8a57dd8a44dd9
Leslie Shi <Yuliang.Shi@amd.com>
2021-05-07 13:59:37 +0800

    drm/amdkcl: wrap the code under HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
    This is caused by "drm/amdgpu: Use device specific BO size & stride check."
    v5.11-3225-gdf0c606a6ced
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -1140,6 +1140,7 @@ static int check_tiling_flags_gfx6(struct amdgpu_framebuffer *afb)
 	}
 }
 
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 static void get_block_dimensions(unsigned int block_log2, unsigned int cpp,
 				 unsigned int *width, unsigned int *height)
 {
@@ -1222,16 +1223,15 @@ static int amdgpu_display_verify_plane(struct amdgpu_framebuffer *rfb, int plane
 		(uint64_t)rfb->base.pitches[plane] / block_pitch *
 		block_size * DIV_ROUND_UP(height, block_height);
 
-	if (rfb->base.obj[0]->size < size) {
+	if (drm_gem_fb_get_obj(&rfb->base, 0)->size < size) {
 		drm_dbg_kms(rfb->base.dev,
 			    "BO size 0x%zx is less than 0x%llx required for plane %d\n",
-			    rfb->base.obj[0]->size, size, plane);
+			    drm_gem_fb_get_obj(&rfb->base, 0)->size, size, plane);
 		return -EINVAL;
 	}
 
 	return 0;
 }
-
 
 static int amdgpu_display_verify_sizes(struct amdgpu_framebuffer *rfb)
 {
@@ -1311,6 +1311,7 @@ static int amdgpu_display_verify_sizes(struct amdgpu_framebuffer *rfb)
 
 	return 0;
 }
+#endif
 
 static int amdgpu_display_get_fb_info(const struct amdgpu_framebuffer *amdgpu_fb,
 				      uint64_t *tiling_flags, bool *tmz_surface)
@@ -1379,6 +1380,8 @@ static int amdgpu_display_gem_fb_verify_and_init(struct drm_device *dev,
 
 	kcl_drm_gem_fb_set_obj(&rfb->base, 0, obj);
 	drm_helper_mode_fill_fb_struct(dev, &rfb->base, mode_cmd);
+
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 	/* Verify that the modifier is supported. */
 	if (!drm_any_plane_has_format(dev, mode_cmd->pixel_format,
 				      mode_cmd->modifier[0])) {
@@ -1389,6 +1392,7 @@ static int amdgpu_display_gem_fb_verify_and_init(struct drm_device *dev,
 		ret = -EINVAL;
 		goto err;
 	}
+#endif
 
 	ret = amdgpu_display_framebuffer_init(dev, rfb, mode_cmd, obj);
 	if (ret)
@@ -1488,29 +1492,34 @@ amdgpu_display_user_framebuffer_create(struct drm_device *dev,
 	domains = amdgpu_display_supported_domains(drm_to_adev(dev), bo->flags);
 	if (obj->import_attach && !(domains & AMDGPU_GEM_DOMAIN_GTT)) {
 		drm_dbg_kms(dev, "Cannot create framebuffer from imported dma_buf\n");
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 		drm_gem_object_put(obj);
+#endif
 		return ERR_PTR(-EINVAL);
 	}
 
 	amdgpu_fb = kzalloc(sizeof(*amdgpu_fb), GFP_KERNEL);
 	if (amdgpu_fb == NULL) {
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 		drm_gem_object_put(obj);
+#endif
 		return ERR_PTR(-ENOMEM);
 	}
 
-#ifdef HAVE_DRM_GEN_FB_INIT_WITH_FUNCS
 	ret = amdgpu_display_gem_fb_verify_and_init(dev, amdgpu_fb, file_priv,
 						    mode_cmd, obj);
-#else
-	ret = amdgpu_display_gem_fb_init(dev, amdgpu_fb, mode_cmd, obj);
-#endif
 	if (ret) {
 		kfree(amdgpu_fb);
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 		drm_gem_object_put(obj);
+#endif
 		return ERR_PTR(ret);
 	}
 
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 	drm_gem_object_put(obj);
+#endif
+
 	return &amdgpu_fb->base;
 }
 
