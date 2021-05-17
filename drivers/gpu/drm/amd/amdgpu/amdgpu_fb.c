4dbd7a6f1b1853b6a7fbfe8d43a95b728e629afb
Jingwen Chen <Jingwen.Chen2@amd.com>
2021-05-17 16:16:10 +0800

    drm/amd/amdgpu: fix refcount leak
    [Why]
    the gem object rfb->base.obj[0] is get according to num_planes
    in amdgpufb_create, but is not put according to num_planes
    
    [How]
    put rfb->base.obj[0] in amdgpu_fbdev_destroy according to num_planes
    
    Signed-off-by: Jingwen Chen <Jingwen.Chen2@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fb.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fb.c
@@ -299,11 +299,14 @@ static int amdgpu_fbdev_destroy(struct drm_device *dev, struct amdgpu_fbdev *rfb
 {
 	struct amdgpu_framebuffer *rfb = &rfbdev->rfb;
 	struct drm_gem_object * obj = NULL;
+	int i;
 
 	drm_fb_helper_unregister_fbi(&rfbdev->helper);
 	obj = drm_gem_fb_get_obj(&rfb->base, 0);
 
 	if (obj) {
+		for (i = 0; i < rfb->base.format->num_planes; i++)
+			drm_gem_object_put(obj);
 		amdgpufb_destroy_pinned_object(obj);
 		kcl_drm_gem_fb_set_obj(&rfb->base, 0, NULL);
 		drm_framebuffer_unregister_private(&rfb->base);
