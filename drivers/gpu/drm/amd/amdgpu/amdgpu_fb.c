8d5b18e4c86b5ec1b6bac671ae6632d7b4c7aaee
Leslie Shi <Yuliang.Shi@amd.com>
2021-05-18 15:53:33 +0800

    drm/amdkcl: adapt code for legacy os
    This is caused by following commits:
    d9e1c5963c97 drm/amd/amdgpu: fix refcount leak
    9faf262c32d3 drm/amdgpu: Add DMA mapping of GTT BOs
    ac89ed0789f8 drm/amdgpu: Move kfd_mem_attach outside reservation
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fb.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fb.c
@@ -304,10 +304,12 @@ static int amdgpu_fbdev_destroy(struct drm_device *dev, struct amdgpu_fbdev *rfb
 	drm_fb_helper_unregister_fbi(&rfbdev->helper);
 	obj = drm_gem_fb_get_obj(&rfb->base, 0);
 
-	if (rfb->base.obj[0]) {
+	if (obj) {
+#ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 		for (i = 0; i < rfb->base.format->num_planes; i++)
-			drm_gem_object_put(rfb->base.obj[0]);
-		amdgpufb_destroy_pinned_object(rfb->base.obj[0]);
+			drm_gem_object_put(obj);
+#endif
+		amdgpufb_destroy_pinned_object(obj);
 		kcl_drm_gem_fb_set_obj(&rfb->base, 0, NULL);
 		drm_framebuffer_unregister_private(&rfb->base);
 		drm_framebuffer_cleanup(&rfb->base);
