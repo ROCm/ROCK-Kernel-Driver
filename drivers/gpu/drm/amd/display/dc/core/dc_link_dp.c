7ae518f0aa71ba52e903dec48737b22fa8fd79c5
Leslie Shi <Yuliang.Shi@amd.com>
2021-05-28 10:59:33 +0800

    drm/amdkcl: wrap code under CONFIG_DRM_AMD_DC_DSC_SUPPORT
    This is caused by "drm/amd/display: add exit training mode and update channel coding in LT"
    v5.11-3511-g43b75506451a
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
@@ -1582,8 +1582,10 @@ static void override_training_settings(
 	if (overrides->enhanced_framing != NULL)
 		lt_settings->enhanced_framing = *overrides->enhanced_framing;
 
+#ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 	if (link->preferred_training_settings.fec_enable != NULL)
 		lt_settings->should_set_fec_ready = *link->preferred_training_settings.fec_enable;
+#endif
 }
 
 uint8_t dp_convert_to_count(uint8_t lttpr_repeater_count)
@@ -2645,7 +2647,9 @@ enum link_training_result dc_link_dp_perform_link_training(
 
 	/* configure link prior to entering training mode */
 	dpcd_configure_lttpr_mode(link, &lt_settings);
+#ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 	dp_set_fec_ready(link, link_res, lt_settings.should_set_fec_ready);
+#endif
 	dpcd_configure_channel_coding(link, &lt_settings);
 
 	/* enter training mode:
