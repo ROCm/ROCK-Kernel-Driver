f11f6ce43b354a53a66c1bb4ec611a0979ab695a
Fangzhi Zuo <Jerry.Zuo@amd.com>
2021-06-09 15:21:35 -0400

    drm/amd/display: Add DP 2.0 HPO Stream Encoder
    HW Blocks:
    
            +--------+  +-----+  +------+
            |  OPTC  |  | HDA |  | HUBP |
            +--------+  +-----+  +------+
                |          |        |
                |          |        |
        HPO ====|==========|========|====
         |      |          v        |
         |      |       +-----+     |
         |      |       | APG |     |
         |      |       +-----+     |
         |      |          |        |
         v      v          v        v
               +----------------------+
               |  HPO Stream Encoder  |
               +----------------------+
    
    Signed-off-by: Fangzhi Zuo <Jerry.Zuo@amd.com>
    Reviewed-by: Nicholas Kazlauskas <Nicholas.Kazlauskas@amd.com>

--- drivers/gpu/drm/amd/display/dc/inc/hw/hw_shared.h
+++ drivers/gpu/drm/amd/display/dc/inc/hw/hw_shared.h
@@ -40,6 +40,9 @@
 #if defined(CONFIG_DRM_AMD_DC_DCN2_x)
 #define MAX_DWB_PIPES	1
 #endif
+#if defined(CONFIG_DRM_AMD_DC_DCN)
+#define MAX_HPO_DP2_ENCODERS	4
+#endif
 
 struct gamma_curve {
 	uint32_t offset;
