506a9cd6f478a229d8e1a896b73171d342cae8ce
Leslie Shi <Yuliang.Shi@amd.com>
2021-06-18 13:14:46 +0800

    drm/amdkcl: fake the sysfs_emit_at
    This is caused by d0d2e18213f7 "amdgpu/pm: replaced snprintf usage in amdgpu_pm.c with sysfs_emit"
    v5.11-3838-gd0d2e18213f7
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_sysfs_emit.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_sysfs_emit.c
@@ -29,4 +29,33 @@ int sysfs_emit(char *buf, const char *fmt, ...)
         return len;
 }
 EXPORT_SYMBOL_GPL(sysfs_emit);
+
+/**
+ *      sysfs_emit_at - scnprintf equivalent, aware of PAGE_SIZE buffer.
+ *      @buf:   start of PAGE_SIZE buffer.
+ *      @at:    offset in @buf to start write in bytes
+ *              @at must be >= 0 && < PAGE_SIZE
+ *      @fmt:   format
+ *      @...:   optional arguments to @fmt
+ *
+ *
+ * Returns number of characters written starting at &@buf[@at].
+ */
+int sysfs_emit_at(char *buf, int at, const char *fmt, ...)
+{
+        va_list args;
+        int len;
+
+        if (WARN(!buf || offset_in_page(buf) || at < 0 || at >= PAGE_SIZE,
+                 "invalid sysfs_emit_at: buf:%p at:%d\n", buf, at))
+                return 0;
+
+        va_start(args, fmt);
+        len = vscnprintf(buf + at, PAGE_SIZE - at, fmt, args);
+        va_end(args);
+
+        return len;
+}
+EXPORT_SYMBOL_GPL(sysfs_emit_at);
+
 #endif
