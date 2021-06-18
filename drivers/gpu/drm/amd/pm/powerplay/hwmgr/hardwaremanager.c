e3ba5b263b2e2d9b132107797fa40758511d419f
Guchun Chen <guchun.chen@amd.com>
2021-06-18 15:32:36 +0800

    Revert "drm/amd/pm: retain the fine grain tuning parameters after resume"
    This reverts commit 1eecb2b0b0f8e9687f3d6a63cd7777160bfeaf51.
    
    Caused regression in SWDEV-291438, so revert it.
    
    Signed-off-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/pm/powerplay/hwmgr/hardwaremanager.c
+++ drivers/gpu/drm/amd/pm/powerplay/hwmgr/hardwaremanager.c
@@ -82,8 +82,7 @@ int phm_enable_dynamic_state_management(struct pp_hwmgr *hwmgr)
 
 	/* Skip for suspend/resume case */
 	if (!hwmgr->pp_one_vf && smum_is_dpm_running(hwmgr)
-	    && !amdgpu_passthrough(adev) && adev->in_suspend
-		&& adev->asic_type != CHIP_RAVEN) {
+	    && !amdgpu_passthrough(adev) && adev->in_suspend) {
 		pr_info("dpm has been enabled\n");
 		return 0;
 	}
