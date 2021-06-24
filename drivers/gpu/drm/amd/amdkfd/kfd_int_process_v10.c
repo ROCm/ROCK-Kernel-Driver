21d9c0026d0e2195c311f9cef9838dc51e1e00e7
Jonathan Kim <jonathan.kim@amd.com>
2021-06-24 18:28:45 -0400

    drm/amdkfd: handle multiple events from trap handler signal
    The trap handler can now send multiple wave events in a single message.
    
    Signed-off-by: Jonathan Kim <jonathan.kim@amd.com>
    Reviewed-by: Felix Kuehling <felix.kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_int_process_v10.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_int_process_v10.c
@@ -144,7 +144,7 @@ static void event_interrupt_wq_v10(struct kfd_dev *dev,
 	} else if (source_id == SOC15_INTSRC_CP_BAD_OPCODE) {
 		kfd_set_dbg_ev_from_interrupt(dev, pasid,
 			KFD_DEBUG_DOORBELL_ID(context_id0),
-			KFD_DEBUG_CP_BAD_OP_ECODE(context_id0),
+			KFD_EC_MASK(KFD_DEBUG_CP_BAD_OP_ECODE(context_id0)),
 			NULL,
 			0);
 	}
@@ -174,7 +174,7 @@ static void event_interrupt_wq_v10(struct kfd_dev *dev,
 		kfd_set_dbg_ev_from_interrupt(dev,
 						pasid,
 						-1,
-						EC_DEVICE_MEMORY_VIOLATION,
+						KFD_EC_MASK(EC_DEVICE_MEMORY_VIOLATION),
 						&exception_data,
 						sizeof(exception_data));
 	} else if (KFD_IRQ_IS_FENCE(client_id, source_id)) {
