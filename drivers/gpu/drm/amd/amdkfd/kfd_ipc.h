8f0338204345f771250fc47ee17909696c3af40a
Philip Yang <Philip.Yang@amd.com>
2021-07-20 22:10:49 -0400

    drm/amdkfd: IPC export and import memory alloc flags
    Support query pointer info memory alloc flags, ex CoarseGrain, for
    shared memory by IPC. App pass memory alloc flags to export handle
    ioctl, to save memory alloc flags to ipc object, import handle ioctl get
    ipc object and pass memory alloc flags back to app which could be
    different process.
    
    Keep import export handle ioctl interface, rename unused _u32 pad field
    to flags.
    
    Signed-off-by: Philip Yang <Philip.Yang@amd.com>
    Reviewed-by: Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_ipc.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_ipc.h
@@ -36,20 +36,23 @@ struct kfd_ipc_obj {
 	struct kref ref;
 	struct dma_buf *dmabuf;
 	uint32_t share_handle[4];
+	uint32_t flags;
 };
 
 int kfd_ipc_import_handle(struct kfd_dev *dev, struct kfd_process *p,
 			  uint32_t gpu_id, uint32_t *share_handle,
 			  uint64_t va_addr, uint64_t *handle,
-			  uint64_t *mmap_offset);
+			  uint64_t *mmap_offset, uint32_t *pflags);
 int kfd_ipc_import_dmabuf(struct kfd_dev *kfd, struct kfd_process *p,
 			  uint32_t gpu_id, int dmabuf_fd,
 			  uint64_t va_addr, uint64_t *handle,
 			  uint64_t *mmap_offset);
 int kfd_ipc_export_as_handle(struct kfd_dev *dev, struct kfd_process *p,
-			     uint64_t handle, uint32_t *ipc_handle);
+			     uint64_t handle, uint32_t *ipc_handle,
+			     uint32_t flags);
 
-int kfd_ipc_store_insert(struct dma_buf *dmabuf, struct kfd_ipc_obj **ipc_obj);
+int kfd_ipc_store_insert(struct dma_buf *dmabuf, struct kfd_ipc_obj **ipc_obj,
+			 uint32_t flags);
 void kfd_ipc_obj_put(struct kfd_ipc_obj **obj);
 
 #endif /* KFD_IPC_H_ */
