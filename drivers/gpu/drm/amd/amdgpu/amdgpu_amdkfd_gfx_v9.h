dd6300430f37eaa36ab72601964209ae63420445
Jonathan Kim <jonathan.kim@amd.com>
2021-07-26 12:14:18 -0400

    drm/amdkfd: disable gfx off per debug trap action
    To prevent the KFD from garbage read/writes on GFX debug registers during
    GFX off, temporarily disable GFX off on each required trap action entry
    (runtime enable/disable, activate/deactivate, launch override, launch
    mode, precise memory and address watch) then re-enable it on trap action
    exit.
    
    If a chip does not support RLC restore of the debug registers, mark the
    chip as unsupported so that it temporarily disables GFX off for the entire
    debug session instead. In addition, the trap configuration registers need
    to be restored by the KFD.  If HSA_ENABLE_DEBUG is set, restoration is
    required on runtime enable in order to save the dispatch pointer in the
    trap temporaries at the proper time.  Otherwise, restore the trap
    configuration on debug activate for general debug trap needs.
    
    As a bonus, abstracting GFX off controls to the KFD debug process call
    level avoids having to maintain GFX off considerations at the KGD level
    per future chips that require the KFD to write to the debug registers.
    
    Signed-off-by: Jonathan Kim <jonathan.kim@amd.com>
    Reviewed-by: Felix Kuehling <felix.kuehling@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v9.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v9.h
@@ -50,6 +50,7 @@ int kgd_gfx_v9_wave_control_execute(struct amdgpu_device *adev,
 bool kgd_gfx_v9_get_atc_vmid_pasid_mapping_info(struct amdgpu_device *adev,
 					uint8_t vmid, uint16_t *p_pasid);
 uint32_t kgd_gfx_v9_enable_debug_trap(struct amdgpu_device *adev,
+				      bool restore_dbg_registers,
 				      uint32_t vmid);
 uint32_t kgd_gfx_v9_disable_debug_trap(struct amdgpu_device *adev, uint32_t vmid);
 int kgd_gfx_v9_set_wave_launch_trap_override(struct amdgpu_device *adev,
