9adb675325c02b9fc6188ee08d710ebcd5ea4073
Alex Deucher <alexander.deucher@amd.com>
2021-07-28 16:31:35 -0400

    drm/amdgpu/display: protect dc_dsc_stream_bandwidth_in_kbps with CONFIG_DRM_AMD_DC_DCN
    It's only used and enabled with DCN, so protect it.
    
    Fixes: b6b76b0315ed7b ("drm/amd/display: Fixed EdidUtility build errors")
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link.c
@@ -3729,11 +3729,13 @@ uint32_t dc_bandwidth_in_kbps_from_timing(
 	uint32_t bits_per_channel = 0;
 	uint32_t kbps;
 
+#if defined(CONFIG_DRM_AMD_DC_DCN)
 	if (timing->flags.DSC)
 		return dc_dsc_stream_bandwidth_in_kbps(timing,
 				timing->dsc_cfg.bits_per_pixel,
 				timing->dsc_cfg.num_slices_h,
 				timing->dsc_cfg.is_dp);
+#endif
 
 	switch (timing->display_color_depth) {
 	case COLOR_DEPTH_666:
