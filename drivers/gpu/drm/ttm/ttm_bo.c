7b1b10da3c22d950b4adccfea9eb976308883c16
Leslie Shi <Yuliang.Shi@amd.com>
2021-08-13 16:26:58 +0800

    drm/amdkcl: access resv field using amdkcl_ttm_resvp
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/ttm/ttm_bo.c
+++ drivers/gpu/drm/ttm/ttm_bo.c
@@ -424,7 +424,7 @@ bool ttm_bo_eviction_valuable(struct ttm_buffer_object *bo,
 	struct ttm_resource *res = bo->resource;
 	struct ttm_device *bdev = bo->bdev;
 
-	dma_resv_assert_held(bo->base.resv);
+	dma_resv_assert_held(amdkcl_ttm_resvp(bo));
 	if (bo->resource->mem_type == TTM_PL_SYSTEM)
 		return true;
 
