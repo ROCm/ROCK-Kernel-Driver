e4428e1c0cb814af227c2a71ade95455c1a88f3b
Asher Song <Asher.Song@amd.com>
2021-08-16 17:04:36 +0800

    drm/amdkcl: add DMA_FENCE_FLAG_USER_BITS macro
    This is caused by de7515d43659f852590645a688f8d493e4a18141 "drm/amd/amdgpu embed hw_fence into amdgpu_job"
    v5.13-2021-gde7515d43659
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_fence.h
+++ include/kcl/kcl_fence.h
@@ -27,6 +27,7 @@
 #define DMA_FENCE_TRACE FENCE_TRACE
 #define DMA_FENCE_FLAG_ENABLE_SIGNAL_BIT FENCE_FLAG_ENABLE_SIGNAL_BIT
 #define DMA_FENCE_FLAG_SIGNALED_BIT FENCE_FLAG_SIGNALED_BIT
+#define DMA_FENCE_FLAG_USER_BITS FENCE_FLAG_USER_BITS
 #define dma_fence_wait fence_wait
 #define dma_fence_get fence_get
 #define dma_fence_put fence_put
