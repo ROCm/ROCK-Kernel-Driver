7b6d18869620b36c6dac18beeef3e2e45cb9a72e
Asher Song <Asher.Song@amd.com>
2021-08-17 18:57:46 +0800

    drm/amdkcl: change CONFIG_DRM_AMD_DC_DCN into CONFIG_DRM_AMD_DC_DCN3_x or CONFIG_DRM_AMD_DC_DCN1_0
    this is caused by 6f2d030645cadab4fa7e16a33907cd9cdc21fd00 "drm/amd/display: Fix multi-display support for idle opt workqueue"
    v5.13-2042-g6f2d030645ca
    
    this is caused by c6961876e7ea96a2e5e06b8e35077cdba748e647 "drm/amd/display: Guard vblank wq flush with DCN guards"
    v5.13-2045-gc6961876e7ea
    
    this is caused by 27fe128f262fdba1be268e450930ec88dc14cd8f "drm/amdgpu/display: fix leftover CONFIG_DRM_AMD_DC_DCN3_1"
    v5.13-2054-g27fe128f262f
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -1350,7 +1350,7 @@ static void amdgpu_dm_fini(struct amdgpu_device *adev)
 {
 	int i;
 
-#if defined(CONFIG_DRM_AMD_DC_DCN)
+#if defined(CONFIG_DRM_AMD_DC_DCN3_x)
 	if (adev->dm.vblank_control_workqueue) {
 		destroy_workqueue(adev->dm.vblank_control_workqueue);
 		adev->dm.vblank_control_workqueue = NULL;
@@ -9613,7 +9613,7 @@ static void amdgpu_dm_commit_planes(struct drm_atomic_state *state,
 	/* Update the planes if changed or disable if we don't have any. */
 	if ((planes_count || acrtc_state->active_planes == 0) &&
 		acrtc_state->stream) {
-#if defined(CONFIG_DRM_AMD_DC_DCN)
+#if defined(CONFIG_DRM_AMD_DC_DCN3_x)
 		/*
 		 * If PSR or idle optimizations are enabled then flush out
 		 * any pending work before hardware programming.
@@ -10066,7 +10066,7 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 	if (dc_state) {
 		/* if there mode set or reset, disable eDP PSR */
 		if (mode_set_reset_required) {
-#if defined(CONFIG_DRM_AMD_DC_DCN)
+#if defined(CONFIG_DRM_AMD_DC_DCN3_x)
 			flush_workqueue(dm->vblank_control_workqueue);
 #endif
 			amdgpu_dm_psr_disable_all(dm);
