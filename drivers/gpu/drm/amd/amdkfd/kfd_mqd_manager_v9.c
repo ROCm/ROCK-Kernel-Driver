4cf694567ddc8e9ae350bf594536390d671330c2
Jonathan Kim <jonathan.kim@amd.com>
2021-08-19 12:51:42 -0400

    drm/amdkfd: remove user control to save dispatch ptr through mqd
    A dispatch that hits CWSR midway through will fail to save the dispatch
    pointer to the ttmps if done in the MQD through the CP_HQD_HQ_STATUS0
    reserved bit since the update of that bit is done on the dispatch
    boundary.
    
    For debugger optimization, the follow on solution will be for the debugger
    to request ttmp setup using the SPI debug TRAP_EN bit instead.
    
    Signed-off-by: Jonathan Kim <jonathan.kim@amd.com>
    Reviewed-by: Felix Kuehling <felix.kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_mqd_manager_v9.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_mqd_manager_v9.c
@@ -208,8 +208,7 @@ static void init_mqd(struct mqd_manager *mm, void **mqd,
 	/* Set cp_hqd_hq_status0 bit 14 to 1 to have the CP set up the
 	 * DISPATCH_PTR.  This is required for the kfd debugger
 	 */
-	if (q->save_ttmp)
-		m->cp_hqd_hq_status0 = 1 << 14;
+	m->cp_hqd_hq_status0 = 1 << 14;
 
 	if (q->format == KFD_QUEUE_FORMAT_AQL) {
 		m->cp_hqd_aql_control =
@@ -320,12 +319,6 @@ static void update_mqd(struct mqd_manager *mm, void *mqd,
 			1 << CP_HQD_PQ_CONTROL__PRIV_STATE__SHIFT;
 	if (mm->dev->cwsr_enabled && q->ctx_save_restore_area_address)
 		m->cp_hqd_ctx_save_control = 0;
-
-	/* Enable or disable DISPATCH_PTR save for the debugger. */
-	if (q->save_ttmp)
-		m->cp_hqd_hq_status0 |= 1 << 14;
-	else
-		m->cp_hqd_hq_status0 &= ~(1UL << 14);
 
 	update_cu_mask(mm, mqd, q);
 	set_priority(m, q);
