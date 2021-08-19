b7c40451a14a9cc206351fee7a9d682f1128e0f5
Darren Powell <darren.powell@amd.com>
2021-08-18 23:31:36 -0400

    amdgpu/pm: (v2) add limit_type to (pptable_funcs)->set_power_limit signature
     v2
     add check for SMU_DEFAULT_PPT_LIMIT
    
     v1
     modify (pptable_funcs)->set_power_limit signature
     modify smu11 set_power_limit signature (arcturus, navi10, sienna_cichlid)
     modify smu13 set_power_limit signature (aldabaran)
     modify vangogh_set_power_limit signature (vangogh)
    
    === Test ===
    sudo bash
    
    AMDGPU_PCI_ADDR=`lspci -nn | grep "VGA\|Display" | cut -d " " -f 1`
    AMDGPU_HWMON=`ls -la /sys/class/hwmon | grep $AMDGPU_PCI_ADDR | awk '{print $9}'`
    HWMON_DIR=/sys/class/hwmon/${AMDGPU_HWMON}
    LOGFILE=pp_show_power_cap.log
    
    cp $LOGFILE{,.old}
    lspci -nn | grep "VGA\|Display" > $LOGFILE
    FILES="
    power1_cap
    power2_cap"
    
    for f in $FILES
    do
      if test -f "$HWMON_DIR/$f"; then
        echo === $f === >> $LOGFILE
        cat $HWMON_DIR/$f >> $LOGFILE
        RESTORE_VALUE=`cat $HWMON_DIR/$f` 2>&1  >> $LOGFILE
        echo RESTORE_VALUE $RESTORE_VALUE >> $LOGFILE
        echo 120000000 > $HWMON_DIR/$f
        sleep 3
        cat $HWMON_DIR/$f >> $LOGFILE
        echo $RESTORE_VALUE > $HWMON_DIR/$f
        sleep 3
        cat $HWMON_DIR/$f >> $LOGFILE
      else
        echo === $f === >> $LOGFILE
        echo File Not Found >> $LOGFILE
      fi
    done
    cat $LOGFILE
    
    Signed-off-by: Darren Powell <darren.powell@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -2310,9 +2310,10 @@ static int smu_set_power_limit(void *handle, uint32_t limit)
 
 	mutex_lock(&smu->mutex);
 
+	limit &= (1<<24)-1;
 	if (limit_type != SMU_DEFAULT_PPT_LIMIT)
 		if (smu->ppt_funcs->set_power_limit) {
-			ret = smu->ppt_funcs->set_power_limit(smu, limit);
+			ret = smu->ppt_funcs->set_power_limit(smu, limit_type, limit);
 			goto out;
 		}
 
@@ -2328,7 +2329,7 @@ static int smu_set_power_limit(void *handle, uint32_t limit)
 		limit = smu->current_power_limit;
 
 	if (smu->ppt_funcs->set_power_limit) {
-		ret = smu->ppt_funcs->set_power_limit(smu, limit);
+		ret = smu->ppt_funcs->set_power_limit(smu, limit_type, limit);
 		if (!ret && !(smu->user_dpm_profile.flags & SMU_DPM_USER_PROFILE_RESTORE))
 			smu->user_dpm_profile.power_limit = limit;
 	}
