e795d31a5a4f9102c0651fa29e2277c8b2797c37
Mukul Joshi <mukul.joshi@amd.com>
2021-08-19 13:18:04 -0400

    drm/amdkfd: CWSR with sw scheduler on Aldebaran and Arcturus
    Program trap handler settings to enable CWSR with software scheduler
    on Aldebaran and Arcturus.
    
    Signed-off-by: Mukul Joshi <mukul.joshi@amd.com>
    Reviewed-by: Amber Lin <Amber.Lin@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_arcturus.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_arcturus.c
@@ -314,5 +314,6 @@ const struct kfd2kgd_calls arcturus_kfd2kgd = {
 	.set_precise_mem_ops = kgd_gfx_v9_set_precise_mem_ops,
 	.get_iq_wait_times = kgd_gfx_v9_get_iq_wait_times,
 	.build_grace_period_packet_info = kgd_gfx_v9_build_grace_period_packet_info,
-	.get_cu_occupancy = kgd_gfx_v9_get_cu_occupancy
+	.get_cu_occupancy = kgd_gfx_v9_get_cu_occupancy,
+	.program_trap_handler_settings = kgd_gfx_v9_program_trap_handler_settings
 };
