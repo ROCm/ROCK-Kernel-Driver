2ef567321f2195b4ba38d3f984a6bd1fb47549ae
Leslie Shi <Yuliang.Shi@amd.com>
2021-08-23 14:20:05 +0800

    drm/amdkcl: wrap the code under macro HAVE_DRM_DRIVER_RELEASE
    This is caused by 07775fc13878 "drm/amdgpu: Unmap all MMIO mappings"
    v5.13-rc1-246-g07775fc13878
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4462,7 +4462,11 @@ static void amdgpu_device_unmap_mmio(struct amdgpu_device *adev)
 {
 
 	/* Clear all CPU mappings pointing to this device */
+#ifdef HAVE_DRM_DRIVER_RELEASE
 	unmap_mapping_range(adev->ddev.anon_inode->i_mapping, 0, 0, 1);
+#else
+	unmap_mapping_range(adev->ddev->anon_inode->i_mapping, 0, 0, 1);
+#endif
 
 	/* Unmap all mapped bars - Doorbell, registers and VRAM */
 	amdgpu_doorbell_fini(adev);
