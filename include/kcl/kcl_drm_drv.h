5839d9c0b3b9986ddaff28ea9e600b0c3387a9d6
Leslie Shi <Yuliang.Shi@amd.com>
2021-08-24 13:22:55 +0800

    drm/amdkcl: fake drm_dev_{enter, exit, is_unplugged} for legacy os
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- include/kcl/kcl_drm_drv.h
+++ include/kcl/kcl_drm_drv.h
@@ -41,4 +41,38 @@ static inline void drm_dev_put(struct drm_device *dev)
 }
 #endif
 
+#ifndef HAVE_DRM_DEV_ENTER
+/* Copied from include/drm/drm_drv.h*/
+
+bool drm_dev_enter(struct drm_device *dev, int *idx);
+void drm_dev_exit(int idx);
+
+#ifndef HAVE_DRM_DEV_IS_UNPLUGGED
+/**
+ * drm_dev_is_unplugged - is a DRM device unplugged
+ * @dev: DRM device
+ *
+ * This function can be called to check whether a hotpluggable is unplugged.
+ * Unplugging itself is singalled through drm_dev_unplug(). If a device is
+ * unplugged, these two functions guarantee that any store before calling
+ * drm_dev_unplug() is visible to callers of this function after it completes
+ *
+ * WARNING: This function fundamentally races against drm_dev_unplug(). It is
+ * recommended that drivers instead use the underlying drm_dev_enter() and
+ * drm_dev_exit() function pairs.
+ */
+static inline bool drm_dev_is_unplugged(struct drm_device *dev)
+{
+        int idx;
+
+        if (drm_dev_enter(dev, &idx)) {
+                drm_dev_exit(idx);
+                return false;
+        }
+
+        return true;
+}
+#endif /* HAVE_DRM_DEV_IS_UNPLUGGED */
+#endif /* HAVE_DRM_DEV_ENTER */
+
 #endif
