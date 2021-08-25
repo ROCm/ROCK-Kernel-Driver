38c11f206ec8780af8b16ce8fb33860f37a29e03
Leslie Shi <Yuliang.Shi@amd.com>
2021-08-25 14:05:33 +0800

    drm/amdkcl: wrap the code under macro PCI_IRQ_MSI
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_irq.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_irq.c
@@ -388,7 +388,11 @@ void amdgpu_irq_fini_hw(struct amdgpu_device *adev)
 		drm_irq_uninstall(&adev->ddev);
 		adev->irq.installed = false;
 		if (adev->irq.msi_enabled)
+#ifdef PCI_IRQ_MSI
 			pci_free_irq_vectors(adev->pdev);
+#else
+			pci_disable_msi(adev->pdev);
+#endif
 
 		if (!amdgpu_device_has_dc_support(adev))
 			flush_work(&adev->hotplug_work);
