a93d60385c6845a9c9c8224b8cb698129c8f22a0
Leslie Shi <Yuliang.Shi@amd.com>
2021-08-25 10:25:27 +0800

    drm/amdkcl: Test whether struct drm_device has pdev member
    This is caused by 5c0cd6459c5a "drm/amdkcl: init the ddev->pdev for legacy os"
    v5.13-2411-g5c0cd6459c5a
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2317,7 +2317,9 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 		goto err_free;
 #endif
 
+#ifdef HAVE_DRM_DEVICE_PDEV
 	ddev->pdev = pdev;
+#endif
 	pci_set_drvdata(pdev, ddev);
 
 	amdgpu_init_debug_options(adev);
