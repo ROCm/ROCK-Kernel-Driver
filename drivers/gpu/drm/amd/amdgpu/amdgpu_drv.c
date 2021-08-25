torvalds/linux

commit 6848c291a54f8cd1e8b32f4d6e0f681acc8d5095

drm/aperture: Convert drivers to aperture interfaces
Mass-convert all drivers from FB helpers to aperture interfaces. No
functional changes besides checking for returned errno codes.

Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
Acked-by: Jani Nikula <jani.nikula@intel.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20210412131043.5787-3-tzimmermann@suse.de

drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -23,6 +23,7 @@
  */
 
 #include <drm/amdgpu_drm.h>
+#include <drm/drm_aperture.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_gem.h>
 #include <drm/drm_vblank.h>
@@ -1196,7 +1197,7 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 #endif
 
 	/* Get rid of things like offb */
-	ret = drm_fb_helper_remove_conflicting_pci_framebuffers(pdev, "amdgpudrmfb");
+	ret = drm_aperture_remove_conflicting_pci_framebuffers(pdev, "amdgpudrmfb");
 	if (ret)
 		return ret;
 

drivers/gpu/drm/armada/armada_drv.c
@@ -9,6 +9,7 @@
 #include <linux/of_graph.h>
 #include <linux/platform_device.h>
 
+#include <drm/drm_aperture.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_ioctl.h>
@@ -94,9 +95,7 @@ static int armada_drm_bind(struct device *dev)
 	}
 
 	/* Remove early framebuffers */
-	ret = drm_fb_helper_remove_conflicting_framebuffers(NULL,
-							    "armada-drm-fb",
-							    false);
+	ret = drm_aperture_remove_framebuffers(false, "armada-drm-fb");
 	if (ret) {
 		dev_err(dev, "[" DRM_NAME ":%s] can't kick out simple-fb: %d\n",
 			__func__, ret);

drivers/gpu/drm/ast/ast_drv.c
@@ -30,10 +30,10 @@
 #include <linux/module.h>
 #include <linux/pci.h>
 
+#include <drm/drm_aperture.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_drv.h>
-#include <drm/drm_fb_helper.h>
 #include <drm/drm_gem_vram_helper.h>
 #include <drm/drm_probe_helper.h>
 
@@ -89,23 +89,18 @@ static const struct pci_device_id ast_pciidlist[] = {
 
 MODULE_DEVICE_TABLE(pci, ast_pciidlist);
 
-static void ast_kick_out_firmware_fb(struct pci_dev *pdev)
+static int ast_remove_conflicting_framebuffers(struct pci_dev *pdev)
 {
-	struct apertures_struct *ap;
 	bool primary = false;
+	resource_size_t base, size;
 
-	ap = alloc_apertures(1);
-	if (!ap)
-		return;
-
-	ap->ranges[0].base = pci_resource_start(pdev, 0);
-	ap->ranges[0].size = pci_resource_len(pdev, 0);
-
+	base = pci_resource_start(pdev, 0);
+	size = pci_resource_len(pdev, 0);
 #ifdef CONFIG_X86
 	primary = pdev->resource[PCI_ROM_RESOURCE].flags & IORESOURCE_ROM_SHADOW;
 #endif
-	drm_fb_helper_remove_conflicting_framebuffers(ap, "astdrmfb", primary);
-	kfree(ap);
+
+	return drm_aperture_remove_conflicting_framebuffers(base, size, primary, "astdrmfb");
 }
 
 static int ast_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
@@ -114,7 +109,9 @@ static int ast_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	struct drm_device *dev;
 	int ret;
 
-	ast_kick_out_firmware_fb(pdev);
+	ret = ast_remove_conflicting_framebuffers(pdev);
+	if (ret)
+		return ret;
 
 	ret = pcim_enable_device(pdev);
 	if (ret)

drivers/gpu/drm/bochs/bochs_drv.c
@@ -6,6 +6,7 @@
 #include <linux/pci.h>
 
 #include <drm/drm_drv.h>
+#include <drm/drm_aperture.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_managed.h>
 
@@ -109,7 +110,7 @@ static int bochs_pci_probe(struct pci_dev *pdev,
 		return -ENOMEM;
 	}
 
-	ret = drm_fb_helper_remove_conflicting_pci_framebuffers(pdev, "bochsdrmfb");
+	ret = drm_aperture_remove_conflicting_pci_framebuffers(pdev, "bochsdrmfb");
 	if (ret)
 		return ret;
 

drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.c
@@ -14,6 +14,7 @@
 #include <linux/module.h>
 #include <linux/pci.h>
 
+#include <drm/drm_aperture.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_gem_framebuffer_helper.h>
@@ -313,8 +314,7 @@ static int hibmc_pci_probe(struct pci_dev *pdev,
 	struct drm_device *dev;
 	int ret;
 
-	ret = drm_fb_helper_remove_conflicting_pci_framebuffers(pdev,
-								"hibmcdrmfb");
+	ret = drm_aperture_remove_conflicting_pci_framebuffers(pdev, "hibmcdrmfb");
 	if (ret)
 		return ret;
 

drivers/gpu/drm/i915/i915_drv.c
@@ -40,6 +40,7 @@
 #include <linux/vt.h>
 #include <acpi/video.h>
 
+#include <drm/drm_aperture.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_ioctl.h>
 #include <drm/drm_irq.h>
@@ -557,7 +558,7 @@ static int i915_driver_hw_probe(struct drm_i915_private *dev_priv)
 	if (ret)
 		goto err_perf;
 
-	ret = drm_fb_helper_remove_conflicting_pci_framebuffers(pdev, "inteldrmfb");
+	ret = drm_aperture_remove_conflicting_pci_framebuffers(pdev, "inteldrmfb");
 	if (ret)
 		goto err_ggtt;
 

drivers/gpu/drm/meson/meson_drv.c
@@ -15,6 +15,7 @@
 #include <linux/platform_device.h>
 #include <linux/soc/amlogic/meson-canvas.h>
 
+#include <drm/drm_aperture.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_fb_helper.h>
@@ -156,23 +157,6 @@ static void meson_vpu_init(struct meson_drm *priv)
 	writel_relaxed(value, priv->io_base + _REG(VPU_WRARB_MODE_L2C1));
 }
 
-static void meson_remove_framebuffers(void)
-{
-	struct apertures_struct *ap;
-
-	ap = alloc_apertures(1);
-	if (!ap)
-		return;
-
-	/* The framebuffer can be located anywhere in RAM */
-	ap->ranges[0].base = 0;
-	ap->ranges[0].size = ~0;
-
-	drm_fb_helper_remove_conflicting_framebuffers(ap, "meson-drm-fb",
-						      false);
-	kfree(ap);
-}
-
 struct meson_drm_soc_attr {
 	struct meson_drm_soc_limits limits;
 	const struct soc_device_attribute *attrs;
@@ -297,8 +281,13 @@ static int meson_drv_bind_master(struct device *dev, bool has_components)
 		}
 	}
 
-	/* Remove early framebuffers (ie. simplefb) */
-	meson_remove_framebuffers();
+	/*
+	 * Remove early framebuffers (ie. simplefb). The framebuffer can be
+	 * located anywhere in RAM
+	 */
+	ret = drm_aperture_remove_framebuffers(false, "meson-drm-fb");
+	if (ret)
+		goto free_drm;
 
 	ret = drmm_mode_config_init(drm);
 	if (ret)

drivers/gpu/drm/mgag200/mgag200_drv.c
@@ -11,6 +11,7 @@
 #include <linux/pci.h>
 #include <linux/vmalloc.h>
 
+#include <drm/drm_aperture.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_file.h>
 #include <drm/drm_ioctl.h>
@@ -341,7 +342,9 @@ mgag200_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	struct drm_device *dev;
 	int ret;
 
-	drm_fb_helper_remove_conflicting_pci_framebuffers(pdev, "mgag200drmfb");
+	ret = drm_aperture_remove_conflicting_pci_framebuffers(pdev, "mgag200drmfb");
+	if (ret)
+		return ret;
 
 	ret = pcim_enable_device(pdev);
 	if (ret)

ROCm/ROCK-Kernel-Driver

commit a636f1b7f48066b6bb3529df603dbec24016cac5

drm/amdkcl: Test whether drm_aperture_* is available
This is caused by 6848c291a54f "drm/aperture: Convert drivers to aperture interfaces"
v5.12-rc3-331-g6848c291a54f

Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -23,7 +23,9 @@
  */
 
 #include <drm/amdgpu_drm.h>
+#ifdef HAVE_DRM_DRM_APERTURE_H
 #include <drm/drm_aperture.h>
+#endif
 #include <drm/drm_drv.h>
 #include <drm/drm_gem.h>
 #include <drm/drm_vblank.h>

drivers/gpu/drm/amd/amdkcl/Makefile
@@ -11,7 +11,7 @@ amdkcl-y += kcl_backlight.o kcl_ioctl.o \
 	kcl_drm_auth.o kcl_drm_fb.o kcl_drm_print.o kcl_fbmem.o \
 	kcl_drm_crtc.o kcl_drm_connector.o kcl_drm_atomic_helper.o \
 	kcl_device_cgroup.o kcl_mn.o kcl_drm_modes.o kcl_time.o kcl_ftrace.o \
-	kcl_acpi_table.o
+	kcl_acpi_table.o kcl_drm_aperture.o
 
 amdkcl-$(CONFIG_DRM_AMD_DC_HDCP) += kcl_drm_hdcp.o
 amdkcl-$(CONFIG_MMU_NOTIFIER) += kcl_mn.o

drivers/gpu/drm/amd/amdkcl/kcl_drm_aperture.c
@@ -1,0 +1,139 @@
+// SPDX-License-Identifier: MIT
+
+#ifndef HAVE_DRM_APERTURE
+#include <linux/fb.h>
+#include <linux/pci.h>
+#include <linux/vgaarb.h>
+#include <linux/list.h>
+#include <linux/mutex.h>
+#include <kcl/kcl_drm_aperture.h>
+
+struct drm_aperture {
+        struct drm_device *dev;
+        resource_size_t base;
+        resource_size_t size;
+        struct list_head lh;
+        void (*detach)(struct drm_device *dev);
+};
+
+static LIST_HEAD(drm_apertures);
+static DEFINE_MUTEX(drm_apertures_lock);
+
+static bool overlap(resource_size_t base1, resource_size_t end1,
+                    resource_size_t base2, resource_size_t end2)
+{
+        return (base1 < end2) && (end1 > base2);
+}
+
+
+static void drm_aperture_detach_drivers(resource_size_t base, resource_size_t size)
+{
+        resource_size_t end = base + size;
+        struct list_head *pos, *n;
+
+        mutex_lock(&drm_apertures_lock);
+
+        list_for_each_safe(pos, n, &drm_apertures) {
+                struct drm_aperture *ap =
+                        container_of(pos, struct drm_aperture, lh);
+                struct drm_device *dev = ap->dev;
+
+                if (WARN_ON_ONCE(!dev))
+                        continue;
+
+                if (!overlap(base, end, ap->base, ap->base + ap->size))
+                        continue;
+
+                ap->dev = NULL; /* detach from device */
+                list_del(&ap->lh);
+
+                ap->detach(dev);
+        }
+
+        mutex_unlock(&drm_apertures_lock);
+}
+
+
+/**
+ * drm_aperture_remove_conflicting_framebuffers - remove existing framebuffers in the given range
+ * @base: the aperture's base address in physical memory
+ * @size: aperture size in bytes
+ * @primary: also kick vga16fb if present
+ * @name: requesting driver name
+ *
+ * This function removes graphics device drivers which use memory range described by
+ * @base and @size.
+ *
+ * Returns:
+ * 0 on success, or a negative errno code otherwise
+ */
+int drm_aperture_remove_conflicting_framebuffers(resource_size_t base, resource_size_t size,
+                                                 bool primary, const char *name)
+{
+#if IS_REACHABLE(CONFIG_FB)
+        struct apertures_struct *a;
+        int ret;
+
+        a = alloc_apertures(1);
+        if (!a)
+                return -ENOMEM;
+
+        a->ranges[0].base = base;
+        a->ranges[0].size = size;
+
+        ret = remove_conflicting_framebuffers(a, name, primary);
+        kfree(a);
+
+        if (ret)
+                return ret;
+#endif
+
+        drm_aperture_detach_drivers(base, size);
+
+        return 0;
+}
+EXPORT_SYMBOL(drm_aperture_remove_conflicting_framebuffers);
+
+/**
+ * drm_aperture_remove_conflicting_pci_framebuffers - remove existing framebuffers for PCI devices
+ * @pdev: PCI device
+ * @name: requesting driver name
+ *
+ * This function removes graphics device drivers using memory range configured
+ * for any of @pdev's memory bars. The function assumes that PCI device with
+ * shadowed ROM drives a primary display and so kicks out vga16fb.
+ *
+ * Returns:
+ * 0 on success, or a negative errno code otherwise
+ */
+int drm_aperture_remove_conflicting_pci_framebuffers(struct pci_dev *pdev, const char *name)
+{
+        resource_size_t base, size;
+        int bar, ret = 0;
+
+        for (bar = 0; bar < PCI_STD_NUM_BARS; ++bar) {
+                if (!(pci_resource_flags(pdev, bar) & IORESOURCE_MEM))
+                        continue;
+                base = pci_resource_start(pdev, bar);
+                size = pci_resource_len(pdev, bar);
+                drm_aperture_detach_drivers(base, size);
+        }
+
+        /*
+         * WARNING: Apparently we must kick fbdev drivers before vgacon,
+         * otherwise the vga fbdev driver falls over.
+         */
+
+#ifdef HAVE_VGA_REMOVE_VGACON
+#if IS_REACHABLE(CONFIG_FB)
+        ret = remove_conflicting_pci_framebuffers(pdev, name);
+#endif
+        if (ret == 0)
+                ret = vga_remove_vgacon(pdev);
+#endif
+
+        return ret;
+}
+EXPORT_SYMBOL(drm_aperture_remove_conflicting_pci_framebuffers);
+
+#endif /* HAVE_DRM_APERTURE */

drivers/gpu/drm/amd/backport/backport.h
@@ -74,5 +74,6 @@
 #include "kcl/kcl_amdgpu_drm_fb_helper.h"
 #include "kcl/kcl_amdgpu.h"
 #include "kcl/kcl_amdgpu_drm_drv.h"
+#include "kcl/kcl_drm_aperture.h"
 
 #endif /* AMDGPU_BACKPORT_H */

drivers/gpu/drm/amd/dkms/m4/drm-aperture.m4
@@ -1,0 +1,16 @@
+dnl #
+dnl # commit 2916059147ea38f76787d7b38dee883da2e9def2
+dnl # drm/aperture: Add infrastructure for aperture ownership
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_APERTURE], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <drm/drm_aperture.h>
+		], [
+			drm_aperture_remove_conflicting_pci_framebuffers(NULL, NULL);
+		], [
+			AC_DEFINE(HAVE_DRM_APERTURE, 1,
+				[drm_aperture_remove_* is availablea])
+		])
+	])
+])

drivers/gpu/drm/amd/dkms/m4/drm-headers.m4
@@ -137,4 +137,10 @@ AC_DEFUN([AC_AMDGPU_DRM_HEADERS], [
 	dnl # drm: add managed resources tied to drm_device
 	dnl #
 	AC_KERNEL_CHECK_HEADERS([drm/drm_managed.h])
+
+	dnl #
+	dnl # v5.12-rc3-330-g2916059147ea
+	dnl # drm/aperture: Add infrastructure for aperture ownership
+	dnl #
+	AC_KERNEL_CHECK_HEADERS([drm/drm_aperture.h])
 ])

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -197,7 +197,9 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_DRM_DP_SUBCONNECTOR
 	AC_AMDGPU_DRM_MEMCPY_FROM_WC
 	AC_AMDGPU_IS_COW_MAPPING
-
+	AC_AMDGPU_VGA_REMOVE_VGACON
+	AC_AMDGPU_DRM_APERTURE
+	
 	AC_KERNEL_WAIT
 	AS_IF([test "$LINUX_OBJ" != "$LINUX"], [
 		KERNEL_MAKE="$KERNEL_MAKE O=$LINUX_OBJ"

drivers/gpu/drm/amd/dkms/m4/vga_remove_vgacon.m4
@@ -1,0 +1,16 @@
+dnl #
+dnl # v5.0-rc1-998-gc6b38fbbde91
+dnl # drm: move i915_kick_out_vgacon to vgaarb
+dnl #
+AC_DEFUN([AC_AMDGPU_VGA_REMOVE_VGACON], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <linux/vgaarb.h>
+		], [
+			vga_remove_vgacon(NULL);
+		], [
+			AC_DEFINE(HAVE_VGA_REMOVE_VGACON, 1,
+				[vga_remove_vgacon() is available])
+		])
+	])
+])

include/kcl/kcl_drm_aperture.h
@@ -1,0 +1,25 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef KCL_KCL_DRM_APERTURE_H
+#define KCL_KCL_DRM_APERTURE_H
+
+#ifndef HAVE_DRM_APERTURE
+
+#include <linux/types.h>
+
+/* Copied from uapi/linux/pci_regs.h */
+#ifndef PCI_STD_NUM_BARS
+#define PCI_STD_NUM_BARS 6
+#endif
+
+/* Copied from drm/drm_aperture.h */
+struct drm_device;
+struct pci_dev;
+
+int drm_aperture_remove_conflicting_framebuffers(resource_size_t base, resource_size_t size,
+                                                 bool primary, const char *name);
+
+int drm_aperture_remove_conflicting_pci_framebuffers(struct pci_dev *pdev, const char *name);
+
+#endif /* HAVE_DRM_APERTURE */
+
+#endif