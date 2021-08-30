fc52f148bb7783db9d8cc682724be40b78377903
Asher Song <Asher.Song@amd.com>
2021-08-30 17:33:36 +0800

    drm/amdkcl: correct the mistakes in the process of merging code.
    It's caused by 808a662bb3a870ec59296f0bf3dac973d3822ab0 "drm/amd/display: Add DP 2.0 SST DC Support"
    v5.13-2093-g808a662bb3a8
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link.c
@@ -4419,9 +4419,11 @@ void core_link_enable_stream(
 	} else { // if (IS_FPGA_MAXIMUS_DC(dc->ctx->dce_environment))
 		if (is_dp_128b_132b_signal(pipe_ctx))
 			fpga_dp_hpo_enable_link_and_stream(state, pipe_ctx);
+#if defined(CONFIG_DRM_AMD_DC_DSC_SUPPORT)
 		if (dc_is_dp_signal(pipe_ctx->stream->signal) ||
 				dc_is_virtual_signal(pipe_ctx->stream->signal))
 			dp_set_dsc_enable(pipe_ctx, true);
+#endif
 	}
 
 	if (dc_is_hdmi_signal(pipe_ctx->stream->signal)) {
