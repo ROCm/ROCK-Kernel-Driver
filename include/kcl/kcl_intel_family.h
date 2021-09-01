0ad2bb8c9bf555aaaf09ee825d04bc7e147fd4c8
Asher Song <Asher.Song@amd.com>
2021-09-01 18:52:10 +0800

    drm/amdkcl: add macro INTEL_FAM6_ROCKETLAKE
    It's caused by 857d1b24aa8d97a7e1cb50ed3b02773a159a8c4d "drm/amdgpu: Disable PCIE_DPM on Intel RKL Platform"
    v5.13-2160-g857d1b24aa8d
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/kcl_intel_family.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef AMDKCL_INTEL_FAMILY_H
+#define AMDKCL_INTEL_FAMILY_H
+
+#include <asm/intel-family.h>
+/* Copied froma asm/intel-family.h*/
+#ifndef INTEL_FAM6_ROCKETLAKE
+#define INTEL_FAM6_ROCKETLAKE       0xA7
+#endif
+
+#endif
+
