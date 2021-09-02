3d933636c7d71be155c018e6f4e114199e5bce49
Meenakshikumar Somasundaram <meenakshikumar.somasundaram@amd.com>
2021-09-02 14:09:30 -0400

    drm/amd/display: MST support for DPIA
    [Why]
    - DPIA MST slot registers are not programmed during payload
    allocation and hence MST does not work with DPIA.
    - HPD RX interrupts are not handled for DPIA.
    
    [How]
    - Added inbox command to program the MST slots whenever
      payload allocation happens for DPIA links.
    - Added support for handling HPD RX interrupts
    
    Signed-off-by: Meenakshikumar Somasundaram <meenakshikumar.somasundaram@amd.com>
    Reviewed-by: Jun Lei <Jun.Lei@amd.com>
    Acked-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -1471,6 +1471,11 @@ bool dc_process_dmub_set_config_async(struct dc *dc,
 				struct set_config_cmd_payload *payload,
 				struct dmub_notification *notify);
 
+enum dc_status dc_process_dmub_set_mst_slots(const struct dc *dc,
+				uint32_t link_index,
+				uint8_t mst_alloc_slots,
+				uint8_t *mst_slots_in_use);
+
 #if defined(CONFIG_DRM_AMD_DC_DSC_SUPPORT)
 /*******************************************************************************
  * DSC Interfaces
