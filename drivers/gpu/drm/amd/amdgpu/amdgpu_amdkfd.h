b872922de9a4209cff82162afa031ad0793cd2d5
James Zhu <James.Zhu@amd.com>
2021-09-07 11:13:02 -0400

    drm/amdkfd: separate kfd_iommu_resume from kfd_resume
    Separate kfd_iommu_resume from kfd_resume for fine-tuning
    of amdgpu device init/resume/reset/recovery sequence.
    
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.h
@@ -378,6 +378,7 @@ bool kgd2kfd_device_init(struct kfd_dev *kfd,
 			 const struct kgd2kfd_shared_resources *gpu_resources);
 void kgd2kfd_device_exit(struct kfd_dev *kfd);
 void kgd2kfd_suspend(struct kfd_dev *kfd, bool run_pm, bool force);
+int kgd2kfd_resume_iommu(struct kfd_dev *kfd);
 int kgd2kfd_resume(struct kfd_dev *kfd, bool run_pm, bool sync);
 int kgd2kfd_pre_reset(struct kfd_dev *kfd);
 int kgd2kfd_post_reset(struct kfd_dev *kfd);
