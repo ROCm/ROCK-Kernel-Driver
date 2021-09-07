87e71bc6548891aebcff7f9d0c4451418ee1b0e3
Jingwen Chen <Jingwen.Chen2@amd.com>
2021-09-07 11:12:06 +0800

    drm/amd/amdgpu: use ordered workqueue for tdr in SRIOV
    [Why]
    When two job timedout happens during a very close time, the job bailing
    will happen. As drm scheduler will delete the bad job from pending list
    when entering the tdr and add it back when calling drm_sched_stop during
    tdr handling, the bailing job will be deleted from pending list but it
    will directly return before calling drm_sched_stop as the first job has
    already sets the in_gpu_reset. So this bailing job will not be added
    back ever. And this can lead to this job never be finished.
    
    [How]
    1. Use an ordered_workqueue as the timeout_wq for all tdr. As there will
       only be one tdr work at the same time, the bailing will never happen.
    2. Use mdelay in sriov flr work to make sure the polling flr won't
       exceeds the default ring timeout.
    
    v2: Add detailed description. Use gpu_recovery=2 for SRIOV
    
    v3: split recovery mode and mdelay into seperate patch
    
    Based on: https://patchwork.freedesktop.org/patch/msgid/20210630062751.2832545-3-boris.brezillon@collabora.com
    
    Signed-off-by: Jingwen Chen <Jingwen.Chen2@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fence.c
@@ -521,7 +521,7 @@ int amdgpu_fence_driver_init_ring(struct amdgpu_ring *ring,
 
 	r = drm_sched_init(&ring->sched, &amdgpu_sched_ops,
 			   num_hw_submission, amdgpu_job_hang_limit,
-			   timeout, NULL, sched_score, ring->name);
+			   timeout, adev->timeout_wq, sched_score, ring->name);
 	if (r) {
 		DRM_ERROR("Failed to create scheduler on ring %s.\n",
 			  ring->name);
