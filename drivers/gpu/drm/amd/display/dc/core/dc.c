b2cab49afc19ca19996b5b8e01cd70109cb4e005
Michael Strauss <michael.strauss@amd.com>
2021-09-09 16:33:52 -0400

    drm/amd/display: Defer LUT memory powerdown until LUT bypass latches
    [WHY]
    Blnd, 3dlut, and shaper LUT select registers are double buffered, however
    their accompanying LUT memory shutdown registers are not. As a result,
    shutting down LUT memory immediately after setting a block to bypass causes
    corruption as bypass only happens at next Vupdate.
    
    [HOW]
    Re-enable mem low power for CM block
    Force optimization on next flip and disable LUT memory during optimization
    sequence if LUT select field is then set to bypass
    
    Reviewed-by: Eric Yang <Eric.Yang2@amd.com>
    Acked-by: Anson Jacob <Anson.Jacob@amd.com>
    Signed-off-by: Michael Strauss <michael.strauss@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -1818,6 +1818,23 @@ static bool is_flip_pending_in_pipes(struct dc *dc, struct dc_state *context)
 	return false;
 }
 
+/* Perform updates here which need to be deferred until next vupdate
+ *
+ * i.e. blnd lut, 3dlut, and shaper lut bypass regs are double buffered
+ * but forcing lut memory to shutdown state is immediate. This causes
+ * single frame corruption as lut gets disabled mid-frame unless shutdown
+ * is deferred until after entering bypass.
+ */
+static void process_deferred_updates(struct dc *dc)
+{
+	int i;
+
+	if (dc->debug.enable_mem_low_power.bits.cm)
+		for (i = 0; i < dc->dcn_ip->max_num_dpp; i++)
+			if (dc->res_pool->dpps[i]->funcs->dpp_deferred_update)
+				dc->res_pool->dpps[i]->funcs->dpp_deferred_update(dc->res_pool->dpps[i]);
+}
+
 void dc_post_update_surfaces_to_stream(struct dc *dc)
 {
 	int i;
@@ -1842,6 +1859,8 @@ void dc_post_update_surfaces_to_stream(struct dc *dc)
 			context->res_ctx.pipe_ctx[i].pipe_idx = i;
 			dc->hwss.disable_plane(dc, &context->res_ctx.pipe_ctx[i]);
 		}
+
+	process_deferred_updates(dc);
 
 	dc->hwss.optimize_bandwidth(dc, context);
 
