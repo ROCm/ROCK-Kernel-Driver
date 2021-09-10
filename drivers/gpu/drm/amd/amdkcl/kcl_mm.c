922912be9ffd843624b4a5775cd18a5c59c4d4d4
Flora Cui <flora.cui@amd.com>
2021-09-10 11:44:49 +0800

    drm/amdkcl: fake a dummy mmput_async
    v2: fix the missing ;
    HAVE_MMPUT_ASYNC must be defined for monolithic build
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-and-tested-by: Jonathan Kim <jonathan.kim@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_mm.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_mm.c
@@ -17,9 +17,23 @@ static struct mm_struct * __kcl_mm_access_stub(struct task_struct *task, unsigne
 }
 #endif
 
+#ifndef HAVE_MMPUT_ASYNC
+void (*_kcl_mmput_async)(struct mm_struct *mm);
+EXPORT_SYMBOL(_kcl_mmput_async);
+
+void __kcl_mmput_async(struct mm_struct *mm)
+{
+	pr_warn_once("This kernel version not support API: mmput_async !\n");
+}
+#endif
+
 void amdkcl_mm_init(void)
 {
 #ifndef HAVE_MM_ACCESS
 	_kcl_mm_access = amdkcl_fp_setup("mm_access", __kcl_mm_access_stub);
+#endif
+
+#ifndef HAVE_MMPUT_ASYNC
+	_kcl_mmput_async = amdkcl_fp_setup("mmput_async", __kcl_mmput_async);
 #endif
 }
