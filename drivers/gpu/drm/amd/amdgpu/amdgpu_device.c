bcecab6d3106f63af28d80392a0ebf321fe09879
Oliver Logush <ollogush@amd.com>
2021-09-16 17:50:11 -0400

    Enable Robin Display
    Modify files to enable the robin display driver.
    
    Signed-off-by: Oliver Logush <ollogush@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2036,6 +2036,9 @@ static int amdgpu_device_parse_gpu_info_fw(struct amdgpu_device *adev)
 	case CHIP_VANGOGH:
 		chip_name = "vangogh";
 		break;
+	case CHIP_CYAN_SKILLFISH:
+		chip_name = "robin";
+		break;
 	case CHIP_YELLOW_CARP:
 		chip_name = "yellow_carp";
 		break;
