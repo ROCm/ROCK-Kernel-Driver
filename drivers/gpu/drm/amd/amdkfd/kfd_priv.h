8a5fd3a947ae6471cb6d800c905972f44d0362e8
Jonathan Kim <jonathan.kim@amd.com>
2021-09-16 16:31:24 -0400

    drm/amdkfd: remove unnecessary mqd updates for ttmp setup during debugging
    Now that the KFD unconditionally requests to the CP to save the dispatch
    index on MQD initialiation (it instead uses SPI debug TRAP_EN to toggle
    ttmp setup by ENV variable), there is no need to update the MQD with the
    ttmp setup status when updating the runlist in debug mode.
    
    Signed-off-by: Jonathan Kim <jonathan.kim@amd.com>
    Reviewed-by: Felix Kuehling <felix.kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_priv.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_priv.h
@@ -483,9 +483,6 @@ enum KFD_QUEUE_PRIORITY {
  * @is_gws should be protected by the DQM lock, since changing it can yield the
  * possibility of updating DQM state on number of GWS queues.
  *
- * @save_ttmp: Enables saving the dispatch pointer to TTMP 6 & 7 during
- * the initialization or update of the MQD.
- *
  * @vmid: If the scheduling mode is no cp scheduling the field defines the vmid
  * of the queue.
  *
@@ -512,7 +509,6 @@ struct queue_properties {
 	bool is_being_destroyed;
 	bool is_active;
 	bool is_gws;
-	bool save_ttmp;
 	/* Not relevant for user mode queues in cp scheduling */
 	unsigned int vmid;
 	/* Relevant only for sdma queues*/
