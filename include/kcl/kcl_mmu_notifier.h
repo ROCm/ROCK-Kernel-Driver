1f13c87826a663a13eca3a44b582270d9cd2b2a7
Flora Cui <flora.cui@amd.com>
2021-10-11 14:49:42 +0800

    drm/amdkcl: fix mmu_notifier_range_blockable on rhel8.5
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- include/kcl/kcl_mmu_notifier.h
+++ include/kcl/kcl_mmu_notifier.h
@@ -4,16 +4,25 @@
 
 #include <linux/mmu_notifier.h>
 
-#if !defined(HAVE_MMU_NOTIFIER_RANGE_BLOCKABLE) && \
-	defined(HAVE_2ARGS_INVALIDATE_RANGE_START)
+#if !defined(HAVE_MMU_NOTIFIER_RANGE_BLOCKABLE)
 /* Copied from v5.1-10225-g4a83bfe916f3 include/linux/mmu_notifier.h */
-#ifdef CONFIG_MMU_NOTIFIER
+#if defined(CONFIG_MMU_NOTIFIER) && \
+	defined(HAVE_2ARGS_INVALIDATE_RANGE_START)
 static inline bool
 mmu_notifier_range_blockable(const struct mmu_notifier_range *range)
 {
+/*
+ * It's for rhel8.5 which has the latest struct mmu_notifier_range
+ * and no mmu_notifier_range_blockable
+ */
+#ifdef MMU_NOTIFIER_RANGE_BLOCKABLE
+	return (range->flags & MMU_NOTIFIER_RANGE_BLOCKABLE);
+#else
 	return range->blockable;
+#endif
 }
 #else
+struct mmu_notifier_range;
 static inline bool
 mmu_notifier_range_blockable(const struct mmu_notifier_range *range)
 {
