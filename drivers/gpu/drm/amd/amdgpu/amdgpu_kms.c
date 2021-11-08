2475c57a462c4377e7edf08ab2505b09dc92a021
Guchun Chen <guchun.chen@amd.com>
2021-11-08 14:58:25 +0800

    drm/amdkcl: guard amdgpu_driver_release_kms by HAVE_DRM_DRIVER_RELEASE
    Fix: 1776040c6227("drm/amd/amdkcl: fix minor index not removed after unload driver")
    
    Signed-off-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -1491,7 +1491,7 @@ void amdgpu_driver_postclose_kms(struct drm_device *dev,
 	pm_runtime_put_autosuspend(dev->dev);
 }
 
-
+#ifdef HAVE_DRM_DRIVER_RELEASE
 void amdgpu_driver_release_kms(struct drm_device *dev)
 {
 	struct amdgpu_device *adev = drm_to_adev(dev);
@@ -1503,6 +1503,7 @@ void amdgpu_driver_release_kms(struct drm_device *dev)
 	kfree(adev);
 #endif
 }
+#endif
 
 /*
  * VBlank related functions.
