19d6d6b0ffeebb2f1f3815314e6c40d3014528af
Leslie Shi <Yuliang.Shi@amd.com>
2021-11-11 15:07:23 +0800

    drm/amdkcl: check whether struct drm_device has fb_helper member
    It's caused by 844612e1149d0cd6fd2346018c91f5744b2615f5 "drm/amdgpu: use generic fb helpers instead of setting up AMD own's."
    v5.13-2445-g844612e1149d
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4591,7 +4591,11 @@ int amdgpu_device_suspend(struct drm_device *dev, bool fbcon)
 		DRM_WARN("smart shift update failed\n");
 
 	if (fbcon)
+#ifdef HAVE_DRM_DEVICE_FB_HELPER
 		drm_fb_helper_set_suspend_unlocked(adev_to_drm(adev)->fb_helper, true);
+#else
+		amdgpu_fbdev_set_suspend(adev, 1);
+#endif
 
 	cancel_delayed_work_sync(&adev->delayed_init_work);
 	flush_delayed_work(&adev->gfx.gfx_off_delay_work);
@@ -4690,7 +4694,11 @@ exit:
 	flush_delayed_work(&adev->delayed_init_work);
 
 	if (fbcon)
+#ifdef HAVE_DRM_DEVICE_FB_HELPER
 		drm_fb_helper_set_suspend_unlocked(adev_to_drm(adev)->fb_helper, false);
+#else
+		amdgpu_fbdev_set_suspend(adev, 0);
+#endif
 
 	amdgpu_ras_resume(adev);
 
@@ -5394,7 +5402,11 @@ int amdgpu_do_asic_reset(struct list_head *device_list_handle,
 				if (r)
 					goto out;
 
+#ifdef HAVE_DRM_DEVICE_FB_HELPER
 				drm_fb_helper_set_suspend_unlocked(adev_to_drm(tmp_adev)->fb_helper, false);
+#else
+				amdgpu_fbdev_set_suspend(tmp_adev, 0);
+#endif
 
 				/*
 				 * The GPU enters bad state once faulty pages
@@ -5664,7 +5676,11 @@ int amdgpu_device_gpu_recover(struct amdgpu_device *adev,
 		 */
 		amdgpu_unregister_gpu_instance(tmp_adev);
 
+#ifdef HAVE_DRM_DEVICE_FB_HELPER
 		drm_fb_helper_set_suspend_unlocked(adev_to_drm(tmp_adev)->fb_helper, true);
+#else
+		amdgpu_fbdev_set_suspend(tmp_adev, 1);
+#endif
 
 		/* disable ras on ALL IPs */
 		if (!need_emergency_restart &&
