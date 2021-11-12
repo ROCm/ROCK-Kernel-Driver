89b228a6b15d846d5f13f4c8561682f0142f742f
Ramesh Errabolu <Ramesh.Errabolu@amd.com>
2021-11-12 15:39:32 -0600

    drm/amdgpu: Fix config macro HAVE_STRUCT_DMA_BUF_OPS_ALLOW_PEER2PEER
    Logic determining status of HAVE_STRUCT_DMA_BUF_OPS_ALLOW_PEER2PEER
    is incorrect. Code body should reference struct dma_buf_attach_ops.
    Rename macro as HAVE_STRUCT_DMA_BUF_ATTACH_OPS_ALLOW_PEER2PEER
    
    Signed-off-by: Ramesh Errabolu <Ramesh.Errabolu@amd.com>
    Acked-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
@@ -247,7 +247,7 @@ static int amdgpu_dma_buf_attach(struct dma_buf *dmabuf,
 	struct amdgpu_device *adev = amdgpu_ttm_adev(bo->tbo.bdev);
 	int r;
 
-#ifdef HAVE_STRUCT_DMA_BUF_OPS_ALLOW_PEER2PEER
+#ifdef HAVE_STRUCT_DMA_BUF_ATTACH_OPS_ALLOW_PEER2PEER
 	if (pci_p2pdma_distance(adev->pdev, attach->dev, false) < 0)
 		attach->peer2peer = false;
 #endif
@@ -362,7 +362,7 @@ static struct sg_table *amdgpu_dma_buf_map(struct dma_buf_attachment *attach,
 		struct ttm_operation_ctx ctx = { false, false };
 		unsigned domains = AMDGPU_GEM_DOMAIN_GTT;
 
-#ifdef HAVE_STRUCT_DMA_BUF_OPS_ALLOW_PEER2PEER
+#ifdef HAVE_STRUCT_DMA_BUF_ATTACH_OPS_ALLOW_PEER2PEER
 		if (bo->preferred_domains & AMDGPU_GEM_DOMAIN_VRAM &&
 		    attach->peer2peer) {
 			bo->flags |= AMDGPU_GEM_CREATE_CPU_ACCESS_REQUIRED;
@@ -797,7 +797,7 @@ amdgpu_dma_buf_move_notify(struct dma_buf_attachment *attach)
 }
 
 static const struct dma_buf_attach_ops amdgpu_dma_buf_attach_ops = {
-#ifdef HAVE_STRUCT_DMA_BUF_OPS_ALLOW_PEER2PEER
+#ifdef HAVE_STRUCT_DMA_BUF_ATTACH_OPS_ALLOW_PEER2PEER
 	.allow_peer2peer = true,
 #endif
 	.move_notify = amdgpu_dma_buf_move_notify
