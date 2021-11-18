4e76d90def49c452ff065b1e0765960ef46aa959
Flora Cui <flora.cui@amd.com>
2021-11-18 16:17:34 +0800

    drm/amdkcl: guard CONFIG_X86 for x86 related stuff
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Tested-by: Emily.Deng <Emily.Deng@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- include/kcl/kcl_mce.h
+++ include/kcl/kcl_mce.h
@@ -2,10 +2,14 @@
 #ifndef AMDKCL_MCE_H
 #define AMDKCL_MCE_H
 
+#ifdef CONFIG_X86
+
 #include <asm/mce.h>
+
 /* Copied from asm/mce.h */
 #ifndef XEC
 #define XEC(x, mask)			(((x) >> 16) & mask)
 #endif
 
+#endif /* CONFIG_X86 */
 #endif
