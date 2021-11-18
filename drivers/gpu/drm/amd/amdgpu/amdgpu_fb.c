ab334e7d05a4ec48a4842762632955db67d97a7f
Flora Cui <flora.cui@amd.com>
2021-11-18 16:25:45 +0800

    drm/amdkcl: update related flags for atomic unsupported case
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fb.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fb.c
@@ -373,7 +373,7 @@ int amdgpu_fbdev_init(struct amdgpu_device *adev)
 #endif
 
 	/* disable all the possible outputs/crtcs before entering KMS mode */
-	if (!amdgpu_device_has_dc_support(adev))
+	if (!drm_drv_uses_atomic_modeset(adev_to_drm(adev)))
 		drm_helper_disable_unused_functions(adev_to_drm(adev));
 
 	drm_fb_helper_initial_config(&rfbdev->helper, bpp_sel);
