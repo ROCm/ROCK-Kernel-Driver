00a3684caf3862c15aceb0bc18748e29a5ddcc31
Leslie Shi <Yuliang.Shi@amd.com>
2021-11-19 10:34:32 +0800

    drm/amdkcl: add strict restriction when calling drm_fbdev_generic_setup
    It's caused by 844612e1149d0cd6fd2346018c91f5744b2615f5 "drm/amdgpu: use generic fb helpers instead of setting up AMD own's."
    v5.13-2445-g844612e1149d
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -3760,6 +3760,10 @@ fence_driver_init:
 	/* Get a log2 for easy divisions. */
 	adev->mm_stats.log2_max_MBps = ilog2(max(1u, max_MBps));
 
+#ifndef AMDKCL_DRM_FBDEV_GENERIC
+	amdgpu_fbdev_init(adev);
+#endif
+
 	r = amdgpu_pm_sysfs_init(adev);
 	if (r) {
 		adev->pm_sysfs_en = false;
@@ -3923,6 +3927,9 @@ void amdgpu_device_fini_hw(struct amdgpu_device *adev)
 		amdgpu_ucode_sysfs_fini(adev);
 	sysfs_remove_files(&adev->dev->kobj, amdgpu_dev_attributes);
 
+#ifndef AMDKCL_DRM_FBDEV_GENERIC
+	amdgpu_fbdev_fini(adev);
+#endif
 	amdgpu_irq_fini_hw(adev);
 
 	amdgpu_device_ip_fini_early(adev);
@@ -4017,7 +4024,7 @@ int amdgpu_device_suspend(struct drm_device *dev, bool fbcon)
 	drm_kms_helper_poll_disable(dev);
 
 	if (fbcon)
-#ifdef HAVE_DRM_DEVICE_FB_HELPER
+#ifdef AMDKCL_DRM_FBDEV_GENERIC
 		drm_fb_helper_set_suspend_unlocked(adev_to_drm(adev)->fb_helper, true);
 #else
 		amdgpu_fbdev_set_suspend(adev, 1);
@@ -4098,7 +4105,7 @@ int amdgpu_device_resume(struct drm_device *dev, bool fbcon)
 	flush_delayed_work(&adev->delayed_init_work);
 
 	if (fbcon)
-#ifdef HAVE_DRM_DEVICE_FB_HELPER
+#ifdef AMDKCL_DRM_FBDEV_GENERIC
 		drm_fb_helper_set_suspend_unlocked(adev_to_drm(adev)->fb_helper, false);
 #else
 		amdgpu_fbdev_set_suspend(adev, 0);
@@ -4734,7 +4741,7 @@ int amdgpu_do_asic_reset(struct list_head *device_list_handle,
 				if (r)
 					goto out;
 
-#ifdef HAVE_DRM_DEVICE_FB_HELPER
+#ifdef AMDKCL_DRM_FBDEV_GENERIC
 				drm_fb_helper_set_suspend_unlocked(adev_to_drm(tmp_adev)->fb_helper, false);
 #else
 				amdgpu_fbdev_set_suspend(tmp_adev, 0);
@@ -5154,7 +5161,7 @@ int amdgpu_device_gpu_recover(struct amdgpu_device *adev,
 		 */
 		amdgpu_unregister_gpu_instance(tmp_adev);
 
-#ifdef HAVE_DRM_DEVICE_FB_HELPER
+#ifdef AMDKCL_DRM_FBDEV_GENERIC
 		drm_fb_helper_set_suspend_unlocked(adev_to_drm(adev)->fb_helper, true);
 #else
 		amdgpu_fbdev_set_suspend(tmp_adev, 1);
