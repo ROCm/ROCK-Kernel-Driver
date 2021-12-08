c7e3784b22171109704c3858943e587ed783a3ee
Perry Yuan <Perry.Yuan@amd.com>
2021-12-08 04:56:12 -0500

    drm/amdkcl: fix build error by adding drm_display_info.is_hdmi check
    The build failure caused by below commit, the patch will check if the
    is_hdmi is available on the current kernel.
    
    dfbe9bf067 drm/amdgpu: replace drm_detect_hdmi_monitor() with drm_display_info.is_hdmi
    v5.13-3121-gdfbe9bf067a2
    
    Signed-off-by: Perry Yuan <Perry.Yuan@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -1748,7 +1748,11 @@ bool amdgpu_display_crtc_scaling_mode_fixup(struct drm_crtc *crtc,
 		if ((!(mode->flags & DRM_MODE_FLAG_INTERLACE)) &&
 		    ((amdgpu_encoder->underscan_type == UNDERSCAN_ON) ||
 		     ((amdgpu_encoder->underscan_type == UNDERSCAN_AUTO) &&
+#if defined(HAVE_DRM_DISPLAY_INFO_IS_HDMI)
 		      connector->display_info.is_hdmi &&
+#else
+		      drm_detect_hdmi_monitor(to_amdgpu_connector(connector)->edid) &&
+#endif
 		      amdgpu_display_is_hdtv_mode(mode)))) {
 			if (amdgpu_encoder->underscan_hborder != 0)
 				amdgpu_crtc->h_border = amdgpu_encoder->underscan_hborder;
