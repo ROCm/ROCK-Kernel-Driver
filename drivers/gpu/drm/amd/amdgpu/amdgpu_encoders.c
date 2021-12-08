13f0106a4c64703439fc585ff9cecf0db355cd66
Perry Yuan <Perry.Yuan@amd.com>
2021-12-08 04:56:12 -0500

    drm/amdkcl: fix build error by adding drm_display_info.is_hdmi check
    The build failure caused by below commit, the patch will check if the
    is_hdmi is available on the current kernel.
    
    dfbe9bf067 drm/amdgpu: replace drm_detect_hdmi_monitor() with drm_display_info.is_hdmi
    v5.13-3121-gdfbe9bf067a2
    
    Signed-off-by: Perry Yuan <Perry.Yuan@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_encoders.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_encoders.c
@@ -252,7 +252,11 @@ bool amdgpu_dig_monitor_is_duallink(struct drm_encoder *encoder,
 	case DRM_MODE_CONNECTOR_HDMIB:
 		if (amdgpu_connector->use_digital) {
 			/* HDMI 1.3 supports up to 340 Mhz over single link */
+#if defined(HAVE_DRM_DISPLAY_INFO_IS_HDMI)
 			if (connector->display_info.is_hdmi) {
+#else
+			if (drm_detect_hdmi_monitor(amdgpu_connector_edid(connector))) {
+#endif
 				if (pixel_clock > 340000)
 					return true;
 				else
@@ -274,7 +278,11 @@ bool amdgpu_dig_monitor_is_duallink(struct drm_encoder *encoder,
 			return false;
 		else {
 			/* HDMI 1.3 supports up to 340 Mhz over single link */
+#if defined(HAVE_DRM_DISPLAY_INFO_IS_HDMI)
 			if (connector->display_info.is_hdmi) {
+#else
+			if (drm_detect_hdmi_monitor(amdgpu_connector_edid(connector))) {
+#endif
 				if (pixel_clock > 340000)
 					return true;
 				else
