5dd17f732269575adb9077c8c3b776f609c5b92c
Leslie Shi <Yuliang.Shi@amd.com>
2021-12-28 12:26:09 +0800

    drm/amdkcl: refactor code for drm_aperture.h
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_drm_aperture.h
+++ include/kcl/kcl_drm_aperture.h
@@ -2,7 +2,7 @@
 #ifndef KCL_KCL_DRM_APERTURE_H
 #define KCL_KCL_DRM_APERTURE_H
 
-#ifndef HAVE_DRM_APERTURE
+#ifndef HAVE_DRM_DRM_APERTURE_H
 
 #include <linux/types.h>
 
@@ -20,6 +20,6 @@ int drm_aperture_remove_conflicting_framebuffers(resource_size_t base, resource_
 
 int drm_aperture_remove_conflicting_pci_framebuffers(struct pci_dev *pdev, const char *name);
 
-#endif /* HAVE_DRM_APERTURE */
+#endif /* HAVE_DRM_DRM_APERTURE_H */
 
 #endif
