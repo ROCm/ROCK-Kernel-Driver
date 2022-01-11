d99fc3c31f43023053faf7d27ba6c356333bd3be
CHANDAN VURDIGERE NATARAJ <chandan.vurdigerenataraj@amd.com>
2022-01-11 19:02:26 +0530

    drm/amdgpu: Enable recovery on yellow carp
    Add yellow carp to devices which support recovery
    
    Signed-off-by: CHANDAN VURDIGERE NATARAJ <chandan.vurdigerenataraj@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Change-Id: Ifdac17f0de5fadef79116b14d36f01cbcb0ffb1f

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4581,6 +4581,7 @@ bool amdgpu_device_should_recover_gpu(struct amdgpu_device *adev)
 		case CHIP_BEIGE_GOBY:
 		case CHIP_VANGOGH:
 		case CHIP_ALDEBARAN:
+		case CHIP_YELLOW_CARP:
 			break;
 		default:
 			goto disabled;
