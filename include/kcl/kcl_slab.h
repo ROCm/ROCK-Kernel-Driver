d1135cd464b075c04156826c3297a58387be0dbc
Leslie Shi <Yuliang.Shi@amd.com>
2022-01-17 13:00:54 +0800

    drm/amdkcl: Test whether krealloc_array() is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/kcl_slab.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Written by Mark Hemment, 1996 (markhe@nextd.demon.co.uk).
+ *
+ * (C) SGI 2006, Christoph Lameter
+ *      Cleaned up and restructured to ease the addition of alternative
+ *      implementations of SLAB allocators.
+ * (C) Linux Foundation 2008-2013
+ *      Unified interface for all slab allocators
+ */
+#ifndef AMDKCL_SLAB_H
+#define AMDKCL_SLAB_H
+
+#include <linux/gfp.h>
+#include <linux/slab.h>
+#include <kcl/kcl_overflow.h>
+
+#ifndef HAVE_KREALLOC_ARRAY
+/**
+ * krealloc_array - reallocate memory for an array.
+ * @p: pointer to the memory chunk to reallocate
+ * @new_n: new number of elements to alloc
+ * @new_size: new size of a single member of the array
+ * @flags: the type of memory to allocate (see kmalloc)
+ */
+static __must_check inline void *
+krealloc_array(void *p, size_t new_n, size_t new_size, gfp_t flags)
+{
+        size_t bytes;
+
+        if (unlikely(check_mul_overflow(new_n, new_size, &bytes)))
+                return NULL;
+
+        return krealloc(p, bytes, flags);
+}
+#endif
+
+#endif
