0093e2365899c5ecad033230beb0a86b8c1c97ea
Leslie Shi <Yuliang.Shi@amd.com>
2022-01-17 15:17:19 +0800

    drm/amdkcl: Test whether linux/stdarg.h is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/header/linux/stdarg.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_LINUX_STDARG_H_H
+#define _KCL_HEADER_LINUX_STDARG_H_H
+
+#if defined(HAVE_LINUX_STDARG_H)
+#include_next <linux/stdarg.h>
+#else
+#include <stdarg.h>
+#endif
+
+#endif
