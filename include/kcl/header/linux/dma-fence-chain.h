d9abdda198e8a545c3e5853036b968e9fc5e28c8
Leslie Shi <Yuliang.Shi@amd.com>
2022-01-18 14:04:37 +0800

    drm/amdkcl: Test whether linux/dma-fence-chain.h is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/header/linux/dma-fence-chain.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER__LINUX_DMA_FENCE_CHAIN_H_H_
+#define _KCL_HEADER__LINUX_DMA_FENCE_CHAIN_H_H_
+
+#if defined(HAVE_LINUX_DMA_FENCE_CHAIN_H)
+#include_next <linux/dma-fence-chain.h>
+#endif
+
+#endif
