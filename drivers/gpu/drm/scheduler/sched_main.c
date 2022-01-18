2f13b4253f3867bea4883146d82ca50ff0b5335a
Leslie Shi <Yuliang.Shi@amd.com>
2022-01-18 14:44:13 +0800

    drm/amdkcl: wrap the code under macro HAVE_DRM_GEM_OBJECT_RESV
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -973,6 +973,7 @@ int drm_sched_job_add_resv_dependencies(struct drm_sched_job *job,
 }
 EXPORT_SYMBOL(drm_sched_job_add_resv_dependencies);
 
+#ifdef HAVE_DRM_GEM_OBJECT_RESV
 /**
  * drm_sched_job_add_implicit_dependencies - adds implicit dependencies as job
  *   dependencies
@@ -996,6 +997,7 @@ int drm_sched_job_add_implicit_dependencies(struct drm_sched_job *job,
 						   dma_resv_usage_rw(write));
 }
 EXPORT_SYMBOL(drm_sched_job_add_implicit_dependencies);
+#endif
 
 /**
  * drm_sched_job_cleanup - clean up scheduler job resources
