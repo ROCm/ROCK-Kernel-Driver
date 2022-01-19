53216b4464248591ffe07c0abf920131c5749137
Leslie Shi <Yuliang.Shi@amd.com>
2022-01-19 15:59:05 +0800

    drm/amdkcl: test whether __dma_fence_is_later() is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -1175,11 +1175,11 @@
 /* zone_managed_pages() is available */
 #define HAVE_ZONE_MANAGED_PAGES 1
 
-/* bitmap_free() is available */
-#define HAVE_BITMAP_FUNCS 1
+/* __dma_fence_is_later() is available and has 2 args */
+/* #undef HAVE__DMA_FENCE_IS_LATER_2ARGS */
 
-/* drm_edid_get_monitor_name is available*/
-#define HAVE_DRM_EDID_GET_MONITOR_NAME 1
+/* __dma_fence_is_later() is available and has ops arg */
+#define HAVE__DMA_FENCE_IS_LATER_WITH_OPS_ARG 1
 
 /* __drm_atomic_helper_connector_destroy_state() wants 1 arg */
 #define HAVE___DRM_ATOMIC_HELPER_CONNECTOR_DESTROY_STATE_P 1
