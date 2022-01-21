05dd6f2ac9d5d456fc587f1279cba993c6f0025a
Flora Cui <flora.cui@amd.com>
2022-01-21 15:29:52 +0800

    drm/amdkcl: drop trict restriction for drm_fbdev_generic_setup
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/backport/kcl_drm_fb.h
+++ include/kcl/backport/kcl_drm_fb.h
@@ -43,8 +43,7 @@ void _kcl_drm_helper_mode_fill_fb_struct(struct drm_device *dev,
 #endif
 
 #if defined(HAVE_DRM_FBDEV_GENERIC_SETUP) &&			\
-	defined(HAVE_DRM_DEVICE_FB_HELPER) &&			\
-	DRM_VERSION_CODE >= DRM_VERSION(5, 15, 0)
+	defined(HAVE_DRM_DEVICE_FB_HELPER)
 #define AMDKCL_DRM_FBDEV_GENERIC
 #endif
 
