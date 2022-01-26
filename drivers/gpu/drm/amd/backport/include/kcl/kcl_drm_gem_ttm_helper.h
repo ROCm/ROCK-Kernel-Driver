8d5c708955420bf3ab78bb96ff20b51aae949262
Flora Cui <flora.cui@amd.com>
2022-01-26 17:04:10 +0800

    drm/amdkcl: rework kcl stuff for drm_ttm_helper.ko
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/backport/include/kcl/kcl_drm_gem_ttm_helper.h
+++ drivers/gpu/drm/amd/backport/include/kcl/kcl_drm_gem_ttm_helper.h
@@ -1,29 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
-/* Copied from include/drm/drm_gem_ttm_helper.h */
-
 #ifndef _KCL_KCL_DRM_GEM_TTM_HELPER_H_H
 #define _KCL_KCL_DRM_GEM_TTM_HELPER_H_H
 
 #include <drm/drm_gem.h>
+#include <drm/drm_gem_ttm_helper.h>
 
-#ifndef HAVE_DRM_GEM_TTM_VMAP
-void _kcl_drm_gem_ttm_vunmap(struct drm_gem_object *gem,
+#ifndef HAVE_DRM_GEM_OBJECT_FUNCS_VMAP_2ARGS
+void amdgpu_gem_prime_vunmap(struct drm_gem_object *gem,
 			void *vaddr);
-
-void *_kcl_drm_gem_ttm_vmap(struct drm_gem_object *obj);
-
-static inline
-void drm_gem_ttm_vunmap(struct drm_gem_object *gem,
-			void *vaddr)
-{
-	_kcl_drm_gem_ttm_vunmap(gem, vaddr);
-}
-
-static inline
-void *drm_gem_ttm_vmap(struct drm_gem_object *obj)
-{
-	return _kcl_drm_gem_ttm_vmap(obj);
-}
+void *amdgpu_gem_prime_vmap(struct drm_gem_object *obj);
 #endif
 
 #ifdef HAVE_STRUCT_DRM_DRV_GEM_OPEN_OBJECT_CALLBACK
@@ -32,16 +17,6 @@ int amdgpu_gem_object_open(struct drm_gem_object *obj,
 				struct drm_file *file_priv);
 void amdgpu_gem_object_close(struct drm_gem_object *obj,
 				struct drm_file *file_priv);
-#endif
-
-#ifndef HAVE_STRUCT_DRM_DRV_GEM_OPEN_OBJECT_CALLBACK
-int _kcl_drm_gem_ttm_mmap(struct drm_gem_object *gem,
-                     struct vm_area_struct *vma);
-static inline
-int drm_gem_ttm_mmap(struct drm_gem_object *gem,
-                     struct vm_area_struct *vma) {
-	return _kcl_drm_gem_ttm_mmap(gem, vma);
-}
 #endif
 
 #endif
