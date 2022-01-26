3c61549d0db2b628f3062046c0e2d6a0d1acadda
Flora Cui <flora.cui@amd.com>
2022-01-26 17:04:10 +0800

    drm/amdkcl: rework kcl stuff for drm_ttm_helper.ko
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -432,9 +432,6 @@
    */
 #define HAVE_DRM_DRM_GEM_FRAMEBUFFER_HELPER_H 1
 
-/* Define to 1 if you have the <drm/drm_gem_ttm_helper.h> header file. */
-#define HAVE_DRM_DRM_GEM_TTM_HELPER_H 1
-
 /* Define to 1 if you have the <drm/drm_hdcp.h> header file. */
 #define HAVE_DRM_DRM_HDCP_H 1
 
@@ -526,6 +523,9 @@
 /* drm_gem_map_attach() wants 2 arguments */
 /* #undef HAVE_DRM_GEM_MAP_ATTACH_2ARGS */
 
+/* drm_gem_object_funcs->vmap() has 2 args */
+#define HAVE_DRM_GEM_OBJECT_FUNCS_VMAP_2ARGS 1
+
 /* drm_gem_object_lookup() wants 2 args */
 #define HAVE_DRM_GEM_OBJECT_LOOKUP_2ARGS 1
 
@@ -537,9 +537,6 @@
 
 /* ttm_buffer_object->base is available */
 #define HAVE_DRM_GEM_OBJECT_RESV 1
-
-/* drm_gem_ttm_vmap() is available */
-#define HAVE_DRM_GEM_TTM_VMAP 1
 
 /* drm_gen_fb_init_with_funcs() is available */
 #define HAVE_DRM_GEN_FB_INIT_WITH_FUNCS 1
