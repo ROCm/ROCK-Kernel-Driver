148f2accfefda6affe931c04328bc9dfb06c14be
Flora Cui <flora.cui@amd.com>
2022-02-07 16:34:52 +0800

    drm/amdkcl: rework drm_aperture_remove_conflicting_pci_framebuffers()
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- include/kcl/backport/kcl_drm_fb.h
+++ include/kcl/backport/kcl_drm_fb.h
@@ -25,10 +25,6 @@
 #include <kcl/kcl_drm_fb.h>
 #include <drm/drm_fb_helper.h>
 
-#if !defined(HAVE_DRM_FB_HELPER_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_PP)
-#define drm_fb_helper_remove_conflicting_pci_framebuffers _kcl_drm_fb_helper_remove_conflicting_pci_framebuffers
-#endif
-
 #ifndef HAVE_DRM_HELPER_MODE_FILL_FB_STRUCT_DEV
 static inline
 void _kcl_drm_helper_mode_fill_fb_struct(struct drm_device *dev,
