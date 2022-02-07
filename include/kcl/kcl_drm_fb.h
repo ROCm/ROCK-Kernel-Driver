918b2662f8b2c3fe9193d0696a8938a16eb37245
Flora Cui <flora.cui@amd.com>
2022-02-07 16:34:52 +0800

    drm/amdkcl: rework drm_aperture_remove_conflicting_pci_framebuffers()
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- include/kcl/kcl_drm_fb.h
+++ include/kcl/kcl_drm_fb.h
@@ -55,63 +55,6 @@
 	.fb_pan_display	= drm_fb_helper_pan_display
 #endif
 
-#if !defined(HAVE_DRM_FB_HELPER_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS)
-#if !defined(IS_REACHABLE)
-/* Copied from include/linux/kconfig.h */
-#define __ARG_PLACEHOLDER_1 0,
-#define __take_second_arg(__ignored, val, ...) val
-
-/*
- * The use of "&&" / "||" is limited in certain expressions.
- * The followings enable to calculate "and" / "or" with macro expansion only.
- */
-#define __and(x, y)			___and(x, y)
-#define ___and(x, y)			____and(__ARG_PLACEHOLDER_##x, y)
-#define ____and(arg1_or_junk, y)	__take_second_arg(arg1_or_junk y, 0)
-
-#define __or(x, y)			___or(x, y)
-#define ___or(x, y)			____or(__ARG_PLACEHOLDER_##x, y)
-#define ____or(arg1_or_junk, y)		__take_second_arg(arg1_or_junk 1, y)
-
-#define IS_REACHABLE(option) __or(IS_BUILTIN(option), \
-				__and(IS_MODULE(option), __is_defined(MODULE)))
-#endif /*IS_REACHABLE*/
-
-#ifdef HAVE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_NO_RES_ID_ARG
-extern int remove_conflicting_pci_framebuffers(struct pci_dev *pdev,
-                                               const char *name);
-#else
-extern int remove_conflicting_pci_framebuffers(struct pci_dev *pdev, int res_id,
-					       const char *name);
-#endif
-
-static inline int
-_kcl_drm_fb_helper_remove_conflicting_pci_framebuffers(struct pci_dev *pdev,
-						  const char *name)
-{
-#if IS_REACHABLE(CONFIG_FB)
-#ifdef HAVE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_NO_RES_ID_ARG
-	return remove_conflicting_pci_framebuffers(pdev, name);
-#else
-	return remove_conflicting_pci_framebuffers(pdev, 0, name);
-#endif
-#else
-	return 0;
-#endif
-}
-#elif !defined(HAVE_DRM_FB_HELPER_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_PP)
-static inline int
-_kcl_drm_fb_helper_remove_conflicting_pci_framebuffers(struct pci_dev *pdev,
-						  const char *name)
-{
-#ifdef HAVE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_WITH_RES_ID_ARG
-	return drm_fb_helper_remove_conflicting_pci_framebuffers(pdev, 0, name);
-#else
-	return drm_fb_helper_remove_conflicting_pci_framebuffers(pdev, name);
-#endif
-}
-#endif /* HAVE_DRM_FB_HELPER_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS */
-
 #ifndef HAVE_DRM_FB_HELPER_FILL_INFO
 void drm_fb_helper_fill_info(struct fb_info *info,
 			     struct drm_fb_helper *fb_helper,
