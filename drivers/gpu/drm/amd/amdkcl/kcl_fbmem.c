11b6eefb3542a99c4e3b7db850e5beba5579409d
Flora Cui <flora.cui@amd.com>
2022-02-07 16:34:52 +0800

    drm/amdkcl: rework drm_aperture_remove_conflicting_pci_framebuffers()
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_fbmem.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_fbmem.c
@@ -14,71 +14,43 @@
 #include <kcl/kcl_drm_fb.h>
 
 /* Copied from drivers/video/fbdev/core/fbmem.c and modified for KCL */
-#if !defined(HAVE_DRM_FB_HELPER_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS)
-
-#ifdef HAVE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_NO_RES_ID_ARG
+#if !defined(HAVE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS)
 int remove_conflicting_pci_framebuffers(struct pci_dev *pdev, const char *name)
-{
-        struct apertures_struct *ap;
-        bool primary = false;
-        int err, idx, bar;
-
-        for (idx = 0, bar = 0; bar < PCI_STD_NUM_BARS; bar++) {
-                if (!(pci_resource_flags(pdev, bar) & IORESOURCE_MEM))
-                        continue;
-                idx++;
-        }
-
-        ap = alloc_apertures(idx);
-        if (!ap)
-                return -ENOMEM;
-
-        for (idx = 0, bar = 0; bar < PCI_STD_NUM_BARS; bar++) {
-                if (!(pci_resource_flags(pdev, bar) & IORESOURCE_MEM))
-                        continue;
-                ap->ranges[idx].base = pci_resource_start(pdev, bar);
-                ap->ranges[idx].size = pci_resource_len(pdev, bar);
-                pci_dbg(pdev, "%s: bar %d: 0x%lx -> 0x%lx\n", __func__, bar,
-                        (unsigned long)pci_resource_start(pdev, bar),
-                        (unsigned long)pci_resource_end(pdev, bar));
-                idx++;
-        }
-
-#ifdef CONFIG_X86
-        primary = pdev->resource[PCI_ROM_RESOURCE].flags &
-                                        IORESOURCE_ROM_SHADOW;
-#endif
-        err = remove_conflicting_framebuffers(ap, name, primary);
-        kfree(ap);
-        return err;
-}
-#else /* HAVE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_NO_RES_ID_ARG */
-int remove_conflicting_pci_framebuffers(struct pci_dev *pdev, int res_id, const char *name)
 {
 	struct apertures_struct *ap;
 	bool primary = false;
-	int err = 0;
+	int err, idx, bar;
 
-	ap = alloc_apertures(1);
+	for (idx = 0, bar = 0; bar < PCI_ROM_RESOURCE; bar++) {
+		if (!(pci_resource_flags(pdev, bar) & IORESOURCE_MEM))
+			continue;
+		idx++;
+	}
+
+	ap = alloc_apertures(idx);
 	if (!ap)
 		return -ENOMEM;
 
-	ap->ranges[0].base = pci_resource_start(pdev, res_id);
-	ap->ranges[0].size = pci_resource_len(pdev, res_id);
+	for (idx = 0, bar = 0; bar < PCI_ROM_RESOURCE; bar++) {
+		if (!(pci_resource_flags(pdev, bar) & IORESOURCE_MEM))
+			continue;
+		ap->ranges[idx].base = pci_resource_start(pdev, bar);
+		ap->ranges[idx].size = pci_resource_len(pdev, bar);
+		dev_dbg(&pdev->dev, "%s: bar %d: 0x%lx -> 0x%lx\n", __func__, bar,
+			(unsigned long)pci_resource_start(pdev, bar),
+			(unsigned long)pci_resource_end(pdev, bar));
+		idx++;
+	}
+
 #ifdef CONFIG_X86
 	primary = pdev->resource[PCI_ROM_RESOURCE].flags &
 					IORESOURCE_ROM_SHADOW;
 #endif
-#ifdef HAVE_REMOVE_CONFLICTING_FRAMEBUFFERS_RETURNS_INT
 	err = remove_conflicting_framebuffers(ap, name, primary);
-#else
-	remove_conflicting_framebuffers(ap, name, primary);
-#endif
 	kfree(ap);
 	return err;
 }
-#endif /* HAVE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_NO_RES_ID_ARG */
-
+EXPORT_SYMBOL(remove_conflicting_pci_framebuffers);
 #endif
 
 #ifndef HAVE_IS_FIRMWARE_FRAMEBUFFER
