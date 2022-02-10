244d625cb6f50c93a3fceb9c5beb07fb6563caf1
Leslie Shi <Yuliang.Shi@amd.com>
2022-02-10 11:01:46 +0800

    drm/amdkcl: Test whether linux/container_of.h is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/header/linux/container_of.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_LINUX_CONTAINER_OF_H_H
+#define _KCL_HEADER_LINUX_CONTAINER_OF_H_H
+
+#if defined(HAVE_LINUX_CONTAINER_OF_H)
+#include_next <linux/container_of.h>
+#endif
+
+#endif
+
