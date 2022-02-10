8f72fedd30614586b3234b7caa2d22a6c3099d51
Leslie Shi <Yuliang.Shi@amd.com>
2022-02-10 11:13:51 +0800

    drm/amdkcl: Test whether linux/cc_platform.h is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/header/linux/cc_platform.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_LINUX_CC_PLATFORM_H_H
+#define _KCL_HEADER_LINUX_CC_PLATFORM_H_H
+
+#if defined(HAVE_LINUX_CC_PLATFORM_H)
+#include_next <linux/cc_platform.h>
+#endif
+
+#endif
+
