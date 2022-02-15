6238a4c29b494934214269d983039638174e9177
Leslie Shi <Yuliang.Shi@amd.com>
2022-02-15 13:50:54 +0800

    drm/amdkcl: Test whether drm_kms_helper_connector_hotplug_event() is available
    This is cause by fc320a6f64044f12128519ca98404b641340d136 "amdgpu: use drm_kms_helper_connector_hotplug_event"
    v5.15-rc2-1276-gfc320a6f6404
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -3496,7 +3496,11 @@ static void handle_hpd_irq_helper(struct amdgpu_dm_connector *aconnector)
 		drm_modeset_unlock_all(dev);
 
 		if (aconnector->base.force == DRM_FORCE_UNSPECIFIED)
+#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
 			drm_kms_helper_connector_hotplug_event(connector);
+#else
+			drm_kms_helper_hotplug_event(dev);
+#endif
 	} else {
 		mutex_lock(&adev->dm.dc_lock);
 		ret = dc_link_detect(aconnector->dc_link, DETECT_REASON_HPD);
@@ -3509,7 +3513,11 @@ static void handle_hpd_irq_helper(struct amdgpu_dm_connector *aconnector)
 			drm_modeset_unlock_all(dev);
 
 			if (aconnector->base.force == DRM_FORCE_UNSPECIFIED)
+#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
 				drm_kms_helper_connector_hotplug_event(connector);
+#else
+				drm_kms_helper_hotplug_event(dev);
+#endif
 		}
 	}
 	mutex_unlock(&aconnector->hpd_lock);
@@ -3642,7 +3650,11 @@ out:
 			dm_restore_drm_connector_state(dev, connector);
 			drm_modeset_unlock_all(dev);
 
+#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
 			drm_kms_helper_connector_hotplug_event(connector);
+#else
+			drm_kms_helper_hotplug_event(dev);
+#endif
 		} else {
 			bool ret = false;
 
@@ -3660,7 +3672,11 @@ out:
 				dm_restore_drm_connector_state(dev, connector);
 				drm_modeset_unlock_all(dev);
 
+#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
 				drm_kms_helper_connector_hotplug_event(connector);
+#else
+				drm_kms_helper_hotplug_event(dev);
+#endif
 			}
 		}
 	}
