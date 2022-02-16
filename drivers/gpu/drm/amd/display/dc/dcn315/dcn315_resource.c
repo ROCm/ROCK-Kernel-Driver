f9b67d9059a74b984c586fd4d37df78ec49d28a2
Shah, Dharati <Dharati.Shah@amd.com>
2022-02-16 14:17:41 -0500

    drm/amd/display: Adding a dc_debug option and dmub setting to use PHY FSM for PSR
    [Why]
    PSR Power on/off is done in PSR. Add a dc_debug option
    and dmub setting to use PHY implementation of this instead.
    
    [How]
    Add a dc_debug option and dmub setting to use
    PHY FSM Power up/down for PSR.
    
    Co-authored-by: Shah Dharati <dharati.shah@amd.com>
    Reviewed-by: Hansen Dsouza <hansen.dsouza@amd.com>
    Acked-by: Alan Liu <HaoPing.Liu@amd.com>
    Signed-off-by: Shah Dharati <dharati.shah@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn315/dcn315_resource.c
+++ drivers/gpu/drm/amd/display/dc/dcn315/dcn315_resource.c
@@ -1041,6 +1041,7 @@ static const struct dc_debug_options debug_defaults_drv = {
 	},
 	.optimize_edp_link_rate = true,
 	.enable_sw_cntl_psr = true,
+	.psr_power_use_phy_fsm = 0,
 };
 
 static const struct dc_debug_options debug_defaults_diags = {
