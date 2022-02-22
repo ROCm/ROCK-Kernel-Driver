285ae04a56fb9808e040e90f05e0ffdcafb7ec1f
Leslie Shi <Yuliang.Shi@amd.com>
2022-02-22 17:16:40 +0800

    drm/amdkcl: Test whether pcie_aspm_enabled() is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -1648,7 +1648,11 @@ bool amdgpu_device_should_use_aspm(struct amdgpu_device *adev)
 		return false;
 	if (!(adev->pm.pp_feature & PP_PCIE_DPM_MASK))
 		return false;
+#ifdef HAVE_PCIE_ASPM_ENABLED
 	return pcie_aspm_enabled(adev->pdev);
+#else
+	return false;
+#endif
 }
 
 /* if we get transitioned to only one device, take VGA back */
