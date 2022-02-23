4078374a10f150b874358892977a0e3d6e149139
Leslie Shi <Yuliang.Shi@amd.com>
2022-02-23 10:15:32 +0800

    drm/amdkcl: Test whether linux/processor.h is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/header/linux/processor.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_LINUX_PROCESSOR_H_H
+#define _KCL_HEADER_LINUX_PROCESSOR_H_H
+
+#if defined(HAVE_LINUX_PROCESSOR_H)
+#include_next <linux/processor.h>
+#endif
+
+#endif
+
