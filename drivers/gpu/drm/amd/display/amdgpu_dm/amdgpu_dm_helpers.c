993927ffd9e4a944b073395c84a95a8a9c0b0e0d
Magali Lemes <magalilemes00@gmail.com>
2022-02-24 16:15:49 -0300

    drm/amd/display: Add conditional around function
    When CONFIG_DRM_AMD_DC_DCN is not set, the function
    'dm_helpers_enable_periodic_detection' doesn't have its prototype defined,
    causing the following warning:
    
    drivers/gpu/drm/amd/amdgpu/../display/amdgpu_dm/amdgpu_dm_helpers.c:805:6:
    warning: no previous prototype for function 'dm_helpers_enable_periodic_detection' [-Wmissing-prototypes]
       void dm_helpers_enable_periodic_detection(struct dc_context *ctx, bool enable)
            ^
       drivers/gpu/drm/amd/amdgpu/../display/amdgpu_dm/amdgpu_dm_helpers.c:805:1:
    note: declare 'static' if the function is not intended to be used outside
    of this translation unit
       void dm_helpers_enable_periodic_detection(struct dc_context *ctx, bool enable)
       ^
       static
       1 warning generated.
    
    This commit silences this warning by adding a conditional directive
    around the mentioned function, and also corrects a small spelling error.
    
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Magali Lemes <magalilemes00@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Change-Id: I24a444e36de5ef10003430d6d4979c702cae1a9a

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
@@ -1100,7 +1100,9 @@ void dm_set_phyd32clk(struct dc_context *ctx, int freq_khz)
        // TODO
 }
 
+#if defined(CONFIG_DRM_AMD_DC_DCN3_x)
 void dm_helpers_enable_periodic_detection(struct dc_context *ctx, bool enable)
 {
-	/* TODO: add peridic detection implementation */
+	/* TODO: add periodic detection implementation */
 }
+#endif
