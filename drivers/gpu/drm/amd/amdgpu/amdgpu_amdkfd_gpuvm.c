77473d9bb3c28ae0bc2e9e0c451e45b7e1d7167b
David Yat Sin <david.yatsin@amd.com>
2022-03-03 14:48:19 -0500

    drm/amdkfd: CRIU Add support for IPC handles
    Add support for checkpointing/restoring BOs with IPC handles.
    
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: David Yat Sin <david.yatsin@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -2798,7 +2798,8 @@ out:
 int amdgpu_amdkfd_gpuvm_export_ipc_obj(struct amdgpu_device *adev, void *vm,
 				       struct kgd_mem *mem,
 				       struct kfd_ipc_obj **ipc_obj,
-				       uint32_t flags)
+				       uint32_t flags,
+				       uint32_t *restore_handle)
 {
 	struct dma_buf *dmabuf;
 	int r = 0;
@@ -2819,7 +2820,7 @@ int amdgpu_amdkfd_gpuvm_export_ipc_obj(struct amdgpu_device *adev, void *vm,
 		goto unlock_out;
 	}
 
-	r = kfd_ipc_store_insert(dmabuf, &mem->ipc_obj, flags);
+	r = kfd_ipc_store_insert(dmabuf, &mem->ipc_obj, flags, restore_handle);
 	if (r)
 		dma_buf_put(dmabuf);
 	else
