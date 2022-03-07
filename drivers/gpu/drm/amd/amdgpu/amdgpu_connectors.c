798326ed77f9c69151f8f92a39e930bd6ef65317
Flora Cui <flora.cui@amd.com>
2022-03-07 12:03:23 +0800

    drm/amdkcl: fix warning for unused variable in amdgpu_connector.c
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c: In function
    ‘amdgpu_connector_update_scratch_regs’:
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:252:6: warning: unused
    variable ‘i’ [-Wunused-variable]
    252 |  int i;
    |      ^
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c: In function
    ‘amdgpu_connector_find_encoder’:
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:275:6: warning:
    unused variable ‘i’ [-Wunused-variable]
    275 |  int i;
    |      ^
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c: In
    function ‘amdgpu_connector_best_single_encoder’:
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:393:6:
    warning: unused variable ‘i’ [-Wunused-variable]
    393 |  int i;
    |      ^
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:
    In function ‘amdgpu_connector_dvi_detect’:
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:1148:7:
    warning: unused variable ‘i’ [-Wunused-variable]
    1148 |   int i;
    |       ^
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:
    In function
    ‘amdgpu_connector_dvi_encoder’:
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:1204:6:
    warning: unused variable ‘i’
    [-Wunused-variable]
    1204 |  int i;
    |      ^
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:
    In function
    ‘amdgpu_connector_encoder_get_dp_bridge_encoder_id’:
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:1372:6:
    warning: unused variable ‘i’
    [-Wunused-variable]
    1372 |  int i;
    |      ^
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:
    In function
    ‘amdgpu_connector_encoder_is_hbr2’:
    drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c:1397:6:
    warning: unused variable
    ‘i’ [-Wunused-variable]
    1397 |  int i;
    |      ^
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c
@@ -248,7 +248,9 @@ amdgpu_connector_update_scratch_regs(struct drm_connector *connector,
 	struct drm_encoder *encoder;
 	const struct drm_connector_helper_funcs *connector_funcs = connector->helper_private;
 	bool connected;
+#ifndef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	int i;
+#endif
 
 	best_encoder = connector_funcs->best_encoder(connector);
 
@@ -271,7 +273,9 @@ amdgpu_connector_find_encoder(struct drm_connector *connector,
 			       int encoder_type)
 {
 	struct drm_encoder *encoder;
+#ifndef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	int i;
+#endif
 
 #ifdef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	drm_connector_for_each_possible_encoder(connector, encoder) {
@@ -388,7 +392,9 @@ static struct drm_encoder *
 amdgpu_connector_best_single_encoder(struct drm_connector *connector)
 {
 	struct drm_encoder *encoder;
+#ifndef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	int i;
+#endif
 
 	/* pick the first one */
 #ifdef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
@@ -1163,7 +1169,9 @@ amdgpu_connector_dvi_detect(struct drm_connector *connector, bool force)
 	/* find analog encoder */
 	if (amdgpu_connector->dac_load_detect) {
 		struct drm_encoder *encoder;
+#ifndef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 		int i;
+#endif
 
 #ifdef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 		drm_connector_for_each_possible_encoder(connector, encoder) {
@@ -1219,7 +1227,9 @@ amdgpu_connector_dvi_encoder(struct drm_connector *connector)
 {
 	struct amdgpu_connector *amdgpu_connector = to_amdgpu_connector(connector);
 	struct drm_encoder *encoder;
+#ifndef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	int i;
+#endif
 
 #ifdef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	drm_connector_for_each_possible_encoder(connector, encoder) {
@@ -1387,7 +1397,9 @@ u16 amdgpu_connector_encoder_get_dp_bridge_encoder_id(struct drm_connector *conn
 {
 	struct drm_encoder *encoder;
 	struct amdgpu_encoder *amdgpu_encoder;
+#ifndef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	int i;
+#endif
 
 #ifdef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	drm_connector_for_each_possible_encoder(connector, encoder) {
@@ -1412,7 +1424,9 @@ static bool amdgpu_connector_encoder_is_hbr2(struct drm_connector *connector)
 {
 	struct drm_encoder *encoder;
 	struct amdgpu_encoder *amdgpu_encoder;
+#ifndef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
 	int i;
+#endif
 	bool found = false;
 
 #ifdef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
