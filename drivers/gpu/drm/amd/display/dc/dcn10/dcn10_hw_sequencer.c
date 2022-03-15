fcb278d9d29364cea6b2553304c76611f09eb71d
Roman Li <Roman.Li@amd.com>
2022-03-15 14:57:34 -0400

    drm/amd/display: Remove redundant dsc power gating from init_hw
    [Why]
    DSC Power down code has been moved from dcn31_init_hw into init_pipes()
    Need to remove it from dcn10_init_hw() as well to avoid duplicated action
    on dcn1.x/2.x
    
    [How]
    Remove DSC power down code from dcn10_init_hw()
    
    Fixes: 8fa6f4c5715c ("drm/amd/display: fixed the DSC power off sequence during Driver PnP")
    
    Reviewed-by: Anthony Koo <Anthony.Koo@amd.com>
    Reviewed-by: Eric Yang <Eric.Yang2@amd.com>
    Acked-by: Alex Hung <alex.hung@amd.com>
    Signed-off-by: Roman Li <Roman.Li@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Change-Id: Ia5b645bb492538c6ac36ef4c485cfdfc3a77a32a

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hw_sequencer.c
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hw_sequencer.c
@@ -1515,15 +1515,6 @@ void dcn10_init_hw(struct dc *dc)
 		}
 	}
 
-#ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
-	/* Power gate DSCs */
-	if (!is_optimized_init_done) {
-		for (i = 0; i < res_pool->res_cap->num_dsc; i++)
-			if (hws->funcs.dsc_pg_control != NULL)
-				hws->funcs.dsc_pg_control(hws, res_pool->dscs[i]->inst, false);
-	}
-#endif
-
 	/* we want to turn off all dp displays before doing detection */
 	dc_link_blank_all_dp_displays(dc);
 
