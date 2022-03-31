f3da31b5e6906a65f40262d44a128e3e7b1e7cab
Flora Cui <flora.cui@amd.com>
2022-03-31 18:11:19 +0800

    drm/amdkcl: rework kcl implementation drm_firmware_drivers_only()
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- include/kcl/backport/kcl_drm_drv.h
+++ include/kcl/backport/kcl_drm_drv.h
@@ -27,6 +27,8 @@
 #ifndef __KCL_BACKPORT_KCL_DRM_DRV_H__
 #define __KCL_BACKPORT_KCL_DRM_DRV_H__
 
+#include <linux/console.h>
+
 #ifdef HAVE_DRM_DEV_UNPLUG
 /*
  * v5.1-rc5-1150-gbd53280ef042 drm/drv: Fix incorrect resolution of merge conflict
@@ -50,5 +52,10 @@ void _kcl_drm_dev_unplug(struct drm_device *dev)
 #define drm_dev_unplug _kcl_drm_dev_unplug
 #endif
 #endif
+
+
+#ifndef HAVE_DRM_FIRMWARE_DRIVERS_ONLY
+#define drm_firmware_drivers_only vgacon_text_force
+#endif /* HAVE_DRM_FIRMWARE_DRIVERS_ONLY */
 
 #endif
