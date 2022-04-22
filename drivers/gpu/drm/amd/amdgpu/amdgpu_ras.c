15b6fd42648b051db3ae868782d9b0358fa2e75f
Haowen Bai <baihaowen@meizu.com>
2022-04-22 14:03:57 +0800

    drm/amdgpu: Remove useless kfree
    After alloc fail, we do not need to kfree.
    
    Signed-off-by: Haowen Bai <baihaowen@meizu.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
@@ -1925,7 +1925,6 @@ static int amdgpu_ras_realloc_eh_data_space(struct amdgpu_device *adev,
 	void *bps = kmalloc(align_space * sizeof(*data->bps), GFP_KERNEL);
 
 	if (!bps) {
-		kfree(bps);
 		return -ENOMEM;
 	}
 
