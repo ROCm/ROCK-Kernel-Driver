ca4e94e8414ce6f7696bdc257d6226f957da43e7
Jonathan Kim <jonathan.kim@amd.com>
2022-05-09 10:51:56 -0400

    drm/amdkfd: add debug set flags operation
    Allow the debugger to set single memory and single ALU operations.
    
    Some exceptions are imprecise (memory violations, address watch) in the
    sense that a trap occurs only when the exception interrupt occurs and
    not at the non-halting faulty instruction.  Trap temporaries 0 & 1 save
    the program counter address, which means that these values will not point
    to the faulty instruction address but to whenever the interrupt was
    raised.
    
    Setting the Single Memory Operations flag will inject an automatic wait
    on every memory operation instruction forcing imprecise memory exceptions
    to become precise at the cost of performance.  This setting is not
    permitted on debug devices that support only a global setting of this
    option.
    
    Return the previous set flags to the debugger as well.
    
    v3: make precise mem op the only available flag for now.
    
    v2: add gfx11 support.
    
    Signed-off-by: Jonathan Kim <jonathan.kim@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_debug.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_debug.c
@@ -23,6 +23,7 @@
 #include "kfd_debug.h"
 #include "kfd_device_queue_manager.h"
 #include <linux/file.h>
+#include <uapi/linux/kfd_ioctl.h>
 
 #define MAX_WATCH_ADDRESSES	4
 
@@ -425,6 +426,40 @@ static void kfd_dbg_clear_process_address_watch(struct kfd_process *target)
 			kfd_dbg_trap_clear_dev_address_watch(target->pdds[i], j);
 }
 
+int kfd_dbg_trap_set_flags(struct kfd_process *target, uint32_t *flags)
+{
+	uint32_t prev_flags = target->dbg_flags;
+	int i, r = 0;
+
+	for (i = 0; i < target->n_pdds; i++) {
+		if (!kfd_dbg_is_per_vmid_supported(target->pdds[i]->dev) &&
+			(*flags & KFD_DBG_TRAP_FLAG_SINGLE_MEM_OP)) {
+			*flags = prev_flags;
+			return -EACCES;
+		}
+	}
+
+	target->dbg_flags = *flags & KFD_DBG_TRAP_FLAG_SINGLE_MEM_OP;
+	*flags = prev_flags;
+	for (i = 0; i < target->n_pdds; i++) {
+		struct kfd_process_device *pdd = target->pdds[i];
+
+		if (!kfd_dbg_is_per_vmid_supported(pdd->dev))
+			continue;
+
+		if (!pdd->dev->shared_resources.enable_mes)
+			r = debug_refresh_runlist(pdd->dev->dqm);
+		else
+			r = kfd_dbg_set_mes_debug_mode(pdd);
+
+		if (r) {
+			target->dbg_flags = prev_flags;
+			break;
+		}
+	}
+
+	return r;
+}
 
 /* kfd_dbg_trap_deactivate:
  *	target: target process
@@ -439,9 +474,12 @@ void kfd_dbg_trap_deactivate(struct kfd_process *target, bool unwind, int unwind
 	int i, count = 0;
 
 	if (!unwind) {
+		uint32_t flags = 0;
 		cancel_work_sync(&target->debug_event_workarea);
 		kfd_dbg_clear_process_address_watch(target);
 		kfd_dbg_trap_set_wave_launch_mode(target, 0);
+
+		kfd_dbg_trap_set_flags(target, &flags);
 	}
 
 	for (i = 0; i < target->n_pdds; i++) {
