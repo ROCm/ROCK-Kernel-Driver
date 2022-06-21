42a01e535d8e865f89fac71e85a50f51589b882d
Leslie Shi <Yuliang.Shi@amd.com>
2022-06-21 13:43:48 +0800

    drm/amdkcl: add kcl/kcl_iosys-map.h
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -13,6 +13,7 @@
 #include <kcl/kcl_compiler_attributes.h>
 #include <kcl/kcl_dma_mapping.h>
 #include <kcl/kcl_dma-buf-map.h>
+#include <kcl/kcl_iosys-map.h>
 #include <kcl/kcl_eventpoll.h>
 #include <kcl/kcl_i2c.h>
 #include <kcl/kcl_firmware.h>
