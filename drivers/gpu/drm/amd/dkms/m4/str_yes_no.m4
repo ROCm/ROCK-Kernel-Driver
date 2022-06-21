f824f8c9964772dee9cafb6aa3a729a0bf73b18b
Leslie Shi <Yuliang.Shi@amd.com>
2022-06-21 16:57:30 +0800

    drm/amdkcl: Test whether str_yes_no() is available
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ drivers/gpu/drm/amd/dkms/m4/str_yes_no.m4
@@ -0,0 +1,16 @@
+dnl #
+dnl # commit ea4692c75e1c63926e4fb0728f5775ef0d733888
+dnl # lib/string_helpers: Consolidate string helpers implementation 
+dnl #
+AC_DEFUN([AC_AMDGPU_STR_YES_NO], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <linux/string_helpers.h>
+		], [
+			const char *str = str_yes_no(true);
+		], [
+			AC_DEFINE(HAVE_STR_YES_NO, 1,
+				[str_yes_no() is defined])
+		])
+	])
+])
---
+++ include/kcl/kcl_string_helpers.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef AMDKCL_STRING_HELPERS_H
+#define AMDKCL_STRING_HELPERS_H
+
+
+/* Copied from v5.17-rc2-224-gea4692c75e1c linux/string_helpers.h */
+
+#ifndef HAVE_STR_YES_NO
+static inline const char *str_yes_no(bool v)
+{
+        return v ? "yes" : "no";
+}
+
+static inline const char *str_on_off(bool v)
+{
+        return v ? "on" : "off";
+}
+
+static inline const char *str_enable_disable(bool v)
+{
+        return v ? "enable" : "disable";
+}
+
+static inline const char *str_enabled_disabled(bool v)
+{
+        return v ? "enabled" : "disabled";
+}
+
+#endif /* HAVE_STR_YES_NO */
+#endif
