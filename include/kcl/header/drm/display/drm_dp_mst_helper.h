4a08f29f4c312bf115fe6e5e1a08d9a5ed508170
Leslie Shi <Yuliang.Shi@amd.com>
2022-07-12 16:34:47 +0800

    drm/amdkcl: test for drm/display/drm_dp_mst_helper.h
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/header/drm/display/drm_dp_mst_helper.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_DISPLAY_DRM_DP_MST_HELPER_H_H_
+#define _KCL_HEADER_DISPLAY_DRM_DP_MST_HELPER_H_H_
+
+#if defined(HAVE_DRM_DISPLAY_DRM_DP_MST_HELPER_H)
+#include_next <drm/display/drm_dp_mst_helper.h>
+#elif defined(HAVE_DRM_DP_DRM_DP_MST_HELPER_H)
+#include_next <drm/dp/drm_dp_mst_helper.h>
+#else
+#include_next <drm/drm_dp_mst_helper.h>
+#endif
+
+#endif
+
