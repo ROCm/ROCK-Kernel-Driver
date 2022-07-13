1d769604bc0430eebca203fbab249f4d21b35a00
Leslie Shi <Yuliang.Shi@amd.com>
2022-07-13 15:06:51 +0800

    drm/amdkcl: test for drm_* headers moved into drm/display directory
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/header/drm/display/drm_dsc.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_DISPLAY_DRM_DSC_H_H_
+#define _KCL_HEADER_DISPLAY_DRM_DSC_H_H_
+
+#if defined(HAVE_DRM_DISPLAY_DRM_DSC_H)
+#include_next <drm/display/drm_dsc.h>
+#else
+#include <drm/drm_dsc.h>
+#endif
+
+#endif
+
