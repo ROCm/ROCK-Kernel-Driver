b5dd28f568b9bf6e3be7dbb9bb865ece5792027b
Leslie Shi <Yuliang.Shi@amd.com>
2022-07-19 18:12:49 +0800

    drm/amdkcl: fix build error by adding missing arguments
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_process.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -1625,7 +1625,7 @@ struct kfd_process_device *kfd_create_process_device_data(struct kfd_node *dev,
 						&pdd->proc_ctx_bo,
 						&pdd->proc_ctx_gpu_addr,
 						&pdd->proc_ctx_cpu_ptr,
-						false);
+						false, true);
 		if (retval) {
 			pr_err("failed to allocate process context bo\n");
 			goto err_free_pdd;
