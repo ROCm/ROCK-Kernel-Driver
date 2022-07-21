bab3f5bac8054b765dfdf2276e42068bbc92d6d2
Leslie Shi <Yuliang.Shi@amd.com>
2022-07-21 15:03:41 +0800

    drm/amdkcl: Test whether struct drm_fb_helper has buffer member
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -1919,6 +1919,7 @@ bool amdgpu_crtc_get_scanout_position(struct drm_crtc *crtc,
 static bool
 amdgpu_display_robj_is_fb(struct amdgpu_device *adev, struct amdgpu_bo *robj)
 {
+#ifdef HAVE_DRM_FB_HELPER_BUFFER
 	struct drm_device *dev = adev_to_drm(adev);
 	struct drm_fb_helper *fb_helper = dev->fb_helper;
 
@@ -1929,6 +1930,9 @@ amdgpu_display_robj_is_fb(struct amdgpu_device *adev, struct amdgpu_bo *robj)
 		return false;
 
 	return true;
+#else
+	return false;
+#endif
 }
 #endif
 
