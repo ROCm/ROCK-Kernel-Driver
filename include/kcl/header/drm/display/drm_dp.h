68cad7d344a286458fd067116b567cb8fddcf6b4
Leslie Shi <Yuliang.Shi@amd.com>
2022-07-27 10:54:21 +0800

    drm/amdkcl: fake macro in drm/display/dsc_dp.h for legacy os
    v2: also include drm_dp_helper.h when drm_dp.h not exist
    v3: drop kcl_drm_dp.h and add some missing macros in kcl_drm_dp_helper.h
        for legacy os
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/header/drm/display/drm_dp.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_DISPLAY_DRM_DP_H_H_
+#define _KCL_HEADER_DISPLAY_DRM_DP_H_H_
+
+#if defined(HAVE_DRM_DISPLAY_DRM_DP_H)
+#include_next <drm/display/drm_dp.h>
+#elif defined(HAVE_DRM_DISPLAY_DRM_DP_HELPER_H)
+#include <drm/display/drm_dp_helper.h>
+#elif defined(HAVE_DRM_DP_DRM_DP_HELPER_H)
+#include <drm/dp/drm_dp_helper.h>
+#else
+#include <drm/drm_dp_helper.h>
+#endif
+
+#endif
+
