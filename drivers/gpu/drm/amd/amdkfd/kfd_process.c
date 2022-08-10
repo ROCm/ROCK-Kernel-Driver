ROCm/ROCK-Kernel-Driver

commit 90fbc2dc692e4f99ceb7fc0656dc3c022a9a3b6b

drm/amdkcl: cleanup kcl_bitmap_xxx
rename to bitmap_xxx to avoid changes in amdgpu part

Reviewed-by: Ma Jun <majun@amd.com>
Signed-off-by: Flora Cui <flora.cui@amd.com>
Change-Id: I3401f1758f146521f7115a14880f779811e3bb4f

drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -1065,7 +1065,7 @@ static void kfd_process_destroy_pdds(struct kfd_process *p)
 #if defined(HAVE_BITMAP_FUNCS)
 		bitmap_free(pdd->qpd.doorbell_bitmap);
 #else
-		kcl_bitmap_free(pdd->qpd.doorbell_bitmap);
+		bitmap_free(pdd->qpd.doorbell_bitmap);
 #endif
 		idr_destroy(&pdd->alloc_idr);
 		mutex_destroy(&pdd->qpd.doorbell_lock);
@@ -1583,7 +1583,7 @@ static int init_doorbell_bitmap(struct qcm_process_device *qpd,
 	qpd->doorbell_bitmap = bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
 					     GFP_KERNEL);
 #else
-	qpd->doorbell_bitmap = kcl_bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
+	qpd->doorbell_bitmap = bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
 					     GFP_KERNEL);
 #endif
 	if (!qpd->doorbell_bitmap)

drivers/gpu/drm/amd/amdkfd/kfd_process_queue_manager.c
@@ -155,7 +155,7 @@ int pqm_init(struct process_queue_manager *pqm, struct kfd_process *p)
 	pqm->queue_slot_bitmap = bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
 					       GFP_KERNEL);
 #else
-	pqm->queue_slot_bitmap = kcl_bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
+	pqm->queue_slot_bitmap = bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
 					       GFP_KERNEL);
 #endif
 	if (!pqm->queue_slot_bitmap)
@@ -182,7 +182,7 @@ void pqm_uninit(struct process_queue_manager *pqm)
 #if defined(HAVE_BITMAP_FUNCS)
 	bitmap_free(pqm->queue_slot_bitmap);
 #else
-	kcl_bitmap_free(pqm->queue_slot_bitmap);
+	bitmap_free(pqm->queue_slot_bitmap);
 #endif
 	pqm->queue_slot_bitmap = NULL;
 }

drivers/gpu/drm/amd/backport/backport.h
@@ -90,6 +90,6 @@
 #include "kcl/kcl_drm_aperture.h"
 #include <kcl/kcl_intel_family.h>
 #include <kcl/kcl_drm_simple_kms_helper.h>
-#include <kcl/kcl_bitmap.h>
+#include <kcl/backport/kcl_bitmap.h>
 
 #endif /* AMDGPU_BACKPORT_H */

include/kcl/backport/kcl_bitmap.h
@@ -1,0 +1,34 @@
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * VA LINUX SYSTEMS AND/OR ITS SUPPLIERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef __KCL_BACKPORT_KCL_BITMAP_H_
+#define __KCL_BACKPORT_KCL_BITMAP_H_
+
+#include <linux/bitmap.h>
+#include <kcl/kcl_bitmap.h>
+
+#ifndef HAVE_BITMAP_FUNCS
+#define bitmap_alloc kcl_bitmap_alloc
+#define bitmap_zalloc kcl_bitmap_zalloc
+#define bitmap_free kcl_bitmap_free
+#endif /* HAVE_BITMAP_FUNCS */
+
+#endif /* KCL_BITMAP_H */

commit 726d6333eec3cdb20324d0dee0f1423ea1f1d5d4

drm/amdkfd: Remove useless #ifdefs
Both branches are exactly the same, so the #ifdefs are no longer needed.

Fixes: 90fbc2dc692e ("drm/amdkcl: cleanup kcl_bitmap_xxx")
CC: Flora Cui <flora.cui@amd.com>
Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
Reviewed-by: Flora Cui <flora.cui@amd.com>
Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -1028,11 +1028,7 @@ static void kfd_process_destroy_pdds(struct kfd_process *p)
 		if (pdd->qpd.cwsr_kaddr && !pdd->qpd.cwsr_base)
 			free_pages((unsigned long)pdd->qpd.cwsr_kaddr,
 				get_order(KFD_CWSR_TBA_TMA_SIZE));
-#if defined(HAVE_BITMAP_FUNCS)
 		bitmap_free(pdd->qpd.doorbell_bitmap);
-#else
-		bitmap_free(pdd->qpd.doorbell_bitmap);
-#endif
 		idr_destroy(&pdd->alloc_idr);
 		mutex_destroy(&pdd->qpd.doorbell_lock);
 
@@ -1548,13 +1544,8 @@ static int init_doorbell_bitmap(struct qcm_process_device *qpd,
 	if (!KFD_IS_SOC15(dev))
 		return 0;
 
-#if defined(HAVE_BITMAP_FUNCS)
 	qpd->doorbell_bitmap = bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
 					     GFP_KERNEL);
-#else
-	qpd->doorbell_bitmap = bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
-					     GFP_KERNEL);
-#endif
 	if (!qpd->doorbell_bitmap)
 		return -ENOMEM;
 

drivers/gpu/drm/amd/amdkfd/kfd_process_queue_manager.c
@@ -151,13 +151,8 @@ void kfd_process_dequeue_from_all_devices(struct kfd_process *p)
 int pqm_init(struct process_queue_manager *pqm, struct kfd_process *p)
 {
 	INIT_LIST_HEAD(&pqm->queues);
-#if defined(HAVE_BITMAP_FUNCS)
 	pqm->queue_slot_bitmap = bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
 					       GFP_KERNEL);
-#else
-	pqm->queue_slot_bitmap = bitmap_zalloc(KFD_MAX_NUM_OF_QUEUES_PER_PROCESS,
-					       GFP_KERNEL);
-#endif
 	if (!pqm->queue_slot_bitmap)
 		return -ENOMEM;
 	pqm->process = p;
@@ -179,11 +174,7 @@ void pqm_uninit(struct process_queue_manager *pqm)
 		kfree(pqn);
 	}
 
-#if defined(HAVE_BITMAP_FUNCS)
 	bitmap_free(pqm->queue_slot_bitmap);
-#else
-	bitmap_free(pqm->queue_slot_bitmap);
-#endif
 	pqm->queue_slot_bitmap = NULL;
 }
 