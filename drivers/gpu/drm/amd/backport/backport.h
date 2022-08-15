67e1e3991ce35f012db8055ce8ee98618f83023e
Asher Song <Asher.Song@amd.com>
2022-08-15 22:07:12 +0800

    drm/amdkcl: fake macro for_each_cpu_wrap and function cpumask_next_wrap
    It's caused by 78231f639e2eec3f14de8bb8309f459413ca86b4
    drm/amdkfd: Try to schedule bottom half on same core
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -105,5 +105,6 @@
 #include <kcl/kcl_hypervisor.h>
 #include <kcl/backport/kcl_workqueue_backport.h>
 #include <kcl/kcl_string_helpers.h>
+#include <kcl/kcl_cpumask.h>
 
 #endif /* AMDGPU_BACKPORT_H */
