eba10fa9a1b07c205a657d3f848af55f2e3f1d53
Leslie Shi <Yuliang.Shi@amd.com>
2022-08-18 10:14:19 +0800

    Revert "drm/amdkfd: Add CMA API"
    This reverts commit 9980ee28b7cf743198f3450c8357135baff4fe8b.
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Asher Song  <asher.song@amd.com>

--- include/uapi/linux/kfd_ioctl.h
+++ include/uapi/linux/kfd_ioctl.h
@@ -1076,37 +1076,6 @@ struct kfd_ioctl_ipc_import_handle_args {
 	__u32 flags;		/* from KFD */
 };
 
-struct kfd_memory_range {
-	__u64 va_addr;
-	__u64 size;
-};
-
-/* flags definitions
- * BIT0: 0: read operation, 1: write operation.
- * This also identifies if the src or dst array belongs to remote process
- */
-#define KFD_CROSS_MEMORY_RW_BIT (1 << 0)
-#define KFD_SET_CROSS_MEMORY_READ(flags) (flags &= ~KFD_CROSS_MEMORY_RW_BIT)
-#define KFD_SET_CROSS_MEMORY_WRITE(flags) (flags |= KFD_CROSS_MEMORY_RW_BIT)
-#define KFD_IS_CROSS_MEMORY_WRITE(flags) (flags & KFD_CROSS_MEMORY_RW_BIT)
-
-struct kfd_ioctl_cross_memory_copy_args {
-	/* to KFD: Process ID of the remote process */
-	__u32 pid;
-	/* to KFD: See above definition */
-	__u32 flags;
-	/* to KFD: Source GPU VM range */
-	__u64 src_mem_range_array;
-	/* to KFD: Size of above array */
-	__u64 src_mem_array_size;
-	/* to KFD: Destination GPU VM range */
-	__u64 dst_mem_range_array;
-	/* to KFD: Size of above array */
-	__u64 dst_mem_array_size;
-	/* from KFD: Total amount of bytes copied */
-	__u64 bytes_copied;
-};
-
 /* Guarantee host access to memory */
 #define KFD_IOCTL_SVM_FLAG_HOST_ACCESS 0x00000001
 /* Fine grained coherency between all devices with access */
@@ -1395,15 +1364,11 @@ struct kfd_ioctl_set_xnack_mode_args {
 #define AMDKFD_IOC_DBG_TRAP			\
 		AMDKFD_IOWR(0x82, struct kfd_ioctl_dbg_trap_args)
 
-#define AMDKFD_IOC_CROSS_MEMORY_COPY		\
-		AMDKFD_IOWR(0x83, struct kfd_ioctl_cross_memory_copy_args)
-
 #define AMDKFD_IOC_RLC_SPM		\
 		AMDKFD_IOWR(0x84, struct kfd_ioctl_spm_args)
 
 
 #define AMDKFD_COMMAND_START_2		0x80
 #define AMDKFD_COMMAND_END_2		0x85
-
 
 #endif
