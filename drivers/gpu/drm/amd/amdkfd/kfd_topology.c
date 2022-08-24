f78e18d9424165d16e7bbf04ad34ee7a8e165a03
Guchun Chen <guchun.chen@amd.com>
2022-08-24 22:09:58 +0800

    drm/amdgpu: create p2p links unconditionally in dkms
    P2P needs to be enabled on old kernels without setting
    CONFIG_HSA_AMD_P2P, so p2p links needs to be created
    unconditionally. This also releases build option from
    upstream.
    
    Suggested-by: Ramesh Errabolu <Ramesh.Errabolu@amd.com>
    Signed-off-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_topology.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_topology.c
@@ -1504,7 +1504,6 @@ static int kfd_create_indirect_link_prop(struct kfd_topology_device *kdev, int g
 	return ret;
 }
 
-#if defined(CONFIG_HSA_AMD_P2P)
 static int kfd_add_peer_prop(struct kfd_topology_device *kdev,
 		struct kfd_topology_device *peer, int from, int to)
 {
@@ -1572,16 +1571,12 @@ static int kfd_add_peer_prop(struct kfd_topology_device *kdev,
 
 	return ret;
 }
-#endif
 
 static int kfd_dev_create_p2p_links(void)
 {
 	struct kfd_topology_device *dev;
 	struct kfd_topology_device *new_dev;
-#if defined(CONFIG_HSA_AMD_P2P)
-	uint32_t i;
-#endif
-	uint32_t k;
+	uint32_t i, k;
 	int ret = 0;
 
 	k = 0;
@@ -1602,7 +1597,6 @@ static int kfd_dev_create_p2p_links(void)
 		goto out;
 
 	/* create p2p links */
-#if defined(CONFIG_HSA_AMD_P2P)
 	i = 0;
 	list_for_each_entry(dev, &topology_device_list, list) {
 		if (dev == new_dev)
@@ -1623,7 +1617,6 @@ static int kfd_dev_create_p2p_links(void)
 next:
 		i++;
 	}
-#endif
 
 out:
 	return ret;
