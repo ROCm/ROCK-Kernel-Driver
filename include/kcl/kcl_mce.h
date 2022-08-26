b43d28aab6465b0b09a396da92dcd7acc4680668
Asher Song <Asher.Song@amd.com>
2022-08-26 17:10:21 +0800

    drm/amdkcl: Test whether smca_get_bank_type() has two arguments
    It's caused by 91f75eb481cfaee5c4ed8fb5214bf2fbfa04bd7b
    "x86/MCE/AMD, EDAC/mce_amd: Support non-uniform MCA bank type enumeration"
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- include/kcl/kcl_mce.h
+++ include/kcl/kcl_mce.h
@@ -11,12 +11,10 @@
 #define XEC(x, mask)			(((x) >> 16) & mask)
 #endif
 
-#if !defined(HAVE_SMCA_GET_BANK_TYPE)
-#ifdef HAVE_SMCA_BANK_STRUCT
-enum smca_bank_types smca_get_bank_type(unsigned int bank);
+#if defined(HAVE_SMCA_GET_BANK_TYPE_WITH_TWO_ARGUMENTS) || defined(HAVE_SMCA_GET_BANK_TYPE_WITH_ONE_ARGUMENT) || defined(HAVE_STRUCT_SMCA_BANK)
+enum smca_bank_types kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank);
 #else
-int smca_get_bank_type(unsigned int bank);
-#endif
+int kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank);
 #endif
 
 #ifndef HAVE_MCE_PRIO_UC
