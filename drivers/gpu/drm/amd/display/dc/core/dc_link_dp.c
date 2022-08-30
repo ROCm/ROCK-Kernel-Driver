62c2a09a235aea726468d7bc44083b9106d5767c
Ian Chen <ian.chen@amd.com>
2022-08-30 14:50:24 +0800

    drm/amd/display: Refactor edp panel power sequencer(PPS) codes
    [Why & How]
    Move extra panel power sequencer settings into panel_cofig struct.
    
    Reviewed-by: Anthony Koo <Anthony.Koo@amd.com>
    Acked-by: Wayne Lin <wayne.lin@amd.com>
    Signed-off-by: Ian Chen <ian.chen@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
@@ -7116,9 +7116,8 @@ void dp_enable_link_phy(
 
 void edp_add_delay_for_T9(struct dc_link *link)
 {
-	if (link->local_sink &&
-			link->local_sink->edid_caps.panel_patch.extra_delay_backlight_off > 0)
-		udelay(link->local_sink->edid_caps.panel_patch.extra_delay_backlight_off * 1000);
+	if (link && link->panel_config.pps.extra_delay_backlight_off > 0)
+		udelay(link->panel_config.pps.extra_delay_backlight_off * 1000);
 }
 
 bool edp_receiver_ready_T9(struct dc_link *link)
@@ -7174,9 +7173,8 @@ bool edp_receiver_ready_T7(struct dc_link *link)
 		} while (time_taken_in_ns < 50 * 1000000); //MAx T7 is 50ms
 	}
 
-	if (link->local_sink &&
-			link->local_sink->edid_caps.panel_patch.extra_t7_ms > 0)
-		udelay(link->local_sink->edid_caps.panel_patch.extra_t7_ms * 1000);
+	if (link && link->panel_config.pps.extra_t7_ms > 0)
+		udelay(link->panel_config.pps.extra_t7_ms * 1000);
 
 	return result;
 }
