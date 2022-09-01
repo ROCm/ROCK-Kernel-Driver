9e6522b0b99869878def5fc3e3d0517a7b712a16
Jonathan Kim <jonathan.kim@amd.com>
2022-09-01 11:27:15 -0400

    drm/amdkfd: enable trap workaround for gfx11
    Due to a HW bug, waves in only half the shader arrays can enter trap.
    
    When starting a debug session, relocate all waves to the first shader
    array of each shader engine and mask off the 2nd shader array as
    unavailable.
    
    When ending a debug session, re-enable the 2nd shader array per
    shader engine.
    
    User CU masking per queue cannot be guaranteed to remain functional
    if requested during debugging (e.g. user cu mask requests only 2nd shader
    array as an available resource leading to zero HW resources available)
    nor can runtime be alerted of any of these changes during execution.
    
    Make user CU masking and debugging mutual exclusive with respect to
    availability.
    
    If the debugger tries to attach to a process with a user cu masked
    queue, return the runtime status as enabled but busy.
    
    If the debugger tries to attach and fails to reallocate queue waves to
    the first shader array of each shader engine, return the runtime status
    as enabled but with an error.
    
    Signed-off-by: Jonathan Kim <jonathan.kim@amd.com>
    Reviewed-by: Felix Kuehling <felix.kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
@@ -242,6 +242,9 @@ static int add_queue_mes(struct device_queue_manager *dqm, struct queue *q,
 	queue_input.paging = false;
 	queue_input.tba_addr = qpd->tba_addr;
 	queue_input.tma_addr = qpd->tma_addr;
+	queue_input.trap_en = KFD_GC_VERSION(q->device) < IP_VERSION(11, 0, 0) ||
+			      KFD_GC_VERSION(q->device) >= IP_VERSION(12, 0, 0) ||
+			      q->properties.is_dbg_wa;
 
 	queue_type = convert_to_mes_queue_type(q->properties.type);
 	if (queue_type < 0) {
@@ -1787,6 +1790,9 @@ static int create_queue_cpsch(struct device_queue_manager *dqm, struct queue *q,
 	 */
 	q->properties.is_evicted = !!qpd->evicted;
 	q->properties.is_suspended = false;
+	q->properties.is_dbg_wa = qpd->pqm->process->debug_trap_enabled &&
+			KFD_GC_VERSION(q->device) >= IP_VERSION(11, 0, 0) &&
+			KFD_GC_VERSION(q->device) < IP_VERSION(12, 0, 0);
 
 	if (qd)
 		mqd_mgr->restore_mqd(mqd_mgr, &q->mqd, q->mqd_mem_obj, &q->gart_mqd_addr,
