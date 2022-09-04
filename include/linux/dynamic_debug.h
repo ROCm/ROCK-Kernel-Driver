ecfd4929236a3949e9243add154adf5c02b5a3a8
Jim Cromie <jim.cromie@gmail.com>
2022-09-04 15:40:52 -0600

    kernel/module: add __dyndbg_classes section
    Add __dyndbg_classes section, using __dyndbg as a model. Use it:
    
    vmlinux.lds.h:
    
    KEEP the new section, which also silences orphan section warning on
    loadable modules.  Add (__start_/__stop_)__dyndbg_classes linker
    symbols for the c externs (below).
    
    kernel/module/main.c:
    - fill new fields in find_module_sections(), using section_objs()
    - extend callchain prototypes
      to pass classes, length
      load_module(): pass new info to dynamic_debug_setup()
      dynamic_debug_setup(): new params, pass through to ddebug_add_module()
    
    dynamic_debug.c:
    - add externs to the linker symbols.
    
    ddebug_add_module():
    - It currently builds a debug_table, and *will* find and attach classes.
    
    dynamic_debug_init():
    - add class fields to the _ddebug_info cursor var: di.
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Link: https://lore.kernel.org/r/20220904214134.408619-16-jim.cromie@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 66f4006b6ace1a1a1a1dca4225972f79a298e251)

--- include/linux/dynamic_debug.h
+++ include/linux/dynamic_debug.h
@@ -54,7 +54,9 @@ struct _ddebug {
 /* encapsulate linker provided built-in (or module) dyndbg data */
 struct _ddebug_info {
 	struct _ddebug *descs;
+	struct ddebug_class_map *classes;
 	unsigned int num_descs;
+	unsigned int num_classes;
 };
 
 #if defined(CONFIG_DYNAMIC_DEBUG_CORE)
