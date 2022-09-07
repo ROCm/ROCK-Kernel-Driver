3e9e5f1c4fce8faf2976f9b9676b6d175ab5e18d
Ma Jun <Jun.Ma2@amd.com>
2022-09-07 14:16:27 +0800

    drm/amdkcl: Optimize cancel_work() test
    Search the __cancel_work() instead of cancel_work() to
    optimize the cancel_work() test.
    
    Suggested-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Change-Id: I595e947b5c98bc601b7e173fbd2ceed4ccd9f7f8

--- include/kcl/backport/kcl_workqueue_backport.h
+++ include/kcl/backport/kcl_workqueue_backport.h
@@ -6,8 +6,8 @@
 #include <linux/workqueue.h>
 
 #ifndef HAVE_CANCEL_WORK
-extern bool (*_kcl_cancel_work)(struct work_struct *work);
-#define cancel_work _kcl_cancel_work
+extern bool kcl_cancel_work(struct work_struct *work);
+#define cancel_work kcl_cancel_work
 #endif
 
 #endif /* KCL_LINUX_WORKQUEUE_BACKPORT_H */
