1bb3f5db2602e5159d8e13ab70e860929f9bc66c
Asher Song <Asher.Song@amd.com>
2022-09-14 15:35:09 +0800

    drm/amdkcl: wrap code under macro HAVE_CHUNK_ID_SYNOBJ_IN_OUT, HAVE_CHUNK_ID_SYNCOBJ_TIMELINE_WAIT_SIGNAL, HAVE_AMDGPU_CHUNK_ID_SCHEDULED_DEPENDENCIES
    It's caused by d8ede49db6249fb8d274dde7475e7b33d2ab126c
    "drm/amdgpu: reorder CS code"
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
@@ -438,7 +438,6 @@ static int amdgpu_cs_p2_dependencies(struct amdgpu_cs_parser *p,
 	}
 	return 0;
 }
-
 static int amdgpu_syncobj_lookup_and_add(struct amdgpu_cs_parser *p,
 					 uint32_t handle, u64 point,
 					 u64 flags)
@@ -522,7 +521,9 @@ static int amdgpu_cs_p2_syncobj_out(struct amdgpu_cs_parser *p,
 			drm_syncobj_find(p->filp, deps[i].handle);
 		if (!p->post_deps[i].syncobj)
 			return -EINVAL;
+#if defined(HAVE_CHUNK_ID_SYNCOBJ_TIMELINE_WAIT_SIGNAL)
 		p->post_deps[i].chain = NULL;
+#endif
 		p->post_deps[i].point = 0;
 		p->num_post_deps++;
 	}
@@ -552,14 +553,14 @@ static int amdgpu_cs_p2_syncobj_timeline_signal(struct amdgpu_cs_parser *p,
 
 	for (i = 0; i < num_deps; ++i) {
 		struct amdgpu_cs_post_dep *dep = &p->post_deps[i];
-
+#if defined(HAVE_CHUNK_ID_SYNCOBJ_TIMELINE_WAIT_SIGNAL)
 		dep->chain = NULL;
 		if (syncobj_deps[i].point) {
 			dep->chain = dma_fence_chain_alloc();
 			if (!dep->chain)
 				return -ENOMEM;
 		}
-
+#endif
 		dep->syncobj = drm_syncobj_find(p->filp,
 						syncobj_deps[i].handle);
 		if (!dep->syncobj) {
@@ -572,7 +573,6 @@ static int amdgpu_cs_p2_syncobj_timeline_signal(struct amdgpu_cs_parser *p,
 
 	return 0;
 }
-#endif
 
 static int amdgpu_cs_p2_shadow(struct amdgpu_cs_parser *p,
 			       struct amdgpu_cs_chunk *chunk)
@@ -611,7 +611,9 @@ static int amdgpu_cs_pass2(struct amdgpu_cs_parser *p)
 				return r;
 			break;
 		case AMDGPU_CHUNK_ID_DEPENDENCIES:
+#if defined(HAVE_AMDGPU_CHUNK_ID_SCHEDULED_DEPENDENCIES)
 		case AMDGPU_CHUNK_ID_SCHEDULED_DEPENDENCIES:
+#endif
 			r = amdgpu_cs_p2_dependencies(p, chunk);
 			if (r)
 				return r;
@@ -626,6 +628,7 @@ static int amdgpu_cs_pass2(struct amdgpu_cs_parser *p)
 			if (r)
 				return r;
 			break;
+#if defined(HAVE_CHUNK_ID_SYNCOBJ_TIMELINE_WAIT_SIGNAL)
 		case AMDGPU_CHUNK_ID_SYNCOBJ_TIMELINE_WAIT:
 			r = amdgpu_cs_p2_syncobj_timeline_wait(p, chunk);
 			if (r)
@@ -636,6 +639,7 @@ static int amdgpu_cs_pass2(struct amdgpu_cs_parser *p)
 			if (r)
 				return r;
 			break;
+#endif
 		case AMDGPU_CHUNK_ID_CP_GFX_SHADOW:
 			r = amdgpu_cs_p2_shadow(p, chunk);
 			if (r)
@@ -1504,7 +1508,6 @@ static void amdgpu_cs_parser_fini(struct amdgpu_cs_parser *parser)
 	amdgpu_sync_free(&parser->sync);
 	drm_exec_fini(&parser->exec);
 
-#if defined(HAVE_CHUNK_ID_SYNOBJ_IN_OUT)
 	for (i = 0; i < parser->num_post_deps; i++) {
 		drm_syncobj_put(parser->post_deps[i].syncobj);
 #if defined(HAVE_CHUNK_ID_SYNCOBJ_TIMELINE_WAIT_SIGNAL)
@@ -1512,7 +1515,6 @@ static void amdgpu_cs_parser_fini(struct amdgpu_cs_parser *parser)
 #endif
 	}
 	kfree(parser->post_deps);
-#endif
 
 	dma_fence_put(parser->fence);
 
