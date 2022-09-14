2e20965cf634aaf694c595428dc0d4094c435321
Asher Song <Asher.Song@amd.com>
2022-09-14 12:24:25 +0800

    drm/amdkcl: wrap code under macro HAVE_CHUNK_ID_SYNOBJ_IN_OUT and HAVE_CHUNK_ID_SYNCOBJ_TIMELINE_WAIT_SIGNAL
    It's caused by 253c2b45dfa82a18530076bb743f67355948550c
    "drm/amdgpu: cleanup CS init/fini and pass1"
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
@@ -1479,11 +1479,15 @@ static void amdgpu_cs_parser_fini(struct amdgpu_cs_parser *parser)
 {
 	unsigned i;
 
+#if defined(HAVE_CHUNK_ID_SYNOBJ_IN_OUT)
 	for (i = 0; i < parser->num_post_deps; i++) {
 		drm_syncobj_put(parser->post_deps[i].syncobj);
+#if defined(HAVE_CHUNK_ID_SYNCOBJ_TIMELINE_WAIT_SIGNAL)
 		kfree(parser->post_deps[i].chain);
+#endif
 	}
 	kfree(parser->post_deps);
+#endif
 
 	dma_fence_put(parser->fence);
 
