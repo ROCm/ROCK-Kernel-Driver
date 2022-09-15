8a66e3b1449af6b6e5d0afc9cce0ec52de85d805
Asher Song <Asher.Song@amd.com>
2022-09-15 14:20:19 +0800

    drm/amdkcl: test whether drm_gem_object->resv is available
    It's caused by e406606fb782b811b6bfb6f51b5fc9e0c8f5d1bf
    "drm/amdgpu: SDMA update use unlocked iterator"
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vm_sdma.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vm_sdma.c
@@ -245,7 +245,7 @@ static int amdgpu_vm_sdma_update(struct amdgpu_vm_update_params *p,
 	int r;
 
 	/* Wait for PD/PT moves to be completed */
-	dma_resv_iter_begin(&cursor, bo->tbo.base.resv, DMA_RESV_USAGE_KERNEL);
+	dma_resv_iter_begin(&cursor, amdkcl_ttm_resvp(&bo->tbo), DMA_RESV_USAGE_KERNEL);
 	dma_resv_for_each_fence_unlocked(&cursor, fence) {
 		dma_fence_get(fence);
 		r = drm_sched_job_add_dependency(&p->job->base, fence);
