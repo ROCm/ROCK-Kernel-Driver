53d38ef605230a63ee48f78a4f49b740250fa5c5
Ma Jun <Jun.Ma2@amd.com>
2022-09-16 10:26:59 +0800

    drm/amdkcl: Check if drm_blend.h exist
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Change-Id: Id54a89bc4e1575d04e1a3004b158a5789da6fc4a

---
+++ include/kcl/header/drm/drm_blend.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_DRM_BLEND_H_H_
+#define _KCL_HEADER_DRM_BLEND_H_H_
+
+#ifdef HAVE_DRM_DRM_BLEND_H
+#include_next <drm/drm_blend.h>
+#endif
+
+#endif
