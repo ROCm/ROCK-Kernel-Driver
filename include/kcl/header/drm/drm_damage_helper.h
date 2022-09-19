59e104c083d409da57edb8876b991ed0138b6dfe
Asher Song <Asher.Song@amd.com>
2022-09-19 17:53:04 +0800

    drm/amdkcl: test drm/drm_damage_helper.h whether exits
    It's caused by 10b6e91bd1ee9cd237ffbc244ad9c25b5fd3e167
    "drm/amdgpu: use dirty framebuffer helper"
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/header/drm/drm_damage_helper.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_DRM_DAMAGE_HELPER_H_H_
+#define _KCL_HEADER_DRM_DAMAGE_HELPER_H_H_
+
+#if defined(HAVE_DRM_DRM_DAMAGE_HELPER_H)
+#include_next <drm/drm_damage_helper.h>
+#endif
+
+#endif
+
+
