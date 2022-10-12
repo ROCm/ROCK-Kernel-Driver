764fc021743a797ff361b2bc294248c162205eeb
Leslie Shi <Yuliang.Shi@amd.com>
2022-10-12 16:29:57 +0800

    drm/amdgpu: [hybrid] fix dgma
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Tested-by: Andy Dong <andy.dong@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -1089,6 +1089,12 @@ out:
 		memset(&cap, 0, sizeof(cap));
 		if (amdgpu_no_evict)
 			cap.flag |= AMDGPU_CAPABILITY_PIN_MEM_FLAG;
+
+		if (amdgpu_direct_gma_size) {
+			cap.flag |= AMDGPU_CAPABILITY_DIRECT_GMA_FLAG;
+			cap.direct_gma_size = amdgpu_direct_gma_size;
+		}
+
 		return copy_to_user(out, &cap,
 				    min((size_t)size, sizeof(cap))) ? -EFAULT : 0;
 	}
