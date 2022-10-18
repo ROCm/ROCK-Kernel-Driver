02495a57bbb143a40fc221b7196863b02a21359c
Leslie Shi <Yuliang.Shi@amd.com>
2022-10-18 16:25:34 +0800

    drm/amdkcl: fix uninitialized bo_dev variable
    When bo is NULL pointer, the bo_dev variable will be used uninitialized.
    
    This is caused by 292718775885 "drm/amdkcl: fix pytorch test memory page fault"
    v5.18-2828-g292718775885
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
@@ -1188,7 +1188,7 @@ int amdgpu_vm_bo_update(struct amdgpu_device *adev, struct amdgpu_bo_va *bo_va,
 	uint64_t flags;
 	bool uncached;
 	int r;
-	struct amdgpu_device *bo_adev;
+	struct amdgpu_device *bo_adev = adev;
 
 	amdgpu_sync_create(&sync);
 	if (clear) {
