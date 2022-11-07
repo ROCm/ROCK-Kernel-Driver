cbc20c06dbddc11536bf9f597c0793aace49e579
Asher Song <Asher.Song@amd.com>
2022-11-07 18:13:43 +0800

    Revert "drm/amd/display: fix array-bounds error in dc_stream_remove_writeback() [take 2]"
    This reverts commit 3f05a01a0cad65127e93751f76f9935e85a76e7d.

--- drivers/gpu/drm/amd/display/dc/core/dc_stream.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_stream.c
@@ -544,9 +544,9 @@ bool dc_stream_remove_writeback(struct dc *dc,
 	}
 
 	/* remove writeback info for disabled writeback pipes from stream */
-	for (i = 0, j = 0; i < stream->num_wb_info; i++) {
+	for (i = 0, j = 0; i < stream->num_wb_info && j < MAX_DWB_PIPES; i++) {
 		if (stream->writeback_info[i].wb_enabled) {
-			if (j < i)
+			if (i != j)
 				/* trim the array */
 				memcpy(&stream->writeback_info[j], &stream->writeback_info[i],
 						sizeof(struct dc_writeback_info));
