9c3c4a74d9e29beb06429cc083f9cb4552dceccc
Bob zhou <bobzhou2@amd.com>
2022-11-15 12:21:47 +0800

    drm/amd/display: fix compilation issue with legacy gcc
        This patch is used to fix following compilation issue with legacy gcc
    
         error: ‘for’ loop initial declarations are only allowed in C99 mode
    	for (int i = flag_vlevel; i < context->bw_ctx.dml.soc.num_states; i++) {
    
    Signed-off-by: Bob zhou <bobzhou2@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/display/dc/dml/dcn32/dcn32_fpu.c
+++ drivers/gpu/drm/amd/display/dc/dml/dcn32/dcn32_fpu.c
@@ -1761,6 +1761,7 @@ bool dcn32_internal_validate_bw(struct dc *dc,
 	if (repopulate_pipes) {
 		int flag_max_mpc_comb = vba->maxMpcComb;
 		int flag_vlevel = vlevel;
+		int i;
 
 		pipe_cnt = dc->res_pool->funcs->populate_dml_pipes(dc, context, pipes, fast_validate);
 
@@ -1779,7 +1780,7 @@ bool dcn32_internal_validate_bw(struct dc *dc,
 				flag_max_mpc_comb != context->bw_ctx.dml.vba.maxMpcComb) {
 			/* check the context constructed with pipe split flags is still valid*/
 			bool flags_valid = false;
-			for (int i = flag_vlevel; i < context->bw_ctx.dml.soc.num_states; i++) {
+			for (i = flag_vlevel; i < context->bw_ctx.dml.soc.num_states; i++) {
 				if (vba->ModeSupport[i][flag_max_mpc_comb]) {
 					vba->maxMpcComb = flag_max_mpc_comb;
 					vba->VoltageLevel = i;
