2974ba197bab6c8c6406beb4ec44124fa87649c9
Ma Jun <Jun.Ma2@amd.com>
2022-11-24 19:32:21 +0800

    drm/amdkcl: Implement the drm_plane_helper_destroy()
    Implement the drm_plane_helper_destroy() to fix the warnning
    
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Change-Id: I16a851eea6fca043fb013e1823bb52b2c84adfba

---
+++ include/kcl/kcl_drm_plane_helper.h
@@ -0,0 +1,15 @@
+#ifndef AMDKCL_DRM_PLANE_HELPER_H
+#define AMDKCL_DRM_PLANE_HELPER_H
+
+#include <drm/drm_plane_helper.h>
+
+#ifndef HAVE_DRM_PLANE_HELPER_DESTROY
+static void kcl_drm_plane_helper_destroy(struct drm_plane *plane)
+{
+	drm_plane_cleanup(plane);
+	kfree(plane);
+}
+
+#define drm_plane_helper_destroy kcl_drm_plane_helper_destroy
+#endif
+#endif
