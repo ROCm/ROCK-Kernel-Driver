2f340ec5a2102accd340f1faf6fa15d5fe81f666
Guchun Chen <guchun.chen@amd.com>
2022-11-24 13:23:09 +0800

    drm/amdgpu: use dev_dbg to print messages in runtime cycle
    Runtime PM can happen pretty frequently, as these printings
    may be annoyed, switch to dev_dbg.
    
    Suggested-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2568,7 +2568,7 @@ static int amdgpu_pmops_runtime_suspend(struct device *dev)
 		amdgpu_device_baco_enter(drm_dev);
 	}
 
-	dev_info(&pdev->dev, "asic/device is runtime suspended\n");
+	dev_dbg(&pdev->dev, "asic/device is runtime suspended\n");
 
 	return 0;
 }
