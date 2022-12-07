3265b52bf530a4f80dd34c228a357e663049024a
Ma Jun <Jun.Ma2@amd.com>
2022-12-07 15:10:47 +0800

    drm/amdkcl: Fix the type of function kcl_drm_plane_helper_destroy
    Fix the type of the function kcl_drm_plane_helper_destroy
    
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Change-Id: Ic2bdbbcd518581b3b5cd2ddd30997b8e7c9ba196

--- include/kcl/kcl_drm_plane_helper.h
+++ include/kcl/kcl_drm_plane_helper.h
@@ -4,12 +4,11 @@
 #include <drm/drm_plane_helper.h>
 
 #ifndef HAVE_DRM_PLANE_HELPER_DESTROY
-static void kcl_drm_plane_helper_destroy(struct drm_plane *plane)
+static inline void kcl_drm_plane_helper_destroy(struct drm_plane *plane)
 {
 	drm_plane_cleanup(plane);
 	kfree(plane);
 }
-
 #define drm_plane_helper_destroy kcl_drm_plane_helper_destroy
 #endif
 #endif
