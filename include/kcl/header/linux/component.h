fd88425c0489f7f5635c4f189a55c39261752a03
Ma Jun <Jun.Ma2@amd.com>
2022-12-14 09:28:50 +0800

    drm/amdkcl: Refactor code for reference of component.h
    Using include_next to optimize the reference of component.h
    
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Change-Id: I599e137e6b7a83a47d589ea29f4e39ed9c0a8886

---
+++ include/kcl/header/linux/component.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER__LINUX_COMPONENT_H_
+#define _KCL_HEADER__LINUX_COMPONENT_H_
+
+
+#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
+#include_next <linux/component.h>
+#endif
+
+#endif
