d689c79d69367ef056dead5eb9c2d48639efc32d
bobzhou2 <bobzhou2@amd.com>
2022-12-21 15:58:36 +0800

    drm/amdkcl: wrap code under macro HAVE_DRM_CONNECTOR_STATE_HDCP_CONTENT_TYPE
    It's caused by d090329b677c2f158755e886199c20b1acf93eb5
    "drm/amd/display: save restore hdcp state when display is unplugged from mst hub"
    
    Signed-off-by: bobzhou2 <bobzhou2@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -408,6 +408,7 @@ static int dm_dp_mst_get_modes(struct drm_connector *connector)
 		 * plugged back with same display index, its hdcp properties
 		 * will be retrieved from hdcp_work within dm_dp_mst_get_modes
 		 */
+#ifdef HAVE_DRM_CONNECTOR_STATE_HDCP_CONTENT_TYPE
 #ifdef CONFIG_DRM_AMD_DC_HDCP
 		if (aconnector->dc_sink && connector->state) {
 			struct drm_device *dev = connector->dev;
@@ -420,6 +421,7 @@ static int dm_dp_mst_get_modes(struct drm_connector *connector)
 			connector->state->content_protection =
 			hdcp_w->content_protection[connector->index];
 		}
+#endif
 #endif
 
 		if (aconnector->dc_sink) {
