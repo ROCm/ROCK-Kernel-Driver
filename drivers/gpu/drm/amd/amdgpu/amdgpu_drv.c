d7b4474f4405f4573c1ae067ceb4194fafe7635c
Lijo Lazar <lijo.lazar@amd.com>
2023-01-27 18:18:17 +0530

    drm/amdgpu: Add parsing of acpi xcc objects
    Add parsing of ACPI xcc objects and fill in relevant info from them by
    invoking the DSM methods.
    
    Signed-off-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-and-tested-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -3025,6 +3025,7 @@ static void __exit amdgpu_exit(void)
 	amdgpu_amdkfd_fini();
 	pci_unregister_driver(&amdgpu_kms_pci_driver);
 	amdgpu_unregister_atpx_handler();
+	amdgpu_acpi_release();
 	amdgpu_sync_fini();
 	amdgpu_fence_slab_fini();
 #ifdef HAVE_MMU_NOTIFIER_SYNCHRONIZE
