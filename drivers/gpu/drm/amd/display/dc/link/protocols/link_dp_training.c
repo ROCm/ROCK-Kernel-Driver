8a693f921f121e3626f6594ef0fda4c23b691a87
Horatio Zhang <Hongkun.Zhang@amd.com>
2023-01-30 16:52:42 +0800

    drm/amdkcl: wrap code under CONFIG_DRM_AMD_DC_DSC_SUPPORT and CONFIG_DRM_AMD_DC_DCN
    It's cause by 'commit 05c1deaaaa92
    ("drm/amd/display: move dp capability related logic to link_dp_capability")'
    'commit 9a29f20c0621
    ("drm/amd/display: move dp link training logic to link_dp_training")'
    
    Missing macro during code movement.
    link_dp_capability - missing CONFIG_DRM_AMD_DC_DCN macro
    link_dp_training - missing CONFIG_DRM_AMD_DC_DSC_SUPPORT macro when moving code from dc_link_dp.c
    link_dp_training_8b_10b - missing CONFIG_DRM_AMD_DC_DSC_SUPPORT macro when moving code from dc_link_dp.c
    link_dp_training_dpia - missing CONFIG_DRM_AMD_DC_DSC_SUPPORT macro when moving code from link_dp_dpia.c
    
    Signed-off-by: Horatio Zhang <Hongkun.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/dc/link/protocols/link_dp_training.c
+++ drivers/gpu/drm/amd/display/dc/link/protocols/link_dp_training.c
@@ -722,8 +722,11 @@ void override_training_settings(
 		lt_settings->pattern_for_eq = *overrides->pattern_for_eq;
 	if (overrides->enhanced_framing != NULL)
 		lt_settings->enhanced_framing = *overrides->enhanced_framing;
+
+#ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 	if (link->preferred_training_settings.fec_enable != NULL)
 		lt_settings->should_set_fec_ready = *link->preferred_training_settings.fec_enable;
+#endif
 
 #if defined(CONFIG_DRM_AMD_DC_DCN)
 	/* Check DP tunnel LTTPR mode debug option. */
@@ -1487,7 +1490,9 @@ enum link_training_result dp_perform_link_training(
 
 	/* configure link prior to entering training mode */
 	dpcd_configure_lttpr_mode(link, &lt_settings);
+#ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 	dp_set_fec_ready(link, link_res, lt_settings.should_set_fec_ready);
+#endif
 	dpcd_configure_channel_coding(link, &lt_settings);
 
 	/* enter training mode:
