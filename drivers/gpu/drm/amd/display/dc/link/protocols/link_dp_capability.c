5ad12d6a045c910bfccfacf6f4ec908b41d40f2b
Horatio Zhang <Hongkun.Zhang@amd.com>
2023-01-30 16:52:42 +0800

    drm/amdkcl: wrap code under CONFIG_DRM_AMD_DC_DSC_SUPPORT and CONFIG_DRM_AMD_DC_DCN
    It's cause by 'commit 05c1deaaaa92
    ("drm/amd/display: move dp capability related logic to link_dp_capability")'
    'commit 9a29f20c0621
    ("drm/amd/display: move dp link training logic to link_dp_training")'
    
    Missing macro during code movement.
    link_dp_capability - missing CONFIG_DRM_AMD_DC_DCN macro
    link_dp_training - missing CONFIG_DRM_AMD_DC_DSC_SUPPORT macro when moving code from dc_link_dp.c
    link_dp_training_8b_10b - missing CONFIG_DRM_AMD_DC_DSC_SUPPORT macro when moving code from dc_link_dp.c
    link_dp_training_dpia - missing CONFIG_DRM_AMD_DC_DSC_SUPPORT macro when moving code from link_dp_dpia.c
    
    Signed-off-by: Horatio Zhang <Hongkun.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/dc/link/protocols/link_dp_capability.c
+++ drivers/gpu/drm/amd/display/dc/link/protocols/link_dp_capability.c
@@ -158,6 +158,7 @@ uint8_t dp_parse_lttpr_repeater_count(uint8_t lttpr_repeater_count)
 	return 0; // invalid value
 }
 
+#if defined(CONFIG_DRM_AMD_DC_DCN)
 uint32_t link_bw_kbps_from_raw_frl_link_rate_data(uint8_t bw)
 {
 	switch (bw) {
@@ -177,6 +178,7 @@ uint32_t link_bw_kbps_from_raw_frl_link_rate_data(uint8_t bw)
 
 	return 0;
 }
+#endif
 
 static enum dc_link_rate linkRateInKHzToLinkRateMultiplier(uint32_t link_rate_in_khz)
 {
@@ -241,6 +243,7 @@ static union dp_cable_id intersect_cable_id(
 	return out;
 }
 
+#if defined(CONFIG_DRM_AMD_DC_DCN)
 /*
  * Return PCON's post FRL link training supported BW if its non-zero, otherwise return max_supported_frl_bw.
  */
@@ -268,6 +271,7 @@ static uint32_t intersect_frl_link_bw_support(
 
 	return supported_bw_in_kbps;
 }
+#endif
 
 static enum clock_source_id get_clock_source_id(struct dc_link *link)
 {
@@ -1159,6 +1163,7 @@ static void get_active_converter_info(
 						translate_dpcd_max_bpc(
 							hdmi_color_caps.bits.MAX_BITS_PER_COLOR_COMPONENT);
 
+#if defined(CONFIG_DRM_AMD_DC_DCN)
 					if (link->dc->caps.dp_hdmi21_pcon_support) {
 						union hdmi_encoded_link_bw hdmi_encoded_link_bw;
 
@@ -1179,6 +1184,7 @@ static void get_active_converter_info(
 						if (link->dpcd_caps.dongle_caps.dp_hdmi_frl_max_link_bw_in_kbps > 0)
 							link->dpcd_caps.dongle_caps.extendedCapValid = true;
 					}
+#endif
 
 					if (link->dpcd_caps.dongle_caps.dp_hdmi_max_pixel_clk_in_khz != 0)
 						link->dpcd_caps.dongle_caps.extendedCapValid = true;
