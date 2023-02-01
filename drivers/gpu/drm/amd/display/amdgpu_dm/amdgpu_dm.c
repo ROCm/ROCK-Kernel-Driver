57ac4f66adc35f238381a9d201c96eaa15f38987
Bob Zhou <bob.zhou@amd.com>
2023-02-01 16:19:14 +0800

    drm/amdkcl: Implement the drm_kms_helper_connector_hotplug_event()
    Implement the drm_kms_helper_connector_hotplug_event() for legacy os
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -1401,11 +1401,7 @@ static void force_connector_state(
 	mutex_unlock(&connector->dev->mode_config.mutex);
 
 	mutex_lock(&aconnector->hpd_lock);
-#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
         drm_kms_helper_connector_hotplug_event(connector);
-#else
-        drm_kms_helper_hotplug_event(connector->dev);
-#endif
 	mutex_unlock(&aconnector->hpd_lock);
 }
 
@@ -3502,11 +3498,7 @@ static void handle_hpd_irq_helper(struct amdgpu_dm_connector *aconnector)
 		drm_modeset_unlock_all(dev);
 
 		if (aconnector->base.force == DRM_FORCE_UNSPECIFIED)
-#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
 			drm_kms_helper_connector_hotplug_event(connector);
-#else
-			drm_kms_helper_hotplug_event(dev);
-#endif
 	} else {
 		mutex_lock(&adev->dm.dc_lock);
 		ret = dc_link_detect(aconnector->dc_link, DETECT_REASON_HPD);
@@ -3519,11 +3511,7 @@ static void handle_hpd_irq_helper(struct amdgpu_dm_connector *aconnector)
 			drm_modeset_unlock_all(dev);
 
 			if (aconnector->base.force == DRM_FORCE_UNSPECIFIED)
-#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
 				drm_kms_helper_connector_hotplug_event(connector);
-#else
-				drm_kms_helper_hotplug_event(dev);
-#endif
 		}
 	}
 	mutex_unlock(&aconnector->hpd_lock);
@@ -3656,11 +3644,7 @@ out:
 			dm_restore_drm_connector_state(dev, connector);
 			drm_modeset_unlock_all(dev);
 
-#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
 			drm_kms_helper_connector_hotplug_event(connector);
-#else
-			drm_kms_helper_hotplug_event(dev);
-#endif
 		} else {
 			bool ret = false;
 
@@ -3678,11 +3662,7 @@ out:
 				dm_restore_drm_connector_state(dev, connector);
 				drm_modeset_unlock_all(dev);
 
-#ifdef HAVE_DRM_KMS_HELPER_CONNECTOR_HOTPLUG_EVENT
 				drm_kms_helper_connector_hotplug_event(connector);
-#else
-				drm_kms_helper_hotplug_event(dev);
-#endif
 			}
 		}
 	}
