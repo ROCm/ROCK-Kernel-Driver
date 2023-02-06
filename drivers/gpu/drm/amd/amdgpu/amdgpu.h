2fdaf11e8b7278ee7bd422e365076cf4eed4a1e5
Graham Sider <Graham.Sider@amd.com>
2023-02-06 14:04:42 -0500

    drm/amdgpu/bu: Add use_mtype_cc_wa module param
    By default, set use_mtype_cc_wa to 1 to set PTE coherence flag MTYPE_CC
    instead of MTYPE_RW by default. This is required for the time being to
    mitigate a bug causing XCCs to hit stale data due to TCC marking fully
    dirty lines as exclusive.
    
    Signed-off-by: Graham Sider <Graham.Sider@amd.com>
    Reviewed-by: Joseph Greathouse <Joseph.Greathouse@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -216,6 +216,7 @@ extern int amdgpu_force_asic_type;
 extern int amdgpu_smartshift_bias;
 extern int amdgpu_use_xgmi_p2p;
 extern bool pcie_p2p;
+extern bool amdgpu_use_mtype_cc_wa;
 #ifdef CONFIG_HSA_AMD
 extern int sched_policy;
 extern bool debug_evictions;
