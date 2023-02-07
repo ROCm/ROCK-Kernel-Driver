37141fe281e9ecab04ff43f9cf52d5dd5550297d
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-07 16:59:27 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_CONNECTOR_FUNCS_REGISTER
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <Flora.Cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c
@@ -812,7 +812,6 @@ amdgpu_connector_lvds_detect(struct drm_connector *connector, bool force)
 	return ret;
 }
 
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 static void amdgpu_connector_unregister(struct drm_connector *connector)
 {
 	struct amdgpu_connector *amdgpu_connector = to_amdgpu_connector(connector);
@@ -822,7 +821,6 @@ static void amdgpu_connector_unregister(struct drm_connector *connector)
 		amdgpu_connector->ddc_bus->has_aux = false;
 	}
 }
-#endif
 
 static void amdgpu_connector_destroy(struct drm_connector *connector)
 {
@@ -881,9 +879,7 @@ static const struct drm_connector_funcs amdgpu_connector_lvds_funcs = {
 	.dpms = drm_helper_connector_dpms,
 	.detect = amdgpu_connector_lvds_detect,
 	.fill_modes = drm_helper_probe_single_connector_modes,
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 	.early_unregister = amdgpu_connector_unregister,
-#endif
 	.destroy = amdgpu_connector_destroy,
 	.set_property = amdgpu_connector_set_lcd_property,
 };
@@ -1001,9 +997,7 @@ static const struct drm_connector_funcs amdgpu_connector_vga_funcs = {
 	.dpms = drm_helper_connector_dpms,
 	.detect = amdgpu_connector_vga_detect,
 	.fill_modes = drm_helper_probe_single_connector_modes,
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 	.early_unregister = amdgpu_connector_unregister,
-#endif
 	.destroy = amdgpu_connector_destroy,
 	.set_property = amdgpu_connector_set_property,
 };
@@ -1320,9 +1314,7 @@ static const struct drm_connector_funcs amdgpu_connector_dvi_funcs = {
 	.detect = amdgpu_connector_dvi_detect,
 	.fill_modes = drm_helper_probe_single_connector_modes,
 	.set_property = amdgpu_connector_set_property,
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 	.early_unregister = amdgpu_connector_unregister,
-#endif
 	.destroy = amdgpu_connector_destroy,
 	.force = amdgpu_connector_dvi_force,
 };
@@ -1617,7 +1609,6 @@ static enum drm_mode_status amdgpu_connector_dp_mode_valid(struct drm_connector 
 	return MODE_OK;
 }
 
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 static int
 amdgpu_connector_late_register(struct drm_connector *connector)
 {
@@ -1631,7 +1622,6 @@ amdgpu_connector_late_register(struct drm_connector *connector)
 
 	return r;
 }
-#endif
 
 static const struct drm_connector_helper_funcs amdgpu_connector_dp_helper_funcs = {
 	.get_modes = amdgpu_connector_dp_get_modes,
@@ -1644,14 +1634,10 @@ static const struct drm_connector_funcs amdgpu_connector_dp_funcs = {
 	.detect = amdgpu_connector_dp_detect,
 	.fill_modes = drm_helper_probe_single_connector_modes,
 	.set_property = amdgpu_connector_set_property,
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 	.early_unregister = amdgpu_connector_unregister,
-#endif
 	.destroy = amdgpu_connector_destroy,
 	.force = amdgpu_connector_dvi_force,
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 	.late_register = amdgpu_connector_late_register,
-#endif
 };
 
 static const struct drm_connector_funcs amdgpu_connector_edp_funcs = {
@@ -1659,14 +1645,10 @@ static const struct drm_connector_funcs amdgpu_connector_edp_funcs = {
 	.detect = amdgpu_connector_dp_detect,
 	.fill_modes = drm_helper_probe_single_connector_modes,
 	.set_property = amdgpu_connector_set_lcd_property,
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 	.early_unregister = amdgpu_connector_unregister,
-#endif
 	.destroy = amdgpu_connector_destroy,
 	.force = amdgpu_connector_dvi_force,
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 	.late_register = amdgpu_connector_late_register,
-#endif
 };
 
 void
--- drivers/gpu/drm/amd/amdgpu/atombios_dp.c
+++ drivers/gpu/drm/amd/amdgpu/atombios_dp.c
@@ -194,9 +194,7 @@ void amdgpu_atombios_dp_aux_init(struct amdgpu_connector *amdgpu_connector)
 	amdgpu_connector->ddc_bus->aux.drm_dev = amdgpu_connector->base.dev;
 #endif
 
-#if defined(HAVE_DRM_CONNECTOR_FUNCS_REGISTER)
 	drm_dp_aux_init(&amdgpu_connector->ddc_bus->aux);
-#endif
 	amdgpu_connector->ddc_bus->has_aux = true;
 }
 
