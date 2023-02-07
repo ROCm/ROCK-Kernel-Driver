3ececfdc23850b4ace6a16ea5729ce21cbe83bf8
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-07 16:21:27 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_ATOMIC_STATE_PUT
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <Flora.Cui@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -68,7 +68,6 @@
 #include <kcl/backport/kcl_drm_dp_helper_backport.h>
 #include <kcl/backport/kcl_drm_dp_mst_helper_backport.h>
 #include <kcl/backport/kcl_drm_atomic_helper_backport.h>
-#include <kcl/kcl_drm_atomic.h>
 #include <kcl/kcl_drm_hdcp.h>
 #include <kcl/kcl_moduleparam.h>
 #include <kcl/kcl_drm_prime.h>
