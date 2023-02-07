8c01e3055ca5fda681100edd78d0f1b683104de7
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-07 14:28:35 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_ATOMIC_STATE_ASYNC_UPDATE
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <Flora.Cui@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -10788,7 +10788,6 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 		goto fail;
 #endif
 
-#if defined(HAVE_DRM_ATOMIC_STATE_ASYNC_UPDATE)
 	if (state->legacy_cursor_update) {
 		/*
 		 * This is a fast cursor update coming from the plane update
@@ -10808,7 +10807,6 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 		if (state->async_update)
 			return 0;
 	}
-#endif
 
 	/* Check scaling and underscan changes*/
 	/* TODO Removed scaling changes validation due to inability to commit
