4412e4969695794bc59eb18b99e9988a2c623112
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-07 12:30:02 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_ATOMIC_NONBLOCKING_COMMIT
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -172,12 +172,6 @@
 /* drm_atomic_helper_check_plane_state() is available */
 #define HAVE_DRM_ATOMIC_HELPER_CHECK_PLANE_STATE 1
 
-/* drm_atomic_helper_shutdown() is available */
-#define HAVE_DRM_ATOMIC_HELPER_SHUTDOWN 1
-
-/* drm_atomic_nonblocking_commit() is available */
-#define HAVE_DRM_ATOMIC_NONBLOCKING_COMMIT 1
-
 /* drm_atomic_plane_disabling() wants drm_plane_state * arg */
 #define HAVE_DRM_ATOMIC_PLANE_DISABLING_DRM_PLANE_STATE 1
 
