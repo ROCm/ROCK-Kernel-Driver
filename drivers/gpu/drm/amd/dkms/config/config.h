d0e401199c4abcd6976725c0833bbbbdabbf8e4a
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-08 14:25:20 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_CRTC_ENABLE_COLOR_MGMT
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -207,9 +207,6 @@
 /* struct drm_connector_state has hdr_output_metadata member */
 #define HAVE_DRM_CONNECTOR_STATE_HDR_OUTPUT_METADATA 1
 
-/* drm_crtc_enable_color_mgmt() is available */
-#define HAVE_DRM_CRTC_ENABLE_COLOR_MGMT 1
-
 /* drm_crtc_from_index() is available */
 #define HAVE_DRM_CRTC_FROM_INDEX 1
 
