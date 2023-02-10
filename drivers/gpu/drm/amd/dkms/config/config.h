553c19b09619d2dc1c929499c2eb54afac42ce92
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-10 15:13:25 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_DRM_DAMAGE_HELPER_H
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -395,9 +395,6 @@
 /* Define to 1 if you have the <drm/drm_backport.h> header file. */
 /* #undef HAVE_DRM_DRM_BACKPORT_H */
 
-/* Define to 1 if you have the <drm/drm_damage_helper.h> header file. */
-#define HAVE_DRM_DRM_DAMAGE_HELPER_H 1
-
 /* Define to 1 if you have the <drm/drm_debugfs.h> header file. */
 #define HAVE_DRM_DRM_DEBUGFS_H 1
 
