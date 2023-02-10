8164a94f69c299c45aac571c24e60250fb4d6c1b
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-10 15:04:48 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_DRM_AUDIO_COMPONENT_H
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -942,7 +942,6 @@ static void amdgpu_dm_fbc_init(struct drm_connector *connector)
 
 }
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 static int amdgpu_dm_audio_component_get_eld(struct device *kdev, int port,
 					  int pipe, bool *enabled,
 					  unsigned char *buf, int max_bytes)
@@ -1075,7 +1074,6 @@ static  void amdgpu_dm_audio_eld_notify(struct amdgpu_device *adev, int pin)
 						 pin, -1);
 	}
 }
-#endif
 
 static int dm_dmub_hw_init(struct amdgpu_device *adev)
 {
@@ -1571,9 +1569,7 @@ static int amdgpu_dm_init(struct amdgpu_device *adev)
 	mutex_init(&adev->dm.dpia_aux_lock);
 	mutex_init(&adev->dm.dc_lock);
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 	mutex_init(&adev->dm.audio_lock);
-#endif
 
 	if(amdgpu_dm_irq_init(adev)) {
 		DRM_ERROR("amdgpu: failed to initialize DM IRQ support.\n");
@@ -1883,9 +1879,7 @@ static int amdgpu_dm_early_fini(void *handle)
 {
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 	amdgpu_dm_audio_fini(adev);
-#endif
 
 	return 0;
 }
@@ -1965,9 +1959,7 @@ static void amdgpu_dm_fini(struct amdgpu_device *adev)
 		adev->dm.freesync_module = NULL;
 	}
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 	mutex_destroy(&adev->dm.audio_lock);
-#endif
 	mutex_destroy(&adev->dm.dc_lock);
 	mutex_destroy(&adev->dm.dpia_aux_lock);
 
@@ -4068,14 +4060,12 @@ static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 		return r;
 	}
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 	r = amdgpu_dm_audio_init(adev);
 	if (r) {
 		dc_release_state(state->context);
 		kfree(state);
 		return r;
 	}
-#endif
 
 	return 0;
 }
@@ -7570,9 +7560,7 @@ void amdgpu_dm_connector_init_helper(struct amdgpu_display_manager *dm,
 	aconnector->base.stereo_allowed = false;
 	aconnector->base.dpms = DRM_MODE_DPMS_OFF;
 	aconnector->hpd.hpd = AMDGPU_HPD_NONE; /* not used */
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 	aconnector->audio_inst = -1;
-#endif
 	aconnector->pack_sdp_v1_3 = false;
 	aconnector->as_type = ADAPTIVE_SYNC_TYPE_NONE;
 	memset(&aconnector->vsdb_info, 0, sizeof(aconnector->vsdb_info));
@@ -8680,7 +8668,6 @@ cleanup:
 	kfree(bundle);
 }
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 static void amdgpu_dm_commit_audio(struct drm_device *dev,
 				   struct drm_atomic_state *state)
 {
@@ -8764,7 +8751,6 @@ static void amdgpu_dm_commit_audio(struct drm_device *dev,
 		amdgpu_dm_audio_eld_notify(adev, inst);
 	}
 }
-#endif
 
 /*
  * amdgpu_dm_crtc_copy_transient_flags - copy mirrored flags from DRM to DC
@@ -9353,10 +9339,8 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 						dm, crtc, wait_for_vblank);
 	}
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 	/* Update audio instances for each connector. */
 	amdgpu_dm_commit_audio(dev, state);
-#endif
 
 	/* restore the backlight level */
 	for (i = 0; i < dm->num_of_edps; i++) {
--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -386,7 +386,6 @@ struct amdgpu_display_manager {
 	 */
 	struct mutex dc_lock;
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 	/**
 	 * @audio_lock:
 	 *
@@ -408,7 +407,6 @@ struct amdgpu_display_manager {
 	 * successfully, false otherwise.
 	 */
 	bool audio_registered;
-#endif
 
 	/**
 	 * @vblank_lock:
@@ -693,10 +691,8 @@ struct amdgpu_dm_connector {
 	int max_vfreq ;
 	int pixel_clock_mhz;
 
-#if defined(HAVE_DRM_DRM_AUDIO_COMPONENT_H)
 	/* Audio instance - protected by audio_lock. */
 	int audio_inst;
-#endif
 
 	struct mutex hpd_lock;
 
