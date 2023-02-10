54a4f62ee185f356e2eae17bbd3269fc6e4fa9e4
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-10 10:18:06 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_DEV_UNPLUG
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <Flora.Cui@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -214,9 +214,6 @@
 /* struct drm_device has pdev member */
 /* #undef HAVE_DRM_DEVICE_PDEV */
 
-/* drm_dev_unplug() is available */
-#define HAVE_DRM_DEV_UNPLUG 1
-
 /* Define to 1 if you have the <drm/display/drm_dp.h> header file. */
 #define HAVE_DRM_DISPLAY_DRM_DP_H 1
 
