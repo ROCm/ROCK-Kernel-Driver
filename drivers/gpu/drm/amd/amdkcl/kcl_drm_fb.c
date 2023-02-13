d50d2c3f11fe64277902da9c47525243485ba9ca
Ma Jun <Jun.Ma2@amd.com>
2023-02-13 09:40:57 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_FB_HELPER_SET_SUSPEND_UNLOCKED
    Change-Id: I47eeadd6c7a9fd91ccaff1d8d7077a6961b543ee
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_drm_fb.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_drm_fb.c
@@ -57,24 +57,3 @@ void drm_fb_helper_fill_info(struct fb_info *info,
 }
 EXPORT_SYMBOL(drm_fb_helper_fill_info);
 #endif
-
-#ifndef HAVE_DRM_FB_HELPER_SET_SUSPEND_UNLOCKED
-/**
- * Copied from drivers/gpu/drm/drm_fb_helper.c and modified for KCL.
- * _kcl_drm_fb_helper_set_suspend_stub - wrapper around fb_set_suspend
- * @fb_helper: driver-allocated fbdev helper
- * @state: desired state, zero to resume, non-zero to suspend
- *
- * A wrapper around fb_set_suspend implemented by fbdev core
- */
-void _kcl_drm_fb_helper_set_suspend_unlocked(struct drm_fb_helper *fb_helper, int state)
-{
-	if (!fb_helper || !fb_helper->fbdev)
-		return;
-
-	console_lock();
-	fb_set_suspend(fb_helper->fbdev, state);
-	console_unlock();
-}
-EXPORT_SYMBOL(_kcl_drm_fb_helper_set_suspend_unlocked);
-#endif
