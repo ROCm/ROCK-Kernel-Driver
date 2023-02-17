4c9dd150c2ee09b52fbb1bdb6090e1df8c265272
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-17 12:44:59 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_FRAMEBUFFER_FORMAT
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -839,11 +839,7 @@ int amdgpu_dm_plane_fill_plane_buffer_attributes(struct amdgpu_device *adev,
 		plane_size->surface_size.width = fb->width;
 		plane_size->surface_size.height = fb->height;
 		plane_size->surface_pitch =
-#ifndef HAVE_DRM_FRAMEBUFFER_FORMAT
-			fb->pitches[0] / (fb->bits_per_pixel / 8);
-#else
 			fb->pitches[0] / fb->format->cpp[0];
-#endif
 
 		address->type = PLN_ADDR_TYPE_GRAPHICS;
 		address->grph.addr.low_part = lower_32_bits(addr);
@@ -857,11 +853,7 @@ int amdgpu_dm_plane_fill_plane_buffer_attributes(struct amdgpu_device *adev,
 		plane_size->surface_size.width = fb->width;
 		plane_size->surface_size.height = fb->height;
 		plane_size->surface_pitch =
-#ifndef HAVE_DRM_FRAMEBUFFER_FORMAT
-			fb->pitches[0] / (fb->bits_per_pixel / 8);
-#else
 			fb->pitches[0] / fb->format->cpp[0];
-#endif
 
 		plane_size->chroma_size.x = 0;
 		plane_size->chroma_size.y = 0;
@@ -870,11 +862,7 @@ int amdgpu_dm_plane_fill_plane_buffer_attributes(struct amdgpu_device *adev,
 		plane_size->chroma_size.height = fb->height / 2;
 
 		plane_size->chroma_pitch =
-#ifndef HAVE_DRM_FRAMEBUFFER_FORMAT
-			fb->pitches[1] / (fb->bits_per_pixel / 8)/2;
-#else
 			fb->pitches[1] / fb->format->cpp[1];
-#endif
 
 		address->type = PLN_ADDR_TYPE_VIDEO_PROGRESSIVE;
 		address->video_progressive.luma_addr.low_part =
@@ -1061,11 +1049,7 @@ static void get_min_max_dc_plane_scaling(struct drm_device *dev,
 	/* Caps for all supported planes are the same on DCE and DCN 1 - 3 */
 	struct dc_plane_cap *plane_cap = &dc->caps.planes[0];
 
-#ifndef HAVE_DRM_FRAMEBUFFER_FORMAT
-	switch (fb->pixel_format) {
-#else
 	switch (fb->format->format) {
-#endif
 	case DRM_FORMAT_P010:
 	case DRM_FORMAT_NV12:
 	case DRM_FORMAT_NV21:
@@ -1178,12 +1162,7 @@ int amdgpu_dm_plane_fill_dc_scaling_info(struct amdgpu_device *adev,
 	 */
 	if (((adev->ip_versions[DCE_HWIP][0] == IP_VERSION(1, 0, 0)) ||
 		(adev->ip_versions[DCE_HWIP][0] == IP_VERSION(1, 0, 1))) &&
-        (state->fb &&
-#ifndef HAVE_DRM_FRAMEBUFFER_FORMAT
-        state->fb->pixel_format == DRM_FORMAT_NV12 &&
-#else
-        state->fb->format->format == DRM_FORMAT_NV12 &&
-#endif
+        	(state->fb && state->fb->format->format == DRM_FORMAT_NV12 &&
 		(scaling_info->src_rect.x != 0 || scaling_info->src_rect.y != 0)))
 		return -EINVAL;
 
@@ -1398,11 +1377,7 @@ void amdgpu_dm_plane_handle_cursor_update(struct drm_plane *plane,
 	attributes.rotation_angle    = 0;
 	attributes.attribute_flags.value = 0;
 
-#ifdef HAVE_DRM_FRAMEBUFFER_FORMAT
 	attributes.pitch = afb->base.pitches[0] / afb->base.format->cpp[0];
-#else
-       attributes.pitch = afb->base.pitches[0] / (afb->base.bits_per_pixel / 8);
-#endif
 
 	if (crtc_state->stream) {
 		mutex_lock(&adev->dm.dc_lock);
