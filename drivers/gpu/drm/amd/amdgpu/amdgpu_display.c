ed1ff476638c9b98c5f3fefffc809530c8de9558
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-17 11:49:57 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_FB_HELPER_BUFFER
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -1684,7 +1684,6 @@ bool amdgpu_crtc_get_scanout_position(struct drm_crtc *crtc,
 static bool
 amdgpu_display_robj_is_fb(struct amdgpu_device *adev, struct amdgpu_bo *robj)
 {
-#ifdef HAVE_DRM_FB_HELPER_BUFFER
 	struct drm_device *dev = adev_to_drm(adev);
 	struct drm_fb_helper *fb_helper = dev->fb_helper;
 
@@ -1695,9 +1694,6 @@ amdgpu_display_robj_is_fb(struct amdgpu_device *adev, struct amdgpu_bo *robj)
 		return false;
 
 	return true;
-#else
-	return false;
-#endif
 }
 #endif
 
