2ae210b65746d24a9ef3fa5c96019b6eb7074392
Ma Jun <Jun.Ma2@amd.com>
2023-02-17 14:10:18 +0800

    drm/amdkcl: kcl-cleanup HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP
    Change-Id: I6e00c1b00b4640cbc0cf29649845d736a00e572c
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.shi@amd.com>

--- include/kcl/kcl_pci.h
+++ include/kcl/kcl_pci.h
@@ -97,27 +97,14 @@
 	0)
 #endif
 
-#if !defined(HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP)
-extern enum pci_bus_speed (*_kcl_pcie_get_speed_cap)(struct pci_dev *dev);
-extern enum pcie_link_width (*_kcl_pcie_get_width_cap)(struct pci_dev *dev);
-#endif
-
 static inline enum pci_bus_speed kcl_pcie_get_speed_cap(struct pci_dev *dev)
 {
-#if defined(HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP)
 	return pcie_get_speed_cap(dev);
-#else
-	return _kcl_pcie_get_speed_cap(dev);
-#endif
 }
 
 static inline enum pcie_link_width kcl_pcie_get_width_cap(struct pci_dev *dev)
 {
-#if defined(HAVE_PCIE_GET_SPEED_AND_WIDTH_CAP)
 	return pcie_get_width_cap(dev);
-#else
-	return _kcl_pcie_get_width_cap(dev);
-#endif
 }
 
 #if !defined(HAVE_PCIE_ENABLE_ATOMIC_OPS_TO_ROOT)
