c18dedc4164d269757b293eba0c1b44dd5dd5eda
Asher Song <Asher.Song@amd.com>
2023-02-18 15:24:08 +0800

    Revert "drm/amdkcl: wrap the code under HAVE_STRUCT_XARRAY for legacy os"
    This reverts commit 872cfa5be97a8f04717741d37ef06ba7c9cec38a.
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- include/kcl/header/linux/xarray.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0+ */
-#ifndef _KCL_HEADER_LINUX_XARRAY_H_H
-#define _KCL_HEADER_LINUX_XARRAY_H_H
-
-#ifdef HAVE_LINUX_XARRAY_H
-#include_next <linux/xarray.h>
-#endif
-
-#endif
