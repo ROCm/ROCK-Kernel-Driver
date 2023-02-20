d549011215fdaf2201a3bbfd0220474480cb7864
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-20 16:16:42 +0800

    drm/amdkcl: kcl-cleanup HAVE_KREF_READ
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <Flora.Cui@amd.com>

--- drivers/gpu/drm/scheduler/backport/backport.h
+++ drivers/gpu/drm/scheduler/backport/backport.h
@@ -8,6 +8,5 @@
 #include <kcl/backport/kcl_fence_backport.h>
 #include <kcl/backport/kcl_drm_print.h>
 #include <kcl/kcl_sched.h>
-#include <kcl/kcl_kref.h>
 
 #endif
--- drivers/gpu/drm/ttm/backport/backport.h
+++ drivers/gpu/drm/ttm/backport/backport.h
@@ -3,7 +3,6 @@
 #define AMDTTM_BACKPORT_H
 
 #include <linux/version.h>
-#include <kcl/kcl_kref.h>
 #include <kcl/kcl_kernel.h>
 #include <kcl/kcl_compiler_attributes.h>
 #include <kcl/kcl_dma_mapping.h>
