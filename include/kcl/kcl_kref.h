4c88035d19fe149776413cefe77ce0d6bedc0761
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-20 16:16:42 +0800

    drm/amdkcl: kcl-cleanup HAVE_KREF_READ
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <Flora.Cui@amd.com>

--- include/kcl/kcl_kref.h
+++ include/kcl/kcl_kref.h
@@ -15,11 +15,4 @@
 #include <linux/kref.h>
 
 /* Copied from include/linux/kref.h */
-#if !defined(HAVE_KREF_READ)
-static inline unsigned int kref_read(const struct kref *kref)
-{
-	return atomic_read(&kref->refcount);
-}
-#endif
-
 #endif /* AMDKCL_KREF_H */
