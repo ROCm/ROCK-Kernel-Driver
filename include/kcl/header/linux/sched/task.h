a07c3eaae34919871641f1072785d196fd302d1b
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-21 10:24:28 +0800

    drm/amdkcl: kcl-cleanup HAVE_LINUX_SCHED_TASK_H
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <Flora.Cui@amd.com>

--- include/kcl/header/linux/sched/task.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: MIT */
-#ifndef _KCL_HEADER_LINUX_SCHED_TASK_H_H_
-#define _KCL_HEADER_LINUX_SCHED_TASK_H_H_
-
-#if defined(HAVE_LINUX_SCHED_TASK_H)
-#include_next <linux/sched/task.h>
-#endif
-
-#endif
