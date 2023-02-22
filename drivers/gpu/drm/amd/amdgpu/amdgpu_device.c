39cc8ec02b6f2cc65f22eef0b77cd415a3b042b7
Asher Song <Asher.Song@amd.com>
2023-02-22 12:22:31 +0800

    drm/amdkcl: wrap the code under macro HAVE_DRM_APERTURE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_DRM_DRIVER_ARG
    It's caused by b7cdb41e7d25ceb4f8c1de7343517b29b58e357b
    drm/amd: Delay removal of the firmware framebuffer
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4175,7 +4175,11 @@ int amdgpu_device_init(struct amdgpu_device *adev,
 		return r;
 
 	/* Get rid of things like offb */
+#ifdef HAVE_DRM_APERTURE_REMOVE_CONFLICTING_PCI_FRAMEBUFFERS_DRM_DRIVER_ARG
 	r = drm_aperture_remove_conflicting_pci_framebuffers(adev->pdev, &amdgpu_kms_driver);
+#else
+	r = drm_aperture_remove_conflicting_pci_framebuffers(pdev, "amdgpudrmfb");
+#endif
 	if (r)
 		return r;
 
