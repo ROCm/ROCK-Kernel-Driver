5504fc142f32bd93c2aa689e4b8263a71192a704
Ma Jun <Jun.Ma2@amd.com>
2023-03-06 17:16:56 +0800

    drm/amdkcl: Using include_next to reference the drm_gem_atomic_helper.h
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/header/drm/drm_gem_atomic_helper.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_DRM_GEM_ATOMIC_HELPER_PREPARE_H_H_
+#define _KCL_HEADER_DRM_GEM_ATOMIC_HELPER_PREPARE_H_H_
+
+#if defined(HAVE_DRM_GEM_PLANE_HELPER_PREPARE_FB)
+#include_next <drm/drm_gem_atomic_helper.h>
+#endif
+
+#endif
