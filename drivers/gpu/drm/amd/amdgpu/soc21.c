41db444540ed0d5695b9fff5d9c7eb0839d4da21
Veerabadhran Gopalakrishnan <veerabadhran.gopalakrishnan@amd.com>
2023-03-08 19:33:53 +0530

    drm/amdgpu/soc21: Add video cap query support for VCN_4_0_4
    Added the video capability query support for VCN version 4_0_4
    
    Signed-off-by: Veerabadhran Gopalakrishnan <veerabadhran.gopalakrishnan@amd.com>
    Reviewed-by: Leo Liu <leo.liu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/soc21.c
+++ drivers/gpu/drm/amd/amdgpu/soc21.c
@@ -82,6 +82,7 @@ static int soc21_query_video_codecs(struct amdgpu_device *adev, bool encode,
 
 	case IP_VERSION(4, 0, 0):
 	case IP_VERSION(4, 0, 2):
+	case IP_VERSION(4, 0, 4):
 		if (encode)
 			*codecs = &vcn_4_0_0_video_codecs_encode;
 		else
