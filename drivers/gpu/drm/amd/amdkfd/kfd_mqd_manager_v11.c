9143b4e078c4320645da16d64981cc3df28297ea
Sreekant Somasekharan <sreekant.somasekharan@amd.com>
2023-03-13 18:05:41 -0400

    drm/amdkfd: Check PCIe atomics support on GFX11 to set CP_HQD_HQ_STATUS0[29]
    CP_HQD_HQ_STATUS0[29] bit will be used by CPFW to acknowledge whether
    PCIe atomics are supported. The default value of this bit is set
    to 0. Driver will check whether PCIe atomics are supported and set the
    bit to 1 if supported. This will force CPFW to use real atomic ops.
    If the bit is not set, CPFW will default to read/modify/write using the
    firmware itself.
    
    This is applicable only to GFX11 RS64 CP with MEC FW >= 509. If MEC
    FW < 509 and for all GFX11 F32 CP, PCIe atomics needs to be supported
    else it will skip the device.
    
    This commit also involves moving amdgpu_amdkfd_device_probe() function
    call after per-IP early_init loop in amdgpu_device_ip_early_init()
    function so as to check for RS64 enabled device.
    
    Change-Id: Ibc88d229d968a645035c3908a99d96bf70fcba1b
    Signed-off-by: Sreekant Somasekharan <sreekant.somasekharan@amd.com>
    Reviewed-by: Graham Sider <Graham.Sider@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_mqd_manager_v11.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_mqd_manager_v11.c
@@ -167,6 +167,12 @@ static void init_mqd(struct mqd_manager *mm, void **mqd,
 	 * DISPATCH_PTR.  This is required for the kfd debugger
 	 */
 	m->cp_hqd_hq_status0 = 1 << 14;
+	/*
+	 * GFX11 RS64 CPFW version >= 509 supports PCIe atomics support
+	 * acknowledgment.
+	 */
+	if (amdgpu_amdkfd_have_atomics_support(mm->dev->adev))
+		m->cp_hqd_hq_status0 |= 1 << 29;
 
 	if (q->format == KFD_QUEUE_FORMAT_AQL) {
 		m->cp_hqd_aql_control =
