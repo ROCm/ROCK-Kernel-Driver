42e0bed8e69f6bf54e48ec80a7d0ac2456b56255
Felix Kuehling <Felix.Kuehling@amd.com>
2023-03-29 19:19:38 -0400

    drm/amdkfd: Fix HMM migrations on monolithic builds
    Use the new zone_device_page_init KCL helper, which encapsulates all the
    logic around locking and reference counting device pages. This makes the
    DKMS branch code in svm_migrate_get_vram_page look the same as upstream.
    
    Cc: Flora Cui <Flora.Cui@amd.com>
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-and-tested-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_migrate.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_migrate.c
@@ -226,10 +226,7 @@ svm_migrate_get_vram_page(struct svm_range *prange, unsigned long pfn)
 	page = pfn_to_page(pfn);
 	svm_range_bo_ref(prange->svm_bo);
 	page->zone_device_data = prange->svm_bo;
-#if IS_ENABLED(CONFIG_DEV_PAGEMAP_OPS)
-	get_page(page);
-#endif
-	lock_page(page);
+	zone_device_page_init(page);
 }
 
 static void
