87c7fb14ffe99ef745868614687ee932fd3ccb3d
bobzhou <bob.zhou@amd.com>
2023-03-31 16:59:59 +0800

    drm/amdkcl: test whether apple_gmux.h is available and fake apple_gmux_detect()
    It's caused by 1e98231de4609b9e4597b52d0f29bb664e4938d2
    "drm/amdgpu: register a vga_switcheroo client for MacBooks with apple-gmux"
    
    Signed-off-by: bobzhou <bob.zhou@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

---
+++ include/kcl/kcl_apple-gmux.h
@@ -0,0 +1,21 @@
+#ifndef AMDKCL_APPLE_GMUX_H
+#define AMDKCL_APPLE_GMUX_H
+
+#include <linux/apple-gmux.h>
+
+#ifndef HAVE_APPLE_GMUX_DETECT
+#if IS_ENABLED(CONFIG_APPLE_GMUX)
+static inline bool apple_gmux_detect(struct pnp_dev *pnp_dev, bool *indexed_ret)
+{
+        pr_warn_once("legacy kernel without apple_gmux_detect()\n");
+        return false;
+}
+#else
+static inline bool apple_gmux_detect(struct pnp_dev *pnp_dev, bool *indexed_ret)
+{
+        return false;
+}
+#endif
+#endif
+
+#endif /* AMDKCL_APPLE_GMUX_H */
