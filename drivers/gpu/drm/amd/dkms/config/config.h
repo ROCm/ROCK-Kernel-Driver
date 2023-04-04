edf220a34e7f3a9908a1c1016e8591522b7bfcb1
Leslie Shi <Yuliang.Shi@amd.com>
2023-04-04 10:04:12 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_DRM_BUDDY_H
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -517,9 +517,6 @@
 /* Define to 1 if you have the <drm/drm_probe_helper.h> header file. */
 #define HAVE_DRM_DRM_PROBE_HELPER_H 1
 
-/* Define to 1 if you have the <drm/drm_buddy.h> header file. */
-#define HAVE_DRM_DRM_BUDDY_H 1
-
 /* Define to 1 if you have the <drm/drm_fbdev_generic.h> header file. */
 #define HAVE_DRM_DRM_FBDEV_GENERIC_H 1
 
