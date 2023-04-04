147ce4b3c2c6269121853f016bb7a9975301b808
Leslie Shi <Yuliang.Shi@amd.com>
2023-04-04 14:43:36 +0800

    drm/amdkcl: kcl-cleanup HAVE_LIST_BULK_MOVE_TAIL
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- include/kcl/kcl_list.h
+++ include/kcl/kcl_list.h
@@ -4,23 +4,6 @@
 
 #include <linux/list.h>
 
-/* Copied from include/linux/list.h */
-#if !defined(HAVE_LIST_BULK_MOVE_TAIL)
-static inline void list_bulk_move_tail(struct list_head *head,
-				       struct list_head *first,
-				       struct list_head *last)
-{
-	first->prev->next = last->next;
-	last->next->prev = first->prev;
-
-	head->prev->next = first;
-	first->prev = head->prev;
-
-	last->next = head;
-	head->prev = last;
-}
-#endif
-
 #if !defined(HAVE_LIST_ROTATE_TO_FRONT)
 static inline void list_rotate_to_front(struct list_head *list,
 					struct list_head *head)
