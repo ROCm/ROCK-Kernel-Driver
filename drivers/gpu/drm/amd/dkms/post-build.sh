6071c8b56aa871cf257b44cb3b5301a3f963dd73
Slava Grigorev <slava.grigorev@amd.com>
2023-05-09 17:16:23 -0400

    Fix unsteady amdgpu dkms build against 5.x kernels
    Make rules robust against "Argument list too long" error.
    
    SWDEV-397841
    
    Change-Id: Ib7e26359097e275801658c1a865831060b11bc51
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Reviewed-by: Jeremy Newton <Jeremy.Newton@amd.com>
    Reviewed-by: Slava Abramov <Slava.Abramov@amd.com>

---
+++ drivers/gpu/drm/amd/dkms/post-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+KERNELVER=$1
+
+#
+# Restore original kernel 5.x scripts/Makefile.build modified by post-add.sh
+#
+if [[ ${KERNELVER%%.*} -eq 5 ]]; then
+	moddir="/lib/modules/$KERNELVER"
+	mkfile="scripts/Makefile.build"
+
+	if [[ -d "$moddir/source" ]]; then
+		mkfile="$moddir/source/$mkfile"
+	else
+		mkfile="$moddir/build/$mkfile"
+	fi
+
+	mkfile=$(readlink -f $mkfile)
+
+	if [[ -f "$mkfile~" ]]; then
+		mv -f $mkfile{~,}
+	fi
+fi
--- drivers/gpu/drm/amd/dkms/pre-build.sh
+++ drivers/gpu/drm/amd/dkms/pre-build.sh
@@ -55,6 +55,40 @@ for config in $AMDGPU_CONFIG $TTM_CONFIG $SCHED_CONFIG; do
 	sed -i "/${config}$/s/$/_AMDKCL/" amd/dkms/Makefile
 done
 
+#!/bin/bash
+
+KERNELVER=$1
+
+#
+# Kernel 5.x scripts/Makefile.build patch
+# The patch makes rules robust against "Argument list too long" error
+#
+if [[ ${KERNELVER%%.*} -eq 5 ]]; then
+	moddir="/lib/modules/$KERNELVER"
+	mkfile="scripts/Makefile.build"
+
+	if [[ -d "$moddir/source" ]]; then
+		mkfile="$moddir/source/$mkfile"
+	else
+		mkfile="$moddir/build/$mkfile"
+	fi
+
+	mkfile=$(readlink -e $mkfile)
+
+	if [[ "$?" -eq 0 ]] && [[ ! -f "$mkfile~" ]]; then
+		cp -a ${mkfile}{,~}
+		sed -i -e "/^cmd_mod = {/,/} > \$@$/c"`
+			`"cmd_mod = printf '%s\x5Cn' \$(call real-search, \$*.o, .o, -objs -y -m) | \\\\\n"`
+			`"\t\$(AWK) '!x[\$\$0]++ { print(\"\$(obj)\/\"\$\$0) }' > \$@" \
+			-e "s/^[[:space:]]\+cmd_link_multi-m =.*$/"`
+			`"cmd_link_multi-m = \\\\\n"`
+			`"\t\$(file >\$@.in,\$(filter %.o,$^)) \\\\\n"`
+			`"\t\$(LD) \$(ld_flags) -r -o \$@ @\$@.in; \\\\\n"`
+			`"\trm -f \$@.in/" \
+			$mkfile
+	fi
+fi
+
 export KERNELVER
 (cd $SRC && ./configure)
 
