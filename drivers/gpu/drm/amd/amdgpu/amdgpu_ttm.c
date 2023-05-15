6293a0a579db35dc40fd9e57e0c2479533184eab
Su Hui <suhui@nfschina.com>
2023-05-15 09:34:28 +0800

    drm/amdgpu: remove unnecessary (void*) conversions
    No need cast (void*) to (struct amdgpu_device *).
    
    Signed-off-by: Su Hui <suhui@nfschina.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -2731,7 +2731,7 @@ int amdgpu_ttm_evict_resources(struct amdgpu_device *adev, int mem_type)
 #if defined(CONFIG_DEBUG_FS)
 static int amdgpu_ttm_page_pool_show(struct seq_file *m, void *unused)
 {
-	struct amdgpu_device *adev = (struct amdgpu_device *)m->private;
+	struct amdgpu_device *adev = m->private;
 
 	return ttm_pool_debugfs(&adev->mman.bdev.pool, m);
 }
