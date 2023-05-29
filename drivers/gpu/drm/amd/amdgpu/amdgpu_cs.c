1eeefcb1b93c4b995b8f3b3409ea1cb341489d72
Asher Song <Asher.Song@amd.com>
2023-05-29 16:08:12 +0800

    drm/amdkcl:drop redundant sched job cleanup
    It's caused by e9cc9cd75f2ce58751619436133cf44617268060
    "drm/amdgpu: drop redundant sched job cleanup when cs is aborted"
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
@@ -1472,7 +1472,8 @@ static int amdgpu_cs_submit(struct amdgpu_cs_parser *p,
 
 		if (amdgpu_ttm_tt_userptr_needs_pages(bo->tbo.ttm)) {
 			r = -ERESTARTSYS;
-			goto error_unlock;
+			amdgpu_mn_unlock(p->mn);
+			return r;
 		}
 	}
 #endif
