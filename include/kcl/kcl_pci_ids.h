49ed1a3e2257017403542431391e741f37162fdf
Bob Zhou <bob.zhou@amd.com>
2023-06-09 15:13:26 +0800

    drm/amdkcl: fake macro PCI_CLASS_ACCELERATOR_PROCESSING
    It's caused by 186e61eda3749bf2ec669f73fa7c10bdf049a7ce
    "drm/amdgpu: add the accelerator PCIe class"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>

---
+++ include/kcl/kcl_pci_ids.h
@@ -0,0 +1,10 @@
+#ifndef AMDKCL_PCI_IDS_H
+#define AMDKCL_PCI_IDS_H
+
+#include <linux/pci_ids.h>
+
+#ifndef PCI_CLASS_ACCELERATOR_PROCESSING
+#define PCI_CLASS_ACCELERATOR_PROCESSING     0x1200
+#endif
+
+#endif
