717688db37e546dc24e83b8a72e3ec7eed6d2a25
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2023-06-21 10:42:07 +0200

    drm/amdgpu: add VISIBLE info in amdgpu_bo_print_info
    This allows tools to distinguish between VRAM and visible VRAM.
    
    Use the opportunity to fix locking before accessing bo.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
@@ -1617,24 +1617,33 @@ u64 amdgpu_bo_print_info(int id, struct amdgpu_bo *bo, struct seq_file *m)
 	unsigned int pin_count;
 	u64 size;
 
-	domain = amdgpu_mem_type_to_domain(bo->tbo.resource->mem_type);
-	switch (domain) {
-	case AMDGPU_GEM_DOMAIN_VRAM:
-		placement = "VRAM";
-		break;
-	case AMDGPU_GEM_DOMAIN_DGMA:
-		placement = "DGMA";
-		break;
-	case AMDGPU_GEM_DOMAIN_DGMA_IMPORT:
-		placement = "DGMA_IMPORT";
-		break;
-	case AMDGPU_GEM_DOMAIN_GTT:
-		placement = " GTT";
-		break;
-	case AMDGPU_GEM_DOMAIN_CPU:
-	default:
-		placement = " CPU";
-		break;
+	if (dma_resv_trylock(bo->tbo.base.resv)) {
+		unsigned int domain;
+		domain = amdgpu_mem_type_to_domain(bo->tbo.resource->mem_type);
+		switch (domain) {
+		case AMDGPU_GEM_DOMAIN_VRAM:
+			if (amdgpu_bo_in_cpu_visible_vram(bo))
+				placement = "VRAM VISIBLE";
+			else
+				placement = "VRAM";
+			break;
+		case AMDGPU_GEM_DOMAIN_DGMA:
+            		placement = "DGMA";
+            		break;
+		case AMDGPU_GEM_DOMAIN_DGMA_IMPORT:
+            		placement = "DGMA_IMPORT";
+            		break;
+		case AMDGPU_GEM_DOMAIN_GTT:
+			placement = "GTT";
+			break;
+		case AMDGPU_GEM_DOMAIN_CPU:
+		default:
+			placement = "CPU";
+			break;
+		}
+		dma_resv_unlock(bo->tbo.base.resv);
+	} else {
+		placement = "UNKNOWN";
 	}
 
 	size = amdgpu_bo_size(bo);
