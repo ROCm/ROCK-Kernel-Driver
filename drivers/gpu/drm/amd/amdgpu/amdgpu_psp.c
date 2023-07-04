ac4cb9d91da00140f0297ab558778e45f40a6b77
Bob Zhou <bob.zhou@amd.com>
2023-07-04 11:13:36 +0800

    drm/amdkcl: Test whether amdgpu_attr_group->is_bin_visible is available
    It's caused by fd536660bcbdbb15d6b715f2104d59fa1de50260
    "drm/amd: Detect IFWI or PD upgrade support in psp_early_init()"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
@@ -3731,6 +3731,7 @@ static umode_t amdgpu_flash_attr_is_visible(struct kobject *kobj, struct attribu
 	return adev->psp.sup_ifwi_up ? 0440 : 0;
 }
 
+#ifdef HAVE_ATTRIBUTE_GROUP_IS_BIN_VISIBLE
 static umode_t amdgpu_bin_flash_attr_is_visible(struct kobject *kobj,
 						struct bin_attribute *attr,
 						int idx)
@@ -3741,11 +3742,14 @@ static umode_t amdgpu_bin_flash_attr_is_visible(struct kobject *kobj,
 
 	return adev->psp.sup_ifwi_up ? 0660 : 0;
 }
+#endif
 
 const struct attribute_group amdgpu_flash_attr_group = {
 	.attrs = flash_attrs,
 	.bin_attrs = bin_flash_attrs,
+#ifdef HAVE_ATTRIBUTE_GROUP_IS_BIN_VISIBLE
 	.is_bin_visible = amdgpu_bin_flash_attr_is_visible,
+#endif
 	.is_visible = amdgpu_flash_attr_is_visible,
 };
 
