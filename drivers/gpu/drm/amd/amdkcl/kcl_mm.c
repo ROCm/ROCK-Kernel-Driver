f9affedfab371622b10a5a1e9d5d2fc070aabc39
Bob Zhou <bob.zhou@amd.com>
2023-07-05 13:50:23 +0800

    drm/amdkcl: kcl-cleanup HAVE_MM_ACCESS
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_mm.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_mm.c
@@ -7,17 +7,6 @@
 #include <linux/sched.h>
 #include <linux/pagemap.h>
 
-#ifndef HAVE_MM_ACCESS
-struct mm_struct* (*_kcl_mm_access)(struct task_struct *task, unsigned int mode);
-EXPORT_SYMBOL(_kcl_mm_access);
-
-static struct mm_struct * __kcl_mm_access_stub(struct task_struct *task, unsigned int mode)
-{
-	pr_warn_once("This kernel version not support API: mm_access !\n");
-	return NULL;
-}
-#endif
-
 #ifndef HAVE_MMPUT_ASYNC
 void (*_kcl_mmput_async)(struct mm_struct *mm);
 EXPORT_SYMBOL(_kcl_mmput_async);
@@ -49,9 +38,6 @@ extern struct kmem_cache *(*_kcl_kmalloc_slab)(size_t size, gfp_t flags);
 
 void amdkcl_mm_init(void)
 {
-#ifndef HAVE_MM_ACCESS
-	_kcl_mm_access = amdkcl_fp_setup("mm_access", __kcl_mm_access_stub);
-#endif
 
 #ifndef HAVE_MMPUT_ASYNC
 	_kcl_mmput_async = amdkcl_fp_setup("mmput_async", __kcl_mmput_async);
