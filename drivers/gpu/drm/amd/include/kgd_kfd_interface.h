21ab4d9a5d3f9ebffe6ffcc21bb347707940775b
James Zhu <James.Zhu@amd.com>
2023-07-21 14:11:56 -0400

    drm/amdkfd: add interface to trigger pc sampling trap
    Add interface to trigger pc sampling trap.
    
    -v6: add multiple instances support
    
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Tested-by: Vladimir Indic <Vladimir.Indic@amd.com>

--- drivers/gpu/drm/amd/include/kgd_kfd_interface.h
+++ drivers/gpu/drm/amd/include/kgd_kfd_interface.h
@@ -31,6 +31,8 @@
 #include <linux/types.h>
 #include <linux/bitmap.h>
 #include <linux/dma-fence.h>
+#include <uapi/linux/kfd_ioctl.h>
+
 #include "amdgpu_irq.h"
 #include "amdgpu_gfx.h"
 
@@ -329,6 +331,12 @@ struct kfd2kgd_calls {
 	uint64_t (*hqd_reset)(struct amdgpu_device *adev,
 			      uint32_t pipe_id, uint32_t queue_id,
 			      uint32_t inst, unsigned int utimeout);
+	uint32_t (*trigger_pc_sample_trap)(struct amdgpu_device *adev,
+					uint32_t vmid,
+					uint32_t *target_simd,
+					uint32_t *target_wave_slot,
+					enum kfd_ioctl_pc_sample_method method,
+					uint32_t inst);
 };
 
 #endif	/* KGD_KFD_INTERFACE_H_INCLUDED */
