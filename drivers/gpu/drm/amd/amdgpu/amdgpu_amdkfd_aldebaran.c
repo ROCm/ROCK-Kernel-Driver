d48ede6819ede7b0b8fadfcfc908a8d3982c82c6
James Zhu <James.Zhu@amd.com>
2023-07-21 14:15:57 -0400

    drm/amdkfd: trigger pc sampling trap for aldebaran
    Implement trigger pc sampling trap for aldebaran.
    
    -v6: add multiple instances support
    
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Tested-by: Vladimir Indic <Vladimir.Indic@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_aldebaran.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_aldebaran.c
@@ -164,6 +164,17 @@ static uint32_t kgd_gfx_aldebaran_set_address_watch(
 	return watch_address_cntl;
 }
 
+static uint32_t kgd_aldebaran_trigger_pc_sample_trap(struct amdgpu_device *adev,
+					    uint32_t vmid,
+					    uint32_t *target_simd,
+					    uint32_t *target_wave_slot,
+					    enum kfd_ioctl_pc_sample_method method,
+					    uint32_t inst)
+{
+	return kgd_gfx_v9_trigger_pc_sample_trap(adev, vmid, 8, 4,
+					target_simd, target_wave_slot, method, inst);
+}
+
 const struct kfd2kgd_calls aldebaran_kfd2kgd = {
 	.program_sh_mem_settings = kgd_gfx_v9_program_sh_mem_settings,
 	.set_pasid_vmid_mapping = kgd_gfx_v9_set_pasid_vmid_mapping,
@@ -192,4 +203,5 @@ const struct kfd2kgd_calls aldebaran_kfd2kgd = {
 	.get_iq_wait_times = kgd_gfx_v9_get_iq_wait_times,
 	.build_grace_period_packet_info = kgd_gfx_v9_build_grace_period_packet_info,
 	.program_trap_handler_settings = kgd_gfx_v9_program_trap_handler_settings,
+	.trigger_pc_sample_trap = kgd_aldebaran_trigger_pc_sample_trap,
 };
