9134ba0e30f0d33dab4d0bfdf9d3c89ba05305bb
Bob Zhou <bob.zhou@amd.com>
2023-07-24 11:58:23 +0800

    drm/amdkcl: apply amdkcl_ttm_resvp to dma_resv_trylock()
    It's caused by d769528e46493d56f9151fdecf25091b74c4eb49
    "drm/amdgpu: add VISIBLE info in amdgpu_bo_print_info"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
@@ -1617,7 +1617,7 @@ u64 amdgpu_bo_print_info(int id, struct amdgpu_bo *bo, struct seq_file *m)
 	unsigned int pin_count;
 	u64 size;
 
-	if (dma_resv_trylock(bo->tbo.base.resv)) {
+	if (dma_resv_trylock(amdkcl_ttm_resvp(&bo->tbo))) {
 		unsigned int domain;
 		domain = amdgpu_mem_type_to_domain(bo->tbo.resource->mem_type);
 		switch (domain) {
@@ -1641,7 +1641,7 @@ u64 amdgpu_bo_print_info(int id, struct amdgpu_bo *bo, struct seq_file *m)
 			placement = "CPU";
 			break;
 		}
-		dma_resv_unlock(bo->tbo.base.resv);
+		dma_resv_unlock(amdkcl_ttm_resvp(&bo->tbo));
 	} else {
 		placement = "UNKNOWN";
 	}
