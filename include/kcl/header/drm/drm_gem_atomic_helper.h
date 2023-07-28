a3ea22be5e896fe1ad23c005dd2ae1e3612c9b18
Bob Zhou <bob.zhou@amd.com>
2023-07-28 15:07:56 +0800

    drm/amdkcl: test whether drm_gem_atomic_helper.h is available
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- include/kcl/header/drm/drm_gem_atomic_helper.h
+++ include/kcl/header/drm/drm_gem_atomic_helper.h
@@ -2,7 +2,7 @@
 #ifndef _KCL_HEADER_DRM_GEM_ATOMIC_HELPER_PREPARE_H_H_
 #define _KCL_HEADER_DRM_GEM_ATOMIC_HELPER_PREPARE_H_H_
 
-#if defined(HAVE_DRM_GEM_PLANE_HELPER_PREPARE_FB)
+#if defined(HAVE_DRM_DRM_GEM_ATOMIC_HELPER_H)
 #include_next <drm/drm_gem_atomic_helper.h>
 #endif
 
