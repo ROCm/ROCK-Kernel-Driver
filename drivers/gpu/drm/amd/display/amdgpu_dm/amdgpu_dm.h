226e00087c85cccf6e5bb026990d1a251a350b8e
Asher Song <Asher.Song@amd.com>
2023-08-01 17:10:04 +0800

    drm/amdkcl: kcl-cleanup for_each_{old/new/oldnew}_{plane/connector/crtc}_in_state
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by:  Leslie Shi <Yuliang.Shi@amd.com>
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -202,31 +202,6 @@ struct amdgpu_dm_backlight_caps {
 };
 
 /**
- * for_each_oldnew_plane_in_state_reverse - iterate over all planes in an atomic
- * update in reverse order
- * @__state: &struct drm_atomic_state pointer
- * @plane: &struct drm_plane iteration cursor
- * @old_plane_state: &struct drm_plane_state iteration cursor for the old state
- * @new_plane_state: &struct drm_plane_state iteration cursor for the new state
- * @__i: int iteration cursor, for macro-internal use
- *
- * This iterates over all planes in an atomic update in reverse order,
- * tracking both old and  new state. This is useful in places where the
- * state delta needs to be considered, for example in atomic check functions.
- */
-#if !defined(for_each_oldnew_plane_in_state_reverse) && \
-	defined(for_each_oldnew_plane_in_state)
-#define for_each_oldnew_plane_in_state_reverse(__state, plane, old_plane_state, new_plane_state, __i) \
-	for ((__i) = ((__state)->dev->mode_config.num_total_plane - 1);	\
-	     (__i) >= 0;						\
-	     (__i)--)							\
-		for_each_if ((__state)->planes[__i].ptr &&		\
-			     ((plane) = (__state)->planes[__i].ptr,	\
-			      (old_plane_state) = (__state)->planes[__i].old_state,\
-			      (new_plane_state) = (__state)->planes[__i].new_state, 1))
-#endif
-
-/**
  * struct dal_allocation - Tracks mapped FB memory for SMU communication
  * @list: list of dal allocations
  * @bo: GPU buffer object
@@ -239,7 +214,6 @@ struct dal_allocation {
 	void *cpu_ptr;
 	u64 gpu_addr;
 };
-
 
 /**
  * struct hpd_rx_irq_offload_work_queue - Work queue to handle hpd_rx_irq
@@ -1034,12 +1008,7 @@ int dm_atomic_get_state(struct drm_atomic_state *state,
 
 struct drm_connector *
 amdgpu_dm_find_first_crtc_matching_connector(struct drm_atomic_state *state,
-#ifndef for_each_new_connector_in_state
-					     struct drm_crtc *crtc,
-					     bool from_state_var);
-#else
 					     struct drm_crtc *crtc);
-#endif
 
 int convert_dc_color_depth_into_bpc(enum dc_color_depth display_color_depth);
 struct idle_workqueue *idle_create_workqueue(struct amdgpu_device *adev);
