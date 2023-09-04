b92a328a5264bcd39bafbf29ccf4fa9413497ec0
Bob Zhou <bob.zhou@amd.com>
2023-09-04 17:12:58 +0800

    drm/amdkcl: fake macro static_assert()
    It's caused by ff6320eb2b5616b4843588b066652837e6972666
    "drm/amdgpu: add UMSCH 4.0 api definition"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

---
+++ include/kcl/kcl_build_bug.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef AMDKCL_LINUX_BUILD_BUG_H
+#define AMDKCL_LINUX_BUILD_BUG_H
+
+#ifndef static_assert
+#define static_assert(expr, ...) __static_assert(expr, ##__VA_ARGS__, #expr)
+#define __static_assert(expr, msg, ...) _Static_assert(expr, msg)
+#endif
+
+#endif /* AMDKCL_LINUX_BUILD_BUG_H */
