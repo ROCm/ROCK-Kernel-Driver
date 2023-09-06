6d4591fc98a34108c11b09950dec5a7d3b27ceb3
Bob Zhou <bob.zhou@amd.com>
2023-09-06 11:15:25 +0800

    drm/amdkcl: fix the missing build_bug.h and update config.h
    It's caused by ff6320eb2b5616b4843588b066652837e6972666
    "drm/amdgpu: add UMSCH 4.0 api definition"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- include/kcl/kcl_build_bug.h
+++ include/kcl/kcl_build_bug.h
@@ -2,6 +2,8 @@
 #ifndef AMDKCL_LINUX_BUILD_BUG_H
 #define AMDKCL_LINUX_BUILD_BUG_H
 
+#include <linux/build_bug.h>
+
 #ifndef static_assert
 #define static_assert(expr, ...) __static_assert(expr, ##__VA_ARGS__, #expr)
 #define __static_assert(expr, msg, ...) _Static_assert(expr, msg)
