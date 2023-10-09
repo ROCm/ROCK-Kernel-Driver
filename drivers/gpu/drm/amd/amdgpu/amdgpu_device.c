622d697870cddf581d4a58b899fcbb81914dd77b
Bob Zhou <bob.zhou@amd.com>
2023-10-09 14:39:02 +0800

    drm/amdkcl: wrap code under macro HAVE_PCI_DRIVER_DEV_GROUPS
    It's caused by 442a478469621f459834b27d5ac1c392bacd9c25
    "drm/amdgpu: Add sysfs attribute to get board info"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -179,7 +179,7 @@ static DEVICE_ATTR(pcie_replay_count, 0444,
  * - "unknown"	- Not known
  *
  */
-
+#ifdef HAVE_PCI_DRIVER_DEV_GROUPS
 static ssize_t amdgpu_device_get_board_info(struct device *dev,
 					    struct device_attribute *attr,
 					    char *buf)
@@ -231,6 +231,7 @@ static const struct attribute_group amdgpu_board_attrs_group = {
 	.attrs = amdgpu_board_attrs,
 	.is_visible = amdgpu_board_attrs_is_visible
 };
+#endif
 
 static void amdgpu_device_get_pcie_info(struct amdgpu_device *adev);
 
@@ -4136,11 +4137,12 @@ fence_driver_init:
 	r = sysfs_create_files(&adev->dev->kobj, amdgpu_dev_attributes);
 	if (r)
 		dev_err(adev->dev, "Could not create amdgpu device attr\n");
-
+#ifdef HAVE_PCI_DRIVER_DEV_GROUPS
 	r = devm_device_add_group(adev->dev, &amdgpu_board_attrs_group);
 	if (r)
 		dev_err(adev->dev,
 			"Could not create amdgpu board attributes\n");
+#endif
 
 	amdgpu_fru_sysfs_init(adev);
 
