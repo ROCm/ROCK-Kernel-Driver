ad8fe71f2be3b7674437966fdb1c68b5dc1251e6
Bob Zhou <bob.zhou@amd.com>
2023-10-09 14:32:52 +0800

    drm/amdkcl: wrap code under macro HAVE_STRUCT_XARRAY
    It's caused by 2b332c97042ed499b7f04d2648736848c8f007a4
    "drm/amdgpu: add new INFO ioctl query for the last GPU page fault"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -1257,6 +1257,7 @@ int amdgpu_info_ioctl(struct drm_device *dev, void *data, struct drm_file *filp)
 		return copy_to_user(out, max_ibs,
 				    min((size_t)size, sizeof(max_ibs))) ? -EFAULT : 0;
 	}
+#ifdef HAVE_STRUCT_XARRAY
 	case AMDGPU_INFO_GPUVM_FAULT: {
 		struct amdgpu_fpriv *fpriv = filp->driver_priv;
 		struct amdgpu_vm *vm = &fpriv->vm;
@@ -1277,6 +1278,7 @@ int amdgpu_info_ioctl(struct drm_device *dev, void *data, struct drm_file *filp)
 		return copy_to_user(out, &gpuvm_fault,
 				    min((size_t)size, sizeof(gpuvm_fault))) ? -EFAULT : 0;
 	}
+#endif
 	default:
 		DRM_DEBUG_KMS("Invalid request %d\n", info->query);
 		return -EINVAL;
