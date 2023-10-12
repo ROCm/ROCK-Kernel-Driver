5d72e8c4172f8f5d89c12ed12e9e4acae7093e5f
Asher Song <Asher.Song@amd.com>
2023-10-12 14:41:17 +0800

    drm/amdkcl: test whether drm_driver->release() is available
    introduced by v4.10-rc5-1045-gf30c92576af4
    This patch is caused by 'drm/amdgpu: Embed drm_device into amdgpu_device (v2)
    
    Although v5.5-rc2-1531-ge62bf83aa1bb has removed checking on dev->dev_private,
    some distro with kernel v5.6+ (such as ubuntu20.04-oem) may still access dev->dev_private
    In case of accessing dev->dev_private in drm code, init dev->dev_private to amdgpu_device.
    Wrap this initialization with kcl inline function for easy tracking.
    
    Squash of
    469aa26d1979 drm/amdkcl: fix reture val for faked __devm_drm_dev_alloc()
    af31a40bf581 drm/amdkcl: rework the faked __devm_drm_dev_alloc
    312a6b63c4e9 drm/amdkcl: fake devm_drm_dev_alloc
    2ec2a0da80d7 drm/amdkcl: always init drm_device.dev_private
    22de9a4cefd6 drm/amdkcl: test whether drm_dev_fini() is available (v2)
    		introduced by v5.5-rc2-1531-ge62bf83aa1bb
    f9896ea4089f drm/amdkcl: test whether drm checking on dev->dev_private
    d4300369d400 drm/amdkcl: test whether drm_dev_fini() is available
    
    Signed-off-by: Yang Xiong <Yang.Xiong@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Guchun Chen <guchun.chen@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2348,7 +2348,11 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 	kcl_pci_configure_extended_tags(pdev);
 	ret = pci_enable_device(pdev);
 	if (ret)
+#ifndef AMDKCL_DEVM_DRM_DEV_ALLOC
 		return ret;
+#else
+		goto err_free;
+#endif
 
 	pci_set_drvdata(pdev, ddev);
 
@@ -2442,6 +2446,10 @@ retry_init:
 
 err_pci:
 	pci_disable_device(pdev);
+#ifdef AMDKCL_DEVM_DRM_DEV_ALLOC
+err_free:
+	amdkcl_drm_dev_release(ddev);
+#endif
 	return ret;
 }
 
@@ -2453,7 +2461,6 @@ amdgpu_pci_remove(struct pci_dev *pdev)
 
 	amdgpu_xcp_dev_unplug(adev);
 	drm_dev_unplug(dev);
-
 	if (adev->pm.rpm_mode != AMDGPU_RUNPM_NONE) {
 		pm_runtime_get_sync(dev->dev);
 		pm_runtime_forbid(dev->dev);
@@ -2469,6 +2476,18 @@ amdgpu_pci_remove(struct pci_dev *pdev)
 	pci_disable_device(pdev);
 	pci_wait_for_pending_transaction(pdev);
 }
+
+#ifdef HAVE_DRM_DRIVER_RELEASE
+#ifndef HAVE_DRM_DRM_MANAGED_H
+static void amdgpu_driver_release(struct drm_device *ddev)
+{
+	struct amdgpu_device *adev = drm_to_adev(ddev);
+
+	drm_dev_fini(ddev);
+	kfree(adev);
+}
+#endif
+#endif
 
 static void
 amdgpu_pci_shutdown(struct pci_dev *pdev)
@@ -3017,6 +3036,7 @@ static const struct drm_driver amdgpu_kms_driver = {
 	.debugfs_cleanup = amdgpu_debugfs_cleanup,
 #endif
 #endif
+
 #ifndef HAVE_STRUCT_DRM_CRTC_FUNCS_GET_VBLANK_TIMESTAMP
 	.get_vblank_counter = kcl_amdgpu_get_vblank_counter_kms,
 	.enable_vblank = kcl_amdgpu_enable_vblank_kms,
@@ -3030,7 +3050,9 @@ static const struct drm_driver amdgpu_kms_driver = {
 	.dumb_create = amdgpu_mode_dumb_create,
 	.dumb_map_offset = amdgpu_mode_dumb_mmap,
 	.fops = &amdgpu_driver_kms_fops,
+#ifdef HAVE_DRM_DRIVER_RELEASE
 	.release = &amdgpu_driver_release_kms,
+#endif
 #ifdef CONFIG_PROC_FS
 	.show_fdinfo = amdgpu_show_fdinfo,
 #endif
