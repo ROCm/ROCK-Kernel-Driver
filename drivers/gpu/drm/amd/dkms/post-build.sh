70deae192c85a4bf3a9c652f228dc29b86793a13
Slava Grigorev <slava.grigorev@amd.com>
2023-10-24 12:47:03 -0400

    Shorten path to DKMS module build directory
    Make the build robust against too long argument error
    
    Change-Id: Ifb3f146f55d645bb1809f79751902690f937b096
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>

--- drivers/gpu/drm/amd/dkms/post-build.sh
+++ drivers/gpu/drm/amd/dkms/post-build.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-KERNELVER=$1
-
-#
-# Restore original kernel 5.x and Kernel 4.x scripts/Makefile.build modified by post-add.sh
-#
-if [ ${KERNELVER%%.*} -eq 5  -o  ${KERNELVER%%.*} -eq 4 ]; then
-	moddir="/lib/modules/$KERNELVER"
-	mkfile="scripts/Makefile.build"
-
-	if [[ -d "$moddir/source" ]]; then
-		mkfile="$moddir/source/$mkfile"
-	else
-		mkfile="$moddir/build/$mkfile"
-	fi
-
-	mkfile=$(readlink -f $mkfile)
-
-	if [[ -f "$mkfile~" ]]; then
-		mv -f $mkfile{~,}
-	fi
-fi
+MODULE_BUILD_DIR=$1
+rm -rf $MODULE_BUILD_DIR
