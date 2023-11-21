85e44b7295224b014b9996614df39e1fa721c896
Yang Wang <kevinyang.wang@amd.com>
2023-11-21 16:47:57 +0800

    drm/amd/kcl: fix issue of missing mca-debug-mode debugfs node
    v1:
    fix issue of missing mca-debug-mode debugfs node.
    
    v2:
    using helper macro DEFINE_SIMPLE_ATTRIBUTE to simple code
    
    Fixes: 96e2072e93c0 ("drm/amdkcl: wrap code under macro DEFINE_DEBUGFS_ATTRIBUTE")
    
    Signed-off-by: Yang Wang <kevinyang.wang@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_mca.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_mca.c
@@ -615,6 +615,8 @@ static const struct file_operations mca_ue_dump_debug_fops = {
 
 #ifdef DEFINE_DEBUGFS_ATTRIBUTE
 DEFINE_DEBUGFS_ATTRIBUTE(mca_debug_mode_fops, NULL, amdgpu_mca_smu_debug_mode_set, "%llu\n");
+#else
+DEFINE_SIMPLE_ATTRIBUTE(mca_debug_mode_fops, NULL, amdgpu_mca_smu_debug_mode_set, "%llu\n");
 #endif
 #endif
 
@@ -624,9 +626,7 @@ void amdgpu_mca_smu_debugfs_init(struct amdgpu_device *adev, struct dentry *root
 	if (!root)
 		return;
 
-#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 	debugfs_create_file("mca_debug_mode", 0200, root, adev, &mca_debug_mode_fops);
-#endif
 	debugfs_create_file("mca_ue_dump", 0400, root, adev, &mca_ue_dump_debug_fops);
 	debugfs_create_file("mca_ce_dump", 0400, root, adev, &mca_ce_dump_debug_fops);
 #endif
