d48511acc0e67f16fd523dad14d3510fc7474b78
Le Ma <le.ma@amd.com>
2024-01-09 18:06:35 +0800

    drm/amdgpu: move debug options init prior to amdgpu device init
    To bring debug options into effect in early initialization phase
    
    Signed-off-by: Le Ma <le.ma@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2274,6 +2274,8 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 #endif
 	pci_set_drvdata(pdev, ddev);
 
+	amdgpu_init_debug_options(adev);
+
 	ret = amdgpu_driver_load_kms(adev, flags);
 	if (ret)
 		goto err_pci;
@@ -2353,8 +2355,6 @@ retry_init:
 		    (adev->asic_type >= CHIP_NAVI10))
 			amdgpu_get_secondary_funcs(adev);
 	}
-
-	amdgpu_init_debug_options(adev);
 
 	return 0;
 
