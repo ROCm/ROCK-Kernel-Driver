617f60bb6b8502e15e327e8eefeda8ddb9e3dfa9
Hawking Zhang <Hawking.Zhang@amd.com>
2024-02-01 15:06:27 +0800

    Revert "drm/amdkfd: Fix sparse __rcu annotation warnings"
    This reverts commit bc98fce83f90bf1e2190a9351202ec1df1c2e45c.
    To resolve server rebooting issue observed while running
    rocGDB UT on MI100/MI200.
    
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_process.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -1107,7 +1107,6 @@ static void kfd_process_wq_release(struct work_struct *work)
 {
 	struct kfd_process *p = container_of(work, struct kfd_process,
 					     release_work);
-	struct dma_fence *ef;
 
 	kfd_process_dequeue_from_all_devices(p);
 	pqm_uninit(&p->pqm);
@@ -1116,9 +1115,7 @@ static void kfd_process_wq_release(struct work_struct *work)
 	 * destroyed. This allows any BOs to be freed without
 	 * triggering pointless evictions or waiting for fences.
 	 */
-	synchronize_rcu();
-	ef = rcu_access_pointer(p->ef);
-	dma_fence_signal(ef);
+	dma_fence_signal(p->ef);
 
 	kfd_process_remove_sysfs(p);
 
@@ -1127,7 +1124,7 @@ static void kfd_process_wq_release(struct work_struct *work)
 	svm_range_list_fini(p);
 
 	kfd_process_destroy_pdds(p);
-	dma_fence_put(ef);
+	dma_fence_put(p->ef);
 
 	kfd_event_free_process(p);
 
