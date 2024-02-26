721b96fea8f48fbee8a0cb7d9c1805ef38156524
Asher Song <Asher.Song@amd.com>
2024-02-26 21:24:54 +0800

    drm/amdkcl: Check MAX_ORDER whether defined
    It's caused by 5e0a760b44417f7cadd79de2204d6247109558a0
    mm, treewide: rename MAX_ORDER to MAX_PAGE_ORDER
    
    On kernel 6.8, macro MAX_ORDER is renamed to MAX_PAGE_ORDER.
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

---
+++ include/kcl/kcl_mmzone.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _KCL_MMZONE_H
+#define _KCL_MMZONE_H
+
+#include <linux/mmzone.h>
+
+#ifndef __ASSEMBLY__
+#ifndef __GENERATING_BOUNDS_H
+
+#ifndef MAX_ORDER
+#define MAX_ORDER MAX_PAGE_ORDER
+#endif
+
+#endif
+#endif
+
+#endif
