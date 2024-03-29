99e02a60d4187b743e78b42ebf8d96a86fab3b8b
Bob Zhou <bob.zhou@amd.com>
2024-03-29 11:29:47 +0800

    drm/amdkcl: wrap code out of macro HAVE_DRM_DP_MST_ATOMIC_CHECK
    It's caused by 1f0f4865388bd1d19701d1d65552535dd890e566
    "drm/amd/display: Add timing pixel encoding for mst mode validation"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -962,6 +962,7 @@ struct dsc_mst_fairness_params {
 	uint32_t bpp_overwrite;
 	struct amdgpu_dm_connector *aconnector;
 };
+#endif
 
 #if defined(CONFIG_DRM_AMD_DC_FP)
 static uint16_t get_fec_overhead_multiplier(struct dc_link *dc_link)
@@ -986,6 +987,7 @@ static int kbps_to_peak_pbn(int kbps, uint16_t fec_overhead_multiplier_x1000)
 	return (int) DIV64_U64_ROUND_UP(peak_kbps * 64, (54 * 8 * 1000));
 }
 
+#if defined(HAVE_DRM_DP_MST_ATOMIC_CHECK)
 static void set_dsc_configs_from_fairness_vars(struct dsc_mst_fairness_params *params,
 		struct dsc_mst_fairness_vars *vars,
 		int count,
