a2100dbe7c3decfb4401ba8a5ec063aab2dd67da
Philip Yang <Philip.Yang@amd.com>
2024-04-05 18:54:51 -0400

    drm/amdkfd: Allow peer direct dma map with null RDMA device
    To test KFD peer direct RDMA functions on system without NIC/RDMA
    device.
    
    Add dynamic debug message to dump sg table segments address and size, to
    help debug contiguous VRAM peer direct RDMA.
    
    Signed-off-by: Philip Yang <Philip.Yang@amd.com>
    Reviewed-by: Felix Kuehling <felix.kuehling@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -2649,9 +2649,11 @@ int amdgpu_amdkfd_gpuvm_get_sg_table(struct amdgpu_device *adev,
 		cur_page++;
 	}
 
-	ret = dma_map_sgtable(dma_dev, sg, dir, DMA_ATTR_SKIP_CPU_SYNC);
-	if (ret)
-		goto out_of_range;
+	if (dma_dev) {
+		ret = dma_map_sgtable(dma_dev, sg, dir, DMA_ATTR_SKIP_CPU_SYNC);
+		if (ret)
+			goto out_of_range;
+	}
 
 	*ret_sg = sg;
 	return 0;
@@ -2675,7 +2677,8 @@ void amdgpu_amdkfd_gpuvm_put_sg_table(struct amdgpu_bo *bo,
 	}
 
 	/* Unmap system memory */
-	dma_unmap_sgtable(dma_dev, sgt, dir, DMA_ATTR_SKIP_CPU_SYNC);
+	if (dma_dev)
+		dma_unmap_sgtable(dma_dev, sgt, dir, DMA_ATTR_SKIP_CPU_SYNC);
 	sg_free_table(sgt);
 	kfree(sgt);
 }
