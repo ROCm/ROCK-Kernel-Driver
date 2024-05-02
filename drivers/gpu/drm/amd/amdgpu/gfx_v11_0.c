29eb1efe114359e9a8ca9fd6c6b0df974da9eb48
Shashank Sharma <shashank.sharma@amd.com>
2024-05-02 12:37:30 +0200

    drm/amdgpu: enable GFX-V11 userqueue support
    This patch enables GFX-v11 IP support in the usermode queue base
    code. It typically:
    - adds a GFX_v11 specific MQD structure
    - sets IP functions to create and destroy MQDs
    - sets MQD objects coming from userspace
    
    V10: introduced this spearate patch for GFX V11 enabling (Alex).
    V11: Addressed review comments:
         - update the comments in GFX mqd structure informing user about using
           the INFO IOCTL for object sizes (Alex)
         - rename struct drm_amdgpu_userq_mqd_gfx_v11 to
           drm_amdgpu_userq_mqd_gfx11 (Marek)
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Christian Koenig <christian.koenig@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Shashank Sharma <shashank.sharma@amd.com>
    Signed-off-by: Arvind Yadav <arvind.yadav@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v11_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v11_0.c
@@ -50,6 +50,7 @@
 #include "gfx_v11_0_3.h"
 #include "nbio_v4_3.h"
 #include "mes_v11_0.h"
+#include "mes_v11_0_userqueue.h"
 
 #define GFX11_NUM_GFX_RINGS		1
 #define GFX11_MEC_HPD_SIZE	2048
@@ -1556,6 +1557,7 @@ static int gfx_v11_0_sw_init(struct amdgpu_ip_block *ip_block)
 		adev->gfx.mec.num_mec = 2;
 		adev->gfx.mec.num_pipe_per_mec = 4;
 		adev->gfx.mec.num_queue_per_pipe = 4;
+		adev->userq_funcs[AMDGPU_HW_IP_GFX] = &userq_mes_v11_0_funcs;
 		break;
 	case IP_VERSION(11, 0, 1):
 	case IP_VERSION(11, 0, 4):
@@ -1568,6 +1570,7 @@ static int gfx_v11_0_sw_init(struct amdgpu_ip_block *ip_block)
 		adev->gfx.mec.num_mec = 1;
 		adev->gfx.mec.num_pipe_per_mec = 4;
 		adev->gfx.mec.num_queue_per_pipe = 4;
+		adev->userq_funcs[AMDGPU_HW_IP_GFX] = &userq_mes_v11_0_funcs;
 		break;
 	default:
 		adev->gfx.me.num_me = 1;
