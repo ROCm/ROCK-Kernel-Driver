7b76c2855491bc0a0532b61663b9e2313659afbf
Alex Deucher <alexander.deucher@amd.com>
2024-05-14 11:11:04 -0400

    drm/amdgpu: Add documentation for AMD_IP_BLOCK_TYPE_ISP
    Add missing documentation for the IP block.
    
    Fixes: a83048bfa402 ("drm/amd/amdgpu: Add ISP support to amdgpu_discovery")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Pratap Nirujogi <pratap.nirujogi@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/include/amd_shared.h
+++ drivers/gpu/drm/amd/include/amd_shared.h
@@ -86,6 +86,7 @@ enum amd_apu_flags {
 * @AMD_IP_BLOCK_TYPE_JPEG: JPEG Engine
 * @AMD_IP_BLOCK_TYPE_VPE: Video Processing Engine
 * @AMD_IP_BLOCK_TYPE_UMSCH_MM: User Mode Schduler for Multimedia
+* @AMD_IP_BLOCK_TYPE_ISP: Image Signal Processor
 * @AMD_IP_BLOCK_TYPE_NUM: Total number of IP block types
 */
 enum amd_ip_block_type {
