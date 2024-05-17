67bb067d3fea3a5d55198eebf081173769e1032b
Asher Song <Asher.Song@amd.com>
2024-05-17 11:52:59 +0800

    drm/amdkcl: fake smca_get_bank_type
    When redefining HAVE_SMCA_UMC_V2, the fake function smca_get_bank_type
    is called by amdgpu_bad_page_notifier. However origin fake function can
    not be referenced when making intree build as it defined in amdkcl
    modules.
    
    So we make a macro for the fake function in backport/kcl_mce.h
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by:  Flora Cui <flora.cui@amd.com>
    Reviewed-by:  Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_mce_amd.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_mce_amd.c
@@ -7,24 +7,11 @@
  *
  *  All MC4_MISCi registers are shared between cores on a node.
  */
-
-
 #ifdef CONFIG_X86_MCE_AMD
 #include <asm/mce.h>
 
-#if defined(HAVE_SMCA_GET_BANK_TYPE_WITH_TWO_ARGUMENTS)
-enum smca_bank_types kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank)
-{
-	return smca_get_bank_type(cpu, bank);
-}
-#elif defined(HAVE_SMCA_GET_BANK_TYPE_WITH_ONE_ARGUMENT)
-enum smca_bank_types kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank)
-{
-        return smca_get_bank_type(bank);
-}
-
-/* Copied from v5.15-rc2-452-gf38ce910d8df:arch/x86/kernel/cpu/mce/amd.c and modified for KCL */
-#elif defined(HAVE_STRUCT_SMCA_BANK)
+#if !defined(HAVE_SMCA_GET_BANK_TYPE_WITH_TWO_ARGUMENTS) && !defined(HAVE_SMCA_GET_BANK_TYPE_WITH_ONE_ARGUMENT)
+#if defined(HAVE_STRUCT_SMCA_BANK)
 enum smca_bank_types smca_get_bank_type(unsigned int bank)
 {
         struct smca_bank *b;
@@ -38,24 +25,14 @@ enum smca_bank_types smca_get_bank_type(unsigned int bank)
 
         return b->hwid->bank_type;
 }
-enum smca_bank_types kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank)
-{
-        return smca_get_bank_type(bank);
-}
-
 #else
 int smca_get_bank_type(unsigned int bank)
 {
-	pr_warn_once("smca_get_bank_type is not supported\n");
-	return 0;
+        pr_warn_once("smca_get_bank_type is not supported\n");
+        return 0;
 }
-
-int kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank)
-{
-        return smca_get_bank_type(bank);
-}
-
-#endif 
-EXPORT_SYMBOL_GPL(kcl_smca_get_bank_type);
+#endif
+EXPORT_SYMBOL_GPL(smca_get_bank_type);
+#endif
 
 #endif /* CONFIG_X86_MCE_AMD */
--- include/kcl/kcl_mce.h
+++ include/kcl/kcl_mce.h
@@ -11,14 +11,28 @@
 #define XEC(x, mask)			(((x) >> 16) & mask)
 #endif
 
-#if defined(HAVE_SMCA_GET_BANK_TYPE_WITH_TWO_ARGUMENTS) || defined(HAVE_SMCA_GET_BANK_TYPE_WITH_ONE_ARGUMENT) || defined(HAVE_STRUCT_SMCA_BANK)
-enum smca_bank_types kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank);
-#else
-int kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank);
-#endif
-
 #ifndef HAVE_MCE_PRIO_UC
 #define MCE_PRIO_UC  MCE_PRIO_SRAO
+#endif
+
+#if !defined(HAVE_SMCA_GET_BANK_TYPE_WITH_TWO_ARGUMENTS)
+#if defined(HAVE_SMCA_GET_BANK_TYPE_WITH_ONE_ARGUMENT) || defined(HAVE_STRUCT_SMCA_BANK)
+#if defined(HAVE_STRUCT_SMCA_BANK)
+enum smca_bank_types smca_get_bank_type(unsigned int bank);
+#endif
+static inline
+enum smca_bank_types _kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank)
+{
+        return smca_get_bank_type(bank);
+}
+#else
+int smca_get_bank_type(unsigned int bank);
+static inline
+int _kcl_smca_get_bank_type(unsigned int cpu, unsigned int bank)
+{
+        return smca_get_bank_type(bank);
+}
+#endif
 #endif
 
 #endif /* CONFIG_X86_MCE_AMD */
