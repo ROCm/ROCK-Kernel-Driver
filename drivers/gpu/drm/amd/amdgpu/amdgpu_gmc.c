80aa9f20b9bc7ec8d07ef65e85c4d6d2c97572b4
Yunxiang Li <Yunxiang.Li@amd.com>
2024-06-11 07:01:05 -0400

    Revert "Revert "drm/amdgpu: rework lock handling for flush_tlb v2""
    This reverts commit d409c203149a0257d2c26424926c880efee61cd4.
    
    The commit is a partial revert that left things broken, also this was
    never ported back to drm-next.
    This revert is needed by patch series
    https://gerrit-git.amd.com/c/brahma/ec/linux/+/1068977

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gmc.c
@@ -623,6 +623,13 @@ void amdgpu_gmc_flush_gpu_tlb(struct amdgpu_device *adev, uint32_t vmid,
 	    !adev->mman.buffer_funcs_enabled || !adev->ib_pool_ready ||
 	    !ring->sched.ready) {
 
+		/*
+		 * A GPU reset should flush all TLBs anyway, so no need to do
+		 * this while one is ongoing.
+		 */
+		if (!down_read_trylock(&adev->reset_domain->sem))
+			return;
+
 		if (adev->gmc.flush_tlb_needs_extra_type_2)
 			adev->gmc.gmc_funcs->flush_gpu_tlb(adev, vmid,
 							   vmhub, 2);
@@ -633,6 +640,7 @@ void amdgpu_gmc_flush_gpu_tlb(struct amdgpu_device *adev, uint32_t vmid,
 
 		adev->gmc.gmc_funcs->flush_gpu_tlb(adev, vmid, vmhub,
 						   flush_type);
+		up_read(&adev->reset_domain->sem);
 		return;
 	}
 
