17dcf4569f785a820ea01c0854975c1a36b44099
Lijo Lazar <lijo.lazar@amd.com>
2024-06-25 16:35:36 +0530

    drm/amd/pm: Add debugfs controls for phase detect
    Add debugfs nodes for enabling/disabling and tuning parameters used in
    phase detect.
    
    Signed-off-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>

--- drivers/gpu/drm/amd/pm/amdgpu_pm.c
+++ drivers/gpu/drm/amd/pm/amdgpu_pm.c
@@ -4864,5 +4864,6 @@ void amdgpu_debugfs_pm_init(struct amdgpu_device *adev)
 					 adev->pm.smu_prv_buffer_size);
 
 	amdgpu_dpm_stb_debug_fs_init(adev);
+	amdgpu_dpm_phase_det_debugfs_init(adev);
 #endif
 }
