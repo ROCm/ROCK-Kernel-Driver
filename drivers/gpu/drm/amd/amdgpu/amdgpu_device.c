a0ef5f1f8d12ff82ffed5718738cbf8f275959d0
Alex Deucher <alexander.deucher@amd.com>
2024-07-12 16:37:33 -0400

    drm/amdgpu/gfx11: add a mutex for the gfx semaphore
    This will be used in more places in the future so
    add a mutex.
    
    Acked-by: Vitaly Prosyak <vitaly.prosyak@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4076,6 +4076,7 @@ int amdgpu_device_init(struct amdgpu_device *adev,
 #endif
 	mutex_init(&adev->pm.stable_pstate_ctx_lock);
 	mutex_init(&adev->benchmark_mutex);
+	mutex_init(&adev->gfx.reset_sem_mutex);
 
 	amdgpu_device_init_apu_flags(adev);
 
