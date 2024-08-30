aee958c4fbafa906b231ef015f42f7e139f2b03b
Asher Song <Asher.Song@amd.com>
2024-08-30 08:58:00 +0800

    drm/amdgpu: fix a call trace when unload amdgpu driver
    Before release ttm_resource_manager, ttm_bo_delay_delete in workqueue
    is required to execute.
    
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -2461,6 +2461,7 @@ void amdgpu_ttm_fini(struct amdgpu_device *adev)
 		drm_dev_exit(idx);
 	}
 
+	drain_workqueue(adev->mman.bdev.wq);
 	amdgpu_direct_gma_fini(adev);
 	amdgpu_vram_mgr_fini(adev);
 	amdgpu_gtt_mgr_fini(adev);
