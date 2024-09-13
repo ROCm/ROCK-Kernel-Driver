f0e8d7f2fb8e97ecc4aa5a1e49a00c15dfd473e2
Mario Limonciello <mario.limonciello@amd.com>
2024-09-13 13:00:39 -0500

    drm/amd/display: Validate backlight caps are sane
    Currently amdgpu takes backlight caps provided by the ACPI tables
    on systems as is.  If the firmware sets maximums that are too low
    this means that users don't get a good experience.
    
    To avoid having to maintain a quirk list of such systems, do a sanity
    check on the values.  Check that the spread is at least half of the
    values that amdgpu would use if no ACPI table was found and if not
    use the amdgpu defaults.
    
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/3020
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -4517,6 +4517,7 @@ static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 
 #define AMDGPU_DM_DEFAULT_MIN_BACKLIGHT 12
 #define AMDGPU_DM_DEFAULT_MAX_BACKLIGHT 255
+#define AMDGPU_DM_MIN_SPREAD ((AMDGPU_DM_DEFAULT_MAX_BACKLIGHT - AMDGPU_DM_DEFAULT_MIN_BACKLIGHT) / 2)
 #ifdef HAVE_HDR_SINK_METADATA
 #define AUX_BL_DEFAULT_TRANSITION_TIME_MS 50
 #endif
@@ -4533,6 +4534,21 @@ static void amdgpu_dm_update_backlight_caps(struct amdgpu_display_manager *dm,
 		return;
 
 	amdgpu_acpi_get_backlight_caps(&caps);
+
+	/* validate the firmware value is sane */
+	if (caps.caps_valid) {
+		int spread = caps.max_input_signal - caps.min_input_signal;
+
+		if (caps.max_input_signal > AMDGPU_DM_DEFAULT_MAX_BACKLIGHT ||
+		    caps.min_input_signal < AMDGPU_DM_DEFAULT_MIN_BACKLIGHT ||
+		    spread > AMDGPU_DM_DEFAULT_MAX_BACKLIGHT ||
+		    spread < AMDGPU_DM_MIN_SPREAD) {
+			DRM_DEBUG_KMS("DM: Invalid backlight caps: min=%d, max=%d\n",
+				      caps.min_input_signal, caps.max_input_signal);
+			caps.caps_valid = false;
+		}
+	}
+
 	if (caps.caps_valid) {
 		dm->backlight_caps[bl_idx].caps_valid = true;
 #ifdef HAVE_HDR_SINK_METADATA
