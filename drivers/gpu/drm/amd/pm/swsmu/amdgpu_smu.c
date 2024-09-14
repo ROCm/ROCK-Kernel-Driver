67553cd5e5c674b148e904451992d9147aec01de
Le Ma <le.ma@amd.com>
2024-09-14 19:35:32 +0800

    Revert "drm/amdgpu/swsmu: always force a state reprogram on init"
    This reverts commit 63009805b6c5ce27d86f4f5cff8d9bbc779b4146.
    
    This patch inserts extra SMU messages to set dpm level during
    intialization and will cause SMU msg timeout on some platforms.
    
    Remove this patch until issue gets fixed.

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -2224,9 +2224,8 @@ static int smu_bump_power_profile_mode(struct smu_context *smu,
 }
 
 static int smu_adjust_power_state_dynamic(struct smu_context *smu,
-					  enum amd_dpm_forced_level level,
-					  bool skip_display_settings,
-					  bool force_update)
+				   enum amd_dpm_forced_level level,
+				   bool skip_display_settings)
 {
 	int ret = 0;
 	int index = 0;
@@ -2255,7 +2254,7 @@ static int smu_adjust_power_state_dynamic(struct smu_context *smu,
 		}
 	}
 
-	if (force_update || smu_dpm_ctx->dpm_level != level) {
+	if (smu_dpm_ctx->dpm_level != level) {
 		ret = smu_asic_set_performance_level(smu, level);
 		if (ret) {
 			dev_err(smu->adev->dev, "Failed to set performance level!");
@@ -2271,7 +2270,7 @@ static int smu_adjust_power_state_dynamic(struct smu_context *smu,
 		index = index > 0 && index <= WORKLOAD_POLICY_MAX ? index - 1 : 0;
 		workload[0] = smu->workload_setting[index];
 
-		if (force_update || smu->power_profile_mode != workload[0])
+		if (smu->power_profile_mode != workload[0])
 			smu_bump_power_profile_mode(smu, workload, 0);
 	}
 
@@ -2292,13 +2291,11 @@ static int smu_handle_task(struct smu_context *smu,
 		ret = smu_pre_display_config_changed(smu);
 		if (ret)
 			return ret;
-		ret = smu_adjust_power_state_dynamic(smu, level, false, false);
+		ret = smu_adjust_power_state_dynamic(smu, level, false);
 		break;
 	case AMD_PP_TASK_COMPLETE_INIT:
-		ret = smu_adjust_power_state_dynamic(smu, level, true, true);
-		break;
 	case AMD_PP_TASK_READJUST_POWER_STATE:
-		ret = smu_adjust_power_state_dynamic(smu, level, true, false);
+		ret = smu_adjust_power_state_dynamic(smu, level, true);
 		break;
 	default:
 		break;
