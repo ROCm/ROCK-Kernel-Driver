a5898e743754c257f07151087c5eea7641a91b8f
Sunil Khatri <sunil.khatri@amd.com>
2024-09-26 13:29:24 +0530

    drm/amdgpu: update the handle ptr in late_init
    Update the ptr handle to amdgpu_ip_block ptr in all
    the functions of late_init function ptr.
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    (cherry picked from commit 813dabea1d31e017d948bc93bde568aff870c00d)

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -826,9 +826,9 @@ static int smu_apply_default_config_table_settings(struct smu_context *smu)
 	return smu_set_config_table(smu, &adev->pm.config_table);
 }
 
-static int smu_late_init(void *handle)
+static int smu_late_init(struct amdgpu_ip_block *ip_block)
 {
-	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
+	struct amdgpu_device *adev = ip_block->adev;
 	struct smu_context *smu = adev->powerplay.pp_handle;
 	int ret = 0;
 
@@ -2068,7 +2068,11 @@ static int smu_reset(struct smu_context *smu)
 	if (ret)
 		return ret;
 
+<<<<<<< HEAD
 	ret = smu_late_init(ip_block);
+=======
+	ret = smu_late_init(&adev->ip_blocks[AMD_IP_BLOCK_TYPE_SMC]);
+>>>>>>> 813dabea1d31 (drm/amdgpu: update the handle ptr in late_init)
 	if (ret)
 		return ret;
 
