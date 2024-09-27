f6ed1db282126b88d469af92629321b277b00443
Jiadong Zhu <Jiadong.Zhu@amd.com>
2024-09-27 17:49:17 +0800

    drm/amd/pm: update smu_v13_0_6 smu header
    update smu header for sdma soft reset.
    
    Signed-off-by: Jiadong Zhu <Jiadong.Zhu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/inc/pmfw_if/smu_v13_0_6_ppsmc.h
+++ drivers/gpu/drm/amd/pm/swsmu/inc/pmfw_if/smu_v13_0_6_ppsmc.h
@@ -101,6 +101,8 @@
 #define PPSMC_MSG_SetPhsDetOnOff                    0x4A
 #define PPSMC_MSG_GetPhsDetResidency                0x4B
 #define PPSMC_Message_Count                         0x4C
+#define PPSMC_MSG_ResetSDMA                         0x4D
+#define PPSMC_Message_Count                         0x4E
 
 //PPSMC Reset Types for driver msg argument
 #define PPSMC_RESET_TYPE_DRIVER_MODE_1_RESET        0x1
