29e8da0f11a70abbb38e0208cdf312e763deb525
Boyuan Zhang <boyuan.zhang@amd.com>
2024-10-01 23:09:13 -0400

    drm/amdgpu: power vcn 5_0_0 by instance
    For vcn 5_0_0, add ip_block for each vcn instance during discovery stage.
    
    And only powering on/off one of the vcn instance using the
    instance value stored in ip_block, instead of powering on/off all
    vcn instances. Modify the existing functions to use the instance value
    in ip_block, and remove the original for loop for all vcn instances.
    
    v2: rename "i"/"j" to "inst" for instance value.
    
    Signed-off-by: Boyuan Zhang <boyuan.zhang@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -2366,7 +2366,8 @@ static int amdgpu_discovery_set_mm_ip_blocks(struct amdgpu_device *adev)
 			amdgpu_device_ip_block_add(adev, &jpeg_v4_0_5_ip_block);
 			break;
 		case IP_VERSION(5, 0, 0):
-			amdgpu_device_ip_block_add(adev, &vcn_v5_0_0_ip_block);
+			for (i = 0; i < adev->vcn.num_vcn_inst; ++i)
+				amdgpu_device_ip_block_add(adev, &vcn_v5_0_0_ip_block);
 			amdgpu_device_ip_block_add(adev, &jpeg_v5_0_0_ip_block);
 			break;
 		default:
