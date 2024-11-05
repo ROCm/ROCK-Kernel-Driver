86a5965d3b3057d36434498673f10ae327ebd388
chengjya <chengjya@amd.com>
2024-11-05 11:29:58 +0800

    drm/amdkcl: wrap code under DEFINE_DEBUGFS_ATTRIBUTE
    It's caused by following commit: b8521b1a8e7d0b
    "drm/amdgpu: add amdgpu_gfx_sched_mask and amdgpu_compute_sched_mask debugfs"
    
    Signed-off-by: chengjya <chengjya@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gfx.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gfx.c
@@ -2085,9 +2085,15 @@ static int amdgpu_debugfs_gfx_sched_mask_get(void *data, u64 *val)
 	return 0;
 }
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 DEFINE_DEBUGFS_ATTRIBUTE(amdgpu_debugfs_gfx_sched_mask_fops,
 			 amdgpu_debugfs_gfx_sched_mask_get,
 			 amdgpu_debugfs_gfx_sched_mask_set, "%llx\n");
+#else
+DEFINE_SIMPLE_ATTRIBUTE(amdgpu_debugfs_gfx_sched_mask_fops,
+			 amdgpu_debugfs_gfx_sched_mask_get,
+			 amdgpu_debugfs_gfx_sched_mask_set, "%llx\n");
+#endif
 
 #endif
 
@@ -2156,9 +2162,15 @@ static int amdgpu_debugfs_compute_sched_mask_get(void *data, u64 *val)
 	return 0;
 }
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 DEFINE_DEBUGFS_ATTRIBUTE(amdgpu_debugfs_compute_sched_mask_fops,
 			 amdgpu_debugfs_compute_sched_mask_get,
 			 amdgpu_debugfs_compute_sched_mask_set, "%llx\n");
+#else
+DEFINE_SIMPLE_ATTRIBUTE(amdgpu_debugfs_compute_sched_mask_fops,
+			 amdgpu_debugfs_compute_sched_mask_get,
+			 amdgpu_debugfs_compute_sched_mask_set, "%llx\n");
+#endif
 
 #endif
 
