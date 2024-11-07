c1f71dfae0e5ddbe6787ab41cfd04d3f647ee16f
Flora Cui <flora.cui@amd.com>
2024-11-07 14:40:44 +0800

    drm/amdkcl: use kprobe for address resolution of unexported functions
    Replaces the usage of kallsyms_lookup_name with kprobe to obtain the
    addresses of kernel functions that are not exported.
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_common.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_common.c
@@ -5,7 +5,26 @@
 #include <linux/printk.h>
 #include <linux/bug.h>
 
-unsigned long (*_kcl_kallsyms_lookup_name)(const char *name);
+static unsigned long _kcl_kallsyms_lookup_name(const char *name)
+{
+	unsigned long addr = 0;
+#ifndef HAVE_KALLSYMS_LOOKUP_NAME
+	struct kprobe kp;
+	int r;
+
+	memset(&kp, 0, sizeof(kp));
+	kp.symbol_name = name;
+	r = register_kprobe(&kp);
+	if (!r) {
+		addr = (unsigned long)kp.addr;
+		unregister_kprobe(&kp);
+	}
+#else
+	addr = kallsyms_lookup_name(name);
+#endif
+
+	return addr;
+}
 
 void *amdkcl_fp_setup(const char *symbol, void *dummy)
 {
@@ -27,23 +46,3 @@ void *amdkcl_fp_setup(const char *symbol, void *dummy)
        return fp;
 }
 
-void amdkcl_symbol_init(void)
-{
-#ifndef HAVE_KALLSYMS_LOOKUP_NAME
-	struct kprobe kp;
-	int r;
-
-	memset(&kp, 0, sizeof(kp));
-	kp.symbol_name = "kallsyms_lookup_name";
-	r = register_kprobe(&kp);
-	if (!r) {
-		_kcl_kallsyms_lookup_name = (void *)kp.addr;
-		unregister_kprobe(&kp);
-	} else {
-		pr_err("fail to get kallsyms_lookup_name, abort...\n");
-		BUG();
-	}
-#else
-	_kcl_kallsyms_lookup_name = kallsyms_lookup_name;
-#endif
-}
