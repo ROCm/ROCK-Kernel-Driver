8951e97e1caf2d11d0b2df157d8c06ee1526ccf9
Asher Song <Asher.Song@amd.com>
2024-11-13 14:46:43 +0800

    drm/amdkcl: test follow_pfnmap_start whether exist
    It's caused by v6.11-rc6-389-g6da8e9634bb7
     mm: new follow_pfnmap API
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/backport/kcl_memory.c
+++ drivers/gpu/drm/amd/backport/kcl_memory.c
@@ -5,14 +5,27 @@ int _kcl_follow_pfn(struct vm_area_struct *vma, unsigned long address,
         unsigned long *pfn)
 {
         int ret = -EINVAL;
+#ifdef HAVE_FOLLOW_PFNMAP_START
+        struct follow_pfnmap_args args = {
+                .vma = vma,
+                .address = address,
+        };
+        ret = follow_pfnmap_start(&args);
+
+        if (ret)
+                return ret;
+        *pfn = args.pfn;
+        follow_pfnmap_end(&args);
+#else
         spinlock_t *ptl;
         pte_t *ptep;
-
         ret = follow_pte(vma, address, &ptep, &ptl);
+
         if (ret)
                 return ret;
         *pfn = pte_pfn(ptep_get(ptep));
         pte_unmap_unlock(ptep, ptl);
+#endif
         return 0;
 }
 
--- drivers/gpu/drm/amd/dkms/m4/follow_pfn.m4
+++ drivers/gpu/drm/amd/dkms/m4/follow_pfn.m4
@@ -14,3 +14,21 @@ AC_DEFUN([AC_AMDGPU_FOLLOW_PFN], [
 		])
 	])
 ])
+
+dnl #
+dnl # v6.11-rc6-389-g6da8e9634bb7
+dnl # mm: new follow_pfnmap API
+dnl #
+AC_DEFUN([AC_AMDGPU_FOLLOW_PFNMAP_START], [
+    AC_KERNEL_DO_BACKGROUND([
+        AC_KERNEL_TRY_COMPILE_SYMBOL([
+            #include <linux/mm.h>
+        ],[
+            follow_pfnmap_start(NULL);
+        ],[follow_pfnmap_start], [mm/memory.c],[
+            AC_DEFINE(HAVE_FOLLOW_PFNMAP_START, 1,
+                [follow_pfnmap_start() is available])
+        ])
+    ])
+])
+
