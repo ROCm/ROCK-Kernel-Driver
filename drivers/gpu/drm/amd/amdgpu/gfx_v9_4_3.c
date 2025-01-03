d02310fb4a628024993d91b57bc3a45862100853
Bokun Zhang <bokun.zhang@amd.com>
2025-01-03 11:30:56 -0600

    drm/amdgpu: Fix for MEC SJT FW Load Fail on VF
    Users might switch to ROCM build does not include MEC SJT FW and driver
    needs to consider this case.w
    
    Signed-off-by: yfeng1 <yfeng1@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    
    Signed-off-by: Bokun Zhang <bokun.zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v9_4_3.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v9_4_3.c
@@ -576,10 +576,16 @@ static int gfx_v9_4_3_init_cp_compute_microcode(struct amdgpu_device *adev,
 {
 	int err;
 
-	if (amdgpu_sriov_vf(adev))
+	if (amdgpu_sriov_vf(adev)) {
 		err = amdgpu_ucode_request(adev, &adev->gfx.mec_fw,
-				"amdgpu/%s_sjt_mec.bin", chip_name);
-	else
+					   AMDGPU_UCODE_REQUIRED,
+					   "amdgpu/%s_sjt_mec.bin", chip_name);
+
+		if (err)
+			err = amdgpu_ucode_request(adev, &adev->gfx.mec_fw,
+							AMDGPU_UCODE_REQUIRED,
+							"amdgpu/%s_mec.bin", chip_name);
+	} else
 		err = amdgpu_ucode_request(adev, &adev->gfx.mec_fw,
 				"amdgpu/%s_mec.bin", chip_name);
 	if (err)
