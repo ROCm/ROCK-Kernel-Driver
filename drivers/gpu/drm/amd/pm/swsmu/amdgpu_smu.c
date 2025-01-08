f0b3d8a06d6b8a297109560c0be4e632b6cb35d2
Alex Deucher <alexander.deucher@amd.com>
2025-01-08 16:11:42 -0500

    drm/amdgpu/swsmu: set workload profile to bootup default
    Now that we can select a workload profile dynamically when
    we submit work, it's best to default to the bootup
    default workload profile.  Defaulting to other profiles
    prevents some power management features from kicking in
    during idle periods.  Once all jobs have finished, the
    workload profile will automatically move back to default
    bootup for max power savings.
    
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -1248,28 +1248,12 @@ static void smu_init_xgmi_plpd_mode(struct smu_context *smu)
 	}
 }
 
-static bool smu_is_workload_profile_available(struct smu_context *smu,
-					      u32 profile)
-{
-	if (profile >= PP_SMC_POWER_PROFILE_COUNT)
-		return false;
-	return smu->workload_map && smu->workload_map[profile].valid_mapping;
-}
-
 static void smu_init_power_profile(struct smu_context *smu)
 {
-	if (smu->power_profile_mode == PP_SMC_POWER_PROFILE_UNKNOWN) {
-		if (smu->is_apu ||
-		    !smu_is_workload_profile_available(
-			    smu, PP_SMC_POWER_PROFILE_FULLSCREEN3D))
-			smu->workload_mask =
-				1 << smu->workload_prority[PP_SMC_POWER_PROFILE_BOOTUP_DEFAULT];
-		else
-			smu->workload_mask =
-				1 << smu->workload_prority[PP_SMC_POWER_PROFILE_FULLSCREEN3D];
-	}
+	if (smu->power_profile_mode == PP_SMC_POWER_PROFILE_UNKNOWN)
+		smu->power_profile_mode =
+			PP_SMC_POWER_PROFILE_BOOTUP_DEFAULT;
 
-	smu->power_profile_mode = PP_SMC_POWER_PROFILE_BOOTUP_DEFAULT;
 }
 
 static int smu_sw_init(struct amdgpu_ip_block *ip_block)
