4d5baab6206a374db7979df3c41366f8c82ec279
Tony Yi <Tony.Yi@amd.com>
2025-02-26 17:03:10 -0500

    drm/amdgpu: Add support for CPERs on virtualization
    Add support for CPERs on VFs.
    
    VFs do not receive PMFW messages directly; as such, they need to
    query them from the host. To avoid hitting host event guard,
    CPER queries need to be rate limited. CPER queries share the same
    RAS telemetry buffer as error count query, so a mutex protecting
    the shared buffer was added as well.
    
    For readability, the amdgpu_detect_virtualization was refactored
    into multiple individual functions.
    
    Signed-off-by: Tony Yi <Tony.Yi@amd.com>
    Reviewed-by: Tao Zhou <tao.zhou1@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -3110,7 +3110,8 @@ static int amdgpu_device_ip_init(struct amdgpu_device *adev)
 
 	amdgpu_fru_get_product_info(adev);
 
-	r = amdgpu_cper_init(adev);
+	if (!amdgpu_sriov_vf(adev) || amdgpu_sriov_ras_cper_en(adev))
+		r = amdgpu_cper_init(adev);
 
 init_failed:
 
@@ -4351,11 +4352,9 @@ int amdgpu_device_init(struct amdgpu_device *adev,
 	 * for throttling interrupt) = 60 seconds.
 	 */
 	ratelimit_state_init(&adev->throttling_logging_rs, (60 - 1) * HZ, 1);
-	ratelimit_state_init(&adev->virt.ras_telemetry_rs, 5 * HZ, 1);
 
 #ifdef RATELIMIT_MSG_ON_RELEASE
 	ratelimit_set_flags(&adev->throttling_logging_rs, RATELIMIT_MSG_ON_RELEASE);
-	ratelimit_set_flags(&adev->virt.ras_telemetry_rs, RATELIMIT_MSG_ON_RELEASE);
 #endif
 
 	/* Registers mapping */
@@ -4388,7 +4387,7 @@ int amdgpu_device_init(struct amdgpu_device *adev,
 		return -ENOMEM;
 
 	/* detect hw virtualization here */
-	amdgpu_detect_virtualization(adev);
+	amdgpu_virt_init(adev);
 
 	amdgpu_device_get_pcie_info(adev);
 
